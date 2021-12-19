96257fcad085f6f9124b00a0b26219577777e025
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2021-12-19 22:37:46 +0200

    Update DIA_Ass_130_CorGalomDemon.d

--- RU-Main/Story/Dialoge/DIA_Ass_130_CorGalomDemon.d
+++ RU-Main/Story/Dialoge/DIA_Ass_130_CorGalomDemon.d
@@ -1,7 +1,7 @@
 instance DIA_Ass_130_CorGalomDemon_EXIT(C_Info)
 {
 	npc = Ass_130_CorGalomDemon;
-	nr = 999;
+	No. = 999 ;
 	condition = DIA_Ass_130_CorGalomDemon_exit_condition;
 	information = DIA_Ass_130_CorGalomDemon_exit_info;
 	permanent = TRUE;
@@ -21,10 +21,10 @@ func void DIA_Ass_130_CorGalomDemon_exit_info()
 instance DIA_Ass_130_CorGalomDemon_Hello(C_Info)
 {
 	npc = Ass_130_CorGalomDemon;
-	nr = 1;
+	no. = 1 ;
 	condition = DIA_Ass_130_CorGalomDemon_hello_condition;
 	information = DIA_Ass_130_CorGalomDemon_hello_info;
-	permanent = FALSE;
+	permanent = FALSE ;
 	important= TRUE;
 };
 	
@@ -43,48 +43,48 @@ func void DIA_Ass_130_CorGalomDemon_hello_info()
 		Npc_RemoveInvItems(other,ItKe_Masiaf_Prision,1);
 	};
 
-	AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_00");	//А вот и наш герой!
-	AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_01");	//Признаться, я с нетерпением ждал твоего появления c момента последней нашей встречи.
-	AI_Output(other,self,"DIA_Ass_130_CorGalomDemon_hello_01_02");	//Разве мы знакомы?
-	AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_03");	//Конечно. Или ты уже позабыл, что произошло в храме Спящего?
-	AI_Output(other,self,"DIA_Ass_130_CorGalomDemon_hello_01_04");	//Ммм... Да, твое лицо мне кажется знакомым. Неужели Кор-Галом?
-	AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_05");	//А я уж думал, ты никогда не догадаешься.
-	AI_Output(other,self,"DIA_Ass_130_CorGalomDemon_hello_01_06");	//Но я же тогда убил тебя там, в храме!
-	AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_07");	//Так оно и есть. Однако какая-то неизвестная мне темная магия вернула меня обратно к жизни.
-	AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_08");	//И, видимо, мой хозяин решил, что я буду более полезен в виде демона, нежели в роли обычного мага-скелета.
+	AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_00");	//Ah, there you are!
+	AI_Output (self, other, " DIA_Ass_130_CorGalomDemon_hello_01_01 " );	// Frankly, I've been looking forward to your arrival since our last meeting.
+	AI_Output (other, self, " DIA_Ass_130_CorGalomDemon_hello_01_02 " );	// Do we know each other?
+	AI_Output (self, other, " DIA_Ass_130_CorGalomDemon_hello_01_03 " );	// Of course. Or have you already forgotten what happened in the Temple of the Sleeper?
+	AI_Output (other, self, " DIA_Ass_130_CorGalomDemon_hello_01_04 " );	// Mmm ... Yes, your face looks familiar to me. Is it Kor-Galom?
+	AI_Output (self, other, " DIA_Ass_130_CorGalomDemon_hello_01_05 " );	// I thought you'd never guess.
+	AI_Output (other, self, " DIA_Ass_130_CorGalomDemon_hello_01_06 " );	// But I killed you there, in the temple!
+	AI_Output (self, other, " DIA_Ass_130_CorGalomDemon_hello_01_07 " );	// Yes, you did. However, some dark magic unknown to me brought me back to life.
+	AI_Output (self, other, " DIA_Ass_130_CorGalomDemon_hello_01_08 " );	// And, apparently, my master decided that I would be more useful as a demon than as a Lich.
 
 	if(MeetDarkTempleMage == TRUE)
 	{
-		AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_09");	//Кстати, ты уже ведь с ним встречался.
-		AI_Output(other,self,"DIA_Ass_130_CorGalomDemon_hello_01_10");	//Если ты имеешь в виду того некроманта в храме - то да.
+		AI_Output (self, other, " DIA_Ass_130_CorGalomDemon_hello_01_09 " );	// By the way, you've already met him.
+		AI_Output (other, self, " DIA_Ass_130_CorGalomDemon_hello_01_10 " );	// If you mean that necromancer in the temple, then yes.
 
 		if(Npc_IsDead(dmt_1298_darkmage) == TRUE)
 		{
 
-			AI_Output(other,self,"DIA_Ass_130_CorGalomDemon_hello_01_11");	//Правда, его магия не смогла спасти его от смерти.
+			AI_Output (other, self, " DIA_Ass_130_CorGalomDemon_hello_01_11 " );	// True, his magic could not save him from death.
 		};
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_12");	//Кстати, я уверен, что ты с ним еще встретишься.
-		AI_Output(other,self,"DIA_Ass_130_CorGalomDemon_hello_01_13");	//Значит, придется убить и его.
+		AI_Output (self, other, " DIA_Ass_130_CorGalomDemon_hello_01_12 " );	// I'm sure you'll see him again, however.
+		AI_Output (other, self, " DIA_Ass_130_CorGalomDemon_hello_01_13 " );	// You might have to kill him again.
 	};
 
-	AI_Output(other,self,"DIA_Ass_130_CorGalomDemon_hello_01_14");	//Но лучше ответь мне на другой вопрос.
-	AI_Output(other,self,"DIA_Ass_130_CorGalomDemon_hello_01_15");	//Зачем тебе понадобились послушники Братства?
-	AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_16");	//Это легко объяснить. Ты ведь всегда знал про мою страсть к различным исследованиям.
-	AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_17");	//А для них, как ты понимаешь, всегда необходимы некоторые расходные материалы.
-	AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_18");	//И Ханиар Слепец с его Тенями вполне сгодились для того, чтобы регулярно снабжать меня ими.
-	AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_19");	//В обмен на его услуги я даровал ему свое покровительство. Ведь он всегда жаждал огромного могущества.
-	AI_Output(other,self,"DIA_Ass_130_CorGalomDemon_hello_01_20");	//Но ему это не очень помогло.
-	AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_21");	//(мрачно) Я уже догадался, что ты убил его. Как и моего слугу Небироса.
-	AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_22");	//Кстати, он как раз был одним из творений моих исследований.
-	AI_Output(other,self,"DIA_Ass_130_CorGalomDemon_hello_01_23");	//Я уже по достоинству оценил твой труд. И отправил его туда, где ему самое место!
-	AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_24");	//Хммм. Похоже, что на твоем пути тебя ничто и никто не сможет остановить.
-	AI_Output(other,self,"DIA_Ass_130_CorGalomDemon_hello_01_25");	//Ты можешь попробовать!
-	AI_Output(self,other,"DIA_Ass_130_CorGalomDemon_hello_01_26");	//Именно это я и собираюсь сейчас сделать. Я ведь уже говорил, что давно ждал этого момента!
+	AI_Output (other, self, " DIA_Ass_130_CorGalomDemon_hello_01_14 " );	// Might you satisfy my curiosity?
+	AI_Output (other, self, " DIA_Ass_130_CorGalomDemon_hello_01_15 " );	// Why did you need the Brotherhood acolytes?
+	AI_Output (self, other, " DIA_Ass_130_CorGalomDemon_hello_01_16 " );	// This is easy to explain. As you know, I am a consummate student, ever learning.
+	AI_Output (self, other, " DIA_Ass_130_CorGalomDemon_hello_01_17 " );	// And I am always in need of fresh materials to further my work.
+	AI_Output (self, other, " DIA_Ass_130_CorGalomDemon_hello_01_18 " );	// Haniar the Blind and his Shadows were quite useful for supplying me with them on a regular basis.
+	AI_Output (self, other, " DIA_Ass_130_CorGalomDemon_hello_01_19 " );	// In exchange for his services, I bestowed my favor on him. After all, he always craved power.
+	AI_Output (other, self, " DIA_Ass_130_CorGalomDemon_hello_01_20 " );	// Too bad it wasn't enough.
+	AI_Output (self, other, " DIA_Ass_130_CorGalomDemon_hello_01_21 " );	// (grimly) Yes, you killed him. As well as my servant Nebiros.
+	AI_Output (self, other, " DIA_Ass_130_CorGalomDemon_hello_01_22 " );	// He was simply a creation of my research. A trifling thing to be discarded.
+	AI_Output (other, self, " DIA_Ass_130_CorGalomDemon_hello_01_23 " );	// Oh, I discarded him all right.
+	AI_Output (self, other, " DIA_Ass_130_CorGalomDemon_hello_01_24 " );	// You think yourself unstoppable, do you? My equal?
+	AI_Output (other, self, " DIA_Ass_130_CorGalomDemon_hello_01_25 " );	// We will find out.
+	AI_Output (self, other, " DIA_Ass_130_CorGalomDemon_hello_01_26 " );	// I thank you, destined one. I have been waiting for this moment for a long time!
 	Info_ClearChoices(DIA_Ass_130_CorGalomDemon_hello);
-	Info_AddChoice(DIA_Ass_130_CorGalomDemon_hello,Dialog_Ende,DIA_Ass_130_CorGalomDemon_hello_Exit);
+	Info_AddChoice (DIA_Ass_130_CorGalomDemon_hello, Dialog_Ende, DIA_Ass_130_CorGalomDemon_hello_Exit);
 };
 
 func void DIA_Ass_130_CorGalomDemon_hello_Exit()
@@ -92,5 +92,5 @@ func void DIA_Ass_130_CorGalomDemon_hello_Exit()
 	CorGalomDontSpeak = TRUE;
 	AI_StopProcessInfos(self);
 	B_Attack(self,other,AR_KILL,0);
-	self.aivar[AIV_EnemyOverride] = FALSE;
+	self.aivar [AIV_EnemyOverride] = FALSE ;
 };	
