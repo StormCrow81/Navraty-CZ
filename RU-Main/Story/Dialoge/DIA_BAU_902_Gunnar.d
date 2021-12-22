5623245bc49f28bd10862f78010359c21efc5abc
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2021-12-22 21:00:28 +0200

    Update DIA_BAU_902_Gunnar.d

--- RU-Main/Story/Dialoge/DIA_BAU_902_Gunnar.d
+++ RU-Main/Story/Dialoge/DIA_BAU_902_Gunnar.d
@@ -1,8 +1,7 @@
-
 instance DIA_Gunnar_EXIT(C_Info)
 {
 	npc = BAU_902_Gunnar;
-	nr = 999;
+	No. = 999 ;
 	condition = DIA_Gunnar_EXIT_Condition;
 	information = DIA_Gunnar_EXIT_Info;
 	permanent = TRUE;
@@ -23,10 +22,10 @@ func void DIA_Gunnar_EXIT_Info()
 
 var int Gunnar_HalloGesagt;
 
-instance DIA_Gunnar_Hallo(C_Info)
+instance DIA_Gunnar_Hallo (C_Info)
 {
 	npc = BAU_902_Gunnar;
-	nr = 1;
+	no. = 1 ;
 	condition = DIA_Gunnar_Hallo_Condition;
 	information = DIA_Gunnar_Hallo_Info;
 	permanent = TRUE;
@@ -34,43 +33,43 @@ instance DIA_Gunnar_Hallo(C_Info)
 };
 
 
-func int DIA_Gunnar_Hallo_Condition()
+func int DIA_Gunnar_Hallo_Condition ()
 {
 	return TRUE;
 };
 
-func void DIA_Gunnar_Hallo_Info()
+func void DIA_Gunnar_Hallo_Info ()
 {
-	AI_Output(other,self,"DIA_Gunnar_Hallo_15_00");	//Как дела?
+	AI_Output(other,self,"DIA_Gunnar_Hallo_15_00");	//How are you?
 	if(other.guild == GIL_NONE)
 	{
-		AI_Output(self,other,"DIA_Gunnar_Hallo_10_01");	//С тех пор, как Онар объявил, что набирает наемников, здесь все кишит бандитами.
-		AI_Output(self,other,"DIA_Gunnar_Hallo_10_02");	//Весь сброд, у которого раньше не хватало мужества выйти из леса, теперь собрался здесь.
-		AI_Output(self,other,"DIA_Gunnar_Hallo_10_03");	//Эти ублюдки приходят отовсюду...
+		AI_Output (self, other, " DIA_Gunnar_Hallo_10_01 " );	// Ever since Onar announced that he was recruiting mercenaries, this place has been swarming with bandits.
+		AI_Output (self, other, " DIA_Gunnar_Hallo_10_02 " );	// All the rabble that previously did not have the courage to leave the forest are now coming here.
+		AI_Output (self, other, " DIA_Gunnar_Hallo_10_03 " );	// They're worse than rats leaving a sinking ship...
 	};
 	if((other.guild == GIL_SLD) || (other.guild == GIL_DJG))
 	{
-		AI_Output(self,other,"DIA_Gunnar_Hallo_10_04");	//Так ты теперь тоже наемник, хм?
+		AI_Output (self, other, " DIA_Gunnar_Hallo_10_04 " );	// So you're a mercenary too now, hmm?
 	};
 	if((other.guild == GIL_NOV) || (other.guild == GIL_KDF))
 	{
-		AI_Output(self,other,"DIA_Gunnar_Hallo_10_05");	//Да пребудет с тобой Иннос!
+		AI_Output (self, other, " DIA_Gunnar_Hallo_10_05 " );	// May Innos be with you!
 	};
 	if((other.guild == GIL_MIL) || (other.guild == GIL_PAL))
 	{
-		AI_Output(self,other,"DIA_Gunnar_Hallo_10_06");	//Тут не рады людям из города.
+		AI_Output (self, other, " DIA_Gunnar_Hallo_10_06 " );	// People from the city are not welcome here.
 	};
 	if((other.guild == GIL_SEK) || (other.guild == GIL_TPL) || (other.guild == GIL_GUR))
 	{
-		AI_Output(self,other,"DIA_Gunnar_Hallo_10_07");	//Ты один из этих безумных сектантов? Хоть проблем от вас никаких... хотя и пользы тоже.
+		AI_Output (self, other, " DIA_Gunnar_Hallo_10_07 " );	// Are you one of these insane cultists? Watch yourself around here.
 	};
 	if((other.guild == GIL_NDW) || (other.guild == GIL_KDW))
 	{
-		AI_Output(self,other,"DIA_Gunnar_Hallo_10_08");	//Аданос с тобой! Все хорошо - спасибо, что поинтересовался.
+		AI_Output (self, other, " DIA_Gunnar_Hallo_10_08 " );	// Adanos is with you, I can tell!
 	};
 	if((other.guild == GIL_NDM) || (other.guild == GIL_KDM))
 	{
-		AI_Output(self,other,"DIA_Gunnar_Hallo_10_09");	//Ты некромант, да?!
+		AI_Output (self, other, " DIA_Gunnar_Hallo_10_09 " );	// A Necromancer, here? Please don't curse us!
 	};
 	Gunnar_HalloGesagt = TRUE;
 };
@@ -79,11 +78,11 @@ func void DIA_Gunnar_Hallo_Info()
 instance DIA_Gunnar_Everywhere(C_Info)
 {
 	npc = BAU_902_Gunnar;
-	nr = 2;
+	no. = 2 ;
 	condition = DIA_Gunnar_Everywhere_Condition;
 	information = DIA_Gunnar_Everywhere_Info;
 	permanent = TRUE;
-	description = "Наемники приходят отовсюду?";
+	description = " Do mercenaries come from everywhere? " ;
 };
 
 
@@ -97,21 +96,21 @@ func int DIA_Gunnar_Everywhere_Condition()
 
 func void DIA_Gunnar_Everywhere_Info()
 {
-	AI_Output(other,self,"DIA_Gunnar_Everywhere_15_00");	//Наемники приходят отовсюду?
-	AI_Output(self,other,"DIA_Gunnar_Everywhere_10_01");	//Большинство из них пришло из колонии.
-	AI_Output(self,other,"DIA_Gunnar_Everywhere_10_02");	//Еще одна группа наемников пришла с юга, там они, вероятно, охотились на орков.
-	AI_Output(self,other,"DIA_Gunnar_Everywhere_10_03");	//И я готов поклясться, что несколько бандитов с гор тоже пришли сюда, чтобы получить бесплатную еду!
+	AI_Output (other, self, " DIA_Gunnar_Everywhere_15_00 " );	// Where are all these mercs coming from?
+	AI_Output (self, other, " DIA_Gunnar_Everywhere_10_01 " );	// Most of them came from the colony.
+	AI_Output (self, other, " DIA_Gunnar_Everywhere_10_02 " );	// Another group of mercenaries came from the south, where they probably hunted orcs.
+	AI_Output (self, other, " DIA_Gunnar_Everywhere_10_03 " );	// And I swear that some of the bandits from the mountains came here to get free food too!
 };
 
 
 instance DIA_Gunnar_South(C_Info)
 {
 	npc = BAU_902_Gunnar;
-	nr = 3;
+	No. = 3 ;
 	condition = DIA_Gunnar_South_Condition;
 	information = DIA_Gunnar_South_Info;
 	permanent = TRUE;
-	description = "Что ты знаешь о наемниках с юга?";
+	description = " What do you know about the mercenaries from the south? " ;
 };
 
 
@@ -125,20 +124,20 @@ func int DIA_Gunnar_South_Condition()
 
 func void DIA_Gunnar_South_Info()
 {
-	AI_Output(other,self,"DIA_Gunnar_South_15_00");	//Что ты знаешь о наемниках с юга?
-	AI_Output(self,other,"DIA_Gunnar_South_10_01");	//Немногое. Насколько мне известно, они охотились там на орков.
-	AI_Output(self,other,"DIA_Gunnar_South_10_02");	//Их босс - Сильвио. Булко, Сентенза, Рауль и еще несколько парней работают на него...
+	AI_Output (other, self, " DIA_Gunnar_South_15_00 " );	// What do you know about the mercenaries from the south?
+	AI_Output (self, other, " DIA_Gunnar_South_10_01 " );	// A little bit. As far as I know, they hunted orcs there.
+	AI_Output (self, other, " DIA_Gunnar_South_10_02 " );	// Their boss is Silvio. Bulko, Sentenza, Raul and a few other guys work for him ...
 };
 
 
 instance DIA_Gunnar_Colony(C_Info)
 {
 	npc = BAU_902_Gunnar;
-	nr = 4;
+	No. = 4 ;
 	condition = DIA_Gunnar_Colony_Condition;
 	information = DIA_Gunnar_Colony_Info;
 	permanent = TRUE;
-	description = "А кто пришел из колонии?";
+	description = " Who came from the colony? " ;
 };
 
 
@@ -152,19 +151,19 @@ func int DIA_Gunnar_Colony_Condition()
 
 func void DIA_Gunnar_Colony_Info()
 {
-	AI_Output(other,self,"DIA_Gunnar_Colony_15_00");	//А кто пришел из колонии?
-	AI_Output(self,other,"DIA_Gunnar_Colony_10_01");	//Ли и его люди. Торлоф, Корд, Джарвис, Вольф и кое-кто еще.
+	AI_Output (other, self, " DIA_Gunnar_Colony_15_00 " );	// Who came from the colony?
+	AI_Output (self, other, " DIA_Gunnar_Colony_10_01 " );	// Lee and his people. Thorlof, Kord, Jarvis, Wolf and a few others.
 };
 
 
 instance DIA_Gunnar_Bandits(C_Info)
 {
 	npc = BAU_902_Gunnar;
-	nr = 5;
+	No. = 5 ;
 	condition = DIA_Gunnar_Bandits_Condition;
 	information = DIA_Gunnar_Bandits_Info;
 	permanent = TRUE;
-	description = "А что это за бандиты, о которых ты говоришь?";
+	description = " Who are these bandits you are talking about? " ;
 };
 
 
@@ -178,18 +177,18 @@ func int DIA_Gunnar_Bandits_Condition()
 
 func void DIA_Gunnar_Bandits_Info()
 {
-	AI_Output(other,self,"DIA_Gunnar_Bandits_15_00");	//А что это за бандиты, о которых ты говоришь?
-	AI_Output(self,other,"DIA_Gunnar_Bandits_10_01");	//А, это я просто так. Если честно, я не совсем уверен в этом.
-	AI_Output(self,other,"DIA_Gunnar_Bandits_10_02");	//К югу от этой долины находятся горы с заброшенными шахтами. Там прячутся какие-то бандиты.
-	AI_Output(self,other,"DIA_Gunnar_Bandits_10_03");	//Они тоже каторжники из колонии, как и большинство наемников здесь.
-	AI_Output(self,other,"DIA_Gunnar_Bandits_10_04");	//Единственная разница - что там совсем отъявленные головорезы. Они убивают всех, кто подходит слишком близко.
+	AI_Output (other, self, " DIA_Gunnar_Bandits_15_00 " );	// And who are these bandits you are talking about?
+	AI_Output (self, other, " DIA_Gunnar_Bandits_10_01 " );	// Oh, it's me just like that. To be honest, I'm not entirely sure about that.
+	AI_Output (self, other, " DIA_Gunnar_Bandits_10_02 " );	// This valley is dotted with caves and abandoned mines. Some of the bandits are hiding there.
+	AI_Output (self, other, " DIA_Gunnar_Bandits_10_03 " );	// They are also convicts from the colony, like most of the mercenaries here.
+	AI_Output (self, other, " DIA_Gunnar_Bandits_10_04 " );	// Many of these men are ruthless, cold blooded killers. Keep your distance if you can help it.
 };
 
 
 instance DIA_Gunnar_PICKPOCKET(C_Info)
 {
 	npc = BAU_902_Gunnar;
-	nr = 900;
+	NO = 900 ;
 	condition = DIA_Gunnar_PICKPOCKET_Condition;
 	information = DIA_Gunnar_PICKPOCKET_Info;
 	permanent = TRUE;
@@ -199,19 +198,19 @@ instance DIA_Gunnar_PICKPOCKET(C_Info)
 
 func int DIA_Gunnar_PICKPOCKET_Condition()
 {
-	return C_Beklauen(45,70);
+	return  C_Beklauen ( 45 , 70 );
 };
 
 func void DIA_Gunnar_PICKPOCKET_Info()
 {
 	Info_ClearChoices(DIA_Gunnar_PICKPOCKET);
-	Info_AddChoice(DIA_Gunnar_PICKPOCKET,Dialog_Back,DIA_Gunnar_PICKPOCKET_BACK);
-	Info_AddChoice(DIA_Gunnar_PICKPOCKET,DIALOG_PICKPOCKET,DIA_Gunnar_PICKPOCKET_DoIt);
+	Info_AddChoice (DIA_Gunnar_PICKPOCKET, Dialog_Back, DIA_Gunnar_PICKPOCKET_BACK);
+	Info_AddChoice (DIA_Gunnar_PICKPOCKET, DIALOG_PICKPOCKET , DIA_Gunnar_PICKPOCKET_DoIt);
 };
 
-func void DIA_Gunnar_PICKPOCKET_DoIt()
+func void DIA_Gunnar_PICKPOCKET_DoIt ()
 {
-	B_Beklauen();
+	B_Beklauen ();
 	Info_ClearChoices(DIA_Gunnar_PICKPOCKET);
 };
 
@@ -219,4 +218,3 @@ func void DIA_Gunnar_PICKPOCKET_BACK()
 {
 	Info_ClearChoices(DIA_Gunnar_PICKPOCKET);
 };
-
