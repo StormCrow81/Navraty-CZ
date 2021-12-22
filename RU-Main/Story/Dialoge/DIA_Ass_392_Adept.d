e3a782e8c9e736098a12573e9fe91c3456434482
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2021-12-22 10:54:52 +0200

    Update DIA_Ass_392_Adept.d

--- RU-Main/Story/Dialoge/DIA_Ass_392_Adept.d
+++ RU-Main/Story/Dialoge/DIA_Ass_392_Adept.d
@@ -1,7 +1,7 @@
 instance DIA_Ass_392_Adept_EXIT(C_Info)
 {
 	npc = Ass_392_Adept;
-	nr = 999;
+	No. = 999 ;
 	condition = DIA_Ass_392_Adept_exit_condition;
 	information = DIA_Ass_392_Adept_exit_info;
 	permanent = TRUE;
@@ -21,10 +21,10 @@ func void DIA_Ass_392_Adept_exit_info()
 instance DIA_Ass_392_Adept_Hello(C_Info)
 {
 	npc = Ass_392_Adept;
-	nr = 1;
+	no. = 1 ;
 	condition = DIA_Ass_392_Adept_hello_condition;
 	information = DIA_Ass_392_Adept_hello_info;
-	permanent = FALSE;
+	permanent = FALSE ;
 	description = "Кто ты?";
 };
 	
@@ -35,28 +35,28 @@ func int DIA_Ass_392_Adept_hello_condition()
 	
 func void DIA_Ass_392_Adept_hello_info()
 {
-	AI_Output(other,self,"DIA_Ass_392_Adept_hello_01_01");	//Кто ты?
-	AI_Output(self,other,"DIA_Ass_392_Adept_hello_01_02");	//Мое имя Рафат, брат.
-	AI_Output(other,self,"DIA_Ass_392_Adept_hello_01_03");	//Я раньше не видел тебя в храме.
-	AI_Output(self,other,"DIA_Ass_392_Adept_hello_01_04");	//Это неудивительно. Ведь практически все время я провожу в молитвах у алтаря Белиара в долине.
-	AI_Output(other,self,"DIA_Ass_392_Adept_hello_01_05");	//А почему ты не молишься в храме?
-	AI_Output(self,other,"DIA_Ass_392_Adept_hello_01_06");	//Эээм... это довольно долгая история, брат.
-	AI_Output(other,self,"DIA_Ass_392_Adept_hello_01_07");	//Я готов ее послушать.
-	AI_Output(self,other,"DIA_Ass_392_Adept_hello_01_08");	//Одним словом, мне просто нельзя молиться тут!
-	AI_Output(other,self,"DIA_Ass_392_Adept_hello_01_09");	//Но почему?
-	AI_Output(self,other,"DIA_Ass_392_Adept_hello_01_10");	//Потому, что приор Ханиар запретил мне это делать до тех пор, пока мой дух не преисполнится силой и уверенностью. 
-	AI_Output(other,self,"DIA_Ass_392_Adept_hello_01_11");	//Похоже, ты в чем-то провинился перед ним.
-	AI_Output(self,other,"DIA_Ass_392_Adept_hello_01_12");	//Да, ты прав. Но я не хочу говорить об этом.
+	AI_Output(other,self,"DIA_Ass_392_Adept_hello_01_01");	//Who are you?
+	AI_Output (self, other, " DIA_Ass_392_Adept_hello_01_02 " );	// My name is Rafat, brother.
+	AI_Output (other, self, " DIA_Ass_392_Adept_hello_01_03 " );	// I haven't seen you in the temple before.
+	AI_Output (self, other, " DIA_Ass_392_Adept_hello_01_04 " );	// I spend almost all of my time in prayer at Beliar's altar in the valley.
+	AI_Output (other, self, " DIA_Ass_392_Adept_hello_01_05 " );	// Why don't you pray in the temple?
+	AI_Output (self, other, " DIA_Ass_392_Adept_hello_01_06 " );	// Um ... that's a long story, brother.
+	AI_Output (other, self, " DIA_Ass_392_Adept_hello_01_07 " );	// I'm ready to listen?
+	AI_Output (self, other, " DIA_Ass_392_Adept_hello_01_08 " );	// I'm not allowed to pray there.
+	AI_Output(other,self,"DIA_Ass_392_Adept_hello_01_09");	//Why not?
+	AI_Output (self, other, " DIA_Ass_392_Adept_hello_01_10 " );	// Because Prior Haniar has forbidden me to until my spirit is worthy of it.
+	AI_Output (other, self, " DIA_Ass_392_Adept_hello_01_11 " );	// It seems like you've done something wrong in his eyes.
+	AI_Output (self, other, " DIA_Ass_392_Adept_hello_01_12 " );	// Yes, you're right. But I don’t want to talk about it.
 };
 
 instance DIA_Ass_392_Adept_Deal(C_Info)
 {
 	npc = Ass_392_Adept;
-	nr = 1;
+	no. = 1 ;
 	condition = DIA_Ass_392_Adept_Deal_condition;
 	information = DIA_Ass_392_Adept_Deal_info;
-	permanent = FALSE;
-	description = "Ты работаешь на приора Ханиара?";
+	permanent = FALSE ;
+	description = " Do you work for Prior Haniar? " ;
 };
 	
 func int DIA_Ass_392_Adept_Deal_condition()
@@ -69,34 +69,34 @@ func int DIA_Ass_392_Adept_Deal_condition()
 	
 func void DIA_Ass_392_Adept_Deal_info()
 {
-	AI_Output(other,self,"DIA_Ass_392_Adept_Deal_01_01");	//Ты работаешь на приора Ханиара?
-	AI_Output(self,other,"DIA_Ass_392_Adept_Deal_01_02");	//Не совсем. Ханиар лишь является моим духовным наставником.
-	AI_Output(self,other,"DIA_Ass_392_Adept_Deal_01_03");	//Правда, я уже чуть не пожалел об этом. Особенно после того случая с Хасимом.
-	AI_Output(other,self,"DIA_Ass_392_Adept_Deal_01_04");	//А ты не думал сменить своего наставника?
-	AI_Output(self,other,"DIA_Ass_392_Adept_Deal_01_05");	//Не думаю, что кто-то из приоров решит взять меня под свое покровительство.
-	AI_Output(self,other,"DIA_Ass_392_Adept_Deal_01_06");	//К тому же Ханиар придет просто в ярость, если вдруг узнает об этом.
-	AI_Output(other,self,"DIA_Ass_392_Adept_Deal_01_07");	//Я слышал, что приору Осаиру нужны такие преданные люди, как ты.
-	AI_Output(self,other,"DIA_Ass_392_Adept_Deal_01_08");	//Ты это серьезно? Или же ты просто испытываешь мое терпение?
-	AI_Output(other,self,"DIA_Ass_392_Adept_Deal_01_09");	//Я лишь хочу сказать, что тебе не стоит упускать такой шанс.
-	AI_Output(other,self,"DIA_Ass_392_Adept_Deal_01_10");	//И тогда, возможно, тебе снова разрешат молиться в храме.
-	AI_Output(self,other,"DIA_Ass_392_Adept_Deal_01_11");	//Хммм...(задумчиво) Это было бы прекрасно!
-	AI_Output(self,other,"DIA_Ass_392_Adept_Deal_01_12");	//Но не будучи полностью уверенным в этом, я вряд ли решусь на такой шаг.
-	AI_Output(other,self,"DIA_Ass_392_Adept_Deal_01_13");	//А если я поговорю с Осаиром и он даст тебе подобное разрешение?
-	AI_Output(self,other,"DIA_Ass_392_Adept_Deal_01_14");	//Если так, тогда я, пожалуй, попрошусь к нему в наставники.
-	AI_Output(self,other,"DIA_Ass_392_Adept_Deal_01_15");	//Но не раньше, чем я получу его разрешение.
-	AI_Output(other,self,"DIA_Ass_392_Adept_Deal_01_16");	//Хорошо, я тебя понял.
+	AI_Output (other, self, " DIA_Ass_392_Adept_Deal_01_01 " );	// Do you work for Prior Haniar?
+	AI_Output (self, other, " DIA_Ass_392_Adept_Deal_01_02 " );	// Not really. Haniar is only my spiritual guide.
+	AI_Output (self, other, " DIA_Ass_392_Adept_Deal_01_03 " );	// Though I now regret it. Especially after that incident with Hasim.
+	AI_Output (other, self, " DIA_Ass_392_Adept_Deal_01_04 " );	// Have you thought about changing your mentor?
+	AI_Output (self, other, " DIA_Ass_392_Adept_Deal_01_05 " );	// I don't think that any of the priors will want to take me under their protection.
+	AI_Output (self, other, " DIA_Ass_392_Adept_Deal_01_06 " );	// Besides, Haniar will be furious if he finds out.
+	AI_Output (other, self, " DIA_Ass_392_Adept_Deal_01_07 " );	// I heard that Prior Osair needs loyal people like you.
+	AI_Output (self, other, " DIA_Ass_392_Adept_Deal_01_08 " );	// Are you serious? Or are you testing me?
+	AI_Output (other, self, " DIA_Ass_392_Adept_Deal_01_09 " );	// There's a chance her. Don't let it pass you by.
+	AI_Output (other, self, " DIA_Ass_392_Adept_Deal_01_10 " );	// Under Osair you will be allowed to pray in the temple again.
+	AI_Output (self, other, " DIA_Ass_392_Adept_Deal_01_11 " );	// Hmmm ... (thoughtfully) That would be great!
+	AI_Output (self, other, " DIA_Ass_392_Adept_Deal_01_12 " );	// But I can't do anything until I'm positive you're right.
+	AI_Output (other, self, " DIA_Ass_392_Adept_Deal_01_13 " );	// What if I talk to Osair and he gives permission?
+	AI_Output (self, other, " DIA_Ass_392_Adept_Deal_01_14 " );	// Then I'll take him as my mentor.
+	AI_Output (self, other, " DIA_Ass_392_Adept_Deal_01_15 " );	// But not before.
+	AI_Output (other, self, " DIA_Ass_392_Adept_Deal_01_16 " );	// Ok, I understand.
 	RafatNeedPermission = TRUE;
-	B_LogEntry(TOPIC_CareOsair,"Рафат хочет снова молиться в храме, но без разрешения одного из приоров он не может этого сделать. Надо поговорить об этом с Осаиром.");
+	B_LogEntry (TOPIC_CareOsair, " Rafat wants to pray in the temple again, but without the permission of one of the priors, he cannot do it. I need to talk about this with Osair. " );
 };
 
 instance DIA_Ass_392_Adept_Deal_Done(C_Info)
 {
 	npc = Ass_392_Adept;
-	nr = 1;
+	no. = 1 ;
 	condition = DIA_Ass_392_Adept_Deal_Done_condition;
 	information = DIA_Ass_392_Adept_Deal_Done_info;
-	permanent = FALSE;
-	description = "Теперь ты можешь молиться в храме.";
+	permanent = FALSE ;
+	description = " You can now pray in the temple. " ;
 };
 	
 func int DIA_Ass_392_Adept_Deal_Done_condition()
@@ -110,12 +110,12 @@ func int DIA_Ass_392_Adept_Deal_Done_condition()
 func void DIA_Ass_392_Adept_Deal_Done_info()
 {
 	B_GivePlayerXP(400);
-	AI_Output(other,self,"DIA_Ass_392_Adept_Deal_Done_01_01");	//Теперь ты можешь молиться в храме.
-	AI_Output(self,other,"DIA_Ass_392_Adept_Deal_Done_01_02");	//(недоверчиво) Ты это серьезно?
-	AI_Output(other,self,"DIA_Ass_392_Adept_Deal_Done_01_03");	//Приор Осаир дал на то свое разрешение. Можешь ни о чем не беспокоиться.
-	AI_Output(self,other,"DIA_Ass_392_Adept_Deal_Done_01_04");	//Что же, тогда, пожалуй, стоит отплатить ему тем же.
-	B_LogEntry(TOPIC_CareOsair,"Рафат теперь может молиться в храме с разрешения Осаира. И наверняка ряды верных приору людей вскоре пополнятся.");
-	RafatAgreed = TRUE;
+	AI_Output (other, self, " DIA_Ass_392_Adept_Deal_Done_01_01 " );	// You can now pray in the temple.
+	AI_Output (self, other, " DIA_Ass_392_Adept_Deal_Done_01_02 " );	// (incredulous) Are you serious?
+	AI_Output (other, self, " DIA_Ass_392_Adept_Deal_Done_01_03 " );	// Prior Osair gave his permission. You don't have to worry about anything.
+	AI_Output (self, other, " DIA_Ass_392_Adept_Deal_Done_01_04 " );	// Well, then, perhaps, it is worth repaying him in kind.
+	B_LogEntry (TOPIC_CareOsair, " Rafat can now pray in the temple with the permission of Osair. The priors ranks of loyal men is filling quickly. " );
+	RafatAgreed = TRUE ;
 	AI_StopProcessInfos(self);
 	Npc_ExchangeRoutine(self,"PrayInHram");
 };
