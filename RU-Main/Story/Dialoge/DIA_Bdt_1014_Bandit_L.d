5e06306843103efdd120f5732a7e973e2ad81201
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-11-30 20:12:58 +0200

    Update DIA_Bdt_1014_Bandit_L.d

--- RU-Main/Story/Dialoge/DIA_Bdt_1014_Bandit_L.d
+++ RU-Main/Story/Dialoge/DIA_Bdt_1014_Bandit_L.d
@@ -41,7 +41,7 @@ func int DIA_BDT_1014_BANDIT_KILLER_Condition()
 
 func void DIA_BDT_1014_BANDIT_KILLER_Info()
 {
-	AI_Output(self,other,"DIA_BDT_1014_BANDIT_KILLER_06_00");	//Это он, точно! Держи его!
+	AI_Output(self,other, " DIA_BDT_1014_BANDIT_KILLER_06_00 " );	// It's him, for sure! Hold it!
 	AI_StopProcessInfos(self);
 	B_Attack(self,other,AR_SuddenEnemyInferno,0);
 };
