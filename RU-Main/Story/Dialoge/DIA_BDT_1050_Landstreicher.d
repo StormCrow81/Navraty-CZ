aba73d76be55fb37d8d85b6f5b7441981f8889af
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-11-30 19:20:02 +0200

    Update DIA_BDT_1050_Landstreicher.d

--- RU-Main/Story/Dialoge/DIA_BDT_1050_Landstreicher.d
+++ RU-Main/Story/Dialoge/DIA_BDT_1050_Landstreicher.d
@@ -1,7 +1,7 @@
 
-instance DIA_Landstreicher_EXIT(C_Info)
+instance DIA_Landstreicher_EXIT (C_Info)
 {
-	npc = BDT_1050_Landstreicher;
+	npc = BDT_1050_tramp;
 	nr = 999;
 	condition = DIA_Landstreicher_EXIT_Condition;
 	information = DIA_Landstreicher_EXIT_Info;
@@ -10,111 +10,110 @@ instance DIA_Landstreicher_EXIT(C_Info)
 };
 
 
-func int DIA_Landstreicher_EXIT_Condition()
+func int DIA_Tramp_EXIT_Condition()
 {
 	return TRUE;
 };
 
-func void DIA_Landstreicher_EXIT_Info()
+func void DIA_Tramp_EXIT_Info()
 {
 	AI_StopProcessInfos(self);
 };
 
 
-instance DIA_Landstreicher_HALLO(C_Info)
+instance DIA_Tramp_HALLO (C_Info)
 {
-	npc = BDT_1050_Landstreicher;
+	npc = BDT_1050_tramp;
 	nr = 5;
-	condition = DIA_Landstreicher_HALLO_Condition;
-	information = DIA_Landstreicher_HALLO_Info;
+	condition = DIA_Tramper_HALLO_Condition;
+	information = DIA_Tramper_HALLO_Info;
 	important = TRUE;
 };
 
 
-func int DIA_Landstreicher_HALLO_Condition()
+func int DIA_tramp_HELLO_Condition()
 {
 	return TRUE;
 };
 
-func void DIA_Landstreicher_HALLO_Info()
+func void DIA_tramp_HELLO_Info()
 {
-	AI_Output(self,other,"DIA_Landstreicher_HALLO_09_00");	//Что происходит?
-	AI_Output(other,self,"DIA_Landstreicher_HALLO_15_01");	//Что ты имеешь в виду?
-	AI_Output(self,other,"DIA_Landstreicher_HALLO_09_02");	//Ты уже второй здесь, кто несется, как будто ужаленный кровавой мухой.
-	Info_ClearChoices(DIA_Landstreicher_HALLO);
-	Info_AddChoice(DIA_Landstreicher_HALLO,"Я должен идти дальше.",DIA_Landstreicher_HALLO_weg);
-	Info_AddChoice(DIA_Landstreicher_HALLO,"Где первый?",DIA_Landstreicher_HALLO_wo);
-	Info_AddChoice(DIA_Landstreicher_HALLO,"Кто второй?",DIA_Landstreicher_HALLO_wer);
+	AI_Output(self,other, " DIA_Landstreicher_HALLO_09_00 " );	// What's going on?
+	AI_Output(other,self, " DIA_Landstreicher_HALLO_15_01 " );	// What do you mean?
+	AI_Output(self,other, " DIA_Landstreicher_HALLO_09_02 " );	// You're already the second one here, rushing around as if stung by a blood fly.
+	Info_ClearChoices(DIA_tramp_HELLO);
+	Info_AddChoice(DIA_Landstreicher_HALLO, " I must go on. " ,DIA_Landstreicher_HALLO_weg);
+	Info_AddChoice(DIA_tramp_HELLO, " Где первый? " ,DIA_tramp_HELLO_where);
+	Info_AddChoice(DIA_tramp_HALLO, " Кто второй? " ,DIA_tramp_HALLO_who);
 };
 
-func void DIA_Landstreicher_HALLO_wo()
+func void DIA_vagrant_HELLO_wo()
 {
-	AI_Output(other,self,"DIA_Landstreicher_HALLO_wo_15_00");	//А куда первый побежал?
-	AI_Output(self,other,"DIA_Landstreicher_HALLO_wo_09_01");	//Вон туда. Бежал, это еще мягко сказано.
+	AI_Output(other,self, " DIA_Landstreicher_HALLO_wo_15_00 " );	// And where did the first one run?
+	AI_Output(self,other, " DIA_Landstreicher_HALLO_wo_09_01 " );	// Over there. Ran, that's putting it mildly.
 };
 
-func void DIA_Landstreicher_HALLO_wer()
+func void DIA_tramp_HELLO_who()
 {
-	AI_Output(other,self,"DIA_Landstreicher_HALLO_wer_15_00");	//А кто это был?
-	AI_Output(self,other,"DIA_Landstreicher_HALLO_wer_09_01");	//Он был похож на одного из этих головастых послушников из монастыря.
-	AI_Output(self,other,"DIA_Landstreicher_HALLO_wer_09_02");	//Можешь говорить, что угодно, но, по-моему, у этих парней не все в порядке с головой.
+	AI_Output(other,self, " DIA_Landstreicher_HALLO_wer_15_00 " );	// Who was that?
+	AI_Output(self,other, " DIA_Landstreicher_HALLO_wer_09_01 " );	// He looked like one of those big-headed novices from the monastery.
+	AI_Output(self,other, " DIA_Landstreicher_HALLO_wer_09_02 " );	// Say what you want, but I don't think these guys are all right in the head.
 	if(hero.guild == GIL_KDF)
 	{
-		AI_Output(self,other,"DIA_Landstreicher_HALLO_wer_09_03");	//Глянь только на твои нелепые лохмотья. Я бы не решился так выйти даже ночью. (смеется)
-		Info_AddChoice(DIA_Landstreicher_HALLO,"Следи за своим языком.",DIA_Landstreicher_HALLO_wer_vorsicht);
+		AI_Output(self,other, " DIA_Landstreicher_HALLO_wer_09_03 " );	// Just look at your ridiculous rags. I wouldn't dare go out like that even at night. (laughs)
+		Info_AddChoice(DIA_Landstreicher_HALLO, " Watch your language. " ,DIA_Landstreicher_HALLO_wer_vorsicht);
 	};
 };
 
-func void DIA_Landstreicher_HALLO_wer_vorsicht()
+func void DIA_vagrant_HELLO_who_caution()
 {
-	AI_Output(other,self,"DIA_Landstreicher_HALLO_wer_vorsicht_15_00");	//Попридержи язык. Я сам был послушником в монастыре.
-	AI_Output(self,other,"DIA_Landstreicher_HALLO_wer_vorsicht_09_01");	//(смеется) Ах ты, бедняга. Это объясняет кое-что.
-	AI_Output(self,other,"DIA_Landstreicher_HALLO_wer_vorsicht_09_02");	//Ничего личного. Ступай с богом. Ты занятой человек, не буду задерживать тебя.
-	Info_AddChoice(DIA_Landstreicher_HALLO,"Кто-нибудь должен поучить тебя хорошим манерам.",DIA_Landstreicher_HALLO_wer_vorsicht_Manieren);
+	AI_Output(other,self, " DIA_Landstreicher_HALLO_wer_vorsicht_15_00 " );	// Hold your tongue. I myself was a novice in a monastery.
+	AI_Output(self,other, " DIA_Landstreicher_HALLO_wer_vorsicht_09_01 " );	// (laughs) Oh, you poor thing. This explains something.
+	AI_Output(self,other, " DIA_Landstreicher_HALLO_wer_vorsicht_09_02 " );	// Nothing personal. Walk with God. You're a busy man, I won't keep you.
+	Info_AddChoice(DIA_Landstreicher_HALLO, " Somebody needs to teach you good manners. " ,DIA_Landstreicher_HALLO_wer_vorsicht_Manieren);
 };
 
-func void DIA_Landstreicher_HALLO_wer_vorsicht_Manieren()
+func void DIA_tramp_HELLO_who_careful_manners()
 {
-	AI_Output(other,self,"DIA_Landstreicher_HALLO_wer_vorsicht_Manieren_15_00");	//Кто-нибудь должен поучить тебя хорошим манерам.
-	AI_Output(self,other,"DIA_Landstreicher_HALLO_wer_vorsicht_Manieren_09_01");	//Ладно, не обижайся. Но если ты так уж хочешь сразиться, то давай.
-	AI_Output(self,other,"DIA_Landstreicher_HALLO_wer_vorsicht_Manieren_09_02");	//Покажи мне, на что вы, послушники, способны.
+	AI_Output(other,self, " DIA_Landstreicher_HALLO_wer_vorsicht_Manieren_15_00 " );	// Someone needs to teach you good manners.
+	AI_Output(self,other, " DIA_Landstreicher_HALLO_wer_vorsicht_Manieren_09_01 " );	// Okay, don't be offended. But if you really want to fight, then go ahead.
+	AI_Output(self,other, " DIA_Landstreicher_HALLO_wer_vorsicht_Manieren_09_02 " );	// Show me what you acolytes can do.
 	AI_StopProcessInfos(self);
 	B_Attack(self,other,AR_NONE,1);
 };
 
-func void DIA_Landstreicher_HALLO_weg()
+func void DIA_vagrant_HELLO_away()
 {
-	AI_Output(other,self,"DIA_Landstreicher_HALLO_weg_15_00");	//Мне нужно идти.
-	AI_Output(self,other,"DIA_Landstreicher_HALLO_weg_09_01");	//Хе. Нет проблем.
+	AI_Output(other,self, " DIA_Landstreicher_HALLO_weg_15_00 " );	// I need to go.
+	AI_Output(self,other, " DIA_Landstreicher_HALLO_weg_09_01 " );	// Heh. No problem.
 	AI_StopProcessInfos(self);
 };
 
 
-instance DIA_Landstreicher_WASMACHSTDU(C_Info)
+instance DIA_Landstreicher_WASMACHSTDU (C_Info)
 {
-	npc = BDT_1050_Landstreicher;
+	npc = BDT_1050_tramp;
 	nr = 6;
 	condition = DIA_Landstreicher_WASMACHSTDU_Condition;
 	information = DIA_Landstreicher_WASMACHSTDU_Info;
 	permanent = TRUE;
-	description = "Что ты делаешь здесь?";
+	description = " What are you doing here? " ;
 };
 
 
-func int DIA_Landstreicher_WASMACHSTDU_Condition()
+func int DIA_vagrant_WASMACHSTDU_Condition()
 {
 	return TRUE;
 };
 
-func void DIA_Landstreicher_WASMACHSTDU_Info()
+func void DIA_vagrant_WASMACHSTDU_Info()
 {
-	AI_Output(other,self,"DIA_Landstreicher_WASMACHSTDU_15_00");	//Что ты делаешь здесь?
-	if(Npc_KnowsInfo(other,DIA_Landstreicher_HALLO))
+	AI_Output(other,self, " DIA_Landstreicher_WASMACHSTDU_15_00 " );	// What are you doing here?
+	if (Npc_KnowsInfo(other,DIA_Landstreicher_HALLO))
 	{
-		AI_Output(self,other,"DIA_Landstreicher_WASMACHSTDU_09_01");	//Ты задаешь слишком много вопросов, молокосос.
+		AI_Output(self,other, " DIA_Landstreicher_WASMACHSTDU_09_01 " );	// You ask too many questions, sucker.
 	};
-	AI_Output(self,other,"DIA_Landstreicher_WASMACHSTDU_09_02");	//Назови пещеру гоблинов, в которой мы ползали вместе, и я расскажу тебе все свои тайны.
-	AI_Output(self,other,"DIA_Landstreicher_WASMACHSTDU_09_03");	//А в противном случае это не твое дело.
+	AI_Output(self,other, " DIA_Landstreicher_WASMACHSTDU_09_02 " );	// Name the goblin cave we crawled in together, and I'll tell you all my secrets.
+	AI_Output(self,other, " DIA_Landstreicher_WASMACHSTDU_09_03 " );	// Otherwise, it's none of your business.
 	AI_StopProcessInfos(self);
 };
-
