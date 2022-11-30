bb40b88498a1f6d824341b99c3d99cc9b34efaf1
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-11-30 18:40:38 +0200

    Update DIA_BAU_945_Egill.d

--- RU-Main/Story/Dialoge/DIA_BAU_945_Egill.d
+++ RU-Main/Story/Dialoge/DIA_BAU_945_Egill.d
@@ -26,8 +26,8 @@ instance DIA_Egill_HALLO(C_Info)
 	npc = BAU_945_Egill;
 	nr = 3;
 	condition = DIA_Egill_HALLO_Condition;
-	information = DIA_Egill_HALLO_Info;
-	description = "В чем дело?";
+	information = DIA_Egill_HELLO_Info;
+	description = " What's wrong? " ;
 };
 
 
@@ -38,24 +38,24 @@ func int DIA_Egill_HALLO_Condition()
 
 func void DIA_Egill_HALLO_Info()
 {
-	AI_Output(other,self,"DIA_Egill_HALLO_15_00");	//В чем дело?
-	AI_Output(self,other,"DIA_Egill_HALLO_08_01");	//Давно я уже не видел чужака на нашей земле, который не хотел бы ободрать нас, как липку.
-	AI_Output(self,other,"DIA_Egill_HALLO_08_02");	//Надеюсь, я не ошибаюсь в тебе.
-	AI_Output(self,other,"DIA_Egill_HALLO_08_03");	//Я Эгилл.
+	AI_Output(other,self, " DIA_Egill_HALLO_15_00 " );	// What's wrong?
+	AI_Output(self,other, " DIA_Egill_HALLO_08_01 " );	// For a long time I have not seen a stranger on our land who would not want to peel us like sticky.
+	AI_Output(self,other, " DIA_Egill_HALLO_08_02 " );	// I hope I'm not mistaken about you.
+	AI_Output(self,other, " DIA_Egill_HALLO_08_03 " );	// I'm Egill.
 	if(Hlp_IsValidNpc(Ehnim) && !C_NpcIsDown(Ehnim))
 	{
-		AI_Output(self,other,"DIA_Egill_HALLO_08_04");	//А этот чудак вон там - мой брат Эним.
+		AI_Output(self,other, " DIA_Egill_HALLO_08_04 " );	// And that weirdo over there is my brother Enim.
 	};
 };
 
 
-instance DIA_Egill_FELDARBEIT(C_Info)
+instance DIA_Egill_FELDARBEIT (C_Info)
 {
 	npc = BAU_945_Egill;
 	nr = 4;
 	condition = DIA_Egill_FELDARBEIT_Condition;
 	information = DIA_Egill_FELDARBEIT_Info;
-	description = "Как идет работа в поле?";
+	description = " How is the work going in the field? " ;
 };
 
 
@@ -69,8 +69,8 @@ func int DIA_Egill_FELDARBEIT_Condition()
 
 func void DIA_Egill_FELDARBEIT_Info()
 {
-	AI_Output(other,self,"DIA_Egill_FELDARBEIT_15_00");	//Как идет работа в поле?
-	AI_Output(self,other,"DIA_Egill_FELDARBEIT_08_01");	//Отлично. Если бы только эти мерзкие полевые хищники не портили наш урожай...
+	AI_Output(other,self, " DIA_Egill_FELDARBEIT_15_00 " );	// How is the work going in the field?
+	AI_Output(self,other, " DIA_Egill_FELDARBEIT_08_01 " );	// Great. If only those nasty field predators weren't ruining our crops...
 };
 
 
@@ -80,13 +80,13 @@ instance DIA_Egill_FELDRAEUBER(C_Info)
 	nr = 5;
 	condition = DIA_Egill_FELDRAEUBER_Condition;
 	information = DIA_Egill_FELDRAEUBER_Info;
-	description = "Почему вы ничего не делаете с этими полевыми хищниками?";
+	description = " Why aren't you doing anything to these field predators? " ;
 };
 
 
 func int DIA_Egill_FELDRAEUBER_Condition()
 {
-	if(Npc_KnowsInfo(other,DIA_Egill_FELDARBEIT))
+	if (Npc_KnowsInfo(other,DIA_Egill_FELDARBEIT))
 	{
 		return TRUE;
 	};
@@ -94,25 +94,25 @@ func int DIA_Egill_FELDRAEUBER_Condition()
 
 func void DIA_Egill_FELDRAEUBER_Info()
 {
-	AI_Output(other,self,"DIA_Egill_FELDRAEUBER_15_00");	//Почему вы ничего не делаете с этими полевыми хищниками?
-	AI_Output(self,other,"DIA_Egill_FELDRAEUBER_08_01");	//Ты шутишь? С тех пор, как я работаю на этой ферме, я убил их столько, что уже сбился со счета.
-	AI_Output(self,other,"DIA_Egill_FELDRAEUBER_08_02");	//Но на место убитых приходят новые.
+	AI_Output(other,self, " DIA_Egill_FELDRAEUBER_15_00 " );	// Why aren't you doing anything to these field predators?
+	AI_Output(self,other, " DIA_Egill_FELDRAEUBER_08_01 " );	// Are you kidding? Since I've been working on this farm, I've killed so many that I've lost count.
+	AI_Output(self,other, " DIA_Egill_FELDRAEUBER_08_02 " );	// But new ones take the place of the dead.
 };
 
 
-instance DIA_Egill_STREIT2(C_Info)
+instance DIA_Egill_STREIT2 (C_Info)
 {
 	npc = BAU_945_Egill;
 	nr = 6;
 	condition = DIA_Egill_STREIT2_Condition;
 	information = DIA_Egill_STREIT2_Info;
-	description = "Твой брат говорит то же самое.";
+	description = " Your brother says the same thing. " ;
 };
 
 
 func int DIA_Egill_STREIT2_Condition()
 {
-	if(Npc_KnowsInfo(other,DIA_Egill_FELDRAEUBER) && Npc_KnowsInfo(other,DIA_Ehnim_FELDRAEUBER) && (Hlp_IsValidNpc(Ehnim) && !C_NpcIsDown(Ehnim)))
+	if ( Npc_KnowsInfo ( other , DIA_Egill_FELDRAEUBER ) && Npc_KnowsInfo ( other , DIA_Ehnim_FELDRAEUBER ) && ( Hlp_IsValidNpc ( Own ) &&  ! C_NpcIsDown ( Own ))) .
 	{
 		return TRUE;
 	};
@@ -120,22 +120,22 @@ func int DIA_Egill_STREIT2_Condition()
 
 func void DIA_Egill_STREIT2_Info()
 {
-	AI_Output(other,self,"DIA_Egill_STREIT2_15_00");	//Твой брат говорит то же самое.
-	AI_Output(self,other,"DIA_Egill_STREIT2_08_01");	//(насмешливо) Ага. А что еще он говорит?
-	AI_Output(other,self,"DIA_Egill_STREIT2_15_02");	//Что он тоже убил множество полевых хищников.
-	AI_Output(self,other,"DIA_Egill_STREIT2_08_03");	//Что? Да этот тормоз даже не может самостоятельно найти путь домой.
-	AI_Output(self,other,"DIA_Egill_STREIT2_08_04");	//Скажи ему, чтобы он больше не бахвалился.
+	AI_Output(other,self, " DIA_Egill_STREIT2_15_00 " );	// Your brother says the same thing.
+	AI_Output(self,other, " DIA_Egill_STREIT2_08_01 " );	// (mockingly) Yeah. What else does he say?
+	AI_Output(other,self, " DIA_Egill_STREIT2_15_02 " );	// That he also killed a lot of field predators.
+	AI_Output(self,other, " DIA_Egill_STREIT2_08_03 " );	// What? Yes, this brake can't even find its way home on its own.
+	AI_Output(self,other, " DIA_Egill_STREIT2_08_04 " );	// Tell him not to brag anymore.
 	AI_StopProcessInfos(self);
 };
 
 
-instance DIA_Egill_STREIT4(C_Info)
+instance DIA_Egill_STREIT4 (C_Info)
 {
 	npc = BAU_945_Egill;
 	nr = 7;
 	condition = DIA_Egill_STREIT4_Condition;
 	information = DIA_Egill_STREIT4_Info;
-	description = "Эним немного раздражен твоими словами.";
+	description = " Enim is a little annoyed by your words. " ;
 };
 
 
@@ -149,8 +149,8 @@ func int DIA_Egill_STREIT4_Condition()
 
 func void DIA_Egill_STREIT4_Info()
 {
-	AI_Output(other,self,"DIA_Egill_STREIT4_15_00");	//Эним немного раздражен твоими словами.
-	AI_Output(self,other,"DIA_Egill_STREIT4_08_01");	//Пусть не расхаживает как павлин, распустив хвост, а то я ему голову оторву. Скажи ему это.
+	AI_Output(other,self, " DIA_Egill_STREIT4_15_00 " );	// Enim is a little annoyed by your words.
+	AI_Output(self,other, " DIA_Egill_STREIT4_08_01 " );	// Let him not walk around like a peacock with his tail spread, otherwise I will tear off his head. Tell him that.
 	AI_StopProcessInfos(self);
 };
 
@@ -167,7 +167,7 @@ instance DIA_Egill_PERMKAP1(C_Info)
 
 func int DIA_Egill_PERMKAP1_Condition()
 {
-	if((DIA_Ehnim_STREIT5_noPerm == TRUE) && Npc_IsInState(self,ZS_Talk))
+	if (( DIA_Ehnim_STREIT5_noPerm ==  TRUE ) && Npc_IsInState ( self , ZS_Talk ) ) ;
 	{
 		return TRUE;
 	};
@@ -175,7 +175,7 @@ func int DIA_Egill_PERMKAP1_Condition()
 
 func void DIA_Egill_PERMKAP1_Info()
 {
-	AI_Output(self,other,"DIA_Egill_PERMKAP1_08_00");	//Тебе мало того, что ты натворил? Я думаю, тебе лучше уйти.
+	AI_Output(self,other, " DIA_Egill_PERMKAP1_08_00 " );	// It's not enough for you what you've done? I think you'd better leave.
 	AI_StopProcessInfos(self);
 };
 
@@ -193,7 +193,7 @@ instance DIA_Egill_PICKPOCKET(C_Info)
 
 func int DIA_Egill_PICKPOCKET_Condition()
 {
-	return C_Beklauen(45,24);
+	return  C_Robbery ( 45 , 24 );
 };
 
 func void DIA_Egill_PICKPOCKET_Info()
@@ -205,7 +205,7 @@ func void DIA_Egill_PICKPOCKET_Info()
 
 func void DIA_Egill_PICKPOCKET_DoIt()
 {
-	B_Beklauen();
+	B_Robbery();
 	Info_ClearChoices(DIA_Egill_PICKPOCKET);
 };
 
