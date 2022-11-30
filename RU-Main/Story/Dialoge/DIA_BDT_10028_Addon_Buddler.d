42c2ce56c43bb9caa6aa7c56246f20a09efc21dd
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-11-30 19:26:41 +0200

    Update DIA_BDT_10028_Addon_Buddler.d

--- RU-Main/Story/Dialoge/DIA_BDT_10028_Addon_Buddler.d
+++ RU-Main/Story/Dialoge/DIA_BDT_10028_Addon_Buddler.d
@@ -28,7 +28,7 @@ instance DIA_Addon_BDT_10028_Buddler_Hi(C_Info)
 	condition = DIA_Addon_10028_Buddler_Hi_Condition;
 	information = DIA_Addon_10028_Buddler_Hi_Info;
 	permanent = TRUE;
-	description = "Как дела?";
+	description = " How are you? " ;
 };
 
 
@@ -40,15 +40,14 @@ func int DIA_Addon_10028_Buddler_Hi_Condition()
 func void DIA_Addon_10028_Buddler_Hi_Info()
 {
 	AI_Output(other,self,"DIA_Addon_BDT_10028_Buddler_Hi_15_00");	//Как дела?
-	if(Sklaven_Flucht == FALSE)
+	if (slave_escape ==  FALSE )
 	{
-		AI_Output(self,other,"DIA_Addon_BDT_10028_Buddler_Hi_12_01");	//Я не собираюсь идти в шахту сам. У нас для этого достаточно рабов.
-		AI_Output(self,other,"DIA_Addon_BDT_10028_Buddler_Hi_12_02");	//Так легко нам золото еще никогда не доставалось...(смеется) 
+		AI_Output(self,other, " DIA_Addon_BDT_10028_Buddler_Hi_12_01 " );	// I'm not going to go into the mine myself. We have enough slaves for this.
+		AI_Output(self,other, " DIA_Addon_BDT_10028_Buddler_Hi_12_02 " );	// Gold has never been so easy for us... (laughs)
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_Addon_BDT_10028_Buddler_Hi_12_03");	//Х-а! Какой идиот освободил рабов?!
+		AI_Output(self,other, " DIA_Addon_BDT_10028_Buddler_Hi_12_03 " );	// Whoa! What idiot freed the slaves?!
 		AI_StopProcessInfos(self);
 	};
 };
-
