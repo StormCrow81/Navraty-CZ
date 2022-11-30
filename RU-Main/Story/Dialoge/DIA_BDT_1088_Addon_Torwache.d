7b8654a570df9543c95a1b78a1165c0c2a5b7efd
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-11-30 19:55:48 +0200

    Update DIA_BDT_1088_Addon_Torwache.d

--- RU-Main/Story/Dialoge/DIA_BDT_1088_Addon_Torwache.d
+++ RU-Main/Story/Dialoge/DIA_BDT_1088_Addon_Torwache.d
@@ -39,13 +39,13 @@ func int DIA_BDT_1088_Addon_Torwache_FirstWarn_Condition()
 	if(Npc_GetDistToWP(other,BDT_1088_Checkpoint) <= 700)
 	{
 		Npc_SetRefuseTalk(self,5);
-		return FALSE;
+		return  FALSE ;
 	};
 	if((self.aivar[AIV_Guardpassage_Status] == GP_NONE) && (self.aivar[AIV_PASSGATE] == FALSE) && (Hlp_StrCmp(Npc_GetNearestWP(self),self.wp) == TRUE) && (Npc_RefuseTalk(self) == FALSE))
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_BDT_1088_Addon_Torwache_FirstWarn_Info()
@@ -53,30 +53,30 @@ func void DIA_BDT_1088_Addon_Torwache_FirstWarn_Info()
 	AI_Output(self,other,"DIA_Addon_BDT_1088_Addon_Torwache_FirstWarn_06_00");	//СТОЙ!
 	if(MIS_Send_Buddler == LOG_SUCCESS)
 	{
-		AI_Output(other,self,"DIA_Addon_BDT_1088_Torwache_FirstWarn_15_01");	//Уйди с дороги, охранник.
-		AI_Output(self,other,"DIA_Addon_BDT_10018_Torwache_FirstWarn_06_02");	//А, все в порядке, можешь проходить.
-		self.aivar[AIV_PASSGATE] = TRUE;
+		AI_Output(other,self, " DIA_Addon_BDT_1088_Torwache_FirstWarn_15_01 " );	// Get out of the way, guard.
+		AI_Output(self,other, " DIA_Addon_BDT_10018_Torwache_FirstWarn_06_02 " );	// Oh, it's all right, you can pass.
+		self.aivar[ AIV_PASSGATE ] = TRUE ;
 	}
 	else if((MIS_Send_Buddler != LOG_Running) && (MIS_Send_Buddler != LOG_SUCCESS))
 	{
 		if(Torwache_Greetings == 0)
 		{
-			AI_Output(self,other,"DIA_Addon_BDT_1088_Addon_Torwache_FirstWarn_06_03");	//Пока Торус не выдал тебе красный камень, тебя здесь не ждут.
+			AI_Output(self,other, " DIA_Addon_BDT_1088_Addon_Torwache_FirstWarn_06_03 " );	// Until Thorus gives you the redstone, you're not welcome here.
 			Torwache_Greetings = 1;
 		}
 		else if(Torwache_Greetings == 1)
 		{
-			AI_Output(self,other,"DIA_Addon_BDT_1088_Addon_Torwache_FirstWarn_06_04");	//Ты что, не понял? (с нажимом) ПОШЕЛ ОТСЮДА!
+			AI_Output(self,other, " DIA_Addon_BDT_1088_Addon_Torwache_FirstWarn_06_04 " );	// Don't you understand? (with pressure) GET OUT OF HERE!
 			Torwache_Greetings = 2;
 		}
 		else
 		{
-			AI_Output(self,other,"DIA_Addon_BDT_1088_Addon_Torwache_FirstWarn_06_05");	//(с нажимом) ПОШЕЛ ОТСЮДА!
+			AI_Output(self,other, " DIA_Addon_BDT_1088_Addon_Torwache_FirstWarn_06_05 " );	// (forcefully) GET OUT OF HERE!
 		};
 	}
 	else if(MIS_Send_Buddler == LOG_Running)
 	{
-		AI_Output(self,other,"DIA_Addon_BDT_1088_Addon_Torwache_FirstWarn_06_06");	//Эй, пока Торус не выдаст тебе пропуск, ты не пройдешь здесь.
+		AI_Output(self,other, " DIA_Addon_BDT_1088_Addon_Torwache_FirstWarn_06_06 " );	// Hey, until Torus gives you a pass, you won't get through here.
 	};
 	AI_StopProcessInfos(self);
 	other.aivar[AIV_LastDistToWP] = Npc_GetDistToWP(other,BDT_1088_Checkpoint);
@@ -97,16 +97,16 @@ instance DIA_BDT_1088_Addon_Torwache_SecondWarn(C_Info)
 
 func int DIA_BDT_1088_Addon_Torwache_SecondWarn_Condition()
 {
-	if((self.aivar[AIV_Guardpassage_Status] == GP_FirstWarnGiven) && (self.aivar[AIV_PASSGATE] == FALSE) && (Hlp_StrCmp(Npc_GetNearestWP(self),self.wp) == TRUE) && (Npc_GetDistToWP(other,BDT_1088_Checkpoint) < (other.aivar[AIV_LastDistToWP] - 50)))
+	if ((self.aivar[AIV_GuardPass_Status] == GP_FirstWarnGiven) && (self.aivar[ AIV_PASSGATE ] ==  FALSE ) && (Help_StrengthCmp(Npc_GetNearestWP(self),self.wp) ==  TRUE ) && (Npc_GetDistToWP(other,BDT_1088_Checkpoint); < ( other . aivar [ AIV_LastDistToWP ] -  50 ))) .
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_BDT_1088_Addon_Torwache_SecondWarn_Info()
 {
-	AI_Output(self,other,"DIA_Addon_BDT_1088_Addon_Torwache_SecondWarn_06_00");	//Так, успокойся. У тебя есть последний шанс. Разворачивайся ПРЯМО СЕЙЧАС, или ты умрешь...
+	AI_Output(self,other, " DIA_Addon_BDT_1088_Addon_Torwache_SecondWarn_06_00 " );	// Ok, calm down. You have one last chance. Turn around RIGHT NOW or you will die...
 	other.aivar[AIV_LastDistToWP] = Npc_GetDistToWP(other,BDT_1088_Checkpoint);
 	self.aivar[AIV_Guardpassage_Status] = GP_SecondWarnGiven;
 	AI_StopProcessInfos(self);
@@ -130,14 +130,14 @@ func int DIA_BDT_1088_Addon_Torwache_Attack_Condition()
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_BDT_1088_Addon_Torwache_Attack_Info()
 {
 	other.aivar[AIV_LastDistToWP] = 0;
-	self.aivar[AIV_Guardpassage_Status] = GP_NONE;
-	AI_Output(self,other,"DIA_Addon_BDT_1088_Addon_Torwache_Attack_06_00");	//(вздыхает) Ты думаешь, я шучу? Я покажу тебе...
+	self.aivar[AIV_Guardpassage_Status] = GP_NONE ;
+	AI_Output(self,other, " DIA_Addon_BDT_1088_Addon_Torwache_Attack_06_00 " );	// (sighs) You think I'm joking? I'll show you...
 	AI_StopProcessInfos(self);
 	B_Attack(self,other,AR_GuardStopsIntruder,0);
 };
