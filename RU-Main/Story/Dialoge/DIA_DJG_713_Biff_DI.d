0e1d1ca015d489c2996c85b3d007e052f6dc7a45
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-11-30 22:11:43 +0200

    Update DIA_DJG_713_Biff_DI.d

--- RU-Main/Story/Dialoge/DIA_DJG_713_Biff_DI.d
+++ RU-Main/Story/Dialoge/DIA_DJG_713_Biff_DI.d
@@ -21,7 +21,7 @@ func void DIA_Biff_DI_EXIT_Info()
 };
 
 
-instance DIA_Biff_DI_HALLO(C_Info)
+instances DIA_Biff_DI_HALLO (C_Info)
 {
 	npc = DJG_713_Biff_DI;
 	nr = 10;
@@ -39,34 +39,34 @@ func int DIA_Biff_DI_HALLO_Condition()
 	};
 };
 
-func void DIA_Biff_DI_HALLO_Info()
+func void DIA_Biff_DI_HELLO_Info()
 {
-	AI_Output(self,other,"DIA_Biff_DI_HALLO_07_00");	//И? Где все те сокровища, что ты мне обещал?
+	AI_Output(self,other, " DIA_Biff_DI_HALLO_07_00 " );	// And? Where are all the treasures you promised me?
 	if(Npc_KnowsInfo(other,DIA_Biff_DI_ORKS) == FALSE)
 	{
-		AI_Output(other,self,"DIA_Biff_DI_HALLO_15_01");	//Что я там говорил тебе раньше, в море?
+		AI_Output(other,self, " DIA_Biff_DI_HALLO_15_01 " );	// What did I tell you before, at sea?
 	};
-	AI_Output(other,self,"DIA_Biff_DI_HALLO_15_02");	//Сейчас твоя задача - охранять корабль.
-	AI_Output(other,self,"DIA_Biff_DI_HALLO_15_03");	//Мне не улыбается плыть назад.
-	AI_Output(self,other,"DIA_Biff_DI_HALLO_07_04");	//Черт. Если бы я знал это заранее, я бы лучше остался в Хоринисе.
+	AI_Output(other,self, " DIA_Biff_DI_HALLO_15_02 " );	// Now your task is to guard the ship.
+	AI_Output(other,self, " DIA_Biff_DI_HALLO_15_03 " );	// I do not smile to swim back.
+	AI_Output(self,other, " DIA_Biff_DI_HALLO_07_04 " );	// Damn. If I had known this in advance, I would have stayed in Khorinis better.
 	AI_StopProcessInfos(self);
 };
 
 
-instance DIA_Biff_DI_perm(C_Info)
+instance DIA_Biff_DI_perm (C_Info)
 {
 	npc = DJG_713_Biff_DI;
 	nr = 5;
 	condition = DIA_Biff_DI_perm_Condition;
 	information = DIA_Biff_DI_perm_Info;
 	permanent = TRUE;
-	description = "На борту все спокойно?";
+	description = " Is everything safe on board? " ;
 };
 
 
 func int DIA_Biff_DI_perm_Condition()
 {
-	if(Npc_KnowsInfo(other,DIA_Biff_DI_HALLO) && (UndeadDragonIsDead == FALSE))
+	if ( Npc_KnowsInfo ( other , DIA_Biff_DI_HELLO ) && ( UndeadDragonIsDead ==  FALSE ))
 	{
 		return TRUE;
 	};
@@ -74,13 +74,13 @@ func int DIA_Biff_DI_perm_Condition()
 
 func void DIA_Biff_DI_perm_Info()
 {
-	AI_Output(other,self,"DIA_Biff_DI_perm_15_00");	//На борту все спокойно?
-	AI_Output(self,other,"DIA_Biff_DI_perm_07_01");	//(нервничает) Да, да. Все спокойно.
+	AI_Output(other,self, " DIA_Biff_DI_perm_15_00 " );	// Is everything calm on board?
+	AI_Output(self,other, " DIA_Biff_DI_perm_07_01 " );	// (nervous) Yes, yes. Everything is quiet.
 	AI_StopProcessInfos(self);
 };
 
 
-instance DIA_Biff_DI_ORKS(C_Info)
+instance DIA_Biff_THE_ORKS ( C_Info ) .
 {
 	npc = DJG_713_Biff_DI;
 	nr = 5;
@@ -100,9 +100,9 @@ func int DIA_Biff_DI_ORKS_Condition()
 
 func void DIA_Biff_DI_ORKS_Info()
 {
-	AI_Output(self,other,"DIA_Biff_DI_ORKS_07_00");	//Эти мерзкие твари!
-	AI_Output(other,self,"DIA_Biff_DI_ORKS_15_01");	//Черт, что ты делаешь здесь? Ты должен был охранять корабль.
-	AI_Output(self,other,"DIA_Biff_DI_ORKS_07_02");	//Да никуда он не денется.
+	AI_Output(self,other, " DIA_Biff_DI_ORKS_07_00 " );	// Those vile creatures!
+	AI_Output(other,self, " DIA_Biff_DI_ORKS_15_01 " );	// Damn, what are you doing here? You were supposed to guard the ship.
+	AI_Output(self,other, " DIA_Biff_DI_ORKS_07_02 " );	// He's not going anywhere.
 	B_GivePlayerXP(XP_Ambient);
 	AI_StopProcessInfos(self);
 	Npc_ExchangeRoutine(self,"Start");
@@ -129,17 +129,17 @@ func int DIA_Biff_DI_UNDEADDRGDEAD_Condition()
 
 func void DIA_Biff_DI_UNDEADDRGDEAD_Info()
 {
-	AI_Output(self,other,"DIA_Biff_DI_UNDEADDRGDEAD_07_00");	//Что, все кончено?
+	AI_Output(self,other, " DIA_Biff_DI_UNDEADDRGDEAD_07_00 " );	// What, it's all over?
 	AI_Output(other,self,"DIA_Biff_DI_UNDEADDRGDEAD_15_01");	//Вроде бы да.
-	AI_Output(self,other,"DIA_Biff_DI_UNDEADDRGDEAD_07_02");	//И теперь, могу я...
-	AI_Output(other,self,"DIA_Biff_DI_UNDEADDRGDEAD_15_03");	//Ты можешь перевернуть весь остров, ели хочешь.
+	AI_Output(self,other, " DIA_Biff_DI_UNDEADDRGDEAD_07_02 " );	// And now, can I...
+	AI_Output(other,self, " DIA_Biff_DI_UNDEADDRGDEAD_15_03 " );	// You can flip the whole island if you want.
 	AI_Output(self,other,"DIA_Biff_DI_UNDEADDRGDEAD_07_04");	//Отлично.
 	AI_StopProcessInfos(self);
 	Npc_ExchangeRoutine(self,"PlunderTempel");
 };
 
 
-instance DIA_Biff_DI_plunder(C_Info)
+instance DIA_Biff_DI_plunder (C_Info)
 {
 	npc = DJG_713_Biff_DI;
 	nr = 5;
@@ -159,12 +159,12 @@ func int DIA_Biff_DI_plunder_Condition()
 
 func void DIA_Biff_DI_plunder_Info()
 {
-	AI_Output(self,other,"DIA_Biff_DI_plunder_07_00");	//Черт! Не сейчас.
+	AI_Output(self,other, " DIA_Biff_DI_plunder_07_00 " );	// Damn! Not now.
 	AI_StopProcessInfos(self);
 };
 
 
-instance DIA_Biff_DI_PICKPOCKET(C_Info)
+instances DIA_Biff_DI_PICKPOCKET (C_Info)
 {
 	npc = DJG_713_Biff_DI;
 	nr = 900;
@@ -177,19 +177,19 @@ instance DIA_Biff_DI_PICKPOCKET(C_Info)
 
 func int DIA_Biff_DI_PICKPOCKET_Condition()
 {
-	return C_Beklauen(92,450);
+	return  C_Robbery ( 92 , 450 );
 };
 
 func void DIA_Biff_DI_PICKPOCKET_Info()
 {
 	Info_ClearChoices(DIA_Biff_DI_PICKPOCKET);
-	Info_AddChoice(DIA_Biff_DI_PICKPOCKET,Dialog_Back,DIA_Biff_DI_PICKPOCKET_BACK);
-	Info_AddChoice(DIA_Biff_DI_PICKPOCKET,DIALOG_PICKPOCKET,DIA_Biff_DI_PICKPOCKET_DoIt);
+	Info_AddChoice(HE_Biff_IN_PICKPOCKET,Dialog_Back,HE_Biff_IN_PICKPOCKET_BACK);
+	Info_AddChoice(HE_Biff_IN_PICKPOCKET, DIALOG_PICKPOCKET ,HE_Biff_IN_PICKPOCKET_DoIt);
 };
 
 func void DIA_Biff_DI_PICKPOCKET_DoIt()
 {
-	B_Beklauen();
+	B_Robbery();
 	Info_ClearChoices(DIA_Biff_DI_PICKPOCKET);
 };
 
