8ea3d428825b33aab9bfaadd23195f4cce677cfd
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-11-30 19:21:03 +0200

    Update DIA_BDT_1025_Bandit_H.d

--- RU-Main/Story/Dialoge/DIA_BDT_1025_Bandit_H.d
+++ RU-Main/Story/Dialoge/DIA_BDT_1025_Bandit_H.d
@@ -22,7 +22,7 @@ func void DIA_NAME_BANDIT_EXIT_Info()
 };
 
 
-instance DIA_NAME_BANDIT_SCHAFBLEIBTHIER(C_Info)
+instance DIA_NAME_BANDIT_SCHAFBLEIBTHIER (C_Info)
 {
 	npc = BDT_1025_Bandit_H;
 	nr = 30;
@@ -37,18 +37,18 @@ func int DIA_NAME_BANDIT_SCHAFBLEIBTHIER_Condition()
 	return TRUE;
 };
 
-func void DIA_NAME_BANDIT_SCHAFBLEIBTHIER_Info()
+func void DIA_NAME_BANDIT_SCHAFLEIBTHIER_Info()
 {
-	AI_Output(self,other,"DIA_NAME_BANDIT_SCHAFBLEIBTHIER_09_00");	//Эй ты, кретин! Ты пришел сюда случайно не за овцой, а?
-	if(MIS_Akil_SchafDiebe == 0)
+	AI_Output(self,other, " DIA_NAME_BANDIT_SCHAFBLEIBTHIER_09_00 " );	// Hey you nerd! You didn't come here by chance for a sheep, did you?
+	if (MIS_Akil_SchafDiebe ==  0 )
 	{
-		AI_Output(other,self,"DIA_NAME_BANDIT_SCHAFBLEIBTHIER_15_01");	//Овцой? Какой овцой?
-		AI_Output(self,other,"DIA_NAME_BANDIT_SCHAFBLEIBTHIER_09_02");	//Это хорошо. А теперь проваливай. Здесь тебе нечего делать.
+		AI_Output(other,self, " DIA_NAME_BANDIT_SCHAFBLEIBTHIER_15_01 " );	// Sheep? What sheep?
+		AI_Output(self,other, " DIA_NAME_BANDIT_SCHAFBLEIBTHIER_09_02 " );	// This is good. Now get lost. There is nothing for you to do here.
 	};
 };
 
 
-instance DIA_NAME_BANDIT_SchafKlau(C_Info)
+instance DIA_NAME_BANDIT_SchafKlau (C_Info)
 {
 	npc = BDT_1025_Bandit_H;
 	nr = 30;
@@ -68,33 +68,33 @@ func int DIA_NAME_BANDIT_SchafKlau_Condition()
 
 func void DIA_NAME_BANDIT_SchafKlau_Info()
 {
-	AI_Output(self,other,"DIA_NAME_BANDIT_SchafKlau_09_00");	//Эй! Я с тобой говорю.
-	Info_AddChoice(DIA_NAME_BANDIT_SchafKlau,Dialog_Ende,DIA_NAME_BANDIT_SchafKlau_weiter);
+	AI_Output(self,other, " DIA_NAME_BANDIT_SchafKlau_09_00 " );	// Hey! I am speaking with you.
+	Info_AddChoice(DIA_NAME_BANDIT_SchafKlau,Dialog_End,DIA_NAME_BANDIT_SchafKlau_weiter);
 };
 
 func void DIA_NAME_BANDIT_SchafKlau_weiter()
 {
 	AI_StopProcessInfos(self);
 	B_Attack(self,other,AR_SuddenEnemyInferno,1);
-	BDT_1025_Bandit_H.aivar[AIV_EnemyOverride] = FALSE;
-	BDT_1026_Bandit_H.aivar[AIV_EnemyOverride] = FALSE;
+	BDT_1025_Bandit_H.aivar[AIV_EnemyOverride] = FALSE ;
+	BDT_1026_Bandit_H.aivar[AIV_EnemyOverride] = FALSE ;
 };
 
 
-instance DIA_NAME_BANDIT_RUECKDASSCHAFRAUS(C_Info)
+instance DIA_NAME_BANDIT_RUECKDASSCHAFRAUS (C_Info)
 {
 	npc = BDT_1025_Bandit_H;
 	nr = 30;
 	condition = DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_Condition;
 	information = DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_Info;
 	permanent = TRUE;
-	description = "Вы украли овцу у фермера Акила.";
+	description = " You stole a sheep from Farmer Akil. " ;
 };
 
 
 func int DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_Condition()
 {
-	if((MIS_Akil_SchafDiebe == LOG_Running) && (DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_NoPerm == FALSE))
+	if ((MY_Akil_SchafDiebe == LOG_Running) && (DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_NoPerm ==  FALSE ))
 	{
 		return TRUE;
 	};
@@ -103,30 +103,30 @@ func int DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_Condition()
 
 var int DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_NoPerm;
 
-func void DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_Info()
+func void DIA_NAME_BANDIT_RETURNDASSCHAFRAUS_Info()
 {
-	AI_Output(other,self,"DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_15_00");	//Вы украли овцу у фермера Акила.
-	AI_Output(self,other,"DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_09_01");	//И что? Что ты будешь делать?
+	AI_Output(other,self, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_15_00 " );	// You stole a sheep from Farmer Akil.
+	AI_Output(self,other, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_09_01 " );	// So what? What will you do?
 	Info_ClearChoices(dia_name_bandit_rueckdasschafraus);
-	Info_AddChoice(dia_name_bandit_rueckdasschafraus,"Ничего. И зачем мне эта тупая овца?",DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_nichts);
-	Info_AddChoice(dia_name_bandit_rueckdasschafraus,"Отдай мне эту овцу, или в глаз получишь.",DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_schaf);
+	Info_AddChoice(dia_name_bandit_rueckdasschafraus, " Nothing. Why do I need this stupid sheep? " ,DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_nichts);
+	Info_AddChoice(dia_name_bandit_rueckdasschafraus, " Give me that sheep or you'll get shot in the eye. " ,DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_schaf);
 };
 
-func void DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_schaf()
+func void DIA_NAME_BANDIT_RETURNDASSCHAFRAUS_sheep()
 {
-	AI_Output(other,self,"DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_schaf_15_00");	//Отдай мне эту овцу, или в глаз получишь.
-	AI_Output(self,other,"DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_schaf_09_01");	//Тогда берегись, приятель. Сейчас тебе придется несладко.
+	AI_Output(other,self, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_schaf_15_00 " );	// Give me that sheep, or you'll get shot in the eye.
+	AI_Output(self,other, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_schaf_09_01 " );	// Then watch out, mate. Now you're going to have a hard time.
 	AI_StopProcessInfos(self);
-	DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_NoPerm = TRUE;
+	DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_NoPerm = TRUE ;
 	B_Attack(self,other,AR_SuddenEnemyInferno,1);
-	BDT_1025_Bandit_H.aivar[AIV_EnemyOverride] = FALSE;
-	BDT_1026_Bandit_H.aivar[AIV_EnemyOverride] = FALSE;
+	BDT_1025_Bandit_H.aivar[AIV_EnemyOverride] = FALSE ;
+	BDT_1026_Bandit_H.aivar[AIV_EnemyOverride] = FALSE ;
 };
 
-func void DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_nichts()
+func void DIA_NAME_BANDIT_RETURNTHISSCAFRAUS_nothing()
 {
-	AI_Output(other,self,"DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_nichts_15_00");	//Ничего. И зачем мне эта тупая овца?
-	AI_Output(self,other,"DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_nichts_09_01");	//Тогда не играй меня на нервах. Проваливай!
+	AI_Output(other,self, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_nichts_15_00 " );	// Nothing. And why do I need this stupid sheep?
+	AI_Output(self,other, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_nichts_09_01 " );	// Then don't get on my nerves. Get out!
 	hero.aivar[AIV_LastDistToWP] = Npc_GetDistToWP(hero,"NW_FOREST_CAVE1_IN_01");
 	AI_StopProcessInfos(self);
 };
