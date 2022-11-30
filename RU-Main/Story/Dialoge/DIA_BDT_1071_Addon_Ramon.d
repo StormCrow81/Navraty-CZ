1b54e2daf5675cfba9d7d3c0dffb89010f85a002
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-11-30 19:15:34 +0200

    Update DIA_BDT_1071_Addon_Ramon.d

--- RU-Main/Story/Dialoge/DIA_BDT_1071_Addon_Ramon.d
+++ RU-Main/Story/Dialoge/DIA_BDT_1071_Addon_Ramon.d
@@ -39,35 +39,35 @@ func int DIA_Addon_Ramon_FirstWarn_Condition()
 	if(Npc_GetDistToWP(other,BDT_1071_Checkpoint) <= 700)
 	{
 		Npc_SetRefuseTalk(self,5);
-		return FALSE;
+		return  FALSE ;
 	};
 	if((self.aivar[AIV_Guardpassage_Status] == GP_NONE) && (self.aivar[AIV_PASSGATE] == FALSE) && (Hlp_StrCmp(Npc_GetNearestWP(self),self.wp) == TRUE) && (Npc_RefuseTalk(self) == FALSE))
 	{
 		return TRUE;
 	};
 
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_Addon_Ramon_FirstWarn_Info()
 {
-	if(Npc_IsDead(Franco))
+	if (Npc_IsDead(Franco))
 	{
-		AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_07_00");	//Эй, что там случилось?
-		AI_Output(other,self,"DIA_Addon_Ramon_FirstWarn_15_01");	//Я укокошил Франко.
-		AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_07_02");	//Так что? Значит это теперь ТВОЯ забота - заставлять этих лентяев выполнять свою работу.
-		AI_Output(other,self,"DIA_Addon_Ramon_FirstWarn_15_03");	//Эй, я здесь не для того, чтобы нянчиться с охотниками. Мне надо попасть в лагерь.
-		AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_07_04");	//Забудь об этом. Ты убил Франко - теперь ты старший, так делай свою работу.
-		AI_Output(other,self,"DIA_Addon_Ramon_FirstWarn_15_05");	//Если я старший, то я решаю, кто входит следующим.
-		AI_Output(other,self,"DIA_Addon_Ramon_FirstWarn_15_06");	//А я решил, что следующим вхожу я.
-		AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_07_07");	//(смеется) Это не так просто. Мы НИКОГО не просили, так что НИКТО не войдет.
-		AI_Output(other,self,"DIA_Addon_Ramon_FirstWarn_15_08");	//Торус может послать нового командира охотникам, тогда там освободится место для меня.
-		AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_07_09");	//(смеется) Ты не отстанешь, верно? Ну ладно, тогда давай сюда.
-		AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_07_10");	//Торус позаботится о том, чтобы охотники не остались без командира.
-		AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_07_11");	//Чего ты ждешь? И не забудь взять кирку.
-		self.aivar[AIV_PASSGATE] = TRUE;
+		AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_07_00 " );	// Hey, what happened there?
+		AI_Output(other,self, " DIA_Addon_Ramon_FirstWarn_15_01 " );	// I killed Franko.
+		AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_07_02 " );	// So what? So it's YOUR job now to get these lazy people to do their job.
+		AI_Output(other,self, " DIA_Addon_Ramon_FirstWarn_15_03 " );	// Hey, I'm not here to babysit hunters. I need to get to the camp.
+		AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_07_04 " );	// Forget it. You killed Franco - now you're the senior, so do your job.
+		AI_Output(other,self, " DIA_Addon_Ramon_FirstWarn_15_05 " );	// If I'm the eldest, then I decide who enters next.
+		AI_Output(other,self, " DIA_Addon_Ramon_FirstWarn_15_06 " );	// And I figured I'd be next in.
+		AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_07_07 " );	// (laughs) It's not that easy. We didn't ask for ANYONE, so NOBODY will come in.
+		AI_Output(other,self, " DIA_Addon_Ramon_FirstWarn_15_08 " );	// Torus can send a new hunter commander to make room for me.
+		AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_07_09 " );	// (laughs) You won't be left behind, right? Okay, then come here.
+		AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_07_10 " );	// Torus will make sure that the hunters are not left without a commander.
+		AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_07_11 " );	// What are you waiting for? And don't forget to take a pickaxe.
+		self.aivar[ AIV_PASSGATE ] = TRUE ;
 		Player_HasTalkedToBanditCamp = TRUE;
-		B_GivePlayerXP(XP_Addon_Hinein);
+		B_GivePlayerXP(XP_Addon_In);
 		AI_StopProcessInfos(self);
 		AI_Teleport(Carlos,"BL_WAIT_FINN");
 		B_StartOtherRoutine(Carlos,"START");
@@ -75,27 +75,27 @@ func void DIA_Addon_Ramon_FirstWarn_Info()
 	}
 	else if(Ramon_News == FALSE)
 	{
-		AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_07_12");	//Эй, чего тебе нужно? Не могу вообразить, что у тебя есть разрешение на проход в лагерь.
+		AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_07_12 " );	// Hey, what do you need? I can't imagine you have permission to enter the camp.
 		if(Npc_KnowsInfo(other,DIA_Addon_Franco_HI))
 		{
-			AI_Output(other,self,"DIA_Addon_Ramon_FirstWarn_15_13");	//Меня послал Франко. Он хочет узнать, не нужен ли кто-нибудь в лагере.
+			AI_Output(other,self, " DIA_Addon_Ramon_FirstWarn_15_13 " );	// Franco sent me. He wants to know if anyone is needed in the camp.
 		};
-		AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_07_14");	//Да, нужен. Вчера еще один парень дал дуба.
-		AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_07_15");	//Этот идиот поссорился с Эстебаном.
-		AI_Output(other,self,"DIA_Addon_Ramon_FirstWarn_15_16");	//Кто такой Эстебан?
-		AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_07_17");	//Командир нижнего лагеря. Он определяет, кто пойдет в шахту.
-		AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_07_18");	//Так что скажи Франко, что Торусу нужен новый человек. Понял?
+		AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_07_14 " );	// Yes, needed. Yesterday another guy gave oak.
+		AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_07_15 " );	// This idiot had a fight with Esteban.
+		AI_Output(other,self, " DIA_Addon_Ramon_FirstWarn_15_16 " );	// Who is Esteban?
+		AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_07_17 " );	// Commander of the lower camp. He determines who goes to the mine.
+		AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_07_18 " );	// So tell Franco that Torus needs a new man. Understood?
 		Log_CreateTopic(Topic_Addon_Franco,LOG_MISSION);
 		Log_SetTopicStatus(Topic_Addon_Franco,LOG_Running);
-		B_LogEntry(Topic_Addon_Franco,"Торусу нужен еще один человек. Это может заинтересовать Франко.");
+		B_LogEntry(Topic_Addon_Franco, " Torus needs another person. Franco might be interested in this. " );
 		Info_ClearChoices(DIA_Addon_Ramon_FirstWarn);
-		Info_AddChoice(DIA_Addon_Ramon_FirstWarn,"Торус? Сам Торус?",DIA_Addon_Ramon_FirstWarn_YES);
-		Info_AddChoice(DIA_Addon_Ramon_FirstWarn,"Торус? Никогда про него слышал...",DIA_Addon_Ramon_FirstWarn_NO);
+		Info_AddChoice(DIA_Addon_Ramon_FirstWarn, " Torus? Torus itself? " ,DIA_Addon_Ramon_FirstWarn_YES);
+		Info_AddChoice(DIA_Addon_Ramon_FirstWarn, " Torus? Never heard of him... " ,DIA_Addon_Ramon_FirstWarn_NO);
 		Ramon_News = TRUE;
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_07_19");	//Эй, притормози. Ты сюда не попадешь, так что проваливай!
+		AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_07_19 " );	// Hey, slow down. You won't make it here, so get out!
 		AI_StopProcessInfos(self);
 	};
 	other.aivar[AIV_LastDistToWP] = Npc_GetDistToWP(other,BDT_1071_Checkpoint);
@@ -105,23 +105,23 @@ func void DIA_Addon_Ramon_FirstWarn_Info()
 func void DIA_Addon_Ramon_FirstWarn_YES()
 {
 	B_GivePlayerXP(100);
-	AI_Output(other,self,"DIA_Addon_Ramon_FirstWarn_YES_15_00");	//Торус? Сам Торус?
-	AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_YES_07_01");	//Да. САМ Торус. Тот, кто принимает новых людей.
-	AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_YES_07_02");	//Тот, кто решает, когда новый человек уходит с болота и приходит в лагерь.
-	AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_YES_07_03");	//Может, ты хочешь сказать, что ты и раньше был знаком с Торусом?
+	AI_Output(other,self, " DIA_Addon_Ramon_FirstWarn_YES_15_00 " );	// Torus? Thorus himself?
+	AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_YES_07_01 " );	// Yes. SAM Torus. The one who accepts new people.
+	AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_YES_07_02 " );	// The one who decides when a new person leaves the swamp and comes to the camp.
+	AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_YES_07_03 " );	// Maybe you're saying that you've known Thorus before?
 	AI_Output(other,self,"DIA_Addon_Ramon_FirstWarn_YES_15_04");	//Ну... да...
-	AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_YES_07_05");	//Послушай, ВСЕ знают Торуса.
-	AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_YES_07_06");	//И это значит, что ты не попадешь внутрь - и я даже не пошлю ему весточку!
+	AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_YES_07_05 " );	// Look, EVERYONE knows Torus.
+	AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_YES_07_06 " );	// And that means you won't get in - and I won't even send word to him!
 	Info_ClearChoices(DIA_Addon_Ramon_FirstWarn);
 };
 
 func void DIA_Addon_Ramon_FirstWarn_NO()
 {
-	AI_Output(other,self,"DIA_Addon_Ramon_FirstWarn_NO_15_00");	//Торус? Никогда про него слышал...
-	AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_NO_07_01");	//О, парень, ты его не знаешь? Он был начальником стражи в Старом Лагере.
-	AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_NO_07_02");	//Все доверяют этому человеку.
-	AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_NO_07_03");	//Он вызывает новых людей и решает, когда новички должны попасть в лагерь.
-	AI_Output(self,other,"DIA_Addon_Ramon_FirstWarn_NO_07_04");	//Если ты проживешь достаточно долго, ты с ним познакомишься.
+	AI_Output(other,self, " DIA_Addon_Ramon_FirstWarn_NO_15_00 " );	// Torus? Never heard of him...
+	AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_NO_07_01 " );	// Oh boy, you don't know him? He was head of the guard at the Old Camp.
+	AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_NO_07_02 " );	// Everyone trusts this person.
+	AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_NO_07_03 " );	// He calls new people and decides when the newcomers should get into the camp.
+	AI_Output(self,other, " DIA_Addon_Ramon_FirstWarn_NO_07_04 " );	// If you live long enough, you'll get to know him.
 	Info_ClearChoices(DIA_Addon_Ramon_FirstWarn);
 };
 
@@ -139,16 +139,16 @@ instance DIA_Addon_Ramon_SecondWarn(C_Info)
 
 func int DIA_Addon_Ramon_SecondWarn_Condition()
 {
-	if((self.aivar[AIV_Guardpassage_Status] == GP_FirstWarnGiven) && (self.aivar[AIV_PASSGATE] == FALSE) && (Hlp_StrCmp(Npc_GetNearestWP(self),self.wp) == TRUE) && (Npc_GetDistToWP(other,BDT_1071_Checkpoint) < (other.aivar[AIV_LastDistToWP] - 50)))
+	if ((self.aivar[AIV_GuardPass_Status] == GP_FirstWarnGiven) && (self.aivar[ AIV_PASSGATE ] ==  FALSE ) && (Help_StrengthCmp(Npc_GetNearestWP(self),self.wp) ==  TRUE ) && (Npc_GetDistToWP(other,BDT_1071_Checkpoint); < ( other . aivar [ AIV_LastDistToWP ] -  50 ))) .
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_Addon_Ramon_SecondWarn_Info()
 {
-	AI_Output(self,other,"DIA_Addon_Ramon_SecondWarn_07_00");	//Следующий шаг будет твоим последним!
+	AI_Output(self,other, " DIA_Addon_Ramon_SecondWarn_07_00 " );	// The next step will be your last!
 	other.aivar[AIV_LastDistToWP] = Npc_GetDistToWP(other,BDT_1071_Checkpoint);
 	self.aivar[AIV_Guardpassage_Status] = GP_SecondWarnGiven;
 	AI_StopProcessInfos(self);
@@ -172,14 +172,14 @@ func int DIA_Addon_Ramon_Attack_Condition()
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_Addon_Ramon_Attack_Info()
 {
 	other.aivar[AIV_LastDistToWP] = 0;
-	self.aivar[AIV_Guardpassage_Status] = GP_NONE;
-	AI_Output(self,other,"DIA_Addon_Ramon_Attack_07_00");	//Некоторые люди совершенно необучаемы...
+	self.aivar[AIV_Guardpassage_Status] = GP_NONE ;
+	AI_Output(self,other, " DIA_Addon_Ramon_Attack_07_00 " );	// Some people are completely unteachable...
 	AI_StopProcessInfos(self);
 	B_Attack(self,other,AR_GuardStopsIntruder,0);
 };
@@ -192,23 +192,23 @@ instance DIA_Addon_Ramon_News(C_Info)
 	condition = DIA_Addon_Ramon_News_Condition;
 	information = DIA_Addon_Ramon_News_Info;
 	permanent = FALSE;
-	description = "Я тот новый человек.";
+	description = " I'm that new person. " ;
 };
 
 
 func int DIA_Addon_Ramon_News_Condition()
 {
-	if(!Npc_IsDead(Franco))
+	if ( ! Npc_IsDead(Franco))
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_Addon_Ramon_News_Info()
 {
-	AI_Output(other,self,"DIA_Addon_Ramon_News_15_00");	//Я тот новый человек.
-	AI_Output(self,other,"DIA_Addon_Ramon_News_07_01");	//Эй, не пытайся меня обдурить. Все равно Франко решает, кто из вас пойдет в лагерь.
+	AI_Output(other,self, " DIA_Addon_Ramon_News_15_00 " );	// I'm that new person.
+	AI_Output(self,other, " DIA_Addon_Ramon_News_07_01 " );	// Hey, don't try to fool me. Anyway, Franco decides which of you will go to the camp.
 };
 
 
@@ -219,23 +219,23 @@ instance DIA_Addon_Ramon_Lie(C_Info)
 	condition = DIA_Addon_Ramon_Lie_Condition;
 	information = DIA_Addon_Ramon_Lie_Info;
 	permanent = FALSE;
-	description = "Франко послал меня...";
+	description = " Franco sent me... " ;
 };
 
 
 func int DIA_Addon_Ramon_Lie_Condition()
 {
-	if(Npc_KnowsInfo(other,DIA_Addon_Ramon_News) && !Npc_IsDead(Franco))
+	if ( Npc_KnowsInfo ( other , DIA_Addon_Ramon_News ) &&  ! Npc_IsDead ( Franco ) )
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_Addon_Ramon_Lie_Info()
 {
-	AI_Output(other,self,"DIA_Addon_Ramon_Lie_15_00");	//Франко послал меня...
-	AI_Output(self,other,"DIA_Addon_Ramon_Lie_07_01");	//Не-а. Проваливай, а не то я переломаю тебе кости...
+	AI_Output(other,self, " DIA_Addon_Ramon_Lie_15_00 " );	// Franco sent me...
+	AI_Output(self,other, " DIA_Addon_Ramon_Lie_07_01 " );	// Nope. Get out, or I'll break your bones...
 	AI_StopProcessInfos(self);
 };
 
@@ -247,23 +247,22 @@ instance DIA_Addon_Ramon_Perm(C_Info)
 	condition = DIA_Addon_Ramon_Perm_Condition;
 	information = DIA_Addon_Ramon_Perm_Info;
 	permanent = TRUE;
-	description = "Ситуация под контролем?";
+	description = " Situation under control? " ;
 };
 
 
 func int DIA_Addon_Ramon_Perm_Condition()
 {
-	if(Npc_KnowsInfo(other,DIA_Addon_Ramon_News) && !Npc_IsDead(Franco))
+	if ( Npc_KnowsInfo ( other , DIA_Addon_Ramon_News ) &&  ! Npc_IsDead ( Franco ) )
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_Addon_Ramon_Perm_Info()
 {
-	AI_Output(other,self,"DIA_Addon_Ramon_Perm_15_00");	//Ситуация под контролем?
-	AI_Output(self,other,"DIA_Addon_Ramon_Perm_07_01");	//Здесь все в порядке, пока в лагерь не попытается пройти кто-нибудь, кому НЕ ПОЛОЖЕНО.
+	AI_Output(other,self, " DIA_Addon_Ramon_Perm_15_00 " );	// Is the situation under control?
+	AI_Output(self,other, " DIA_Addon_Ramon_Perm_07_01 " );	// Everything is fine here until someone who is NOT SHOULD try to enter the camp.
 	AI_StopProcessInfos(self);
 };
-
