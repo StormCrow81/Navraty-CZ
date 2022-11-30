a8b129ee5a345ed1d4dd1a991ef08a744a6aec41
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-11-30 19:27:51 +0200

    Update DIA_BDT_10026_Addon_Wache.d

--- RU-Main/Story/Dialoge/DIA_BDT_10026_Addon_Wache.d
+++ RU-Main/Story/Dialoge/DIA_BDT_10026_Addon_Wache.d
@@ -28,7 +28,7 @@ instance DIA_Addon_BDT_10026_Wache_Hi(C_Info)
 	condition = DIA_Addon_10026_Wache_Hi_Condition;
 	information = DIA_Addon_10026_Wache_Hi_Info;
 	permanent = TRUE;
-	description = "Как дела?";
+	description = " How are you? " ;
 };
 
 
@@ -39,31 +39,31 @@ func int DIA_Addon_10026_Wache_Hi_Condition()
 
 func void DIA_Addon_10026_Wache_Hi_Info()
 {
-	AI_Output(other,self,"DIA_Addon_BDT_10026_Wache_Hi_15_00");	//Как дела?
-	if(Sklaven_Flucht == FALSE)
+	AI_Output(other,self, " DIA_Addon_BDT_10026_Wache_Hi_15_00 " );	// How are you?
+	if (slave_escape ==  FALSE )
 	{
 		CreateInvItems(self,ItFo_Bacon,1);
-		AI_Output(self,other,"DIA_Addon_BDT_10026_Wache_Hi_13_01");	//Жаль, что у нас так мало рабов. Нам следует достать новых.
-		AI_Output(self,other,"DIA_Addon_BDT_10026_Wache_Hi_13_02");	//С этими ребятами покончено. Возможно, потому что они почти ничего не ели. (смеется)
+		AI_Output(self,other, " DIA_Addon_BDT_10026_Wache_Hi_13_01 " );	// Too bad we have so few slaves. We should get new ones.
+		AI_Output(self,other, " DIA_Addon_BDT_10026_Wache_Hi_13_02 " );	// These guys are done. Perhaps because they hardly ate anything. (laughs)
 		B_UseItem(self,ItFo_Bacon);
-		AI_Output(self,other,"DIA_Addon_BDT_10026_Wache_Hi_13_03");	//Но я лучше отправлю рудокопов работать дальше, чем дам этим собакам хоть кусок мяса.
+		AI_Output(self,other, " DIA_Addon_BDT_10026_Wache_Hi_13_03 " );	// But I'd rather send the miners off to work than give those dogs a piece of meat.
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_Addon_BDT_10026_Wache_Hi_13_04");	//Рабы ушли, очень жаль. Ну что ж, тогда нам придется снова загнать туда рудокопов.
+		AI_Output(self,other, " DIA_Addon_BDT_10026_Wache_Hi_13_04 " );	// The slaves are gone, too bad. Well, then we'll have to drive the miners there again.
 	};
 	AI_StopProcessInfos(self);
 };
 
 
-instance DIA_Addon_BDT_10026_Wache_Gruft(C_Info)
+instance DIA_Addon_BDT_10026_Guard_Crypt (C_Info)
 {
 	npc = BDT_10026_Addon_Wache;
 	nr = 2;
 	condition = DIA_Addon_10026_Wache_Gruft_Condition;
 	information = DIA_Addon_10026_Wache_Gruft_Info;
 	permanent = TRUE;
-	description = "Ты знаешь что-нибудь про гробницу?";
+	description = " Do you know anything about the tomb? " ;
 };
 
 
@@ -72,10 +72,9 @@ func int DIA_Addon_10026_Wache_Gruft_Condition()
 	return TRUE;
 };
 
-func void DIA_Addon_10026_Wache_Gruft_Info()
+func void DIA_Addon_10026_Guard_Crypt_Info()
 {
-	AI_Output(other,self,"DIA_Addon_BDT_10026_Wache_Gruft_15_00");	//Ты знаешь что-нибудь про гробницу?
-	AI_Output(self,other,"DIA_Addon_BDT_10026_Wache_Gruft_13_01");	//Да. Она в конце этого прохода.
-	AI_Output(self,other,"DIA_Addon_BDT_10026_Wache_Gruft_13_02");	//(спокойно) Просто следуй по трупам - и ты найдешь ее.
+	AI_Output(other,self, " DIA_Addon_BDT_10026_Wache_Gruft_15_00 " );	// Do you know anything about the tomb?
+	AI_Output(self,other, " DIA_Addon_BDT_10026_Wache_Gruft_13_01 " );	// Yes. She's at the end of this passage.
+	AI_Output(self,other, " DIA_Addon_BDT_10026_Wache_Gruft_13_02 " );	// (calmly) Just follow the corpses and you'll find her.
 };
-
