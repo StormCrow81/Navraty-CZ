c70d169a55f8c16ccfed0b7efdc0567ff3f5d0f4
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-11-30 19:47:00 +0200

    Update DIA_BDT_1073_Addon_Sancho.d

--- RU-Main/Story/Dialoge/DIA_BDT_1073_Addon_Sancho.d
+++ RU-Main/Story/Dialoge/DIA_BDT_1073_Addon_Sancho.d
@@ -34,7 +34,7 @@ instance DIA_Addon_Sancho_PICKPOCKET(C_Info)
 
 func int DIA_Addon_Sancho_PICKPOCKET_Condition()
 {
-	return C_Beklauen(50,40);
+	return  C_Robbery ( 50 , 40 );
 };
 
 func void DIA_Addon_Sancho_PICKPOCKET_Info()
@@ -46,7 +46,7 @@ func void DIA_Addon_Sancho_PICKPOCKET_Info()
 
 func void DIA_Addon_Sancho_PICKPOCKET_DoIt()
 {
-	B_Beklauen();
+	B_Robbery();
 	Info_ClearChoices(DIA_Addon_Sancho_PICKPOCKET);
 };
 
@@ -56,7 +56,7 @@ func void DIA_Addon_Sancho_PICKPOCKET_BACK()
 };
 
 
-instance DIA_Addon_Sancho_HI(C_Info)
+instance DIA_Addon_Sancho_HI (C_Info)
 {
 	npc = BDT_1073_Addon_Sancho;
 	nr = 2;
@@ -74,11 +74,11 @@ func int DIA_Addon_Sancho_HI_Condition()
 
 func void DIA_Addon_Sancho_HI_Info()
 {
-	AI_Output(self,other,"DIA_Addon_Sancho_HI_06_00");	//Черт, еще один новичок. (смеется) Тоже за золотом, да?
+	AI_Output(self,other, " DIA_Addon_Sancho_HI_06_00 " );	// Damn, another newbie. (laughs) Also for gold, right?
 	AI_Output(other,self,"DIA_Addon_Sancho_HI_15_01");	//За золотом?
-	AI_Output(self,other,"DIA_Addon_Sancho_HI_06_02");	//(смеется) Не пытайся обдурить меня!
-	AI_Output(self,other,"DIA_Addon_Sancho_HI_06_03");	//Все, кто приходят сюда, хотят попасть в шахту.
-	AI_Output(self,other,"DIA_Addon_Sancho_HI_06_04");	//Но не думай, что это будет легко!
+	AI_Output(self,other, " DIA_Addon_Sancho_HI_06_02 " );	// (laughs) Don't try to fool me!
+	AI_Output(self,other, " DIA_Addon_Sancho_HI_06_03 " );	// Everyone who comes here wants to get into the mine.
+	AI_Output(self,other, " DIA_Addon_Sancho_HI_06_04 " );	// But don't think it will be easy!
 	if(SC_KnowsRavensGoldmine == FALSE)
 	{
 		B_LogEntry(TOPIC_Addon_RavenKDW,LogText_Addon_RavensGoldmine);
@@ -96,7 +96,7 @@ instance DIA_Addon_Sancho_Lager(C_Info)
 	condition = DIA_Addon_Sancho_Lager_Condition;
 	information = DIA_Addon_Sancho_Lager_Info;
 	permanent = FALSE;
-	description = "Как мне попасть в лагерь?";
+	description = " How do I get to the camp? " ;
 };
 
 
@@ -107,16 +107,16 @@ func int DIA_Addon_Sancho_Lager_Condition()
 
 func void DIA_Addon_Sancho_Lager_Info()
 {
-	AI_Output(other,self,"DIA_Addon_Sancho_Lager_15_00");	//Как мне попасть в лагерь?
-	AI_Output(self,other,"DIA_Addon_Sancho_Lager_06_01");	//Просто иди по настилу!
-	if(!Npc_IsDead(Franco))
+	AI_Output(other,self, " DIA_Addon_Sancho_Lager_15_00 " );	// How do I get to the camp?
+	AI_Output(self,other, " DIA_Addon_Sancho_Lager_06_01 " );	// Just walk on the deck!
+	if ( ! Npc_IsDead(Franco))
 	{
-		AI_Output(self,other,"DIA_Addon_Sancho_Lager_06_02");	//Но если ты захочешь попасть в шахту, тебе придется иметь дело с Франко.
-		AI_Output(self,other,"DIA_Addon_Sancho_Lager_06_03");	//Он считает, что все новички должны сначала немного поработать снаружи!
+		AI_Output(self,other, " DIA_Addon_Sancho_Lager_06_02 " );	// But if you want to get into the mine, you'll have to deal with Franco.
+		AI_Output(self,other, " DIA_Addon_Sancho_Lager_06_03 " );	// He thinks all newbies should do some outside work first!
 	};
 	Log_CreateTopic(Topic_Addon_Franco,LOG_MISSION);
 	Log_SetTopicStatus(Topic_Addon_Franco,LOG_Running);
-	B_LogEntry(Topic_Addon_Franco,"Все новички должны поработать на болоте, прежде чем получить доступ в шахту.");
+	B_LogEntry(Topic_Addon_Franco, " All beginners must work in the swamp before gaining access to the mine. " );
 };
 
 
@@ -127,85 +127,85 @@ instance DIA_Addon_Sancho_Mine(C_Info)
 	condition = DIA_Addon_Sancho_Mine_Condition;
 	information = DIA_Addon_Sancho_Mine_Info;
 	permanent = FALSE;
-	description = "Расскажи мне про шахту...";
+	description = " Tell me about the mine... " ;
 };
 
 
 func int DIA_Addon_Sancho_Mine_Condition()
 {
-	if(!Npc_IsDead(Franco))
+	if ( ! Npc_IsDead(Franco))
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_Addon_Sancho_Mine_Info()
 {
-	AI_Output(other,self,"DIA_Addon_Sancho_Mine_15_00");	//Расскажи мне про шахту...
-	AI_Output(self,other,"DIA_Addon_Sancho_Mine_06_01");	//Я дам тебе совет: если ты хочешь попасть туда, ты должен понравиться Франко. Он тут главный.
-	AI_Output(self,other,"DIA_Addon_Sancho_Mine_06_02");	//Он решает, кто пойдет туда следующим.
-	AI_Output(self,other,"DIA_Addon_Sancho_Mine_06_03");	//И он пустит тебя туда, только если ты не будешь тут лодырничать.
-	AI_Output(self,other,"DIA_Addon_Sancho_Mine_06_04");	//Так что пойди к нему и попроси у него какую-нибудь работу!
-	B_LogEntry(Topic_Addon_Franco,"Франко решает, кто попадет в лагерь. Также он занимается распределением заданий.");
+	AI_Output(other,self, " DIA_Addon_Sancho_Mine_15_00 " );	// Tell me about the mine...
+	AI_Output(self,other, " DIA_Addon_Sancho_Mine_06_01 " );	// I'll give you a piece of advice: if you want to get there, Franco must like you. He's in charge here.
+	AI_Output(self,other, " DIA_Addon_Sancho_Mine_06_02 " );	// He decides who goes there next.
+	AI_Output(self,other, " DIA_Addon_Sancho_Mine_06_03 " );	// And he'll only let you in there if you don't loaf around here.
+	AI_Output(self,other, " DIA_Addon_Sancho_Mine_06_04 " );	// So go to him and ask him for some work!
+	B_LogEntry(Topic_Addon_Franco, " Franco decides who goes to the camp. He also distributes tasks. " );
 };
 
 
-instance DIA_Addon_Sancho_Franco(C_Info)
+instance DIA_Addon_Sancho_Franco (C_Info)
 {
 	npc = BDT_1073_Addon_Sancho;
 	nr = 4;
 	condition = DIA_Addon_Sancho_Franco_Condition;
 	information = DIA_Addon_Sancho_Franco_Info;
 	permanent = FALSE;
-	description = "Где я могу найти Франко?";
+	description = " Where can I find Franco? " ;
 };
 
 
 func int DIA_Addon_Sancho_Franco_Condition()
 {
-	if(Npc_KnowsInfo(other,DIA_Addon_Sancho_Mine) && !Npc_IsDead(Franco))
+	if ( Npc_KnowsInfo ( other , DIA_Addon_Sancho_Mine ) &&  ! Npc_IsDead ( Franco ) )
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_Addon_Sancho_Franco_Info()
 {
-	AI_Output(other,self,"DIA_Addon_Sancho_Franco_15_00");	//Где я могу найти Франко?
-	AI_Output(self,other,"DIA_Addon_Sancho_Franco_06_01");	//Обычно он торчит во дворе перед лагерем.
-	AI_Output(self,other,"DIA_Addon_Sancho_Franco_06_02");	//Будь с ним в хороших отношениях - и ты получишь какую-нибудь работу получше той, что делаю я!
-	B_LogEntry(Topic_Addon_Franco,"Франко можно найти перед лагерем.");
+	AI_Output(other,self, " DIA_Addon_Sancho_Franco_15_00 " );	// Where can I find Franco?
+	AI_Output(self,other, " DIA_Addon_Sancho_Franco_06_01 " );	// Usually he sticks out in the courtyard in front of the camp.
+	AI_Output(self,other, " DIA_Addon_Sancho_Franco_06_02 " );	// Be on good terms with him and you'll get a better job than I do!
+	B_LogEntry(Topic_Addon_Franco, " Franco can be found in front of the camp. " );
 };
 
 
-instance DIA_Addon_Sancho_Spitzel(C_Info)
+instance DIA_Addon_Sancho_Spitzel (C_Info)
 {
 	npc = BDT_1073_Addon_Sancho;
 	nr = 5;
 	condition = DIA_Addon_Sancho_Spitzel_Condition;
 	information = DIA_Addon_Sancho_Spitzel_Info;
 	permanent = FALSE;
-	description = "Ты должен все время здесь сидеть?";
+	description = " Are you supposed to sit here all the time? " ;
 };
 
 
 func int DIA_Addon_Sancho_Spitzel_Condition()
 {
-	if(Npc_KnowsInfo(other,DIA_Addon_Sancho_Franco) || Npc_IsDead(Franco))
+	if ( Npc_KnowsInfo ( other , DIA_Addon_Sancho_Franco ) || Npc_IsDead ( Franco ))
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_Addon_Sancho_Spitzel_Info()
 {
-	AI_Output(other,self,"DIA_Addon_Sancho_Spitzel_15_00");	//Ты должен все время здесь сидеть?
-	AI_Output(self,other,"DIA_Addon_Sancho_Spitzel_06_01");	//Я должен встречать всех новичков, чтобы убедиться, что к нам не пробрались шпионы.
-	AI_Output(self,other,"DIA_Addon_Sancho_Spitzel_06_02");	//Но об этом можно забыть. Я хочу сказать, что мы здесь на краю света - в середине болота.
-	AI_Output(self,other,"DIA_Addon_Sancho_Spitzel_06_03");	//Никто не может найти нас, и никто нас не найдет. Потому что кому нужно посылать сюда шпионов?
+	AI_Output(other,self, " DIA_Addon_Sancho_Spitzel_15_00 " );	// Do you have to sit here all the time?
+	AI_Output(self,other, " DIA_Addon_Sancho_Spitzel_06_01 " );	// I have to meet all the newcomers to make sure we don't have spies sneaking in.
+	AI_Output(self,other, " DIA_Addon_Sancho_Spitzel_06_02 " );	// But you can forget about it. I want to say that we are here at the end of the world - in the middle of a swamp.
+	AI_Output(self,other, " DIA_Addon_Sancho_Spitzel_06_03 " );	// No one can find us, and no one will find us. Because who needs to send spies here?
 };
 
 
@@ -216,17 +216,17 @@ instance DIA_Addon_Sancho_Perm(C_Info)
 	condition = DIA_Addon_Sancho_Perm_Condition;
 	information = DIA_Addon_Sancho_Perm_Info;
 	permanent = TRUE;
-	description = "Какие новости?";
+	description = " What's new? " ;
 };
 
 
 func int DIA_Addon_Sancho_Perm_Condition()
 {
-	if(Npc_KnowsInfo(other,DIA_Addon_Sancho_Spitzel))
+	if ( Npc_KnowsInfo ( other , DIA_Addon_Sancho_Spitzel ))
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 
@@ -235,28 +235,27 @@ var int Comment_Esteban;
 
 func void DIA_Addon_Sancho_Perm_Info()
 {
-	AI_Output(other,self,"DIA_Addon_Sancho_Perm_15_00");	//Какие новости?
-	if(Npc_IsDead(Franco) && (Comment_Franco == FALSE))
+	AI_Output(other,self, " DIA_Addon_Sancho_Perm_15_00 " );	// What's the news?
+	if ( Npc_IsDead ( Frank ) && ( Frank_Comment ==  FALSE ))
 	{
-		AI_Output(self,other,"DIA_Addon_Sancho_Perm_06_01");	//Я слышал, ты убил Франко. Хорошая работа...
-		if(!Npc_IsDead(Carlos))
+		AI_Output(self,other, " DIA_Addon_Sancho_Perm_06_01 " );	// I heard you killed Franco. Good job...
+		if ( ! Npc_IsDead(Carlos))
 		{
-			AI_Output(self,other,"DIA_Addon_Sancho_Perm_06_02");	//...но теперь нам на шею сел этот Карлос. С ним шутки плохи...
+			AI_Output(self,other, " DIA_Addon_Sancho_Perm_06_02 " );	// ...but now this Carlos sat on our necks. He's bad jokes...
 		}
 		else
 		{
-			AI_Output(self,other,"DIA_Addon_Sancho_Perm_06_03");	//Ты покончил и с Карлосом? Приятель, ты и правда опасный убийца, не так ли? Оставь меня в покое.
+			AI_Output(self,other, " DIA_Addon_Sancho_Perm_06_03 " );	// Are you done with Carlos too? Buddy, you really are a dangerous killer, aren't you? Leave me alone.
 		};
 		Comment_Franco = TRUE;
 	}
-	else if(Npc_IsDead(Esteban) && (Comment_Esteban == FALSE))
+	else  if ( Npc_IsDead ( Stephen ) && ( Comment_Stephen ==  FALSE ))
 	{
-		AI_Output(self,other,"DIA_Addon_Sancho_Perm_06_04");	//Говорят, ты убил Эстебана. Приятель, ты явно что-то затеваешь, да?
+		AI_Output(self,other, " DIA_Addon_Sancho_Perm_06_04 " );	// They say you killed Esteban. Dude, you're obviously up to something, right?
 		Comment_Esteban = TRUE;
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_Addon_Sancho_Perm_06_05");	//Да нет ничего нового.
+		AI_Output(self,other, " DIA_Addon_Sancho_Perm_06_05 " );	// Yes, nothing new.
 	};
 };
-
