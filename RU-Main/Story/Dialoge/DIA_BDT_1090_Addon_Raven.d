1afcf6ad77e55e15e74350d5dbbd6bdfb8e55baa
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-11-30 19:58:43 +0200

    Update DIA_BDT_1090_Addon_Raven.d

--- RU-Main/Story/Dialoge/DIA_BDT_1090_Addon_Raven.d
+++ RU-Main/Story/Dialoge/DIA_BDT_1090_Addon_Raven.d
@@ -45,7 +45,7 @@ func void DIA_Addon_Raven_Hi_Info()
 		RavenDoorClosed = TRUE;
 	};
 
-	AI_Output(self,other,"DIA_Addon_Raven_Add_10_00");	//Посмотрите, кто пришел.
+	AI_Output(self,other, " DIA_Addon_Raven_Add_10_00 " );	// Look who's here.
 
 	if(C_BodyStateContains(self,BS_SIT))
 	{
@@ -58,63 +58,63 @@ func void DIA_Addon_Raven_Hi_Info()
 		Npc_RemoveInvItems(other,ItMi_TempelTorKey,1);
 	};
 
-	AI_Output(self,other,"DIA_Addon_Raven_Add_10_01");	//Повелитель предупредил меня, что Иннос пошлет своего слугу.
-	AI_Output(self,other,"DIA_Addon_Raven_Add_10_02");	//Но я не думал, что ты придешь так быстро.
-	AI_Output(self,other,"DIA_Addon_Raven_Add_10_03");	//Что ж, раз ты так спешил, (смеется) то и смерть твоя будет быстрой.
+	AI_Output(self,other, " DIA_Addon_Raven_Add_10_01 " );	// The master warned me that Innos would send his servant.
+	AI_Output(self,other, " DIA_Addon_Raven_Add_10_02 " );	// But I didn't think you'd come so soon.
+	AI_Output(self,other, " DIA_Addon_Raven_Add_10_03 " );	// Well, since you were in such a hurry, (laughs) then your death will be quick.
 	Info_ClearChoices(DIA_Addon_Raven_Hi);
-	Info_AddChoice(DIA_Addon_Raven_Hi,"Ты прав, один из нас умрет. И это будешь ты!",DIA_Addon_Raven_Hi_DU);
-	Info_AddChoice(DIA_Addon_Raven_Hi,"Ты продал душу Белиару!",DIA_Addon_Raven_Hi_Soul);
+	Info_AddChoice(DIA_Addon_Raven_Hi, " You're right, one of us will die. And it will be you! " ,DIA_Addon_Raven_Hi_DU);
+	Info_AddChoice(DIA_Addon_Raven_Hi, " You sold your soul to Beliar! " ,DIA_Addon_Raven_Hi_Soul);
 };
 
 func void DIA_Addon_Raven_Hi_DU()
 {
-	AI_Output(other,self,"DIA_Addon_Raven_Add_15_00");	//Ты прав, один из нас умрет. И это будешь ты!
-	AI_Output(self,other,"DIA_Addon_Raven_Add_10_04");	//Тебе со мной не справиться. У меня меч Белиара!
-	AI_Output(self,other,"DIA_Addon_Raven_Add_10_05");	//Я поведу армию драконов на континент. Вместе мы погрузим мир людей в вечную тьму!
+	AI_Output(other,self, " DIA_Addon_Raven_Add_15_00 " );	// You're right, one of us will die. And it will be you!
+	AI_Output(self,other, " DIA_Addon_Raven_Add_10_04 " );	// You can't handle me. I have Beliar's sword!
+	AI_Output(self,other, " DIA_Addon_Raven_Add_10_05 " );	// I will lead an army of dragons to the continent. Together we will plunge the world of people into eternal darkness!
 };
 
 func void DIA_Addon_Raven_Hi_Soul()
 {
-	AI_Output(other,self,"DIA_Addon_Raven_Add_15_01");	//Ты продал душу Белиару!
-	AI_Output(self,other,"DIA_Addon_Raven_Add_10_06");	//Да, и за хорошую цену. Я стану генералом его армий.
-	AI_Output(self,other,"DIA_Addon_Raven_Add_10_07");	//А как насчет тебя? Тебе известно, сколько стоит ТВОЯ душа?
+	AI_Output(other,self, " DIA_Addon_Raven_Add_15_01 " );	// You sold your soul to Beliar!
+	AI_Output(self,other, " DIA_Addon_Raven_Add_10_06 " );	// Yes, and for a good price. I will become the general of his armies.
+	AI_Output(self,other, " DIA_Addon_Raven_Add_10_07 " );	// What about you? Do you know how much YOUR soul is worth?
 	Info_ClearChoices(DIA_Addon_Raven_Hi);
-	Info_AddChoice(DIA_Addon_Raven_Hi,"Достаточно! Приступим к делу.",DIA_Addon_Raven_Hi_Attack);
-	Info_AddChoice(DIA_Addon_Raven_Hi,"Я лишь делаю то, что должно быть исполнено.",DIA_Addon_Raven_Hi_only);
+	Info_AddChoice(DIA_Addon_Raven_Hi, " Enough! Let's get down to business. " ,DIA_Addon_Raven_Hi_Attack);
+	Info_AddChoice(DIA_Addon_Raven_Hi, " I just do what needs to be done. " ,DIA_Addon_Raven_Hi_only);
 };
 
 func void DIA_Addon_Raven_Hi_only()
 {
-	AI_Output(other,self,"DIA_Addon_Raven_Add_15_02");	//Я лишь делаю то, что должно быть исполнено.
-	AI_Output(self,other,"DIA_Addon_Raven_Add_10_08");	//Вы только послушайте! Ты прислал мне покорного раба, Иннос!
+	AI_Output(other,self, " DIA_Addon_Raven_Add_15_02 " );	// I'm just doing what needs to be done.
+	AI_Output(self,other, " DIA_Addon_Raven_Add_10_08 " );	// Just listen! You sent me a humble slave, Innos!
 	Info_ClearChoices(DIA_Addon_Raven_Hi);
-	Info_AddChoice(DIA_Addon_Raven_Hi,"Достаточно! Приступим к делу.",DIA_Addon_Raven_Hi_Attack);
-	Info_AddChoice(DIA_Addon_Raven_Hi,"Ты уверен, что я служу Инносу?",DIA_Addon_Raven_Hi_Sure);
+	Info_AddChoice(DIA_Addon_Raven_Hi, " Enough! Let's get down to business. " ,DIA_Addon_Raven_Hi_Attack);
+	Info_AddChoice(DIA_Addon_Raven_Hi, " Are you sure I serve Innos? " ,DIA_Addon_Raven_Hi_Sure);
 };
 
 func void DIA_Addon_Raven_Hi_Sure()
 {
-	AI_Output(other,self,"DIA_Addon_Raven_Add_15_03");	//Ты уверен, что я служу Инносу?
-	AI_Output(self,other,"DIA_Addon_Raven_Add_10_09");	//Что? О чем ты говоришь?
-	AI_Output(other,self,"DIA_Addon_Raven_Add_15_04");	//Ты не думал, что я могу быть и исполнителем воли Аданоса?
+	AI_Output(other,self, " DIA_Addon_Raven_Add_15_03 " );	// Are you sure I serve Innos?
+	AI_Output(self,other, " DIA_Addon_Raven_Add_10_09 " );	// What? What are you talking about?
+	AI_Output(other,self, " DIA_Addon_Raven_Add_15_04 " );	// You didn't think that I could be the executor of the will of Adanos?
 	AI_Output(self,other,"DIA_Addon_Raven_Add_10_10");	//Чепуха!
-	AI_Output(other,self,"DIA_Addon_Raven_Add_15_05");	//А может, я тоже служу Белиару? Или лишь самому себе?
-	AI_Output(self,other,"DIA_Addon_Raven_Add_10_11");	//Что ж, в таком случае ты мне даже не противник!
+	AI_Output(other,self, " DIA_Addon_Raven_Add_15_05 " );	// Maybe I also serve Beliar? Or just to yourself?
+	AI_Output(self,other, " DIA_Addon_Raven_Add_10_11 " );	// Well, in that case, you're not even my opponent!
 	Info_ClearChoices(DIA_Addon_Raven_Hi);
-	Info_AddChoice(DIA_Addon_Raven_Hi,"Достаточно! Приступим к делу.",DIA_Addon_Raven_Hi_Attack);
+	Info_AddChoice(DIA_Addon_Raven_Hi, " Enough! Let's get down to business. " ,DIA_Addon_Raven_Hi_Attack);
 };
 
 func void DIA_Addon_Raven_Hi_Attack()
 {
-	AI_Output(other,self,"DIA_Addon_Raven_Add_15_06");	//Достаточно! Приступим к делу.
-	AI_Output(self,other,"DIA_Addon_Raven_Add_10_12");	//Ты так торопишься умереть?...(усмехается) Ну, раз ты настаиваешь!
+	AI_Output(other,self, " DIA_Addon_Raven_Add_15_06 " );	// Enough! Let's get down to business.
+	AI_Output(self,other, " DIA_Addon_Raven_Add_10_12 " );	// Are you in such a hurry to die?...(grins) Well, since you insist!
 	AI_StopProcessInfos(self);
 	RavenCanAway = TRUE;
 	B_Attack(self,other,AR_SuddenEnemyInferno,1);
 };
 
 
-//---------------Равен на плато------------------------------
+// ---------------Equal to plateau------------------------------
 
 instance DIA_BDT_2090_Addon_Raven_EXIT(C_Info)
 {
@@ -135,7 +135,7 @@ func void DIA_BDT_2090_Addon_Raven_EXIT_Info()
 {
 	AI_StopProcessInfos(self);
 	B_Attack(self,other,AR_SuddenEnemyInferno,1);
-	self.aivar[AIV_EnemyOverride] = FALSE;
+	self.aivar[AIV_EnemyOverride] = FALSE ;
 };
 
 instance DIA_BDT_2090_Addon_Raven_Hi(C_Info)
@@ -165,29 +165,29 @@ func void DIA_BDT_2090_Addon_Raven_Hi_Info()
 		AI_TurnToNPC(self,other);
 	};
 
-	AI_Output(self,other,"DIA_BDT_2090_Addon_Raven_Hi_01_00");	//Итак. Значит, ты и есть тот, кого Аданос выбрал в качестве своего избранника!
-	AI_Output(self,other,"DIA_BDT_2090_Addon_Raven_Hi_01_01");	//Признаться, я немного разочарован увиденным. Даже учитывая то, что ты смог добраться сюда.
-	AI_Output(other,self,"DIA_BDT_2090_Addon_Raven_Hi_01_03");	//Ворон?! Так ты и есть то зло, о котором говорил страж перед храмом?
-	AI_Output(other,self,"DIA_BDT_2090_Addon_Raven_Hi_01_04");	//Мне кажется, или ты действительно немного изменился с момента нашей последней встречи?
-	AI_Output(self,other,"DIA_BDT_2090_Addon_Raven_Hi_01_05");	//У меня много имен, и все они мало что значат для меня. Как и моя внешность.
-	AI_Output(other,self,"DIA_BDT_2090_Addon_Raven_Hi_01_06");	//Но ты носишь Коготь Белиара! А его последним хозяином был рудный барон Ворон.
-	AI_Output(other,self,"DIA_BDT_2090_Addon_Raven_Hi_01_07");	//Да и выглядишь ты почти как он. Только немного изменился в лице.
-	AI_Output(self,other,"DIA_BDT_2090_Addon_Raven_Hi_01_08");	//Мой повелитель даровал мне знак его силы и душу того ничтожества, которое посмело носить этот могущественный артефакт.
-	AI_Output(self,other,"DIA_BDT_2090_Addon_Raven_Hi_01_09");	//Остальное можешь узнать у него самого, когда окажешься в царстве мертвых.
+	AI_Output(self,other, " DIA_BDT_2090_Addon_Raven_Hi_01_00 " );	// So. So you are the one whom Adanos chose as his chosen one!
+	AI_Output(self,other, " DIA_BDT_2090_Addon_Raven_Hi_01_01 " );	// Frankly, I'm a little disappointed with what I saw. Even though you were able to get here.
+	AI_Output(other,self, " DIA_BDT_2090_Addon_Raven_Hi_01_03 " );	// Raven?! So you are the evil that the guard in front of the temple spoke of?
+	AI_Output(other,self, " DIA_BDT_2090_Addon_Raven_Hi_01_04 " );	// Is it me or have you really changed a little since we last met?
+	AI_Output(self,other, " DIA_BDT_2090_Addon_Raven_Hi_01_05 " );	// I have many names, and they all mean little to me. As is my appearance.
+	AI_Output(other,self, " DIA_BDT_2090_Addon_Raven_Hi_01_06 " );	// But you carry the Claw of Beliar! And its last owner was the ore baron Raven.
+	AI_Output(other,self, " DIA_BDT_2090_Addon_Raven_Hi_01_07 " );	// And you look almost like him. Only a little changed in the face.
+	AI_Output(self,other, " DIA_BDT_2090_Addon_Raven_Hi_01_08 " );	// My master bestowed upon me the token of his power and the soul of that nonentity who dared to wear this mighty artifact.
+	AI_Output(self,other, " DIA_BDT_2090_Addon_Raven_Hi_01_09 " );	// You can learn the rest from him when you find yourself in the realm of the dead.
 	Info_ClearChoices(DIA_BDT_2090_Addon_Raven_Hi);
-	Info_AddChoice(DIA_BDT_2090_Addon_Raven_Hi,"Так кто же ты на самом деле?",DIA_BDT_2090_Addon_Raven_Hi_Who);
+	Info_AddChoice(DIA_BDT_2090_Addon_Raven_Hi, " So who are you really? " ,DIA_BDT_2090_Addon_Raven_Hi_Who);
 };
 
 func void DIA_BDT_2090_Addon_Raven_Hi_Who()
 {
-	AI_Output(other,self,"DIA_BDT_2090_Addon_Raven_Hi_Who_01_00");	//Так кто же ты на самом деле?
-	AI_Output(self,other,"DIA_BDT_2090_Addon_Raven_Hi_Who_01_01");	//(смеется) Неужели ты так этого и не понял?
-	AI_Output(self,other,"DIA_BDT_2090_Addon_Raven_Hi_Who_01_02");	//Взгляни на себя - и ты поймешь, в чем кроется ответ на твой вопрос.
-	AI_Output(self,other,"DIA_BDT_2090_Addon_Raven_Hi_Who_01_03");	//Издревле боги сражаются друг с другом за власть в этом мире...
-	AI_Output(self,other,"DIA_BDT_2090_Addon_Raven_Hi_Who_01_04");	//Сражаются руками своих избранников. Таких, как ты или я.
-	AI_Output(self,other,"DIA_BDT_2090_Addon_Raven_Hi_Who_01_05");	//Наша с тобой встреча почти ничего не решит в той войне, которая бушует уже на протяжении многих тысячелетий.
-	AI_Output(self,other,"DIA_BDT_2090_Addon_Raven_Hi_Who_01_06");	//Но чем меньше на моем пути будет подобных тебе, тем больше шансов, что мой повелитель одержит в ней окончательную победу!
-	AI_Output(self,other,"DIA_BDT_2090_Addon_Raven_Hi_Who_01_07");	//Пришло время выяснить, кто сегодня достоин оказаться победителем, а кто будет навсегда предан забвению!
+	AI_Output(other,self, " DIA_BDT_2090_Addon_Raven_Hi_Who_01_00 " );	// So who are you really?
+	AI_Output(self,other, " DIA_BDT_2090_Addon_Raven_Hi_Who_01_01 " );	// (laughs) Don't you understand that?
+	AI_Output(self,other, " DIA_BDT_2090_Addon_Raven_Hi_Who_01_02 " );	// Take a look at yourself - and you will understand what is the answer to your question.
+	AI_Output(self,other, " DIA_BDT_2090_Addon_Raven_Hi_Who_01_03 " );	// Since ancient times, the gods have been fighting each other for power in this world...
+	AI_Output(self,other, " DIA_BDT_2090_Addon_Raven_Hi_Who_01_04 " );	// Fight with the hands of their chosen ones. People like you or me.
+	AI_Output(self,other, " DIA_BDT_2090_Addon_Raven_Hi_Who_01_05 " );	// Our meeting with you will solve almost nothing in the war that has been raging for many millennia.
+	AI_Output(self,other, " DIA_BDT_2090_Addon_Raven_Hi_Who_01_06 " );	// But the fewer like you there are in my path, the greater the chance that my master will win the final victory in it!
+	AI_Output(self,other, " DIA_BDT_2090_Addon_Raven_Hi_Who_01_07 " );	// It's time to find out who deserves to be the winner today, and who will be forgotten forever!
 	Info_ClearChoices(DIA_BDT_2090_Addon_Raven_Hi);
 	Info_AddChoice(DIA_BDT_2090_Addon_Raven_Hi,Dialog_Ende,DIA_BDT_2090_Addon_Raven_Hi_Exit);
 };
