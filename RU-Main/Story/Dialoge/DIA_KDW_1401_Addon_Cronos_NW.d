631657626fe27591b01a97a7cdeee093d5dba7c3
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-11-30 22:53:22 +0200

    Update DIA_KDW_1401_Addon_Cronos_NW.d

--- RU-Main/Story/Dialoge/DIA_KDW_1401_Addon_Cronos_NW.d
+++ RU-Main/Story/Dialoge/DIA_KDW_1401_Addon_Cronos_NW.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_Addon_Cronos_EXIT(C_Info)
 {
 	npc = KDW_1401_Addon_Cronos_NW;
@@ -32,7 +33,7 @@ instance DIA_Addon_Cronos_PICKPOCKET(C_Info)
 
 func int DIA_Addon_Cronos_PICKPOCKET_Condition()
 {
-	return C_Beklauen(49,35);
+	return  C_Robbery ( 49 , 35 );
 };
 
 func void DIA_Addon_Cronos_PICKPOCKET_Info()
@@ -44,7 +45,7 @@ func void DIA_Addon_Cronos_PICKPOCKET_Info()
 
 func void DIA_Addon_Cronos_PICKPOCKET_DoIt()
 {
-	B_Beklauen();
+	B_Robbery();
 	Info_ClearChoices(DIA_Addon_Cronos_PICKPOCKET);
 };
 
@@ -53,13 +54,13 @@ func void DIA_Addon_Cronos_PICKPOCKET_BACK()
 	Info_ClearChoices(DIA_Addon_Cronos_PICKPOCKET);
 };
 
-instance DIA_Addon_Cronos_Hallo(C_Info)
+instance DIA_Addon_Cronos_Hallo (C_Info)
 {
 	npc = KDW_1401_Addon_Cronos_NW;
 	nr = 5;
 	condition = DIA_Addon_Cronos_Hallo_Condition;
 	information = DIA_Addon_Cronos_Hallo_Info;
-	description = "Я могу тебя побеспокоить?";
+	description = " Can I disturb you? " ;
 };
 
 
@@ -70,20 +71,20 @@ func int DIA_Addon_Cronos_Hallo_Condition()
 
 func void DIA_Addon_Cronos_Hallo_Info()
 {
-	AI_Output(other,self,"DIA_Addon_Cronos_Hallo_15_00");	//Я могу тебя побеспокоить?
-	AI_Output(self,other,"DIA_Addon_Cronos_Hallo_04_01");	//(удивленно) Это ТЫ? Не думал, что мне еще когда-нибудь доведется увидеть твое лицо.
-	AI_Output(self,other,"DIA_Addon_Cronos_Hallo_04_02");	//Ты храбрец, если решил здесь показаться. Некоторое время назад мы были готовы заживо содрать с тебя кожу.
-	AI_Output(self,other,"DIA_Addon_Cronos_Hallo_04_03");	//К счастью для тебя, с того времени прошло уже несколько недель, так что мы немного успокоились.
+	AI_Output(other,self, " DIA_Addon_Cronos_Hallo_15_00 " );	// Can I disturb you?
+	AI_Output(self,other, " DIA_Addon_Cronos_Hallo_04_01 " );	// (surprised) Is that YOU? I didn't think I'd ever see your face again.
+	AI_Output(self,other, " DIA_Addon_Cronos_Hallo_04_02 " );	// You're brave enough to show yourself here. Some time ago we were ready to skin you alive.
+	AI_Output(self,other, " DIA_Addon_Cronos_Hallo_04_03 " );	// Luckily for you, it's been a few weeks since then, so we've calmed down a bit.
 };
 
 
-instance DIA_Addon_Cronos_WasMachstdu(C_Info)
+instance DIA_Addon_Cronos_WasMachstdu (C_Info)
 {
 	npc = KDW_1401_Addon_Cronos_NW;
 	nr = 5;
 	condition = DIA_Addon_Cronos_WasMachstdu_Condition;
 	information = DIA_Addon_Cronos_WasMachstdu_Info;
-	description = "Что ты здесь делаешь?";
+	description = " What are you doing here? " ;
 };
 
 
@@ -93,26 +94,26 @@ func int DIA_Addon_Cronos_WasMachstdu_Condition()
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_Addon_Cronos_WasMachstdu_Info()
 {
-	AI_Output(other,self,"DIA_Addon_Cronos_WasMachstdu_15_00");	//Что ты здесь делаешь?
-	AI_Output(self,other,"DIA_Addon_Cronos_WasMachstdu_04_01");	//Моя задача - выяснить как можно больше об охранниках храма.
-	AI_Output(self,other,"DIA_Addon_Cronos_WasMachstdu_04_02");	//Непростая задача, скажу я тебе. Свойства этих каменных стражей мне совершенно незнакомы.
-	AI_Output(self,other,"DIA_Addon_Cronos_WasMachstdu_04_03");	//Они могут стоять, совершенно не подавая признаков жизни, а через секунду уже нападать на тебя, как одержимые.
-	AI_Output(self,other,"DIA_Addon_Cronos_WasMachstdu_04_04");	//Я еще не определил силу, которая приводит их в движение, но в одном у меня сомнений нет: природа этой силы магическая.
+	AI_Output(other,self, " DIA_Addon_Cronos_WasMachstdu_15_00 " );	// What are you doing here?
+	AI_Output(self,other, " DIA_Addon_Cronos_WasMachstdu_04_01 " );	// My task is to find out as much as possible about the temple guards.
+	AI_Output(self,other, " DIA_Addon_Cronos_WasMachstdu_04_02 " );	// Not an easy task, let me tell you. The properties of these stone guards are completely unfamiliar to me.
+	AI_Output(self,other, " DIA_Addon_Cronos_WasMachstdu_04_03 " );	// They can stand completely lifeless, and in a second they will attack you like possessed.
+	AI_Output(self,other, " DIA_Addon_Cronos_WasMachstdu_04_04 " );	// I have not yet determined the force that drives them, but I have no doubt about one thing: the nature of this force is magical.
 };
 
 
-instance DIA_Addon_Cronos_Waechter(C_Info)
+instances of DIA_Addon_Cronos_Waechter (C_Info)
 {
 	npc = KDW_1401_Addon_Cronos_NW;
 	nr = 5;
 	condition = DIA_Addon_Cronos_Waechter_Condition;
 	information = DIA_Addon_Cronos_Waechter_Info;
-	description = "Расскажи мне о каменных стражах.";
+	description = " Tell me about the stone guards. " ;
 };
 
 
@@ -122,16 +123,16 @@ func int DIA_Addon_Cronos_Waechter_Condition()
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_Addon_Cronos_Waechter_Info()
 {
-	AI_Output(other,self,"DIA_Addon_Cronos_Waechter_15_00");	//Расскажи мне о каменных стражах.
-	AI_Output(self,other,"DIA_Addon_Cronos_Waechter_04_01");	//Многого я сказать пока не могу. К сожалению, нам пришлось их уничтожить. Теперь они не опасны.
-	AI_Output(self,other,"DIA_Addon_Cronos_Waechter_04_02");	//Когда мы только пришли сюда, они напали на нас.
-	AI_Output(self,other,"DIA_Addon_Cronos_Waechter_04_03");	//Лишь объединенными силами нам удалось их победить. Боюсь, за порталом нас ждут новые стражи.
-	AI_Output(self,other,"DIA_Addon_Cronos_Waechter_04_04");	//Должен быть способ закрыть источник их силы, чтобы они оставались лишь безжизненными статуями.
+	AI_Output(other,self, " DIA_Addon_Cronos_Waechter_15_00 " );	// Tell me about the stone guards.
+	AI_Output(self,other, " DIA_Addon_Cronos_Waechter_04_01 " );	// I can't say much yet. Unfortunately, we had to destroy them. Now they are not dangerous.
+	AI_Output(self,other, " DIA_Addon_Cronos_Waechter_04_02 " );	// When we first got here, they attacked us.
+	AI_Output(self,other, " DIA_Addon_Cronos_Waechter_04_03 " );	// Only by united forces we managed to defeat them. I'm afraid there are new guards waiting for us beyond the portal.
+	AI_Output(self,other, " DIA_Addon_Cronos_Waechter_04_04 " );	// There must be a way to shut down the source of their power so that they remain just lifeless statues.
 };
 
 
@@ -143,7 +144,7 @@ instance DIA_Addon_Cronos_NW_Trade(C_Info)
 	information = DIA_Addon_Cronos_NW_Trade_Info;
 	trade = TRUE;
 	permanent = TRUE;
-	description = "Ты можешь продать мне какие-нибудь припасы?";
+	description = " Can you sell me some supplies? " ;
 };
 
 
@@ -171,17 +172,17 @@ func void DIA_Addon_Cronos_NW_Trade_Info()
 		CreateInvItems(self,itpo_anpois,3);
 	};
 
-	AI_Output(other,self,"DIA_Addon_Cronos_NW_Trade_15_00");	//Ты можешь продать мне какие-нибудь припасы?
+	AI_Output(other,self, " DIA_Addon_Cronos_NW_Trade_15_00 " );	// Can you sell me some supplies?
 
 	if(DIA_Addon_Cronos_NW_Trade_OneTime == FALSE)
 	{
-		AI_Output(self,other,"DIA_Addon_Cronos_NW_Trade_04_01");	//Почему бы и нет? Если я правильно помню, мы с тобой уже вели дела.
+		AI_Output(self,other, " DIA_Addon_Cronos_NW_Trade_04_01 " );	// Why not? If I remember correctly, you and I have already done business.
 		DIA_Addon_Cronos_NW_Trade_OneTime = TRUE;
 		Log_CreateTopic(Topic_OutTrader,LOG_NOTE);
 		B_LogEntry(Topic_OutTrader,LogText_Addon_CronosTrade);
 	};
 
-	AI_Output(self,other,"DIA_Addon_Cronos_NW_Trade_04_02");	//Скажи мне, что тебе нужно.
+	AI_Output(self,other, " DIA_Addon_Cronos_NW_Trade_04_02 " );	// Tell me what you need.
 	B_GiveTradeInv(self);
 };
 
@@ -193,7 +194,7 @@ instance DIA_ADDON_CRONOS_NW_PRAYFORGOMEZ(C_Info)
 	condition = dia_addon_cronos_nw_prayforgomez_condition;
 	information = dia_addon_cronos_nw_prayforgomez_info;
 	permanent = FALSE;
-	description = "Мне нужно благословение Аданоса.";
+	description = " I need the blessing of Adanos. " ;
 };
 
 
@@ -207,11 +208,11 @@ func int dia_addon_cronos_nw_prayforgomez_condition()
 
 func void dia_addon_cronos_nw_prayforgomez_info()
 {
-	AI_Output(other,self,"DIA_Addon_Cronos_NW_PrayForGomez_01_01");	//Мне нужно благословление Аданоса.
-	AI_Output(self,other,"DIA_Addon_Cronos_NW_PrayForGomez_01_02");	//Поговори об этом с Сатурасом.
-	AI_Output(self,other,"DIA_Addon_Cronos_NW_PrayForGomez_01_03");	//Думаю, он сможет помочь тебе.
-	AI_Output(other,self,"DIA_Addon_Cronos_NW_PrayForGomez_01_04");	//Спасибо за совет.
-	SENTTOSATURASGOMEZ = TRUE;
+	AI_Output(other,self, " DIA_Addon_Cronos_NW_PrayForGomez_01_01 " );	// I need the blessing of Adanos.
+	AI_Output(self,other, " DIA_Addon_Cronos_NW_PrayForGomez_01_02 " );	// Talk to Saturas about this.
+	AI_Output(self,other, " DIA_Addon_Cronos_NW_PrayForGomez_01_03 " );	// I think he can help you.
+	AI_Output(other,self, " DIA_Addon_Cronos_NW_PrayForGomez_01_04 " );	// Thanks for the tip.
+	SENTTOSATURASGOMEZ = TRUE ;
 };
 
 
@@ -222,7 +223,7 @@ instance DIA_ADDON_CRONOS_RUNEMAGICNOTWORK(C_Info)
 	condition = dia_addon_cronos_runemagicnotwork_condition;
 	information = dia_addon_cronos_runemagicnotwork_info;
 	permanent = FALSE;
-	description = "Ты все еще можешь использовать свою рунную магию?";
+	description = " Can you still use your rune magic? " ;
 };
 
 
@@ -237,12 +238,11 @@ func int dia_addon_cronos_runemagicnotwork_condition()
 func void dia_addon_cronos_runemagicnotwork_info()
 {
 	B_GivePlayerXP(200);
-	AI_Output(other,self,"DIA_Addon_Cronos_RuneMagicNotWork_01_00");	//Ты все еще можешь использовать свою рунную магию?
-	AI_Output(self,other,"DIA_Addon_Cronos_RuneMagicNotWork_01_01");	//Боюсь тебя огорчить, но все мои магические руны каким-то образом утратили свою силу.
-	AI_Output(self,other,"DIA_Addon_Cronos_RuneMagicNotWork_01_02");	//Причем, не только у меня, но и остальных магов Воды! И я понятия не имею, как это могло произойти.
-	AI_Output(self,other,"DIA_Addon_Cronos_RuneMagicNotWork_01_03");	//Возможно, в этом мире произошло нечто страшное и непоправимое, раз все мы вдруг утратили этот дар.
-	AI_Output(other,self,"DIA_Addon_Cronos_RuneMagicNotWork_01_04");	//Хммм... все ясно.
-	B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Кажется, Маги Воды также потеряли свою способность использовать рунную магию.");
+	AI_Output(other,self, " DIA_Addon_Cronos_RuneMagicNotWork_01_00 " );	// Can you still use your rune magic?
+	AI_Output(self,other, " DIA_Addon_Cronos_RuneMagicNotWork_01_01 " );	// I'm afraid to disappoint you, but all my magic runes have somehow lost their power.
+	AI_Output(self,other, " DIA_Addon_Cronos_RuneMagicNotWork_01_02 " );	// And, not only for me, but also for other Waterbenders! And I have no idea how this could happen.
+	AI_Output(self,other, " DIA_Addon_Cronos_RuneMagicNotWork_01_03 " );	// Perhaps something terrible and irreparable happened in this world, since we all suddenly lost this gift.
+	AI_Output(other,self, " DIA_Addon_Cronos_RuneMagicNotWork_01_04 " );	// Hmmm... all clear.
+	B_LogEntry( TOPIC_RUNEMAGICNOTWORK , " It seems that the Water Mages have also lost their ability to use rune magic. " );
 	WATERMAGERUNESNOT = TRUE;
 };
-
