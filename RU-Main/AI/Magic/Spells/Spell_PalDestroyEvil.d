b9e8b523f50dd5b5e668b58ff4b9ac1c6ccd6657
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-01 18:55:59 +0200

    Update Spell_PalDestroyEvil.d

--- RU-Main/AI/Magic/Spells/Spell_PalDestroyEvil.d
+++ RU-Main/AI/Magic/Spells/Spell_PalDestroyEvil.d
@@ -1,4 +1,5 @@
 
+
 instance Spell_PalDestroyEvil(C_Spell_Proto)
 {
 	time_per_mana = 0;
@@ -64,14 +65,14 @@ func void Spell_Cast_PalDestroyEvil()
 	{
 		if((FIREMAGERUNESNOT == TRUE) || (WATERMAGERUNESNOT == TRUE) || (GURUMAGERUNESNOT == TRUE) || (PALADINRUNESNOT == TRUE))
 		{
-			B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Как интересно! В отличие от Пирокара и других прочих магов, я могу использовать рунную магию. Что бы это значило?!");
+			B_LogEntry( TOPIC_RUNEMAGICNOTWORK , " How interesting! Unlike Pyrocar and other mages, I can use rune magic. What does that mean?! " );
 		}
 		else
 		{
-			B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Как интересно! В отличие от Пирокара, я могу использовать рунную магию. Что бы это значило?!");
+			B_LogEntry( TOPIC_RUNEMAGICNOTWORK , " How interesting! Unlike Pyrocar, I can use rune magic. What does that mean?! " );
 		};
-		TESTRUNEME = TRUE;
+		TESTRUNEME = TRUE ;
 	};
-	self.aivar[AIV_SelectSpell] += 1;
+	self.aivar[AIV_SelectSpell] +=  1 ;
 };
 
