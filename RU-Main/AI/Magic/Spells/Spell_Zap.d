cc3dfb9d94fd73ef82e89e79ff3915c233827790
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-01 19:22:27 +0200

    Update Spell_Zap.d

--- RU-Main/AI/Magic/Spells/Spell_Zap.d
+++ RU-Main/AI/Magic/Spells/Spell_Zap.d
@@ -1,7 +1,8 @@
 
+
 instance Spell_Zap(C_Spell_Proto)
 {
-	time_per_mana = 0;
+	time_per_mana = 0 ;
 	damage_per_level = SPL_Damage_Zap;
 	damagetype = DAM_MAGIC;
 };
@@ -42,14 +43,14 @@ func void Spell_Cast_Zap(var int spellLevel)
 	{
 		if((FIREMAGERUNESNOT == TRUE) || (WATERMAGERUNESNOT == TRUE) || (GURUMAGERUNESNOT == TRUE) || (PALADINRUNESNOT == TRUE))
 		{
-			B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Как интересно! В отличие от Пирокара и других прочих магов, я могу использовать рунную магию. Что бы это значило?!");
+			B_LogEntry( TOPIC_RUNEMAGICNOTWORK , " How interesting! Unlike Pyrocar and other mages, I can use rune magic. What does that mean?! " );
 		}
 		else
 		{
-			B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Как интересно! В отличие от Пирокара, я могу использовать рунную магию. Что бы это значило?!");
+			B_LogEntry( TOPIC_RUNEMAGICNOTWORK , " How interesting! Unlike Pyrocar, I can use rune magic. What does that mean?! " );
 		};
-		TESTRUNEME = TRUE;
+		TESTRUNEME = TRUE ;
 	};
-	self.aivar[AIV_SelectSpell] += 1;
+	self.aivar[AIV_SelectSpell] +=  1 ;
 };
 
