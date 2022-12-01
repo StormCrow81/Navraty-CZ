ab794189291fcb12591398b183f18cc1aa7a7d82
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-01 19:28:31 +0200

    Update spell_summonshoal.d

--- RU-Main/AI/Magic/Spells/spell_summonshoal.d
+++ RU-Main/AI/Magic/Spells/spell_summonshoal.d
@@ -1,10 +1,11 @@
 
+
 var int sumtime_iw_1;
 var int sumtime_iw_2;
 
 instance SPELL_SUMMONSHOAL(C_Spell_Proto)
 {
-	time_per_mana = 0;
+	time_per_mana = 0 ;
 	targetCollectAlgo = TARGET_COLLECT_NONE;
 };
 
@@ -13,7 +14,7 @@ func int spell_logic_summonshoal(var int manaInvested)
 {
 	if(CurrentLevel == LOSTVALLEY_ZEN)
 	{
-		AI_Print("Что-то мешает это сделать...");
+		AI_Print( " Something is preventing this from being done... " );
 		//B_Say(self,self,"$DOESNTWORK");
 		return SPL_SENDSTOP;
 	};
@@ -78,14 +79,14 @@ func void spell_cast_summonshoal(var int spellLevel)
 	{
 		if((FIREMAGERUNESNOT == TRUE) || (WATERMAGERUNESNOT == TRUE) || (GURUMAGERUNESNOT == TRUE) || (PALADINRUNESNOT == TRUE))
 		{
-			B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Как интересно! В отличие от Пирокара и других прочих магов, я могу использовать рунную магию. Что бы это значило?!");
+			B_LogEntry( TOPIC_RUNEMAGICNOTWORK , " How interesting! Unlike Pyrocar and other mages, I can use rune magic. What does that mean?! " );
 		}
 		else
 		{
-			B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Как интересно! В отличие от Пирокара, я могу использовать рунную магию. Что бы это значило?!");
+			B_LogEntry( TOPIC_RUNEMAGICNOTWORK , " How interesting! Unlike Pyrocar, I can use rune magic. What does that mean?! " );
 		};
-		TESTRUNEME = TRUE;
+		TESTRUNEME = TRUE ;
 	};
-	self.aivar[AIV_SelectSpell] += 1;
+	self.aivar[AIV_SelectSpell] +=  1 ;
 };
 
