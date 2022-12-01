323f9d6a0f1bae8ca370cac7a46689b1bbee6217
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-01 17:43:43 +0200

    Update Spell_ChargeFireball.d

--- RU-Main/AI/Magic/Spells/Spell_ChargeFireball.d
+++ RU-Main/AI/Magic/Spells/Spell_ChargeFireball.d
@@ -1,10 +1,11 @@
 
+
 instance Spell_ChargeFireball(C_Spell_Proto)
 {
-	time_per_mana = 60;
+	time_per_mana = 60 ;
 	damage_per_level = SPL_Damage_ChargeFireball;
 	damagetype = DAM_FIRE;
-	canTurnDuringInvest = TRUE;
+	canTurnDuringInvest = TRUE ;
 };
 
 
@@ -12,7 +13,7 @@ func int Spell_Logic_ChargeFireball(var int manaInvested)
 {
 	if((self.guild == GIL_FIREGOLEM) || (self.aivar[AIV_MM_REAL_ID] == ID_DRAGON_FIRE))
 	{
-		AI_PrintClr("Это не сработает...",177,58,17);
+		AI_PrintClr( " This won't work... " , 177 , 58 , 17 );
 		//B_Say(self,self,"$DONTWORK");
 		return SPL_SENDSTOP;
 	};
@@ -22,7 +23,7 @@ func int Spell_Logic_ChargeFireball(var int manaInvested)
 	};
 	if(manaInvested <= (STEP_ChargeFireball * 1))
 	{
-		self.aivar[AIV_SpellLevel] = 1;
+		self.aivar[AIV_SpellLevel] = 1 ;
 		return SPL_STATUS_CANINVEST_NO_MANADEC;
 	}
 	else if((manaInvested > (STEP_ChargeFireball * 1)) && (self.aivar[AIV_SpellLevel] <= 1))
@@ -32,7 +33,7 @@ func int Spell_Logic_ChargeFireball(var int manaInvested)
 		{
 			self.attribute[ATR_MANA] = 0;
 		};
-		self.aivar[AIV_SpellLevel] = 2;
+		self.aivar[AIV_SpellLevel] = 2 ;
 		return SPL_NEXTLEVEL;
 	}
 	else if((manaInvested > (STEP_ChargeFireball * 2)) && (self.aivar[AIV_SpellLevel] <= 2))
@@ -42,7 +43,7 @@ func int Spell_Logic_ChargeFireball(var int manaInvested)
 		{
 			self.attribute[ATR_MANA] = 0;
 		};
-		self.aivar[AIV_SpellLevel] = 3;
+		self.aivar[AIV_SpellLevel] = 3 ;
 		return SPL_NEXTLEVEL;
 	}
 	else if((manaInvested > (STEP_ChargeFireball * 3)) && (self.aivar[AIV_SpellLevel] <= 3))
@@ -52,7 +53,7 @@ func int Spell_Logic_ChargeFireball(var int manaInvested)
 		{
 			self.attribute[ATR_MANA] = 0;
 		};
-		self.aivar[AIV_SpellLevel] = 4;
+		self.aivar[AIV_SpellLevel] = 4 ;
 		return SPL_NEXTLEVEL;
 	}
 	else if((manaInvested > (STEP_ChargeFireball * 3)) && (self.aivar[AIV_SpellLevel] == 4))
@@ -77,14 +78,14 @@ func void Spell_Cast_ChargeFireball(var int spellLevel)
 	{
 		if((FIREMAGERUNESNOT == TRUE) || (WATERMAGERUNESNOT == TRUE) || (GURUMAGERUNESNOT == TRUE) || (PALADINRUNESNOT == TRUE))
 		{
-			B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Как интересно! В отличие от Пирокара и других прочих магов, я могу использовать рунную магию. Что бы это значило?!");
+			B_LogEntry( TOPIC_RUNEMAGICNOTWORK , " How interesting! Unlike Pyrocar and other mages, I can use rune magic. What does that mean?! " );
 		}
 		else
 		{
-			B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Как интересно! В отличие от Пирокара, я могу использовать рунную магию. Что бы это значило?!");
+			B_LogEntry( TOPIC_RUNEMAGICNOTWORK , " How interesting! Unlike Pyrocar, I can use rune magic. What does that mean?! " );
 		};
-		TESTRUNEME = TRUE;
+		TESTRUNEME = TRUE ;
 	};
-	self.aivar[AIV_SelectSpell] += 1;
+	self.aivar[AIV_SelectSpell] +=  1 ;
 };
 
