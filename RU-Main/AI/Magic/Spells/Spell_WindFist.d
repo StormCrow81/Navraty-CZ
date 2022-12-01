818ebe39d8242050033f76397270c98e8f816245
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-01 19:22:02 +0200

    Update Spell_WindFist.d

--- RU-Main/AI/Magic/Spells/Spell_WindFist.d
+++ RU-Main/AI/Magic/Spells/Spell_WindFist.d
@@ -1,10 +1,11 @@
 
+
 instance Spell_WindFist(C_Spell_Proto)
 {
-	time_per_mana = 30;
+	time_per_mana = 30 ;
 	damage_per_level = SPL_Damage_Windfist;
 	damagetype = DAM_FLY | DAM_MAGIC;
-	canTurnDuringInvest = TRUE;
+	canTurnDuringInvest = TRUE ;
 	targetCollectAlgo = TARGET_COLLECT_FOCUS_FALLBACK_NONE;
 	targetCollectRange = 1000;
 };
@@ -18,40 +19,40 @@ func int Spell_Logic_WindFist(var int manaInvested)
 	};
 	if(manaInvested <= (STEP_WindFist * 1))
 	{
-		self.aivar[AIV_SpellLevel] = 1;
+		self.aivar[AIV_SpellLevel] = 1 ;
 		return SPL_STATUS_CANINVEST_NO_MANADEC;
 	}
-	else if((manaInvested > (STEP_WindFist * 1)) && (self.aivar[AIV_SpellLevel] <= 1))
+	else  if ((manaInvested > (STEP_WindFist *  1 )) && (self.aivar[AIV_SpellLevel] <=  1 ))
 	{
 		self.attribute[ATR_MANA] = self.attribute[ATR_MANA] - STEP_WindFist;
 		if(self.attribute[ATR_MANA] < 0)
 		{
 			self.attribute[ATR_MANA] = 0;
 		};
-		self.aivar[AIV_SpellLevel] = 2;
+		self.aivar[AIV_SpellLevel] = 2 ;
 		return SPL_NEXTLEVEL;
 	}
-	else if((manaInvested > (STEP_WindFist * 2)) && (self.aivar[AIV_SpellLevel] <= 2))
+	else  if ((manaInvested > (STEP_WindFist *  2 )) && (self.aivar[AIV_SpellLevel] <=  2 ))
 	{
 		self.attribute[ATR_MANA] = self.attribute[ATR_MANA] - STEP_WindFist;
 		if(self.attribute[ATR_MANA] < 0)
 		{
 			self.attribute[ATR_MANA] = 0;
 		};
-		self.aivar[AIV_SpellLevel] = 3;
+		self.aivar[AIV_SpellLevel] = 3 ;
 		return SPL_NEXTLEVEL;
 	}
-	else if((manaInvested > (STEP_WindFist * 3)) && (self.aivar[AIV_SpellLevel] <= 3))
+	else  if ((manaInvested > (STEP_WindFist *  3 )) && (self.aivar[AIV_SpellLevel] <=  3 ))
 	{
 		self.attribute[ATR_MANA] = self.attribute[ATR_MANA] - STEP_WindFist;
 		if(self.attribute[ATR_MANA] < 0)
 		{
 			self.attribute[ATR_MANA] = 0;
 		};
-		self.aivar[AIV_SpellLevel] = 4;
+		self.aivar[AIV_SpellLevel] = 4 ;
 		return SPL_NEXTLEVEL;
 	}
-	else if((manaInvested > (STEP_WindFist * 3)) && (self.aivar[AIV_SpellLevel] == 4))
+	else  if ((manaInvested > (STEP_WindFist *  3 )) && (self.aivar[AIV_SpellLevel] ==  4 ))
 	{
 		return SPL_DONTINVEST;
 	};
@@ -77,10 +78,10 @@ func void Spell_Cast_WindFist(var int spellLevel)
 		}
 		else
 		{
-			B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Как интересно! В отличие от Пирокара, я могу использовать рунную магию. Что бы это значило?!");
+			B_LogEntry( TOPIC_RUNEMAGICNOTWORK , " How interesting! Unlike Pyrocar, I can use rune magic. What does that mean?! " );
 		};
-		TESTRUNEME = TRUE;
+		TESTRUNEME = TRUE ;
 	};
-	self.aivar[AIV_SelectSpell] += 1;
+	self.aivar[AIV_SelectSpell] +=  1 ;
 };
 
