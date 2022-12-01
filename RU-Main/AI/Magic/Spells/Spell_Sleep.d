fd363f4e6b9f2f6d6c1e860737d3b0d36955a6c1
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-01 19:11:41 +0200

    Update Spell_Sleep.d

--- RU-Main/AI/Magic/Spells/Spell_Sleep.d
+++ RU-Main/AI/Magic/Spells/Spell_Sleep.d
@@ -1,7 +1,8 @@
 
+
 instance Spell_Sleep(C_Spell_Proto)
 {
-	time_per_mana = 0;
+	time_per_mana = 0 ;
 	spellType = SPELL_NEUTRAL;
 	targetCollectAlgo = TARGET_COLLECT_FOCUS;
 	canTurnDuringInvest = 0;
@@ -11,7 +12,7 @@ func int Spell_Logic_Sleep(var int manaInvested)
 {
 	if((other.guild == GIL_KDW) || (Hlp_GetInstanceID(other) == Hlp_GetInstanceID(Ass_126_Haniar)) || (other.guild == GIL_KDF) || (other.guild == GIL_KDM) || (other.guild == GIL_GUR) || (other.guild == GIL_DRAGON) || (other.guild == GIL_DEMON) || (Hlp_GetInstanceID(other) == Hlp_GetInstanceID(BDT_1090_Addon_Raven)) || (Hlp_GetInstanceID(other) == Hlp_GetInstanceID(BDT_2090_Addon_Raven)) || (Hlp_GetInstanceID(other) == Hlp_GetInstanceID(PAL_299_Sergio)) || (Hlp_GetInstanceID(other) == Hlp_GetInstanceID(NONE_100_Xardas)) || (Hlp_GetInstanceID(other) == Hlp_GetInstanceID(none_102_kreol)) || (other.aivar[90] == TRUE))
 	{
-		AI_PrintClr("Это не сработает...",177,58,17);
+		AI_PrintClr( " This won't work... " , 177 , 58 , 17 );
 		//B_Say(self,self,"$DONTWORK");
 		return SPL_SENDSTOP;
 	};
@@ -29,10 +30,10 @@ func int Spell_Logic_Sleep(var int manaInvested)
 		};
 		if((Hlp_GetInstanceID(other) == Hlp_GetInstanceID(Orc_8206_Dakar)) && (MIS_NeedRest == LOG_Running) && (DabarShakIsSleep == FALSE))
 		{
-			DabarShakIsSleep = TRUE;
+			DabarShakIsSleep = TRUE ;
 			return SPL_SENDCAST;
 		};
-		if(!C_BodyStateContains(other,BS_SWIM) && !C_BodyStateContains(other,BS_DIVE) && !C_NpcIsDown(other) && (other.guild < GIL_SEPERATOR_HUM) && (other.flags != NPC_FLAG_IMMORTAL) && (Npc_GetDistToNpc(self,other) <= 1000) && (other.guild != GIL_KDF) && (other.guild != GIL_GUR) && (other.guild != GIL_DMT) && (other.guild != GIL_PAL) && (other.guild != GIL_DRAGON) && (other.guild != GIL_KDW) && (Hlp_GetInstanceID(other) != Hlp_GetInstanceID(BDT_1090_Addon_Raven)) && (Hlp_GetInstanceID(other) != Hlp_GetInstanceID(BDT_2090_Addon_Raven)) && (Hlp_GetInstanceID(other) != Hlp_GetInstanceID(NONE_100_Xardas)) && (Hlp_GetInstanceID(other) != Hlp_GetInstanceID(none_102_kreol)) && (Hlp_GetInstanceID(other) != Hlp_GetInstanceID(vlk_6027_taliasan)))
+		if(!C_BodyStateContains(other,BS_SWIM) && !C_BodyStateContains(other,BS_DIVE) && !C_NpcIsDown(other) && (other.guild < GIL_SEPERATOR_HUM) && (other.flags != NPC_FLAG_IMMORTAL) && (Npc_GetDistToNpc(self,other) <= 1000) && (other.guild != GIL_KDF) && (other.guild != GIL_GUR) && (other.guild != GIL_DMT) && (other.guild != GIL_PAL) && (other.guild != GIL_DRAGON) && (other.guild != GIL_KDW) && (Hlp_GetInstanceID(other) != Hlp_GetInstanceID(BDT_1090_Addon_Raven)) && (Hlp_GetInstanceID(other) != Hlp_GetInstanceID(BDT_2090_Addon_Raven)) && (Hlp_GetInstanceID(other) != Hlp_GetInstanceID(NONE_100_Xardas)) && (Hlp_GetInstanceID(other) != Hlp_GetInstanceID(none_102_kreol)) && (Hlp_GetInstanceID(other) !=Hlp_GetInstanceID(vlk_6027_taliasan)))
 		{
 			Npc_ClearAIQueue(other);
 			B_ClearPerceptions(other);
@@ -62,14 +63,14 @@ func void Spell_Cast_Sleep()
 	{
 		if((FIREMAGERUNESNOT == TRUE) || (WATERMAGERUNESNOT == TRUE) || (GURUMAGERUNESNOT == TRUE) || (PALADINRUNESNOT == TRUE))
 		{
-			B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Как интересно! В отличие от Пирокара и других прочих магов, я могу использовать рунную магию. Что бы это значило?!");
+			B_LogEntry( TOPIC_RUNEMAGICNOTWORK , " How interesting! Unlike Pyrocar and other mages, I can use rune magic. What does that mean?! " );
 		}
 		else
 		{
-			B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Как интересно! В отличие от Пирокара, я могу использовать рунную магию. Что бы это значило?!");
+			B_LogEntry( TOPIC_RUNEMAGICNOTWORK , " How interesting! Unlike Pyrocar, I can use rune magic. What does that mean?! " );
 		};
-		TESTRUNEME = TRUE;
+		TESTRUNEME = TRUE ;
 	};
-	self.aivar[AIV_SelectSpell] += 1;
+	self.aivar[AIV_SelectSpell] +=  1 ;
 };
 
