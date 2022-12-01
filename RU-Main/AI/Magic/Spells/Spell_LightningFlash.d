6c37b9b06bcd984aa71407783abf0b220040da49
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-01 19:52:52 +0200

    Update Spell_LightningFlash.d

--- RU-Main/AI/Magic/Spells/Spell_LightningFlash.d
+++ RU-Main/AI/Magic/Spells/Spell_LightningFlash.d
@@ -1,10 +1,11 @@
 
+
 instance Spell_LightningFlash(C_Spell_Proto)
 {
-	time_per_mana = 0;
+	time_per_mana = 0 ;
 	damage_per_level = SPL_Damage_LightningFlash;
 	damagetype = DAM_MAGIC;
-	isMultiEffect = 1;
+	isMultiEffect = 1 ;
 };
 
 
@@ -43,14 +44,14 @@ func void Spell_Cast_LightningFlash()
 	{
 		if((FIREMAGERUNESNOT == TRUE) || (WATERMAGERUNESNOT == TRUE) || (GURUMAGERUNESNOT == TRUE) || (PALADINRUNESNOT == TRUE))
 		{
-			B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Как интересно! В отличие от Пирокара и других прочих магов, я могу использовать рунную магию. Что бы это значило?!");
+			B_LogEntry( TOPIC_RUNEMAGICNOTWORK , " How interesting! Unlike Pyrocar and other mages, I can use rune magic. What does that mean?! " );
 		}
 		else
 		{
-			B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Как интересно! В отличие от Пирокара, я могу использовать рунную магию. Что бы это значило?!");
+			B_LogEntry( TOPIC_RUNEMAGICNOTWORK , " How interesting! Unlike Pyrocar, I can use rune magic. What does that mean?! " );
 		};
-		TESTRUNEME = TRUE;
+		TESTRUNEME = TRUE ;
 	};
-	self.aivar[AIV_SelectSpell] += 1;
+	self.aivar[AIV_SelectSpell] +=  1 ;
 };
 
