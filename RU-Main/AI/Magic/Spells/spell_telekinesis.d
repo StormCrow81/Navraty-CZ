bab202425e08d4c8c7c0c5da1d35d3c760cafd7d
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-01 19:29:45 +0200

    Update spell_telekinesis.d

--- RU-Main/AI/Magic/Spells/spell_telekinesis.d
+++ RU-Main/AI/Magic/Spells/spell_telekinesis.d
@@ -1,7 +1,8 @@
 
+
 instance SPELL_TELEKINESIS(C_Spell_Proto)
 {
-	time_per_mana = 500;
+	time_per_mana = 500 ;
 	spellType = SPELL_BAD;
 	damage_per_level = SPL_TELEKINESIS_DAMAGE_PER_SEC;
 	targetCollectAlgo = TARGET_COLLECT_FOCUS;
@@ -37,14 +38,14 @@ func void spell_cast_telekinesis()
 	{
 		if((FIREMAGERUNESNOT == TRUE) || (WATERMAGERUNESNOT == TRUE) || (GURUMAGERUNESNOT == TRUE) || (PALADINRUNESNOT == TRUE))
 		{
-			B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Как интересно! В отличие от Пирокара и других прочих магов, я могу использовать рунную магию. Что бы это значило?!");
+			B_LogEntry( TOPIC_RUNEMAGICNOTWORK , " How interesting! Unlike Pyrocar and other mages, I can use rune magic. What does that mean?! " );
 		}
 		else
 		{
-			B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Как интересно! В отличие от Пирокара, я могу использовать рунную магию. Что бы это значило?!");
+			B_LogEntry( TOPIC_RUNEMAGICNOTWORK , " How interesting! Unlike Pyrocar, I can use rune magic. What does that mean?! " );
 		};
-		TESTRUNEME = TRUE;
+		TESTRUNEME = TRUE ;
 	};
-	self.aivar[AIV_SelectSpell] += 1;
+	self.aivar[AIV_SelectSpell] +=  1 ;
 };
 
