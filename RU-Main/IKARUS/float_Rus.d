da40b13dbb363571e1b76590f3972895a9fcdb55
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-01 20:59:48 +0200

    Update float_Rus.d

--- RU-Main/IKARUS/float_Rus.d
+++ RU-Main/IKARUS/float_Rus.d
@@ -1,53 +1,47 @@
+
 //#################################################################
 //
-//  32-битные (стандарт IEEE 754) числа с плавающей запятой
-//  (вещественные) для Daedalus:
-//      Первоначальный скрипт от 26.11.2008:
+//   32-bit (IEEE 754 standard) floating point numbers
+//   (real) for Daedalus:
+//       Initial script dated 11/26/2008:
 // http://forum.worldofplayers.de/forum/threads/500080-Instrumentarium-Floats
 //
-//  Эта версия сохраняет оригинальный интерфейс, но использует 
-//  современные инструкции для чисел с плавающей запятой.
-//  Так что эта версия быстрее и точнее.
+//   This version retains the original interface but uses
+//   modern instructions for floating point numbers.
+//   So this version is faster and more accurate.
 //
-//  Требуется Ikarus.
+//   Requires Ikarus.
 //
-//  Завершено 6.4.2012:
-//    - переход на Ikarus
-//    - добавлены castFromIntf и castToIntf
+//   Completed on 4/6/2012:
+//     - switch to Ikarus
+//     - added castFromIntf and castToIntf
 //
-//  Перевод на русский язык: НастасьСанна /*NS*/
+//   Translation into Russian: NastasSanna /*NS*/
 //
 //#################################################################
 
 
 /*####################################
-//      Краткие сведения
+// Brief information
 //####################################
-
 //*********************
-//  Принципиально
+// Fundamentally
 //*********************
-
-Функции в этом сценарии могут выполнять действия с числами с плавающей запятой (вещественные). В качестве контейнера для вещественных чисел служат обычные 32-разрядные целые int (integer).
-"За кулисами" синтаксические отличия между целыми и вещественными числами отсутствуют, так что вещественное представление весьма ограничено парсером Готики.
-Поэтому необходимо четко понимать, что имеется в виду, поскольку формально целые и вещественные числа представлены одним типом данных. Если вещественное число обрабатывается как целое, или в функцию для вещественных чисел передается целое значение, или наоборот, в лучшем случае получится просто ерунда. 
-
+The functions in this scenario can operate on floating point (real) numbers. The usual 32-bit integers int (integer) serve as a container for real numbers.
+Behind the scenes, there is no syntactic difference between integers and real numbers, so the real representation is quite limited by the Gothic parser.
+Therefore, it is necessary to clearly understand what is meant, since formally integers and real numbers are represented by the same data type. If a real number is treated as an integer, or an integer value is passed to a function for real numbers, or vice versa, it will be just nonsense at best.
 //************************************
-//  Инструментарий
+// Toolkit
 //************************************
-
-Чтобы работать с вещественными числа, их необходимо сначала сгенерировать. Здесь присутствуют функции mkf и fracf, первая из которых из целого числа получает соответствующее вещественное, а вторая дает результат от деления двух целых чисел.
-Между собой вещественные числа можно складывать, вычитать, умножать и делить. Также добавлена функция извлечения квадратного корня.
-Для операций сравнения необходимо использовать функции ge, g, l, le (вместо операторов >=, >, <, <=).
-Затем вещественные числа можно конвертировать обратно в целые с помощью truncf и roundf.
-
+To work with real numbers, they must first be generated. Here there are functions mkf and fracf, the first of which receives the corresponding real number from an integer, and the second gives the result of dividing two integers.
+Real numbers can be added, subtracted, multiplied and divided among themselves. A square root function has also been added.
+For comparison operations, you must use the functions ge, g, l, le (instead of the >=, >, <, <= operators).
+Real numbers can then be converted back to integers using truncf and roundf.
 //************************************
-//  Обзор функций
+// Overview of functions
 //************************************
-
-формулировка "func float" и "var float", как было указано, недопустима (для Готики все числа являются целыми). Такая запись используется только для иллюстративных целей.
-Для запоминания функций может пригодиться следующий список с расшифровкой сокращений:
-
+the wording "func float" and "var float", as stated, is invalid (for Gothic, all numbers are integers). This notation is used for illustrative purposes only.
+To remember the functions, the following list with decoding of abbreviations may come in handy:
 mkf    = make float
 truncf = truncate float
 roundf = round float
@@ -63,105 +57,80 @@ lf     = lower
 lef    = lower or equal
 sqrtf  = square root of float
 printf = print float
-
-*************** Конвертирование ******************
-func float mkf (var int x) //Создает вещественное число из целого.
-func int truncf (var float x) //обрезает вещественное значение до целого, отбрасывая дробную часть. Возвращает целое число. (для -1.5 получим -1.0, а не -2)
-func int roundf (var float x) //преобразует вещественное число к целому, округляя его. Возвращает целое число.
-
-**************** Сложение ************************
-func float addf (var float x, var float y) //складывает x и y и возвращает результат.
-func float subf (var float x, var float y) //вычитает y из x и возвращает результат.
-func float negf (var float x) //Возвращает число, противоположное к x (т.е. -x).
-
-**************** Умножение ***********************
-func float mulf (var float x, var float y) //умножает x на y и возвращает результат
-func float divf (var float x, var float y) //делит x на y и возвращает результат.
-func float invf (var float x) //Возвращает число, обратное к x, т.е. 1/x. Если x = 0, возникает ошибка.
-func float fracf (var int p, var int q) //Возвращает результат деления p/q как вещественое число. Эквивалентно divf(mkf(p), mkf(q))
-
-**************** Операции сравнения **************
-func int gf (var float x, var float y) //если x > y возвращает TRUE, иначе FALSE
-func int gef (var float x, var float y) //если fur x >= y возвращает TRUE, иначе FALSE
-func int lef (var float x, var float y) //если fur x <= y возвращает TRUE, иначе FALSE
-func int lf (var float x, var float y) //если fur x < y возвращает TRUE, иначе FALSE
-
-**************** Разное **************************
-func float sqrf  (var float x) //возвращает квадрат числа x
-func float sqrtf (var float x) //возвращает квадратный корень из x
-func string toStringf(var float x) //Дает строковое представление числа x, напр. "12.345"
-
+*************** Conversion ******************
+func float mkf (var int x) //Creates a real number from an integer.
+func int truncf (var float x) //truncates a real value to an integer, discarding the fractional part. Returns an integer. (for -1.5 we get -1.0, not -2)
+func int roundf (var float x) //converts a real number to an integer, rounding it up. Returns an integer.
+**************** Addition ************************
+func float addf (var float x, var float y) //adds x and y and returns the result.
+func float subf (var float x, var float y) //subtracts y from x and returns the result.
+func float negf (var float x) //Returns the negative of x (i.e. -x).
+***************** Multiplication *************************
+func float mulf (var float x, var float y) //multiplies x by y and returns the result
+func float divf (var float x, var float y) //divides x by y and returns the result.
+func float invf (var float x) //Returns the inverse of x, i.e. 1/x. If x = 0, an error occurs.
+func float fracf (var int p, var int q) //Returns the result of dividing p/q as a real number. Equivalent to divf(mkf(p), mkf(q))
+**************** Comparison operations ****************
+func int gf (var float x, var float y) //if x > y returns TRUE, otherwise FALSE
+func int gef (var float x, var float y) //if fur x >= y returns TRUE, otherwise FALSE
+func int lef (var float x, var float y) //if fur x <= y returns TRUE, otherwise FALSE
+func int lf (var float x, var float y) //if fur x < y returns TRUE, otherwise FALSE
+**************** Miscellaneous **************************
+func float sqrf (var float x) //returns the square of x
+func float sqrtf (var float x) //returns the square root of x
+func string toStringf(var float x) //Gives a string representation of the number x, eg. "12.345"
 **************** Casts ************************
-В Daedalus есть свой собственный, очень ограниченный "var float".
-Чтобы floats в формате этого скрипта в Floats преобразовать в формат Daedalus, и наоборот, есть две функции.
-
-func float castFromIntf(var int   f) //Преобразует float в формате этого скрипта в формат Daedalus
-func int   castToIntf  (var float f) //Преобразует float в формате Daedalus  в формат этого скрипта
-
-Обе функции, в конечном счете, ничего не делают. Они "обманывают" парсер путем простого изменения типа данных.
-В обоих случаях параметры и возвращаемые значения обеих функций фактически имеют тип float.
-
+Daedalus has its own very limited "var float".
+To convert floats in the format of this script in Floats to Daedalus format, and vice versa, there are two functions.
+func float castFromIntf(var int f) //Converts float in this script's format to Daedalus format
+func int castToIntf (var float f) //Converts a Daedalus-formatted float to this script's format
+Both functions end up doing nothing. They "trick" the parser by simply changing the data type.
+In both cases, the parameters and return values ​​of both functions are actually of type float.
 //************************************
-//  Прочее
+// Other
 //************************************
-
-Определены 5 констант, которые можно использовать без вычисления/конвертирования целых чисел:
-
+There are 5 constants defined that can be used without calculating/converting integers:
 FLOATNULL = 0
 FLOATEINS = 1
 FLOATHALB = 0.5
 PI = 3.1415...
 E = 2.7182...
-
 //************************************
-//  Примеры
+// Examples
 //************************************
-
-Следующая функция вычисляет объем цилиндра в куб.см и возвращает округленное значение.
-Параметры передаются как целые числа.
-
+The following function calculates the volume of a cylinder in cc and returns the rounded value.
+Parameters are passed as integers.
 func int CylinderVolume (var int radius, var int height) {
     var int radiusf, var int heightf;
-    radiusf = mkf (radius);
+    radiusf = mkf(radius);
     heightf = mkf (height);
-
     //V = r * PI * h
-
     var int resultf;
     resultf = mulf (radiusf, radiusf);
     resultf = mulf (resultf, PI);
     resultf = mulf (resultf, h);
-
     return roundf (resultf);
 };
-
-Следующая функция вычисляет значение и выводит его на экран. Несколько вложенных функций
-
+The following function calculates a value and prints it to the screen. Multiple nested functions
 func void reply () {
     var int foo;
-
     foo = mulf (mkf (1337), PI);
     printf (divf (foo, mkf (100)));
-
-    //(1337*PI)/100 с удивительной точностью равно 42. ;-)
-	//NS: Увы, увы, только в формате single. При более точных расчетах, отличие уже в 3 знаке после запятой
+    //(1337*PI)/100 equals 42 with surprising accuracy. ;-)
+	//NS: Alas, alas, only in single format. With more accurate calculations, the difference is already in 3 decimal places
 };
-
-Следующая функция выполняет несколько сравнений. Ни в одном случае не выдается "ERROR".
-
+The following function performs several comparisons. In no case is "ERROR" returned.
 func void floattest()
 {
-    var int halb; var int eins; var int zwei;	//NS: 0.5, 1, 2
+    var int half; var int one; var int two; //NS: 0.5, 1, 2
     var int null;	//NS: 0
-    var int minuseins; var int minuszwei;	//NS: -1, -2
-
-    halb = invf (mkf (2));
-    eins = mkf (1);
-    zwei = mkf (2);
+    be int minuseins; where int minuszwei; //NS: -1, -2
+    half = invf(mkf(2));
+    same = mkf (1);
+    two = mkf(2);
     null = mkf (0);
     minuseins = mkf (-1);
     minuszwei = mkf (-2);
-
     if (gf (zwei,eins))           {} else { print ("ERROR!"); };
     if (gf (eins,null))           {} else { print ("ERROR!"); };
     if (lf (minuseins,null))      {} else { print ("ERROR!"); };
@@ -171,25 +140,21 @@ func void floattest()
     if (lef (null,null))          {} else { print ("ERROR!"); };
     if (gef (null,null))          {} else { print ("ERROR!"); };
 };
-
 //************************************
-//  Ограничения и подводные камни
+// Restrictions and pitfalls
 //************************************
-
-***********  Ошибки пользователя *****************
-Ни при каких обстоятельствах не применяйте операторы +, -, * или / к вещественным числам. Тут они бесполезны. В лучшем случае, получится просто ерунда. Используйте addf, subf, mulf и divf, как написано выше.
-Если вы знаете внутреннее представление вещественных чисел, например, только для положительных вещественных, можно использовать операторы сравнения (>, <, <= >=). Если вы не уверены, лучше используйте предоставленные функции.
-Разумеется, вещественные функции не подходят для целых чисел. Запись sqrtf (4) не равна 2, а просто неправильна, поскольку это будет 81 в виде целого! Чтобы извлечь квадратный корень из 4, нужно сделать так: roundf(sqrtf(mkf(4))). Тогда получится 2.
-
-*********** Погрешность представления ************
-Точность представления вещественных чисел весьма ограничена, поэтому начиная с 4 знака после запятой может возникнуть погрешность округления. Это касается очень больших или очень маленьких чисел.
-Есть обычные рекомендации и ограничения для вещественных чисел. Например, часто бывает необходимо проверить вещественные числа на равенство, но из-за погрешности округления получается так, что два числа, которые фактически должны быть равны друг другу, немного различаются. В этом случае лучше проверить, будет ли разница между этими двумя числами меньше некоторой малой величины (относительно решаемой задачи).
-
+*********** User error ****************
+Under no circumstances should you use the +, -, *, or / operators on real numbers. Here they are useless. At best, it will just be nonsense. Use addf, subf, mulf and divf as above.
+If you know the internal representation of real numbers, for example, only for positive real numbers, you can use the comparison operators (>, <, <= >=). If you're unsure, it's best to use the provided functions.
+Of course, real functions are not suitable for integers. The sqrtf(4) notation is not 2, it's just wrong, because that would be 81 as an integer! To take the square root of 4, you need to do this: roundf(sqrtf(mkf(4))). Then it will be 2.
+*********** Representation error ***********
+The accuracy of the representation of real numbers is very limited, therefore, starting from 4 decimal places, a rounding error may occur. This applies to very large or very small numbers.
+There are the usual guidelines and restrictions for real numbers. For example, it is often necessary to test real numbers for equality, but due to rounding errors, it turns out that two numbers that should actually be equal to each other are slightly different. In this case, it is better to check whether the difference between these two numbers will be less than some small value (relative to the problem being solved).
 */
 
 //#################################################################
 //
-//  ФУНКЦИИ
+//   FUNCTIONS
 //
 //#################################################################
 
@@ -197,21 +162,21 @@ const int FLOAT_SIGN_MASK = 1 << 31;
 
 const int FLOATNULL = 0;
 const int FLOATEINS = 1065353216; //vz 0, exp 0 (also char 127), mt 1.0
-const int FLOATONE  = FLOATEINS;  //то же
+const  int  FLOATONE   = FLOATEINS ;  // same
 const int FLOATHALB = 1056964608; //vz 0, exp -1 (also char 126), mt 1.0
 const int FLOATHALF = FLOATHALB;  //то же
 
-const int PI = 1078530011;
-const int E =  1076754516;
+const  int  PI = 1078530011 ;
+const  int E =   1076754516 ;
 
-/* Следующие массивы содержат код ассемблера. */
-const int FLTINT_trunc_code[9] = { -2082109099, 2111375596, 1166763774, 1712108798, -637778551, 1842939973, -128066564, -1946260007, -1010173883 };
+/* The following arrays contain the assembler code. */
+const  int FLTINT_trunc_code[ 9 ] = { - 2082109099 , 2111375596 , 1166763774 , 1712108798 , - 637778551 , 1842939973 , - 128066564 , - 1946260007 1946260007 , - 3838010 };
 const int FLTINT_mul_code  [3] = { -639268523, 1306004549, 12830984 };
 const int FLTINT_div_code  [3] = { -639268523, 2111310917, 12830984 };
 const int FLTINT_add_code  [3] = { -639268523, 1171786821, 12830984 };
 const int FLTINT_mk_code   [2] = { -605714091, -1010235323 };
 
-/* Вспомогательная функция: */
+/* Helper function: */
 func int _FLT_Build_Func(var int codePtr, var int param1Ptr, var int param2Ptr, var int retValIsFloat, var int resultPtr) {
     CALL_Open();
     if (param2Ptr) {
@@ -229,7 +194,7 @@ func int _FLT_Build_Func(var int codePtr, var int param1Ptr, var int param2Ptr, 
 };
 
 //************************************
-//      Упаковщик:
+//       Packer:
 //************************************
 
 func int mkf (var int x) {
@@ -283,7 +248,7 @@ func int divf (var int x, var int y) {
 };
 
 //************************************
-//  Возведение в квадрат и извлечение корня
+//   Squaring and rooting
 //************************************
 
 func int sqrf(var int f) {
@@ -305,8 +270,8 @@ func int sqrtf(var int f) {
 };
 
 //************************************
-//  Можно реализовать 5
-//  небольших вспомогательных функции:
+//   Can be implemented 5
+//   small helper functions:
 //************************************
 
 func int negf (var int x) {
@@ -334,26 +299,26 @@ func int invf (var int x) {
     return divf(FLOATEINS, x);
 };
 
-/* спасибо orcwarriorPL за идею! */
+/* thanks to orcwarriorPL for the idea! */
 func int fracf (var int p, var int q) {
     if (!q) {
         MEM_Error("fracf: Denominator is 0.");
     };
-    return divf(mkf(p), mkf(q));
+    return  divf (mkf(p), mkf(q));
 };
 
- /* обесценена */
+ /* depreciated */
 func int sqrtf_approx(var int f) { return sqrtf(f); };
 
 //************************************
-//  Операции сравнения
+//   Comparison operations
 //************************************
 
 func int gf (var int x, var int y) {
     var int isnegX; isnegX = x & FLOAT_SIGN_MASK;
     var int isnegY; isnegY = y & FLOAT_SIGN_MASK;
 
-    if (isNegX && isNegY) { //оба отрицательные
+    if (isNegX && isNegY) { // both are negative
         if (x < y) {
             return TRUE;
         };
@@ -364,7 +329,7 @@ func int gf (var int x, var int y) {
         };
     };
 
-    return FALSE;
+    return  FALSE ;
 };
 
 func int gef (var int x, var int y) {
@@ -381,7 +346,7 @@ func int lf (var int x, var int y) {
 };
 
 //*****************************************
-//  Функции вывода, спасибо Gottfried!
+//   Output functions, thanks Gottfried!
 //*****************************************
 
 func float castFromIntf(var int   f) { f; };
@@ -392,7 +357,7 @@ func string toStringf(var int x) {
     return FloatToString(f);
 };
 
-/* Неудачное имя. Сохранено ради совместимости. */
+/* Bad name. Saved for compatibility. */
 func void printf(var int x) {
     Print(toStringf(x));
 };
