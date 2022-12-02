b128c3173ae983b4e453c0df75889a8ff50b021c
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-02 21:46:43 +0200

    Update DIA_vlk_6109_luis.d

--- RU-Main/Story/Dialoge/DIA_vlk_6109_luis.d
+++ RU-Main/Story/Dialoge/DIA_vlk_6109_luis.d
@@ -1,5 +1,6 @@
 
-instance DIA_LUIS_EXIT(C_Info)
+
+instance DAY_LUIS_EXIT (C_Info)
 {
 	npc = vlk_6109_luis;
 	nr = 999;
@@ -15,13 +16,13 @@ func int dia_luis_exit_condition()
 	return TRUE;
 };
 
-func void dia_luis_exit_info()
+func void luis_day_exit_info()
 {
 	AI_StopProcessInfos(self);
 };
 
 
-instance DIA_LUIS_PICKPOCKET(C_Info)
+instances DIA_LUIS_PICKPOCKET (C_Info)
 {
 	npc = vlk_6109_luis;
 	nr = 900;
@@ -34,19 +35,19 @@ instance DIA_LUIS_PICKPOCKET(C_Info)
 
 func int dia_luis_pickpocket_condition()
 {
-	return C_Beklauen(30,28);
+	return  C_Robbery ( 30 , 28 );
 };
 
 func void dia_luis_pickpocket_info()
 {
 	Info_ClearChoices(dia_luis_pickpocket);
-	Info_AddChoice(dia_luis_pickpocket,Dialog_Back,dia_luis_pickpocket_back);
-	Info_AddChoice(dia_luis_pickpocket,DIALOG_PICKPOCKET,dia_luis_pickpocket_doit);
+	Info_AddChoice(dia_luis_pickpocket, Dialog_Back, dia_luis_pickpocket_back);
+	Info_AddChoice(dia_luis_pickpocket, DIALOG_PICKPOCKET ,dia_luis_pickpocket_doit);
 };
 
 func void dia_luis_pickpocket_doit()
 {
-	B_Beklauen();
+	B_Robbery();
 	Info_ClearChoices(dia_luis_pickpocket);
 };
 
@@ -56,18 +57,18 @@ func void dia_luis_pickpocket_back()
 };
 
 
-instance DIA_LUIS_HALLO(C_Info)
+instances DIA_LUIS_HALLO (C_Info)
 {
 	npc = vlk_6109_luis;
 	nr = 2;
 	condition = dia_luis_hallo_condition;
 	information = dia_luis_hallo_info;
 	permanent = FALSE;
-	description = "Это ты Луис?";
+	description = " Is that you Louis? " ;
 };
 
 
-func int dia_luis_hallo_condition()
+func int luis_day_hallo_condition()
 {
 	if(MIS_KILLTARGET1 == LOG_Running)
 	{
@@ -75,22 +76,22 @@ func int dia_luis_hallo_condition()
 	};
 };
 
-func void dia_luis_hallo_info()
+func void dia_luis_hello_info()
 {
 	B_GivePlayerXP(50);
-	AI_Output(other,self,"DIA_Luis_Hallo_01_00");	//Это ты Луис?
-	AI_Output(self,other,"DIA_Luis_Hallo_01_01");	//Да, я. А что, у тебя ко мне дело?
-	AI_Output(other,self,"DIA_Luis_Hallo_01_03");	//Говорят, что ты слишком много болтаешь о том, о чем бы следовало молчать.
-	AI_Output(other,self,"DIA_Luis_Hallo_01_04");	//Некоторым людям это очень не нравится.
-	AI_Output(self,other,"DIA_Luis_Hallo_01_05");	//(возмущенно) ЧТО?! Знаешь, если кому-то что-то не нравится, - то это их личные проблемы!
-	AI_Output(self,other,"DIA_Luis_Hallo_01_06");	//А я буду говорить то, что хочу!
-	AI_Output(other,self,"DIA_Luis_Hallo_01_07");	//Я вижу, ты не понял то, о чем я тебе сказал!
-	AI_Output(self,other,"DIA_Luis_Hallo_01_08");	//Да? И что же ты сделаешь?
-	AI_Output(other,self,"DIA_Luis_Hallo_01_09");	//Просто сейчас перережу тебе глотку, чтобы ты заткнулся раз и навсегда.
-	AI_Output(self,other,"DIA_Luis_Hallo_01_10");	//ЧТО?! Ты это серьезно?!
+	AI_Output(other,self, " DIA_Luis_Hallo_01_00 " );	// Is that you Louis?
+	AI_Output(self,other, " DIA_Luis_Hallo_01_01 " );	// Yes, I am. What do you have to do with me?
+	AI_Output(other,self, " DIA_Luis_Hallo_01_03 " );	// They say that you talk too much about things that you should be silent about.
+	AI_Output(other,self, " DIA_Luis_Hallo_01_04 " );	// Some people really don't like it.
+	AI_Output(self,other, " DIA_Luis_Hallo_01_05 " );	// (indignantly) WHAT?! You know, if someone does not like something, then this is their personal problem!
+	AI_Output(self,other, " DIA_Luis_Hallo_01_06 " );	// And I will say what I want!
+	AI_Output(other,self, " DIA_Luis_Hallo_01_07 " );	// I see you didn't understand what I told you!
+	AI_Output(self,other, " DIA_Luis_Hallo_01_08 " );	// Yes? And what will you do?
+	AI_Output(other,self, " DIA_Luis_Hallo_01_09 " );	// I'll just slit your throat right now to shut you up once and for all.
+	AI_Output(self,other, " DIA_Luis_Hallo_01_10 " );	// WHAT?! Are you serious?!
 	AI_ReadyMeleeWeapon(other);
-	AI_Output(self,other,"DIA_Luis_Hallo_01_12");	//О боже! Да ты с ума сошел!
-	AI_Output(self,other,"DIA_Luis_Hallo_01_13");	//Стража! Помогите кто-нибудь...(в панике) Меня хотят убить!
+	AI_Output(self,other, " DIA_Luis_Hallo_01_12 " );	// Oh god! Yes, you're crazy!
+	AI_Output(self,other, " DIA_Luis_Hallo_01_13 " );	// Guard! Help someone... (in a panic) They want to kill me!
 	AI_StopProcessInfos(self);
 	B_Attack(self,other,AR_ReactToWeapon,0);
 };
