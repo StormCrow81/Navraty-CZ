ef4a17753a3070821247b37c7f121174ebb40ce0
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-02 21:58:36 +0200

    Update DIA_vlk_6006_kardor.d

--- RU-Main/Story/Dialoge/DIA_vlk_6006_kardor.d
+++ RU-Main/Story/Dialoge/DIA_vlk_6006_kardor.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_KARDOR_EXIT(C_Info)
 {
 	npc = vlk_6006_kardor;
@@ -29,13 +30,13 @@ instance DIA_KARDOR_HELLO(C_Info)
 	information = dia_kardor_hello_info;
 	permanent = TRUE;
 	important = FALSE;
-	description = "Что ты делаешь?";
+	description = " What are you doing? " ;
 };
 
 
 func int dia_kardor_hello_condition()
 {
-	if(MEETKARDOR == FALSE)
+	if ( MEETKARDOR  ==  FALSE )
 	{
 		return TRUE;
 	};
@@ -43,19 +44,19 @@ func int dia_kardor_hello_condition()
 
 func void dia_kardor_hello_info()
 {
-	AI_Output(other,self,"DIA_Kardor_Hello_01_01");	//Что ты делаешь?
-	AI_Output(self,other,"DIA_Kardor_Hello_01_02");	//А что, разве не видно? Готовлюсь к бою, что же еще.
-	AI_Output(other,self,"DIA_Kardor_Hello_01_03");	//К какому бою?
-	AI_Output(self,other,"DIA_Kardor_Hello_01_04");	//Я тебе вот что скажу, парень. Рано или поздно вся эта нечить, что бродит вокруг, наверняка решит заняться и этим местечком.
-	AI_Output(self,other,"DIA_Kardor_Hello_01_05");	//А я не хочу сидеть сложа руки и ждать этого момента!
-	AI_Output(self,other,"DIA_Kardor_Hello_01_06");	//Надо сматываться из этого богом забытого местечка... а тут такая возможность!
-	AI_Output(other,self,"DIA_Kardor_Hello_01_07");	//Какая возможность?
-	AI_Output(self,other,"DIA_Kardor_Hello_01_08");	//Дюго сказал, что тот телепорт наверху пирамиды ведет напрямик в город орков.
-	AI_Output(self,other,"DIA_Kardor_Hello_01_09");	//И если мы решим им воспользоваться, чтобы убраться отсюда, нам, скорее всего, придется пробиваться через орды этих зеленокожих тварей.
-	AI_Output(self,other,"DIA_Kardor_Hello_01_10");	//Судя по всему, орков там много! Поэтому получше владеть своим мечом мне не помешает.
-	AI_Output(other,self,"DIA_Kardor_Hello_01_11");	//Все-таки Дюго успел заразить и других своей безумной идеей. Поверь мне: то, что вы задумали - чистое самоубийство!
-	AI_Output(other,self,"DIA_Kardor_Hello_01_12");	//Вам никогда не прорваться через их город. Вам даже через их аванпост не пройти, не то, что уж через целый город!
-	AI_Output(self,other,"DIA_Kardor_Hello_01_13");	//Может и так, но, по крайней мере, лучше уж погибнуть, чем стать бездушной нежитью, как те парни, что уже погибли здесь.
+	AI_Output(other,self, " DIA_Kardor_Hello_01_01 " );	// What are you doing?
+	AI_Output(self,other, " DIA_Kardor_Hello_01_02 " );	// Why, can't you see? Ready for battle, what else.
+	AI_Output(other,self, " DIA_Kardor_Hello_01_03 " );	// For what fight?
+	AI_Output(self,other, " DIA_Kardor_Hello_01_04 " );	// I'll tell you what, kid. Sooner or later, all this bastard that roams around will probably decide to take up this place as well.
+	AI_Output(self,other, " DIA_Kardor_Hello_01_05 " );	// And I don't want to sit back and wait for this moment!
+	AI_Output(self,other, " DIA_Kardor_Hello_01_06 " );	// We must get out of this godforsaken place... and here is such an opportunity!
+	AI_Output(other,self, " DIA_Kardor_Hello_01_07 " );	// What is the possibility?
+	AI_Output(self,other, " DIA_Kardor_Hello_01_08 " );	// Dugo said that the teleport at the top of the pyramid leads directly to the orc city.
+	AI_Output(self,other, " DIA_Kardor_Hello_01_09 " );	// And if we decide to use it to get out of here, we'll probably have to fight our way through hordes of these green-skinned creatures.
+	AI_Output(self,other, " DIA_Kardor_Hello_01_10 " );	// Apparently there are a lot of orcs out there! Therefore, it will not hurt me to better wield my sword.
+	AI_Output(other,self, " DIA_Kardor_Hello_01_11 " );	// Still, Dugo managed to infect others with his crazy idea. Believe me: what you have in mind is pure suicide!
+	AI_Output(other,self, " DIA_Kardor_Hello_01_12 " );	// You'll never break through their city. You can't even get through their outpost, let alone through an entire city!
+	AI_Output(self,other, " DIA_Kardor_Hello_01_13 " );	// Maybe so, but at least it's better to die than to become soulless undead, like those guys who already died here.
 	MEETKARDOR = TRUE;
 };
 
@@ -68,7 +69,7 @@ instance DIA_KARDOR_HELLO_TEACH(C_Info)
 	information = dia_kardor_hello_teach_info;
 	permanent = TRUE;
 	important = FALSE;
-	description = "Я слышал, ты неплохо обращаешься с мечом.";
+	description = " I hear you're good with a sword. " ;
 };
 
 
@@ -82,11 +83,11 @@ func int dia_kardor_hello_teach_condition()
 
 func void dia_kardor_hello_teach_info()
 {
-	AI_Output(other,self,"DIA_Kardor_Hello_Teach_01_01");	//Я слышал, ты неплохо обращаешься с мечом.
-	AI_Output(self,other,"DIA_Kardor_Hello_Teach_01_02");	//Что есть, то есть. А как же иначе? Здесь, в долине трудно выжить, если ты не умеешь постоять за себя.
-	AI_Output(self,other,"DIA_Kardor_Hello_Teach_01_03");	//Могу и тебя научить получше обращаться со своим оружием.
-	AI_Output(other,self,"DIA_Kardor_Hello_Teach_01_04");	//Да, я не прочь потренироваться!
-	AI_Output(self,other,"DIA_Kardor_Hello_Teach_01_05");	//Ну, тогда просто скажи, как будешь готов.
+	AI_Output(other,self, " DIA_Kardor_Hello_Teach_01_01 " );	// I hear you're pretty good with a sword.
+	AI_Output(self,other, " DIA_Kardor_Hello_Teach_01_02 " );	// What is, is. How else? It's hard to survive here in the valley if you can't stand up for yourself.
+	AI_Output(self,other, " DIA_Kardor_Hello_Teach_01_03 " );	// I can teach you how to use your weapons better.
+	AI_Output(other,self, " DIA_Kardor_Hello_Teach_01_04 " );	// Yes, I'm willing to work out!
+	AI_Output(self,other, " DIA_Kardor_Hello_Teach_01_05 " );	// Well, then just tell me when you're ready.
 	KARDORREADYTEACH = TRUE;
 };
 
@@ -98,7 +99,7 @@ instance DIA_KARDOR_TH(C_Info)
 	condition = dia_kardor_th_condition;
 	information = dia_kardor_th_info;
 	permanent = TRUE;
-	description = "Давай начнем тренировку!";
+	description = " Let's start training! " ;
 };
 
 
@@ -122,7 +123,7 @@ func void dia_kardor_th_choices()
 
 func void dia_kardor_th_info()
 {
-	AI_Output(other,self,"DIA_Kardor_TH_01_01");	//Начнем тренировку!
+	AI_Output(other,self, " DIA_Kardor_TH_01_01 " );	// Let's start training!
 	dia_kardor_th_choices();
 };
 
