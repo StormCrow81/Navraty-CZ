068876e5f7f25b14c7bfd1e005cd372661b698d2
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-02 23:05:53 +0200

    Update DIA_BAU_935_Bronko.d

--- RU-Main/Story/Dialoge/DIA_BAU_935_Bronko.d
+++ RU-Main/Story/Dialoge/DIA_BAU_935_Bronko.d
@@ -38,13 +38,13 @@ func int DIA_Bronko_HALLO_Condition()
 
 func void DIA_Bronko_HALLO_Info()
 {
-	AI_Output(self,other, " DIA_Bronko_HALLO_06_00 " );	// (fatherly) And where are we going, huh?
+	AI_Output(self,other, " DIA_Bronko_HALLO_06_00 " );	// And where are we going?
 	AI_Output(other,self, " DIA_Bronko_HALLO_15_01 " );	// Are you the senior here?
 	if(hero.guild == GIL_NONE)
 	{
-		AI_Output(self,other, " DIA_Bronko_HALLO_06_02 " );	// Now you'll get in the face, moron.
+		AI_Output(self,other, " DIA_Bronko_HALLO_06_02 " );	// I'll cave your face in, fool!
 	};
-	AI_Output(self,other, " DIA_Bronko_HALLO_06_03 " );	// If you want to walk freely on my land, you must pay me 5 gold or I'll beat your soul out!
+	AI_Output(self,other, " DIA_Bronko_HALLO_06_03 " );	// If you want to walk freely on my land, you must pay me 5 gold or I send you to the nearest landfill!
 	Info_ClearChoices(DIA_Bronko_HALLO);
 	Info_AddChoice(DIA_Bronko_HALLO, " Forget it. You won't get anything from me. " ,DIA_Bronko_HALLO_vergisses);
 	Info_AddChoice(DIA_Bronko_HALLO, " Looks like I have no choice - here's your money. " ,DIA_Bronko_HALLO_hiergeld);
@@ -54,8 +54,8 @@ func void DIA_Bronko_HALLO_Info()
 func void DIA_Bronko_HALLO_deinland()
 {
 	AI_Output(other,self, " DIA_Bronko_HALLO_deinland_15_00 " );	// Your land? Are you a local farmer?
-	AI_Output(self,other, " DIA_Bronko_HALLO_deinland_06_01 " );	// Naturally. Why else would you have to pay me duty?
-	AI_Output(self,other, " DIA_Bronko_HALLO_deinland_06_02 " );	// I don't mind if you ask the others about me. Hehe!
+	AI_Output(self,other, " DIA_Bronko_HALLO_deinland_06_01 " );	// Naturally. Why else would you have to pay me my due?
+	AI_Output(self,other, " DIA_Bronko_HALLO_deinland_06_02 " );	// You should ask the others about me. Hehe!
 };
 
 func void DIA_Bronko_HALLO_hiergeld()
@@ -64,12 +64,12 @@ func void DIA_Bronko_HALLO_hiergeld()
 	if(Npc_HasItems(other,ItMi_Gold) >= 5)
 	{
 		B_GiveInvItems(other,self,ItMi_Gold,5);
-		AI_Output(self,other, " DIA_Bronko_HALLO_hiergeld_06_01 " );	// (fake) Thank you. Have a good trip.
+		AI_Output(self,other, " DIA_Bronko_HALLO_hiergeld_06_01 " );	// Why thank you. Have a good trip.
 		AI_StopProcessInfos(self);
 	}
 	else
 	{
-		AI_Output(self,other, " DIA_Bronko_HALLO_hiergeld_06_02 " );	// You don't even have 5 coins. Do you want to trick me?
+		AI_Output(self,other, " DIA_Bronko_HALLO_hiergeld_06_02 " );	// You don't even have 5 coins. What's your game shithead?
 		AI_StopProcessInfos(self);
 		B_Attack(self,other,AR_NONE,1);
 	};
@@ -80,7 +80,7 @@ func void DIA_Bronko_HALLO_vergisses()
 	AI_Output(other,self, " DIA_Bronko_HALLO_vergisses_15_00 " );	// Forget it. You won't get anything from me.
 	if((hero.guild == GIL_NONE) || (hero.guild == GIL_SLD) || (hero.guild == GIL_DJG))
 	{
-		AI_Output(self,other, " DIA_Bronko_HALLO_vergisses_06_01 " );	// Then I'm afraid I'll have to punch you in the face.
+		AI_Output(self,other, " DIA_Bronko_HALLO_vergisses_06_01 " );	// Then I'm afraid I'll have to remove your teeth.
 	}
 	else
 	{
@@ -90,25 +90,25 @@ func void DIA_Bronko_HALLO_vergisses()
 		};
 		if((hero.guild == GIL_KDF) || (hero.guild == GIL_KDW) || (hero.guild == GIL_KDM))
 		{
-			AI_Output(self,other, " DIA_Bronko_HALLO_vergisses_06_03 " );	// I don't care that you're a mage. You will have to pay. Clear?
+			AI_Output(self,other, " DIA_Bronko_HALLO_vergisses_06_03 " );	// I don't care that you're a mage. You still have to pay. Clear?
 		};
 		if((hero.guild == GIL_NOV) || (hero.guild == GIL_NDW) || (hero.guild == GIL_NDM))
 		{
-			AI_Output(self,other, " DIA_Bronko_HALLO_vergisses_06_04 " );	// What, the novices don't have money or what? It doesn't matter, you'll still pay!
+			AI_Output(self,other, " DIA_Bronko_HALLO_vergisses_06_04 " );	// What, another broke novice? It doesn't matter, you'll still pay!
 		};
 		if((other.guild == GIL_SEK) || (other.guild == GIL_TPL) || (other.guild == GIL_GUR))
 		{
-			AI_Output(self,other, " DIA_Bronko_HALLO_vergisses_06_05 " );	// Hey, I don't know who you are or where you come from, but you'll pay anyway.
+			AI_Output(self,other, " DIA_Bronko_HALLO_vergisses_06_05 " );	// Hey, I don't know who you are or where you come from, but you'll pay like everybody else.
 		};
 	};
 	Info_ClearChoices(DIA_Bronko_HALLO);
 	Info_AddChoice(DIA_Bronko_HALLO, " Looks like I have no choice - here's your money. " ,DIA_Bronko_HALLO_hiergeld);
-	Info_AddChoice(DIA_Bronko_HALLO, " Then try and take them yourself. " ,DIA_Bronko_HALLO_attack);
+	Info_AddChoice(DIA_Bronko_HALLO, " Then try and take it from me. " ,DIA_Bronko_HALLO_attack);
 };
 
 func void DIA_Bronko_HALLO_attack()
 {
-	AI_Output(other,self, " DIA_Bronko_HALLO_attack_15_00 " );	// Then try, take them yourself.
+	AI_Output(other,self, " DIA_Bronko_HALLO_attack_15_00 " );	// Then try and take it from me.
 	AI_Output(self,other, " DIA_Bronko_HALLO_attack_06_01 " );	// Well, since you're like that...
 	AI_StopProcessInfos(self);
 	B_Attack(self,other,AR_NONE,1);
@@ -122,7 +122,7 @@ instances DIA_Bronko_KEINBAUER (C_Info)
 	condition = DIA_Bronko_KEINBAUER_Condition;
 	information = DIA_Bronko_KEINBAUER_Info;
 	permanent = TRUE;
-	description = " You?! Farmer?! Don't make me laugh. " ;
+	description = " You?! A farmer?! Don't make me laugh. " ;
 };
 
 
@@ -138,8 +138,8 @@ func int DIA_Bronko_KEINBAUER_Condition();
 
 func void DIA_Bronko_KEINBAUER_Info()
 {
-	AI_Output(other,self, " DIA_Bronko_KEINBAUER_15_00 " );	// You?! Farmer?! Do not make me laugh. Yes, you are nobody.
-	AI_Output(self,other, " DIA_Bronko_KEINBAUER_06_01 " );	// Whatoooo? Do you want to get punched in the face?
+	AI_Output(other,self, " DIA_Bronko_KEINBAUER_15_00 " );	// You?! A farmer?! Don't make me laugh. You're a nobody.
+	AI_Output(self,other, " DIA_Bronko_KEINBAUER_06_01 " );	// Say that again? You are begging for it you bastard!
 	Info_ClearChoices(DIA_Bronko_KEINBAUER);
 	if(hero.guild == GIL_NONE)
 	{
@@ -153,7 +153,7 @@ func void DIA_Bronko_KEINBAUER_Info()
 		};
 	};
 	Info_AddChoice(DIA_Bronko_KEINBAUER, " Well... Let's see what you can do. " ,DIA_Bronko_KEINBAUER_attack);
-	Info_AddChoice(DIA_Bronko_KEINBAUER, " Забудь! " ,DIA_Bronko_KEINBAUER_schongut);
+	Info_AddChoice(DIA_Bronko_KEINBAUER, " Forget it! " ,DIA_Bronko_KEINBAUER_schongut);
 };
 
 func void DIA_Bronko_KEINBAUER_attack()
@@ -166,16 +166,16 @@ func void DIA_Bronko_KEINBAUER_attack()
 
 func void DIA_Bronko_KEINBAUER_sekobderbauer()
 {
-	AI_Output(other,self, " DIA_Bronko_KEINBAUER_sekobderbauer_15_00 " );	// The farmer here is Sekob, and you're just a petty crook trying to trick money out of ordinary people's wallets.
-	AI_Output(self,other, " DIA_Bronko_KEINBAUER_sekobderbauer_06_01 " );	// Who said that?
-	AI_Output(other,self, " DIA_Bronko_KEINBAUER_sekobderbauer_15_02 " );	// I said. Sekob wants you back to work, not hanging around here doing nothing.
-	AI_Output(self,other, " DIA_Bronko_KEINBAUER_sekobderbauer_06_03 " );	// So what? What will you do now?
+	AI_Output(other,self, " DIA_Bronko_KEINBAUER_sekobderbauer_15_00 " );	// The farmer here is Sekob, and you're just a petty crook fleecing innocent people.
+	AI_Output(self,other, " DIA_Bronko_KEINBAUER_sekobderbauer_06_01 " );	// Is that right? Says who?
+	AI_Output(other,self, " DIA_Bronko_KEINBAUER_sekobderbauer_15_02 " );	// Says me. Sekob wants you back to work, not hanging around here stinking up the place.
+	AI_Output(self,other, " DIA_Bronko_KEINBAUER_sekobderbauer_06_03 " );	// Oh? Why don't you make me?
 };
 
 func void DIA_Bronko_KEINBAUER_schongut()
 {
 	AI_Output(other,self, " DIA_Bronko_KEINBAUER_schongut_15_00 " );	// Forget it!
-	AI_Output(self,other, " DIA_Bronko_KEINBAUER_schongut_06_01 " );	// Get lost!
+	AI_Output(self,other, " DIA_Bronko_KEINBAUER_schongut_06_01 " );	// Get lost then!
 	AI_StopProcessInfos(self);
 };
 
@@ -183,13 +183,13 @@ func void DIA_Bronko_KEINBAUER_SLD()
 {
 	AI_Output(other,self, " DIA_Bronko_KEINBAUER_SLD_15_00 " );	// Okay, then maybe I'll have to tell Onar that there's an impudent farmer here who refuses to pay rent.
 	AI_Output(self,other, " DIA_Bronko_KEINBAUER_SLD_06_01 " );	// Damn. Wait a minute. Onar will send mercenaries here.
-	AI_Output(other,self,"DIA_Bronko_KEINBAUER_SLD_15_02");	//И что?
-	AI_Output(self,other, " DIA_Bronko_KEINBAUER_SLD_06_03 " );	// Okay, okay. I'll give you whatever you want, but just don't involve mercenaries in this business, okay?
+	AI_Output(other,self,"DIA_Bronko_KEINBAUER_SLD_15_02");	//So what?
+	AI_Output(self,other, " DIA_Bronko_KEINBAUER_SLD_06_03 " );	// Okay, okay. I'll give you whatever you want, but just don't involve the mercenaries in this, okay?
 	if(B_GiveInvItems(self,other,ItMi_Gold,Npc_HasItems(self,ItMi_Gold)))
 	{
 		AI_Output(self,other, " DIA_Bronko_KEINBAUER_SLD_06_04 " );	// Here, I'll even give you all my gold.
 	};
-	AI_Output(self,other, " DIA_Bronko_KEINBAUER_SLD_06_05 " );	// And I'll go back to the field and work. Anything, just do not need mercenaries.
+	AI_Output(self,other, " DIA_Bronko_KEINBAUER_SLD_06_05 " );	// And I'll go back to the field and work. Anything, just keep those bastards away from me, ok?
 	AI_StopProcessInfos(self);
 	DIA_Bronko_KEINBAUER_noPerm = TRUE ;
 	self.guild = GIL_BAU;
@@ -226,19 +226,19 @@ func void DIA_Bronko_DILIGIOUS_Info()
 	}
 	else
 	{
-		AI_Output(other,self, " DIA_Bronko_FLEISSIG_15_01 " );	// How are you? Have you lost the desire to poke around yet?
+		AI_Output(other,self, " DIA_Bronko_FLEISSIG_15_01 " );	// How are you? Have you lost the desire to screw around yet?
 	};
 	if((hero.guild == GIL_SLD) || (hero.guild == GIL_DJG))
 	{
-		AI_Output(self,other, " DIA_Bronko_FLEISSIG_06_02 " );	// You're a mercenary, right? I could guess.
+		AI_Output(self,other, " DIA_Bronko_FLEISSIG_06_02 " );	// You're a mercenary, aren't you? Its obvious.
 	}
 	else  if (MIS_Sekob_Bronko_einschuechtert ==  LOG_SUCCESS )
 	{
-		AI_Output(self,other, " DIA_Bronko_FLEISSIG_06_03 " );	// (in fear) You're not bringing these mercenaries here, are you?
+		AI_Output(self,other, " DIA_Bronko_FLEISSIG_06_03 " );	// (in fear) You're not bringing the mercenaries here, are you?
 	};
 	if((self.aivar[AIV_LastFightAgainstPlayer] == FIGHT_LOST) || (hero.guild == GIL_SLD) || (hero.guild == GIL_DJG))
 	{
-		AI_Output(self,other, " DIA_Bronko_FLEISSIG_06_04 " );	// Don't hit me, please.
+		AI_Output(self,other, " DIA_Bronko_FLEISSIG_06_04 " );	// Don't hurt me, please.
 	};
 	AI_Output(self,other, " DIA_Bronko_FLEISSIG_06_05 " );	// I'll even get back to work, okay?
 	MIS_Sekob_Bronko_einschuechtert = LOG_SUCCESS ;
