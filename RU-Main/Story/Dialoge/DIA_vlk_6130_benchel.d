f56dad70cac3fe55d4b44c99970823879e5779be
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-02 21:11:12 +0200

    Update DIA_vlk_6130_benchel.d

--- RU-Main/Story/Dialoge/DIA_vlk_6130_benchel.d
+++ RU-Main/Story/Dialoge/DIA_vlk_6130_benchel.d
@@ -1,5 +1,6 @@
 
-instance DIA_DIA_VLK_6130_BENCHEL_EXIT(C_Info)
+
+instances DIA_DIA_VLK_6130_BENCHEL_EXIT (C_Info)
 {
 	npc = vlk_6130_benchel;
 	nr = 999;
@@ -21,12 +22,12 @@ func void dia_vlk_6130_benchel_exit_info()
 };
 
 
-instance DIA_VLK_6130_BENCHEL_MISSBRENDI(C_Info)
+instance DIA_VLK_6130_BENCHEL_MISSBRENDI (C_Info)
 {
 	npc = vlk_6130_benchel;
 	nr = 1;
 	condition = dia_vlk_6130_benchel_missbrendi_condition;
-	information = dia_vlk_6130_benchel_missbrendi_info;
+	info = dia_vlk_6130_benchel_missbrendi_info;
 	permanent = FALSE;
 	important = TRUE;
 };
@@ -43,21 +44,21 @@ func int dia_vlk_6130_benchel_missbrendi_condition()
 func void dia_vlk_6130_benchel_missbrendi_info()
 {
 	B_GivePlayerXP(100);
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_MissBrendi_Info_01_01");	//(переводя дыхание) Спасибо, что помог мне!
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_MissBrendi_Info_01_02");	//Если бы не ты, то эти ублюдки отправили бы меня на тот свет!
-	AI_Output(other,self,"DIA_VLK_6130_Benchel_MissBrendi_Info_01_04");	//Успокойся, парень. Тебя зовут Бенчел?
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_MissBrendi_Info_01_05");	//Да, это я Бенчел.
-	AI_Output(other,self,"DIA_VLK_6130_Benchel_MissBrendi_Info_01_06");	//Тогда я хочу сказать тебе, Бенчел, что Лютеро уже заждался тебя.
-	AI_Output(other,self,"DIA_VLK_6130_Benchel_MissBrendi_Info_01_08");	//Ты ведь должен был отнести ему партию черного бренди, который дал тебе Орлан.
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_MissBrendi_Info_01_13");	//Да, да. Я уже шел обратно в город, чтобы отнести Лютеро это чертово пойло.
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_MissBrendi_Info_01_14");	//Но потом вон из-за тех кустов выбежали эти парни и окружили меня.
-	AI_Output(other,self,"DIA_VLK_6130_Benchel_MissBrendi_Info_01_18");	//Что им от тебя было нужно?
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_MissBrendi_Info_01_24");	//В таверне я ляпнул, что несу одному крупному торговцу в Хоринис очень дорогой товар.
-	AI_Output(other,self,"DIA_VLK_6130_Benchel_MissBrendi_Info_01_25");	//Дай угадаю - черный бренди?
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_MissBrendi_Info_01_28");	//Именно. Одна бутылка стоит целое состояние - не сказать больше!
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_MissBrendi_Info_01_30");	//Раньше его возили с материка, но после того, как в нашу гавань перестали заходить торговые корабли, новые партии перестали поступать.
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_MissBrendi_Info_01_31");	//Поэтому цены на этот черный бренди так и взлетели!
-	B_LogEntry(TOPIC_BLACKBRENDI,"Я помог Бенчелу разобраться с бандитами.");
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_MissBrendi_Info_01_01 " );	// (breathing) Thanks for helping me!
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_MissBrendi_Info_01_02 " );	// If it wasn't for you, these bastards would have sent me to the next world!
+	AI_Output(other,self, " DIA_VLK_6130_Benchel_MissBrendi_Info_01_04 " );	// Calm down, boy. Is your name Benchel?
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_MissBrendi_Info_01_05 " );	// Yes, it's me Benchel.
+	AI_Output(other,self, " DIA_VLK_6130_Benchel_MissBrendi_Info_01_06 " );	// Then I want to tell you, Benchel, that Luthero has been waiting for you.
+	AI_Output(other,self, " DIA_VLK_6130_Benchel_MissBrendi_Info_01_08 " );	// You were supposed to take him the black brandy that Orlan gave you.
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_MissBrendi_Info_01_13 " );	// Yes, yes. I was already on my way back to town to take Luthero the damn swill.
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_MissBrendi_Info_01_14 " );	// But then these guys ran out from behind those bushes and surrounded me.
+	AI_Output(other,self, " DIA_VLK_6130_Benchel_MissBrendi_Info_01_18 " );	// What did they want from you?
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_MissBrendi_Info_01_24 " );	// In the tavern, I blurted out that I was carrying a very expensive product to a large merchant in Khorinis.
+	AI_Output(other,self, " DIA_VLK_6130_Benchel_MissBrendi_Info_01_25 " );	// Let me guess - black brandy?
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_MissBrendi_Info_01_28 " );	// Exactly. One bottle costs a fortune - to say the least!
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_MissBrendi_Info_01_30 " );	// It used to be brought from the mainland, but after merchant ships stopped entering our harbor, new shipments stopped coming.
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_MissBrendi_Info_01_31 " );	// That's why the price of this black brandy soared!
+	B_LogEntry( TOPIC_BLACKBRENDI , " I helped Benchel deal with the bandits. " );
 };
 
 
@@ -68,13 +69,13 @@ instance DIA_VLK_6130_BENCHEL_MISSBRENDIFOLLOW(C_Info)
 	condition = dia_vlk_6130_benchel_missbrendifollow_condition;
 	information = dia_vlk_6130_benchel_missbrendifollow_info;
 	permanent = FALSE;
-	description = "Нужно отнести этот бренди Лютеро.";
+	description = " We need to take this brandy to Luthero. " ;
 };
 
 
 func int dia_vlk_6130_benchel_missbrendifollow_condition()
 {
-	if(Npc_KnowsInfo(hero,dia_vlk_6130_benchel_missbrendi))
+	if (Npc_KnowsInfo(hero,dia_vlk_6130_benchel_missbrendi))
 	{
 		return TRUE;
 	};
@@ -82,26 +83,26 @@ func int dia_vlk_6130_benchel_missbrendifollow_condition()
 
 func void dia_vlk_6130_benchel_missbrendifollow_info()
 {
-	AI_Output(other,self,"DIA_VLK_6130_Benchel_MissBrendiFollow_Info_01_01");	//Нужно отнести этот бренди Лютеро.
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_MissBrendiFollow_Info_01_02");	//Ты прав! Но... но до города еще так далеко, а здешняя местность просто кишит бандитами!
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_MissBrendiFollow_Info_01_04");	//Поэтому я решил, что, пожалуй, лучше останусь тут.
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_MissBrendiFollow_Info_01_07");	//Я не хочу больше рисковать жизнью из-за всякого барахла.
-	AI_Output(other,self,"DIA_VLK_6130_Benchel_MissBrendiFollow_Info_01_11");	//Что скажешь, если я помогу тебе добраться до городских ворот?
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_MissBrendiFollow_Info_01_12");	//Ты пойдешь со мной?
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_MissBrendiFollow_Info_01_18");	//(думает) Хорошо, пошли!
-	B_LogEntry(TOPIC_BLACKBRENDI,"Я согласился сопроводить Бенчела до городских ворот. Похоже, один он просто никуда не пойдет. Так что придется некоторое время побыть ему нянькой.");
+	AI_Output(other,self, " DIA_VLK_6130_Benchel_MissBrendiFollow_Info_01_01 " );	// We need to take this brandy to Luthero.
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_MissBrendiFollow_Info_01_02 " );	// You're right! But ... but the city is still so far away, and the local area is simply teeming with bandits!
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_MissBrendiFollow_Info_01_04 " );	// So I figured I'd rather stay here.
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_MissBrendiFollow_Info_01_07 " );	// I don't want to risk my life for junk anymore.
+	AI_Output(other,self, " DIA_VLK_6130_Benchel_MissBrendiFollow_Info_01_11 " );	// What do you say if I help you get to the city gates?
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_MissBrendiFollow_Info_01_12 " );	// Will you come with me?
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_MissBrendiFollow_Info_01_18 " );	// (thinks) Alright, let's go!
+	B_LogEntry( TOPIC_BLACKBRENDI , " I've agreed to escort Benchel to the city gates. Looks like he's not going anywhere alone. So I'll have to babysit him for a while. " );
 	AI_StopProcessInfos(self);
-	self.aivar[AIV_PARTYMEMBER] = TRUE;
+	self.aivar[ AIV_PARTYMEMBER ] = TRUE ;
 	BENCHELFOLLOWME = TRUE;
 	Npc_ExchangeRoutine(self,"FOLLOW");
 };
 
-instance DIA_VLK_6130_BENCHEL_ANGEKOMMEN(C_Info)
+instance DIA_VLK_6130_BENCHEL_ARRIVED (C_Info)
 {
 	npc = vlk_6130_benchel;
 	nr = 55;
-	condition = dia_vlk_6130_benchel_angekommen_condition;
-	information = dia_vlk_6130_benchel_angekommen_info;
+	condition = dia_vlk_6130_benchel_arrived_condition;
+	information = dia_vlk_6130_benchel_arrived_info;
 	important = TRUE;
 };
 
@@ -116,36 +117,36 @@ func int dia_vlk_6130_benchel_angekommen_condition()
 func void dia_vlk_6130_benchel_angekommen_info()
 {
 	B_GivePlayerXP(200);
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_00");	//Постой! Думаю, дальше я смогу сам.
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_01");	//Да, и кстати, пока мы шли, я тут немного пораскинул мозгами...
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_02");	//...и решил, что хватит с меня всего этого!
-	AI_Output(other,self,"DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_03");	//Что ты имеешь в виду?
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_04");	//Хватит с меня всех этих приключений!
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_05");	//Я до сих пор в себя прийти не могу после той разборки с бандитами.
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_06");	//Поэтому, если встретишь Лютеро, - передай ему, что я больше на него не работаю.
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_09");	//Вот, возьми эти пять бутылок черного думтрейского бренди и отдай ему их сам!
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_10");	//Наверняка он отблагодарит тебя за это. А, может, даже возьмет на работу. Хе-хе...
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_00 " );	// Wait! I think I can do it myself.
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_01 " );	// Oh, and by the way, while we were walking, I was thinking a little here...
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_02 " );	// ...and decided I've had enough of all this!
+	AI_Output(other,self, " DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_03 " );	// What do you mean?
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_04 " );	// I've had enough of all these adventures!
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_05 " );	// I still can't come to my senses after that showdown with the bandits.
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_06 " );	// So if you see Luthero, tell him I don't work for him anymore.
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_09 " );	// Here, take these five bottles of Doomtrey black brandy and give them to him yourself!
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_10 " );	// He'll probably thank you for this. Or maybe even get a job. Hehe...
 	B_GiveInvItems(self,other,itmi_blackbrendi,5);
-	AI_Output(other,self,"DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_12");	//И что ты теперь будешь делать?
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_13");	//Ровным счетом - ничего.
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_14");	//Буду сидеть целыми днями в таверне, пить пиво и наслаждаться покоем.
-	AI_Output(other,self,"DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_15");	//Ну, как знаешь. Я передам Лютеро твои слова.
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_16");	//Тогда удачи, приятель! Мы наверняка еще увидимся.
+	AI_Output(other,self, " DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_12 " );	// And what are you going to do now?
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_13 " );	// Absolutely nothing.
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_14 " );	// I will sit all day in a tavern, drink beer and enjoy peace.
+	AI_Output(other,self, " DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_15 " );	// Well, as you know. I'll pass on your words to Luther.
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_ANGEKOMMEN_Info_01_16 " );	// Then good luck, mate! We will surely see you again.
 	BENCHELLEAVEME = TRUE;
-	B_LogEntry(TOPIC_BLACKBRENDI,"После того как я довел Бенчела до ворот города, он отдал мне черный думтрейский бренди и попросил передать Лютеро, что больше на него не работает. Осталось рассказать обо всем Лютеро.");
+	; _ _ _ _ _ _
 	AI_StopProcessInfos(self);
-	self.aivar[AIV_PARTYMEMBER] = FALSE;
+	self.aivar[ AIV_PARTYMEMBER ] = FALSE ;
 	Npc_ExchangeRoutine(self,"RestInTavern");
 };
 
-instance DIA_VLK_6130_BENCHEL_HALLO(C_Info)
+instances DIA_VLK_6130_BENCHEL_HALLO (C_Info)
 {
 	npc = vlk_6130_benchel;
 	nr = 55;
 	condition = dia_vlk_6130_benchel_hallo_condition;
 	information = dia_vlk_6130_benchel_hallo_info;
 	permanent = TRUE;
-	description = "Как дела, Бенчел?";
+	description = " How are you, Benchel? " ;
 };
 
 func int dia_vlk_6130_benchel_hallo_condition()
@@ -158,20 +159,20 @@ func int dia_vlk_6130_benchel_hallo_condition()
 
 func void dia_vlk_6130_benchel_hallo_info()
 {
-	AI_Output(other,self,"DIA_VLK_6130_Benchel_Hallo_01_00");	//Как дела?
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_Hallo_01_01");	//Лучше не придумаешь, приятель.
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_Hallo_01_02");	//Никогда не чувствовал себя так спокойно!
+	AI_Output(other,self, " DIA_VLK_6130_Benchel_Hallo_01_00 " );	// How are you?
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_Hallo_01_01 " );	// You can't do better, mate.
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_Hallo_01_02 " );	// Never felt so calm!
 };
 
 
-instance DIA_VLK_6130_BENCHEL_GO(C_Info)
+instance DIA_VLK_6130_BENCH_GO (C_Info) .
 {
 	npc = vlk_6130_benchel;
 	nr = 55;
 	condition = dia_vlk_6130_benchel_go_condition;
 	information = dia_vlk_6130_benchel_go_info;
 	permanent = TRUE;
-	description = "Пойдем, я провожу тебя.";
+	description = " Let's go, I'll show you. " ;
 };
 
 
@@ -185,11 +186,11 @@ func int dia_vlk_6130_benchel_go_condition()
 
 func void dia_vlk_6130_benchel_go_info()
 {
-	AI_Output(other,self,"DIA_VLK_6130_Benchel_Go_01_00");	//Не бойся, пойдем! Я провожу тебя.
-	AI_Output(self,other,"DIA_VLK_6130_Benchel_Go_01_01");	//Ну хорошо, пошли...
+	AI_Output(other,self, " DIA_VLK_6130_Benchel_Go_01_00 " );	// Don't be afraid, let's go! I will accompany you.
+	AI_Output(self,other, " DIA_VLK_6130_Benchel_Go_01_01 " );	// Okay, let's go...
 	BENCHELFOLLOWME = TRUE;
 	AI_StopProcessInfos(self);
-	self.aivar[AIV_PARTYMEMBER] = TRUE;
+	self.aivar[ AIV_PARTYMEMBER ] = TRUE ;
 	Npc_ExchangeRoutine(self,"FOLLOW");
 };
 
