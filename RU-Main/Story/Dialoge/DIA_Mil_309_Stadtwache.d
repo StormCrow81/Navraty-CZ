df4b5d21558668568fa808fd68e5f2757b92d261
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-02 19:43:18 +0200

    Update DIA_Mil_309_Stadtwache.d

--- RU-Main/Story/Dialoge/DIA_Mil_309_Stadtwache.d
+++ RU-Main/Story/Dialoge/DIA_Mil_309_Stadtwache.d
@@ -1,7 +1,8 @@
 
-instance DIA_Mil_309_Stadtwache_EXIT(C_Info)
+
+instance DIA_Mil_309_Stadtwache_EXIT (C_Info)
 {
-	npc = Mil_309_Stadtwache;
+	npc = Mil_309_City Guard;
 	nr = 999;
 	condition = DIA_Mil_309_Stadtwache_EXIT_Condition;
 	information = DIA_Mil_309_Stadtwache_EXIT_Info;
@@ -21,9 +22,9 @@ func void DIA_Mil_309_Stadtwache_EXIT_Info()
 };
 
 
-instance DIA_MIL_309_STADTWACHE_TOARMSCITYGATE(C_Info)
+instance DIA_MIL_309_STADTWACHE_TOARMSCITYGATE (C_Info)
 {
-	npc = Mil_309_Stadtwache;
+	npc = Mil_309_City Guard;
 	nr = 1;
 	condition = dia_mil_309_stadtwache_toarmscitygate_condition;
 	information = dia_mil_309_stadtwache_toarmscitygate_info;
@@ -49,14 +50,14 @@ func void dia_mil_309_stadtwache_toarmscitygate_info()
 };
 
 
-instance DIA_Mil_309_Stadtwache_Hallo(C_Info)
+instance DIA_Mil_309_Stadtwache_Hello (C_Info)
 {
-	npc = Mil_309_Stadtwache;
+	npc = Mil_309_City Guard;
 	nr = 2;
 	condition = DIA_Mil_309_Stadtwache_Hallo_Condition;
 	information = DIA_Mil_309_Stadtwache_Hallo_Info;
 	permanent = TRUE;
-	description = "Что новенького?";
+	description = " What's new? " ;
 };
 
 
@@ -67,31 +68,31 @@ func int DIA_Mil_309_Stadtwache_Hallo_Condition()
 
 func void DIA_Mil_309_Stadtwache_Hallo_Info()
 {
-	AI_Output(other,self,"DIA_Mil_309_Stadtwache_Hallo_15_00");	//Как дела?
+	AI_Output(other,self, " DIA_Mil_309_Stadtwache_Hallo_15_00 " );	// How are you?
 	if((hero.guild == GIL_MIL) || (hero.guild == GIL_PAL) || (hero.guild == GIL_KDF) || (hero.guild == GIL_KDW))
 	{
-		AI_Output(self,other,"DIA_Mil_309_Stadtwache_Hallo_06_01");	//Все в порядке! Но мы должны быть настороже.
+		AI_Output(self,other, " DIA_Mil_309_Stadtwache_Hallo_06_01 " );	// All right! But we must be careful.
 	}
-	else if((Stadtwache_310.aivar[AIV_PASSGATE] == FALSE) && (MIl_309_News < 1))
+	else  if ((Citywatch_310.aivar[ AIV_PASSGATE ] ==  FALSE ) && (MIl_309_News <  1 )) ;
 	{
-		AI_Output(self,other,"DIA_Mil_309_Stadtwache_Hallo_06_02");	//Послушай. Мы не можем пропустить тебя в город.
-		AI_Output(self,other,"DIA_Mil_309_Stadtwache_Hallo_06_03");	//Но я дам тебе совет, и совершенно бесплатно.
-		AI_Output(self,other,"DIA_Mil_309_Stadtwache_Hallo_06_04");	//Держись подальше от этого леса впереди - там бродят полчища ужасных монстров.
+		AI_Output(self,other, " DIA_Mil_309_Stadtwache_Hallo_06_02 " );	// Listen. We can't let you into the city.
+		AI_Output(self,other, " DIA_Mil_309_Stadtwache_Hallo_06_03 " );	// But I'll give you advice, and it's completely free.
+		AI_Output(self,other, " DIA_Mil_309_Stadtwache_Hallo_06_04 " );	// Stay away from this forest ahead - there are hordes of terrible monsters roaming there.
 		MIl_309_News = 1;
 	}
-	else if((Stadtwache_310.aivar[AIV_PASSGATE] == FALSE) && (MIl_309_News == 1))
+	else  if ((Stadwatch_310.aivar[ AIV_PASSGATE ] ==  FALSE ) && (MIl_309_News ==  1 )) ;
 	{
-		AI_Output(self,other,"DIA_Mil_309_Stadtwache_Hallo_06_05");	//Возвращайся туда, откуда выполз, подонок!
+		AI_Output(self,other, " DIA_Mil_309_Stadtwache_Hallo_06_05 " );	// Go back to where you crawled out, you bastard!
 	};
-	if((Stadtwache_310.aivar[AIV_PASSGATE] == TRUE) && (MIl_309_News < 2))
+	if ((Citywatch_310.aivar[ AIV_PASSGATE ] ==  TRUE ) && (MIl_309_News <  2 )) ;
 	{
-		AI_Output(self,other,"DIA_Mil_309_Stadtwache_Hallo_06_06");	//Послушай, ты имеешь право находиться в городе. Но это не означает, что ты можешь делать здесь все, что тебе заблагорассудится.
-		AI_Output(self,other,"DIA_Mil_309_Stadtwache_Hallo_06_07");	//Если ты не придерживаешься общепринятых правил, ты потеряешь свои права здесь!
+		AI_Output(self,other, " DIA_Mil_309_Stadtwache_Hallo_06_06 " );	// Look, you have the right to be in the city. But that doesn't mean you can do whatever you want here.
+		AI_Output(self,other, " DIA_Mil_309_Stadtwache_Hallo_06_07 " );	// If you don't follow the accepted rules, you will lose your rights here!
 		MIl_309_News = 2;
 	}
-	else if((Stadtwache_310.aivar[AIV_PASSGATE] == TRUE) && (MIl_309_News == 2))
+	else  if (( Citywatch_310 .aivar[ AIV_PASSGATE ] ==  TRUE ) && (MIl_309_News ==  2 )) ;
 	{
-		AI_Output(self,other,"DIA_Mil_309_Stadtwache_Hallo_06_08");	//Давай - проходи!
+		AI_Output(self,other, " DIA_Mil_309_Stadtwache_Hallo_06_08 " );	// Come on - come on!
 	};
 	AI_StopProcessInfos(self);
 };
