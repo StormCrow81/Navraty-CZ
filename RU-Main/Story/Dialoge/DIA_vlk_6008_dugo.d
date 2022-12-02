631d14e77039fc1f2ac76379d2136e483257c963
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-02 21:57:45 +0200

    Update DIA_vlk_6008_dugo.d

--- RU-Main/Story/Dialoge/DIA_vlk_6008_dugo.d
+++ RU-Main/Story/Dialoge/DIA_vlk_6008_dugo.d
@@ -1,7 +1,8 @@
 
+
 instance DIA_DUGO_EXIT(C_Info)
 {
-	npc = vlk_6008_dugo;
+	npc = vlk_6008_long;
 	nr = 999;
 	condition = dia_dugo_exit_condition;
 	information = dia_dugo_exit_info;
@@ -21,9 +22,9 @@ func void dia_dugo_exit_info()
 };
 
 
-instance DIA_DUGO_HELLO(C_Info)
+instance DIA_DUGO_HELLO (C_Info)
 {
-	npc = vlk_6008_dugo;
+	npc = vlk_6008_long;
 	nr = 1;
 	condition = dia_dugo_hello_condition;
 	information = dia_dugo_hello_info;
@@ -39,27 +40,27 @@ func int dia_dugo_hello_condition()
 
 func void dia_dugo_hello_info()
 {
-	AI_Output(self,other,"DIA_Dugo_Hello_01_00");	//Не могу поверить своим глазам! Откуда? (удивленно) Как ты попал сюда?!
-	AI_Output(other,self,"DIA_Dugo_Hello_01_01");	//Я прошел через портал, тот, что наверху этой пирамиды. А что тебя так удивляет?
-	AI_Output(self,other,"DIA_Dugo_Hello_01_03");	//За все то время, что я себя помню, ты - первый, кого занесло в эти богом забытые края...
+	AI_Output(self,other, " DIA_Dugo_Hello_01_00 " );	// I can't believe my eyes! Where? (surprised) How did you get here?!
+	AI_Output(other,self, " DIA_Dugo_Hello_01_01 " );	// I went through the portal, the one at the top of this pyramid. What surprises you so much?
+	AI_Output(self,other, " DIA_Dugo_Hello_01_03 " );	// For all the time that I can remember, you are the first who was brought to these godforsaken lands...
 };
 
 
 instance DIA_DUGO_HELLO_TWO(C_Info)
 {
-	npc = vlk_6008_dugo;
+	npc = vlk_6008_long;
 	nr = 1;
 	condition = dia_dugo_hello_two_condition;
 	information = dia_dugo_hello_two_info;
 	permanent = TRUE;
 	important = FALSE;
-	description = "Кто ты и что это за место?";
+	description = " Who are you and what is this place? " ;
 };
 
 
 func int dia_dugo_hello_two_condition()
 {
-	if(DUGOMEET == 0)
+	if ( DUGOMEET  ==  0 )
 	{
 		return TRUE;
 	};
@@ -67,30 +68,30 @@ func int dia_dugo_hello_two_condition()
 
 func void dia_dugo_hello_two_info()
 {
-	AI_Output(other,self,"DIA_Dugo_Hello_Two_01_00");	//Кто ты и что это за место?
-	AI_Output(self,other,"DIA_Dugo_Hello_Two_01_01");	//Мое имя Дюго. А все, что ты видишь вокруг, зовется Долиной Теней...
-	AI_Output(self,other,"DIA_Dugo_Hello_Two_01_02");	//И скажу тебе честно, парень - ты попал в самое пекло ада!
-	AI_Output(self,other,"DIA_Dugo_Hello_Two_01_03");	//Это долина проклята! Кругом бродит нечисть, а то и пострашнее создания.
-	AI_Output(self,other,"DIA_Dugo_Hello_Two_01_04");	//А по ночам вообще не стоит бродить в этих местах - иначе моментально рискуешь расстаться с жизнью.
-	DUGOMEET = 1;
+	AI_Output(other,self, " DIA_Dugo_Hello_Two_01_00 " );	// Who are you and what is this place?
+	AI_Output(self,other, " DIA_Dugo_Hello_Two_01_01 " );	// My name is Dugo. And all that you see around is called the Valley of Shadows...
+	AI_Output(self,other, " DIA_Dugo_Hello_Two_01_02 " );	// And I'll tell you honestly, boy - you're in the middle of hell!
+	AI_Output(self,other, " DIA_Dugo_Hello_Two_01_03 " );	// This valley is cursed! Evil spirits roam around, and even more terrible creatures.
+	AI_Output(self,other, " DIA_Dugo_Hello_Two_01_04 " );	// And at night you should not wander in these places at all - otherwise you immediately risk losing your life.
+	DUGOMEET = 1 ;
 };
 
 
 instance DIA_DUGO_HELLO_THREE(C_Info)
 {
-	npc = vlk_6008_dugo;
+	npc = vlk_6008_long;
 	nr = 1;
 	condition = dia_dugo_hello_three_condition;
 	information = dia_dugo_hello_three_info;
 	permanent = TRUE;
 	important = FALSE;
-	description = "Расскажи мне об этой долине.";
+	description = " Tell me about this valley. " ;
 };
 
 
 func int dia_dugo_hello_three_condition()
 {
-	if(DUGOMEET == 1)
+	if ( DUGOMEET  ==  1 )
 	{
 		return TRUE;
 	};
@@ -98,43 +99,43 @@ func int dia_dugo_hello_three_condition()
 
 func void dia_dugo_hello_three_info()
 {
-	AI_Output(other,self,"DIA_Dugo_Hello_Three_01_15");	//Расскажи мне об этой долине.
-	AI_Output(self,other,"DIA_Dugo_Hello_Three_01_00");	//Да что тут рассказывать...
-	AI_Output(self,other,"DIA_Dugo_Hello_Three_01_01");	//(вздыхает) Когда-то это была прекрасная долина с красивыми лугами и вечнозелеными лесами у подножья этих величественных гор.
-	AI_Output(self,other,"DIA_Dugo_Hello_Three_01_02");	//К сожалению, от этого остались лишь воспоминания и ничего более.
-	AI_Output(self,other,"DIA_Dugo_Hello_Three_01_03");	//Теперь это безжизненная пустошь, покрытая золой и пеплом. А все вокруг пропитано злом и ужасом.
-	AI_Output(self,other,"DIA_Dugo_Hello_Three_01_04");	//Поверь, здесь нет ничего кроме смерти.
-	AI_Output(other,self,"DIA_Dugo_Hello_Three_01_05");	//А что здесь случилось?
-	AI_Output(self,other,"DIA_Dugo_Hello_Three_01_06");	//Зло пришло в этот мир. Страшное зло! Зло, которому нет имени.
-	AI_Output(self,other,"DIA_Dugo_Hello_Three_01_08");	//Говорили, что это было невиданное чудовище, которое изрыгнула сама преисподняя!
-	AI_Output(other,self,"DIA_Dugo_Hello_Three_01_09");	//А это случайно не был дракон?
-	AI_Output(self,other,"DIA_Dugo_Hello_Three_01_10");	//Я не знаю, кто это был. Может и дракон, а может и нет. Никто не знает.
-	AI_Output(self,other,"DIA_Dugo_Hello_Three_01_11");	//Одно знаю: немногим в те ужасные времена уладось спасти свои жизни...
-	AI_Output(self,other,"DIA_Dugo_Hello_Three_01_12");	//А те, кому все-таки это удалось - были обречены вечно прозябать в этой долине смерти! (вздыхает)
-	AI_Output(self,other,"DIA_Dugo_Hello_Three_01_14");	//Это все, что я мог тебе рассказать. Если хочешь узнать больше, тебе стоит спросить об этом у Эльваиса.
+	AI_Output(other,self, " DIA_Dugo_Hello_Three_01_15 " );	// Tell me about this valley.
+	AI_Output(self,other, " DIA_Dugo_Hello_Three_01_00 " );	// What can I say...
+	AI_Output(self,other, " DIA_Dugo_Hello_Three_01_01 " );	// (sighs) Once it was a beautiful valley with beautiful meadows and evergreen forests at the foot of these majestic mountains.
+	AI_Output(self,other, " DIA_Dugo_Hello_Three_01_02 " );	// Unfortunately, only memories remain of this and nothing more.
+	AI_Output(self,other, " DIA_Dugo_Hello_Three_01_03 " );	// Now it's a lifeless wasteland, covered in ashes and ashes. And everything around is saturated with evil and horror.
+	AI_Output(self,other, " DIA_Dugo_Hello_Three_01_04 " );	// Trust me, there's nothing here but death.
+	AI_Output(other,self, " DIA_Dugo_Hello_Three_01_05 " );	// What happened here?
+	AI_Output(self,other, " DIA_Dugo_Hello_Three_01_06 " );	// Evil has come into this world. Terrible evil! An evil that has no name.
+	AI_Output(self,other, " DIA_Dugo_Hello_Three_01_08 " );	// It was said that it was an unprecedented monster that was vomited up by the underworld itself!
+	AI_Output(other,self, " DIA_Dugo_Hello_Three_01_09 " );	// Wasn't that a dragon by any chance?
+	AI_Output(self,other, " DIA_Dugo_Hello_Three_01_10 " );	// I don't know who it was. Maybe a dragon, maybe not. No one knows.
+	AI_Output(self,other, " DIA_Dugo_Hello_Three_01_11 " );	// I know one thing: few managed to save their lives in those terrible times...
+	AI_Output(self,other, " DIA_Dugo_Hello_Three_01_12 " );	// And those who did succeed were doomed to vegetate forever in this valley of death! (sighs)
+	AI_Output(self,other, " DIA_Dugo_Hello_Three_01_14 " );	// That's all I could tell you. If you want to know more, you should ask Elvais about it.
 	if(MIS_URNAZULRAGE == LOG_Running)
 	{
-		B_LogEntry(TOPIC_URNAZULRAGE,"Недалеко от пирамиды с телепортером, который меня привел в Долину Теней, я встретил охотника по имени Дюго. Судя по его словам, место, в которое я попал - сущий ад!");
+		) ; _ _ _ _ _
 	};
-	DUGOMEET = 2;
+	LENGTH = 2 ;
 };
 
 
 instance DIA_DUGO_HELLO_FOUR(C_Info)
 {
-	npc = vlk_6008_dugo;
+	npc = vlk_6008_long;
 	nr = 1;
 	condition = dia_dugo_hello_four_condition;
 	information = dia_dugo_hello_four_info;
 	permanent = TRUE;
 	important = FALSE;
-	description = "А вы не думали убраться отсюда?";
+	description = " Have you thought about getting out of here? " ;
 };
 
 
 func int dia_dugo_hello_four_condition()
 {
-	if((DUGOMEET == 2) && (DUGOMEETONE == 0))
+	if (( DUGOMEET  ==  2 ) && ( DUGOMEETONE  ==  0 ))
 	{
 		return TRUE;
 	};
@@ -142,37 +143,37 @@ func int dia_dugo_hello_four_condition()
 
 func void dia_dugo_hello_four_info()
 {
-	AI_Output(other,self,"DIA_Dugo_Hello_Four_01_01");	//А вы не думали убраться отсюда?
-	AI_Output(self,other,"DIA_Dugo_Hello_Four_01_03");	//Из этой долины нет выхода. Проход через горы, ведущий отсюда, давно завалило камнями после одного из землетрясений.
-	AI_Output(self,other,"DIA_Dugo_Hello_Four_01_04");	//Оставался только каменный пьедестал в пирамиде, похожий на портал...
-	AI_Output(self,other,"DIA_Dugo_Hello_Four_01_05");	//Но до сегодняшнего дня все наши попытки активировать его ни к чему не привели.
-	AI_Output(other,self,"DIA_Dugo_Hello_Four_01_08");	//Я бы не стал сильно рассчитывать на это.
-	AI_Output(other,self,"DIA_Dugo_Hello_Four_01_11");	//Дело в том, что портал ведем прямиком в подземный город орков, и, скорее всего, вы не сможете пробится через их орды.
-	AI_Output(self,other,"DIA_Dugo_Hello_Four_01_13");	//Что?! О боже, это была наша последняя надежда на спасение.
-	AI_Output(self,other,"DIA_Dugo_Hello_Four_01_14");	//Но как ТЫ смог пройти мимо орков?!
-	AI_Output(other,self,"DIA_Dugo_Hello_Four_01_15");	//У меня есть орочий талисман. Один из орков сделал его для меня, поскольку я спас ему жизнь.
-	AI_Output(other,self,"DIA_Dugo_Hello_Four_01_16");	//Для орков он означает знак силы и дружбы. Они уважают его владельца и не смеют на него нападать.
-	AI_Output(self,other,"DIA_Dugo_Hello_Four_01_17");	//(зло) Чертовы орки!
-	AI_Output(self,other,"DIA_Dugo_Hello_Four_01_18");	//Тогда, боюсь, у нас нет другого выбора. Придется пробивать себе дорогу мечом.
-	DUGOMEETONE = 1;
+	AI_Output(other,self, " DIA_Dugo_Hello_Four_01_01 " );	// Have you thought about getting out of here?
+	AI_Output(self,other, " DIA_Dugo_Hello_Four_01_03 " );	// There is no way out of this valley. The passage through the mountains leading from here was blocked with stones long ago after one of the earthquakes.
+	AI_Output(self,other, " DIA_Dugo_Hello_Four_01_04 " );	// Only the stone pedestal in the pyramid remained, looking like a portal...
+	AI_Output(self,other, " DIA_Dugo_Hello_Four_01_05 " );	// But until today, all our attempts to activate it have failed.
+	AI_Output(other,self, " DIA_Dugo_Hello_Four_01_08 " );	// I wouldn't count on it too much.
+	AI_Output(other,self, " DIA_Dugo_Hello_Four_01_11 " );	// The fact is that the portal leads straight to the underground city of the orcs, and, most likely, you will not be able to break through their hordes.
+	AI_Output(self,other, " DIA_Dugo_Hello_Four_01_13 " );	// What?! Oh god, this was our last hope for salvation.
+	AI_Output(self,other, " DIA_Dugo_Hello_Four_01_14 " );	// But how could YOU get past the orcs?!
+	AI_Output(other,self, " DIA_Dugo_Hello_Four_01_15 " );	// I have an orc talisman. One of the orcs made it for me because I saved his life.
+	AI_Output(other,self, " DIA_Dugo_Hello_Four_01_16 " );	// For orcs, it means a sign of strength and friendship. They respect its owner and do not dare to attack him.
+	AI_Output(self,other, " DIA_Dugo_Hello_Four_01_17 " );	// (angry) Damn orcs!
+	AI_Output(self,other, " DIA_Dugo_Hello_Four_01_18 " );	// Then I'm afraid we have no other choice. You'll have to punch your way with a sword.
+	LONGOMETONE = 1 ;
 };
 
 
 instance DIA_DUGO_HELLO_FIVE(C_Info)
 {
-	npc = vlk_6008_dugo;
+	npc = vlk_6008_long;
 	nr = 1;
 	condition = dia_dugo_hello_five_condition;
 	information = dia_dugo_hello_five_info;
 	permanent = TRUE;
 	important = FALSE;
-	description = "Эльваис? А кто это?";
+	description = " Elvais? Who is this? " ;
 };
 
 
 func int dia_dugo_hello_five_condition()
 {
-	if((DUGOMEET == 2) && (DUGOMEETTWO == 0))
+	if (( CITYMEET  ==  2 ) && ( CITIZENS  ==  0 ))
 	{
 		return TRUE;
 	};
@@ -180,17 +181,17 @@ func int dia_dugo_hello_five_condition()
 
 func void dia_dugo_hello_five_info()
 {
-	AI_Output(other,self,"DIA_Dugo_Hello_Five_01_01");	//Эльваис? А кто это?
-	AI_Output(self,other,"DIA_Dugo_Hello_Five_01_02");	//Он что-то вроде нашего старейшины в лагере. Кроме того, он самый умный из нас, и может о многом тебе рассказать.
-	AI_Output(self,other,"DIA_Dugo_Hello_Five_01_04");	//Позади меня пещера - это наш лагерь. Там можно чувствовать себя в безопасности!
-	AI_Output(self,other,"DIA_Dugo_Hello_Five_01_05");	//Ты найдешь Эльваиса в одной из дальних частей пещеры. Можешь пообщаться и с другими ребятами.
-	AI_Output(self,other,"DIA_Dugo_Hello_Five_01_06");	//Некоторые из них отличные охотники - например, Багрус. А Гунмар - лучший из лучников, которых мне приходилось знать.
-	AI_Output(self,other,"DIA_Dugo_Hello_Five_01_07");	//У них есть чему поучиться!
+	AI_Output(other,self, " DIA_Dugo_Hello_Five_01_01 " );	// Elvais? And who is it?
+	AI_Output(self,other, " DIA_Dugo_Hello_Five_01_02 " );	// He's kind of like our camp elder. Besides, he's the smartest of us, and he can tell you a lot.
+	AI_Output(self,other, " DIA_Dugo_Hello_Five_01_04 " );	// Behind me, the cave is our camp. You can feel safe there!
+	AI_Output(self,other, " DIA_Dugo_Hello_Five_01_05 " );	// You will find Elvais in one of the farther parts of the cave. You can also chat with other guys.
+	AI_Output(self,other, " DIA_Dugo_Hello_Five_01_06 " );	// Some of them are great hunters, like Bagrus. And Gunmar is the best archer I've ever known.
+	AI_Output(self,other, " DIA_Dugo_Hello_Five_01_07 " );	// They have a lot to learn!
 	if(MIS_URNAZULRAGE == LOG_Running)
 	{
-		B_LogEntry(TOPIC_URNAZULRAGE,"Дюго посоветовал мне поговорить с Эльваисом - старейшиной в лагере. Он сможет рассказать мне больше об этом месте. Возможно, я смогу разузнать у него кое-что о черном драконе. Я смогу найти Эльваиса в дальней из пещер, что слева от входа в лагерь.");
+		B_LogEntry( TOPIC_URNAZULRAGE , " Dugo advised me to talk to Elvais, the elder in the camp. He can tell me more about this place. Perhaps I can find out something about the black dragon from him. I can find Elvais in the farthest of the caves on the left from the entrance to the camp. " );
 	};
-	DUGOMEETTWO = 1;
+	LONGOMEETTWO = 1 ;
 	KNOWABELV = TRUE;
 	Wld_InsertNpc(vlk_6020_harum,"SV_HUMAN_20");
 };
@@ -198,19 +199,19 @@ func void dia_dugo_hello_five_info()
 
 instance DIA_DUGO_HELLO_SEVEN(C_Info)
 {
-	npc = vlk_6008_dugo;
+	npc = vlk_6008_long;
 	nr = 1;
 	condition = dia_dugo_hello_seven_condition;
 	information = dia_dugo_hello_seven_info;
 	permanent = TRUE;
 	important = FALSE;
-	description = "Возможно мне удастся помочь вам.";
+	description = " Maybe I can help you. " ;
 };
 
 
 func int dia_dugo_hello_seven_condition()
 {
-	if((DUGOMEETONE == 1) && (MIS_URNAZULRAGE == LOG_Running))
+	if (( DUGOMEETONE  ==  1 ) && ( MIS_URNAZULRAGE  == LOG_Running ))
 	{
 		return TRUE;
 	};
@@ -218,21 +219,21 @@ func int dia_dugo_hello_seven_condition()
 
 func void dia_dugo_hello_seven_info()
 {
-	AI_Output(other,self,"DIA_Dugo_Hello_Seven_01_01");	//Возможно, мне все-таки удастся помочь вам выбраться отсюда.
-	AI_Output(other,self,"DIA_Dugo_Hello_Seven_01_02");	//Идея прорываться через орды орков просто безумна. Вы и понятия не имеете, сколько их там!
-	AI_Output(other,self,"DIA_Dugo_Hello_Seven_01_03");	//Они разорвут вас на куски!
-	AI_Output(self,other,"DIA_Dugo_Hello_Seven_01_04");	//А что, у тебя есть предложение получше?
-	AI_Output(other,self,"DIA_Dugo_Hello_Seven_01_07");	//Возможно. Но для начала мне надо уладить здесь одного рода дело.
-	AI_Output(self,other,"DIA_Dugo_Hello_Seven_01_08");	//И какое же дело могло привести человека в эти забытые Инносом края?
-	AI_Output(other,self,"DIA_Dugo_Hello_Seven_01_10");	//Я слышал, в этой долине живет один очень могущественный дракон. Его имя Азгалор.
-	AI_Output(self,other,"DIA_Dugo_Hello_Seven_01_11");	//Я уже слышал это имя. Эльваис упоминал его в одной из своих историй.
-	AI_Output(self,other,"DIA_Dugo_Hello_Seven_01_12");	//Тебе следует поговорить с ним. Он знает об этом создании больше, чем я.
-	AI_Output(self,other,"DIA_Dugo_Hello_Seven_01_14");	//Скажи, а на кой черт тебе вообще сдался этот дракон?
-	AI_Output(other,self,"DIA_Dugo_Hello_Seven_01_15");	//Я пришел сюда, чтобы убить эту тварь.
-	AI_Output(self,other,"DIA_Dugo_Hello_Seven_01_16");	//Ну, парень, ты не перестаешь удивлять меня. Дело, конечно, твое, но по-моему у тебя крыша съехала!
+	AI_Output(other,self, " DIA_Dugo_Hello_Seven_01_01 " );	// I might still be able to help you get out of here.
+	AI_Output(other,self, " DIA_Dugo_Hello_Seven_01_02 " );	// The idea of ​​breaking through hordes of orcs is crazy. You have no idea how many there are!
+	AI_Output(other,self, " DIA_Dugo_Hello_Seven_01_03 " );	// They'll tear you to pieces!
+	AI_Output(self,other, " DIA_Dugo_Hello_Seven_01_04 " );	// Do you have a better suggestion?
+	AI_Output(other,self, " DIA_Dugo_Hello_Seven_01_07 " );	// Possibly. But first I have to settle one sort of business here.
+	AI_Output(self,other, " DIA_Dugo_Hello_Seven_01_08 " );	// And what business could bring a person to these lands forgotten by Innos?
+	AI_Output(other,self, " DIA_Dugo_Hello_Seven_01_10 " );	// I heard there's a very powerful dragon living in this valley. His name is Azgalor.
+	AI_Output(self,other, " DIA_Dugo_Hello_Seven_01_11 " );	// I've heard this name before. Elvais mentioned him in one of his stories.
+	AI_Output(self,other, " DIA_Dugo_Hello_Seven_01_12 " );	// You should talk to him. He knows more about this creature than I do.
+	AI_Output(self,other, " DIA_Dugo_Hello_Seven_01_14 " );	// Tell me, why the hell did you give up on this dragon?
+	AI_Output(other,self, " DIA_Dugo_Hello_Seven_01_15 " );	// I came here to kill this thing.
+	AI_Output(self,other, " DIA_Dugo_Hello_Seven_01_16 " );	// Well, boy, you never cease to amaze me. It's up to you, of course, but I think you've lost your mind!
 	Log_CreateTopic(TOPIC_HUMANINVALLEY,LOG_MISSION);
 	Log_SetTopicStatus(TOPIC_HUMANINVALLEY,LOG_Running);
-	B_LogEntry(TOPIC_HUMANINVALLEY,"Возможно, я мог бы помочь живущим здесь людям выбраться из этого адского местечка. Вопрос в том - как?");
-	DUGOMEETONE = 2;
+	B_LogEntry( TOPIC_HUMANINVALLEY , " Maybe I could help the people who live here get out of this hellish place. The question is how? " );
+	LONGOMETONE = 2 ;
 };
 
