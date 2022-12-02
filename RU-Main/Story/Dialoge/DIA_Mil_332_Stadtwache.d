3d09e28df28686318c706faf8600fe564c00d720
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-02 20:28:06 +0200

    Update DIA_Mil_332_Stadtwache.d

--- RU-Main/Story/Dialoge/DIA_Mil_332_Stadtwache.d
+++ RU-Main/Story/Dialoge/DIA_Mil_332_Stadtwache.d
@@ -1,7 +1,8 @@
 
-instance DIA_Mil_332_Stadtwache_EXIT(C_Info)
+
+instance DIA_Mil_332_Stadtwache_EXIT (C_Info)
 {
-	npc = Mil_332_Stadtwache;
+	npc = Mil_332_City Guard;
 	nr = 999;
 	condition = DIA_Mil_332_Stadtwache_EXIT_Condition;
 	information = DIA_Mil_332_Stadtwache_EXIT_Info;
@@ -21,9 +22,9 @@ func void DIA_Mil_332_Stadtwache_EXIT_Info()
 };
 
 
-instance DIA_MIL_332_STADTWACHE_TOARMSCITYGATE(C_Info)
+instance DIA_MIL_332_STADTWACHE_TOARMSCITYGATE (C_Info)
 {
-	npc = Mil_332_Stadtwache;
+	npc = Mil_332_City Guard;
 	nr = 1;
 	condition = dia_mil_332_stadtwache_toarmscitygate_condition;
 	information = dia_mil_332_stadtwache_toarmscitygate_info;
@@ -49,14 +50,14 @@ func void dia_mil_332_stadtwache_toarmscitygate_info()
 };
 
 
-instance DIA_Mil_332_Stadtwache_PERM(C_Info)
+instance DIA_Mil_332_Stadtwache_PERM (C_Info)
 {
-	npc = Mil_332_Stadtwache;
+	npc = Mil_332_City Guard;
 	nr = 1;
 	condition = DIA_Mil_332_Stadtwache_PERM_Condition;
 	information = DIA_Mil_332_Stadtwache_PERM_Info;
 	permanent = TRUE;
-	description = "Как дела?";
+	description = " How are you? " ;
 };
 
 
@@ -67,18 +68,18 @@ func int DIA_Mil_332_Stadtwache_PERM_Condition()
 
 func void DIA_Mil_332_Stadtwache_PERM_Info()
 {
-	AI_Output(other,self,"DIA_Mil_332_Stadtwache_PERM_15_00");	//Как дела?
-	AI_Output(self,other,"DIA_Mil_332_Stadtwache_PERM_04_01");	//Проходи! Я на посту!
+	AI_Output(other,self, " DIA_Mil_332_Stadtwache_PERM_15_00 " );	// How are you?
+	AI_Output(self,other, " DIA_Mil_332_Stadtwache_PERM_04_01 " );	// Come on! I'm on duty!
 	AI_StopProcessInfos(self);
 };
 
 
-//----------------------------------------дезертиры-----------------------------------------------
+// ---------------------------------------- deserters------- ----------------------------------------
 
 
-instance DIA_Mil_3320_Stadtwache_EXIT(C_Info)
+instance DIA_Mil_3320_Stadtwache_EXIT (C_Info)
 {
-	npc = Mil_3320_Stadtwache;
+	npc = Mil_3320_City Guard;
 	nr = 999;
 	condition = DIA_Mil_3320_Stadtwache_EXIT_Condition;
 	information = DIA_Mil_3320_Stadtwache_EXIT_Info;
@@ -96,9 +97,9 @@ func void DIA_Mil_3320_Stadtwache_EXIT_Info()
 	AI_StopProcessInfos(self);
 };
 
-instance DIA_Mil_3321_Stadtwache_EXIT(C_Info)
+instance DIA_Mil_3321_Stadtwache_EXIT (C_Info)
 {
-	npc = Mil_3321_Stadtwache;
+	npc = Mil_3321_City Guard;
 	nr = 999;
 	condition = DIA_Mil_3321_Stadtwache_EXIT_Condition;
 	information = DIA_Mil_3321_Stadtwache_EXIT_Info;
@@ -116,9 +117,9 @@ func void DIA_Mil_3321_Stadtwache_EXIT_Info()
 	AI_StopProcessInfos(self);
 };
 
-instance DIA_Mil_3322_Stadtwache_EXIT(C_Info)
+instance DIA_Mil_3322_Stadtwache_EXIT (C_Info)
 {
-	npc = Mil_3322_Stadtwache;
+	npc = Mil_3322_City Guard;
 	nr = 999;
 	condition = DIA_Mil_3322_Stadtwache_EXIT_Condition;
 	information = DIA_Mil_3322_Stadtwache_EXIT_Info;
@@ -136,9 +137,9 @@ func void DIA_Mil_3322_Stadtwache_EXIT_Info()
 	AI_StopProcessInfos(self);
 };
 
-instance DIA_Mil_3321_Stadtwache_FUCKOFF(C_Info)
+instance DIA_Mil_3321_Stadtwache_FUCKOFF (C_Info)
 {
-	npc = Mil_3321_Stadtwache;
+	npc = Mil_3321_City Guard;
 	nr = 10;
 	condition = DIA_Mil_3321_Stadtwache_FUCKOFF_condition;
 	information = DIA_Mil_3321_Stadtwache_FUCKOFF_info;
@@ -161,9 +162,9 @@ func void DIA_Mil_3321_Stadtwache_FUCKOFF_info()
 	Npc_SetRefuseTalk(self,300);
 };
 
-instance DIA_Mil_3322_Stadtwache_FUCKOFF(C_Info)
+instance DIA_Mil_3322_Stadtwache_FUCKOFF (C_Info)
 {
-	npc = Mil_3322_Stadtwache;
+	npc = Mil_3322_City Guard;
 	nr = 10;
 	condition = DIA_Mil_3322_Stadtwache_FUCKOFF_condition;
 	information = DIA_Mil_3322_Stadtwache_FUCKOFF_info;
@@ -186,14 +187,14 @@ func void DIA_Mil_3322_Stadtwache_FUCKOFF_info()
 	Npc_SetRefuseTalk(self,300);
 };
 
-instance DIA_Mil_3320_Stadtwache_Run(C_Info)
+instance DIA_Mil_3320_Stadtwache_Run (C_Info)
 {
-	npc = Mil_3320_Stadtwache;
+	npc = Mil_3320_City Guard;
 	nr = 1;
 	condition = DIA_Mil_3320_Stadtwache_Run_condition;
 	information = DIA_Mil_3320_Stadtwache_Run_info;
 	permanent = FALSE;
-	description = "Эй, вы что тут делаете?";
+	description = " Hey, what are you doing here? " ;
 };
 
 func int DIA_Mil_3320_Stadtwache_Run_condition()
@@ -207,28 +208,28 @@ func int DIA_Mil_3320_Stadtwache_Run_condition()
 func void DIA_Mil_3320_Stadtwache_Run_info()
 {
 	B_GivePlayerXP(150);
-	AI_Output(other,self,"DIA_Mil_3320_Stadtwache_Run_01_00");	//Эй, вы что тут делаете?
-	AI_Output(self,other,"DIA_Mil_3320_Stadtwache_Run_01_01");	//Эмм...(замялся) Просто у нас есть приказ вернутся обратно в Хоринис.
-	AI_Output(other,self,"DIA_Mil_3320_Stadtwache_Run_01_02");	//Чей приказ?
-	AI_Output(self,other,"DIA_Mil_3320_Stadtwache_Run_01_03");	//Ну...(задумавшись) Его отдал нам... лорд Андрэ. Да, он самый.
-	AI_Output(other,self,"DIA_Mil_3320_Stadtwache_Run_01_04");	//Но лорд Андрэ сейчас в Хоринисе. Ты что, за дурака меня держишь?
-	AI_Output(self,other,"DIA_Mil_3320_Stadtwache_Run_01_05");	//Да нет, что ты... я... просто перепутал... конечно, это был не лорд Андрэ!
-	AI_Output(other,self,"DIA_Mil_3320_Stadtwache_Run_01_06");	//А знаешь, обычно подобные приказы должны сопровождаться письменными документами. Они у вас есть?
-	AI_Output(self,other,"DIA_Mil_3320_Stadtwache_Run_01_08");	//Слушай, приятель...(нервно) Шел бы ты уже своей дорогой, и не лез в чужие дела.
-	AI_Output(other,self,"DIA_Mil_3320_Stadtwache_Run_01_09");	//Тогда мне придется сообщить о вас паладинам.
-	AI_Output(other,self,"DIA_Mil_3320_Stadtwache_Run_01_10");	//А то вдруг вы на самом деле обычные дезертиры.
-	AI_Output(self,other,"DIA_Mil_3320_Stadtwache_Run_01_11");	//Эх, парень! А ведь я просил тебя не лезть на рожон.
-	AI_Output(self,other,"DIA_Mil_3320_Stadtwache_Run_01_12");	//Извини, но ты сам напросился!
-	B_LogEntry(TOPIC_DeadHead,"Недалеко от прохода в Хоринис я встретил трех ополченцев. Похоже, что они дезертиры. Однако вряд ли они теперь отпустят меня живым, иначе я смогу донести на них паладинам.");
+	AI_Output(other,self, " DIA_Mil_3320_Stadtwache_Run_01_00 " );	// Hey, what are you doing here?
+	AI_Output(self,other, " DIA_Mil_3320_Stadtwache_Run_01_01 " );	// Umm...(stammered) It's just that we have an order to return back to Khorinis.
+	AI_Output(other,self, " DIA_Mil_3320_Stadtwache_Run_01_02 " );	// Whose order?
+	AI_Output(self,other, " DIA_Mil_3320_Stadtwache_Run_01_03 " );	// Well... (thinking) It was given to us by... Lord Andre. Yes, he is the one.
+	AI_Output(other,self, " DIA_Mil_3320_Stadtwache_Run_01_04 " );	// But Lord Andre is in Khorinis now. Are you taking me for a fool?
+	AI_Output(self,other, " DIA_Mil_3320_Stadtwache_Run_01_05 " );	// No, what are you... I... just mixed up... of course, it was not Lord Andre!
+	AI_Output(other,self, " DIA_Mil_3320_Stadtwache_Run_01_06 " );	// You know, usually such orders must be accompanied by written documents. Do you have them?
+	AI_Output(self,other, " DIA_Mil_3320_Stadtwache_Run_01_08 " );	// Listen, buddy... (nervously) You should go your own way, and don't get into other people's business.
+	AI_Output(other,self, " DIA_Mil_3320_Stadtwache_Run_01_09 " );	// Then I'll have to report you to the paladins.
+	AI_Output(other,self, " DIA_Mil_3320_Stadtwache_Run_01_10 " );	// And then all of a sudden you are actually ordinary deserters.
+	AI_Output(self,other, " DIA_Mil_3320_Stadtwache_Run_01_11 " );	// Oh boy! But I asked you not to climb on the rampage.
+	AI_Output(self,other, " DIA_Mil_3320_Stadtwache_Run_01_12 " );	// Sorry, but you asked for it!
+	B_LogEntry(TOPIC_DeadHead, " I met three militiamen near the entrance to Khorinis. It looks like they are deserters. However, they are unlikely to let me go alive now, otherwise I will be able to report them to the paladins. " );
 	Info_ClearChoices(DIA_Mil_3320_Stadtwache_Run);
 	Info_AddChoice(DIA_Mil_3320_Stadtwache_Run,Dialog_Ende,DIA_Mil_3320_Stadtwache_Run_End);
 };
 
-func void DIA_Mil_3320_Stadtwache_Run_End()
+func void DIA_Mil_3320_City Guard_Run_End()
 {
 	AI_StopProcessInfos(self);
-	self.aivar[AIV_EnemyOverride] = FALSE;
-	Mil_3321_Stadtwache.aivar[AIV_EnemyOverride] = FALSE;
-	Mil_3322_Stadtwache.aivar[AIV_EnemyOverride] = FALSE;
+	self.aivar[AIV_EnemyOverride] = FALSE ;
+	Mil_3321_Stadtwache.aivar[AIV_EnemyOverride] = FALSE ;
+	Mil_3322_Stadtwache.aivar[AIV_EnemyOverride] = FALSE ;
 	B_Attack(self,other,AR_KILL,0);
 };
