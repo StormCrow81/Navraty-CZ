0f2a9d8bf06fc18009517d5f75bb98dd5cbc5559
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-02 20:51:00 +0200

    Update DIA_xbs_1_exit.d

--- RU-Main/Story/Dialoge/DIA_xbs_1_exit.d
+++ RU-Main/Story/Dialoge/DIA_xbs_1_exit.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_XBS_1_EXIT(C_Info)
 {
 	nr = 999;
@@ -26,7 +27,7 @@ instance DIA_XBS_1_ABOUT(C_Info)
 	condition = dia_xbs_1_about_condition;
 	information = dia_xbs_1_about_info;
 	permanent = TRUE;
-	description = "Что вы тут вообще делаете?";
+	description = " What are you even doing here? " ;
 };
 
 
@@ -37,8 +38,8 @@ func int dia_xbs_1_about_condition()
 
 func void dia_xbs_1_about_info()
 {
-	AI_Output(other,self,"DIA_XBS_1_ABOUT_15_00");	//Чем вы тут занимаетесь?
-	AI_Output(self,other,"DIA_XBS_1_ABOUT_01_01");	//Живем, охотимся, обсуждаем прошлое и слушаем голоса будущего.
+	AI_Output(other,self, " DIA_XBS_1_ABOUT_15_00 " );	// What are you doing here?
+	AI_Output(self,other, " DIA_XBS_1_ABOUT_01_01 " );	// We live, hunt, discuss the past and listen to the voices of the future.
 	b_heroknowforestbase();
 };
 
@@ -49,7 +50,7 @@ instance DIA_XBS_1_PEOPLE(C_Info)
 	condition = dia_xbs_1_people_condition;
 	information = dia_xbs_1_people_info;
 	permanent = TRUE;
-	description = "Кто здесь старший?";
+	description = " Who's in charge here? " ;
 };
 
 
@@ -60,11 +61,11 @@ func int dia_xbs_1_people_condition()
 
 func void dia_xbs_1_people_info()
 {
-	AI_Output(other,self,"DIA_XBS_1_PEOPLE_15_00");	//Кто здесь старший?
-	AI_Output(self,other,"DIA_XBS_1_PEOPLE_01_01");	//Мы собрались, чтобы учиться у Нетбека.
-	AI_Output(self,other,"DIA_XBS_1_PEOPLE_01_02");	//Но он не занимается жизнью лагеря.
-	AI_Output(self,other,"DIA_XBS_1_PEOPLE_01_03");	//Старший по хозяйству сейчас Гор На Бар.
-	AI_Output(self,other,"DIA_XBS_1_PEOPLE_01_04");	//В остальном мы все равны - никаких чинов и привилегий.
+	AI_Output(other,self, " DIA_XBS_1_PEOPLE_15_00 " );	// Who's in charge here?
+	AI_Output(self,other, " DIA_XBS_1_PEOPLE_01_01 " );	// We're here to learn from Netback.
+	AI_Output(self,other, " DIA_XBS_1_PEOPLE_01_02 " );	// But he doesn't take care of camp life.
+	AI_Output(self,other, " DIA_XBS_1_PEOPLE_01_03 " );	// The house manager is now Gor Na Bar.
+	AI_Output(self,other, " DIA_XBS_1_PEOPLE_01_04 " );	// Otherwise, we are all equal - no ranks and privileges.
 	b_heroknowforestbase();
 };
 
@@ -75,7 +76,7 @@ instance DIA_XBS_1_LOCATION(C_Info)
 	condition = dia_xbs_1_location_condition;
 	information = dia_xbs_1_location_info;
 	permanent = TRUE;
-	description = "Что интересного есть в лагере?";
+	description = " What's in the camp? " ;
 };
 
 
@@ -86,10 +87,10 @@ func int dia_xbs_1_location_condition()
 
 func void dia_xbs_1_location_info()
 {
-	AI_Output(other,self,"DIA_XBS_1_LOCATION_15_00");	//Что интересного есть в лагере?
-	AI_Output(self,other,"DIA_XBS_1_LOCATION_01_01");	//Ничего! Это же не город и даже не замок.
-	AI_Output(self,other,"DIA_XBS_1_LOCATION_01_02");	//Несколько палаток и несколько послушников.
-	AI_Output(self,other,"DIA_XBS_1_LOCATION_01_03");	//Впрочем, внизу в пещере обосновался отряд паладинов Инноса. Сходи к ним - у них, наверняка, жизнь поинтересней.
+	AI_Output(other,self, " DIA_XBS_1_LOCATION_15_00 " );	// What is interesting in the camp?
+	AI_Output(self,other, " DIA_XBS_1_LOCATION_01_01 " );	// Nothing! It's not a city or even a castle.
+	AI_Output(self,other, " DIA_XBS_1_LOCATION_01_02 " );	// Several tents and several novices.
+	AI_Output(self,other, " DIA_XBS_1_LOCATION_01_03 " );	// However, a detachment of paladins of Innos settled down in the cave. Go to them - they probably have a more interesting life.
 	b_heroknowforestbase();
 };
 
@@ -100,7 +101,7 @@ instance DIA_XBS_1_STANDARD(C_Info)
 	condition = dia_xbs_1_standard_condition;
 	information = dia_xbs_1_standard_info;
 	permanent = TRUE;
-	description = "Что нового?";
+	description = " What's new? " ;
 };
 
 
@@ -111,30 +112,30 @@ func int dia_xbs_1_standard_condition()
 
 func void dia_xbs_1_standard_info()
 {
-	AI_Output(other,self,"DIA_XBS_1_STANDARD_15_00");	//Что нового?
-	if(Kapitel == 1)
+	AI_Output(other,self, " DIA_XBS_1_STANDARD_15_00 " );	// What's new?
+	if (chapter ==  1 )
 	{
-		AI_Output(self,other,"DIA_XBS_1_STANDARD_01_01");	//Уже несколько недель не видно никого из Болотного лагеря - кроме тех, что собрались здесь, конечно.
+		AI_Output(self,other, " DIA_XBS_1_STANDARD_01_01 " );	// No one has been seen from the Swamp Camp for several weeks - except for those who have gathered here, of course.
 	};
-	if(Kapitel == 2)
+	if (chapter ==  2 )
 	{
-		AI_Output(self,other,"DIA_XBS_1_STANDARD_01_02");	//Вопли из-за забора ветер иногда доносит даже сюда. Не хотел бы я сейчас оказаться в замке!
+		AI_Output(self,other, " DIA_XBS_1_STANDARD_01_02 " );	// The wind sometimes carries screams from behind the fence even here. I wouldn't want to be in a castle right now!
 	};
-	if(Kapitel == 3)
+	if (chapter ==  3 )
 	{
-		AI_Output(self,other,"DIA_XBS_1_STANDARD_01_03");	//Какая-то грусть напала на всех парней. Кажется, что мы навсегда потеряли что-то или кого-то.
+		AI_Output(self,other, " DIA_XBS_1_STANDARD_01_03 " );	// Some kind of sadness attacked all the guys. It seems that we have lost something or someone forever.
 	};
-	if(Kapitel == 4)
+	if (chapter ==  4 )
 	{
-		AI_Output(self,other,"DIA_XBS_1_STANDARD_01_04");	//Недавно сюда прибыл новый отряд орков. Думаю, защитникам замка в скором времени придется очень туго!
+		AI_Output(self,other, " DIA_XBS_1_STANDARD_01_04 " );	// A new group of orcs has recently arrived here. I think the defenders of the castle will soon have a very tight time!
 	};
-	if(Kapitel == 5)
+	if (chapter ==  5 )
 	{
-		AI_Output(self,other,"DIA_XBS_1_STANDARD_01_05");	//Поговоривают, что именно ты истребил всех драконов. Как тебе это удалось, парень?!
+		AI_Output(self,other, " DIA_XBS_1_STANDARD_01_05 " );	// Rumor has it that it was you who exterminated all the dragons. How did you do it, boy?!
 	};
-	if(Kapitel >= 6)
+	if (Chapter >=  6 )
 	{
-		AI_Output(self,other,"DIA_XBS_1_STANDARD_01_06");	//Говорят, что орки все-таки взяли замок штурмом! Боюсь, скоро они и до нас доберуться.
+		AI_Output(self,other, " DIA_XBS_1_STANDARD_01_06 " );	// They say the orcs stormed the castle after all! I'm afraid they'll get to us soon.
 	};
 	b_heroknowforestbase();
 };
