ea5a1c861605356f852433ca9393b7b1969ca3b1
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-02 20:28:46 +0200

    Update DIA_Mil_333_Stadtwache.d

--- RU-Main/Story/Dialoge/DIA_Mil_333_Stadtwache.d
+++ RU-Main/Story/Dialoge/DIA_Mil_333_Stadtwache.d
@@ -1,7 +1,8 @@
 
-instance DIA_Mil_333_Stadtwache_EXIT(C_Info)
+
+instance DIA_Mil_333_Stadtwache_EXIT (C_Info)
 {
-	npc = Mil_333_Stadtwache;
+	npc = Mil_333_City Guard;
 	nr = 999;
 	condition = DIA_Mil_333_Stadtwache_EXIT_Condition;
 	information = DIA_Mil_333_Stadtwache_EXIT_Info;
@@ -25,9 +26,9 @@ const string Mil_333_Checkpoint = "NW_CITY_MERCHANT_PATH_29";
 
 var int MIL_333_Personal_AbsolutionLevel;
 
-instance DIA_Mil_333_Stadtwache_FirstWarn(C_Info)
+instance DIA_Mil_333_Stadtwache_FirstWarn (C_Info)
 {
-	npc = Mil_333_Stadtwache;
+	npc = Mil_333_City Guard;
 	nr = 1;
 	condition = DIA_Mil_333_Stadtwache_FirstWarn_Condition;
 	information = DIA_Mil_333_Stadtwache_FirstWarn_Info;
@@ -40,12 +41,12 @@ func int DIA_Mil_333_Stadtwache_FirstWarn_Condition()
 {
 	if((B_GetGreatestPetzCrime(self) >= CRIME_ATTACK) && (B_GetCurrentAbsolutionLevel(self) == MIL_333_Personal_AbsolutionLevel))
 	{
-		self.aivar[AIV_PASSGATE] = FALSE;
+		self.aivar[ AIV_PASSGATE ] = FALSE ;
 	}
-	else if(Mil_333_schonmalreingelassen == TRUE)
+	else  if (Mil_333_schonmalreinlett ==  TRUE )
 	{
 		CITYGATEACCESSGRANTED = TRUE;
-		self.aivar[AIV_PASSGATE] = TRUE;
+		self.aivar[ AIV_PASSGATE ] = TRUE ;
 	};
 	if((self.aivar[AIV_Guardpassage_Status] == GP_NONE) && (self.aivar[AIV_PASSGATE] == FALSE) && (Hlp_StrCmp(Npc_GetNearestWP(self),self.wp) == TRUE) && (Npc_RefuseTalk(self) == FALSE))
 	{
@@ -60,54 +61,54 @@ func void DIA_Mil_333_Stadtwache_FirstWarn_Info()
 
 	if(B_GetGreatestPetzCrime(self) >= CRIME_ATTACK)
 	{
-		AI_Output(other,self,"DIA_Mil_333_Stadtwache_FirstWarn_15_01");	//(вздыхает) Что еще?
-		AI_Output(self,other,"DIA_Mil_333_Stadtwache_FirstWarn_06_02");	//Ты прекрасно знаешь, что!
+		AI_Output(other,self, " DIA_Mil_333_Stadtwache_FirstWarn_15_01 " );	// (sighs) What else?
+		AI_Output(self,other, " DIA_Mil_333_Stadtwache_FirstWarn_06_02 " );	// You know very well that!
 
 		if(B_GetGreatestPetzCrime(self) == CRIME_MURDER)
 		{
-			AI_Output(self,other,"DIA_Mil_333_Stadtwache_FirstWarn_06_03");	//Ты разыскиваешься за убийство в городе!
+			AI_Output(self,other, " DIA_Mil_333_Stadtwache_FirstWarn_06_03 " );	// You're wanted for murder in the city!
 		};
 		if(B_GetGreatestPetzCrime(self) == CRIME_THEFT)
 		{
-			AI_Output(self,other,"DIA_Mil_333_Stadtwache_FirstWarn_06_04");	//Мы не потерпим здесь грязных воров!
+			AI_Output(self,other, " DIA_Mil_333_Stadtwache_FirstWarn_06_04 " );	// We won't tolerate dirty thieves here!
 		};
 		if(B_GetGreatestPetzCrime(self) == CRIME_ATTACK)
 		{
-			AI_Output(self,other,"DIA_Mil_333_Stadtwache_FirstWarn_06_05");	//Нам не нужны смутьяны в городе!
+			AI_Output(self,other, " DIA_Mil_333_Stadtwache_FirstWarn_06_05 " );	// We don't need troublemakers in the city!
 		};
 
-		AI_Output(self,other,"DIA_Mil_333_Stadtwache_FirstWarn_06_06");	//С чего ты взял, что мы должны впустить тебя?
+		AI_Output(self,other, " DIA_Mil_333_Stadtwache_FirstWarn_06_06 " );	// What makes you think we should let you in?
 	}
 	else
 	{
 		itm = Npc_GetEquippedArmor(other);
 
-		if((hero.guild == GIL_NONE) && ((Npc_HasEquippedArmor(other) == FALSE) || Hlp_IsItem(itm,ITAR_Her_M) || Hlp_IsItem(itm,ITAR_prisonel) || Hlp_IsItem(itm,ITAR_Her_N) || Hlp_IsItem(itm,ITAR_Prisoner) || Hlp_IsItem(itm,itar_sekbed)))
+		if ((hero.guild ==  GIL_NONE ) && ((Npc_HasEquippedArmor(other) ==  FALSE ) || Hlp_IsItem(itm,ITAR_Her_M) || Hlp_IsItem(itm,ITAR_prisonel) || Hlp_IsItem(itm,ITAR_Her_N) || Hlp_IsItem (itm,ITAR_Her_N) ,ITAR_Prisoner) || Hlp_IsItem(itm,itar_sekbed)))
 		{
 			AI_Output(other,self,"DIA_Mil_333_Stadtwache_FirstWarn_15_07");	//Да?
-			AI_Output(self,other,"DIA_Mil_333_Stadtwache_FirstWarn_06_08");	//Ты похож на нищего! В этом городе нам не нужны люди, у которых нет денег.
+			AI_Output(self,other, " DIA_Mil_333_Stadtwache_FirstWarn_06_08 " );	// You look like a beggar! In this city, we don't need people who don't have money.
 
-			if(Npc_HasItems(other,ItWr_Passierschein))
+			if (Npc_HasItems(other,ItWr_Passierschein))
 			{
-				AI_Output(other,self,"DIA_Mil_333_Stadtwache_FirstWarn_15_09");	//Но у меня есть пропуск!
-				AI_Output(self,other,"DIA_Mil_333_Stadtwache_FirstWarn_06_10");	//Он годен только для других ворот!
+				AI_Output(other,self, " DIA_Mil_333_Stadtwache_FirstWarn_15_09 " );	// But I have a pass!
+				AI_Output(self,other, " DIA_Mil_333_Stadtwache_FirstWarn_06_10 " );	// It's only good for other gates!
 			};
-			if(Npc_KnowsInfo(other,DIA_Mil_310_Stadtwache_MilizWerden))
+			if (Npc_KnowsInfo(other,DIA_Mil_310_Stadtwache_MilizWerden))
 			{
-				AI_Output(other,self,"DIA_Mil_333_Stadtwache_FirstWarn_15_11");	//Но я хочу вступить в городскую стражу!
-				AI_Output(self,other,"DIA_Mil_333_Stadtwache_FirstWarn_06_12");	//Ха! Не смеши меня! Иди к другим воротам и попробуй рассказать это там.
-				AI_Output(other,self,"DIA_Mil_333_Stadtwache_FirstWarn_15_13");	//Я уже был там, они отправили меня сюда.
-				AI_Output(self,other,"DIA_Mil_333_Stadtwache_FirstWarn_06_14");	//(грязный смех) Ох, парень! Они всегда посылают к нам сливки общества...
+				AI_Output(other,self, " DIA_Mil_333_Stadtwache_FirstWarn_15_11 " );	// But I want to join the city guard!
+				AI_Output(self,other, " DIA_Mil_333_Stadtwache_FirstWarn_06_12 " );	// Ha! Do not make me laugh! Go to another gate and try to tell it there.
+				AI_Output(other,self, " DIA_Mil_333_Stadtwache_FirstWarn_15_13 " );	// I was already there, they sent me here.
+				AI_Output(self,other, " DIA_Mil_333_Stadtwache_FirstWarn_06_14 " );	// (dirty laughter) Oh man! They always send us the cream of society...
 			};
 		}
 		else
 		{
-			AI_Output(other,self,"DIA_Mil_333_Stadtwache_FirstWarn_15_15");	//(Спокойно) Что?
-			AI_Output(self,other,"DIA_Mil_333_Stadtwache_FirstWarn_06_16");	//Только хотел тебя рассмотреть. Мда, кажется, у тебя водятся деньги. Проходи!
+			AI_Output(other,self, " DIA_Mil_333_Stadtwache_FirstWarn_15_15 " );	// (Calmly) What?
+			AI_Output(self,other, " DIA_Mil_333_Stadtwache_FirstWarn_06_16 " );	// Just wanted to see you. Well, it looks like you have money. Come on!
 			CITYGATEACCESSGRANTED = TRUE;
-			self.aivar[AIV_PASSGATE] = TRUE;
-			Stadtwache_310.aivar[AIV_PASSGATE] = TRUE;
-			Mil_333_schonmalreingelassen = TRUE;
+			self.aivar[ AIV_PASSGATE ] = TRUE ;
+			Stadtwache_310.aivar[ AIV_PASSGATE ] = TRUE ;
+			Mil_333_schonmalreinlett = TRUE ;
 			AI_StopProcessInfos(self);
 		};
 	};
@@ -117,9 +118,9 @@ func void DIA_Mil_333_Stadtwache_FirstWarn_Info()
 };
 
 
-instance DIA_Mil_333_Stadtwache_SecondWarn(C_Info)
+instance DIA_Mil_333_Stadtwache_SecondWarn (C_Info)
 {
-	npc = Mil_333_Stadtwache;
+	npc = Mil_333_City Guard;
 	nr = 2;
 	condition = DIA_Mil_333_Stadtwache_SecondWarn_Condition;
 	information = DIA_Mil_333_Stadtwache_SecondWarn_Info;
@@ -138,15 +139,15 @@ func int DIA_Mil_333_Stadtwache_SecondWarn_Condition()
 
 func void DIA_Mil_333_Stadtwache_SecondWarn_Info()
 {
-	AI_Output(self,other,"DIA_Mil_333_Stadtwache_SecondWarn_06_00");	//У тебя со слухом все в порядке? Еще один шаг, и ты познакомишься с моим мечом!
+	AI_Output(self,other, " DIA_Mil_333_Stadtwache_SecondWarn_06_00 " );	// Are your hearing okay? One more step and you will meet my sword!
 	other.aivar[AIV_LastDistToWP] = Npc_GetDistToWP(other,Mil_333_Checkpoint);
 	self.aivar[AIV_Guardpassage_Status] = GP_SecondWarnGiven;
 	AI_StopProcessInfos(self);
 };
 
-instance DIA_Mil_333_Stadtwache_Attack(C_Info)
+instance DIA_Mil_333_Stadtwache_Attack (C_Info)
 {
-	npc = Mil_333_Stadtwache;
+	npc = Mil_333_City Guard;
 	nr = 3;
 	condition = DIA_Mil_333_Stadtwache_Attack_Condition;
 	information = DIA_Mil_333_Stadtwache_Attack_Info;
@@ -165,15 +166,15 @@ func int DIA_Mil_333_Stadtwache_Attack_Condition()
 func void DIA_Mil_333_Stadtwache_Attack_Info()
 {
 	other.aivar[AIV_LastDistToWP] = 0;
-	self.aivar[AIV_Guardpassage_Status] = GP_NONE;
-	AI_Output(self,other,"DIA_Mil_333_Stadtwache_Attack_06_00");	//Ты сам напросился...
+	self.aivar[AIV_Guardpassage_Status] = GP_NONE ;
+	AI_Output(self,other, " DIA_Mil_333_Stadtwache_Attack_06_00 " );	// You asked for it...
 	AI_StopProcessInfos(self);
 	B_Attack(self,other,AR_GuardStopsIntruder,0);
 };
 
-instance DIA_Mil_333_Stadtwache_PERM(C_Info)
+instance DIA_Mil_333_Stadtwache_PERM (C_Info)
 {
-	npc = Mil_333_Stadtwache;
+	npc = Mil_333_City Guard;
 	nr = 5;
 	condition = DIA_Mil_333_Stadtwache_PERM_Condition;
 	information = DIA_Mil_333_Stadtwache_PERM_Info;
@@ -183,7 +184,7 @@ instance DIA_Mil_333_Stadtwache_PERM(C_Info)
 
 func int DIA_Mil_333_Stadtwache_PERM_Condition()
 {
-	if((self.aivar[AIV_PASSGATE] == TRUE) && (MIS_KILLIGNAZ != LOG_Running) && Npc_IsInState(self,ZS_Talk))
+	if (( self . aivar [ AIV_PASSGATE ] ==  TRUE ) && ( MIS_KILLIGNAZ  != LOG_Running ) && Npc_IsInState ( self , ZS_Talk ) ) ;
 	{
 		return TRUE;
 	};
@@ -191,23 +192,23 @@ func int DIA_Mil_333_Stadtwache_PERM_Condition()
 
 func void DIA_Mil_333_Stadtwache_PERM_Info()
 {
-	AI_Output(self,other,"DIA_Mil_333_Stadtwache_PERM_06_00");	//Проходи!
+	AI_Output(self,other, " DIA_Mil_333_Stadtwache_PERM_06_00 " );	// Come on!
 	AI_StopProcessInfos(self);
 };
 
-instance DIA_MIL_333_STADTWACHE_KILLIGNAZ(C_Info)
+instance DIA_MIL_333_STADTWACHE_KILLIGNAZ (C_Info)
 {
-	npc = Mil_333_Stadtwache;
+	npc = Mil_333_City Guard;
 	nr = 5;
 	condition = dia_mil_333_stadtwache_killignaz_condition;
 	information = dia_mil_333_stadtwache_killignaz_info;
 	permanent = FALSE;
-	description = "Ты тут все замечаешь?";
+	description = " Do you notice everything here? " ;
 };
 
 func int dia_mil_333_stadtwache_killignaz_condition()
 {
-	if((MIS_KILLIGNAZ == LOG_Running) && (READORTEGO == FALSE) && (HELPKILLIGNAZ == TRUE))
+	if (( MIS_KILLIGNAZ  == LOG_Running) && ( READORTER  ==  FALSE ) && ( HELPKILLIGNAZ  ==  TRUE ))
 	{
 		return TRUE;
 	};
@@ -215,15 +216,15 @@ func int dia_mil_333_stadtwache_killignaz_condition()
 
 func void dia_mil_333_stadtwache_killignaz_info()
 {
-	AI_Output(other,self,"DIA_Mil_333_Stadtwache_KillIgnaz_01_00");	//Ты тут все замечаешь?
-	AI_Output(self,other,"DIA_Mil_333_Stadtwache_KillIgnaz_01_01");	//Это ты у меня спаршиваешь? Парень, я сюда для того и поставлен, чтобы за всем следить.
-	AI_Output(self,other,"DIA_Mil_333_Stadtwache_KillIgnaz_01_02");	//Хотя, по правде говоря, сейчас работы стало совсем мало. Следить особо не за чем.
+	AI_Output(other,self, " DIA_Mil_333_Stadtwache_KillIgnaz_01_00 " );	// Do you notice everything here?
+	AI_Output(self,other, " DIA_Mil_333_Stadtwache_KillIgnaz_01_01 " );	// Are you asking me? Boy, I'm here to keep track of everything.
+	AI_Output(self,other, " DIA_Mil_333_Stadtwache_KillIgnaz_01_02 " );	// Although, in truth, there is very little work now. There is nothing special to follow.
 	AI_Output(other,self,"DIA_Mil_333_Stadtwache_KillIgnaz_01_03");	//Почему?
-	AI_Output(self,other,"DIA_Mil_333_Stadtwache_KillIgnaz_01_04");	//Потому, что в последнее время никто особо и не спешит покидать пределы города.
-	AI_Output(self,other,"DIA_Mil_333_Stadtwache_KillIgnaz_01_05");	//Нынче снаружи стало слишком опасно! Хотя, бывает, и попадаются смельчаки...(ухмыляясь)
-	AI_Output(other,self,"DIA_Mil_333_Stadtwache_KillIgnaz_01_06");	//Кого ты имеешь в виду?
-	AI_Output(self,other,"DIA_Mil_333_Stadtwache_KillIgnaz_01_07");	//Да тут вот недавно проходил один какой-то парень. Вышел из города и прямиком направился вон туда, в сторону маяка.
-	AI_Output(self,other,"DIA_Mil_333_Stadtwache_KillIgnaz_01_08");	//Кстати, поговаривают, что там нынче обосновались бандиты. Вот я ему не позавидую, если он наткнется на них!
+	AI_Output(self,other, " DIA_Mil_333_Stadtwache_KillIgnaz_01_04 " );	// Because, lately, no one is particularly in a hurry to leave the city.
+	AI_Output(self,other, " DIA_Mil_333_Stadtwache_KillIgnaz_01_05 " );	// It's too dangerous outside today! Although, sometimes, daredevils come across ... (grinning)
+	AI_Output(other,self, " DIA_Mil_333_Stadtwache_KillIgnaz_01_06 " );	// Who do you mean?
+	AI_Output(self,other, " DIA_Mil_333_Stadtwache_KillIgnaz_01_07 " );	// Yes, a guy passed here recently. I left the city and went straight over there, towards the lighthouse.
+	AI_Output(self,other, " DIA_Mil_333_Stadtwache_KillIgnaz_01_08 " );	// By the way, they say that bandits have settled there today. I won't envy him if he stumbles upon them!
 	AI_StopProcessInfos(self);
 	Wld_InsertNpc(vlk_6150_ortego,"NW_CITY_SMFOREST_BANDIT_02");
 };
