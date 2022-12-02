121849bf275ec03f318868d82e119784ea3f1413
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-02 21:16:31 +0200

    Update DIA_NOV_11114_Holger.d

--- RU-Main/Story/Dialoge/DIA_NOV_11114_Holger.d
+++ RU-Main/Story/Dialoge/DIA_NOV_11114_Holger.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_HOLGER_EXIT(C_Info)
 {
 	npc = nov_11114_holger;
@@ -29,7 +30,7 @@ instance DIA_HOLGER_STORY(C_Info)
 	information = dia_holger_story_info;
 	important = FALSE;
 	permanent = TRUE;
-	description = "Расскажи мне всю историю.";
+	description = " Tell me the whole story. " ;
 };
 
 func int dia_holger_story_condition()
@@ -43,26 +44,26 @@ func int dia_holger_story_condition()
 func void dia_holger_story_info()
 {
 	B_GivePlayerXP(50);
-	AI_Output(other,self,"DIA_Holger_01_01");	//Расскажи мне всю историю, как и обещал.
-	AI_Output(self,other,"DIA_Holger_01_02");	//Да что тут теперь скрывать. В монастыре я около года.
-	AI_Output(self,other,"DIA_Holger_01_03");	//Тогда еще над Долиной Рудников стоял Барьер, и Хоринис процветал. Но в один 'прекрасный' день я подрался с кем-то в трактире Корагона.
-	AI_Output(self,other,"DIA_Holger_01_04");	//Я был в стельку пьян, и не помню, с чего все началось. Но драка закончилась плохо для нас двоих.
-	AI_Output(self,other,"DIA_Holger_01_05");	//Рона - так звали того парня - я избил чуть ли не до полусмерти, и меня уже хотели кинуть за Барьер.
-	AI_Output(self,other,"DIA_Holger_01_06");	//Но мой отец собрал все деньги, которые у нас тогда были, и отдал меня в монастырь.
-	AI_Output(self,other,"DIA_Holger_01_07");	//Он был вынужден продать почти все. И после этого я не могу смотреть ему в глаза.
-	AI_Output(self,other,"DIA_Holger_01_08");	//И именно поэтому я не смог отправиться к нему сам.
-	AI_Output(other,self,"DIA_Holger_01_09");	//Да уж, история не из веселых. Но я хочу тебе сказать, что твои родители очень по тебе скучают и хотят тебя увидеть.
-	AI_Output(self,other,"DIA_Holger_01_10");	//Правда? Они не гневаются на меня? Ты не лжешь?
-	AI_Output(other,self,"DIA_Holger_01_11");	//Нет конечно.
-	AI_Output(self,other,"DIA_Holger_01_12");	//Тогда я скоро буду у них!
+	AI_Output(other, self, " DIA_Holger_01_01 " );	// Tell me the whole story, as you promised.
+	AI_Output(self,other, " DIA_Holger_01_02 " );	// What's there to hide now. I have been in the monastery for about a year.
+	AI_Output(self,other, " DIA_Holger_01_03 " );	// Then the Barrier stood over the Valley of the Mines, and Khorinis prospered. But one 'beautiful' day, I got into a fight with someone in the Koragon Inn.
+	AI_Output(self,other, " DIA_Holger_01_04 " );	// I was drunk as hell, and I don't remember how it all started. But the fight ended badly for the two of us.
+	AI_Output(self,other, " DIA_Holger_01_05 " );	// Ron - that was the name of that guy - I beat almost half to death, and they already wanted to throw me over the Barrier.
+	AI_Output(self,other, " DIA_Holger_01_06 " );	// But my father collected all the money we had then and sent me to a monastery.
+	AI_Output(self,other, " DIA_Holger_01_07 " );	// He was forced to sell almost everything. And after that, I can't look him in the eye.
+	AI_Output(self,other, " DIA_Holger_01_08 " );	// And that's why I couldn't go to him myself.
+	AI_Output(other,self, " DIA_Holger_01_09 " );	// Yeah, not a fun story. But I want to tell you that your parents miss you very much and want to see you.
+	AI_Output(self,other, " DIA_Holger_01_10 " );	// True? Are they not angry with me? Are you lying?
+	AI_Output(other,self, " DIA_Holger_01_11 " );	// Of course not.
+	AI_Output(self,other, " DIA_Holger_01_12 " );	// Then I'll be with them soon!
 	MIS_HOLGER_QUEST = 4;
 	Log_SetTopicStatus(TOPIC_BADSON,LOG_SUCCESS);
-	B_LogEntry(TOPIC_BADSON,"Теперь, наконец, все встало на свои места. Хольгер - человек, которому просто в жизни не повезло. И монастырь для него был единственным спасением. Надеюсь, что теперь у него с Альвином будут нормальные отношения.");
+	; _ _ _ _ _ _
 	INNOSPRAYCOUNT += 1;
 	Npc_ExchangeRoutine(nov_11114_holger,"Smoke");
 };
 
-instance DIA_HOLGER_HELLO(C_Info)
+instance DIA_HOLGER_HELLO (C_Info)
 {
 	npc = nov_11114_holger;
 	nr = 1;
@@ -74,7 +75,7 @@ instance DIA_HOLGER_HELLO(C_Info)
 
 func int dia_holger_hello_condition()
 {
-	if(Wld_IsTime(20,0,8,0) && (Kapitel <= 2) && (MIS_HOLGER_QUEST < 1))
+	if ( Wld_IsTime ( 20 , 0 , 8 , 0 ) && ( Chapter <=  2 ) && ( MY_HOLGER_QUEST  <  1 )) ;
 	{
 		return TRUE;
 	};
@@ -82,58 +83,58 @@ func int dia_holger_hello_condition()
 
 func void dia_holger_hello_info()
 {
-	AI_Output(self,other,"DIA_Holger_Hello_01_01");	//Эй! Подожди минутку.
-	AI_Output(self,other,"DIA_Holger_Hello_01_02");	//У меня к тебе есть одно дело. Ты можешь принести мне кое-что?
-	AI_Output(other,self,"DIA_Holger_Hello_01_03");	//Воды? Еды? Болотника?
-	AI_Output(self,other,"DIA_Holger_Hello_01_04");	//Нет, мне это не нужно. Здесь в монастыре этого добра хватает.
-	AI_Output(self,other,"DIA_Holger_Hello_01_05");	//А если ты упомянешь болотник при магах или еще как-нибудь провинишься, тебе придется долго мести пыль в подвалах.
-	AI_Output(self,other,"DIA_Holger_Hello_01_06");	//Вон, бедолага Рэм случайно уронил черный гриб в чан с вином. Так Горакс его уже третью неделю заставляет работать метлой.
-	AI_Output(self,other,"DIA_Holger_Hello_01_07");	//И если честно, я даже не знаю, когда он остынет.
-	AI_Output(other,self,"DIA_Holger_Hello_01_08");	//Жаль, конечно, этого парня. Но давай ближе к делу!
-	AI_Output(self,other,"DIA_Holger_Hello_01_09");	//Ах да. Заговорился я что-то... Так вот, не мог бы ты забрать одну вещицу у Альвина? Если быть точным - один амулет.
-	AI_Output(other,self,"DIA_Holger_Hello_01_10");	//И как мне его достать - украсть, купить?
-	AI_Output(self,other,"DIA_Holger_Hello_01_11");	//Нет, что ты! Просто назови о... э-э... ему мое имя и передай вот это письмо. Он поймет, о чем идет речь.
-	AI_Output(other,self,"DIA_Holger_Hello_01_12");	//А почему ты сам не сходишь в Хоринис, чтобы забрать у Альвина этот амулет?
-	AI_Output(self,other,"DIA_Holger_Hello_01_13");	//(замявшись) Понимаешь, это долгая история...
-	AI_Output(self,other,"DIA_Holger_Hello_01_14");	//Давай так: ты принесешь мне амулет, тогда я тебе расскажу всю историю и еще кое-чем помогу. Согласен?
+	AI_Output(self,other, " DIA_Holger_Hello_01_01 " );	// Hey! Wait a minute.
+	AI_Output(self,other, " DIA_Holger_Hello_01_02 " );	// I have one thing for you. Can you bring me something?
+	AI_Output(other, self, " DIA_Holger_Hello_01_03 " );	// Water? Food? Bolotnik?
+	AI_Output(self,other, " DIA_Holger_Hello_01_04 " );	// No, I don't need it. There is enough of this goodness here in the monastery.
+	AI_Output(self,other, " DIA_Holger_Hello_01_05 " );	// And if you mention a swamp with magicians or do something else, you will have to sweep the dust in the cellars for a long time.
+	AI_Output(self,other, " DIA_Holger_Hello_01_06 " );	// There, poor old Ram accidentally dropped a black mushroom into a vat of wine. So Gorax has been making him work with a broom for the third week.
+	AI_Output(self,other, " DIA_Holger_Hello_01_07 " );	// And to be honest, I don't even know when it will cool down.
+	AI_Output(other,self, " DIA_Holger_Hello_01_08 " );	// It's a pity, of course, this guy. But let's get down to business!
+	AI_Output(self,other, " DIA_Holger_Hello_01_09 " );	// Oh yeah. I started talking about something ... So, could you take one little thing from Alvin? To be precise - one amulet.
+	AI_Output(other, self, " DIA_Holger_Hello_01_10 " );	// And how can I get it - steal it, buy it?
+	AI_Output(self,other, " DIA_Holger_Hello_01_11 " );	// No, what are you! Just tell o... uh... my name to him and give him this letter. He will understand what is at stake.
+	AI_Output(other,self, " DIA_Holger_Hello_01_12 " );	// Why don't you go to Khorinis yourself to take this amulet from Alvin?
+	AI_Output(self,other, " DIA_Holger_Hello_01_13 " );	// (hesitantly) You see, it's a long story...
+	AI_Output(self,other, " DIA_Holger_Hello_01_14 " );	// Let's do this: you bring me an amulet, then I'll tell you the whole story and help you with something else. I agree?
 	Info_ClearChoices(dia_holger_hello);
-	Info_AddChoice(dia_holger_hello,"Хорошо, интересно будет послушать.",dia_holger_hello_yes);
-	Info_AddChoice(dia_holger_hello,"Нет, не хочу вмешиваться в ваши дела.",dia_holger_hello_no);
+	Info_AddChoice(dia_holger_hello, " Okay, that'll be interesting to hear. " ,dia_holger_hello_yes);
+	Info_AddChoice(dia_holger_hello, " No, I don't want to interfere in your business. " ,dia_holger_hello_no);
 };
 
 func void dia_holger_hello_yes()
 {
-	AI_Output(other,self,"DIA_Holger_Hello_Yes_01_01");	//Хорошо, интересно будет послушать.
-	AI_Output(self,other,"DIA_Holger_Hello_Yes_01_02");	//Тогда жду тебя с новостями.
+	AI_Output(other,self, " DIA_Holger_Hello_Yes_01_01 " );	// Okay, interesting to hear.
+	AI_Output(self,other, " DIA_Holger_Hello_Yes_01_02 " );	// Then I'll wait for you with the news.
 	B_GiveInvItems(self,hero,itwr_holger_letter,1);
 	Log_CreateTopic(TOPIC_BADSON,LOG_MISSION);
 	Log_SetTopicStatus(TOPIC_BADSON,LOG_Running);
 	MIS_HOLGER_QUEST = 1;
-	B_LogEntry(TOPIC_BADSON,"Я поговорил с послушником по имени Хольгер. Он попросил меня принести ему из города какой-то амулет. И эта драгоценность почему-то находится у пастуха Альвина. Какая-то мутная история... Стоит в ней разобраться.");
+	B_LogEntry( TOPIC_BADSON , " I talked to a novice named Holger. He asked me to bring him some kind of amulet from the city. And for some reason this jewel is in the possession of the shepherd Alvin. Some kind of muddy story... It's worth figuring it out. " );
 	Info_ClearChoices(dia_holger_hello);
 };
 
 func void dia_holger_hello_no()
 {
-	AI_Output(other,self,"DIA_Holger_Hello_No_01_01");	//Нет, я не хочу вмешиваться в ваши дела.
-	AI_Output(self,other,"DIA_Holger_Hello_No_01_02");	//Что же. Нет так нет.
+	AI_Output(other,self, " DIA_Holger_Hello_No_01_01 " );	// No, I don't want to interfere in your business.
+	AI_Output(self,other, " DIA_Holger_Hello_No_01_02 " );	// Well. No so no.
 	Info_ClearChoices(dia_holger_hello);
 };
 
 
-instance DIA_Holger_GiveAmulet(C_Info)
+instance DIA_Holger_GiveAmulet (C_Info)
 {
 	npc = nov_11114_holger;
 	nr = 1;
 	condition = DIA_Holger_GiveAmulet_condition;
 	information = DIA_Holger_GiveAmulet_info;
 	permanent = FALSE;
-	description = "Я принес тебе твой амулет.";
+	description = " I brought you your amulet. " ;
 };
 
 func int DIA_Holger_GiveAmulet_condition()
 {
-	if((MIS_HOLGER_QUEST == 2) && (Npc_HasItems(other,itam_holger_amulet) >= 1))
+	if (( MY_HOLDER_QUEST  ==  2 ) && (Npc_HasItems(other,item_holder_amulet) >=  1 )) ;
 	{
 		return TRUE;
 	};
@@ -142,44 +143,44 @@ func int DIA_Holger_GiveAmulet_condition()
 func void DIA_Holger_GiveAmulet_info()
 {
 	B_GivePlayerXP(100);
-	AI_Output(other,self,"DIA_Holger_GiveAmulet_01_01");	//Я принес тебе твой амулет.
-	AI_Output(self,other,"DIA_Holger_GiveAmulet_01_02");	//Ты не шутишь, приятель?
-	AI_Output(other,self,"DIA_Holger_GiveAmulet_01_03");	//Вот, держи его.
+	AI_Output(other,self, " DIA_Holger_GiveAmulet_01_01 " );	// I brought you your amulet.
+	AI_Output(self,other, " DIA_Holger_GiveAmulet_01_02 " );	// You're not kidding, buddy?
+	AI_Output(other,self, " DIA_Holger_GiveAmulet_01_03 " );	// Here, hold it.
 	B_GiveInvItems(other,self,itam_holger_amulet,1);
 	Npc_RemoveInvItems(self,itam_holger_amulet,1);
-	AI_Output(self,other,"DIA_Holger_GiveAmulet_01_04");	//Да, это действительно он! Большое тебе спасибо.
-	AI_Output(self,other,"DIA_Holger_GiveAmulet_01_05");	//Вот, теперь этот свиток огненной бури твой.
+	AI_Output(self,other, " DIA_Holger_GiveAmulet_01_04 " );	// Yes, it's really him! Thank you very much.
+	AI_Output(self,other, " DIA_Holger_GiveAmulet_01_05 " );	// Here, now this firestorm scroll is yours.
 	B_GiveInvItems(self,other,ItSc_Firestorm,1);
-	B_LogEntry(TOPIC_BADSON,"Я поговорил с послушником по имени Хольгер. Он попросил меня принести ему из города какой-то амулет. И эта драгоценность почему-то находится у пастуха Альвина. Какая-то мутная история... Стоит в ней разобраться.");
+	B_LogEntry( TOPIC_BADSON , " I talked to a novice named Holger. He asked me to bring him some kind of amulet from the city. And for some reason this jewel is in the possession of the shepherd Alvin. Some kind of muddy story... It's worth figuring it out. " );
 	MIS_HOLGER_QUEST = 3;
 };
 
-instance DIA_Holger_Wurst(C_Info)
+instance DIA_Holger_Wurst (C_Info)
 {
 	npc = nov_11114_holger;
 	nr = 2;
 	condition = DIA_Holger_Wurst_Condition;
-	information = DIA_Holger_Wurst_Info;
+	information = DIA_Holger_Sausage_Info;
 	permanent = FALSE;
-	description = "Вот, у меня есть баранья колбаса для тебя.";
+	description = " Here, I have lamb sausage for you. " ;
 };
 
 func int DIA_Holger_Wurst_Condition()
 {
-	if((Kapitel == 1) && (MIS_HOLGER_QUEST < 4) && (MIS_GoraxEssen == LOG_Running) && (Npc_HasItems(self,ItFo_Schafswurst) == 0) && (Npc_HasItems(other,ItFo_Schafswurst) >= 1))
+	if ((Chapter ==  1 ) && ( MY_HOLGER_QUEST  <  4 ) && (MY_GoraxEssen == LOG_Running) && (Npc_HasItems(self,ItFo_Schafswurst) ==  0 ) && (Npc_HasItems(other,ItFo_Schafswurst) >=  1 )) ;
 	{
 		return TRUE;
 	};
 };
 
-func void DIA_Holger_Wurst_Info()
+func void DIA_Holger_Sausage_Info()
 {
 	var string NovizeText;
-	var string NovizeLeft;
-	AI_Output(other,self,"DIA_Agon_Wurst_15_00");	//Вот, у меня есть баранья колбаса для тебя.
-	AI_Output(self,other,"DIA_Agon_Wurst_07_03");	//Ладно, давай ее сюда.
+	var string NoviceLeft;
+	AI_Output(other,self, " DIA_Agon_Wurst_15_00 " );	// Here, I have lamb sausage for you.
+	AI_Output(self,other, " DIA_Agon_Wurst_07_03 " );	// Okay, give it here.
 	B_GiveInvItems(other,self,ItFo_Schafswurst,1);
-	Wurst_Gegeben = Wurst_Gegeben + 1;
+	Sausage_Given = Sausage_Given +  1 ;
 	CreateInvItems(self,ItFo_Sausage,1);
 	B_UseItem(self,ItFo_Sausage);
 };
@@ -192,7 +193,7 @@ instance DIA_HOLGER_TRADE(C_Info)
 	information = dia_holger_trade_info;
 	permanent = TRUE;
 	trade = TRUE;
-	description = "У тебя есть что-нибудь на продажу?";
+	description = " Do you have anything for sale? " ;
 };
 
 func int dia_holger_trade_condition()
@@ -211,8 +212,8 @@ func void dia_holger_trade_info()
 		AI_TurnToNPC(self,other);
 	};
 
-	AI_Output(other,self,"DIA_holger_TRADE_01_01");	//У тебя есть что-нибудь на продажу?
-	AI_Output(self,other,"DIA_holger_TRADE_01_02");	//Смотри сам...
+	AI_Output(other,self, " DIA_holger_TRADE_01_01 " );	// Do you have anything for sale?
+	AI_Output(self,other, " DIA_holger_TRADE_01_02 " );	// See for yourself...
 	B_GiveTradeInv(self);
 };
 
@@ -223,7 +224,7 @@ instance DIA_HOLGER_RELAX(C_Info)
 	condition = dia_holger_relax_condition;
 	information = dia_holger_relax_info;
 	permanent = TRUE;
-	description = "Ну, как дела?";
+	description = " Well, how are you? " ;
 };
 
 func int dia_holger_relax_condition()
@@ -236,16 +237,16 @@ func int dia_holger_relax_condition()
 
 func void dia_holger_relax_info()
 {
-	AI_Output(other,self,"DIA_holger_Relax_01_01");	//Ну, как дела?
-	AI_Output(self,other,"DIA_holger_Relax_01_02");	//Прекрасно! Родители приняли меня, простив прошлые промашки.
-	AI_Output(self,other,"DIA_holger_Relax_01_03");	//Теперь я снова дома, и могу хоть немного побыть рядом с ними.
+	AI_Output(other,self, " DIA_holger_Relax_01_01 " );	// Well, how are you?
+	AI_Output(self,other, " DIA_holger_Relax_01_02 " );	// Great! My parents accepted me, forgiving past mistakes.
+	AI_Output(self,other, " DIA_holger_Relax_01_03 " );	// Now I'm home again, and I can spend a little time with them.
 
 	if(hero.guild != GIL_KDF)
 	{
-		AI_Output(self,other,"DIA_holger_Relax_01_04");	//И еще я чертовски рад выкурить старый добрый косяк! А то в монастыре этим особо не позанимаешься.
+		AI_Output(self,other, " DIA_holger_Relax_01_04 " );	// And I'm damned happy to smoke a good old joint! And then in the monastery you don’t really work out this.
 	};
 
-	AI_Output(self,other,"DIA_holger_Relax_01_05");	//Я отпросился всего на пару недель. Если я понадоблюсь тебе, можешь потом найти меня там же, в монастыре.
+	AI_Output(self,other, " DIA_holger_Relax_01_05 " );	// I only took a couple of weeks off. If you need me, you can then find me there, in the monastery.
 
 	MIS_HOLGER_QUEST = 5;
 	HOLGER_TRADE_COND = TRUE;
@@ -263,7 +264,7 @@ instance DIA_HOLGER_PICKPOCKET(C_Info)
 
 func int dia_holger_pickpocket_condition()
 {
-	return C_Beklauen(54,70);
+	return  C_Robbery ( 54 , 70 );
 };
 
 func void dia_holger_pickpocket_info()
