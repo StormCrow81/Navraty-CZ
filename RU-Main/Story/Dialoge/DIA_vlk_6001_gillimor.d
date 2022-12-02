1de388d871071796989dbbea6366c7275cf794e2
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-02 22:04:27 +0200

    Update DIA_vlk_6001_gillimor.d

--- RU-Main/Story/Dialoge/DIA_vlk_6001_gillimor.d
+++ RU-Main/Story/Dialoge/DIA_vlk_6001_gillimor.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_GILLIMOR_EXIT(C_Info)
 {
 	npc = vlk_6001_gillimor;
@@ -29,7 +30,7 @@ instance DIA_GILLIMOR_HELLO(C_Info)
 	information = dia_gillimor_hello_info;
 	permanent = TRUE;
 	important = FALSE;
-	description = "Привет.";
+	description = " Hello " ;
 };
 
 
@@ -44,16 +45,16 @@ func int dia_gillimor_hello_condition()
 func void dia_gillimor_hello_info()
 {
 	AI_Output(other,self,"DIA_Gillimor_Hello_01_01");	//Привет.
-	AI_Output(self,other,"DIA_Gillimor_Hello_01_02");	//Приветствую тебя, чужеземец. Весьма смело с твоей стороны было заявиться в эту долину.
-	AI_Output(self,other,"DIA_Gillimor_Hello_01_03");	//Что же привело тебя в это место?
-	AI_Output(other,self,"DIA_Gillimor_Hello_01_04");	//Я ищу черного дракона. Слышал, что он бы обитает именно здесь.
-	AI_Output(other,self,"DIA_Gillimor_Hello_01_05");	//А ты случаем не встречал его в долине?
-	AI_Output(self,other,"DIA_Gillimor_Hello_01_06");	//Хмм... я крайне редко покидаю лагерь. И мало что знаю.
-	AI_Output(other,self,"DIA_Gillimor_Hello_01_07");	//А ты разве не ходишь на охоту, как другие парни?
-	AI_Output(self,other,"DIA_Gillimor_Hello_01_08");	//Нет, не хожу. Видишь ли, охотник из меня вышел никудышный.
-	AI_Output(self,other,"DIA_Gillimor_Hello_01_09");	//Если я и покидаю лагерь, то только для того, чтобы пополнить свой запас лекарственных трав.
-	AI_Output(other,self,"DIA_Gillimor_Hello_01_10");	//Ты разбираешься в травах?
-	AI_Output(self,other,"DIA_Gillimor_Hello_01_11");	//Ну, более или менее кое-что я в них смыслю.
+	AI_Output(self,other, " DIA_Gillimor_Hello_01_02 " );	// Greetings, outlander. It was very brave of you to come into this valley.
+	AI_Output(self,other, " DIA_Gillimor_Hello_01_03 " );	// What brings you to this place?
+	AI_Output(other,self, " DIA_Gillimor_Hello_01_04 " );	// I'm looking for a black dragon. I heard that he would live here.
+	AI_Output(other,self, " DIA_Gillimor_Hello_01_05 " );	// Did you happen to meet him in the valley?
+	AI_Output(self,other, " DIA_Gillimor_Hello_01_06 " );	// Hmm... I rarely leave the camp. And I don't know much.
+	AI_Output(other,self, " DIA_Gillimor_Hello_01_07 " );	// Don't you go hunting like the other guys do?
+	AI_Output(self,other, " DIA_Gillimor_Hello_01_08 " );	// No, I don't go. You see, the hunter came out of me useless.
+	AI_Output(self,other, " DIA_Gillimor_Hello_01_09 " );	// If I'm leaving the camp, it's only to replenish my stock of medicinal herbs.
+	AI_Output(other,self, " DIA_Gillimor_Hello_01_10 " );	// Are you good with herbs?
+	AI_Output(self,other, " DIA_Gillimor_Hello_01_11 " );	// Well, more or less I understand them.
 	MEETGILLIMOR = TRUE;
 };
 
@@ -66,7 +67,7 @@ instance DIA_GILLIMOR_HELLO_TEACH(C_Info)
 	information = dia_gillimor_hello_teach_info;
 	permanent = TRUE;
 	important = FALSE;
-	description = "Я слышал, ты хорошо разибраешься в алхимии.";
+	description = " I hear you're good at alchemy. " ;
 };
 
 
@@ -80,14 +81,14 @@ func int dia_gillimor_hello_teach_condition()
 
 func void dia_gillimor_hello_teach_info()
 {
-	AI_Output(other,self,"DIA_Gillimor_Hello_Teach_01_01");	//Я слышал, ты хорошо разибраешься в алхимии.
-	AI_Output(self,other,"DIA_Gillimor_Hello_Teach_01_02");	//Да, неплохо. Приготовление различных зелий или снадобий - это мой конек.
-	AI_Output(self,other,"DIA_Gillimor_Hello_Teach_01_03");	//К сожалению, в этих местах непросто отыскать ингредиенты для некоторых из них.
-	AI_Output(self,other,"DIA_Gillimor_Hello_Teach_01_04");	//Так что, в основном, я практикуюсь в приготовлении лечебных эликсиров и эссенций.
-	AI_Output(other,self,"DIA_Gillimor_Hello_Teach_01_05");	//А ты можешь для меня сварить несколько таких зелий?
-	AI_Output(self,other,"DIA_Gillimor_Hello_Teach_01_06");	//Что ж, я вполне бы мог сварить для тебя парочку эликсиров, с условием, если ты мне принесешь для них нужные ингредиенты.
-	AI_Output(self,other,"DIA_Gillimor_Hello_Teach_01_07");	//Я бы мог и научить тебя варить их самому, если тебе, конечно, это интересно.
-	AI_Output(self,other,"DIA_Gillimor_Hello_Teach_01_09");	//Просто скажи, если тебе будет что-то нужно.
+	AI_Output(other,self, " DIA_Gillimor_Hello_Teach_01_01 " );	// I hear you're good at alchemy.
+	AI_Output(self,other, " DIA_Gillimor_Hello_Teach_01_02 " );	// Yes, not bad. Preparing various potions or potions is my forte.
+	AI_Output(self,other, " DIA_Gillimor_Hello_Teach_01_03 " );	// Unfortunately, it's not easy to find the ingredients for some of them in these places.
+	AI_Output(self,other, " DIA_Gillimor_Hello_Teach_01_04 " );	// So basically I practice making healing elixirs and essences.
+	AI_Output(other,self, " DIA_Gillimor_Hello_Teach_01_05 " );	// Can you brew some of these potions for me?
+	AI_Output(self,other, " DIA_Gillimor_Hello_Teach_01_06 " );	// Well, I could brew a couple of elixirs for you, as long as you bring me the right ingredients for them.
+	AI_Output(self,other, " DIA_Gillimor_Hello_Teach_01_07 " );	// I could teach you how to cook them yourself, if you're interested, of course.
+	AI_Output(self,other, " DIA_Gillimor_Hello_Teach_01_09 " );	// Just say if you need anything.
 	GILLIMORREADYTEACH = TRUE;
 };
 
@@ -99,7 +100,7 @@ instance DIA_GILLIMOR_BREWFORME(C_Info)
 	condition = dia_gillimor_brewforme_condition;
 	information = dia_gillimor_brewforme_info;
 	permanent = TRUE;
-	description = "Приготовь мне...";
+	description = " Cook me... " ;
 };
 
 
@@ -115,7 +116,7 @@ func void dia_gillimor_brewforme_info()
 {
 	Info_ClearChoices(dia_gillimor_brewforme);
 	Info_AddChoice(dia_gillimor_brewforme,Dialog_Back,dia_gillimor_brewforme_back);
-	Info_AddChoice(dia_gillimor_brewforme,"...лечебный экстракт.",dia_gillimor_brewforme_health);
+	Info_AddChoice(dia_gillimor_brewforme, " ...healing extract. " ,dia_gillimor_brewforme_health);
 };
 
 func void dia_gillimor_brewforme_back()
@@ -125,23 +126,23 @@ func void dia_gillimor_brewforme_back()
 
 func void dia_gillimor_brewforme_health()
 {
-	AI_Output(other,self,"DIA_Gillimor_BrewForMe_Health_01_01");	//Приготовь мне эликсир лечения.
-	AI_Output(self,other,"DIA_Gillimor_BrewForMe_Health_01_02");	//А у тебя есть компоненты?
+	AI_Output(other,self, " DIA_Gillimor_BrewForMe_Health_01_01 " );	// Prepare me an elixir of healing.
+	AI_Output(self,other, " DIA_Gillimor_BrewForMe_Health_01_02 " );	// Do you have components?
 	if((Npc_HasItems(other,ItPl_Health_Herb_02) >= 2) && (Npc_HasItems(other,ItPl_Temp_Herb) >= 1))
 	{
 		AI_Output(other,self,"DIA_Gillimor_BrewForMe_Health_01_03");	//Держи.
 		B_GiveInvItems(other,self,ItPl_Health_Herb_02,2);
 		B_GiveInvItems(other,self,ItPl_Temp_Herb,1);
-		AI_Output(self,other,"DIA_Gillimor_BrewForMe_Health_01_04");	//Хорошо, уже даю, тебе не придется долго ждать.
+		AI_Output(self,other, " DIA_Gillimor_BrewForMe_Health_01_04 " );	// Okay, I'll give it to you, you won't have to wait long.
 		B_GiveInvItems(self,other,ItPo_Health_02,1);
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_Gillimor_BrewForMe_Health_01_05");	//У тебя нет нужных компонентов. Приходи, когда их найдешь.
+		AI_Output(self,other, " DIA_Gillimor_BrewForMe_Health_01_05 " );	// You don't have the required components. Come back when you find them.
 	};
 	Info_ClearChoices(dia_gillimor_brewforme);
 	Info_AddChoice(dia_gillimor_brewforme,Dialog_Back,dia_gillimor_brewforme_back);
-	Info_AddChoice(dia_gillimor_brewforme,"Приготовь мне лечебный экстракт.",dia_gillimor_brewforme_health);
+	Info_AddChoice(dia_gillimor_brewforme, " Brew me a healing extract. " ,dia_gillimor_brewforme_health);
 };
 
 
@@ -152,7 +153,7 @@ instance DIA_GILLIMOR_TEACH(C_Info)
 	condition = dia_gillimor_teach_condition;
 	information = dia_gillimor_teach_info;
 	permanent = TRUE;
-	description = "Научи меня варить зелья...";
+	description = " Teach me how to brew potions... " ;
 };
 
 
@@ -166,8 +167,8 @@ func int dia_gillimor_teach_condition()
 
 func void dia_gillimor_teach_info()
 {
-	AI_Output(other,self,"DIA_Gillimor_Teach_01_01");	//Научи меня варить зелья.
-	AI_Output(self,other,"DIA_Gillimor_Teach_01_02");	//Я открою тебе все тайны алхимии.
+	AI_Output(other,self, " DIA_Gillimor_Teach_01_01 " );	// Teach me how to brew potions.
+	AI_Output(self,other, " DIA_Gillimor_Teach_01_02 " );	// I will reveal to you all the secrets of alchemy.
 	Info_ClearChoices(dia_gillimor_teach);
 	Info_AddChoice(dia_gillimor_teach,Dialog_Back,dia_gillimor_teach_back);
 	if(PLAYER_TALENT_ALCHEMY[POTION_Health_01] == FALSE)
