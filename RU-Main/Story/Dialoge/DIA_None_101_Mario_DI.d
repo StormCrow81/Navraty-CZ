de286c372583769491a0f29ce8ed366b6a2ee530
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-02 21:25:22 +0200

    Update DIA_None_101_Mario_DI.d

--- RU-Main/Story/Dialoge/DIA_None_101_Mario_DI.d
+++ RU-Main/Story/Dialoge/DIA_None_101_Mario_DI.d
@@ -1,5 +1,6 @@
 
-instance DIA_NONE_101_MARIO_DI_EXIT(C_Info)
+
+instance DIA_NONE_101_MARIO_DI_EXIT (C_Info)
 {
 	npc = None_101_Mario_DI;
 	nr = 999;
@@ -21,14 +22,14 @@ func void DIA_NONE_101_MARIO_DI_EXIT_Info()
 };
 
 
-instance DIA_NONE_101_MARIO_DI_Job(C_Info)
+instance DIA_NONE_101_MARIO_OF_Job (C_Info)
 {
 	npc = None_101_Mario_DI;
 	nr = 4;
 	condition = DIA_NONE_101_MARIO_DI_Job_Condition;
-	information = DIA_NONE_101_MARIO_DI_Job_Info;
+	information = DIA_NONE_101_MARIO_OF_Job_Info;
 	permanent = TRUE;
-	description = "Тогда у тебя есть шанс показать свои боевые способности.";
+	description = " Then you have a chance to show off your fighting skills. " ;
 };
 
 
@@ -40,20 +41,20 @@ func int DIA_NONE_101_MARIO_DI_Job_Condition()
 	};
 };
 
-func void DIA_NONE_101_MARIO_DI_Job_Info()
+func void DIA_NONE_101_MARIO_OF_Job_Info()
 {
-	AI_Output(other,self,"DIA_NONE_101_MARIO_DI_Job_15_00");	//Тогда у тебя есть шанс показать свои боевые способности.
-	AI_Output(self,other,"DIA_NONE_101_MARIO_DI_Job_07_01");	//Помедленнее! Всему свое время.
-	AI_Output(other,self,"DIA_NONE_101_MARIO_DI_Job_15_02");	//Ммм... Я именно этого и ожидал от тебя.
+	AI_Output(other,self, " DIA_NONE_101_MARIO_DI_Job_15_00 " );	// Then you have a chance to show your combat abilities.
+	AI_Output(self,other, " DIA_NONE_101_MARIO_DI_Job_07_01 " );	// Slow down! Everything has its time.
+	AI_Output(other,self, " DIA_NONE_101_MARIO_DI_Job_15_02 " );	// Mmm... That's exactly what I expected from you.
 	AI_Output(self,other,"DIA_NONE_101_MARIO_DI_Job_07_03");	//Подожди.
 };
 
-instance DIA_NONE_101_MARIO_DI_ambush(C_Info)
+instance DIA_NONE_101_MARIO_DI_ambush (C_Info)
 {
 	npc = None_101_Mario_DI;
 	nr = 4;
 	condition = DIA_NONE_101_MARIO_DI_ambush_Condition;
-	information = DIA_NONE_101_MARIO_DI_ambush_Info;
+	information = DIA_NONE_101_MARIO_OF_ambush_Info;
 	important = TRUE;
 };
 
@@ -68,12 +69,12 @@ func int DIA_NONE_101_MARIO_DI_ambush_Condition()
 
 func void DIA_NONE_101_MARIO_DI_ambush_Info()
 {
-	AI_Output(self,other,"DIA_NONE_101_MARIO_DI_ambush_07_00");	//Подойди поближе! Так, мой друг. А теперь покажи мне, на что ты способен.
-	AI_Output(other,self,"DIA_NONE_101_MARIO_DI_ambush_15_01");	//Что ты имеешь в виду?
-	AI_Output(self,other,"DIA_NONE_101_MARIO_DI_ambush_07_02");	//Это просто! Хозяин уже устал от тебя.
-	AI_Output(self,other,"DIA_NONE_101_MARIO_DI_ambush_07_03");	//Мне стоило убить тебя раньше. Но мои друзья и я сейчас исправим эту ошибку.
+	AI_Output(self,other, " DIA_NONE_101_MARIO_DI_ambush_07_00 " );	// Come closer! Yes, my friend. Now show me what you can do.
+	AI_Output(other,self, " DIA_NONE_101_MARIO_DI_ambush_15_01 " );	// What do you mean?
+	AI_Output(self,other, " DIA_NONE_101_MARIO_DI_ambush_07_02 " );	// It's easy! The owner is already tired of you.
+	AI_Output(self,other, " DIA_NONE_101_MARIO_DI_ambush_07_03 " );	// I should have killed you earlier. But my friends and I will correct this mistake now.
 	Info_ClearChoices(DIA_NONE_101_MARIO_DI_ambush);
-	Info_AddChoice(DIA_NONE_101_MARIO_DI_ambush,Dialog_Ende,DIA_NONE_101_MARIO_DI_ambush_ambush);
+	Info_AddChoice(DIA_NONE_101_MARIO_Ambush,Dialog_End,DIA_NONE_101_MARIO_Ambush_ambush);
 	B_GivePlayerXP(XP_Mario_Ambush);
 	MIS_Mario_Ambush = LOG_SUCCESS;
 };
@@ -82,14 +83,14 @@ func void DIA_NONE_101_MARIO_DI_ambush_ambush()
 {
 	AI_StopProcessInfos(self);
 	B_Attack(self,other,AR_SuddenEnemyInferno,1);
-	Skeleton_Mario1.aivar[AIV_EnemyOverride] = FALSE;
-	Skeleton_Mario2.aivar[AIV_EnemyOverride] = FALSE;
-	Skeleton_Mario3.aivar[AIV_EnemyOverride] = FALSE;
-	Skeleton_Mario4.aivar[AIV_EnemyOverride] = FALSE;
+	Skeleton_Mario1.aivar[AIV_EnemyOverride] = FALSE ;
+	Skeleton_Mario2.aivar[AIV_EnemyOverride] = FALSE ;
+	Skeleton_Mario3.aivar[AIV_EnemyOverride] = FALSE ;
+	Skeleton_Mario4.aivar[AIV_EnemyOverride] = FALSE ;
 };
 
 
-instance DIA_MARIO_DI_PICKPOCKET(C_Info)
+instance DIA_MARIO_OF_PICKPOCKET (C_Info)
 {
 	npc = None_101_Mario_DI;
 	nr = 900;
@@ -102,19 +103,19 @@ instance DIA_MARIO_DI_PICKPOCKET(C_Info)
 
 func int DIA_MARIO_DI_PICKPOCKET_Condition()
 {
-	return C_Beklauen(71,110);
+	return  C_Robbery ( 71 , 110 );
 };
 
-func void DIA_MARIO_DI_PICKPOCKET_Info()
+func void DIA_MARIO_OF_PICKPOCKET_Info()
 {
 	Info_ClearChoices(dia_mario_di_pickpocket);
 	Info_AddChoice(dia_mario_di_pickpocket,Dialog_Back,dia_mario_di_pickpocket_back);
-	Info_AddChoice(dia_mario_di_pickpocket,DIALOG_PICKPOCKET,DIA_MARIO_DI_PICKPOCKET_DoIt);
+	Info_AddChoice(dia_mario_di_pickpocket, DIALOG_PICKPOCKET ,DIA_MARIO_DI_PICKPOCKET_DoIt);
 };
 
 func void DIA_MARIO_DI_PICKPOCKET_DoIt()
 {
-	B_Beklauen();
+	B_Robbery();
 	Info_ClearChoices(dia_mario_di_pickpocket);
 };
 
