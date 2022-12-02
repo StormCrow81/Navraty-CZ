6bc62485f4e2f8ce540684eba7c183ba998adf70
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-02 21:01:54 +0200

    Update DIA_vlk_6137_gaspar.d

--- RU-Main/Story/Dialoge/DIA_vlk_6137_gaspar.d
+++ RU-Main/Story/Dialoge/DIA_vlk_6137_gaspar.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_VLK_6137_GASPAR_EXIT(C_Info)
 {
 	npc = vlk_6137_gaspar;
@@ -27,7 +28,7 @@ instance DIA_VLK_6137_GASPAR_HELLO(C_Info)
 	condition = dia_vlk_6137_gaspar_hello_condition;
 	information = dia_vlk_6137_gaspar_hello_info;
 	permanent = FALSE;
-	description = "Эй, какого черта ты здесь делаешь?!";
+	description = " Hey, what the hell are you doing here?! " ;
 };
 
 
@@ -41,31 +42,31 @@ func int dia_vlk_6137_gaspar_hello_condition()
 
 func void dia_vlk_6137_gaspar_hello_info()
 {
-	AI_Output(other,self,"DIA_VLK_6137_Gaspar_Hello_01_00");	//Эй, какого черта ты здесь делаешь?! А где Ханна?
-	AI_Output(self,other,"DIA_VLK_6137_Gaspar_Hello_01_01");	//Меня зовут Гаспар. Я...
-	AI_Output(other,self,"DIA_VLK_6137_Gaspar_Hello_01_02");	//Кажется, я тебя не об этом спросил.
-	AI_Output(self,other,"DIA_VLK_6137_Gaspar_Hello_01_03");	//Теперь я тут хозяин, и это моя гостиница.
-	AI_Output(self,other,"DIA_VLK_6137_Gaspar_Hello_01_05");	//Пару дней назад Ханну арестовало городское ополчение - ее обвиняют в краже.
-	AI_Output(other,self,"DIA_VLK_6137_Gaspar_Hello_01_06");	//В чем ее обвиняют?!
-	AI_Output(self,other,"DIA_VLK_6137_Gaspar_Hello_01_07");	//В краже! Она обокрала одного купца и теперь сидит в тюрьме. Думаю, ей там самое место!
-	AI_Output(other,self,"DIA_VLK_6137_Gaspar_Hello_01_08");	//Что? Да ты в своем уме, приятель? Этого просто не может быть!
-	AI_Output(self,other,"DIA_VLK_6137_Gaspar_Hello_01_09");	//Еще как может! В ее сундуке нашли украденные вещи этого бедолаги.
-	AI_Output(self,other,"DIA_VLK_6137_Gaspar_Hello_01_10");	//Так что все доказательства ее вины просто очевидны.
-	AI_Output(self,other,"DIA_VLK_6137_Gaspar_Hello_01_12");	//Если тебя интересуют подробности этого дела, то тебе лучше поговорить об этом с самим лордом Андрэ.
-	AI_Output(self,other,"DIA_VLK_6137_Gaspar_Hello_01_13");	//А мне тут с тобой болтать некогда!
+	AI_Output(other,self, " DIA_VLK_6137_Gaspar_Hello_01_00 " );	// Hey, what the hell are you doing here?! Where is Hannah?
+	AI_Output(self,other, " DIA_VLK_6137_Gaspar_Hello_01_01 " );	// My name is Gaspar. I...
+	AI_Output(other,self, " DIA_VLK_6137_Gaspar_Hello_01_02 " );	// I don't think I asked you about this.
+	AI_Output(self,other, " DIA_VLK_6137_Gaspar_Hello_01_03 " );	// Now I'm the owner here, and this is my hotel.
+	AI_Output(self,other, " DIA_VLK_6137_Gaspar_Hello_01_05 " );	// A couple of days ago, Hanna was arrested by the city militia - she is accused of theft.
+	AI_Output(other,self, " DIA_VLK_6137_Gaspar_Hello_01_06 " );	// What is she being accused of?!
+	AI_Output(self,other, " DIA_VLK_6137_Gaspar_Hello_01_07 " );	// Steal! She robbed a merchant and is now in prison. I think she belongs there!
+	AI_Output(other,self, " DIA_VLK_6137_Gaspar_Hello_01_08 " );	// What? Are you out of your mind, buddy? It just can't be!
+	AI_Output(self,other, " DIA_VLK_6137_Gaspar_Hello_01_09 " );	// Still how can! In her chest they found the stolen things of this poor fellow.
+	AI_Output(self,other, " DIA_VLK_6137_Gaspar_Hello_01_10 " );	// So all the evidence of her guilt is simply obvious.
+	AI_Output(self,other, " DIA_VLK_6137_Gaspar_Hello_01_12 " );	// If you're interested in the details of this case, you'd better talk to Lord Andre himself about it.
+	AI_Output(self,other, " DIA_VLK_6137_Gaspar_Hello_01_13 " );	// And I have no time to chat with you here!
 	KNOWSHANNAINPRISION = TRUE;
-	B_LogEntry(TOPIC_ABIGEILHELPHANNA,"По прибытию в Хоринис оказалось, что Ханну арестовали. Ее обвиняют в краже вещей у одного торговца. Вместо нее в гостинице теперь всем заправляет человек по имени Гаспар - мерзкий типчик! Надо бы поговрить с лордом Андрэ и выяснить, что именно здесь произошло.");
+	B_LogEntry( TOPIC_ABIGEILHELPHANNA , " Upon arrival in Khorinis, it turned out that Hannah was arrested. She is accused of stealing things from a merchant. Instead, the inn is now run by a man named Gaspar - a vile type! I should talk to Lord Andre and find out exactly what happened here. " );
 };
 
 
-instance DIA_VLK_6137_GASPAR_INPRISOIN(C_Info)
+instances DIA_VLK_6137_GASPAR_INPRISOIN (C_Info)
 {
 	npc = vlk_6137_gaspar;
 	nr = 5;
 	condition = dia_vlk_6137_gaspar_inprisoin_condition;
 	information = dia_vlk_6137_gaspar_inprisoin_info;
 	permanent = FALSE;
-	description = "Так это ты тот самый свидетель в деле Ханны";
+	description = " So you're the witness in the Hanna case " ;
 };
 
 
@@ -80,29 +81,29 @@ func int dia_vlk_6137_gaspar_inprisoin_condition()
 func void dia_vlk_6137_gaspar_inprisoin_info()
 {
 	B_GivePlayerXP(50);
-	AI_Output(other,self,"DIA_VLK_6137_Gaspar_InPrisoin_01_00");	//Так это ты тот самый свидетель в деле Ханны?
+	AI_Output(other,self, " DIA_VLK_6137_Gaspar_InPrisoin_01_00 " );	// So you're the witness in Hannah's case?
 	AI_Output(self,other,"DIA_VLK_6137_Gaspar_InPrisoin_01_01");	//Да, я.
-	AI_Output(other,self,"DIA_VLK_6137_Gaspar_InPrisoin_01_02");	//Тогда расскажи мне, что ты видел.
-	AI_Output(self,other,"DIA_VLK_6137_Gaspar_InPrisoin_01_03");	//Все, что я видел, я уже рассказал лорду Андрэ. Так что все вопросы к нему.
-	AI_Output(other,self,"DIA_VLK_6137_Gaspar_InPrisoin_01_04");	//Почему же ты не хочешь говорить об этом?
-	AI_Output(self,other,"DIA_VLK_6137_Gaspar_InPrisoin_01_05");	//Потому что...(нервничает) Потому что это не твоего ума дело!
-	AI_Output(other,self,"DIA_VLK_6137_Gaspar_InPrisoin_01_07");	//Думаю, скоро ты окажешься там, где тебе самое место.
-	AI_Output(self,other,"DIA_VLK_6137_Gaspar_InPrisoin_01_08");	//Ты что, мне угрожаешь?!
-	AI_Output(self,other,"DIA_VLK_6137_Gaspar_InPrisoin_01_10");	//Послушай...(нервничает) Мне начихать на все твои угрозы и предупреждения!
-	AI_Output(self,other,"DIA_VLK_6137_Gaspar_InPrisoin_01_11");	//Так что лучше проваливай из моей гостиницы, пока я не позвал стражу.
-	B_LogEntry(TOPIC_ABIGEILHELPHANNA,"Мне показалось, что Гаспар явно что-то скрывает в деле Ханны. Думаю, надо побольше узнать об этом пижоне - он явно не чист на руку.");
+	AI_Output(other,self, " DIA_VLK_6137_Gaspar_InPrisoin_01_02 " );	// Then tell me what you saw.
+	AI_Output(self,other, " DIA_VLK_6137_Gaspar_InPrisoin_01_03 " );	// Everything I've seen, I've already told Lord Andre. So all questions to him.
+	AI_Output(other,self, " DIA_VLK_6137_Gaspar_InPrisoin_01_04 " );	// Why don't you want to talk about it?
+	AI_Output(self,other, " DIA_VLK_6137_Gaspar_InPrisoin_01_05 " );	// Because... (nervous) Because it's none of your business!
+	AI_Output(other,self, " DIA_VLK_6137_Gaspar_InPrisoin_01_07 " );	// I think you'll soon be where you belong.
+	AI_Output(self,other, " DIA_VLK_6137_Gaspar_InPrisoin_01_08 " );	// Are you threatening me?!
+	AI_Output(self,other, " DIA_VLK_6137_Gaspar_InPrisoin_01_10 " );	// Look...(nervous) I don't give a shit about all your threats and warnings!
+	AI_Output(self,other, " DIA_VLK_6137_Gaspar_InPrisoin_01_11 " );	// So you'd better get out of my inn before I call the guards.
+	B_LogEntry( TOPIC_ABIGEILHELPHANNA , " I thought Gasper was obviously hiding something about Hannah's case. I think we need to know more about this dude - he's obviously not clean. " );
 	AI_StopProcessInfos(self);
 };
 
 
-instance DIA_VLK_6137_GASPAR_INPRISOINAB(C_Info)
+instances DIA_VLK_6137_GASPAR_INPRISOINAB (C_Info)
 {
 	npc = vlk_6137_gaspar;
 	nr = 5;
 	condition = dia_vlk_6137_gaspar_inprisoinab_condition;
 	information = dia_vlk_6137_gaspar_inprisoinab_info;
 	permanent = TRUE;
-	description = "Это ты подставил Ханну!";
+	description = " You framed Hannah! " ;
 };
 
 
@@ -116,28 +117,28 @@ func int dia_vlk_6137_gaspar_inprisoinab_condition()
 
 func void dia_vlk_6137_gaspar_inprisoinab_info()
 {
-	AI_Output(other,self,"DIA_VLK_6137_Gaspar_InPrisoinAb_01_00");	//Это ты подставил Ханну!
-	AI_Output(self,other,"DIA_VLK_6137_Gaspar_InPrisoinAb_01_01");	//(испуганно) Что?!
-	AI_Output(other,self,"DIA_VLK_6137_Gaspar_InPrisoinAb_01_03");	//Ты подсунул украденные вещи Ханне!
-	AI_Output(self,other,"DIA_VLK_6137_Gaspar_InPrisoinAb_01_04");	//Кто? Я?! Я тут не причем! Я ничего не знаю!
-	AI_Output(other,self,"DIA_VLK_6137_Gaspar_InPrisoinAb_01_05");	//Знаешь! И мне не составит труда это доказать.
-	AI_Output(self,other,"DIA_VLK_6137_Gaspar_InPrisoinAb_01_06");	//Ну, что же, попробуй.
+	AI_Output(other,self, " DIA_VLK_6137_Gaspar_InPrisoinAb_01_00 " );	// You set Hannah up!
+	AI_Output(self,other, " DIA_VLK_6137_Gaspar_InPrisoinAb_01_01 " );	// (frightened) What?!
+	AI_Output(other,self, " DIA_VLK_6137_Gaspar_InPrisoinAb_01_03 " );	// You slipped the stolen items to Hannah!
+	AI_Output(self,other, " DIA_VLK_6137_Gaspar_InPrisoinAb_01_04 " );	// Who? I?! I have nothing to do with it! I do not know anything!
+	AI_Output(other,self, " DIA_VLK_6137_Gaspar_InPrisoinAb_01_05 " );	// You know! And it won't be hard for me to prove it.
+	AI_Output(self,other, " DIA_VLK_6137_Gaspar_InPrisoinAb_01_06 " );	// Well, try it.
 
-	if((Npc_HasItems(other,itri_hakonmissring) >= 1) && (JOTELLSGASPAR == TRUE))
+	if ((Npc_HasItems(other,itri_ismissing) >=  1 ) && ( JOTELLSAVINGS  ==  TRUE )) .
 	{
 		B_GivePlayerXP(50);
-		AI_Output(other,self,"DIA_VLK_6137_Gaspar_InPrisoinAb_01_08");	//Смотри, какое у меня есть красивое колечко.
-		AI_Output(other,self,"DIA_VLK_6137_Gaspar_InPrisoinAb_01_09");	//И еще я знаю одного человека, его зовут Джо. Кстати, он тебя тоже знает.
-		AI_Output(other,self,"DIA_VLK_6137_Gaspar_InPrisoinAb_01_10");	//Причем знает про тебя столько, что этого хватит чтобы, упрятать тебя за решетку.
-		AI_Output(self,other,"DIA_VLK_6137_Gaspar_InPrisoinAb_01_11");	//(бледнея) Что?!
-		AI_Output(self,other,"DIA_VLK_6137_Gaspar_InPrisoinAb_01_13");	//Делай, что хочешь - мне уже все равно.
+		AI_Output(other,self, " DIA_VLK_6137_Gaspar_InPrisoinAb_01_08 " );	// Look what a beautiful ring I have.
+		AI_Output(other,self, " DIA_VLK_6137_Gaspar_InPrisoinAb_01_09 " );	// And I also know one person, his name is Joe. By the way, he knows you too.
+		AI_Output(other,self, " DIA_VLK_6137_Gaspar_InPrisoinAb_01_10 " );	// And he knows so much about you that this is enough to put you behind bars.
+		AI_Output(self,other, " DIA_VLK_6137_Gaspar_InPrisoinAb_01_11 " );	// (blushing) What?!
+		AI_Output(self,other, " DIA_VLK_6137_Gaspar_InPrisoinAb_01_13 " );	// Do what you want - I don't care anymore.
 		GASPARPISSOFF = TRUE;
 	};
 	AI_StopProcessInfos(self);
 };
 
 
-instance DIA_VLK_6137_GASPAR_FUCKOFF(C_Info)
+instances DIA_VLK_6137_GASPAR_FUCKOFF (C_Info)
 {
 	npc = vlk_6137_gaspar;
 	nr = 10;
