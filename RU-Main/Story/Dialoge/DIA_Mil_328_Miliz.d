13ce0dc705dc80b7e3bd248ff61d0c551c08fd8e
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-02 20:27:16 +0200

    Update DIA_Mil_328_Miliz.d

--- RU-Main/Story/Dialoge/DIA_Mil_328_Miliz.d
+++ RU-Main/Story/Dialoge/DIA_Mil_328_Miliz.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_328_Miliz_EXIT(C_Info)
 {
 	npc = Mil_328_Miliz;
@@ -21,7 +22,7 @@ func void DIA_328_Miliz_EXIT_Info()
 };
 
 
-instance DIA_328_Miliz_Hi(C_Info)
+instances DIA_328_Miliz_Hi (C_Info)
 {
 	npc = Mil_328_Miliz;
 	nr = 1;
@@ -39,17 +40,17 @@ func int DIA_328_Miliz_Hi_Condition()
 
 func void DIA_328_Miliz_Hi_Info()
 {
-	AI_Output(self,other,"DIA_328_Miliz_Hi_08_00");	//Эй, тебе нечего делать здесь. Понятно?
-	AI_Output(other,self,"DIA_328_Miliz_Hi_15_01");	//Кто ты такой?
-	AI_Output(self,other,"DIA_328_Miliz_Hi_08_02");	//Это не твое дело! Здесь я главный. Понял?
-	AI_Output(other,self,"DIA_328_Miliz_Hi_15_03");	//Главный? Над чем? Над этими сундуками?
-	AI_Output(self,other,"DIA_328_Miliz_Hi_08_04");	//Эй, этот склад находится в моем ведении, понял? Так что проваливай, или я раскрою твой череп.
+	AI_Output(self,other, " DIA_328_Miliz_Hi_08_00 " );	// Hey, there's nothing for you to do here. Clear?
+	AI_Output(other,self, " DIA_328_Miliz_Hi_15_01 " );	// Who are you?
+	AI_Output(self,other, " DIA_328_Miliz_Hi_08_02 " );	// It's none of your business! I'm in charge here. Understood?
+	AI_Output(other,self, " DIA_328_Miliz_Hi_15_03 " );	// Main? Above what? Over those chests?
+	AI_Output(self,other, " DIA_328_Miliz_Hi_08_04 " );	// Hey, this warehouse is in my charge, got it? So get out or I'll crack your skull open.
 	AI_StopProcessInfos(self);
 	Npc_SetRefuseTalk(self,30);
 };
 
 
-instance DIA_328_Miliz_Kill(C_Info)
+instance DIA_328_Miliz_Kill (C_Info)
 {
 	npc = Mil_328_Miliz;
 	nr = 2;
@@ -70,8 +71,8 @@ func int DIA_328_Miliz_Kill_Condition()
 
 func void DIA_328_Miliz_Kill_Info()
 {
-	AI_Output(self,other,"DIA_328_Miliz_Kill_08_00");	//Эй, ты все еще здесь! Разве я не сказал тебе, чтобы ты проваливал?
-	AI_Output(self,other,"DIA_328_Miliz_Kill_08_01");	//Ну, держись! Сейчас я покажу тебе, кто здесь главный!
+	AI_Output(self,other, " DIA_328_Miliz_Kill_08_00 " );	// Hey, you're still here! Didn't I tell you to get lost?
+	AI_Output(self,other, " DIA_328_Miliz_Kill_08_01 " );	// Well, hold on! Now I'll show you who's in charge here!
 	AI_StopProcessInfos(self);
 	Npc_SetRefuseTalk(self,20);
 	B_Attack(self,other,AR_KILL,1);
