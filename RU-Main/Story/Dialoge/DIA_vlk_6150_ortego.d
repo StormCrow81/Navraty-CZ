df7b7841bd824e1c6e39ba2a0fb7adea84f6d8fa
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-02 21:01:17 +0200

    Update DIA_vlk_6150_ortego.d

--- RU-Main/Story/Dialoge/DIA_vlk_6150_ortego.d
+++ RU-Main/Story/Dialoge/DIA_vlk_6150_ortego.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_VLK_6150_ORTEGO_EXIT(C_Info)
 {
 	npc = vlk_6150_ortego;
@@ -42,21 +43,21 @@ func int dia_vlk_6150_ortego_hello_condition()
 func void dia_vlk_6150_ortego_hello_info()
 {
 	B_GivePlayerXP(200);
-	AI_Output(self,other,"DIA_VLK_6150_Ortego_Hello_01_00");	//Наконец-то ты пришел! А то я уже тебя заждался.
-	AI_Output(other,self,"DIA_VLK_6150_Ortego_Hello_01_01");	//Ты меня ждал?
-	AI_Output(self,other,"DIA_VLK_6150_Ortego_Hello_01_02");	//Ну да, тебя... кого же еще? Хотя я и представлял тебя немного другим.
-	AI_Output(self,other,"DIA_VLK_6150_Ortego_Hello_01_03");	//Но лучше ответь - ты приготовил для меня мои деньги?
-	AI_Output(other,self,"DIA_VLK_6150_Ortego_Hello_01_04");	//Какие еще деньги?
-	AI_Output(self,other,"DIA_VLK_6150_Ortego_Hello_01_05");	//Не понял...(злобно) Ты что, меня за идиота держишь?
-	AI_Output(self,other,"DIA_VLK_6150_Ortego_Hello_01_06");	//Те деньги, которые ты мне обещал за смерть этого жалкого старикашки алхимика.
-	AI_Output(self,other,"DIA_VLK_6150_Ortego_Hello_01_07");	//(смеется) Этот бедняга даже пикнуть не успел, как был уже мертв!
-	AI_Output(self,other,"DIA_VLK_6150_Ortego_Hello_01_08");	//И теперь я жду своего золота, которое ты обещал мне в своем письме.
-	AI_Output(other,self,"DIA_VLK_6150_Ortego_Hello_01_11");	//Так ты кого-то убил?
-	AI_Output(self,other,"DIA_VLK_6150_Ortego_Hello_01_12");	//(непонимающее) Да что с тобой, парень?! Я что, не ясно говорю? Или ты последние мозги пропил?
-	AI_Output(other,self,"DIA_VLK_6150_Ortego_Hello_01_13");	//Со мной все в порядке. Просто, думаю, что я не тот, кто тебе нужен.
-	AI_Output(self,other,"DIA_VLK_6150_Ortego_Hello_01_14");	//Что?! Хммм... хочешь сказать, что я ошибся?
-	AI_Output(other,self,"DIA_VLK_6150_Ortego_Hello_01_15");	//Так оно и есть.
-	AI_Output(self,other,"DIA_VLK_6150_Ortego_Hello_01_16");	//Ну что ж, это много объясняет. Погоди минутку, я сейчас тебе все объясню...
+	AI_Output(self,other, " DIA_VLK_6150_Ortego_Hello_01_00 " );	// You've finally arrived! And then I was already waiting for you.
+	AI_Output(other,self, " DIA_VLK_6150_Ortego_Hello_01_01 " );	// Were you waiting for me?
+	AI_Output(self,other, " DIA_VLK_6150_Ortego_Hello_01_02 " );	// Well, yes, you... who else? Although I imagined you a little different.
+	AI_Output(self,other, " DIA_VLK_6150_Ortego_Hello_01_03 " );	// But you'd better answer - have you prepared my money for me?
+	AI_Output(other,self, " DIA_VLK_6150_Ortego_Hello_01_04 " );	// What other money?
+	AI_Output(self,other, " DIA_VLK_6150_Ortego_Hello_01_05 " );	// I don't understand... (viciously) Are you taking me for an idiot?
+	AI_Output(self,other, " DIA_VLK_6150_Ortego_Hello_01_06 " );	// The money you promised me for the death of that pathetic old alchemist.
+	AI_Output(self,other, " DIA_VLK_6150_Ortego_Hello_01_07 " );	// (laughs) This poor fellow didn't even have time to utter a word, as he was already dead!
+	AI_Output(self,other, " DIA_VLK_6150_Ortego_Hello_01_08 " );	// And now I'm waiting for my gold, which you promised me in your letter.
+	AI_Output(other,self, " DIA_VLK_6150_Ortego_Hello_01_11 " );	// So you killed someone?
+	AI_Output(self,other, " DIA_VLK_6150_Ortego_Hello_01_12 " );	// (uncomprehending) What's wrong with you, boy?! Am I not speaking clearly? Or have you drunk your last brains?
+	AI_Output(other,self, " DIA_VLK_6150_Ortego_Hello_01_13 " );	// I'm fine. I just don't think I'm the one for you.
+	AI_Output(self,other, " DIA_VLK_6150_Ortego_Hello_01_14 " );	// What?! Hmmm... are you saying I was wrong?
+	AI_Output(other,self, " DIA_VLK_6150_Ortego_Hello_01_15 " );	// So it is.
+	AI_Output(self,other, " DIA_VLK_6150_Ortego_Hello_01_16 " );	// Well, that explains a lot. Wait a minute, I'll explain everything to you...
 	AI_StopProcessInfos(self);
 	B_Attack(self,other,AR_NONE,1);
 };
