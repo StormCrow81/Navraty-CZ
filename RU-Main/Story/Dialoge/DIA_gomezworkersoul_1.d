d9d3ecf5b10474ec20fdcf33c7c3303fea0c42f8
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 22:21:29 +0200

    Update DIA_gomezworkersoul_1.d

--- RU-Main/Story/Dialoge/DIA_gomezworkersoul_1.d
+++ RU-Main/Story/Dialoge/DIA_gomezworkersoul_1.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_GOMEZWORKERSOUL_1_EXIT(C_Info)
 {
 	npc = gomezworkersoul_1;
@@ -21,12 +22,12 @@ func void dia_gomezworkersoul_1_exit_info()
 };
 
 
-instance DIA_GOMEZWORKERSOUL_1_HALLO(C_Info)
+instance DIA_GOMEZWORKERSOUL_1_HELLO (C_Info)
 {
 	npc = gomezworkersoul_1;
 	nr = 1;
 	condition = dia_gomezworkersoul_1_hallo_condition;
-	information = dia_gomezworkersoul_1_hallo_info;
+	information = dia_gomezworkersoul_1_hello_info;
 	permanent = FALSE;
 	important = TRUE;
 };
@@ -40,29 +41,29 @@ func int dia_gomezworkersoul_1_hallo_condition()
 	};
 };
 
-func void dia_gomezworkersoul_1_hallo_info()
+func void dia_gomezworkersoul_1_hello_info()
 {
-	self.aivar[AIV_EnemyOverride] = FALSE;
+	self.aivar[AIV_EnemyOverride] = FALSE ;
 	Snd_Play("MFX_FEAR_CAST");
 	B_GivePlayerXP(50);
-	AI_Output(self,other,"DIA_GomezWorkerSoul_1_HALLO_01_00");	//(мертвым голосом) ЗАЧЕМ ТЫ БЕСПОКОИШЬ МЕНЯ, ЧЕЛОВЕК? ЧТО ТЕБЕ ОТ МЕНЯ НУЖНО?
-	AI_Output(other,self,"DIA_GomezWorkerSoul_1_HALLO_01_01");	//Я пришел сюда по поручению Гомеза, чтобы упокоить твою душу, призрак.
-	AI_Output(self,other,"DIA_GomezWorkerSoul_1_HALLO_01_02");	//Ты пришел свободить меня? Аргххх... Неужели моим мукам придет конец?
-	AI_Output(other,self,"DIA_GomezWorkerSoul_1_HALLO_01_04");	//Да, но перед тем как я это сделаю, Гомез хочет, чтобы и ты даровал прощение его душе.
-	AI_Output(self,other,"DIA_GomezWorkerSoul_1_HALLO_01_05");	//(мертвый вздох) Все мы здесь прокляты только из-за него!
-	AI_Output(self,other,"DIA_GomezWorkerSoul_1_HALLO_01_06");	//И, видимо, его душа также не знает покоя в этом мире.
-	AI_Output(other,self,"DIA_GomezWorkerSoul_1_HALLO_01_07");	//Ты совершенно прав, призрак.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_1_HALLO_01_00 " );	// (dead voice) WHY DO YOU TROUBLE ME, HUMAN? WHAT DO YOU WANT FROM ME?
+	AI_Output(other,self, " DIA_GomezWorkerSoul_1_HALLO_01_01 " );	// I came here on behalf of Gomez to put your soul to rest, ghost.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_1_HALLO_01_02 " );	// Have you come to set me free? Arghhh... Will my suffering come to an end?
+	AI_Output(other,self, " DIA_GomezWorkerSoul_1_HALLO_01_04 " );	// Yes, but before I do that, Gomez wants you to grant forgiveness to his soul as well.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_1_HALLO_01_05 " );	// (dead breath) We're all damned here because of him!
+	AI_Output(self,other, " DIA_GomezWorkerSoul_1_HALLO_01_06 " );	// And, apparently, his soul also does not know peace in this world.
+	AI_Output(other,self, " DIA_GomezWorkerSoul_1_HALLO_01_07 " );	// You're absolutely right, ghost.
 	Info_ClearChoices(dia_gomezworkersoul_1_hallo);
-	Info_AddChoice(dia_gomezworkersoul_1_hallo,"Так ты даешь ему свое прощение?",dia_gomezworkersoul_1_hallo_end);
+	Info_AddChoice(dia_gomezworkersoul_1_hallo, " So you give him your forgiveness? " ,dia_gomezworkersoul_1_hallo_end);
 };
 
-func void dia_gomezworkersoul_1_hallo_end()
+func void dia_gomezworkersoul_1_hello_end()
 {
 	Snd_Play("SFX_INNOSEYE");
-	AI_Output(other,self,"DIA_GomezWorkerSoul_1_HALLO_End_01_01");	//Так ты даешь ему свое прощение?
-	AI_Output(self,other,"DIA_GomezWorkerSoul_1_HALLO_End_01_02");	//Хорошо...(мертвым голосом) Я даю ему то, что он просит.
-	AI_Output(self,other,"DIA_GomezWorkerSoul_1_HALLO_End_01_03");	//Поскольку знаю, что значит быть проклятым.
-	AI_Output(self,other,"DIA_GomezWorkerSoul_1_HALLO_End_01_04");	//А теперь, человек, - освободи меня! Аргххх...
+	AI_Output(other,self, " DIA_GomezWorkerSoul_1_HALLO_End_01_01 " );	// So you give him your forgiveness?
+	AI_Output(self,other, " DIA_GomezWorkerSoul_1_HALLO_End_01_02 " );	// Okay... (dead voice) I give him what he asks for.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_1_HALLO_End_01_03 " );	// Because I know what it means to be cursed.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_1_HALLO_End_01_04 " );	// And now, man, set me free! Arghhh...
 	GOMEZSOULTALK_1 = TRUE;
 	AI_StopProcessInfos(self);
 };
