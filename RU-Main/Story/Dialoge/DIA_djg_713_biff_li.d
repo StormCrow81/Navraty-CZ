da8e524874472b97acc80339ab2e212842d3f17f
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 23:11:42 +0200

    Update DIA_djg_713_biff_li.d

--- RU-Main/Story/Dialoge/DIA_djg_713_biff_li.d
+++ RU-Main/Story/Dialoge/DIA_djg_713_biff_li.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_BIFF_LI_EXIT(C_Info)
 {
 	npc = djg_713_biff_li;
@@ -28,7 +29,7 @@ instance DIA_BIFF_LI_PERM(C_Info)
 	condition = dia_biff_li_perm_condition;
 	information = dia_biff_li_perm_info;
 	permanent = TRUE;
-	description = "На корабле все нормально?";
+	description = " Is everything okay on the ship? " ;
 };
 
 
@@ -39,8 +40,8 @@ func int dia_biff_li_perm_condition()
 
 func void dia_biff_li_perm_info()
 {
-	AI_Output(other,self,"DIA_Biff_DI_perm_15_00");	//На борту все спокойно?
-	AI_Output(self,other,"DIA_Biff_DI_perm_07_01");	//(нервничает) Да, да. Все тихо.
+	AI_Output(other,self, " DIA_Biff_DI_perm_15_00 " );	// Is everything calm on board?
+	AI_Output(self,other, " DIA_Biff_DI_perm_07_01 " );	// (nervous) Yes, yes. Everything is quiet.
 	AI_StopProcessInfos(self);
 };
 
@@ -58,7 +59,7 @@ instance DIA_BIFF_LI_PICKPOCKET(C_Info)
 
 func int dia_biff_li_pickpocket_condition()
 {
-	return C_Beklauen(92,450);
+	return  C_Robbery ( 92 , 450 );
 };
 
 func void dia_biff_li_pickpocket_info()
@@ -70,7 +71,7 @@ func void dia_biff_li_pickpocket_info()
 
 func void dia_biff_li_pickpocket_doit()
 {
-	B_Beklauen();
+	B_Robbery();
 	Info_ClearChoices(dia_biff_li_pickpocket);
 };
 
@@ -87,7 +88,7 @@ instance DIA_BIFF_LI_MISSMYGOLD(C_Info)
 	condition = dia_biff_li_missmygold_condition;
 	information = dia_biff_li_missmygold_info;
 	permanent = FALSE;
-	description = "Что-нибудь знаешь о моем золоте?";
+	description = " Do you know anything about my gold? " ;
 };
 
 
@@ -101,9 +102,9 @@ func int dia_biff_li_missmygold_condition()
 
 func void dia_biff_li_missmygold_info()
 {
-	AI_Output(other,self,"DIA_Biff_LI_MissMyGold_01_01");	//Ты, случайно, не в курсе, куда подевалось мое золото?
-	AI_Output(self,other,"DIA_Biff_LI_MissMyGold_01_02");	//Навряд ли! Я предпочитаю думать только о своем.
-	B_LogEntry(TOPIC_MISSMYGOLD,"Бифф ничего толком и не смог сказать насчет пропажи моего золота.");
+	AI_Output(other,self, " DIA_Biff_LI_MissMyGold_01_01 " );	// Do you happen to know where my gold went?
+	AI_Output(self,other, " DIA_Biff_LI_MissMyGold_01_02 " );	// Hardly! I prefer to think only of myself.
+	B_LogEntry( TOPIC_MISSMYGOLD , " Biff didn't really have anything to say about missing my gold. " );
 };
 
 
@@ -114,7 +115,7 @@ instance DIA_BIFF_LI_FINDMAGICORECAVE(C_Info)
 	condition = dia_biff_li_findmagicorecave_condition;
 	information = dia_biff_li_findmagicorecave_info;
 	permanent = FALSE;
-	description = "Я нашел пещеру с залежами магической руды.";
+	description = " I found a cave with magical ore deposits. " ;
 };
 
 
@@ -128,9 +129,9 @@ func int dia_biff_li_findmagicorecave_condition()
 
 func void dia_biff_li_findmagicorecave_info()
 {
-	AI_Output(other,self,"DIA_Biff_LI_FindMagicOreCave_01_01");	//Я нашел пещеру с залежами магической руды.
-	AI_Output(self,other,"DIA_Biff_LI_FindMagicOreCave_01_02");	//Правда? Тогда тебе стоит поговорить об этом с нашим капитаном.
-	AI_Output(self,other,"DIA_Biff_LI_FindMagicOreCave_01_03");	//Думаю, ему будет интересно услышать эту новость.
+	AI_Output(other,self, " DIA_Biff_LI_FindMagicOreCave_01_01 " );	// I found a cave with magical ore deposits.
+	AI_Output(self,other, " DIA_Biff_LI_FindMagicOreCave_01_02 " );	// True? Then you should talk to our captain about it.
+	AI_Output(self,other, " DIA_Biff_LI_FindMagicOreCave_01_03 " );	// I think he will be interested to hear this news.
 	GOTOORECAPITAN = TRUE;
 };
 
@@ -142,7 +143,7 @@ instance DIA_BIFF_LI_CHANGECOURSE(C_Info)
 	condition = dia_biff_li_changecourse_condition;
 	information = dia_biff_li_changecourse_info;
 	permanent = FALSE;
-	description = "Мы должны вернуться в Хоринис.";
+	description = " We must return to Khorinis. " ;
 };
 
 
@@ -156,29 +157,29 @@ func int dia_biff_li_changecourse_condition()
 
 func void dia_biff_li_changecourse_info()
 {
-	AI_Output(other,self,"DIA_Biff_LI_ChangeCourse_01_00");	//Мы должны снова вернуться в Хоринис.
-	AI_Output(self,other,"DIA_Biff_LI_ChangeCourse_01_01");	//С чего бы это?
+	AI_Output(other,self, " DIA_Biff_LI_ChangeCourse_01_00 " );	// We must return to Khorinis again.
+	AI_Output(self,other, " DIA_Biff_LI_ChangeCourse_01_01 " );	// Why would it?
 	Info_ClearChoices(dia_biff_li_changecourse);
-	Info_AddChoice(dia_biff_li_changecourse,"Мне надо срочно поговорить с паладинами.",dia_biff_li_changecourse_ore);
-	Info_AddChoice(dia_biff_li_changecourse,"Дело касается золота.",dia_biff_li_changecourse_gold);
+	Info_AddChoice(dia_biff_li_changecourse, " I need to speak to the paladins urgently. " ,dia_biff_li_changecourse_ore);
+	Info_AddChoice(dia_biff_li_changecourse, " It's about gold. " ,dia_biff_li_changecourse_gold);
 };
 
 func void dia_biff_li_changecourse_ore()
 {
 	B_GivePlayerXP(50);
-	AI_Output(other,self,"DIA_Biff_LI_ChangeCourse_Ore_01_01");	//Мне необходимо срочно поговорить с лордом Хагеном!
-	AI_Output(self,other,"DIA_Biff_LI_ChangeCourse_Ore_01_02");	//Это еще зачем?
-	AI_Output(other,self,"DIA_Biff_LI_ChangeCourse_Ore_01_03");	//Необходимо сообщить паладинам о магической руде, которую мы нашли на этом острове.
-	AI_Output(other,self,"DIA_Biff_LI_ChangeCourse_Ore_01_04");	//Эта новость должна помочь им в войне против орков.
-	AI_Output(self,other,"DIA_Biff_LI_ChangeCourse_Ore_01_05");	//Не думаю, что это должно нас так сильно волновать.
-	AI_Output(self,other,"DIA_Biff_LI_ChangeCourse_Ore_01_07");	//Пускай паладины сами разбираются с этими мерзкими тварями, а нам лучше держаться в стороне от всего этого.
-	AI_Output(self,other,"DIA_Biff_LI_ChangeCourse_Ore_01_09");	//У меня нет ни малейшего желания помогать паладинам!
-	B_LogEntry(TOPIC_CHANGECOURSE,"Наемник Бифф против того, чтобы вернуться в Хоринис и помочь паладинам.");
+	AI_Output(other,self, " DIA_Biff_LI_ChangeCourse_Ore_01_01 " );	// I need to speak to Lord Hagen urgently!
+	AI_Output(self,other, " DIA_Biff_LI_ChangeCourse_Ore_01_02 " );	// Why else?
+	AI_Output(other,self, " DIA_Biff_LI_ChangeCourse_Ore_01_03 " );	// We need to inform the paladins about the magical ore we found on this island.
+	AI_Output(other,self, " DIA_Biff_LI_ChangeCourse_Ore_01_04 " );	// This news should help them in their war against the orcs.
+	AI_Output(self,other, " DIA_Biff_LI_ChangeCourse_Ore_01_05 " );	// I don't think this should worry us that much.
+	AI_Output(self,other, " DIA_Biff_LI_ChangeCourse_Ore_01_07 " );	// Let the paladins deal with these vile creatures, we'd better stay out of it.
+	AI_Output(self,other, " DIA_Biff_LI_ChangeCourse_Ore_01_09 " );	// I have no desire to help paladins!
+	B_LogEntry( TOPIC_CHANGECOURSE , " Biff the mercenary is opposed to returning to Khorinis and helping the paladins. " );
 	COUNTPEOPLEDECIDEPRG = COUNTPEOPLEDECIDEPRG + 1;
 	if(COUNTPEOPLEDECIDEPRG >= COUNTPEOPLEDECIDE)
 	{
 		READYCHANGECOURSE = TRUE;
-		B_LogEntry(TOPIC_CHANGECOURSE,"Кажется, я поговорил со всеми парнями. Теперь стоит сообщить об этом нашему капитану.");
+		B_LogEntry( TOPIC_CHANGECOURSE , " I think I've talked to all the guys. Now it's time to tell our captain. " );
 	};
 	Info_ClearChoices(dia_biff_li_changecourse);
 };
@@ -186,19 +187,19 @@ func void dia_biff_li_changecourse_ore()
 func void dia_biff_li_changecourse_gold()
 {
 	B_GivePlayerXP(100);
-	AI_Output(other,self,"DIA_Biff_LI_ChangeCourse_Gold_01_01");	//Дело касается золота.
-	AI_Output(self,other,"DIA_Biff_LI_ChangeCourse_Gold_01_02");	//В самом деле? И каким местом?
-	AI_Output(other,self,"DIA_Biff_LI_ChangeCourse_Gold_01_03");	//Необходимо сообщить паладинам о магической руде, которую мы нашли на этом острове.
-	AI_Output(other,self,"DIA_Biff_LI_ChangeCourse_Gold_01_04");	//Уверен, что было бы неплохо заработать на всем этом деле. Что скажешь?
-	AI_Output(self,other,"DIA_Biff_LI_ChangeCourse_Gold_01_05");	//Отличная идея, приятель!
+	AI_Output(other,self, " DIA_Biff_LI_ChangeCourse_Gold_01_01 " );	// It's about gold.
+	AI_Output(self,other, " DIA_Biff_LI_ChangeCourse_Gold_01_02 " );	// Really? And what place?
+	AI_Output(other,self, " DIA_Biff_LI_ChangeCourse_Gold_01_03 " );	// We need to inform the paladins about the magical ore we found on this island.
+	AI_Output(other,self, " DIA_Biff_LI_ChangeCourse_Gold_01_04 " );	// I'm sure it would be nice to make money on this whole thing. What do you say?
+	AI_Output(self,other, " DIA_Biff_LI_ChangeCourse_Gold_01_05 " );	// Great idea, mate!
 	AI_Output(self,other,"DIA_Biff_LI_ChangeCourse_Gold_01_07");	//Я с тобой!
-	B_LogEntry(TOPIC_CHANGECOURSE,"Наемник Бифф не против вернуться обратно в Хоринис.");
+	B_LogEntry( TOPIC_CHANGECOURSE , " Mercenary Biff doesn't mind going back to Khorinis. " );
 	CREWAGREEAWAYBACKSELL = CREWAGREEAWAYBACKSELL + 1;
 	COUNTPEOPLEDECIDEPRG = COUNTPEOPLEDECIDEPRG + 1;
 	if(COUNTPEOPLEDECIDEPRG >= COUNTPEOPLEDECIDE)
 	{
 		READYCHANGECOURSE = TRUE;
-		Log_AddEntry(TOPIC_CHANGECOURSE,"Кажется, я поговорил со всеми парнями. Пора сообщить об этом нашему капитану.");
+		Log_AddEntry( TOPIC_CHANGECOURSE , " I think I've talked to all the guys. Time to tell our captain. " );
 	};
 	Info_ClearChoices(dia_biff_li_changecourse);
 };
