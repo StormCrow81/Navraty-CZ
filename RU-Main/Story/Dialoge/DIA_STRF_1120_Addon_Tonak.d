45a1d46efb46689349765c5cb3ee8401153bc8b2
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 18:31:45 +0200

    Update DIA_STRF_1120_Addon_Tonak.d

--- RU-Main/Story/Dialoge/DIA_STRF_1120_Addon_Tonak.d
+++ RU-Main/Story/Dialoge/DIA_STRF_1120_Addon_Tonak.d
@@ -1,7 +1,8 @@
 
+
 instance DIA_Addon_Tonak_EXIT(C_Info)
 {
-	npc = STRF_1120_Addon_Tonak;
+	npc = STRF_1120_Addon_Tons;
 	nr = 999;
 	condition = DIA_Addon_Tonak_EXIT_Condition;
 	information = DIA_Addon_Tonak_EXIT_Info;
@@ -23,7 +24,7 @@ func void DIA_Addon_Tonak_EXIT_Info()
 
 instance DIA_Addon_Tonak_Hi(C_Info)
 {
-	npc = STRF_1120_Addon_Tonak;
+	npc = STRF_1120_Addon_Tons;
 	nr = 2;
 	condition = DIA_Addon_Tonak_Hi_Condition;
 	information = DIA_Addon_Tonak_Hi_Info;
@@ -34,16 +35,16 @@ instance DIA_Addon_Tonak_Hi(C_Info)
 
 func int DIA_Addon_Tonak_Hi_Condition()
 {
-	if(Npc_IsInState(self,ZS_Talk) && (Sklaven_Flucht == FALSE))
+	if (Npc_IsInState(self,ZS_Talk) && (slave_escape ==  FALSE ))
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_Addon_Tonak_Hi_Info()
 {
-	AI_Output(self,other,"DIA_Addon_Tonak_Hi_03_00");	//Что тебе нужно? Я - всего лишь пленный рабочий. Я не сделал ничего плохого.
+	AI_Output(self,other, " DIA_Addon_Tonak_Hi_03_00 " );	// What do you need? I am just a captive worker. I didn't do anything wrong.
 	AI_StopProcessInfos(self);
 };
 
