51e16c5bf5ab0ebfaa8f108b914b52a1a02250c8
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 23:34:49 +0200

    Update DIA_VLK_444_Jack_DI.d

--- RU-Main/Story/Dialoge/DIA_VLK_444_Jack_DI.d
+++ RU-Main/Story/Dialoge/DIA_VLK_444_Jack_DI.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_Jack_DI_EXIT(C_Info)
 {
 	npc = VLK_444_Jack_DI;
@@ -21,7 +22,7 @@ func void DIA_Jack_DI_EXIT_Info()
 };
 
 
-instance DIA_Jack_DI_GREET(C_Info)
+instance DIA_Jack_DI_GREET (C_Info)
 {
 	npc = VLK_444_Jack_DI;
 	nr = 4;
@@ -47,22 +48,22 @@ func void DIA_Jack_DI_GREET_Info()
 {
 	if(ORkSturmDI == FALSE)
 	{
-		AI_Output(self,other,"DIA_Jack_DI_GREET_14_00");	//Надеюсь, ты знаешь, что делаешь, приятель.
-		AI_Output(other,self,"DIA_Jack_DI_GREET_15_01");	//На это не уйдет много времени.
-		AI_Output(self,other,"DIA_Jack_DI_GREET_14_02");	//Будь осторожен, и возвращайся назад целым и невредимым.
+		AI_Output(self,other, " DIA_Jack_DI_GREET_14_00 " );	// I hope you know what you're doing, mate.
+		AI_Output(other, self, " DIA_Jack_DI_GREET_15_01 " );	// This won't take long.
+		AI_Output(self,other, " DIA_Jack_DI_GREET_14_02 " );	// Be careful, and come back safe and sound.
 		AI_StopProcessInfos(self);
 	}
 	else
 	{
 		if(DIA_Jack_DI_GREET_OneTime == FALSE)
 		{
-			AI_Output(other,self,"DIA_Jack_DI_GREET_15_03");	//Что ты здесь делаешь?
+			AI_Output(other,self, " DIA_Jack_DI_GREET_15_03 " );	// What are you doing here?
 			B_GivePlayerXP(XP_Ambient);
 			DIA_Jack_DI_GREET_OneTime = TRUE;
 		};
-		AI_Output(self,other,"DIA_Jack_DI_GREET_14_04");	//Если на нас опять обрушится этот ад, я уведу корабль отсюда. С тобой или без тебя! Ты слышал?
-		AI_Output(other,self,"DIA_Jack_DI_GREET_15_05");	//Не болтай ерунды. Я со всем справлюсь.
-		AI_Output(self,other,"DIA_Jack_DI_GREET_14_06");	//Тогда поторопись.
+		AI_Output(self,other, " DIA_Jack_DI_GREET_14_04 " );	// If this hell falls on us again, I'll get the ship out of here. With you or without you! You heard?
+		AI_Output(other,self, " DIA_Jack_DI_GREET_15_05 " );	// Don't talk nonsense. I can handle everything.
+		AI_Output(self,other, " DIA_Jack_DI_GREET_14_06 " );	// Then hurry up.
 		AI_StopProcessInfos(self);
 		Npc_ExchangeRoutine(self,"Start");
 	};
@@ -75,7 +76,7 @@ instance DIA_Jack_DI_UndeadDragonDead(C_Info)
 	condition = DIA_Jack_DI_UndeadDragonDead_Condition;
 	information = DIA_Jack_DI_UndeadDragonDead_Info;
 	permanent = TRUE;
-	description = "Я сделал это. Я уничтожил врага!";
+	description = " I did it. I destroyed the enemy! " ;
 };
 
 func int DIA_Jack_DI_UndeadDragonDead_Condition()
@@ -88,25 +89,25 @@ func int DIA_Jack_DI_UndeadDragonDead_Condition()
 
 func void DIA_Jack_DI_UndeadDragonDead_Info()
 {
-	AI_Output(other,self,"DIA_Jack_DI_UndeadDragonDead_15_00");	//Я сделал это. Я уничтожил врага!
-	AI_Output(self,other,"DIA_Jack_DI_UndeadDragonDead_14_01");	//Я надеялся, что все кончится хорошо. Но, должен признать, последние несколько часов я не был так уверен.
-	AI_Output(self,other,"DIA_Jack_DI_UndeadDragonDead_14_02");	//Мы можем наконец поднять паруса?
+	AI_Output(other,self, " DIA_Jack_DI_UndeadDragonDead_15_00 " );	// I did it. I destroyed the enemy!
+	AI_Output(self,other, " DIA_Jack_DI_UndeadDragonDead_14_01 " );	// I hoped everything would end well. But I have to admit, I haven't been so sure the last few hours.
+	AI_Output(self,other, " DIA_Jack_DI_UndeadDragonDead_14_02 " );	// Can we finally set sail?
 	Info_ClearChoices(DIA_Jack_DI_UndeadDragonDead);
-	Info_AddChoice(DIA_Jack_DI_UndeadDragonDead,"Подожди немного.",DIA_Jack_DI_UndeadDragonDead_moment);
-	Info_AddChoice(DIA_Jack_DI_UndeadDragonDead,"Да. Закончим с этим.",DIA_Jack_DI_UndeadDragonDead_over);
+	Info_AddChoice(DIA_Jack_DI_UndeadDragonDead, " Wait a moment " ,DIA_Jack_DI_UndeadDragonDead_moment);
+	Info_AddChoice(DIA_Jack_DI_UndeadDragonDead, " Yes. Let's finish this. " ,DIA_Jack_DI_UndeadDragonDead_over);
 };
 
 func void DIA_Jack_DI_UndeadDragonDead_moment()
 {
-	AI_Output(other,self,"DIA_Jack_DI_UndeadDragonDead_moment_15_00");	//Подожди немного. Я еще кое-что должен сделать.
-	AI_Output(self,other,"DIA_Jack_DI_UndeadDragonDead_moment_14_01");	//Тогда поторопись. Я не хочу подхватить здесь цингу.
+	AI_Output(other,self, " DIA_Jack_DI_UndeadDragonDead_moment_15_00 " );	// Wait a moment. There is one more thing I must do.
+	AI_Output(self,other, " DIA_Jack_DI_UndeadDragonDead_moment_14_01 " );	// Then hurry up. I don't want to catch scurvy here.
 	AI_StopProcessInfos(self);
 };
 
 func void DIA_Jack_DI_UndeadDragonDead_over()
 {
 	CAPITANORDERDIAWAY = TRUE;
-	AI_Output(other,self,"DIA_Jack_DI_UndeadDragonDead_over_15_00");	//Да! Давай отплывать.
-	AI_Output(self,other,"DIA_Jack_DI_UndeadDragonDead_over_14_01");	//Если ты уверен, что у тебя все есть, тогда иди в каюту капитана и поспи там. Тебе это понадобиться.
+	AI_Output(other,self, " DIA_Jack_DI_UndeadDragonDead_over_15_00 " );	// Yes! Let's sail away.
+	AI_Output(self,other, " DIA_Jack_DI_UndeadDragonDead_over_14_01 " );	// If you're sure you've got everything, then go to the captain's quarters and sleep there. You will need it.
 	AI_StopProcessInfos(self);
 };
