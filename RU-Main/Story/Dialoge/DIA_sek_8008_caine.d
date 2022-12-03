c9fdd4f038e03ea9400d21c0292e56f536371366
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 10:18:33 +0200

    Update DIA_sek_8008_caine.d

--- RU-Main/Story/Dialoge/DIA_sek_8008_caine.d
+++ RU-Main/Story/Dialoge/DIA_sek_8008_caine.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_CAINE_EXIT(C_Info)
 {
 	npc = sek_8008_caine;
@@ -32,7 +33,7 @@ instance dia_caine_PICKPOCKET(C_Info)
 
 func int dia_caine_PICKPOCKET_Condition()
 {
-	return C_Beklauen(20,43);
+	return  C_Robbery ( 20 , 43 );
 };
 
 func void dia_caine_PICKPOCKET_Info()
@@ -44,7 +45,7 @@ func void dia_caine_PICKPOCKET_Info()
 
 func void dia_caine_PICKPOCKET_DoIt()
 {
-	B_Beklauen();
+	B_Robbery();
 	Info_ClearChoices(dia_caine_PICKPOCKET);
 };
 
@@ -61,7 +62,7 @@ instance DIA_CAINE_WHATYOUDO(C_Info)
 	information = dia_caine_whatyoudo_info;
 	permanent = FALSE;
 	important = FALSE;
-	description = "Чем ты занимаешься?";
+	description = " What are you doing? " ;
 };
 
 
@@ -75,8 +76,8 @@ func int dia_caine_whatyoudo_condition()
 
 func void dia_caine_whatyoudo_info()
 {
-	AI_Output(other,self,"DIA_Caine_WhatYouDo_01_00");	//Чем ты занимаешься?
-	AI_Output(self,other,"DIA_Caine_WhatYouDo_01_01");	//Мы собираем болотник. После того как наши братья закончат переработку, его можно будет курить.
+	AI_Output(other,self, " DIA_Caine_WhatYouDo_01_00 " );	// What are you doing?
+	AI_Output(self,other, " DIA_Caine_WhatYouDo_01_01 " );	// We collect the swamp. After our brothers finish processing, it can be smoked.
 };
 
 
@@ -88,7 +89,7 @@ instance DIA_CAINE_CAINSENTTOCADAR(C_Info)
 	information = dia_caine_cainsenttocadar_info;
 	permanent = FALSE;
 	important = FALSE;
-	description = "Я хотел бы присоединиться к вашему Лагерю. Ты мог бы мне помочь?";
+	description = " I would like to join your Camp. Could you help me? " ;
 };
 
 
@@ -102,13 +103,13 @@ func int dia_caine_cainsenttocadar_condition()
 
 func void dia_caine_cainsenttocadar_info()
 {
-	AI_Output(other,self,"DIA_Caine_CainSentToCadar_01_00");	//Я хотел бы присоединиться к вашему лагерю. Ты мог бы мне помочь?
-	AI_Output(self,other,"DIA_Caine_CainSentToCadar_01_01");	//Ты хочешь присоединиться к нам? Хорошо!
-	AI_Output(self,other,"DIA_Caine_CainSentToCadar_01_02");	//Ты, должно быть, заметил, что никто из Гуру не хочет с тобой разговаривать.
-	AI_Output(self,other,"DIA_Caine_CainSentToCadar_01_03");	//Чтобы добиться своего, нужно произвести на них впечатление!
-	AI_Output(other,self,"DIA_Caine_CainSentToCadar_01_04");	//Как произвести впечатление на Гуру?
-	AI_Output(self,other,"DIA_Caine_CainSentToCadar_01_05");	//Просто постарайся быть полезным.
-	AI_Output(other,self,"DIA_Caine_CainSentToCadar_01_06");	//Благодарю за совет.
+	AI_Output(other,self, " DIA_Caine_CainSentToCadar_01_00 " );	// I would like to join your camp. Could you help me?
+	AI_Output(self,other, " DIA_Caine_CainSentToCadar_01_01 " );	// Do you want to join us? Good!
+	AI_Output(self,other, " DIA_Caine_CainSentToCadar_01_02 " );	// You must have noticed that none of the Guru wants to talk to you.
+	AI_Output(self,other, " DIA_Caine_CainSentToCadar_01_03 " );	// To get your way, you need to impress them!
+	AI_Output(other,self, " DIA_Caine_CainSentToCadar_01_04 " );	// How to impress Guru?
+	AI_Output(self,other, " DIA_Caine_CainSentToCadar_01_05 " );	// Just try to be helpful.
+	AI_Output(other,self, " DIA_Caine_CainSentToCadar_01_06 " );	// Thanks for the advice.
 };
 
 
@@ -125,7 +126,7 @@ instance DIA_CAINE_SENTPLANTSTOCADAR(C_Info)
 
 func int dia_caine_sentplantstocadar_condition()
 {
-	if((other.guild == GIL_SEK) && (PSI_TALK == TRUE) && Npc_KnowsInfo(hero,dia_caine_cainsenttocadar) && Wld_IsTime(8,0,21,0))
+	if ((other.guild ==  GIL_SEK ) && ( PSI_TALK  ==  TRUE ) && Npc_KnowsInfo(hero, his_chain_is_not_in_cadar ) && Wld_IsTime( 8 , 0 , 21 , 0 )) ;
 	{
 		return TRUE;
 	};
@@ -133,19 +134,19 @@ func int dia_caine_sentplantstocadar_condition()
 
 func void dia_caine_sentplantstocadar_info()
 {
-	AI_Output(self,other,"DIA_Caine_SentPlantsToCadar_01_00");	//Постой-ка... Да, думаю, что я смогу тебе помочь.
-	AI_Output(self,other,"DIA_Caine_SentPlantsToCadar_01_01");	//Сделай мне одолжение.
-	AI_Output(other,self,"DIA_Caine_SentPlantsToCadar_01_02");	//Что я должен сделать?
-	AI_Output(self,other,"DIA_Caine_SentPlantsToCadar_01_03");	//Будь добр, отнеси этот сбор болотника Идолу Кадару. Наверняка он его уже заждался.
-	AI_Output(other,self,"DIA_Caine_SentPlantsToCadar_01_04");	//А почему ты сам это не сделаешь?
-	AI_Output(self,other,"DIA_Caine_SentPlantsToCadar_01_05");	//Я же тебе сказал... Чтобы произвести впечатление на Гуру, ты должен постараться быть полезным. Понимаешь?
-	AI_Output(self,other,"DIA_Caine_SentPlantsToCadar_01_06");	//Считай, что я даю тебе шанс проявить себя перед Гуру.
-	AI_Output(other,self,"DIA_Caine_SentPlantsToCadar_01_07");	//Хорошо, я передам ему этот болотник.
-	AI_Output(self,other,"DIA_Caine_SentPlantsToCadar_01_08");	//Отлично! Только не тяни с этим.
+	AI_Output(self,other, " DIA_Caine_SentPlantsToCadar_01_00 " );	// Wait a minute... Yes, I think I can help you.
+	AI_Output(self,other, " DIA_Caine_SentPlantsToCadar_01_01 " );	// Do me a favor.
+	AI_Output(other,self, " DIA_Caine_SentPlantsToCadar_01_02 " );	// What should I do?
+	AI_Output(self,other, " DIA_Caine_SentPlantsToCadar_01_03 " );	// Kindly take this collection of bogweed to Idol Kadar. He must have been waiting for it.
+	AI_Output(other,self, " DIA_Caine_SentPlantsToCadar_01_04 " );	// Why don't you do it yourself?
+	AI_Output(self,other, " DIA_Caine_SentPlantsToCadar_01_05 " );	// I told you... In order to impress the Guru, you must try to be helpful. Understand?
+	AI_Output(self,other, " DIA_Caine_SentPlantsToCadar_01_06 " );	// Consider me giving you a chance to prove yourself to the Guru.
+	AI_Output(other,self, " DIA_Caine_SentPlantsToCadar_01_07 " );	// Okay, I'll give him this swamp.
+	AI_Output(self,other, " DIA_Caine_SentPlantsToCadar_01_08 " );	// Great! Just don't mess with it.
 	B_GiveInvItems(self,other,ItPl_SwampHerb,50);
 	Log_CreateTopic(TOPIC_CAINSENTTOCADAR,LOG_MISSION);
 	Log_SetTopicStatus(TOPIC_CAINSENTTOCADAR,LOG_Running);
-	B_LogEntry(TOPIC_CAINSENTTOCADAR,"Каин попросил меня отнести очередной сбор болотника Идолу Кадару. Таким образом, я смогу быть полезным Братству.");
+	B_LogEntry( TOPIC_CAINSENTTOCADAR , " Cain asked me to take another collection of bogweed to Idol Kadar. That way I can be of service to the Brotherhood. " );
 	MIS_CAINSENTTOCADAR = LOG_Running;
 };
 
@@ -158,7 +159,7 @@ instance DIA_CAINE_SENTPLANTSTOCADAROK(C_Info)
 	information = dia_caine_sentplantstocadarok_info;
 	permanent = FALSE;
 	important = FALSE;
-	description = "Я отнес твой болотник.";
+	description = " I've taken your swamp. " ;
 };
 
 
@@ -173,10 +174,10 @@ func int dia_caine_sentplantstocadarok_condition()
 func void dia_caine_sentplantstocadarok_info()
 {
 	B_GivePlayerXP(50);
-	AI_Output(other,self,"DIA_Caine_SentPlantsToCadarOk_01_00");	//Я отнес твой болотник.
+	AI_Output(other,self, " DIA_Caine_SentPlantsToCadarOk_01_00 " );	// I took your swamp.
 	AI_Output(self,other,"DIA_Caine_SentPlantsToCadarOk_01_01");	//И?
-	AI_Output(other,self,"DIA_Caine_SentPlantsToCadarOk_01_02");	//Идол Кадар заговорил со мной и дал мне одно поручение.
-	AI_Output(self,other,"DIA_Caine_SentPlantsToCadarOk_01_03");	//Вот видишь! Продолжай в том же духе, и скоро ты наверняка станешь одним из нас.
+	AI_Output(other,self, " DIA_Caine_SentPlantsToCadarOk_01_02 " );	// Idol Kadar spoke to me and gave me one task.
+	AI_Output(self,other, " DIA_Caine_SentPlantsToCadarOk_01_03 " );	// You see! Keep up the good work, and soon you will surely become one of us.
 };
 
 
@@ -188,7 +189,7 @@ instance DIA_CAINE_HOWWORK(C_Info)
 	information = dia_caine_howwork_info;
 	permanent = TRUE;
 	important = FALSE;
-	description = "Как сбор урожая?";
+	description = " How is harvesting? " ;
 };
 
 
@@ -202,25 +203,25 @@ func int dia_caine_howwork_condition()
 
 func void dia_caine_howwork_info()
 {
-	AI_Output(other,self,"DIA_Caine_HowWork_01_00");	//Как сбор урожая?
-	AI_Output(self,other,"DIA_Caine_HowWork_01_01");	//Да-да, смейся, смейся. Уверен, тебе Гуру доверяют работенку получше.
-	AI_Output(self,other,"DIA_Caine_HowWork_01_02");	//По крайней мере, мне не приходится горбатиться самому.
+	AI_Output(other,self, " DIA_Caine_HowWork_01_00 " );	// How is the harvest?
+	AI_Output(self,other, " DIA_Caine_HowWork_01_01 " );	// Yes, yes, laugh, laugh. I'm sure the Gurus entrust you with a better job.
+	AI_Output(self,other, " DIA_Caine_HowWork_01_02 " );	// At least I don't have to hump myself.
 };
 
-instance DIA_CAINE_SEKTEHEILEN(C_Info)
+instance DIA_CAINE_SEKTEHEILEN (C_Info)
 {
 	npc = sek_8008_caine;
 	nr = 1;
 	condition = dia_caine_sekteheilen_condition;
 	information = dia_caine_sekteheilen_info;
 	permanent = FALSE;
-	description = "Выпей напиток! Он помогает от головной боли.";
+	description = " Drink a drink! It helps with a headache. " ;
 };
 
 
 func int dia_caine_sekteheilen_condition()
 {
-	if((Npc_HasItems(other,ItPo_HealObsession_MIS) > 0) && (MIS_SEKTEHEILEN == LOG_Running) && Npc_KnowsInfo(hero,dia_baalorun_sekteheilengot))
+	if ((Npc_HasItems(other,ItPo_Heal_Obsession_MY) >  0 ) && ( MY_SECTS_HEALTH  == LOG_Running) && Npc_KnowsInfo(hero, dia_heal_sectarian_obsession));
 	{
 		return TRUE;
 	};
@@ -229,10 +230,10 @@ func int dia_caine_sekteheilen_condition()
 func void dia_caine_sekteheilen_info()
 {
 	B_GivePlayerXP(50);
-	AI_Output(other,self,"DIA_Caine_SekteHeilen_01_00");	//Выпей напиток! Он помогает от головной боли.
+	AI_Output(other,self, " DIA_Caine_SekteHeilen_01_00 " );	// Have a drink! It helps with headaches.
 	B_GiveInvItems(other,self,ItPo_HealObsession_MIS,1);
 	B_UseItem(self,ItPo_HealObsession_MIS);
-	SEKTEHEILENCOUNT = SEKTEHEILENCOUNT + 1;
-	AI_Output(self,other,"DIA_Caine_SekteHeilen_01_01");	//Мне уже стало лучше!
-	AI_Output(self,other,"DIA_Caine_SekteHeilen_01_02");	//Спасибо тебе!
+	SECTEHEILENCOUNT = SECTEHEILENCOUNT  +  1 ;
+	AI_Output(self,other, " DIA_Caine_SekteHeilen_01_01 " );	// I already feel better!
+	AI_Output(self,other, " DIA_Caine_SekteHeilen_01_02 " );	// Thank you!
 };
