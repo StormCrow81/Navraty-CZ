4cafbbceb9b9e075c677ebadc06ad951a89b5049
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 22:21:08 +0200

    Update DIA_gomezworkersoul_10.d

--- RU-Main/Story/Dialoge/DIA_gomezworkersoul_10.d
+++ RU-Main/Story/Dialoge/DIA_gomezworkersoul_10.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_GOMEZWORKERSOUL_100_EXIT(C_Info)
 {
 	npc = gomezworkersoul_10;
@@ -21,12 +22,12 @@ func void dia_gomezworkersoul_10_exit_info()
 };
 
 
-instance DIA_GOMEZWORKERSOUL_10_HALLO(C_Info)
+instance DIA_GOMEZWORKERSOUL_10_HELLO (C_Info)
 {
 	npc = gomezworkersoul_10;
 	nr = 1;
 	condition = dia_gomezworkersoul_10_hallo_condition;
-	information = dia_gomezworkersoul_10_hallo_info;
+	information = dia_gomezworkersoul_10_hello_info;
 	permanent = FALSE;
 	important = TRUE;
 };
@@ -40,27 +41,27 @@ func int dia_gomezworkersoul_10_hallo_condition()
 	};
 };
 
-func void dia_gomezworkersoul_10_hallo_info()
+func void dia_gomezworkersoul_10_hello_info()
 {
-	self.aivar[AIV_EnemyOverride] = FALSE;
+	self.aivar[AIV_EnemyOverride] = FALSE ;
 	Snd_Play("MFX_FEAR_CAST");
 	B_GivePlayerXP(50);
-	AI_Output(self,other,"DIA_GomezWorkerSoul_10_HALLO_01_00");	//(мертвым голосом) ЧЕЛОВЕК? ЗАЧЕМ ТЫ ПРИШЕЛ СЮДА?
-	AI_Output(other,self,"DIA_GomezWorkerSoul_10_HALLO_01_01");	//Я тут по поручению Гомеза, чтобы навеки упокоить твою душу.
-	AI_Output(self,other,"DIA_GomezWorkerSoul_10_HALLO_01_02");	//Аргххх...
-	AI_Output(other,self,"DIA_GomezWorkerSoul_10_HALLO_01_05");	//Но перед тем как я освобожу твою душу от проклятия, мне нужно, чтобы ты даровал свое прощение душе рудного барона.
-	AI_Output(self,other,"DIA_GomezWorkerSoul_10_HALLO_01_08");	//Ха ха ха...
-	AI_Output(self,other,"DIA_GomezWorkerSoul_10_HALLO_01_09");	//Значит, этот Гомез все-таки поплатился за свои злодеяния и его душа была также проклята богами.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_10_HALLO_01_00 " );	// (dead voice) HUMAN? WHY HAVE YOU COME HERE?
+	AI_Output(other,self, " DIA_GomezWorkerSoul_10_HALLO_01_01 " );	// I'm here on behalf of Gomez to put your soul to rest forever.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_10_HALLO_01_02 " );	// Arghhh...
+	AI_Output(other,self, " DIA_GomezWorkerSoul_10_HALLO_01_05 " );	// But before I release your soul from the curse, I need you to grant your forgiveness to the Ore Baron's soul.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_10_HALLO_01_08 " );	// Ha ha ha...
+	AI_Output(self,other, " DIA_GomezWorkerSoul_10_HALLO_01_09 " );	// So, this Gomez still paid for his atrocities and his soul was also cursed by the gods.
 	Info_ClearChoices(dia_gomezworkersoul_10_hallo);
-	Info_AddChoice(dia_gomezworkersoul_10_hallo,"Так ты дашь ему это прощение?",dia_gomezworkersoul_10_hallo_end);
+	Info_AddChoice(dia_gomezworkersoul_10_hallo, " So will you give him this forgiveness? " ,dia_gomezworkersoul_10_hallo_end);
 };
 
-func void dia_gomezworkersoul_10_hallo_end()
+func void dia_gomezworkersoul_10_hello_end()
 {
 	Snd_Play("SFX_INNOSEYE");
-	AI_Output(other,self,"DIA_GomezWorkerSoul_10_HALLO_End_01_01");	//Так ты дашь ему это прощение?
-	AI_Output(self,other,"DIA_GomezWorkerSoul_10_HALLO_End_01_02");	//Хорошо... Считай, что он прощен.
-	AI_Output(self,other,"DIA_GomezWorkerSoul_10_HALLO_End_01_03");	//А теперь - освободи мою душу! Аргххх...
+	AI_Output(other,self, " DIA_GomezWorkerSoul_10_HALLO_End_01_01 " );	// So will you give him this forgiveness?
+	AI_Output(self,other, " DIA_GomezWorkerSoul_10_HALLO_End_01_02 " );	// Good... Consider him forgiven.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_10_HALLO_End_01_03 " );	// And now - free my soul! Arghhh...
 	GOMEZSOULTALK_10 = TRUE;
 	AI_StopProcessInfos(self);
 };
