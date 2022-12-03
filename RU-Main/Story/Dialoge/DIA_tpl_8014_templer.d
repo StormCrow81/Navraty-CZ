15dd196c53c6d39929681611a415a456ff7ed31d
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 09:20:39 +0200

    Update DIA_tpl_8014_templer.d

--- RU-Main/Story/Dialoge/DIA_tpl_8014_templer.d
+++ RU-Main/Story/Dialoge/DIA_tpl_8014_templer.d
@@ -1,3 +1,4 @@
+
 var int FirstMeet;
 
 instance DIA_TPL_8014_TEMPLER_EXIT(C_Info)
@@ -43,32 +44,32 @@ func int dia_tpl_8014_templer_first_condition()
 
 func void dia_tpl_8014_templer_first_info()
 {
-	AI_Output(self,other,"DIA_TPL_8014_Templer_First_13_00");	//Привет, незнакомец!
-	AI_Output(self,other,"DIA_TPL_8014_Templer_First_13_01");	//То, что ты видишь перед собой - новый лагерь нашего Братства.
-	AI_Output(self,other,"DIA_TPL_8014_Templer_First_13_02");	//Что привело тебя сюда?
+	AI_Output(self,other, " DIA_TPL_8014_Templer_First_13_00 " );	// Hello stranger!
+	AI_Output(self,other, " DIA_TPL_8014_Templer_First_13_01 " );	// What you see before you is the new camp of our Brotherhood.
+	AI_Output(self,other, " DIA_TPL_8014_Templer_First_13_02 " );	// What brings you here?
 	FirstMeet = TRUE;
 	Info_ClearChoices(dia_tpl_8014_templer_first);
-	Info_AddChoice(dia_tpl_8014_templer_first,"Я хочу осмотреть Лагерь.",dia_tpl_8014_templer_first_justlooking);
+	Info_AddChoice(dia_tpl_8014_templer_first, " I want to look around the Camp. " ,dia_tpl_8014_templer_first_justlooking);
 	if(other.guild == GIL_NONE)
 	{
-		Info_AddChoice(dia_tpl_8014_templer_first,"Я хочу присоединиться к вам.",dia_tpl_8014_templer_first_join);
+		Info_AddChoice(dia_tpl_8014_templer_first, " I want to join you. " ,dia_tpl_8014_templer_first_join);
 	};
 };
 
 func void dia_tpl_8014_templer_first_join()
 {
-	AI_Output(other,self,"DIA_TPL_8014_Templer_First_Join_15_00");	//Я слышал о том, что вам нужны люди. Я хочу присоединиться к вам.
-	AI_Output(self,other,"DIA_TPL_8014_Templer_First_Join_13_01");	//Добро пожаловать! Братство с радостью примет любого, кто ищет душевного покоя.
-	AI_Output(self,other,"DIA_TPL_8014_Templer_First_Join_13_02");	//Если твои намерения серьезны, постарайся обратить на себя внимание Идола Намиба. Ты найдешь его во дворе за воротами.
-	AI_Output(self,other,"DIA_TPL_8014_Templer_First_Join_13_04");	//Но ты не должен обращаться к нему до тех пор, пока он сам не заговорит с тобой.
+	AI_Output(other,self, " DIA_TPL_8014_Templer_First_Join_15_00 " );	// I heard that you need people. I want to join you.
+	AI_Output(self,other, " DIA_TPL_8014_Templer_First_Join_13_01 " );	// Welcome! The Brotherhood will gladly accept anyone who seeks peace of mind.
+	AI_Output(self,other, " DIA_TPL_8014_Templer_First_Join_13_02 " );	// If your intentions are serious, try to attract the attention of the Namib Idol. You will find him in the courtyard behind the gate.
+	AI_Output(self,other, " DIA_TPL_8014_Templer_First_Join_13_04 " );	// But you must not speak to him until he himself speaks to you.
 	Info_ClearChoices(dia_tpl_8014_templer_first);
 };
 
 func void dia_tpl_8014_templer_first_justlooking()
 {
-	AI_Output(other,self,"DIA_TPL_8014_Templer_First_JustLooking_15_00");	//Я хочу осмотреть Лагерь.
-	AI_Output(self,other,"DIA_TPL_8014_Templer_First_JustLooking_13_01");	//Заходи, друг! Мы всегда рады гостям.
-	AI_Output(self,other,"DIA_TPL_8014_Templer_First_JustLooking_13_02");	//Но только не создавай здесь проблем и не отвлекай Идолов от работы.
+	AI_Output(other,self, " DIA_TPL_8014_Templer_First_JustLooking_15_00 " );	// I want to inspect the Camp.
+	AI_Output(self,other, " DIA_TPL_8014_Templer_First_JustLooking_13_01 " );	// Come in, friend! We always welcome guests.
+	AI_Output(self,other, " DIA_TPL_8014_Templer_First_JustLooking_13_02 " );	// But just don't create trouble here and don't distract the Idols from their work.
 	Info_ClearChoices(dia_tpl_8014_templer_first);
 };
 
@@ -80,7 +81,7 @@ instance DIA_TPL_8014_TEMPLER_SIT(C_Info)
 	condition = dia_tpl_8014_templer_sit_condition;
 	information = dia_tpl_8014_templer_sit_info;
 	permanent = 1;
-	description = "Как дела в Лагере?";
+	description = " How are you at the Camp? " ;
 };
 
 
@@ -94,8 +95,8 @@ func int dia_tpl_8014_templer_sit_condition()
 
 func void dia_tpl_8014_templer_sit_info()
 {
-	AI_Output(other,self,"DIA_TPL_8014_Templer_Sit_15_00");	//Как дела в Лагере?
-	AI_Output(self,other,"DIA_TPL_8014_Templer_Sit_13_01");	//Все тихо. Заходи.
+	AI_Output(other,self, " DIA_TPL_8014_Templer_Sit_15_00 " );	// How are things at the Camp?
+	AI_Output(self,other, " DIA_TPL_8014_Templer_Sit_13_01 " );	// Everything is quiet. Come in.
 };
 
 instance DIA_TPL_8014_TEMPLER_PrioratStart(C_Info)
@@ -111,7 +112,7 @@ instance DIA_TPL_8014_TEMPLER_PrioratStart(C_Info)
 
 func int dia_tpl_8014_templer_PrioratStart_condition()
 {
-	if((STARTBIGBATTLE == FALSE) && (BIGBATTLEBACKPOSITION == FALSE) && (Kapitel >= 3) && (FirstMeet == TRUE))
+	if (( STARTBIGBATTLE  ==  FALSE ) && ( BIGBATTLEBACKPOSITION  ==  FALSE ) && (Chapter >=  3 ) && (FirstMeet ==  TRUE )) ;
 	{
 		return TRUE;
 	};
@@ -123,30 +124,30 @@ func void dia_tpl_8014_templer_PrioratStart_info()
 	{
 		if(hero.guild == GIL_SEK)
 		{
-			AI_Output(self,other,"DIA_TPL_8014_Templer_PrioratStart_01_00");	//Постой, послушник!
-			AI_Output(other,self,"DIA_TPL_8014_Templer_PrioratStart_01_01");	//Что такое?
-			AI_Output(self,other,"DIA_TPL_8014_Templer_PrioratStart_01_02");	//Идол Намиб срочно хотел поговорить с тобой. Поэтому сразу же ступай к нему.
+			AI_Output(self,other, " DIA_TPL_8014_Templer_PrioratStart_01_00 " );	// Wait, novice!
+			AI_Output(other,self, " DIA_TPL_8014_Templer_PrioratStart_01_01 " );	// What is it?
+			AI_Output(self,other, " DIA_TPL_8014_Templer_PrioratStart_01_02 " );	// The Namib Idol wanted to speak to you urgently. Therefore, immediately go to him.
 		}
-		else if(hero.guild == GIL_GUR)
+		else  if (hero.guild ==  GIL_GUR )
 		{
-			AI_Output(self,other,"DIA_TPL_8014_Templer_PrioratStart_01_05");	//Прошу простить меня, мастер...
-			AI_Output(other,self,"DIA_TPL_8014_Templer_PrioratStart_01_06");	//Я слушаю тебя, страж. Что случилось?
-			AI_Output(self,other,"DIA_TPL_8014_Templer_PrioratStart_01_07");	//Ничего, мастер. Просто Идол Намиб попросил меня передать вам, что он срочно хотел вас видеть.
+			AI_Output(self,other, " DIA_TPL_8014_Templer_PrioratStart_01_05 " );	// Please forgive me, master...
+			AI_Output(other,self, " DIA_TPL_8014_Templer_PrioratStart_01_06 " );	// I'm listening to you, guard. What happened?
+			AI_Output(self,other, " DIA_TPL_8014_Templer_PrioratStart_01_07 " );	// Nothing, master. It's just that the Namib Idol asked me to tell you that he wanted to see you urgently.
 		}
 		else if(hero.guild == GIL_TPL)
 		{
-			AI_Output(self,other,"DIA_TPL_8014_Templer_PrioratStart_01_10");	//Постой, брат.
-			AI_Output(other,self,"DIA_TPL_8014_Templer_PrioratStart_01_11");	//Что такое?
-			AI_Output(self,other,"DIA_TPL_8014_Templer_PrioratStart_01_12");	//Идол Намиб срочно хотел поговорить с тобой. Так что лучше сразу иди к нему.
+			AI_Output(self,other, " DIA_TPL_8014_Templer_PrioratStart_01_10 " );	// Wait, brother.
+			AI_Output(other,self, " DIA_TPL_8014_Templer_PrioratStart_01_11 " );	// What is it?
+			AI_Output(self,other, " DIA_TPL_8014_Templer_PrioratStart_01_12 " );	// The Namib Idol wanted to speak to you urgently. So it's best to go straight to him.
 		};
 
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_TPL_8014_Templer_PrioratStart_01_15");	//Постой, незнакомец! Один из наших духовных наставников очень хотел бы поговорить с тобой.
-		AI_Output(other,self,"DIA_TPL_8014_Templer_PrioratStart_01_18");	//И что же ему вдруг понадобилось от меня?
-		AI_Output(self,other,"DIA_TPL_8014_Templer_PrioratStart_01_19");	//Это ты выяснишь у него сам. Глупо полагать, что наши наставники посвящают нас, стражей, во все свои дела.
-		AI_Output(self,other,"DIA_TPL_8014_Templer_PrioratStart_01_21");	//Тебе следует обратиться к Идолу Намибу. Обычно он находится на площадке для медитаций, справа от входа в лагерь.
+		AI_Output(self,other, " DIA_TPL_8014_Templer_PrioratStart_01_15 " );	// Wait, stranger! One of our spirit guides would very much like to speak with you.
+		AI_Output(other,self, " DIA_TPL_8014_Templer_PrioratStart_01_18 " );	// And what did he suddenly need from me?
+		AI_Output(self,other, " DIA_TPL_8014_Templer_PrioratStart_01_19 " );	// You will find out from him yourself. It is foolish to believe that our mentors initiate us, the guards, in all their affairs.
+		AI_Output(self,other, " DIA_TPL_8014_Templer_PrioratStart_01_21 " );	// You should contact the Namib Idol. It is usually located on the meditation platform, to the right of the entrance to the camp.
 	};
 
 	PrioratPreStart = TRUE;
