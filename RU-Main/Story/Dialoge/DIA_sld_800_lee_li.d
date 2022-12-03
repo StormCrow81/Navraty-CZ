4313be4bd6df54f9469cc85845494f8c0c1e5d3a
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 09:46:15 +0200

    Update DIA_sld_800_lee_li.d

--- RU-Main/Story/Dialoge/DIA_sld_800_lee_li.d
+++ RU-Main/Story/Dialoge/DIA_sld_800_lee_li.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_LEE_LI_EXIT(C_Info)
 {
 	npc = sld_800_lee_li;
@@ -39,8 +40,8 @@ func int dia_lee_li_perm6_condition()
 
 func void dia_lee_li_perm6_info()
 {
-	AI_Output(other,self,"DIA_Lee_DI_PERM6_15_00");	//Как мой корабль?
-	AI_Output(self,other,"DIA_Lee_DI_PERM6_04_01");	//Не волнуйся. Я держу все под контролем.
+	AI_Output(other,self, " DIA_Lee_DI_PERM6_15_00 " );	// How is my ship?
+	AI_Output(self,other, " DIA_Lee_DI_PERM6_04_01 " );	// Don't worry. I have everything under control.
 };
 
 
@@ -51,7 +52,7 @@ instance DIA_LEE_LI_TEACH(C_Info)
 	condition = dia_lee_li_teach_condition;
 	information = dia_lee_li_teach_info;
 	permanent = TRUE;
-	description = "Я нуждаюсь в тренировке.";
+	description = " I need a workout. " ;
 };
 
 
@@ -62,8 +63,8 @@ func int dia_lee_li_teach_condition()
 
 func void dia_lee_li_teach_info()
 {
-	AI_Output(other,self,"DIA_Lee_DI_Teach_15_00");	//Я хочу потренироваться.
-	AI_Output(self,other,"DIA_Lee_DI_Teach_04_01");	//Что именно ты хочешь улучшить?
+	AI_Output(other,self, " DIA_Lee_DI_Teach_15_00 " );	// I want to practice.
+	AI_Output(self,other, " DIA_Lee_DI_Teach_04_01 " );	// What exactly do you want to improve?
 	Info_ClearChoices(dia_lee_li_teach);
 	Info_AddChoice(dia_lee_li_teach,Dialog_Back,dia_lee_li_teach_back);
 	Info_AddChoice(dia_lee_li_teach,b_buildlearnstringforfight(PRINT_Learn2h1,B_GetLearnCostTalent(other,NPC_TALENT_2H,1)),dia_lee_li_teach_2h_1);
@@ -81,7 +82,7 @@ func void dia_lee_li_teach_1h_1()
 {
 	if(B_TeachFightTalentPercent(self,other,NPC_TALENT_1H,1,100))
 	{
-		AI_Output(self,other,"DIA_Lee_DI_Teach_1H_1_04_00");	//Твоя защита ужасна, но мы что-нибудь сделаем с этим.
+		AI_Output(self,other, " DIA_Lee_DI_Teach_1H_1_04_00 " );	// Your defense is terrible, but we'll do something about it.
 	};
 	Info_ClearChoices(dia_lee_li_teach);
 	Info_AddChoice(dia_lee_li_teach,Dialog_Back,dia_lee_li_teach_back);
@@ -95,7 +96,7 @@ func void dia_lee_li_teach_1h_5()
 {
 	if(B_TeachFightTalentPercent(self,other,NPC_TALENT_1H,5,100))
 	{
-		AI_Output(self,other,"DIA_Lee_DI_Teach_1H_5_04_00");	//Твои кисти слишком напряжены. Ты должен держать оружие свободнее.
+		AI_Output(self,other, " DIA_Lee_DI_Teach_1H_5_04_00 " );	// Your brushes are too tight. You should keep your weapons free.
 	};
 	Info_ClearChoices(dia_lee_li_teach);
 	Info_AddChoice(dia_lee_li_teach,Dialog_Back,dia_lee_li_teach_back);
@@ -109,7 +110,7 @@ func void dia_lee_li_teach_2h_1()
 {
 	if(B_TeachFightTalentPercent(self,other,NPC_TALENT_2H,1,100))
 	{
-		AI_Output(self,other,"DIA_DIA_Lee_DI_Teach_2H_1_04_00");	//Всегда помни: боковой удар должен идти от бедра, а не от запястья.
+		AI_Output(self,other, " DIA_DIA_Lee_DI_Teach_2H_1_04_00 " );	// Always remember: the side kick should come from the hip, not the wrist.
 	};
 	Info_ClearChoices(dia_lee_li_teach);
 	Info_AddChoice(dia_lee_li_teach,Dialog_Back,dia_lee_li_teach_back);
@@ -123,7 +124,7 @@ func void dia_lee_li_teach_2h_5()
 {
 	if(B_TeachFightTalentPercent(self,other,NPC_TALENT_2H,5,100))
 	{
-		AI_Output(self,other,"DIA_Lee_DI_Teach_2H_5_04_00");	//Сильнейший удар бесполезен, если он приходится в никуда. Так что старайся точно рассчитывать удары.
+		AI_Output(self,other, " DIA_Lee_DI_Teach_2H_5_04_00 " );	// Strongest hit is useless if it goes nowhere. So try to accurately count the blows.
 	};
 	Info_ClearChoices(dia_lee_li_teach);
 	Info_AddChoice(dia_lee_li_teach,Dialog_Back,dia_lee_li_teach_back);
@@ -147,7 +148,7 @@ instance DIA_LEE_LI_PICKPOCKET(C_Info)
 
 func int dia_lee_li_pickpocket_condition()
 {
-	return C_Beklauen(110,570);
+	return  C_Robbery ( 110 , 570 );
 };
 
 func void dia_lee_li_pickpocket_info()
@@ -159,7 +160,7 @@ func void dia_lee_li_pickpocket_info()
 
 func void dia_lee_li_pickpocket_doit()
 {
-	B_Beklauen();
+	B_Robbery();
 	Info_ClearChoices(dia_lee_li_pickpocket);
 };
 
@@ -176,7 +177,7 @@ instance DIA_LEE_LI_MISSMYGOLD(C_Info)
 	condition = dia_lee_li_missmygold_condition;
 	information = dia_lee_li_missmygold_info;
 	permanent = FALSE;
-	description = "Ты не знаешь, куда подевалось мое золото?";
+	description = " Do you know where my gold went? " ;
 };
 
 
@@ -190,10 +191,10 @@ func int dia_lee_li_missmygold_condition()
 
 func void dia_lee_li_missmygold_info()
 {
-	AI_Output(other,self,"DIA_Lee_LI_MissMyGold_01_01");	//Ты не знаешь, куда подевалось мое золото?
-	AI_Output(self,other,"DIA_Lee_LI_MissMyGold_01_02");	//Нет, парень, не знаю. Да и некогда мне следить за ним.
+	AI_Output(other,self, " DIA_Lee_LI_MissMyGold_01_01 " );	// Do you know where my gold went?
+	AI_Output(self,other, " DIA_Lee_LI_MissMyGold_01_02 " );	// No, man, I don't know. And no time for me to follow him.
 	AI_Output(other,self,"DIA_Lee_LI_MissMyGold_01_03");	//Ясно.
-	B_LogEntry(TOPIC_MISSMYGOLD,"Ли тоже мне не может помочь в этом вопросе.");
+	B_LogEntry( TOPIC_MISSMYGOLD , " Lee can't help me with this either. " );
 };
 
 
@@ -204,7 +205,7 @@ instance DIA_LEE_LI_FINDMAGICORECAVE(C_Info)
 	condition = dia_lee_li_findmagicorecave_condition;
 	information = dia_lee_li_findmagicorecave_info;
 	permanent = FALSE;
-	description = "Я нашел пещеру с залежами магической руды.";
+	description = " I found a cave with magical ore deposits. " ;
 };
 
 
@@ -218,9 +219,9 @@ func int dia_lee_li_findmagicorecave_condition()
 
 func void dia_lee_li_findmagicorecave_info()
 {
-	AI_Output(other,self,"DIA_Lee_LI_FindMagicOreCave_01_01");	//Я нашел пещеру с залежами магической руды.
-	AI_Output(self,other,"DIA_Lee_LI_FindMagicOreCave_01_02");	//Правда? Тогда тебе стоит поговорить об этом с нашим капитаном.
-	AI_Output(self,other,"DIA_Lee_LI_FindMagicOreCave_01_03");	//Думаю, ему будет интересно услышать эту новость.
+	AI_Output(other,self, " DIA_Lee_LI_FindMagicOreCave_01_01 " );	// I found a cave with magical ore deposits.
+	AI_Output(self,other, " DIA_Lee_LI_FindMagicOreCave_01_02 " );	// Really? Then you should talk to our captain about it.
+	AI_Output(self,other, " DIA_Lee_LI_FindMagicOreCave_01_03 " );	// I think he will be interested to hear this news.
 	GOTOORECAPITAN = TRUE;
 };
 
@@ -232,7 +233,7 @@ instance DIA_LEE_LI_CHANGECOURSE(C_Info)
 	condition = dia_lee_li_changecourse_condition;
 	information = dia_lee_li_changecourse_info;
 	permanent = FALSE;
-	description = "Нам необходимо вернуться обратно в Хоринис.";
+	description = " We need to get back to Khorinis. " ;
 };
 
 
@@ -246,35 +247,35 @@ func int dia_lee_li_changecourse_condition()
 
 func void dia_lee_li_changecourse_info()
 {
-	AI_Output(other,self,"DIA_Lee_LI_ChangeCourse_01_00");	//Нам стоит вернуться обратно в Хоринис.
-	AI_Output(self,other,"DIA_Lee_LI_ChangeCourse_01_01");	//И с какой стати нам это делать?
+	AI_Output(other,self, " DIA_Lee_LI_ChangeCourse_01_00 " );	// We should head back to Khorinis.
+	AI_Output(self,other, " DIA_Lee_LI_ChangeCourse_01_01 " );	// And why should we do this?
 	Info_ClearChoices(dia_lee_li_changecourse);
-	Info_AddChoice(dia_lee_li_changecourse,"Для того, чтобы сообщить паладинам о найденной нами руде.",dia_lee_li_changecourse_ore);
-	Info_AddChoice(dia_lee_li_changecourse,"Ради огромной кучи золота!",dia_lee_li_changecourse_gold);
+	Info_AddChoice(dia_lee_li_changecourse, " To inform the paladins about the ore we found. " ,dia_lee_li_changecourse_ore);
+	Info_AddChoice(dia_lee_li_changecourse, " For a huge pile of gold! " ,dia_lee_li_changecourse_gold);
 };
 
 func void dia_lee_li_changecourse_ore()
 {
 	B_GivePlayerXP(100);
-	AI_Output(other,self,"DIA_Lee_LI_ChangeCourse_Ore_01_01");	//Мы должны помочь паладинам! Необходимо сообщить им о магической руде, что мы нашли на этом острове.
-	AI_Output(other,self,"DIA_Lee_LI_ChangeCourse_Ore_01_02");	//Думаю, это даст им небольшое преимущество в войне с орками.
-	AI_Output(self,other,"DIA_Lee_LI_ChangeCourse_Ore_01_03");	//И что с того? Нам-то какое дело до паладинов!
-	AI_Output(self,other,"DIA_Lee_LI_ChangeCourse_Ore_01_04");	//Меня интересует судьба только моих людей. Я не буду ничего делать для этих идиотов!
-	AI_Output(other,self,"DIA_Lee_LI_ChangeCourse_Ore_01_07");	//Лучше подумай о том, что, оказав подобную услугу паладинам, ты сможешь потребовать от них все, что захочешь.
-	AI_Output(self,other,"DIA_Lee_LI_ChangeCourse_Ore_01_08");	//Хммм. Очень интересно. И что же, по-твоему, мне сможет предложить лорд Хаген?
-	AI_Output(other,self,"DIA_Lee_LI_ChangeCourse_Ore_01_09");	//Ты бы мог потребовать от него амнистию для всех своих людей.
-	AI_Output(other,self,"DIA_Lee_LI_ChangeCourse_Ore_01_10");	//Ведь большинство из них остались в Хоринисе.
-	AI_Output(self,other,"DIA_Lee_LI_ChangeCourse_Ore_01_14");	//(задумчиво) Да, ты прав. Возможно, это действительно шанс спасти моих людей!
-	AI_Output(self,other,"DIA_Lee_LI_ChangeCourse_Ore_01_15");	//Я должен попытаться сделать для них все, что в моих силах.
-	AI_Output(other,self,"DIA_Lee_LI_ChangeCourse_Ore_01_16");	//Так ты со мной?
-	AI_Output(self,other,"DIA_Lee_LI_ChangeCourse_Ore_01_17");	//Хорошо! Я отправлюсь с тобой в Хоринис.
-	B_LogEntry(TOPIC_CHANGECOURSE,"Я смог убедить Ли отправиться обратно в Хоринис, чтобы помочь паладинам.");
+	AI_Output(other,self, " DIA_Lee_LI_ChangeCourse_Ore_01_01 " );	// We must help the paladins! We need to inform them about the magical ore that we found on this island.
+	AI_Output(other,self, " DIA_Lee_LI_ChangeCourse_Ore_01_02 " );	// I think this will give them a slight advantage in the war against the orcs.
+	AI_Output(self,other, " DIA_Lee_LI_ChangeCourse_Ore_01_03 " );	// So what? What do we care about paladins!
+	AI_Output(self,other, " DIA_Lee_LI_ChangeCourse_Ore_01_04 " );	// I'm only interested in the fate of my people. I won't do anything for these idiots!
+	AI_Output(other,self, " DIA_Lee_LI_ChangeCourse_Ore_01_07 " );	// Better think about the fact that by doing a similar service to the paladins, you can demand whatever you want from them.
+	AI_Output(self,other, " DIA_Lee_LI_ChangeCourse_Ore_01_08 " );	// Hmmm. Very interesting. And what do you think Lord Hagen can offer me?
+	AI_Output(other,self, " DIA_Lee_LI_ChangeCourse_Ore_01_09 " );	// You could demand amnesty from him for all your people.
+	AI_Output(other,self, " DIA_Lee_LI_ChangeCourse_Ore_01_10 " );	// After all, most of them remained in Khorinis.
+	AI_Output(self,other, " DIA_Lee_LI_ChangeCourse_Ore_01_14 " );	// (thoughtfully) Yes, you're right. Maybe this really is a chance to save my people!
+	AI_Output(self,other, " DIA_Lee_LI_ChangeCourse_Ore_01_15 " );	// I should try to do my best for them.
+	AI_Output(other,self, " DIA_Lee_LI_ChangeCourse_Ore_01_16 " );	// So are you with me?
+	AI_Output(self,other, " DIA_Lee_LI_ChangeCourse_Ore_01_17 " );	// Good! I will go with you to Khorinis.
+	B_LogEntry( TOPIC_CHANGECOURSE , " I was able to convince Lee to go back to Khorinis to help the paladins. " );
 	CREWAGREEAWAYBACKPAL = CREWAGREEAWAYBACKPAL + 1;
 	COUNTPEOPLEDECIDEPRG = COUNTPEOPLEDECIDEPRG + 1;
 	if(COUNTPEOPLEDECIDEPRG >= COUNTPEOPLEDECIDE)
 	{
 		READYCHANGECOURSE = TRUE;
-		Log_AddEntry(TOPIC_CHANGECOURSE,"Кажется, я поговорил со всеми парнями. Пора сообщить об этом нашему капитану.");
+		Log_AddEntry( TOPIC_CHANGECOURSE , " I think I've talked to all the guys. Time to tell our captain. " );
 	};
 	Info_ClearChoices(dia_lee_li_changecourse);
 };
@@ -282,19 +283,19 @@ func void dia_lee_li_changecourse_ore()
 func void dia_lee_li_changecourse_gold()
 {
 	B_GivePlayerXP(50);
-	AI_Output(other,self,"DIA_Lee_LI_ChangeCourse_Gold_01_01");	//Ради огромной кучи золота! Разве это не повод, чтобы вернуться?
-	AI_Output(self,other,"DIA_Lee_LI_ChangeCourse_Gold_01_02");	//Очень интересно. И где ты собираешься искать эту самую кучу?
-	AI_Output(other,self,"DIA_Lee_LI_ChangeCourse_Gold_01_03");	//Ее нам дадут паладины, когда мы сообщим им о руде, найденной на этом острове.
-	AI_Output(self,other,"DIA_Lee_LI_ChangeCourse_Gold_01_07");	//Я сомневаюсь, что у паладинов найдется такое количество золота, ради которого стоило бы возвращаться.
-	AI_Output(self,other,"DIA_Lee_LI_ChangeCourse_Gold_01_08");	//К тому же в Хоринисе уже могут быть орки! А значит, мы просто проделаем наш путь впустую.
-	AI_Output(self,other,"DIA_Lee_LI_ChangeCourse_Gold_01_10");	//(серьезно) Думаю, наше возвращение не имеет никакого смысла.
-	AI_Output(self,other,"DIA_Lee_LI_ChangeCourse_Gold_01_11");	//Это мое мнение. Но решение принимать тебе.
-	B_LogEntry(TOPIC_CHANGECOURSE,"Ли считает, что возвращаться в Хоринис не имеет смысла.");
+	AI_Output(other,self, " DIA_Lee_LI_ChangeCourse_Gold_01_01 " );	// For a huge pile of gold! Isn't that a reason to come back?
+	AI_Output(self,other, " DIA_Lee_LI_ChangeCourse_Gold_01_02 " );	// Very interesting. And where are you going to look for this very pile?
+	AI_Output(other,self, " DIA_Lee_LI_ChangeCourse_Gold_01_03 " );	// The paladins will give it to us when we tell them about the ore found on this island.
+	AI_Output(self,other, " DIA_Lee_LI_ChangeCourse_Gold_01_07 " );	// I doubt paladins have that much gold worth returning for.
+	AI_Output(self,other, " DIA_Lee_LI_ChangeCourse_Gold_01_08 " );	// Plus, there might already be Orcs in Khorinis! Which means we're just wasting our way.
+	AI_Output(self,other, " DIA_Lee_LI_ChangeCourse_Gold_01_10 " );	// (seriously) I don't think our return makes any sense.
+	AI_Output(self,other, " DIA_Lee_LI_ChangeCourse_Gold_01_11 " );	// This is my opinion. But the decision is up to you.
+	B_LogEntry( TOPIC_CHANGECOURSE , " Lee doesn't think it makes sense to return to Khorinis. " );
 	COUNTPEOPLEDECIDEPRG = COUNTPEOPLEDECIDEPRG + 1;
 	if(COUNTPEOPLEDECIDEPRG >= COUNTPEOPLEDECIDE)
 	{
 		READYCHANGECOURSE = TRUE;
-		B_LogEntry(TOPIC_CHANGECOURSE,"Кажется, я поговорил со всеми парнями. Пора сообщить об этом нашему капитану.");
+		B_LogEntry( TOPIC_CHANGECOURSE , " I think I've talked to all the guys. Time to tell our captain. " );
 	};
 	Info_ClearChoices(dia_lee_li_changecourse);
 };
