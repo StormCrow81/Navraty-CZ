9f193b053a3fdcaa0538c6c9b0966a05568f6d03
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-04 00:29:06 +0200

    Update DIA_Stoneguardian_Boss.d

--- RU-Main/Story/Dialoge/DIA_Stoneguardian_Boss.d
+++ RU-Main/Story/Dialoge/DIA_Stoneguardian_Boss.d
@@ -1,3 +1,4 @@
+
 instance DIA_Stoneguardian_Boss_Exit(C_Info)
 {
 	npc = Stoneguardian_Boss;
@@ -16,7 +17,7 @@ func int DIA_Stoneguardian_Boss_Exit_Condition()
 func void DIA_Stoneguardian_Boss_Exit_Info()
 {
 	AI_StopProcessInfos(self);
-	self.aivar[AIV_EnemyOverride] = FALSE;
+	self.aivar[AIV_EnemyOverride] = FALSE ;
 	self.flags = FALSE;
 	Snd_Play("MFX_FEAR_CAST");
 	B_Attack(self,other,AR_NONE,0);
@@ -43,30 +44,30 @@ func int DIA_Stoneguardian_Boss_Hello_Condition()
 func void DIA_Stoneguardian_Boss_Hello_Info()
 {
 	B_GivePlayerXP(500);
-	AI_Output(self,other,"DIA_Stoneguardian_Boss_Hello_01_01");	//КТО ПОСМЕЛ ПОТРЕВОЖИТЬ МОЙ СВЯЩЕННЫЙ ПОКОЙ?!
-	AI_Output(other,self,"DIA_Stoneguardian_Boss_Hello_01_02");	//Собственно, это был я.
-	AI_Output(self,other,"DIA_Stoneguardian_Boss_Hello_01_03");	//Как интересно. И что же тебе нужно от меня, человек?
-	AI_Output(other,self,"DIA_Stoneguardian_Boss_Hello_01_04");	//Я хочу узнать об одном артефакте, который вы когда-то охраняли.
-	AI_Output(self,other,"DIA_Stoneguardian_Boss_Hello_01_05");	//ЧТО?! Откуда тебе это известно?
-	AI_Output(other,self,"DIA_Stoneguardian_Boss_Hello_01_06");	//Из записей в магическом свитке древних, с помощью которого я и пробудил тебя.
-	AI_Output(self,other,"DIA_Stoneguardian_Boss_Hello_01_07");	//Даже если ты смог прочитать надписи, это еще не делает тебя достойным обладать священными знаниями.
-	AI_Output(self,other,"DIA_Stoneguardian_Boss_Hello_01_08");	//Ибо только избранный Аданоса сможет познать его мудрость!
-	AI_Output(self,other,"DIA_Stoneguardian_Boss_Hello_01_09");	//Но я не думаю, что ты таковым являешься.
-	AI_Output(other,self,"DIA_Stoneguardian_Boss_Hello_01_10");	//Так ты все-таки расскажешь мне про этот артефакт?
-	AI_Output(self,other,"DIA_Stoneguardian_Boss_Hello_01_11");	//И почему я должен это делать?
-	AI_Output(other,self,"DIA_Stoneguardian_Boss_Hello_01_12");	//Может, я действительно являюсь избранником Аданоса!
-	AI_Output(self,other,"DIA_Stoneguardian_Boss_Hello_01_13");	//Ха ха ха... Не смеши меня, жалкий человечишка! Не может такого быть.
-	AI_Output(self,other,"DIA_Stoneguardian_Boss_Hello_01_14");	//Но если ты на самом деле считаешь себя им - докажи это.
-	AI_Output(other,self,"DIA_Stoneguardian_Boss_Hello_01_15");	//Каким образом?
-	AI_Output(self,other,"DIA_Stoneguardian_Boss_Hello_01_16");	//Сразись со мной!
-	AI_Output(self,other,"DIA_Stoneguardian_Boss_Hello_01_17");	//Только если ты сможешь меня одолеть, тебе откроется тайна этого священного артефакта!
-	AI_Output(self,other,"DIA_Stoneguardian_Boss_Hello_01_18");	//В противном случае ты просто умрешь.
-	AI_Output(other,self,"DIA_Stoneguardian_Boss_Hello_01_19");	//Ну, на другое я и не рассчитывал.
-	B_LogEntry(TOPIC_AdanosCrone,"Я смог оживить древнего Стража, но это мне ничего не дало. Страж отказался рассказать мне об этом загадочном артефакте. Только победив его самого, я смогу продвинуться дальше в своих поисках.");
+	AI_Output(self,other, " DIA_Stoneguardian_Boss_Hello_01_01 " );	// WHO DARE TO DISTURB MY SACRED PEACE?!
+	AI_Output(other,self, " DIA_Stoneguardian_Boss_Hello_01_02 " );	// Actually, it was me.
+	AI_Output(self,other, " DIA_Stoneguardian_Boss_Hello_01_03 " );	// How interesting. And what do you want from me, man?
+	AI_Output(other,self, " DIA_Stoneguardian_Boss_Hello_01_04 " );	// I want to know about an artifact you once guarded.
+	AI_Output(self,other, " DIA_Stoneguardian_Boss_Hello_01_05 " );	// WHAT?! How do you know this?
+	AI_Output(other,self, " DIA_Stoneguardian_Boss_Hello_01_06 " );	// From the records in the magic scroll of the ancients, with the help of which I awakened you.
+	AI_Output(self,other, " DIA_Stoneguardian_Boss_Hello_01_07 " );	// Even if you can read the inscriptions, that doesn't make you worthy of sacred knowledge.
+	AI_Output(self,other, " DIA_Stoneguardian_Boss_Hello_01_08 " );	// For only the chosen one of Adanos can know his wisdom!
+	AI_Output(self,other, " DIA_Stoneguardian_Boss_Hello_01_09 " );	// But I don't think you are.
+	AI_Output(other,self, " DIA_Stoneguardian_Boss_Hello_01_10 " );	// So you're still going to tell me about this artifact?
+	AI_Output(self,other, " DIA_Stoneguardian_Boss_Hello_01_11 " );	// And why should I do this?
+	AI_Output(other,self, " DIA_Stoneguardian_Boss_Hello_01_12 " );	// Maybe I really am the chosen one of Adanos!
+	AI_Output(self,other, " DIA_Stoneguardian_Boss_Hello_01_13 " );	// Ha ha ha... Don't make me laugh, you pathetic human! It can not be.
+	AI_Output(self,other, " DIA_Stoneguardian_Boss_Hello_01_14 " );	// But if you really think you're one, prove it.
+	AI_Output(other,self, " DIA_Stoneguardian_Boss_Hello_01_15 " );	// How?
+	AI_Output(self,other, " DIA_Stoneguardian_Boss_Hello_01_16 " );	// Fight me!
+	AI_Output(self,other, " DIA_Stoneguardian_Boss_Hello_01_17 " );	// Only if you can defeat me will the secret of this sacred artifact be revealed to you!
+	AI_Output(self,other, " DIA_Stoneguardian_Boss_Hello_01_18 " );	// Otherwise, you'll just die.
+	AI_Output(other,self, " DIA_Stoneguardian_Boss_Hello_01_19 " );	// Well, I didn't count on anything else.
+	B_LogEntry(TOPIC_AdanosCrone, " I was able to revive the ancient Guardian, but it did nothing for me. The Guardian refused to tell me about this mysterious artifact. Only by defeating him will I be able to progress further in my quest. " );
 	KnowAboutAdVal = TRUE;
 };
 
-//=============================Спящий=================================================
+// ============================Sleeping================== ===============================
 
 instance DIA_Sleeper_Old_Exit(C_Info)
 {
@@ -106,62 +107,62 @@ func int DIA_Sleeper_Old_Hello_Condition()
 func void DIA_Sleeper_Old_Hello_Info()
 {
 	B_GivePlayerXP(2500);
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_01_01");	//Аргххх...(зловеще) НАКОНЕЦ-ТО МЫ ПОВСТРЕЧАЛИСЬ, ЧЕЛОВЕК!
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_01_02");	//Как долго я ждал этой встречи!
-	AI_Output(other,self,"DIA_Sleeper_Old_Hello_01_03");	//Спящий! Никогда бы не подумал, что увижу тебя здесь вновь.
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_01_04");	//Все мои чувства подсказывают мне, что маг, призвавший меня сюда, уже давно мертв.
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_01_05");	//Готов поспорить, что это ты приложил к этому руку.
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_01_06");	//Хотя, впрочем, это уже не имеет значения.
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_01_07");	//Главное, что сейчас ты стоишь передо мной!
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_01_01 " );	//Arghhh...(ominously ) WE FINALLY DATE, HUMAN!
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_01_02 " );	// How long have I been waiting for this meeting!
+	AI_Output(other,self, " DIA_Sleeper_Old_Hello_01_03 " );	// Asleep! I never thought I'd see you here again.
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_01_04 " );	// All my senses tell me that the magician who summoned me here is long dead.
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_01_05 " );	// I bet you had a hand in this.
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_01_06 " );	// Although, however, it no longer matters.
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_01_07 " );	// The main thing is that now you are standing in front of me!
 	SleeperHello = TRUE;
-	B_LogEntry(TOPIC_SLEEPERBACK,"Спящий вернулся в мир людей! Похоже, этот демон мой злой рок. Теперь мне надо разобраться с ним раз и навсегда, коли представилась такая возможность.");
+	B_LogEntry( TOPIC_SLEEPERBACK , " The sleeper has returned to the human world! Looks like this demon is my doom. Now I need to deal with him once and for all, if the opportunity presents itself. " );
 	Info_ClearChoices(DIA_Sleeper_Old_Hello);
-	Info_AddChoice(DIA_Sleeper_Old_Hello,"Очень хочется мне отомстить?",DIA_Sleeper_Old_Hello_Battle);
-	Info_AddChoice(DIA_Sleeper_Old_Hello,"Зачем ты пробудил драконов?",DIA_Sleeper_Old_Hello_Dragons);
-	Info_AddChoice(DIA_Sleeper_Old_Hello,"А кто ты вообще такой?",DIA_Sleeper_Old_Hello_WhoYou);
+	Info_AddChoice(DIA_Sleeper_Old_Hello, " Do I really want revenge? " ,DIA_Sleeper_Old_Hello_Battle);
+	Info_AddChoice(DIA_Sleeper_Old_Hello, " Why did you wake up the dragons? " ,DIA_Sleeper_Old_Hello_Dragons);
+	Info_AddChoice(DIA_Sleeper_Old_Hello, " Who are you anyway? " ,DIA_Sleeper_Old_Hello_WhoYou);
 };
 
 func void DIA_Sleeper_Old_Hello_WhoYou()
 {
-	AI_Output(other,self,"DIA_Sleeper_Old_Hello_WhoYou_01_01");	//А кто ты вообще такой?
-	AI_Output(other,self,"DIA_Sleeper_Old_Hello_WhoYou_01_02");	//На вид ты не очень похож на существо, созданное Белиаром.
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_WhoYou_01_03");	//Это так...(зловеще) Однако его могущество помогло мне вернутся в этот мир!
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_WhoYou_01_04");	//Я - древнейший демон, порожденный магией этого мира в эпоху его создания.
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_WhoYou_01_05");	//Когда-то люди поклонялись мне так же, как и вы поклоняетесь сейчас своим богам.
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_WhoYou_01_06");	//Но мое время давно прошло. И лишь благодаря оркам я вновь смог возродится из небытия.
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_WhoYou_01_07");	//Но ты, жалкий червь, тогда все испортил! 
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_WhoYou_01_08");	//А я был слишком слаб, чтобы сопротивляться силе твоего оружия.
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_WhoYou_01_09");	//Но теперь, благодаря Белиару и его слуге, я вновь готов явиться этому миру!
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_WhoYou_01_10");	//И на этот раз ты уже не застанешь меня врасплох.
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_WhoYou_01_11");	//Я полностью обрел былую мощь, и теперь готов обрушить свой гнев на любого, кто осмелится восстать против моего повелителя.
-	AI_Output(other,self,"DIA_Sleeper_Old_Hello_WhoYou_01_12");	//Выходит, что из могучего демона ты превратился в очередную марионетку Белиара.
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_WhoYou_01_13");	//Говори что хочешь, человек! Все равно жить тебе осталось совсем недолго...
+	AI_Output(other,self, " DIA_Sleeper_Old_Hello_WhoYou_01_01 " );	// Who are you anyway?
+	AI_Output(other,self, " DIA_Sleeper_Old_Hello_WhoYou_01_02 " );	// You don't look much like the creature Beliar created.
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_WhoYou_01_03 " );	// That's right... (ominously) However, his power helped me to return to this world!
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_WhoYou_01_04 " );	// I am the oldest demon, spawned by the magic of this world in the era of its creation.
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_WhoYou_01_05 " );	// People once worshiped me as you worship your gods now.
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_WhoYou_01_06 " );	// But my time is long gone. And only thanks to the orcs I was again able to be reborn from oblivion.
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_WhoYou_01_07 " );	// But you, pathetic worm, ruined everything then!
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_WhoYou_01_08 " );	// And I was too weak to resist the power of your weapon.
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_WhoYou_01_09 " );	// But now, thanks to Beliar and his servant, I'm ready to appear in this world again!
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_WhoYou_01_10 " );	// And this time you won't take me by surprise.
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_WhoYou_01_11 " );	// I have fully regained my former power, and now I am ready to unleash my wrath on anyone who dares to rebel against my master.
+	AI_Output(other,self, " DIA_Sleeper_Old_Hello_WhoYou_01_12 " );	// It turns out that from a mighty demon you turned into another puppet of Beliar.
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_WhoYou_01_13 " );	// Say what you want, man! You still don't have long to live...
 };
 
 func void DIA_Sleeper_Old_Hello_Dragons()
 {
-	AI_Output(other,self,"DIA_Sleeper_Old_Hello_Dragons_01_01");	//Зачем ты пробудил драконов?
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_Dragons_01_02");	//Те, кого ты называешь драконами, такие же древнейшие создания, как и я.
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_Dragons_01_03");	//Они просто не могли не услышать мой зов о помощи!
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_Dragons_01_04");	//Но их пробудил не я...(зловеще) Они повинуются лишь воле тех, кто на самом деле сотворил их!
-	AI_Output(other,self,"DIA_Sleeper_Old_Hello_Dragons_01_05");	//Ты говоришь о Хранителях?
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_Dragons_01_06");	//Именно о них! 
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_Dragons_01_07");	//Древние стражи мироздания, сокрывшиеся в тенях, дабы не затмить славу ваших богов...
+	AI_Output(other,self, " DIA_Sleeper_Old_Hello_Dragons_01_01 " );	// Why did you awaken the dragons?
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_Dragons_01_02 " );	// Those you call dragons are as ancient as I am.
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_Dragons_01_03 " );	// They just couldn't help but hear my call for help!
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_Dragons_01_04 " );	// But I didn't awaken them... (ominously) They only obey the will of those who actually created them!
+	AI_Output(other,self, " DIA_Sleeper_Old_Hello_Dragons_01_05 " );	// Are you talking about Guardians?
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_Dragons_01_06 " );	// About them!
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_Dragons_01_07 " );	// Ancient guardians of the universe, hiding in the shadows, so as not to overshadow the glory of your gods...
 };
 
 func void DIA_Sleeper_Old_Hello_Battle()
 {
-	AI_Output(other,self,"DIA_Sleeper_Old_Hello_Battle_01_01");	//По всей видимости, ты хочешь отомстить мне за тот случай в храме.
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_Battle_01_02");	//Глупо скрывать это! Я жажду этого больше всего на свете!
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_Battle_01_03");	//А то, что произошло в прошлый раз, было лишь нелепой случайностью.
-	AI_Output(other,self,"DIA_Sleeper_Old_Hello_Battle_01_04");	//Что ж, тогда не стоит тратить время на любезности.
-	AI_Output(other,self,"DIA_Sleeper_Old_Hello_Battle_01_05");	//Как и в прошлый раз, я вновь отправлю тебя туда, откуда ты выполз на этот свет!
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_Battle_01_06");	//(зловещий смех) Этому больше не бывать, человек!
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_Battle_01_07");	//Сначала я сокрушу тебя, а потом и весь твой жалкий мирок!
-	AI_Output(other,self,"DIA_Sleeper_Old_Hello_Battle_01_08");	//Тогда покажи, на что ты способен, слизняк!
-	AI_Output(self,other,"DIA_Sleeper_Old_Hello_Battle_01_09");	//Не волнуйся! Тебе не придется долго ждать...
+	AI_Output(other,self, " DIA_Sleeper_Old_Hello_Battle_01_01 " );	// Apparently, you want to take revenge on me for that case in the temple.
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_Battle_01_02 " );	// Silly to hide it! I want this more than anything!
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_Battle_01_03 " );	// And what happened last time was just an absurd accident.
+	AI_Output(other,self, " DIA_Sleeper_Old_Hello_Battle_01_04 " );	// Well, then don't waste your time on pleasantries.
+	AI_Output(other,self, " DIA_Sleeper_Old_Hello_Battle_01_05 " );	// Just like last time, I will send you back to where you crawled into this world from!
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_Battle_01_06 " );	// (evil laughter) It won't happen again, human!
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_Battle_01_07 " );	// First I'll crush you, and then your whole miserable little world!
+	AI_Output(other,self, " DIA_Sleeper_Old_Hello_Battle_01_08 " );	// Then show me what you can do, slug!
+	AI_Output(self,other, " DIA_Sleeper_Old_Hello_Battle_01_09 " );	// Don't worry! You won't have to wait long...
 	AI_StopProcessInfos(self);
-	self.aivar[AIV_EnemyOverride] = FALSE;
+	self.aivar[AIV_EnemyOverride] = FALSE ;
 	self.flags = FALSE;
 	Snd_Play("MFX_FEAR_CAST");
 };
@@ -185,7 +186,7 @@ func void DIA_XranFreg_Exit_Info()
 {
 	AI_StopProcessInfos(self);
 	self.flags = 0;
-	self.aivar[AIV_EnemyOverride] = FALSE;
+	self.aivar[AIV_EnemyOverride] = FALSE ;
 	B_Attack(self,other,AR_NONE,0);
 };
 
@@ -206,24 +207,24 @@ func int DIA_XranFreg_Hello_Condition()
 
 func void DIA_XranFreg_Hello_Info()
 {
-	AI_Output(self,other,"DIA_XranFreg_Hello_01_01");	//Человек, ты пришел...
-	AI_Output(other,self,"DIA_XranFreg_Hello_01_02");	//Ты еще и разговариваешь? А я уж думал, что повстречаю очередного каменного болвана.
-	AI_Output(other,self,"DIA_XranFreg_Hello_01_03");	//Так ты и есть этот живой каменный утес? Что-то по размерам не похож.
-	AI_Output(self,other,"DIA_XranFreg_Hello_01_04");	//Мои размеры не имеют значения, смертный!
-	AI_Output(self,other,"DIA_XranFreg_Hello_01_05");	//Если я захочу, я могу стать и каменной скалой и маленьким камушком у тебя под ногами.
-	AI_Output(self,other,"DIA_XranFreg_Hello_01_06");	//Но моя сила и могущество от этого нисколько не уменьшатся.
-	AI_Output(other,self,"DIA_XranFreg_Hello_01_07");	//Кто ты и зачем ты здесь?
-	AI_Output(self,other,"DIA_XranFreg_Hello_01_08");	//Хранители дали мне имя Фрегарах, и я страж врат Эгезарта.
-	AI_Output(other,self,"DIA_XranFreg_Hello_01_09");	//А кто такой Эгезарт?
-	AI_Output(self,other,"DIA_XranFreg_Hello_01_10");	//Тот, кто находится за этими вратами. Большего тебе знать необязательно.
-	AI_Output(other,self,"DIA_XranFreg_Hello_01_11");	//И как мне пройти дальше?
-	AI_Output(self,other,"DIA_XranFreg_Hello_01_12");	//Твоя дорога заканчивается здесь, смертный. Поворачивай назад или умрешь.
-	AI_Output(other,self,"DIA_XranFreg_Hello_01_13");	//Ты не оставляешь мне выбора, страж. Я все равно пройду дальше!
-	AI_Output(self,other,"DIA_XranFreg_Hello_01_15");	//Выбора не оставляешь мне ты. Дальше тебе не пройти.
+	AI_Output(self,other, " DIA_XranFreg_Hello_01_01 " );	// Man, you've come...
+	AI_Output(other,self, " DIA_XranFreg_Hello_01_02 " );	// Are you still talking? And I already thought that I would meet another stone blockhead.
+	AI_Output(other,self, " DIA_XranFreg_Hello_01_03 " );	// So you are this living stone cliff? Something is not the same size.
+	AI_Output(self,other, " DIA_XranFreg_Hello_01_04 " );	// My size doesn't matter, mortal!
+	AI_Output(self,other, " DIA_XranFreg_Hello_01_05 " );	// If I want, I can become both a stone rock and a small pebble under your feet.
+	AI_Output(self,other, " DIA_XranFreg_Hello_01_06 " );	// But my strength and power will not decrease from this at all.
+	AI_Output(other,self, " DIA_XranFreg_Hello_01_07 " );	// Who are you and why are you here?
+	AI_Output(self,other, " DIA_XranFreg_Hello_01_08 " );	// The Guardians named me Fregarach, and I am the guardian of the gates of Egesarth.
+	AI_Output(other,self, " DIA_XranFreg_Hello_01_09 " );	// And who is Egesart?
+	AI_Output(self,other, " DIA_XranFreg_Hello_01_10 " );	// The one behind this gate. You don't need to know more.
+	AI_Output(other, self, " DIA_XranFreg_Hello_01_11 " );	// And how do I go further?
+	AI_Output(self,other, " DIA_XranFreg_Hello_01_12 " );	// Your road ends here, mortal. Turn back or die.
+	AI_Output(other,self, " DIA_XranFreg_Hello_01_13 " );	// You leave me no choice, guardian. I'll still move on!
+	AI_Output(self,other, " DIA_XranFreg_Hello_01_15 " );	// You leave me no choice. You can't go further.
 
 	if(MIS_XRANFREG == LOG_Running)
 	{
 		B_GivePlayerXP(1000);
-		B_LogEntry(TOPIC_XRANFREG,"Я встретил того каменного гиганта, о котором упоминалось в записях. По правде говоря, он не произвел на меня особого впечатления. Однако внешность порой обманчива...");
+		B_LogEntry( TOPIC_XRANFREG , " I met the stone giant mentioned in the entries. To tell the truth, he didn't impress me much. Looks can be deceiving, though... " );
 	};
 };
