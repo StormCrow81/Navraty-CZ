019e37cd267aedcb8f0f4892be15a63d8443a185
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 13:48:12 +0200

    Update DIA_pal_25000_garond.d

--- RU-Main/Story/Dialoge/DIA_pal_25000_garond.d
+++ RU-Main/Story/Dialoge/DIA_pal_25000_garond.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_PAL_25000_GAROND_EXIT(C_Info)
 {
 	npc = pal_25000_garond;
@@ -28,7 +29,7 @@ instance DIA_PAL_25000_GAROND_EXITNW(C_Info)
 	condition = dia_pal_25000_garond_exitnw_condition;
 	information = dia_pal_25000_garond_exitnw_info;
 	permanent = FALSE;
-	description = "Теперь твои люди и ты сам в безопасности.";
+	description = " Your people and yourself are safe now. " ;
 };
 
 
@@ -40,45 +41,45 @@ func int dia_pal_25000_garond_exitnw_condition()
 func void dia_pal_25000_garond_exitnw_info()
 {
 	B_GivePlayerXP(200);
-	AI_Output(other,self,"DIA_PAL_25000_Garond_ExitNW_01_00");	//Теперь твои люди и ты сам в безопасности.
-	AI_Output(self,other,"DIA_PAL_25000_Garond_ExitNW_01_01");	//Да, однако орки все еще осаждают Хоринис, и это не может радовать.
-	AI_Output(self,other,"DIA_PAL_25000_Garond_ExitNW_01_02");	//Мы должны что-то предпринять, чтобы выбить их с острова! Иначе все наши труды будут напрасны.
-	AI_Output(other,self,"DIA_PAL_25000_Garond_ExitNW_01_03");	//Не волнуйся. Что-нибудь придумаем.
+	AI_Output(other,self, " DIA_PAL_25000_Garond_ExitNW_01_00 " );	// Now your people and you are safe.
+	AI_Output(self,other, " DIA_PAL_25000_Garond_ExitNW_01_01 " );	// Yes, but the orcs are still besieging Khorinis, and that's no good.
+	AI_Output(self,other, " DIA_PAL_25000_Garond_ExitNW_01_02 " );	// We need to do something to kick them off the island! Otherwise, all our labors will be in vain.
+	AI_Output(other,self, " DIA_PAL_25000_Garond_ExitNW_01_03 " );	// Don't worry. We'll come up with something.
 	AI_Output(self,other,"DIA_PAL_25000_Garond_ExitNW_01_04");	//Надеюсь.
 };
 
 
-instance DIA_PAL_25000_GAROND_PERMEXITNW(C_Info)
+instance DIA_PAL_25000_GAROND_PERMEXITNW (C_Info)
 {
 	npc = pal_25000_garond;
 	nr = 1;
 	condition = dia_pal_25000_garond_permexitnw_condition;
 	information = dia_pal_25000_garond_permexitnw_info;
 	permanent = TRUE;
-	description = "Как обстоят дела?";
+	description = " How are things? " ;
 };
 
 
-func int dia_pal_25000_garond_permexitnw_condition()
+func int pal_day_25000_garond_permexitnw_condition()
 {
 	return TRUE;
 };
 
 func void dia_pal_25000_garond_permexitnw_info()
 {
-	AI_Output(other,self,"DIA_PAL_25000_Garond_PermExitNW_01_01");	//Как обстоят дела?
+	AI_Output(other,self, " DIA_PAL_25000_Garond_PermExitNW_01_01 " );	// How are things going?
 	if(Npc_KnowsInfo(hero,dia_pal_2000_hagen_donejoinothersagreed))
 	{
-		AI_Output(self,other,"DIA_PAL_25000_Garond_PermExitNW_01_06");	//Уже лучше...(воинственно) Наконец-то лорд Хаген отдал всеобщий приказ о выступлении.
-		AI_Output(self,other,"DIA_PAL_25000_Garond_PermExitNW_01_07");	//Скоро нам предстоит большое сражение с орками, и клянусь Инносом, что теперь этим тварям точно не поздоровится!
-		AI_Output(other,self,"DIA_PAL_25000_Garond_PermExitNW_01_08");	//Будем надеяться, что так оно и будет.
+		AI_Output(self,other, " DIA_PAL_25000_Garond_PermExitNW_01_06 " );	// Better... (belligerently) Lord Hagen has finally given the general order to march.
+		AI_Output(self,other, " DIA_PAL_25000_Garond_PermExitNW_01_07 " );	// Soon we will have a big battle with the orcs, and I swear by Innos that now these creatures will definitely not be in trouble!
+		AI_Output(other,self, " DIA_PAL_25000_Garond_PermExitNW_01_08 " );	// Let's hope it stays that way.
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_PAL_25000_Garond_PermExitNW_01_02");	//Лорд Хаген до сих пор не решился атаковать орков, и я не понимаю, почему.
-		AI_Output(self,other,"DIA_PAL_25000_Garond_PermExitNW_01_03");	//Возможно, он ждет более удобного момента, но по моему мнению - он давно настал!
-		AI_Output(other,self,"DIA_PAL_25000_Garond_PermExitNW_01_04");	//Думаю, это долго не затянется. Уверен, паладины еще скажут свое веское слово в этой войне.
-		AI_Output(self,other,"DIA_PAL_25000_Garond_PermExitNW_01_05");	//Будь уверен - так и будет. Клянусь Инносом!
+		AI_Output(self,other, " DIA_PAL_25000_Garond_PermExitNW_01_02 " );	// Lord Hagen still hasn't decided to attack the orcs, and I don't understand why.
+		AI_Output(self,other, " DIA_PAL_25000_Garond_PermExitNW_01_03 " );	// Perhaps he is waiting for a more convenient moment, but in my opinion - he has come a long time ago!
+		AI_Output(other,self, " DIA_PAL_25000_Garond_PermExitNW_01_04 " );	// I don't think this will last long. I am sure that the paladins will still have their say in this war.
+		AI_Output(self,other, " DIA_PAL_25000_Garond_PermExitNW_01_05 " );	// Be sure - it will. I swear by Innos!
 	};
 };
 
@@ -89,7 +90,7 @@ instance DIA_PAL_25000_Garond_VenzelDone(C_Info)
 	condition = DIA_PAL_25000_Garond_VenzelDone_condition;
 	information = DIA_PAL_25000_Garond_VenzelDone_info;
 	permanent = FALSE;
-	description = "Я выяснил, что случилось с Венцелем.";
+	description = " I found out what happened to Wenzel. " ;
 };
 
 func int DIA_PAL_25000_Garond_VenzelDone_condition()
@@ -103,29 +104,29 @@ func int DIA_PAL_25000_Garond_VenzelDone_condition()
 func void DIA_PAL_25000_Garond_VenzelDone_info()
 {
 	B_GivePlayerXP(250);
-	AI_Output(other,self,"DIA_PAL_25000_Garond_VenzelDone_01_01");	//Я выяснил, что случилось с Венцелем.
-	AI_Output(self,other,"DIA_PAL_25000_Garond_VenzelDone_01_02");	//Рассказывай все, что знаешь. Он жив?
-	AI_Output(other,self,"DIA_PAL_25000_Garond_VenzelDone_01_03");	//Жив, но попал в плен к оркам. Они отправили его в железную шахту добывать руду.
-	AI_Output(self,other,"DIA_PAL_25000_Garond_VenzelDone_01_04");	//Хвала Инносу...(облегченно) Значит, еще не все потеряно! Надо попробовать вытащить его оттуда.
-	AI_Output(other,self,"DIA_PAL_25000_Garond_VenzelDone_01_05");	//В этом уже нет необходимости. В шахте орков я нашел магический портал.
-	AI_Output(other,self,"DIA_PAL_25000_Garond_VenzelDone_01_06");	//Венцель вместе с остальными рабами воспользуется им, чтобы сбежать из плена.
-	AI_Output(other,self,"DIA_PAL_25000_Garond_VenzelDone_01_07");	//Однако есть подозрение, что портал ведет прямиком на материк.
-	AI_Output(other,self,"DIA_PAL_25000_Garond_VenzelDone_01_08");	//Поэтому Венцель попросил меня передать тебе, что скорее всего, не вернется обратно в Хоринис.
-	AI_Output(self,other,"DIA_PAL_25000_Garond_VenzelDone_01_09");	//Что же, пусть будет так. Главное, что с ним все будет в порядке.
-	AI_Output(other,self,"DIA_PAL_25000_Garond_VenzelDone_01_10");	//Он сказал, что вернется в Миртану и будет сражаться с орками там.
-	AI_Output(other,self,"DIA_PAL_25000_Garond_VenzelDone_01_11");	//По всей видимости, он был прав, когда говорил, что у короля там дела обстоят не лучше вашего.
-	AI_Output(self,other,"DIA_PAL_25000_Garond_VenzelDone_01_12");	//Эти проклятые орки...(гневно) Они заполонили всю страну, как саранча!
-	AI_Output(self,other,"DIA_PAL_25000_Garond_VenzelDone_01_13");	//Но мы будем сражаться до последнего! Просто так им нас не сломить.
-	AI_Output(self,other,"DIA_PAL_25000_Garond_VenzelDone_01_14");	//Что касается ситуации на материке...(с неохотой) Я не стану тебя обманывать! Да, это так.
-	AI_Output(self,other,"DIA_PAL_25000_Garond_VenzelDone_01_15");	//Король из последних сил старается сдержать орды орков, прорвавшиеся из Нордмара.
-	AI_Output(self,other,"DIA_PAL_25000_Garond_VenzelDone_01_16");	//Но ему это плохо удается. Орков слишком много, а королю не хватает ни людей, ни оружия.
-	AI_Output(self,other,"DIA_PAL_25000_Garond_VenzelDone_01_17");	//Поэтому вся надежда лежит на нас! С помощью магической руды возможно еще можно будет переломить ход войны.
-	AI_Output(self,other,"DIA_PAL_25000_Garond_VenzelDone_01_18");	//Но как видишь, мы сами тут с трудом отбиваемся от орков.
-	AI_Output(self,other,"DIA_PAL_25000_Garond_VenzelDone_01_19");	//Я не привык сдаваться раньше времени...(обреченно) Но похоже, что в этой войне мы уже проиграли.
-	AI_Output(self,other,"DIA_PAL_25000_Garond_VenzelDone_01_20");	//Но только не вздумай об этом рассказывать кому-либо! 
-	AI_Output(self,other,"DIA_PAL_25000_Garond_VenzelDone_01_21");	//Я не позволю, чтобы мои солдаты пали духом только из-за каких то глупых домыслов.
-	AI_Output(other,self,"DIA_PAL_25000_Garond_VenzelDone_01_22");	//Само собой.
+	AI_Output(other,self, " DIA_PAL_25000_Garond_VenzelDone_01_01 " );	// I found out what happened to Wenzel.
+	AI_Output(self,other, " DIA_PAL_25000_Garond_VenzelDone_01_02 " );	// Tell me everything you know. Is he alive?
+	AI_Output(other,self, " DIA_PAL_25000_Garond_VenzelDone_01_03 " );	// Alive, but captured by orcs. They sent him to an iron mine to extract ore.
+	AI_Output(self,other, " DIA_PAL_25000_Garond_VenzelDone_01_04 " );	// Praise Innos... (relieved) So, all is not lost! We must try to get him out of there.
+	AI_Output(other,self, " DIA_PAL_25000_Garond_VenzelDone_01_05 " );	// This is no longer necessary. In the orc mine, I found a magical portal.
+	AI_Output(other,self, " DIA_PAL_25000_Garond_VenzelDone_01_06 " );	// Wenzel, along with the rest of the slaves, will use it to escape from captivity.
+	AI_Output(other,self, " DIA_PAL_25000_Garond_VenzelDone_01_07 " );	// However, there is a suspicion that the portal leads directly to the mainland.
+	AI_Output(other,self, " DIA_PAL_25000_Garond_VenzelDone_01_08 " );	// Therefore, Wenzel asked me to tell you that most likely he will not return back to Khorinis.
+	AI_Output(self,other, " DIA_PAL_25000_Garond_VenzelDone_01_09 " );	// Well, so be it. The main thing is that everything will be fine with him.
+	AI_Output(other,self, " DIA_PAL_25000_Garond_VenzelDone_01_10 " );	// He said he would return to Myrtana and fight the orcs there.
+	AI_Output(other,self, " DIA_PAL_25000_Garond_VenzelDone_01_11 " );	// Apparently, he was right when he said that the king is no better off there than you are.
+	AI_Output(self,other, " DIA_PAL_25000_Garond_VenzelDone_01_12 " );	// Those damn orcs...(angrily) They've swept across the country like locusts!
+	AI_Output(self,other, " DIA_PAL_25000_Garond_VenzelDone_01_13 " );	// But we will fight to the last! They just won't break us.
+	AI_Output(self,other, " DIA_PAL_25000_Garond_VenzelDone_01_14 " );	// As for the situation on the mainland... (reluctantly) I won't lie to you! Yes it is.
+	AI_Output(self,other, " DIA_PAL_25000_Garond_VenzelDone_01_15 " );	// The king is doing his best to hold back the hordes of orcs that have broken through from Nordmar.
+	AI_Output(self,other, " DIA_PAL_25000_Garond_VenzelDone_01_16 " );	// But he's not good at it. There are too many orcs, and the king does not have enough people or weapons.
+	AI_Output(self,other, " DIA_PAL_25000_Garond_VenzelDone_01_17 " );	// Therefore, all hope lies with us! With the help of magic ore, it may still be possible to turn the tide of the war.
+	AI_Output(self,other, " DIA_PAL_25000_Garond_VenzelDone_01_18 " );	// But as you can see, we ourselves are struggling to fight off the orcs here.
+	AI_Output(self,other, " DIA_PAL_25000_Garond_VenzelDone_01_19 " );	// I'm not used to giving up prematurely...(doomed) But it looks like we've already lost this war.
+	AI_Output(self,other, " DIA_PAL_25000_Garond_VenzelDone_01_20 " );	// But don't you dare tell anyone about it!
+	AI_Output(self,other, " DIA_PAL_25000_Garond_VenzelDone_01_21 " );	// I won't let my soldiers lose heart just because of some stupid speculation.
+	AI_Output(other,self, " DIA_PAL_25000_Garond_VenzelDone_01_22 " );	// Of course.
 	MIS_LostPaladins = LOG_Success;
 	Log_SetTopicStatus(TOPIC_LostPaladins,LOG_Success);
-	B_LogEntry(TOPIC_LostPaladins,"Я рассказал Гаронду о судьбе Венцеля.");
+	B_LogEntry(TOPIC_LostPaladins, " I told Garond about Wenzel's fate. " );
 };
