b0b1d276f7e1bbfdf69e2c5e62b03182e66988a1
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 18:41:04 +0200

    Update DIA_STRF_8125_Addon_Nuts.d

--- RU-Main/Story/Dialoge/DIA_STRF_8125_Addon_Nuts.d
+++ RU-Main/Story/Dialoge/DIA_STRF_8125_Addon_Nuts.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_STRF_8125_Addon_Nuts_EXIT(C_Info)
 {
 	npc = STRF_8125_Addon_Nuts;
@@ -40,7 +41,7 @@ func int DIA_STRF_8125_Addon_Nuts_PreHello_condition()
 
 func void DIA_STRF_8125_Addon_Nuts_PreHello_info()
 {
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_PreHello_01_00");	//Эй, не мешай мне работать! Поговорим позже.
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_PreHello_01_00 " );	// Hey, don't disturb my work! Let's talk later.
 	AI_StopProcessInfos(self);
 };
 
@@ -51,7 +52,7 @@ instance DIA_STRF_8125_Addon_Nuts_NotWork(C_Info)
 	condition = DIA_STRF_8125_Addon_Nuts_NotWork_Condition;
 	information = DIA_STRF_8125_Addon_Nuts_NotWork_Info;
 	permanent = FALSE;
-	description = "Да я смотрю, ты тут особо и не работаешь.";
+	description = " Yes, I see, you don't really work here. " ;
 };
 
 func int DIA_STRF_8125_Addon_Nuts_NotWork_Condition()
@@ -64,16 +65,16 @@ func int DIA_STRF_8125_Addon_Nuts_NotWork_Condition()
 
 func void DIA_STRF_8125_Addon_Nuts_NotWork_Info()
 {
-	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_NotWork_01_00");	//Да я смотрю, ты тут особо и не работаешь.
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_NotWork_01_01");	//Ну...(лукаво) Только когда этот орк охранник спит.
-	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_NotWork_01_02");	//А спит он, по всей видимости, очень долго.
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_NotWork_01_03");	//Даже если так...(растерянно) Кто об этом узнает?
-	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_NotWork_01_04");	//Я мог бы рассказать про тебя оркам.
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_NotWork_01_05");	//Но ты ведь этого не сделаешь? Правда?
-	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_NotWork_01_06");	//Не сделаю. Если только поможешь мне в одном деле.
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_NotWork_01_07");	//Что тебе от меня нужно?
-	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_NotWork_01_08");	//Да ладно, расслабься! Я просто пошутил.
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_NotWork_01_09");	//Ну и шутки у тебя, приятель...(нервно) Так можно и последние нервы потерять!
+	AI_Output(other,self, " DIA_STRF_8125_Addon_Nuts_NotWork_01_00 " );	// Yes, I see, you don't really work here.
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_NotWork_01_01 " );	// Well...(slyly) Only when that orc guard is asleep.
+	AI_Output(other,self, " DIA_STRF_8125_Addon_Nuts_NotWork_01_02 " );	// And he sleeps, apparently, for a very long time.
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_NotWork_01_03 " );	// Even so...(bewildered) Who will know about this?
+	AI_Output(other,self, " DIA_STRF_8125_Addon_Nuts_NotWork_01_04 " );	// I could tell the orcs about you.
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_NotWork_01_05 " );	// But you won't do that, will you? Truth?
+	AI_Output(other,self, " DIA_STRF_8125_Addon_Nuts_NotWork_01_06 " );	// I won't. If only you could help me with one thing.
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_NotWork_01_07 " );	// What do you want from me?
+	AI_Output(other,self, " DIA_STRF_8125_Addon_Nuts_NotWork_01_08 " );	// Come on, relax! I was just joking.
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_NotWork_01_09 " );	// What a joke, buddy... (nervously) You can lose your last nerves like that!
 };
 
 instance DIA_STRF_8125_Addon_Nuts_YouHereLongTime(C_Info)
@@ -83,7 +84,7 @@ instance DIA_STRF_8125_Addon_Nuts_YouHereLongTime(C_Info)
 	condition = DIA_STRF_8125_Addon_Nuts_YouHereLongTime_Condition;
 	information = DIA_STRF_8125_Addon_Nuts_YouHereLongTime_Info;
 	permanent = FALSE;
-	description = "Ты здесь давно?";
+	description = " Have you been here long? " ;
 };
 
 func int DIA_STRF_8125_Addon_Nuts_YouHereLongTime_Condition()
@@ -96,9 +97,9 @@ func int DIA_STRF_8125_Addon_Nuts_YouHereLongTime_Condition()
 
 func void DIA_STRF_8125_Addon_Nuts_YouHereLongTime_Info()
 {
-	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_YouHereLongTime_01_00");	//Ты здесь давно?
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_YouHereLongTime_01_01");	//По правде говоря, я уже потерял счет времени. 
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_YouHereLongTime_01_02");	//Но если орки и дальше будут заставлять меня столько работать, долго я тут не протяну.
+	AI_Output(other,self, " DIA_STRF_8125_Addon_Nuts_YouHereLongTime_01_00 " );	// How long have you been here?
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_YouHereLongTime_01_01 " );	// In truth, I've already lost track of time.
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_YouHereLongTime_01_02 " );	// But if the orcs keep making me work so hard, I won't last long.
 };
 
 instance DIA_STRF_8125_Addon_Nuts_Teleport(C_Info)
@@ -108,7 +109,7 @@ instance DIA_STRF_8125_Addon_Nuts_Teleport(C_Info)
 	condition = DIA_STRF_8125_Addon_Nuts_Teleport_Condition;
 	information = DIA_STRF_8125_Addon_Nuts_Teleport_Info;
 	permanent = FALSE;
-	description = "Чем ты тут занимаешься, когда... работаешь?";
+	description = " What do you do here when... you work? " ;
 };
 
 func int DIA_STRF_8125_Addon_Nuts_Teleport_Condition()
@@ -121,17 +122,17 @@ func int DIA_STRF_8125_Addon_Nuts_Teleport_Condition()
 
 func void DIA_STRF_8125_Addon_Nuts_Teleport_Info()
 {
-	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_Teleport_01_00");	//Чем ты тут занимаешься, когда... работаешь?
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_Teleport_01_01");	//Ты будешь удивлен, но я читаю.
-	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_Teleport_01_02");	//Читаешь? Что тут можно читать?
+	AI_Output(other,self, " DIA_STRF_8125_Addon_Nuts_Teleport_01_00 " );	// What do you do here when... you work?
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_Teleport_01_01 " );	// You'll be surprised, but I'm reading.
+	AI_Output(other,self, " DIA_STRF_8125_Addon_Nuts_Teleport_01_02 " );	// Are you reading? What can be read here?
 	AI_PlayAni(self,"T_SEARCH");
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_Teleport_01_03");	//В этой пещере я нашел одну старую каменную табличку.
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_Teleport_01_04");	//Язык в ней немного похож на человеческий. Хотя точно не уверен.
-	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_Teleport_01_05");	//Дашь взглянуть?
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_Teleport_01_06");	//Эй, руки прочь! Считай, это единственная вещь, которая наполняет хоть каким-то смыслом всю мою жизнь.
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_Teleport_01_03 " );	// In this cave, I found one old stone tablet.
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_Teleport_01_04 " );	// The language in it is a bit like a human. Although I'm not exactly sure.
+	AI_Output(other,self, " DIA_STRF_8125_Addon_Nuts_Teleport_01_05 " );	// Can you take a look?
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_Teleport_01_06 " );	// Hey, hands off! Consider it the only thing that fills my whole life with at least some meaning.
 	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_Teleport_01_07");	//Даже так?
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_Teleport_01_08");	//Конечно! Ведь кроме орков и этой проклятой жилы я больше уже тут ничего не увижу.
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_Teleport_01_09");	//А так хоть какое то разнообразие!
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_Teleport_01_08 " );	// Of course! After all, apart from the orcs and this accursed vein, I will not see anything here anymore.
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_Teleport_01_09 " );	// And so at least some variety!
 };
 
 instance DIA_STRF_8125_Addon_Nuts_Want(C_Info)
@@ -141,7 +142,7 @@ instance DIA_STRF_8125_Addon_Nuts_Want(C_Info)
 	condition = DIA_STRF_8125_Addon_Nuts_Want_Condition;
 	information = DIA_STRF_8125_Addon_Nuts_Want_Info;
 	permanent = FALSE;
-	description = "Что ты хочешь за свою табличку?";
+	description = " What do you want for your sign? " ;
 };
 
 func int DIA_STRF_8125_Addon_Nuts_Want_Condition()
@@ -154,19 +155,19 @@ func int DIA_STRF_8125_Addon_Nuts_Want_Condition()
 
 func void DIA_STRF_8125_Addon_Nuts_Want_Info()
 {
-	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_Want_01_00");	//Что ты хочешь за свою табличку?
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_Want_01_01");	//Хммм...(лукаво) Вообще-то она для меня бесценна!
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_Want_01_02");	//Но раз уж ты спросил... Полагаю, что хорошая книга будет достойной заменой для нее.
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_Want_01_03");	//И желательно про звезды.
+	AI_Output(other,self, " DIA_STRF_8125_Addon_Nuts_Want_01_00 " );	// What do you want for your sign?
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_Want_01_01 " );	// Hmmm...(slyly) She's actually priceless to me!
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_Want_01_02 " );	// But since you asked... I guess a good book would be a worthy replacement for her.
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_Want_01_03 " );	// And preferably about the stars.
 	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_Want_01_04");	//Про звезды?
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_Want_01_05");	//Я всегда любил наблюдать за звездами. Даже еще когда был мальчишкой!
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_Want_01_06");	//Но судьба распорядилась иначе...(обреченно) И теперь я их вряд ли когда-либо увижу.
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_Want_01_07");	//Но зато хотя бы почитаю про них.
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_Want_01_05 " );	// I've always loved watching the stars. Even when I was a kid!
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_Want_01_06 " );	// But fate decreed otherwise... (doomed) And now I'm unlikely to ever see them.
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_Want_01_07 " );	// But at least I'll read about them.
 	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_Want_01_08");	//Понимаю.
 	MIS_Astronomy = LOG_Running;
 	Log_CreateTopic(TOPIC_Astronomy,LOG_MISSION);
 	Log_SetTopicStatus(TOPIC_Astronomy,LOG_Running);
-	B_LogEntry(TOPIC_Astronomy,"В обмен на свою табличку Нутс хочет заполучить какую-нибудь книгу - и желательно про звезды.");
+	B_LogEntry(TOPIC_Astronomy, " In exchange for his tablet, Nuts wants to get some book - and preferably about the stars. " );
 };
 
 instance DIA_STRF_8125_Addon_Nuts_WantDone(C_Info)
@@ -176,7 +177,7 @@ instance DIA_STRF_8125_Addon_Nuts_WantDone(C_Info)
 	condition = DIA_STRF_8125_Addon_Nuts_WantDone_Condition;
 	information = DIA_STRF_8125_Addon_Nuts_WantDone_Info;
 	permanent = FALSE;
-	description = "Вот нужная тебе книга.";
+	description = " Here's the book you need. " ;
 };
 
 func int DIA_STRF_8125_Addon_Nuts_WantDone_Condition()
@@ -190,23 +191,23 @@ func int DIA_STRF_8125_Addon_Nuts_WantDone_Condition()
 func void DIA_STRF_8125_Addon_Nuts_WantDone_Info()
 {
 	B_GivePlayerXP(300);
-	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_WantDone_01_00");	//Вот нужная тебе книга.
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_WantDone_01_01");	//Хммм...(недоверчиво) А она хоть интересная?
-	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_WantDone_01_02");	//Ну, все как ты и просил. Про звезды там...
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_WantDone_01_03");	//Лучше дай я посмотрю.
+	AI_Output(other,self, " DIA_STRF_8125_Addon_Nuts_WantDone_01_00 " );	// Here's the book you need.
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_WantDone_01_01 " );	// Hmmm...(incredulously) Is she even interesting?
+	AI_Output(other,self, " DIA_STRF_8125_Addon_Nuts_WantDone_01_02 " );	// Well, everything is as you asked. About the stars...
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_WantDone_01_03 " );	// Better let me take a look.
 	B_GiveInvItems(other,self,ASTRONOMIE,1);
 	Npc_RemoveInvItems(self,ASTRONOMIE,1);
 	B_UseFakeScroll();
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_WantDone_01_04");	//Отлично, приятель! Похоже, что именно это мне и надо!
-	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_WantDone_01_05");	//А как насчет нашего уговора?
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_WantDone_01_06");	//Все в силе! Вот - держи ту табличку.
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_WantDone_01_04 " );	// Great, mate! It looks like this is exactly what I need!
+	AI_Output(other,self, " DIA_STRF_8125_Addon_Nuts_WantDone_01_05 " );	// What about our deal?
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_WantDone_01_06 " );	// All is good! Here, hold that sign.
 	B_GiveInvItems(self,other,ItWr_OldTextMine,1);
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_WantDone_01_07");	//А книгу, значит, я оставлю себе, да?
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_WantDone_01_07 " );	// And the book, then, I'll keep myself, right?
 	AI_Output(other,self,"DIA_STRF_8125_Addon_Nuts_WantDone_01_08");	//Конечно.
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_WantDone_01_09");	//Кстати, этой табличкой интересовался в свое время Кроу.
-	AI_Output(self,other,"DIA_STRF_8125_Addon_Nuts_WantDone_01_10");	//Ну, это я так, к слову...
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_WantDone_01_09 " );	// By the way, Crowe was interested in this tablet at one time.
+	AI_Output(self,other, " DIA_STRF_8125_Addon_Nuts_WantDone_01_10 " );	// Well, that's me, by the way...
 	RT_Respect = RT_Respect + 1;
 	MIS_Astronomy = LOG_Success;
 	Log_SetTopicStatus(TOPIC_Astronomy,LOG_Success);
-	B_LogEntry(TOPIC_Astronomy,"Я принес книгу Нутсу.");
+	B_LogEntry(TOPIC_Astronomy, " I brought the book to Nuts. " );
 };
