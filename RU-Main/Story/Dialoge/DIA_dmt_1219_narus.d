7d74d5368504066d7eb253f3f95c08c6052ab30d
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 22:27:04 +0200

    Update DIA_dmt_1219_narus.d

--- RU-Main/Story/Dialoge/DIA_dmt_1219_narus.d
+++ RU-Main/Story/Dialoge/DIA_dmt_1219_narus.d
@@ -1,5 +1,6 @@
 
-instance DMT_1219_NARUS_EXIT(C_Info)
+
+instance DMT_1219_NARUS_EXIT (C_Info)
 {
 	npc = dmt_1219_narus;
 	nr = 999;
@@ -34,7 +35,7 @@ instance DMT_1219_NARUS_HELLO(C_Info)
 
 func int dmt_1219_narus_hello_condition()
 {
-	if((MIS_GUARDIANSTEST == LOG_Running) && (GADER_AGREE == TRUE))
+	if (( MY_GUARDIANTEST  == LOG_Running) && ( STREETS_AGREE  ==  TRUE ))
 	{
 		return TRUE;
 	};
@@ -42,42 +43,42 @@ func int dmt_1219_narus_hello_condition()
 
 func void dmt_1219_narus_hello_info()
 {
-	AI_Output(self,other,"DMT_1219_Narus_Hello_00");	//(вкрадчиво) Итак, ты здесь.
-	AI_Output(self,other,"DMT_1219_Narus_Hello_01");	//Честно говоря, я немного удивлен нашей встрече. Не думал, что тебе удастся зайти так далеко!
-	AI_Output(self,other,"DMT_1219_Narus_Hello_02");	//Но, по всей видимости, какая-то божественная сила хранит тебя от опасностей и помогает преодолеть тебе этот нелегкий путь.
-	AI_Output(self,other,"DMT_1219_Narus_Hello_03");	//Что же, это хороший знак.
+	AI_Output(self,other, " DMT_1219_Narus_Hello_00 " );	// (smartly) So, here you are.
+	AI_Output(self,other, " DMT_1219_Narus_Hello_01 " );	// Honestly, I'm a little surprised by our meeting. I didn't think you could get this far!
+	AI_Output(self,other, " DMT_1219_Narus_Hello_02 " );	// But, apparently, some divine power keeps you from danger and helps you overcome this difficult path.
+	AI_Output(self,other, " DMT_1219_Narus_Hello_03 " );	// Well, that's a good sign.
 	AI_Output(other,self,"DMT_1219_Narus_Hello_04");	//Кто ты?
-	AI_Output(self,other,"DMT_1219_Narus_Hello_05");	//(могущественно) Я - Нарус. Второй страж священных вод Адана и шестой Хранитель священных врат!
-	AI_Output(self,other,"DMT_1219_Narus_Hello_06");	//И я здесь для того, чтобы предложить тебе пройти твое следующее испытание.
-	AI_Output(self,other,"DMT_1219_Narus_Hello_07");	//Конечно, глядя на уже пройденный тобою путь, может показаться, что мое поручение не составит для тебя особого труда.
-	AI_Output(self,other,"DMT_1219_Narus_Hello_08");	//Ты уже многим доказал на что действительно способен, и, думаю, сам сейчас полон уверености в том, что уже практически достиг своей основной цели.
-	AI_Output(self,other,"DMT_1219_Narus_Hello_09");	//Теперь настало время пройти мое испытание. Ты готов?
+	AI_Output(self,other, " DMT_1219_Narus_Hello_05 " );	// (powerfully) I am Narus. The second guardian of the sacred waters of Adana and the sixth guardian of the sacred gates!
+	AI_Output(self,other, " DMT_1219_Narus_Hello_06 " );	// And I'm here to challenge you for your next test.
+	AI_Output(self,other, " DMT_1219_Narus_Hello_07 " );	// Of course, looking at the path you have already traveled, it may seem that my assignment will not be difficult for you.
+	AI_Output(self,other, " DMT_1219_Narus_Hello_08 " );	// You have already proven to many people what you are really capable of, and, I think, you yourself are now full of confidence that you have almost reached your main goal.
+	AI_Output(self,other, " DMT_1219_Narus_Hello_09 " );	// Now it's time to pass my test. Are you ready?
 	Info_ClearChoices(dmt_1219_narus_hello);
-	Info_AddChoice(dmt_1219_narus_hello,"Да, я готов.",dmt_1219_narus_hello_test);
+	Info_AddChoice(dmt_1219_narus_hello, " Yes, I'm ready . " ,dmt_1219_narus_hello_test);
 };
 
 func void dmt_1219_narus_hello_test()
 {
-	AI_Output(other,self,"DMT_1219_Narus_Hello_10");	//Да, я готов.
+	AI_Output(other,self, " DMT_1219_Narus_Hello_10 " );	// Yes, I'm ready.
 	Wld_PlayEffect("spellFX_INCOVATION_BLUE",self,self,0,0,0,FALSE);
 	Wld_PlayEffect("FX_EarthQuake",self,self,0,0,0,FALSE);
 	Wld_PlayEffect("SFX_Circle",self,self,0,0,0,FALSE);
 	AI_PlayAni(self,"T_PRACTICEMAGIC5");
-	AI_Output(self,other,"DMT_1219_Narus_Hello_11");	//Тогда слушай меня внимательно.
-	AI_Output(self,other,"DMT_1219_Narus_Hello_12");	//Стоя здесь и ожидая твоего скорого появления, я глубоко размышлял о ходе времени и о том, как оно скоротечно уплывает от нас, словно песок сквозь пальцы.
-	AI_Output(self,other,"DMT_1219_Narus_Hello_13");	//В своих раздумьях я, видимо, совершенно не заметил того, как случайно обронил вниз свое сапфировое кольцо.
-	AI_Output(self,other,"DMT_1219_Narus_Hello_14");	//Думаю, оно упало в тот глубокий пруд, что под нами...
-	AI_Output(self,other,"DMT_1219_Narus_Hello_15");	//Эта вещь очень дорога для меня, и я хочу ее вернуть!
-	AI_Output(self,other,"DMT_1219_Narus_Hello_16");	//Поэтому не удивляйся, если это испытание будет, скорее всего, звучать для тебя как обычная просьба.
-	AI_Output(self,other,"DMT_1219_Narus_Hello_17");	//И она проста: найди мое потерянное кольцо и принеси его обратно мне.
-	AI_Output(other,self,"DMT_1219_Narus_Hello_18");	//Хорошо. Я найду твое кольцо, мастер.
-	AI_Output(self,other,"DMT_1219_Narus_Hello_19");	//Тогда я жду тебя с ним. Ступай!
+	AI_Output(self,other, " DMT_1219_Narus_Hello_11 " );	// Then listen to me carefully.
+	AI_Output(self,other, " DMT_1219_Narus_Hello_12 " );	// As I stood here, waiting for you to arrive soon, I thought deeply about the passage of time and how it slips away from us like sand through our fingers.
+	AI_Output(self,other, " DMT_1219_Narus_Hello_13 " );	// In my thoughts, I apparently did not notice at all how I accidentally dropped my sapphire ring down.
+	AI_Output(self,other, " DMT_1219_Narus_Hello_14 " );	// I think it fell into that deep pool below us...
+	AI_Output(self,other, " DMT_1219_Narus_Hello_15 " );	// This thing is very dear to me and I want it back!
+	AI_Output(self,other, " DMT_1219_Narus_Hello_16 " );	// So don't be surprised if this test is likely to sound like a simple request to you.
+	AI_Output(self,other, " DMT_1219_Narus_Hello_17 " );	// And it's simple: find my lost ring and bring it back to me.
+	AI_Output(other,self, " DMT_1219_Narus_Hello_18 " );	// Good. I will find your ring, master.
+	AI_Output(self,other, " DMT_1219_Narus_Hello_19 " );	// Then I'll wait for you with him. Go!
 	AI_StopProcessInfos(self);
 	MIS_NARUSTEST = LOG_Running;
-	Wld_InsertItem(itri_narus,"FP_ITEM_NARUSRING");
+	Wld_InsertItem(itri_narus, " FP_ITEM_NARUSRING " );
 	Log_CreateTopic(TOPIC_NARUSTEST,LOG_MISSION);
 	Log_SetTopicStatus(TOPIC_NARUSTEST,LOG_Running);
-	B_LogEntry(TOPIC_NARUSTEST,"Хранитель Нарус попросил найти его кольцо, которое он случайно обронил в глубокий пруд рядом с фермой Бенгара. Мне кажется, Хранитель передумал испытывать меня, если дал такое пустяковое задание.");
+	; _ _ _ _ _ _
 };
 
 
@@ -88,13 +89,13 @@ instance DMT_1219_NARUS_TEST(C_Info)
 	condition = dmt_1219_narus_test_condition;
 	information = dmt_1219_narus_test_info;
 	permanent = FALSE;
-	description = "Я нашел твое кольцо.";
+	description = " I found your ring. " ;
 };
 
 
 func int dmt_1219_narus_test_condition()
 {
-	if((MIS_GUARDIANSTEST == LOG_Running) && (MIS_NARUSTEST == LOG_Running) && (Npc_HasItems(other,itri_narus) >= 1))
+	if (( MY_GUARDIANTEST  == LOG_Running ) && ( MY_NARUTEST  == LOG_Running ) && ( Npc_HasItems ( other , itri_narus ) >=  1 )) .
 	{
 		return TRUE;
 	};
@@ -103,45 +104,45 @@ func int dmt_1219_narus_test_condition()
 func void dmt_1219_narus_test_info()
 {
 	B_GivePlayerXP(100);
-	AI_Output(other,self,"DMT_1219_Narus_Test_00");	//Я нашел твое кольцо.
-	AI_Output(self,other,"DMT_1219_Narus_Test_01");	//Хорошо. Раз так - покажи!
-	AI_Output(other,self,"DMT_1219_Narus_Test_02");	//Вот, смотри.
-	AI_Output(self,other,"DMT_1219_Narus_Test_03");	//Хммм. Да, похоже, здесь сомнений быть не может, - это действительно оно!
-	AI_Output(other,self,"DMT_1219_Narus_Test_04");	//Тогда я возвращаю это кольцо тебе, мастер.
-	AI_Output(self,other,"DMT_1219_Narus_Test_05");	//Постой! Не спеши отдавать его мне. Мне кажется, что будет лучше, если эта вещь пока немного побудет у тебя.
-	AI_Output(self,other,"DMT_1219_Narus_Test_06");	//(вкрадчиво) Я уверен, что она тебе и самому еще не раз пригодится. Ты можешь в этом не сомневаться!
-	AI_Output(self,other,"DMT_1219_Narus_Test_07");	//Думаю, что это не первая подобная вещь, которую ты получил от Хранителей?
-	AI_Output(other,self,"DMT_1219_Narus_Test_08");	//Да, ты прав. Хранитель Дагот тоже подарил мне одно кольцо.
-	AI_Output(other,self,"DMT_1219_Narus_Test_09");	//Правда, он тогда так и не объяснил мне, зачем оно нужно.
-	AI_Output(self,other,"DMT_1219_Narus_Test_10");	//Всему свой черед, герой времен...(вкрадчиво) Ты еще успеешь познать истинный смысл этих колец.
-	AI_Output(self,other,"DMT_1219_Narus_Test_11");	//Но это произойдет только тогда, когда придет время.
-	AI_Output(other,self,"DMT_1219_Narus_Test_12");	//Я все понял, мастер.
-	AI_Output(self,other,"DMT_1219_Narus_Test_13");	//(могущественно) Хорошо. Теперь же вернемся к твоему испытанию.
-	AI_Output(self,other,"DMT_1219_Narus_Test_14");	//Как я и ожидал, тебе удалось выполнить все поставленные мной условия для того, чтобы успешно пройти его.
-	AI_Output(self,other,"DMT_1219_Narus_Test_15");	//И ты справился!
-	MIS_NARUSTEST = LOG_SUCCESS;
+	AI_Output(other,self, " DMT_1219_Narus_Test_00 " );	// I found your ring.
+	AI_Output(self,other, " DMT_1219_Narus_Test_01 " );	// Good. If so, show me!
+	AI_Output(other,self, " DMT_1219_Narus_Test_02 " );	// Here, look.
+	AI_Output(self,other, " DMT_1219_Narus_Test_03 " );	// Hmmm. Yes, it seems that there can be no doubts here - this is really it!
+	AI_Output(other,self, " DMT_1219_Narus_Test_04 " );	// Then I return this ring to you, master.
+	AI_Output(self,other, " DMT_1219_Narus_Test_05 " );	// Wait! Don't rush to give it to me. It seems to me that it would be better if this thing stays with you for a while.
+	AI_Output(self,other, " DMT_1219_Narus_Test_06 " );	// (smartly) I'm sure it will come in handy for you and yourself more than once. You can be sure of it!
+	AI_Output(self,other, " DMT_1219_Narus_Test_07 " );	// I'm guessing this isn't the first item you've received from the Guardians?
+	AI_Output(other,self, " DMT_1219_Narus_Test_08 " );	// Yes, you're right. Guardian Dagoth also gave me one ring.
+	AI_Output(other,self, " DMT_1219_Narus_Test_09 " );	// True, he never explained to me why it was needed.
+	AI_Output(self,other, " DMT_1219_Narus_Test_10 " );	// Everything in its turn, hero of the times... (insinuatingly) You still have time to know the true meaning of these rings.
+	AI_Output(self,other, " DMT_1219_Narus_Test_11 " );	// But this will only happen when the time is right.
+	AI_Output(other,self, " DMT_1219_Narus_Test_12 " );	// I got it, master.
+	AI_Output(self,other, " DMT_1219_Narus_Test_13 " );	// (powerfully) Good. Now back to your test.
+	AI_Output(self,other, " DMT_1219_Narus_Test_14 " );	// As I expected, you managed to fulfill all the conditions I set in order to successfully pass it.
+	AI_Output(self,other, " DMT_1219_Narus_Test_15 " );	// And you did it!
+	MIS_NARUSTEST = LOG_SUCCESS ;
 	Log_SetTopicStatus(TOPIC_NARUSTEST,LOG_SUCCESS);
-	B_LogEntry(TOPIC_NARUSTEST,"Хранитель Нарус был доволен тем, что мне удалось отыскать его кольцо. В награду он подарил его мне и признал пройденным его испытание.");
+	B_LogEntry( TOPIC_NARUSTEST , " Keeper Narus was pleased that I managed to find his ring. As a reward, he gave it to me and recognized that he passed his test. " );
 	Info_ClearChoices(dmt_1219_narus_test);
-	Info_AddChoice(dmt_1219_narus_test,"Значит, теперь я могу рассчитывать на твое согласие?",dmt_1219_narus_test_pass);
+	Info_AddChoice(dmt_1219_narus_test, " So now I can count on your consent? " ,dmt_1219_narus_test_pass);
 };
 
 func void dmt_1219_narus_test_pass()
 {
-	AI_Output(other,self,"DMT_1219_Narus_Test_16");	//Значит, теперь я могу рассчитывать на твое согласие?
+	AI_Output(other,self, " DMT_1219_Narus_Test_16 " );	// So now I can count on your consent?
 	Wld_PlayEffect("spellFX_INCOVATION_BLUE",self,self,0,0,0,FALSE);
 	Wld_PlayEffect("FX_EarthQuake",self,self,0,0,0,FALSE);
 	Wld_PlayEffect("SFX_Circle",self,self,0,0,0,FALSE);
 	AI_PlayAni(self,"T_PRACTICEMAGIC5");
-	B_LogEntry(TOPIC_GUARDIANSTEST,"Я смог заручиться согласием Хранителя Наруса.");
-	NARUS_AGREE = TRUE;
-	AI_Output(self,other,"DMT_1219_Narus_Test_17");	//(властно) Безусловно, так! И властью, данной мне священными водами Адана, я даю его тебе!
-	AI_Output(self,other,"DMT_1219_Narus_Test_18");	//Пусть это поможет сделать тебе еще один шаг по тому пути, что ты избрал для себя.
-	AI_Output(self,other,"DMT_1219_Narus_Test_19");	//Теперь же ступай. Ищи последнего Хранителя воды - Вакона.
-	AI_Output(self,other,"DMT_1219_Narus_Test_20");	//Он даст тебе твое последнее испытание, после чего ты сможешь встретиться с главой нашего Круга.
+	B_LogEntry( TOPIC_GUARDIANSTEST , " I was able to secure the consent of Guardian Narusa. " );
+	NARUS_AGREE = TRUE ;
+	AI_Output(self,other, " DMT_1219_Narus_Test_17 " );	// (imperiously) Of course, so! And by the power given to me by the sacred waters of Adan, I give it to you!
+	AI_Output(self,other, " DMT_1219_Narus_Test_18 " );	// Let this help you take one more step on the path that you have chosen for yourself.
+	AI_Output(self,other, " DMT_1219_Narus_Test_19 " );	// Now go. Look for the last Guardian of Water - Wakon.
+	AI_Output(self,other, " DMT_1219_Narus_Test_20 " );	// He will give you your final test, after which you can meet with the head of our Circle.
 	AI_Output(self,other,"DMT_1219_Narus_Test_21");	//Прощай!
 	Info_ClearChoices(dmt_1219_narus_test);
-	Info_AddChoice(dmt_1219_narus_test,"(закончить разговор)",dmt_1219_narus_test_end);
+	Info_AddChoice(dmt_1219_narus_test, " (end conversation) " ,dmt_1219_narus_test_end);
 };
 
 func void dmt_1219_narus_test_end()
