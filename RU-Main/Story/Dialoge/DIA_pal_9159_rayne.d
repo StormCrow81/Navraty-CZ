ef9937078f4e09fc131df5584353f1cafbeda54b
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 13:40:26 +0200

    Update DIA_pal_9159_rayne.d

--- RU-Main/Story/Dialoge/DIA_pal_9159_rayne.d
+++ RU-Main/Story/Dialoge/DIA_pal_9159_rayne.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_PAL_9159_RAYNE_EXIT(C_Info)
 {
 	npc = pal_9159_rayne;
@@ -28,7 +29,7 @@ instance DIA_PAL_9159_RAYNE_MATTER(C_Info)
 	condition = dia_pal_9159_rayne_matter_condition;
 	information = dia_pal_9159_rayne_matter_info;
 	permanent = FALSE;
-	description = "Привет, ты кто?";
+	description = " Hi, who are you? " ;
 };
 
 
@@ -40,12 +41,12 @@ func int dia_pal_9159_rayne_matter_condition()
 func void dia_pal_9159_rayne_matter_info()
 {
 	AI_Output(other,self,"DIA_Pal_9159_Rayne_Matter_01_00");	//Ты кто?
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_Matter_01_01");	//Меня зовут Рэйн, и если у тебя хорошо с глазами, то, думаю, ты заметил, что я паладин короля.
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_Matter_01_03");	//Лорд Варус назначил меня управляющим складом тут в форте.
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_Matter_01_04");	//(раздраженно) Как будто кроме меня этим больше и заняться некому! 
-	AI_Output(other,self,"DIA_Pal_9159_Rayne_Matter_01_06");	//Кажется, тебе не очень нравится твоя работа?
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_Matter_01_07");	//А что тут может понравиться? Каждый день только и делаю, что проверяю содержимое этих ящиков.
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_Matter_01_13");	//Так что если у тебя ко мне нет больше никаких вопросов - оставь меня в покое и позволь заняться своими обязанностями!
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_Matter_01_01 " );	// My name is Rain, and if you're good with your eyes, then I think you've noticed that I'm a paladin of the king.
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_Matter_01_03 " );	// Lord Varus has appointed me warehouse manager here at the fort.
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_Matter_01_04 " );	// (annoyed) As if there's no one else to do this but me!
+	AI_Output(other,self, " DIA_Pal_9159_Rayne_Matter_01_06 " );	// You don't seem to like your job very much?
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_Matter_01_07 " );	// And what can you like here? Every day, all I do is check the contents of these boxes.
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_Matter_01_13 " );	// So if you don't have any more questions for me, leave me alone and let me do my duty!
 };
 
 
@@ -56,7 +57,7 @@ instance DIA_PAL_9159_RAYNE_SKLADWATCH(C_Info)
 	condition = dia_pal_9159_rayne_skladwatch_condition;
 	information = dia_pal_9159_rayne_skladwatch_info;
 	permanent = TRUE;
-	description = "Ничего не пропало?";
+	description = " Nothing missing? " ;
 };
 
 
@@ -70,16 +71,16 @@ func int dia_pal_9159_rayne_skladwatch_condition()
 
 func void dia_pal_9159_rayne_skladwatch_info()
 {
-	AI_Output(other,self,"DIA_Pal_9159_Rayne_SkladWatch_01_00");	//Ничего не пропало?
+	AI_Output(other,self, " DIA_Pal_9159_Rayne_SkladWatch_01_00 " );	// Nothing missing?
 	if(MIS_RAYNEHELP == LOG_SUCCESS)
 	{
-		AI_Output(self,other,"DIA_Pal_9159_Rayne_SkladWatch_01_01");	//Теперь, когда у меня есть помощник, об этом можно не беспокоиться. (смеется)
-		AI_Output(self,other,"DIA_Pal_9159_Rayne_SkladWatch_01_02");	//Все в полном порядке!
+		AI_Output(self,other, " DIA_Pal_9159_Rayne_SkladWatch_01_01 " );	// Now that I have a helper, I don't have to worry about this. (laughs)
+		AI_Output(self,other, " DIA_Pal_9159_Rayne_SkladWatch_01_02 " );	// Everything is fine!
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_Pal_9159_Rayne_SkladWatch_01_04");	//Очень смешно, умник!
-		AI_Output(self,other,"DIA_Pal_9159_Rayne_SkladWatch_01_05");	//Тебе бы такую работенку - может, тогда ты бы понял, что это такое.
+		AI_Output(self,other, " DIA_Pal_9159_Rayne_SkladWatch_01_04 " );	// Very funny, smart guy!
+		AI_Output(self,other, " DIA_Pal_9159_Rayne_SkladWatch_01_05 " );	// You should have such a job - maybe then you would understand what it is.
 	};
 };
 
@@ -91,7 +92,7 @@ instance DIA_PAL_9159_RAYNE_SKLADGOODS(C_Info)
 	condition = dia_pal_9159_rayne_skladgoods_condition;
 	information = dia_pal_9159_rayne_skladgoods_info;
 	permanent = FALSE;
-	description = "А что хранится на складе?";
+	description = " What is stored in the warehouse? " ;
 };
 
 
@@ -105,12 +106,12 @@ func int dia_pal_9159_rayne_skladgoods_condition()
 
 func void dia_pal_9159_rayne_skladgoods_info()
 {
-	AI_Output(other,self,"DIA_Pal_9159_Rayne_SkladGoods_01_00");	//А что хранится на складе?
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_SkladGoods_01_03");	//Да барахло всякое - оружие, припасы, одежда, ну и прочая ерунда.
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_SkladGoods_01_05");	//Вижу по глазам, что эта информация тебя немного заинтересовала.
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_SkladGoods_01_06");	//Но только можешь губу особо не раскатывать - тебе туда все равно не попасть.
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_SkladGoods_01_08");	//Склад закрыт, а ключ находится у самого лорда Варуса.
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_SkladGoods_01_10");	//Так что выбрось эту затею из головы!
+	AI_Output(other,self, " DIA_Pal_9159_Rayne_SkladGoods_01_00 " );	// And what is stored in the warehouse?
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_SkladGoods_01_03 " );	// Yes, all kinds of junk - weapons, supplies, clothes, and other nonsense.
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_SkladGoods_01_05 " );	// I can see from your eyes that you are a little interested in this information.
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_SkladGoods_01_06 " );	// But if you don't roll your lip too much, you won't get there anyway.
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_SkladGoods_01_08 " );	// The warehouse is closed, and the key is with Lord Varus himself.
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_SkladGoods_01_10 " );	// So get this idea out of your head!
 	KNOWSVARUSKEY = TRUE;
 };
 
@@ -122,7 +123,7 @@ instance DIA_PAL_9159_RAYNE_HELP(C_Info)
 	condition = dia_pal_9159_rayne_help_condition;
 	information = dia_pal_9159_rayne_help_info;
 	permanent = FALSE;
-	description = "У тебя есть серебряные кубки?";
+	description = " Do you have silver goblets? " ;
 };
 
 
@@ -136,39 +137,39 @@ func int dia_pal_9159_rayne_help_condition()
 
 func void dia_pal_9159_rayne_help_info()
 {
-	AI_Output(other,self,"DIA_Pal_9159_Rayne_Help_01_00");	//У тебя есть серебряные кубки?
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_Help_01_01");	//(задумался) Ну да, кажется, на складе есть пара штук.
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_Help_01_02");	//А с чего вдруг ты этим заинтересовался?
-	AI_Output(other,self,"DIA_Pal_9159_Rayne_Help_01_03");	//Паладин Фердинанд попросил меня достать для него один такой кубок.
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_Help_01_10");	//Послушай, эти кубки - собственность ордена паладинов.
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_Help_01_11");	//Поэтому забудь об этом.
-	AI_Output(other,self,"DIA_Pal_9159_Rayne_Help_01_12");	//Может, сделаешь исключение?
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_Help_01_14");	//Пойми - я бы с удовольствием помог Фердинанду, но если Варус узнает о том, что я торгую вещами со склада - он убьет меня!
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_Help_01_15");	//Все только и делают, что ноют и обижаются на меня!
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_Help_01_16");	//А вместо этого лучше бы кто-нибудь из них вызвался мне помочь управляться со складом.
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_Help_01_17");	//Один я тут просто не справляюсь. Но это уже никого не интересует.
-	AI_Output(other,self,"DIA_Pal_9159_Rayne_Help_01_18");	//Мне кажется, что тебе бы не помешал помощник.
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_Help_01_19");	//(печально) Это точно.
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_Help_01_21");	//Вот только что скажет Варус? Думаю, он не даст людей для этой работы.
+	AI_Output(other,self, " DIA_Pal_9159_Rayne_Help_01_00 " );	// Do you have silver goblets?
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_Help_01_01 " );	// (thinking) Well, yes, it looks like there are a couple of pieces in stock.
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_Help_01_02 " );	// And why are you suddenly interested in this?
+	AI_Output(other,self, " DIA_Pal_9159_Rayne_Help_01_03 " );	// Paladin Ferdinand asked me to get him one of these cups.
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_Help_01_10 " );	// Look, these cups are the property of the paladin order.
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_Help_01_11 " );	// So forget about it.
+	AI_Output(other,self, " DIA_Pal_9159_Rayne_Help_01_12 " );	// Can you throw an exception?
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_Help_01_14 " );	// Understand - I would love to help Ferdinand, but if Varus finds out that I sell things from the warehouse - he will kill me!
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_Help_01_15 " );	// All they do is whine and take offense at me!
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_Help_01_16 " );	// Instead, it would be better if one of them volunteered to help me manage the warehouse.
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_Help_01_17 " );	// I just can't handle it alone. But this is of no interest to anyone.
+	AI_Output(other,self, " DIA_Pal_9159_Rayne_Help_01_18 " );	// I think you could use a helper.
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_Help_01_19 " );	// (sadly) That's right.
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_Help_01_21 " );	// Just what does Varus say? I think he will not give people for this work.
 	Info_ClearChoices(dia_pal_9159_rayne_help);
-	Info_AddChoice(dia_pal_9159_rayne_help,"Может, я смогу помочь?",dia_pal_9159_rayne_help_ok);
-	Info_AddChoice(dia_pal_9159_rayne_help,"Тогда не буду мешать.",dia_pal_9159_rayne_help_no);
+	Info_AddChoice(dia_pal_9159_rayne_help, " Can I help you? " ,dia_pal_9159_rayne_help_ok);
+	Info_AddChoice(dia_pal_9159_rayne_help, " Then I won't interfere. " ,dia_pal_9159_rayne_help_no);
 };
 
 func void dia_pal_9159_rayne_help_ok()
 {
-	AI_Output(other,self,"DIA_Pal_9159_Rayne_Help_Ok_01_00");	//Я что-нибудь придумаю.
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_Help_Ok_01_09");	//Хорошо. Может, у тебя что-нибудь и получится.
+	AI_Output(other,self, " DIA_Pal_9159_Rayne_Help_Ok_01_00 " );	// I'll think of something.
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_Help_Ok_01_09 " );	// Good. Maybe you can work something out.
 	MIS_RAYNEHELP = LOG_Running;
 	Log_CreateTopic(TOPIC_RAYNEHELP,LOG_MISSION);
 	Log_SetTopicStatus(TOPIC_RAYNEHELP,LOG_Running);
-	B_LogEntry(TOPIC_RAYNEHELP,"Я пообещал паладину Рэйну найти для него помощника для работы на складе.");
+	B_LogEntry( TOPIC_RAYNEHELP , " I promised paladin Rayne to find a helper for him to work in the warehouse. " );
 	Info_ClearChoices(dia_pal_9159_rayne_help);
 };
 
 func void dia_pal_9159_rayne_help_no()
 {
-	AI_Output(other,self,"DIA_Pal_9159_Rayne_Help_No_01_00");	//Тогда тебе придется работать самому - не буду мешать.
+	AI_Output(other,self, " DIA_Pal_9159_Rayne_Help_No_01_00 " );	// Then you'll have to work on your own - I won't interfere.
 	AI_Output(self,other,"DIA_Pal_9159_Rayne_Help_No_01_01");	//Хммм...
 	Info_ClearChoices(dia_pal_9159_rayne_help);
 };
@@ -181,7 +182,7 @@ instance DIA_PAL_9159_RAYNE_HELPDONE(C_Info)
 	condition = dia_pal_9159_rayne_helpdone_condition;
 	information = dia_pal_9159_rayne_helpdone_info;
 	permanent = FALSE;
-	description = "Лорд Варус пришлет тебе человека.";
+	description = " Lord Varus will send you a man. " ;
 };
 
 
@@ -195,24 +196,24 @@ func int dia_pal_9159_rayne_helpdone_condition()
 
 func void dia_pal_9159_rayne_helpdone_info()
 {
-	AI_Output(other,self,"DIA_Pal_9159_Rayne_HelpDone_01_00");	//Лорд Варус пришлет тебе человека.
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_HelpDone_01_04");	//Отлично! Спасибо, парень.
-	AI_Output(self,other,"DIA_Pal_9159_Rayne_HelpDone_01_05");	//Если честно, я даже особо и не рассчитывал на твою помощь.
+	AI_Output(other,self, " DIA_Pal_9159_Rayne_HelpDone_01_00 " );	// Lord Varus will send you a man.
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_HelpDone_01_04 " );	// Great! Thanks boy.
+	AI_Output(self,other, " DIA_Pal_9159_Rayne_HelpDone_01_05 " );	// To be honest, I didn't even really count on your help.
 	if(MIS_FERDSILVERCUP == LOG_Running)
 	{
-		AI_Output(self,other,"DIA_Pal_9159_Rayne_HelpDone_01_08");	//Вот - возьми этот серебряный кубок, что ты искал.
+		AI_Output(self,other, " DIA_Pal_9159_Rayne_HelpDone_01_08 " );	// Here, take this silver goblet you were looking for.
 		B_GiveInvItems(self,other,ItMi_SilverCup,1);
-		AI_Output(self,other,"DIA_Pal_9159_Rayne_HelpDone_01_09");	//Думаю, тебе он пригодится.
+		AI_Output(self,other, " DIA_Pal_9159_Rayne_HelpDone_01_09 " );	// I think you'll need it.
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_Pal_9159_Rayne_HelpDone_01_12");	//Вот - возьми это растение. Говорят, оно встречается чрезвычайно редко.
+		AI_Output(self,other, " DIA_Pal_9159_Rayne_HelpDone_01_12 " );	// Here, take this plant. It is said to be extremely rare.
 		B_GiveInvItems(self,other,ItPl_Perm_Herb,1);
-		AI_Output(self,other,"DIA_Pal_9159_Rayne_HelpDone_01_13");	//Наверняка эта вещица тебе пригодится.
+		AI_Output(self,other, " DIA_Pal_9159_Rayne_HelpDone_01_13 " );	// I'm sure you'll need this item.
 	};
 	MIS_RAYNEHELP = LOG_SUCCESS;
 	Log_SetTopicStatus(TOPIC_RAYNEHELP,LOG_SUCCESS);
-	B_LogEntry(TOPIC_RAYNEHELP,"Рэйн был счастлив узнать, что лорд Варус пришлет ему человека в помощь.");
+	B_LogEntry( TOPIC_RAYNEHELP , " Rayne was happy to know that Lord Varus would send a man to help him. " );
 	Wld_InsertNpc(pal_9162_ritter,"LGR_ZUMBANVIERTEL_02");
 };
 
@@ -230,7 +231,7 @@ instance DIA_PAL_9159_RAYNE_PICKPOCKET(C_Info)
 
 func int dia_pal_9159_rayne_pickpocket_condition()
 {
-	return C_Beklauen(80,150);
+	return  C_Robbery ( 80 , 150 );
 };
 
 func void dia_pal_9159_rayne_pickpocket_info()
@@ -242,7 +243,7 @@ func void dia_pal_9159_rayne_pickpocket_info()
 
 func void dia_pal_9159_rayne_pickpocket_doit()
 {
-	B_Beklauen();
+	B_Robbery();
 	Info_ClearChoices(dia_pal_9159_rayne_pickpocket);
 };
 
