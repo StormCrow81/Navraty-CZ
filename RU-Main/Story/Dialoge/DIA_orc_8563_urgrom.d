e4362ada6b818d923c03e94e2d6702634a32196b
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 19:28:53 +0200

    Update DIA_orc_8563_urgrom.d

--- RU-Main/Story/Dialoge/DIA_orc_8563_urgrom.d
+++ RU-Main/Story/Dialoge/DIA_orc_8563_urgrom.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_ORC_8563_URGROM_EXIT(C_Info)
 {
 	npc = orc_8563_urgrom;
@@ -29,7 +30,7 @@ instance DIA_ORC_8563_URGROM_HELLO_WAIT(C_Info)
 	information = dia_orc_8563_urgrom_hello_wait_info;
 	important = FALSE;
 	permanent = TRUE;
-	description = "Похоже я знаю тебя, призрак!";
+	description = " Looks like I know you, ghost! " ;
 };
 
 
@@ -43,30 +44,30 @@ func int dia_orc_8563_urgrom_hello_wait_condition()
 
 func void dia_orc_8563_urgrom_hello_wait_info()
 {
-	AI_Output(other,self,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_01");	//Похоже, я тебя знаю, призрак! Твое лицо... оно кажется мне знакомым.
-	AI_Output(other,self,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_02");	//Ты... ты наверно... ну да... конечно! Ур-Гром!
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_03");	//...Человек говорить Ур-Гром? Откуда он знать мое имя?
-	AI_Output(other,self,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_04");	//Твой брат, Ур-Тралл рассказывал о тебе.
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_05");	//Брат Ур-Грома... брат... брат... Ур-Тралл...(думает)
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_06");	//Да, думать, что так.
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_07");	//Иметь брат очень давно, много зим назад. Ур-Гром помнить брата Ур-Тралла!
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_08");	//Ур-Гром и брат сражаться вместе против своих врагов, сражаться против злой демон.
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_09");	//Человек знать брата Ур-Грома?
-	AI_Output(other,self,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_10");	//Да, я встречал твоего брата. Скажу больше, именно он та причина, что заставила меня появится здесь.
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_11");	//Зачем человек прийти сюда?
-	AI_Output(other,self,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_12");	//Я здесь потому, что Ур-Тралл хочет отомстить Азгалору. Отомстить за свой народ, отомстить за тебя и за себя!
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_13");	//Он не забыть ужасный демон?!
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_14");	//Брат приходить убить его?
-	AI_Output(other,self,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_15");	//Нет, Ур-Тралл не может появится в долине, иначе проклятие, наложенное на всех вас Азгалором, снова возобладает над ним.
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_16");	//Человек один хотеть убить демон?!
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_18");	//Ур-Гром думать, что человек быть очень смелый воин. Но человек не сможет один победить демон.
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_19");	//Человек умирать, отдавать дух демону. Демон очень сильный, очень злой!
-	AI_Output(other,self,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_20");	//Мне уже не раз приходилось сражаться с чудовищами, подобными ему. Справлюсь и в этот раз.
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_21");	//Не знать силу демон! Думать, демон слабый. Думать, что он сильнее, чем демон.
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_22");	//Ур-Гром говорить, человек ошибаться! Много братьев хотеть убить демон, но все мертвы, все теперь служат ему.
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_23");	//Человек нужен помощь. Один он умирать!
-	B_LogEntry(TOPIC_URNAZULRAGE,"В заброшенном лагере орков-призраков я встретил погибшего брата Ур-Тралла - Ур-Грома. По его словам, черный дракон Азгалор - очень могущественное создание, и в одиночку мне с ним не справиться.");
-	self.name[0] = "Ур-Гром, орк-призрак";
+	AI_Output(other,self, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_01 " );	// Looks like I know you, ghost! Your face... it seems familiar to me.
+	AI_Output(other,self, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_02 " );	// You... you must... well, yes... of course! Ur-Thunder!
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_03 " );	// ...Human talking Ur-Thunder? How does he know my name?
+	AI_Output(other,self, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_04 " );	// Your brother, Ur-Thrall told about you.
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_05 " );	// Ur-Grom's brother... brother... brother... Ur-Thrall... (thinks)
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_06 " );	// Yes, think so.
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_07 " );	// Have a brother a very long time ago, many winters ago. Ur-Grom remember brother Ur-Thrall!
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_08 " );	// Ur-Grom and brother fight together against their enemies, fight against the evil demon.
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_09 " );	// Man to know Brother Ur-Grom?
+	AI_Output(other,self, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_10 " );	// Yes, I met your brother. I will say more, it is he who is the reason that made me appear here.
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_11 " );	// Why would a person come here?
+	AI_Output(other,self, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_12 " );	// I'm here because Ur-Thrall wants revenge on Azgalor. To avenge my people, to avenge you and myself!
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_13 " );	// He didn't forget the terrible demon?!
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_14 " );	// Brother come to kill him?
+	AI_Output(other,self, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_15 " );	// No, Ur-Thrall cannot appear in the valley, otherwise the curse that Azgalor placed on all of you will take over him again.
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_16 " );	// Human alone wants to kill a demon?!
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_18 " );	// Ur-Thunder think the man to be a very brave warrior. But a man cannot defeat a demon alone.
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_19 " );	// Man to die, give spirit to the demon. The demon is very strong, very angry!
+	AI_Output(other,self, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_20 " );	// I've had to fight monsters like him more than once. I'll do it this time too.
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_21 " );	// Don't know the strength of a demon! To think the demon is weak. To think that he is stronger than a demon.
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_22 " );	// Ur-Thunder speak, man err! Many brothers want to kill the demon, but everyone is dead, everyone now serves him.
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Wait_Info_01_23 " );	// Man needs help. Alone he die!
+	B_LogEntry( TOPIC_URNAZULRAGE , " In an abandoned ghost orc camp, I met Ur-Thrall's dead brother, Ur-Grom. According to him, the black dragon Azgalor is a very powerful creature, and I cannot defeat him alone. " );
+	self.name[ 0 ] = " Ur-Thunder, orc ghost " ;
 	MEETURGROM = TRUE;
 };
 
@@ -78,7 +79,7 @@ instance DIA_ORC_8563_URGROM_HELLO_HELP(C_Info)
 	information = dia_orc_8563_urgrom_hello_help_info;
 	important = FALSE;
 	permanent = TRUE;
-	description = "Ты можешь мне чем-то помочь?";
+	description = " Can you help me with something? " ;
 };
 
 
@@ -92,23 +93,23 @@ func int dia_orc_8563_urgrom_hello_help_condition()
 
 func void dia_orc_8563_urgrom_hello_help_info()
 {
-	AI_Output(other,self,"DIA_Orc_8563_UrGrom_Hello_Help_Info_01_01");	//А ты сможешь помочь мне уничтожить эту тварь?
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Help_Info_01_02");	//Ур-Гром думать, как он может помочь человек...
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Help_Info_01_03");	//Хотеть идти сражаться вместе с человек против демон! (гневно) Мстить за братьев!
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Help_Info_01_04");	//Но не может. Азгалор забрал душу Ур-Грома - Ур-Гром умереть, если пойти против демон.
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Help_Info_01_05");	//Человеку нужен другой помощник... Думать, человеку нужно найти ДВАРТАЗДАХ.
-	AI_Output(other,self,"DIA_Orc_8563_UrGrom_Hello_Help_Info_01_06");	//А что это такое - ДВАРТАЗДАХ?
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Help_Info_01_07");	//Быть древний оружие! Оружие великого воина-духа!
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Help_Info_01_08");	//С ним человек может победить демон.
-	AI_Output(other,self,"DIA_Orc_8563_UrGrom_Hello_Help_Info_01_09");	//Где мне искать это оружие?
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Help_Info_01_10");	//Ур-Гром не знать точно. Только слышать когда-то о нем от сынов духа, но сам не видеть никогда.
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Help_Info_01_11");	//Человек сам находить ДВАРТАЗДАХ, приносить его Ур-Грому, а потом идти сражаться против демон.
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Help_Info_01_12");	//Если находить ДВАРТАЗДАХ, приносить его Ур-Грому. Потом идти сражаться против демон.
-	AI_Output(other,self,"DIA_Orc_8563_UrGrom_Hello_Help_Info_01_13");	//Хорошо, я попробую отыскать этот... как его там...
+	AI_Output(other,self, " DIA_Orc_8563_UrGrom_Hello_Help_Info_01_01 " );	// Can you help me destroy this creature?
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Help_Info_01_02 " );	// Ur-Thunder think how he can help the man...
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Help_Info_01_03 " );	// Want to go fight alongside a human against a demon! (angrily) Avenge for the brothers!
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Help_Info_01_04 " );	// But it can't. Azgalor took the soul of Ur-Grom - Ur-Grom will die if you go against the demon.
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Help_Info_01_05 " );	// Man needs another helper... Think man needs to find TWARTAZDAH.
+	AI_Output(other,self, " DIA_Orc_8563_UrGrom_Hello_Help_Info_01_06 " );	// And what is this - DVARTAZDACH?
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Help_Info_01_07 " );	// Be an ancient weapon! Great Spirit Warrior Weapon!
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Help_Info_01_08 " );	// With it, a human can defeat a demon.
+	AI_Output(other,self, " DIA_Orc_8563_UrGrom_Hello_Help_Info_01_09 " );	// Where can I find this weapon?
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Help_Info_01_10 " );	// Ur-Grom doesn't know for sure. Only to hear once about it from the sons of the spirit, but never to see it yourself.
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Help_Info_01_11 " );	// The man himself find TWARTAZDACH, bring it to Ur-Grom, and then go fight against the demon.
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Help_Info_01_12 " );	// If you find TWARTAZDACH, bring it to Ur-Grom. Then go fight against the demon.
+	AI_Output(other,self, " DIA_Orc_8563_UrGrom_Hello_Help_Info_01_13 " );	// Okay, I'll try to find this one... whatever it is...
 	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Help_Info_01_14");	//ДВАРТАЗДАХ!
 	AI_Output(other,self,"DIA_Orc_8563_UrGrom_Hello_Help_Info_01_15");	//...точно, именно его.
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Help_Info_01_16");	//Тогда человек идти, Ур-Гром его ждать.
-	B_LogEntry(TOPIC_URNAZULRAGE,"Ур-Гром поведал мне, что где-то в долине спрятано древнее оружие духов - по-орочьи оно называется ДВАРТАЗДАХ. Это оружие, по словам Ур-Грома, способно доставить серьезные неприятности Азгалору.");
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Help_Info_01_16 " );	// Then the man go, Ur-Grom wait for him.
+	; _ _ _ _ _ _
 	MEETURGROMNEXT = TRUE;
 };
 
@@ -120,7 +121,7 @@ instance DIA_ORC_8563_URGROM_HELLO_FIND(C_Info)
 	information = dia_orc_8563_urgrom_hello_find_info;
 	important = FALSE;
 	permanent = TRUE;
-	description = "Это случайно не то оружие?";
+	description = " Is this the wrong weapon? " ;
 };
 
 
@@ -135,11 +136,11 @@ func int dia_orc_8563_urgrom_hello_find_condition()
 func void dia_orc_8563_urgrom_hello_find_info()
 {
 	B_GivePlayerXP(200);
-	AI_Output(other,self,"DIA_Orc_8563_UrGrom_Hello_Find_01_01");	//А это случайно не то оружие, про которое ты говорил?
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Find_01_02");	//Ур-Гром думать, что то. Человек нашел ДВАРТАЗДАХ!!!
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Hello_Find_01_03");	//Теперь идти убивать демон! Мстить за братьев!
-	AI_Output(other,self,"DIA_Orc_8563_UrGrom_Hello_Find_01_04");	//Тогда я пошел.
-	B_LogEntry(TOPIC_URNAZULRAGE,"Я нашел ДВАРТАЗДАХ, точнее Вершитель - древнее оружие воинов-духов, обитавших когда-то в этих местах...");
+	AI_Output(other,self, " DIA_Orc_8563_UrGrom_Hello_Find_01_01 " );	// Isn't that the weapon you were talking about, by any chance?
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Find_01_02 " );	// Ur-Thunder think something. Man found TWARTAZDAH!!!
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Hello_Find_01_03 " );	// Now go kill the demon! Revenge for the brothers!
+	AI_Output(other,self, " DIA_Orc_8563_UrGrom_Hello_Find_01_04 " );	// Then I went.
+	B_LogEntry( TOPIC_URNAZULRAGE , " I found DVARTAZDAH, or rather the Executor - an ancient weapon of spirit warriors who once lived in these places... " );
 	AI_StopProcessInfos(self);
 	MEETURGROMCOME = TRUE;
 	Npc_ExchangeRoutine(self,"UrGromGoBattle");
@@ -166,12 +167,12 @@ func int dia_orc_8563_urgrom_azgolor_wait_condition()
 
 func void dia_orc_8563_urgrom_azgolor_wait_info()
 {
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Azgolor_Wait_01_01");	//Демон быть уже здесь, ждать человек.
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Azgolor_Wait_01_02");	//Человек идти дальше и встречать демон...
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Azgolor_Wait_01_03");	//Ур-Гром смотреть, как человек убивать демон.
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Azgolor_Wait_01_04");	//Человек не забыть носить с собой ДВАРТАЗДАХ!
-	AI_Output(other,self,"DIA_Orc_8563_UrGrom_Azgolor_Wait_01_05");	//Само собой.
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Azgolor_Wait_01_06");	//Ур-Гром желать человек удачная охота.
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Azgolor_Wait_01_01 " );	// Demon be already here, wait man.
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Azgolor_Wait_01_02 " );	// Man go on and meet the demon...
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Azgolor_Wait_01_03 " );	// Ur-Thunder watch as a man kill a demon.
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Azgolor_Wait_01_04 " );	// Man don't forget to carry DVARTAZDAH!
+	AI_Output(other,self, " DIA_Orc_8563_UrGrom_Azgolor_Wait_01_05 " );	// Of course.
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Azgolor_Wait_01_06 " );	// Ur-Thunder wish man good hunting.
 	self.flags = 0;
 	MEETURGROMEND = TRUE;
 	AI_StopProcessInfos(self);
@@ -198,7 +199,7 @@ func int dia_orc_8563_urgrom_azgolor_wait_again_condition()
 
 func void dia_orc_8563_urgrom_azgolor_wait_again_info()
 {
-	AI_Output(self,other,"DIA_Orc_8563_UrGrom_Azgolor_Wait_Again_01_01");	//Человек идти убивать демон. Ур-Гром ждать его здесь!
+	AI_Output(self,other, " DIA_Orc_8563_UrGrom_Azgolor_Wait_Again_01_01 " );	// Human go kill demon. Ur-Thunder wait for him here!
 	AI_StopProcessInfos(self);
 };
 
