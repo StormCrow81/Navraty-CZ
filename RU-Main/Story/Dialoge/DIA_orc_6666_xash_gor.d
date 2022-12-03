40426e1b94a116d68781b688c3287e84390d4781
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 20:25:39 +0200

    Update DIA_orc_6666_xash_gor.d

--- RU-Main/Story/Dialoge/DIA_orc_6666_xash_gor.d
+++ RU-Main/Story/Dialoge/DIA_orc_6666_xash_gor.d
@@ -1,7 +1,8 @@
 
+
 instance DIA_ORC_6666_XASH_GOR_EXIT(C_Info)
 {
-	npc = orc_6666_xash_gor;
+	npc = orc_6666_hash_gor;
 	condition = dia_orc_6666_xash_gor_exit_condition;
 	information = dia_orc_6666_xash_gor_exit_info;
 	important = FALSE;
@@ -24,7 +25,7 @@ func void dia_orc_6666_xash_gor_exit_info()
 
 instance DIA_ORC_6666_XASH_GOR_FINALLY(C_Info)
 {
-	npc = orc_6666_xash_gor;
+	npc = orc_6666_hash_gor;
 	condition = dia_orc_6666_xash_gor_finally_condition;
 	information = dia_orc_6666_xash_gor_finally_info;
 	important = TRUE;
@@ -40,14 +41,14 @@ func int dia_orc_6666_xash_gor_finally_condition()
 func void dia_orc_6666_xash_gor_finally_info()
 {
 	Wld_SendTrigger("EVT_ORCGRYRD_HASHGOR");
-	AI_Output(self,other,"DIA_Orc_6666_Xash_Gor_Finally_Info_18_01");	//Смертный!
-	AI_Output(self,other,"DIA_Orc_6666_Xash_Gor_Finally_Info_18_02");	//Как ты посмел потревожить меня?
-	AI_Output(self,other,"DIA_Orc_6666_Xash_Gor_Finally_Info_18_03");	//Готовься к смерти!
-	AI_Output(other,self,"DIA_Orc_6666_Xash_Gor_Finally_Info_18_04");	//Мы еще посмотрим, кому из нас придется готовиться к смерти - ты, гнилой труп!
-	AI_Output(self,other,"DIA_Orc_6666_Xash_Gor_Finally_Info_18_05");	//Теперь ты умрешь!!!
+	AI_Output(self,other, " DIA_Orc_6666_Xash_Gor_Finally_Info_18_01 " );	// Mortal!
+	AI_Output(self,other, " DIA_Orc_6666_Xash_Gor_Finally_Info_18_02 " );	// How dare you disturb me?
+	AI_Output(self,other, " DIA_Orc_6666_Xash_Gor_Finally_Info_18_03 " );	// Prepare to die!
+	AI_Output(other,self, " DIA_Orc_6666_Xash_Gor_Finally_Info_18_04 " );	// We'll see which of us will have to prepare for death - you rotten corpse!
+	AI_Output(self,other, " DIA_Orc_6666_Xash_Gor_Finally_Info_18_05 " );	// Now you will die!!!
 	HashGorBattleStart = TRUE;
 	AI_StopProcessInfos(self);
-	self.aivar[AIV_EnemyOverride] = FALSE;
+	self.aivar[AIV_EnemyOverride] = FALSE ;
 
 	if(SBMODE == TRUE)
 	{
