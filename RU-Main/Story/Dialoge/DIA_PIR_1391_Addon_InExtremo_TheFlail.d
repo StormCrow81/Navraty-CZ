3c602cfacb3fcef5c2e9f75d28997f20813d6641
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 12:56:23 +0200

    Update DIA_PIR_1391_Addon_InExtremo_TheFlail.d

--- RU-Main/Story/Dialoge/DIA_PIR_1391_Addon_InExtremo_TheFlail.d
+++ RU-Main/Story/Dialoge/DIA_PIR_1391_Addon_InExtremo_TheFlail.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_ADDON_INEXTREMO_THEFLAIL_EXIT(C_Info)
 {
 	npc = PIR_1391_Addon_InExtremo_TheFlail;
@@ -34,7 +35,7 @@ instance DIA_ADDON_INEXTREMO_THEFLAIL_PICKPOCKET(C_Info)
 
 func int dia_addon_inextremo_theflail_pickpocket_condition()
 {
-	return C_Beklauen(80,100);
+	return  C_Robbery ( 80 , 100 );
 };
 
 func void dia_addon_inextremo_theflail_pickpocket_info()
@@ -46,7 +47,7 @@ func void dia_addon_inextremo_theflail_pickpocket_info()
 
 func void dia_addon_inextremo_theflail_pickpocket_doit()
 {
-	B_Beklauen();
+	B_Robbery();
 	Info_ClearChoices(dia_addon_inextremo_theflail_pickpocket);
 };
 
@@ -81,32 +82,32 @@ func void dia_addon_inextremo_theflail_hai_info()
 	{
 		if(KILLMEMBER2 == FALSE)
 		{
-			AI_Output(self,other,"DIA_Addon_InExtremo_TheFlail_Hai_01_00");	//Лихо ты разобрался с Асмалом!
-			AI_Output(self,other,"DIA_Addon_InExtremo_TheFlail_Hai_01_01");	//Скажу честно - мы считали, что ты не на многое способен, но теперь...
-			AI_Output(other,self,"DIA_Addon_InExtremo_TheFlail_Hai_01_02");	//Теперь все будет по-другому.
-			AI_Output(self,other,"DIA_Addon_InExtremo_TheFlail_Hai_01_03");	//Конечно, босс! Как скажешь.
+			AI_Output(self,other, " DIA_Addon_InExtremo_TheFlail_Hai_01_00 " );	// Famously you dealt with Asmal!
+			AI_Output(self,other, " DIA_Addon_InExtremo_TheFlail_Hai_01_01 " );	// To be honest, we thought you weren't capable of much, but now...
+			AI_Output(other,self, " DIA_Addon_InExtremo_TheFlail_Hai_01_02 " );	// Now everything will be different.
+			AI_Output(self,other, " DIA_Addon_InExtremo_TheFlail_Hai_01_03 " );	// Of course, boss! As you say.
 			KILLMEMBER2 = TRUE;
 		}
 		else if(CharlotteMyAss == TRUE)
 		{
-			AI_Output(self,other,"DIA_Addon_InExtremo_TheFlail_Hi_01_77");	//Если тебе что-то нужно, поговори с Шарлоттой.
-			AI_Output(self,other,"DIA_Addon_InExtremo_TheFlail_Hi_01_78");	//Она ведь твоя помощница.
+			AI_Output(self,other, " DIA_Addon_InExtremo_TheFlail_Hi_01_77 " );	// If you need anything, talk to Charlotte.
+			AI_Output(self,other, " DIA_Addon_InExtremo_TheFlail_Hi_01_78 " );	// She's your assistant.
 			AI_Output(other,self,"DIA_Addon_InExtremo_TheFlail_Hi_01_79");	//Ну да, конечно.
 		}
 		else
 		{
-			AI_Output(self,other,"DIA_Addon_InExtremo_TheFlail_Hi_01_04");	//Что скажешь?
+			AI_Output(self,other, " DIA_Addon_InExtremo_TheFlail_Hi_01_04 " );	// What do you say?
 		};
 	}
 	else if(Wld_IsTime(8,0,20,0) && (JOINKILLERS == FALSE))
 	{
-		AI_Output(self,other,"DIA_Addon_InExtremo_TheFlail_Hai_01_05");	//Не отвлекай меня!
+		AI_Output(self,other, " DIA_Addon_InExtremo_TheFlail_Hai_01_05 " );	// Don't distract me!
 		AI_StopProcessInfos(self);
 		Npc_SetRefuseTalk(self,300);
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_Addon_InExtremo_TheFlail_Hai_01_06");	//Оставь меня в покое!
+		AI_Output(self,other, " DIA_Addon_InExtremo_TheFlail_Hai_01_06 " );	// Leave me alone!
 		AI_StopProcessInfos(self);
 		Npc_SetRefuseTalk(self,300);
 	};
@@ -119,7 +120,7 @@ instance DIA_Addon_InExtremo_TheFlail_Lute(C_Info)
 	condition = DIA_Addon_InExtremo_TheFlail_Lute_condition;
 	information = DIA_Addon_InExtremo_TheFlail_Lute_info;
 	permanent = FALSE;
-	description = "Та лютня, на которой ты играл. Она еще при тебе?";
+	description = " That lute you played. Do you still have it? " ;
 };
 
 func int DIA_Addon_InExtremo_TheFlail_Lute_condition()
@@ -133,15 +134,15 @@ func int DIA_Addon_InExtremo_TheFlail_Lute_condition()
 func void DIA_Addon_InExtremo_TheFlail_Lute_info()
 {
 	B_GivePlayerXP(200);
-	AI_Output(other,self,"DIA_Addon_InExtremo_TheFlail_Lute_01_01");	//Та лютня, на которой ты играл. Она еще при тебе?
-	AI_Output(self,other,"DIA_Addon_InExtremo_TheFlail_Lute_01_02");	//А где же ей быть...(смеется) Здесь она, родимая!
-	AI_Output(other,self,"DIA_Addon_InExtremo_TheFlail_Lute_01_03");	//Тут просто такое дело, в общем, мне позарез нужен этот инструмент. 
-	AI_Output(other,self,"DIA_Addon_InExtremo_TheFlail_Lute_01_04");	//Можешь отдать ее мне?
-	AI_Output(self,other,"DIA_Addon_InExtremo_TheFlail_Lute_01_05");	//Для тебя, босс, все, что захочешь! Вот, держи.
-	AI_Output(self,other,"DIA_Addon_InExtremo_TheFlail_Lute_01_06");	//Но погоди. А ты хоть играть на ней умеешь?
-	AI_Output(other,self,"DIA_Addon_InExtremo_TheFlail_Lute_01_07");	//Это сейчас неважно.
+	AI_Output(other,self, " DIA_Addon_InExtremo_TheFlail_Lute_01_01 " );	// That lute you played. Is she still with you?
+	AI_Output(self,other, " DIA_Addon_InExtremo_TheFlail_Lute_01_02 " );	// And where should she be... (laughs) Here she is, dear!
+	AI_Output(other,self, " DIA_Addon_InExtremo_TheFlail_Lute_01_03 " );	// It's just such a thing, in general, I desperately need this tool.
+	AI_Output(other,self, " DIA_Addon_InExtremo_TheFlail_Lute_01_04 " );	// Can you give it to me?
+	AI_Output(self,other, " DIA_Addon_InExtremo_TheFlail_Lute_01_05 " );	// For you, boss, whatever you want! Here you are.
+	AI_Output(self,other, " DIA_Addon_InExtremo_TheFlail_Lute_01_06 " );	// But wait. Do you even know how to play it?
+	AI_Output(other,self, " DIA_Addon_InExtremo_TheFlail_Lute_01_07 " );	// It doesn't matter now.
 	B_GiveInvItems(self,other,ItMi_IELaute,1);
 	Npc_RemoveInvItems(self,ItMi_IELaute,1);
-	AI_Output(self,other,"DIA_Addon_InExtremo_TheFlail_Lute_01_08");	//Ну ладно. Как знаешь.
-	B_LogEntry(TOPIC_KASSIAMUSIC,"Флейл отдал мне свою лютню.");
+	AI_Output(self,other, " DIA_Addon_InExtremo_TheFlail_Lute_01_08 " );	// Okay. As you know.
+	B_LogEntry( TOPIC_KASSIAMUSIC , " Flail gave me his lute. " );
 };
