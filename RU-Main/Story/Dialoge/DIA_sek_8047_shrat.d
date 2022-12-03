72b14780af3f817897f0bb4eb13a2bbce67f10fa
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 10:02:21 +0200

    Update DIA_sek_8047_shrat.d

--- RU-Main/Story/Dialoge/DIA_sek_8047_shrat.d
+++ RU-Main/Story/Dialoge/DIA_sek_8047_shrat.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_SHRAT_EXIT(C_Info)
 {
 	npc = sek_8047_shrat;
@@ -28,7 +29,7 @@ instance DIA_SHRAT_NOFOREVER(C_Info)
 	condition = dia_shrat_noforever_condition;
 	information = dia_shrat_noforever_info;
 	permanent = FALSE;
-	description = "Что вы тут делаете?";
+	description = " What are you doing here? " ;
 };
 
 func int dia_shrat_noforever_condition()
@@ -38,16 +39,16 @@ func int dia_shrat_noforever_condition()
 
 func void dia_shrat_noforever_info()
 {
-	AI_Output(other,self,"DIA_Shrat_NoForever_01_00");	//Что вы тут делаете?
-	AI_Output(self,other,"DIA_Shrat_NoForever_01_01");	//А что, не видно? Собираем болотник, чтобы потом наши братья в лагере смогли его курить.
-	AI_Output(self,other,"DIA_Shrat_NoForever_01_02");	//Знаешь, сколько в нашем Братстве любителей забить косячок? (смеется) ОЧЕНЬ МНОГО!
+	AI_Output(other,self, " DIA_Shrat_NoForever_01_00 " );	// What are you doing here?
+	AI_Output(self,other, " DIA_Shrat_NoForever_01_01 " );	// Why can't you see? We collect the swamp so that later our brothers in the camp can smoke it.
+	AI_Output(self,other, " DIA_Shrat_NoForever_01_02 " );	// Do you know how many people in our Brotherhood who like to score a joint? (laughs) A LOT!
 
 	MeetLobartPsiCamp = TRUE;
 
 	if(MIS_Lobart_Psicamp == LOG_Running)
 	{
 		B_GivePlayerXP(50);
-		B_LogEntry(TOPIC_Lobart_Psicamp,"Кажется, те люди, о которых говорил Лобарт - всего-навсего обыкновенные сборщики болотной травы.");
+		B_LogEntry(TOPIC_Lobart_Psicamp, " It seems that the people Lobart was talking about are just ordinary swamp grass pickers. " );
 	};
 };
 
@@ -59,13 +60,13 @@ instance DIA_SHRAT_PSIINFO(C_Info)
 	condition = dia_shrat_psiinfo_condition;
 	information = dia_shrat_psiinfo_info;
 	permanent = FALSE;
-	description = "В Братстве? В каком Братстве?!";
+	description = " In the Brotherhood? What Brotherhood?! " ;
 };
 
 
 func int dia_shrat_psiinfo_condition()
 {
-	if(!Npc_KnowsInfo(hero,dia_tpl_8014_templer_first) && !Npc_KnowsInfo(hero,dia_hanis_psiinfo) && !Npc_KnowsInfo(hero,dia_balam_psiinfo) && Npc_KnowsInfo(hero,dia_shrat_noforever))
+	if ( ! Npc_KnowsInfo(hero,dia_tpl_8014_templer_first) && !  Npc_KnowsInfo (hero, dia_hanis_psiinfo ) &&  !
 	{
 		return TRUE;
 	};
@@ -73,14 +74,14 @@ func int dia_shrat_psiinfo_condition()
 
 func void dia_shrat_psiinfo_info()
 {
-	AI_Output(other,self,"DIA_Shrat_PsiInfo_01_00");	//В Братстве? В каком Братстве?
-	AI_Output(self,other,"DIA_Shrat_PsiInfo_01_01");	//В Братстве Спящего, конечно! Хотя теперь его можно называть просто - Братство.
-	AI_Output(other,self,"DIA_Shrat_PsiInfo_01_02");	//Но я думал, что после падения барьера, Братство Спящего перестало существовать!
-	AI_Output(self,other,"DIA_Shrat_PsiInfo_01_03");	//Это не так. Большинство наших братьев погибли, а многие сошли с ума от тлетворного влияния демонических сил.
-	AI_Output(self,other,"DIA_Shrat_PsiInfo_01_04");	//Но были и те, кто выжил. Именно они стали лидерами нашего нового лагеря.
-	AI_Output(other,self,"DIA_Shrat_PsiInfo_01_05");	//А где находится ваш лагерь?
-	AI_Output(self,other,"DIA_Shrat_PsiInfo_01_06");	//Ступай по направлению к проходу в Долину Рудников. Не доходя до самого прохода, ты найдешь небольшую ферму.
-	AI_Output(self,other,"DIA_Shrat_PsiInfo_01_07");	//Недалеко от нее и находится наша новая обитель.
+	AI_Output(other,self, " DIA_Shrat_PsiInfo_01_00 " );	// In the Brotherhood? Which Brotherhood?
+	AI_Output(self,other, " DIA_Shrat_PsiInfo_01_01 " );	// In the Brotherhood of the Sleeper, of course! Although now it can be called simply - the Brotherhood.
+	AI_Output(other,self, " DIA_Shrat_PsiInfo_01_02 " );	// But I thought that after the fall of the barrier, the Brotherhood of the Sleeper ceased to exist!
+	AI_Output(self,other, " DIA_Shrat_PsiInfo_01_03 " );	// It's not. Most of our brothers died, and many went crazy from the corrupting influence of demonic forces.
+	AI_Output(self,other, " DIA_Shrat_PsiInfo_01_04 " );	// But there were those who survived. It was they who became the leaders of our new camp.
+	AI_Output(other,self, " DIA_Shrat_PsiInfo_01_05 " );	// Where is your camp?
+	AI_Output(self,other, " DIA_Shrat_PsiInfo_01_06 " );	// Head towards the passage to the Valley of Mines. Before reaching the passage itself, you will find a small farm.
+	AI_Output(self,other, " DIA_Shrat_PsiInfo_01_07 " );	// Not far from it is our new abode.
 };
 
 
@@ -91,7 +92,7 @@ instance DIA_SHRAT_HELLO(C_Info)
 	condition = dia_shrat_hello_condition;
 	information = dia_shrat_hello_info;
 	permanent = FALSE;
-	description = "Эй, я тебя знаю!";
+	description = " Hey, I know you! " ;
 };
 
 
@@ -106,14 +107,14 @@ func int dia_shrat_hello_condition()
 func void dia_shrat_hello_info()
 {
 	B_GivePlayerXP(50);
-	AI_Output(other,self,"DIA_Shrat_Hello_01_00");	//Эй, я тебя знаю!
-	AI_Output(other,self,"DIA_Shrat_Hello_01_01");	//Ты тот парень, что прятался в хижине на болотах.
-	AI_Output(self,other,"DIA_Shrat_Hello_01_02");	//Хммм... Верно. Разве мы с тобой раньше встречались?
-	AI_Output(self,other,"DIA_Shrat_Hello_01_03");	//Хотя постой! (внимательно всматривается) Ну-ка...
-	AI_Output(self,other,"DIA_Shrat_Hello_01_04");	//Ах, да! Теперь я тебя вспоминаю...(смеется) Ты тот парень, что принес нам яйца ползунов.
-	AI_Output(other,self,"DIA_Shrat_Hello_01_05");	//Ну вот, вспомнил наконец-то.
-	AI_Output(self,other,"DIA_Shrat_Hello_01_06");	//Ну ладно, не обижайся. За последнее время столько всего произошло - разве все и всех упомнишь.
-	AI_Output(self,other,"DIA_Shrat_Hello_01_07");	//Рад тебя видеть - здорового и невредимого!
+	AI_Output(other, self, " DIA_Shrat_Hello_01_00 " );	// Hey, I know you!
+	AI_Output(other,self, " DIA_Shrat_Hello_01_01 " );	// You're the guy hiding in the cabin in the swamps.
+	AI_Output(self,other, " DIA_Shrat_Hello_01_02 " );	// Hmmm... Right. Have we met before?
+	AI_Output(self,other, " DIA_Shrat_Hello_01_03 " );	// But wait! (looks closely) Well...
+	AI_Output(self,other, " DIA_Shrat_Hello_01_04 " );	// Oh yes! Now I remember you... (laughs) You are the guy who brought us the eggs of the crawlers.
+	AI_Output(other,self, " DIA_Shrat_Hello_01_05 " );	// Well, I finally remembered.
+	AI_Output(self,other, " DIA_Shrat_Hello_01_06 " );	// Okay, don't be offended. So much has happened lately - unless you remember everything and everyone.
+	AI_Output(self,other, " DIA_Shrat_Hello_01_07 " );	// Glad to see you - healthy and unharmed!
 };
 
 
@@ -124,7 +125,7 @@ instance DIA_SHRAT_HELLOTWO(C_Info)
 	condition = dia_shrat_hellotwo_condition;
 	information = dia_shrat_hellotwo_info;
 	permanent = TRUE;
-	description = "Как продвигается сбор болотника?";
+	description = " How's the bog harvest going? " ;
 };
 
 
@@ -138,8 +139,8 @@ func int dia_shrat_hellotwo_condition()
 
 func void dia_shrat_hellotwo_info()
 {
-	AI_Output(other,self,"DIA_Shrat_HelloTwo_01_00");	//Как продвигается сбор болотника?
-	AI_Output(self,other,"DIA_Shrat_HelloTwo_01_01");	//Если ты не будешь приставать к нашим сборщикам, он будет продвигаться еще быстрее!
+	AI_Output(other,self, " DIA_Shrat_HelloTwo_01_00 " );	// How's the bog harvest progressing?
+	AI_Output(self,other, " DIA_Shrat_HelloTwo_01_01 " );	// If you don't pester our pickers, he'll move even faster!
 };
 
 instance dia_shrat_PrioratStart(C_Info)
@@ -149,7 +150,7 @@ instance dia_shrat_PrioratStart(C_Info)
 	condition = dia_shrat_PrioratStart_condition;
 	information = dia_shrat_PrioratStart_info;
 	permanent = FALSE;
-	description = "Меня прислал Идол Намиб.";
+	description = "The Namib Idol sent me. " ;
 };
 
 func int dia_shrat_PrioratStart_condition()
@@ -163,20 +164,20 @@ func int dia_shrat_PrioratStart_condition()
 func void dia_shrat_PrioratStart_info()
 {
 	B_GivePlayerXP(100);
-	AI_Output(other,self,"dia_shrat_PrioratStart_01_00");	//Меня прислал Идол Намиб.
-	AI_Output(self,other,"dia_shrat_PrioratStart_01_01");	//Я весь во внимании...(уважительно) Что хочет от нас Гуру Братства?
-	AI_Output(other,self,"dia_shrat_PrioratStart_01_02");	//Он хочет знать, какова на данный момент обстановка у вас в лагере.
-	AI_Output(self,other,"dia_shrat_PrioratStart_01_03");	//Можешь передать ему, что у нас все хорошо.
-	AI_Output(self,other,"dia_shrat_PrioratStart_01_04");	//Сбор болотника продвигается нормальными темпами, и его новая партия будет доставлена точно в срок.
-	AI_Output(other,self,"dia_shrat_PrioratStart_01_05");	//Хорошо, так ему и передам. У меня есть еще один вопрос.
-	AI_Output(self,other,"dia_shrat_PrioratStart_01_06");	//Спрашивай, если есть.
-	AI_Output(other,self,"dia_shrat_PrioratStart_01_07");	//Вы что-нибудь слышали о пропавших послушниках Братства? 
-	AI_Output(self,other,"dia_shrat_PrioratStart_01_08");	//Ты это серьезно?! (в ужасе) Несколько наших послушников пропало?
-	AI_Output(other,self,"dia_shrat_PrioratStart_01_09");	//Да нет, это всего лишь слухи. Ничего серьезного!
-	AI_Output(self,other,"dia_shrat_PrioratStart_01_10");	//А... ну, если так...(растерянно) Я уже подумал, что случилось действительно что-то плохое.
-	AI_Output(self,other,"dia_shrat_PrioratStart_01_11");	//Нет, мы ничего не знаем об этом.
+	AI_Output(other, self, " dia_shrat_PrioratStart_01_00 " );	// The Namib Idol sent me.
+	AI_Output(self,other, " dia_shrat_PrioratStart_01_01 " );	// I'm all ears... (respectfully) What does the Guru of the Brotherhood want from us?
+	AI_Output(other,self, " dia_shrat_PrioratStart_01_02 " );	// He wants to know what the situation is at your camp at the moment.
+	AI_Output(self,other, " dia_shrat_PrioratStart_01_03 " );	// You can tell him that we're fine.
+	AI_Output(self,other, " dia_shrat_PrioratStart_01_04 " );	// The harvest of the bogweed is progressing at a normal pace, and a new batch will be delivered right on time.
+	AI_Output(other,self, " dia_shrat_PrioratStart_01_05 " );	// Okay, so I'll pass it on to him. I've got one more question.
+	AI_Output(self,other, " dia_shrat_PrioratStart_01_06 " );	// Ask if there is.
+	AI_Output(other,self, " dia_shrat_PrioratStart_01_07 " );	// Have you heard anything about the missing acolytes of the Brotherhood?
+	AI_Output(self,other, " dia_shrat_PrioratStart_01_08 " );	// Are you serious?! (horrified) Are a few of our acolytes missing?
+	AI_Output(other, self, " dia_shrat_PrioratStart_01_09 " );	// No, these are just rumors. Nothing serious!
+	AI_Output(self,other, " dia_shrat_PrioratStart_01_10 " );	// Ah... well, if so... (bewildered) I already thought something really bad had happened.
+	AI_Output(self,other, " dia_shrat_PrioratStart_01_11 " );	// No, we don't know anything about it.
 	PsiCamp_02_Ok = TRUE;
-	B_LogEntry(TOPIC_PrioratStart,"В лагере сборщиков Шрэта все без изменений. О пропавших послушниках им тоже ничего не известно.");
+	B_LogEntry(TOPIC_PrioratStart, " Nothing has changed in Shrat's gathering camp. They don't know about the missing acolytes either. " );
 };
 
 instance DIA_SHRAT_GIVEPLANT(C_Info)
@@ -186,13 +187,13 @@ instance DIA_SHRAT_GIVEPLANT(C_Info)
 	condition = dia_shrat_giveplant_condition;
 	information = dia_shrat_giveplant_info;
 	permanent = TRUE;
-	description = "Я от Идола Кадара.";
+	description = " I'm from the Idol of Kadar. " ;
 };
 
 
 func int dia_shrat_giveplant_condition()
 {
-	if((MIS_PLANTSFORBAALCADAR == LOG_Running) && (THIRDGROUPSEK == FALSE) && (other.guild == GIL_SEK) && Npc_KnowsInfo(hero,dia_shrat_noforever))
+	if (( MY_PLANTFORBAALFRAME  == LOG_Running) && ( THIRDGROUPSEK  ==  FALSE ) && (other.guild ==  GIL_SEK ) && Npc_KnowsInfo(hero,dia_shrat_noforever));
 	{
 		return TRUE;
 	};
@@ -202,20 +203,20 @@ func void dia_shrat_giveplant_info()
 {
 	var C_Item itm;
 	itm = Npc_GetEquippedArmor(other);
-	AI_Output(other,self,"DIA_Shrat_GivePlant_01_00");	//Я от Идола Кадара.
-	AI_Output(self,other,"DIA_Shrat_GivePlant_01_01");	//Идол Кадар прислал тебя? Но зачем?
-	AI_Output(other,self,"DIA_Shrat_GivePlant_01_02");	//Я должен забрать ваш урожай болотника, который вы успели собрать к этому моменту.
+	AI_Output(other, self, " DIA_Shrat_GivePlant_01_00 " );	// I'm from Idol Kadar.
+	AI_Output(self,other, " DIA_Shrat_GivePlant_01_01 " );	// Idol Kadar sent you? But why?
+	AI_Output(other,self, " DIA_Shrat_GivePlant_01_02 " );	// I should pick up your bogweed crop you've harvested so far.
 	if((Hlp_IsItem(itm,itar_sekbed) == TRUE) || (Hlp_IsItem(itm,itar_sekbed_v1) == TRUE) || (Hlp_IsItem(itm,itar_slp_ul) == TRUE) || (Hlp_IsItem(itm,itar_slp_l) == TRUE))
 	{
-		AI_Output(self,other,"DIA_Shrat_GivePlant_01_03");	//Наконец-то! А я уж думал, что про нас все забыли.
+		AI_Output(self,other, " DIA_Shrat_GivePlant_01_03 " );	// Finally! I thought everyone forgot about us.
 		B_GiveInvItems(self,other,ItPl_SwampHerb,50);
-		AI_Output(self,other,"DIA_Shrat_GivePlant_01_04");	//Вот, держи и не потеряй. Тут весь болотник, что у нас есть.
+		AI_Output(self,other, " DIA_Shrat_GivePlant_01_04 " );	// Here, hold it and don't lose it. Here is all the swamp that we have.
 		THIRDGROUPSEK = TRUE;
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_Shrat_GivePlant_01_05");	//Идол Кадар не мог послать к нам такого оборванца!
-		AI_Output(self,other,"DIA_Shrat_GivePlant_01_06");	//Ты верно пошутил, приятель.
+		AI_Output(self,other, " DIA_Shrat_GivePlant_01_05 " );	// The idol Kadar couldn't have sent us such a ragamuffin!
+		AI_Output(self,other, " DIA_Shrat_GivePlant_01_06 " );	// You're joking right, mate.
 	};
 };
 
