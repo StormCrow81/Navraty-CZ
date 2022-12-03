18c3c5b17bbca34784b1a36970dc8c5d5c4af45a
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 22:27:28 +0200

    Update DIA_dmt_12190_narus.d

--- RU-Main/Story/Dialoge/DIA_dmt_12190_narus.d
+++ RU-Main/Story/Dialoge/DIA_dmt_12190_narus.d
@@ -1,5 +1,6 @@
 
-instance DMT_12190_NARUS_EXIT(C_Info)
+
+instance DMT_12190_NARUS_EXIT (C_Info)
 {
 	npc = dmt_12190_narus;
 	nr = 999;
@@ -47,41 +48,41 @@ func void dmt_12190_narus_hello_info()
 		TELLWHATDONE = TRUE;
 		if(CHOOSEDARK == TRUE)
 		{
-			AI_Output(self,other,"DMT_12190_Narus_Hello_01");	//Тебе следует обратится к Хранителю Даготу, адепт!
-			AI_Output(self,other,"DMT_12190_Narus_Hello_02");	//Теперь он будет твоим наставником.
-			AI_Output(self,other,"DMT_12190_Narus_Hello_03");	//Я же ничем тебе более не могу помочь.
-			AI_Output(self,other,"DMT_12190_Narus_Hello_04");	//Теперь ступай!
+			AI_Output(self,other, " DMT_12190_Narus_Hello_01 " );	// You should contact Guardian Dagoth, adept!
+			AI_Output(self,other, " DMT_12190_Narus_Hello_02 " );	// He will now be your mentor.
+			AI_Output(self,other, " DMT_12190_Narus_Hello_03 " );	// I can't help you anymore.
+			AI_Output(self,other, " DMT_12190_Narus_Hello_04 " );	// Now go!
 			AI_StopProcessInfos(self);
 		}
 		else if(CHOOSESTONE == TRUE)
 		{
-			AI_Output(self,other,"DMT_12190_Narus_Hello_05");	//Тебе следует обратится к Хранителю Стонносу, адепт!
-			AI_Output(self,other,"DMT_12190_Narus_Hello_02");	//Теперь он будет твоим наставником.
-			AI_Output(self,other,"DMT_12190_Narus_Hello_03");	//Я же ничем тебе более не могу помочь.
-			AI_Output(self,other,"DMT_12190_Narus_Hello_04");	//Теперь ступай!
+			AI_Output(self,other, " DMT_12190_Narus_Hello_05 " );	// You should contact Guardian Stonnos, adept!
+			AI_Output(self,other, " DMT_12190_Narus_Hello_02 " );	// He will now be your mentor.
+			AI_Output(self,other, " DMT_12190_Narus_Hello_03 " );	// I can't help you anymore.
+			AI_Output(self,other, " DMT_12190_Narus_Hello_04 " );	// Now go!
 			AI_StopProcessInfos(self);
 		}
 		else if(CHOOSEWATER == TRUE)
 		{
-			AI_Output(self,other,"DMT_12190_Narus_Hello_09");	//Тебе следует обратится к Хранителю Вакону, адепт!
-			AI_Output(self,other,"DMT_12190_Narus_Hello_02");	//Теперь он будет твоим наставником.
-			AI_Output(self,other,"DMT_12190_Narus_Hello_03");	//Я же ничем тебе более не могу помочь.
-			AI_Output(self,other,"DMT_12190_Narus_Hello_04");	//Теперь ступай!
+			AI_Output(self,other, " DMT_12190_Narus_Hello_09 " );	// You should contact Guardian Vakon, adept!
+			AI_Output(self,other, " DMT_12190_Narus_Hello_02 " );	// He will now be your mentor.
+			AI_Output(self,other, " DMT_12190_Narus_Hello_03 " );	// I can't help you anymore.
+			AI_Output(self,other, " DMT_12190_Narus_Hello_04 " );	// Now go!
 			AI_StopProcessInfos(self);
 		}
 		else if(CHOOSEFIRE == TRUE)
 		{
-			AI_Output(self,other,"DMT_12190_Narus_Hello_13");	//Тебе следует обратится к Хранителю Келиосу, адепт!
-			AI_Output(self,other,"DMT_12190_Narus_Hello_02");	//Теперь он будет твоим наставником.
-			AI_Output(self,other,"DMT_12190_Narus_Hello_03");	//Я же ничем тебе более не могу помочь.
-			AI_Output(self,other,"DMT_12190_Narus_Hello_04");	//Теперь ступай!
+			AI_Output(self,other, " DMT_12190_Narus_Hello_13 " );	// You should contact Guardian Kelios, adept!
+			AI_Output(self,other, " DMT_12190_Narus_Hello_02 " );	// He will now be your mentor.
+			AI_Output(self,other, " DMT_12190_Narus_Hello_03 " );	// I can't help you anymore.
+			AI_Output(self,other, " DMT_12190_Narus_Hello_04 " );	// Now go!
 			AI_StopProcessInfos(self);
 		};
 	}
 	else
 	{
-		AI_Output(self,other,"DMT_12190_Narus_Hello_17");	//Ты уже знаешь, что тебе следует делать.
-		AI_Output(self,other,"DMT_12190_Narus_Hello_04");	//Теперь ступай!
+		AI_Output(self,other, " DMT_12190_Narus_Hello_17 " );	// You already know what you should do.
+		AI_Output(self,other, " DMT_12190_Narus_Hello_04 " );	// Now go!
 		AI_StopProcessInfos(self);
 	};
 };
