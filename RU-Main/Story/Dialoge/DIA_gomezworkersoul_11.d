7db7e58f639cfc12cbab0a7ad76e3ff8ba8961f9
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 20:33:12 +0200

    Update DIA_gomezworkersoul_11.d

--- RU-Main/Story/Dialoge/DIA_gomezworkersoul_11.d
+++ RU-Main/Story/Dialoge/DIA_gomezworkersoul_11.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_GOMEZWORKERSOUL_111_EXIT(C_Info)
 {
 	npc = gomezworkersoul_11;
@@ -21,12 +22,12 @@ func void dia_gomezworkersoul_11_exit_info()
 };
 
 
-instance DIA_GOMEZWORKERSOUL_11_HALLO(C_Info)
+instance DIA_GOMEZWORKERSOUL_11_HELLO (C_Info)
 {
 	npc = gomezworkersoul_11;
 	nr = 1;
 	condition = dia_gomezworkersoul_11_hallo_condition;
-	information = dia_gomezworkersoul_11_hallo_info;
+	information = dia_gomezworkersoul_11_hello_info;
 	permanent = FALSE;
 	important = TRUE;
 };
@@ -40,33 +41,33 @@ func int dia_gomezworkersoul_11_hallo_condition()
 	};
 };
 
-func void dia_gomezworkersoul_11_hallo_info()
+func void dia_gomezworkersoul_11_hello_info()
 {
-	self.aivar[AIV_EnemyOverride] = FALSE;
+	self.aivar[AIV_EnemyOverride] = FALSE ;
 	Snd_Play("MFX_FEAR_CAST");
 	B_GivePlayerXP(50);
-	AI_Output(self,other,"DIA_GomezWorkerSoul_11_HALLO_01_00");	//(мертвым голосом) ЧТО НУЖНО ЗДЕСЬ ТАКОМУ, КАК ТЫ, СМЕРТНЫЙ?
-	AI_Output(other,self,"DIA_GomezWorkerSoul_11_HALLO_01_01");	//Это нужно, скорее, не мне, а тебе, призрак!
-	AI_Output(other,self,"DIA_GomezWorkerSoul_11_HALLO_01_02");	//Ты жаждешь, чтобы кто-нибудь снял с тебя проклятие и упокоил твою душу. Я прав?
-	AI_Output(self,other,"DIA_GomezWorkerSoul_11_HALLO_01_03");	//ТЫ И ВПРАВДУ ХОЧЕШЬ ОСВОБОДИТЬ МЕНЯ? Аргххх...
-	AI_Output(self,other,"DIA_GomezWorkerSoul_11_HALLO_01_04");	//Тогда я не буду тебе мешать. Делай то, что задумал!
-	AI_Output(other,self,"DIA_GomezWorkerSoul_11_HALLO_01_05");	//Не так быстро! Для начала сделай кое-что и для меня.
-	AI_Output(self,other,"DIA_GomezWorkerSoul_11_HALLO_01_06");	//Что ты хочешь?
-	AI_Output(other,self,"DIA_GomezWorkerSoul_11_HALLO_01_07");	//Все, что я прошу тебя сделать, - даровать свое прощение Гомезу.
-	AI_Output(self,other,"DIA_GomezWorkerSoul_11_HALLO_01_09");	//Гомез?! Хммм...(мертвый вздох) Я помню это имя.
-	AI_Output(self,other,"DIA_GomezWorkerSoul_11_HALLO_01_10");	//Даже сейчас, когда я уже мертв, оно внушает мне ужас и страх!
-	AI_Output(self,other,"DIA_GomezWorkerSoul_11_HALLO_01_11");	//И мне кажется, что таким людям, как Гомез, нет прощения в этом мире.
-	AI_Output(self,other,"DIA_GomezWorkerSoul_11_HALLO_01_13");	//Но я уже не в силах больше терпеть эти страдания.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_11_HALLO_01_00 " );	// (dead voice) WHAT DOES A MORTAL LIKE YOU NEED HERE?
+	AI_Output(other,self, " DIA_GomezWorkerSoul_11_HALLO_01_01 " );	// This is needed, rather, not for me, but for you, ghost!
+	AI_Output(other,self, " DIA_GomezWorkerSoul_11_HALLO_01_02 " );	// You long for someone to lift the curse from you and put your soul to rest. I'm right?
+	AI_Output(self,other, " DIA_GomezWorkerSoul_11_HALLO_01_03 " );	// DO YOU REALLY WANT TO FREE ME? Arghhh...
+	AI_Output(self,other, " DIA_GomezWorkerSoul_11_HALLO_01_04 " );	// Then I won't bother you. Do what you think!
+	AI_Output(other,self, " DIA_GomezWorkerSoul_11_HALLO_01_05 " );	// Not so fast! First, do something for me.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_11_HALLO_01_06 " );	// What do you want?
+	AI_Output(other,self, " DIA_GomezWorkerSoul_11_HALLO_01_07 " );	// All I'm asking you to do is grant your forgiveness to Gomez.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_11_HALLO_01_09 " );	// Gomez?! Hmmm... (dead breath) I remember that name.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_11_HALLO_01_10 " );	// Even now, when I'm already dead, it fills me with horror and fear!
+	AI_Output(self,other, " DIA_GomezWorkerSoul_11_HALLO_01_11 " );	// And it seems to me that people like Gomez have no forgiveness in this world.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_11_HALLO_01_13 " );	// But I can no longer endure this suffering.
 	Info_ClearChoices(dia_gomezworkersoul_11_hallo);
-	Info_AddChoice(dia_gomezworkersoul_11_hallo,"Значит ли это, что он прощен тобой?",dia_gomezworkersoul_11_hallo_end);
+	Info_AddChoice(dia_gomezworkersoul_11_hallo, " Does this mean he is forgiven by you? " ,dia_gomezworkersoul_11_hallo_end);
 };
 
-func void dia_gomezworkersoul_11_hallo_end()
+func void dia_gomezworkersoul_11_hello_end()
 {
 	Snd_Play("SFX_INNOSEYE");
-	AI_Output(other,self,"DIA_GomezWorkerSoul_11_HALLO_End_01_01");	//Значит ли это, что он прощен тобой?
-	AI_Output(self,other,"DIA_GomezWorkerSoul_11_HALLO_End_01_02");	//Да... Его душа прощена мною.
-	AI_Output(self,other,"DIA_GomezWorkerSoul_11_HALLO_End_01_03");	//Теперь же помоги мне снять мое проклятие! Аргххх...
+	AI_Output(other,self, " DIA_GomezWorkerSoul_11_HALLO_End_01_01 " );	// Does this mean that he is forgiven by you?
+	AI_Output(self,other, " DIA_GomezWorkerSoul_11_HALLO_End_01_02 " );	// Yes... His soul is forgiven by me.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_11_HALLO_End_01_03 " );	// Now help me break my curse! Arghhh...
 	GOMEZSOULTALK_11 = TRUE;
 	AI_StopProcessInfos(self);
 };
