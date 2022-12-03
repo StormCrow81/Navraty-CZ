1bfdfa8b0377700bd46ad993d74b8127db80f7bf
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 13:48:43 +0200

    Update DIA_pal_2170_marcos.d

--- RU-Main/Story/Dialoge/DIA_pal_2170_marcos.d
+++ RU-Main/Story/Dialoge/DIA_pal_2170_marcos.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_PAL_2170_MARCOS_EXIT(C_Info)
 {
 	npc = pal_2170_marcos;
@@ -28,7 +29,7 @@ instance DIA_PAL_2170_MARCOS_PERM(C_Info)
 	condition = dia_pal_2170_marcos_perm_condition;
 	information = dia_pal_2170_marcos_perm_info;
 	permanent = TRUE;
-	description = "Как обстоят дела?";
+	description = " How are things? " ;
 };
 
 
@@ -39,11 +40,11 @@ func int dia_pal_2170_marcos_perm_condition()
 
 func void dia_pal_2170_marcos_perm_info()
 {
-	AI_Output(other,self,"DIA_PAL_2170_Marcos_Perm_01_00");	//Как обстоят дела?
-	AI_Output(self,other,"DIA_PAL_2170_Marcos_Perm_01_01");	//Ты и сам все видишь. Честно говоря, я и представить себе не мог, что орки так быстро доберутся сюда.
-	AI_Output(other,self,"DIA_PAL_2170_Marcos_Perm_01_02");	//Да уж. Здесь сейчас даже опаснее, чем в Долине Рудников.
-	AI_Output(self,other,"DIA_PAL_2170_Marcos_Perm_01_03");	//Как раз это меня не сильно пугает. Я уже привык смотреть смерти в лицо. Иннос тому свидетель!
-	AI_Output(self,other,"DIA_PAL_2170_Marcos_Perm_01_04");	//(воинственно) Ну, ничего! Мы еще покажем этим мерзким тварям. В конце концов, победа будет за нами!
-	AI_Output(other,self,"DIA_PAL_2170_Marcos_Perm_01_05");	//Ну да, конечно.
+	AI_Output(other,self, " DIA_PAL_2170_Marcos_Perm_01_00 " );	// How are things going?
+	AI_Output(self,other, " DIA_PAL_2170_Marcos_Perm_01_01 " );	// You yourself see everything. To be honest, I never imagined that the orcs would get here so quickly.
+	AI_Output(other,self, " DIA_PAL_2170_Marcos_Perm_01_02 " );	// Yeah. It's even more dangerous here now than in the Valley of Mines.
+	AI_Output(self,other, " DIA_PAL_2170_Marcos_Perm_01_03 " );	// It just doesn't scare me that much. I'm used to looking death in the face. Innos is a witness!
+	AI_Output(self,other, " DIA_PAL_2170_Marcos_Perm_01_04 " );	// (belligerently) Well, nothing! We'll show these vile creatures. In the end, victory will be ours!
+	AI_Output(other,self, " DIA_PAL_2170_Marcos_Perm_01_05 " );	// Well, yes, of course.
 };
 
