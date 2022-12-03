ed901ca8daf9835e7fa92397ac86534fe04ffec9
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 09:51:19 +0200

    Update DIA_skeleton_lord_kadar.d

--- RU-Main/Story/Dialoge/DIA_skeleton_lord_kadar.d
+++ RU-Main/Story/Dialoge/DIA_skeleton_lord_kadar.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_SKELETON_LORD_KADAR_EXIT(C_Info)
 {
 	npc = skeleton_lord_kadar;
@@ -21,7 +22,7 @@ func void dia_skeleton_lord_kadar_exit_info()
 };
 
 
-instance DIA_SKELETON_LORD_KADAR_HELLO(C_Info)
+instance DIA_SKELETON_LORD_KADAR_HELLO (C_Info)
 {
 	npc = skeleton_lord_kadar;
 	nr = 1;
@@ -43,22 +44,22 @@ func void dia_skeleton_lord_kadar_hello_info()
 	//Wld_PlayEffect("DEMENTOR_FX",hero,hero,0,0,0,FALSE);
 	Wld_PlayEffect("spellFX_Fear",self,self,0,0,0,FALSE);
 	Snd_Play("DEM_Warn");
-	AI_Output(self,other,"DIA_Skeleton_Lord_Kadar_Hello_01_00");	//СМЕЕЕЕЕЕЕРТНЫЫЫЙ!
-	AI_Output(self,other,"DIA_Skeleton_Lord_Kadar_Hello_01_01");	//КАК ОСМЕЕЕЛИЛСЯ ТЫ ПОТРЕВОЖИТЬ МЕНЯ???!!!
-	AI_Output(self,other,"DIA_Skeleton_Lord_Kadar_Hello_01_02");	//БУДЬ ЖЕ ПРОКЛЯТ!
-	AI_Output(other,self,"DIA_Skeleton_Lord_Kadar_Hello_01_03");	//Проклинай САМ СЕБЯ!
-	AI_Output(self,other,"DIA_Skeleton_Lord_Kadar_Hello_01_04");	//ТВОООЙ ДОООЛГИЙ ПУУУТЬ БУУУДЕТ ОКОООНЧЕН ЗДЕЕЕЕСЬ!!!
-	AI_Output(other,self,"DIA_Skeleton_Lord_Kadar_Hello_01_05");	//Признаться, эта идея мне не слишком по душе!
-	AI_Output(self,other,"DIA_Skeleton_Lord_Kadar_Hello_01_06");	//ГОТОВЬСЯ УМЕРЕТЬ!
+	AI_Output(self,other, " DIA_Skeleton_Lord_Kadar_Hello_01_00 " );	// FOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOD!
+	AI_Output(self,other, " DIA_Skeleton_Lord_Kadar_Hello_01_01 " );	// HOW DARE YOU ALARM ME???!!!
+	AI_Output(self,other, " DIA_Skeleton_Lord_Kadar_Hello_01_02 " );	// BE DAMNED!
+	AI_Output(other,self, " DIA_Skeleton_Lord_Kadar_Hello_01_03 " );	// Curse YOURSELF!
+	AI_Output(self,other, " DIA_Skeleton_Lord_Kadar_Hello_01_04 " );	// YOUR SOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOT out out here!!!
+	AI_Output(other,self, " DIA_Skeleton_Lord_Kadar_Hello_01_05 " );	// Frankly, this idea is not to my liking!
+	AI_Output(self,other, " DIA_Skeleton_Lord_Kadar_Hello_01_06 " );	// PREPARE TO DIE!
 	Info_ClearChoices(dia_skeleton_lord_kadar_hello);
-	Info_AddChoice(dia_skeleton_lord_kadar_hello,"(закончить разговор)",dia_skeleton_lord_kadar_hello_ex1);
+	Info_AddChoice(dia_skeleton_lord_kadar_hello, " (end call) " ,dia_skeleton_lord_kadar_hello_ex1);
 };
 
 func void dia_skeleton_lord_kadar_hello_ex1()
 {
 	Wld_StopEffect("DEMENTOR_FX");
 	AI_StopProcessInfos(self);
-	self.aivar[AIV_EnemyOverride] = FALSE;
+	self.aivar[AIV_EnemyOverride] = FALSE ;
 	self.start_aistate = ZS_MM_AllScheduler;
 };
 
