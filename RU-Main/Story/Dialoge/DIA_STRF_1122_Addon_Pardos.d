f4a4d6e136386d82e7a8f038a1843abefde9b114
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 18:35:54 +0200

    Update DIA_STRF_1122_Addon_Pardos.d

--- RU-Main/Story/Dialoge/DIA_STRF_1122_Addon_Pardos.d
+++ RU-Main/Story/Dialoge/DIA_STRF_1122_Addon_Pardos.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_Addon_Pardos_EXIT(C_Info)
 {
 	npc = STRF_1122_Addon_Pardos;
@@ -28,25 +29,25 @@ instance DIA_Addon_Pardos_Hi(C_Info)
 	condition = DIA_Addon_Pardos_Hi_Condition;
 	information = DIA_Addon_Pardos_Hi_Info;
 	permanent = FALSE;
-	description = "Как ты?";
+	description = " How are you? " ;
 };
 
 
 func int DIA_Addon_Pardos_Hi_Condition()
 {
-	if(Sklaven_Flucht == FALSE)
+	if (slave_escape ==  FALSE )
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_Addon_Pardos_Hi_Info()
 {
 	AI_Output(self,other,"DIA_Addon_Pardos_Hi_03_00");	//Как ты?
-	AI_Output(self,other,"DIA_Addon_Pardos_Hi_03_01");	//Я устал...(устало) Я совершенно изможден! Скажи, сейчас солнечный день или ясная ночь, и тысячи звезд мерцают на небосклоне?
-	AI_Output(self,other,"DIA_Addon_Pardos_Hi_03_02");	//Я не знаю, идет ли теплый летний дождь или дует прохладный вечерний ветер. Я больше ничего не чувствую!
-	AI_Output(self,other,"DIA_Addon_Pardos_Hi_03_03");	//Первыми здесь отказывают глаза.
+	AI_Output(self,other, " DIA_Addon_Pardos_Hi_03_01 " );	// I'm tired... (tired) I'm completely exhausted! Tell me, is it a sunny day or a clear night, and thousands of stars twinkle in the sky?
+	AI_Output(self,other, " DIA_Addon_Pardos_Hi_03_02 " );	// I don't know if it's a warm summer rain or a cool evening wind. I don't feel anything anymore!
+	AI_Output(self,other, " DIA_Addon_Pardos_Hi_03_03 " );	// The eyes fail here first.
 };
 
 
@@ -57,75 +58,75 @@ instance DIA_Addon_Pardos_save(C_Info)
 	condition = DIA_Addon_Pardos_save_Condition;
 	information = DIA_Addon_Pardos_save_Info;
 	permanent = FALSE;
-	description = "Держись. Помощь скоро придет.";
+	description = " Hold on. Help is coming soon. " ;
 };
 
 
 func int DIA_Addon_Pardos_save_Condition()
 {
-	if(Npc_KnowsInfo(other,DIA_Addon_Pardos_Hi) && (Sklaven_Flucht == FALSE))
+	if ( Npc_KnowsInfo ( other , DIA_Addon_Pardos_Hi ) && ( Slave_Escape ==  FALSE ))
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_Addon_Pardos_save_Info()
 {
-	AI_Output(other,self,"DIA_Addon_Pardos_save_15_00");	//Держись. Помощь скоро придет.
-	AI_Output(self,other,"DIA_Addon_Pardos_save_03_01");	//(устало) Спасибо. Спасибо за добрые слова.
+	AI_Output(other,self, " DIA_Addon_Pardos_save_15_00 " );	// Hold on. Help will come soon.
+	AI_Output(self,other, " DIA_Addon_Pardos_save_03_01 " );	// (wearily) Thank you. Thanks for the kind words.
 };
 
 
-var int Pardos_Geheilt;
+var int Pardos_Healed;
 
-instance DIA_Addon_Pardos_trank(C_Info)
+instance DIA_Addon_Pardos_trank (C_Info)
 {
 	npc = STRF_1122_Addon_Pardos;
 	nr = 2;
 	condition = DIA_Addon_Pardos_trank_Condition;
 	information = DIA_Addon_Pardos_trank_Info;
 	permanent = FALSE;
-	description = "Вот, выпей. (дать зелье)";
+	description = " Here, drink. (give potion) " ;
 };
 
 
 func int DIA_Addon_Pardos_trank_Condition()
 {
-	if((Pardos_Geheilt == FALSE) && Npc_KnowsInfo(other,DIA_Addon_Pardos_save))
+	if ((Pardos_Geheilt ==  FALSE ) && Npc_KnowsInfo(other,DAY_Addon_Pardos_save))
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_Addon_Pardos_trank_Info()
 {
-	AI_Output(other,self,"DIA_Addon_Pardos_trank_15_00");	//Вот, выпей.
+	AI_Output(other,self, " DIA_Addon_Pardos_trank_15_00 " );	// Here, have a drink.
 	AI_StandupQuick(self);
 	Info_ClearChoices(DIA_Addon_Pardos_trank);
 	Info_AddChoice(DIA_Addon_Pardos_trank,Dialog_Back,DIA_Addon_Pardos_trank_BACK);
 	if(Npc_HasItems(other,ItPo_Health_01))
 	{
-		Info_AddChoice(DIA_Addon_Pardos_trank,"Дать эссенцию лечения",DIA_Addon_Pardos_trank_01);
+		Info_AddChoice(DIA_Addon_Pardos_trank, " Give healing essence " ,DIA_Addon_Pardos_trank_01);
 	};
 	if(Npc_HasItems(other,ItPo_Health_02))
 	{
-		Info_AddChoice(DIA_Addon_Pardos_trank,"дать экстракт лечения",DIA_Addon_Pardos_trank_02);
+		Info_AddChoice(DIA_Addon_Pardos_trank, " give healing extract " ,DIA_Addon_Pardos_trank_02);
 	};
 	if(Npc_HasItems(other,ItPo_Health_03))
 	{
-		Info_AddChoice(DIA_Addon_Pardos_trank,"Дать эликсир лечения",DIA_Addon_Pardos_trank_03);
+		Info_AddChoice(DIA_Addon_Pardos_trank, " Give Healing Elixir " ,DIA_Addon_Pardos_trank_03);
 	};
 	if(Npc_HasItems(other,ItFo_Addon_Meatsoup))
 	{
-		Info_AddChoice(DIA_Addon_Pardos_trank,"Дать мясной суп.",DIA_Addon_Pardos_trank_Soup);
+		Info_AddChoice(DIA_Addon_Pardos_trank, " Give Meat Soup. " ,DIA_Addon_Pardos_trank_Soup);
 	};
 };
 
 func void B_SayPardosThanks()
 {
-	AI_Output(self,other,"DIA_Addon_Pardos_trank_03_00");	//Спасибо. Мне уже лучше.
+	AI_Output(self,other, " DIA_Addon_Pardos_trank_03_00 " );	// Thank you. I'm better now.
 };
 
 func void DIA_Addon_Pardos_trank_BACK()
@@ -140,7 +141,7 @@ func void DIA_Addon_Pardos_trank_01()
 		B_UseItem(self,ItPo_Health_01);
 	};
 	Info_ClearChoices(DIA_Addon_Pardos_trank);
-	Pardos_Geheilt = TRUE;
+	Pardos_Healed = TRUE ;
 	B_SayPardosThanks();
 	B_GivePlayerXP(XP_Ambient);
 };
@@ -152,7 +153,7 @@ func void DIA_Addon_Pardos_trank_02()
 		B_UseItem(self,ItPo_Health_02);
 	};
 	Info_ClearChoices(DIA_Addon_Pardos_trank);
-	Pardos_Geheilt = TRUE;
+	Pardos_Healed = TRUE ;
 	B_SayPardosThanks();
 	B_GivePlayerXP(XP_Ambient * 2);
 };
@@ -164,20 +165,20 @@ func void DIA_Addon_Pardos_trank_03()
 		B_UseItem(self,ItPo_Health_03);
 	};
 	Info_ClearChoices(DIA_Addon_Pardos_trank);
-	Pardos_Geheilt = TRUE;
+	Pardos_Healed = TRUE ;
 	B_SayPardosThanks();
 	B_GivePlayerXP(XP_Ambient * 3);
 };
 
 func void DIA_Addon_Pardos_trank_Soup()
 {
-	AI_Output(other,self,"DIA_Addon_Pardos_trank_soup_15_00");	//Вот, поешь мясного супчика.
+	AI_Output(other,self, " DIA_Addon_Pardos_trank_soup_15_00 " );	// Here, eat meat soup.
 
 	if(B_GiveInvItems(other,self,ItFo_Addon_Meatsoup,1))
 	{
 		B_UseItem(self,ItFo_Addon_Meatsoup);
 	};
-	AI_Output(self,other,"DIA_Addon_Pardos_trank_03_01");	//Спасибо. Я уже не так слаб.
+	AI_Output(self,other, " DIA_Addon_Pardos_trank_03_01 " );	// Thank you. I'm not so weak anymore.
 	B_GivePlayerXP(XP_Ambient * 3);
 };
 
@@ -189,22 +190,22 @@ instance DIA_Addon_Pardos_perm(C_Info)
 	condition = DIA_Addon_Pardos_perm_Condition;
 	information = DIA_Addon_Pardos_perm_Info;
 	permanent = TRUE;
-	description = "Как ты себя чувствуешь?";
+	description = " How are you feeling? " ;
 };
 
 
 func int DIA_Addon_Pardos_perm_Condition()
 {
-	if(Pardos_Geheilt == TRUE)
+	if (Pardos_Healed ==  TRUE )
 	{
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
 func void DIA_Addon_Pardos_perm_Info()
 {
-	AI_Output(other,self,"DIA_Addon_Pardos_perm_15_00");	//Как ты себя чувствуешь?
-	AI_Output(self,other,"DIA_Addon_Pardos_perm_03_01");	//Спасибо, лучше. Ко мне возвращаются силы.
+	AI_Output(other,self, " DIA_Addon_Pardos_perm_15_00 " );	// How do you feel?
+	AI_Output(self,other, " DIA_Addon_Pardos_perm_03_01 " );	// Thank you, better. My strength is returning.
 };
 
