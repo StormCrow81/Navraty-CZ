a0105c293fc4167a0d629c7d467dfa59ce80d365
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 23:12:48 +0200

    Update DIA_djg_7110_godar.d

--- RU-Main/Story/Dialoge/DIA_djg_7110_godar.d
+++ RU-Main/Story/Dialoge/DIA_djg_7110_godar.d
@@ -1,7 +1,8 @@
 
+
 instance DIA_DJG_7110_GODAR_EXIT(C_Info)
 {
-	npc = djg_7110_godar;
+	npc = djg_7110_goddar;
 	nr = 999;
 	condition = dia_djg_7110_godar_exit_condition;
 	information = dia_djg_7110_godar_exit_info;
@@ -23,11 +24,11 @@ func void dia_djg_7110_godar_exit_info()
 
 instance DIA_DJG_7110_GODAR_PERM(C_Info)
 {
-	npc = djg_7110_godar;
+	npc = djg_7110_goddar;
 	condition = dia_djg_7110_godar_perm_condition;
 	information = dia_djg_7110_godar_perm_info;
 	permanent = TRUE;
-	description = "Проголодался?";
+	description = " Hungry? " ;
 };
 
 
@@ -41,21 +42,21 @@ func int dia_djg_7110_godar_perm_condition()
 
 func void dia_djg_7110_godar_perm_info()
 {
-	AI_Output(other,self,"DIA_DJG_7110_Godar_Perm_01_00");	//Проголодался?
-	AI_Output(self,other,"DIA_DJG_7110_Godar_Perm_01_01");	//А сам не видишь? Там в Долине Рудников нормальная еда на вес золота. Не то что здесь.
-	AI_Output(self,other,"DIA_DJG_7110_Godar_Perm_01_02");	//Поэтому, пока есть возможность, я хочу набить свое брюхо до отвала.
-	AI_Output(self,other,"DIA_DJG_7110_Godar_Perm_01_03");	//Кто знает, что там будет потом!
-	AI_Output(other,self,"DIA_DJG_7110_Godar_Perm_01_04");	//Ну ладно, тогда не буду мешать.
+	AI_Output(other,self, " DIA_DJG_7110_Godar_Perm_01_00 " );	// Hungry?
+	AI_Output(self,other, " DIA_DJG_7110_Godar_Perm_01_01 " );	// Can't you see it yourself? There in the Valley of Mines, normal food is worth its weight in gold. Not like here.
+	AI_Output(self,other, " DIA_DJG_7110_Godar_Perm_01_02 " );	// Therefore, while I can, I want to stuff my belly to the bone.
+	AI_Output(self,other, " DIA_DJG_7110_Godar_Perm_01_03 " );	// Who knows what will happen next!
+	AI_Output(other,self, " DIA_DJG_7110_Godar_Perm_01_04 " );	// Okay, then I won't interfere.
 	AI_StopProcessInfos(self);
 };
 
 
 instance DIA_DJG_7110_GODAR_PICKPOCKET(C_Info)
 {
-	npc = djg_7110_godar;
+	npc = djg_7110_goddar;
 	nr = 900;
 	condition = dia_djg_7110_godar_pickpocket_condition;
-	information = dia_djg_7110_godar_pickpocket_info;
+	info = dia_djg_7110_godar_pickpocket_info;
 	permanent = TRUE;
 	description = PICKPOCKET_COMM;
 };
@@ -63,19 +64,19 @@ instance DIA_DJG_7110_GODAR_PICKPOCKET(C_Info)
 
 func int dia_djg_7110_godar_pickpocket_condition()
 {
-	return C_Beklauen(100,350);
+	return  C_Robbery ( 100 , 350 );
 };
 
 func void dia_djg_7110_godar_pickpocket_info()
 {
 	Info_ClearChoices(dia_djg_7110_godar_pickpocket);
 	Info_AddChoice(dia_djg_7110_godar_pickpocket,Dialog_Back,dia_djg_7110_godar_pickpocket_back);
-	Info_AddChoice(dia_djg_7110_godar_pickpocket,DIALOG_PICKPOCKET,dia_djg_7110_godar_pickpocket_doit);
+	Info_AddChoice(dia_djg_7110_godar_pickpocket, DIALOG_PICKPOCKET ,dia_djg_7110_godar_pickpocket_doit);
 };
 
 func void dia_djg_7110_godar_pickpocket_doit()
 {
-	B_Beklauen();
+	B_Robbery();
 	Info_ClearChoices(dia_djg_7110_godar_pickpocket);
 };
 
@@ -87,7 +88,7 @@ func void dia_djg_7110_godar_pickpocket_back()
 
 instance DIA_DJG_7110_GODAR_FUCKOFF(C_Info)
 {
-	npc = djg_7110_godar;
+	npc = djg_7110_goddar;
 	nr = 2;
 	condition = dia_djg_7110_godar_fuckoff_condition;
 	information = dia_djg_7110_godar_fuckoff_info;
