31b4c219cf5d14cf5634665bf527f12951c3c0b4
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 22:42:48 +0200

    Update DIA_gur_8004_parvez.d

--- RU-Main/Story/Dialoge/DIA_gur_8004_parvez.d
+++ RU-Main/Story/Dialoge/DIA_gur_8004_parvez.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_BAALPARVEZ_EXIT(C_Info)
 {
 	npc = gur_8004_parvez;
@@ -32,19 +33,19 @@ instance dia_baalparvez_PICKPOCKET(C_Info)
 
 func int dia_baalparvez_PICKPOCKET_Condition()
 {
-	return C_Beklauen(49,35);
+	return  C_Robbery ( 49 , 35 );
 };
 
 func void dia_baalparvez_PICKPOCKET_Info()
 {
 	Info_ClearChoices(dia_baalparvez_PICKPOCKET);
 	Info_AddChoice(dia_baalparvez_PICKPOCKET,Dialog_Back,dia_baalparvez_PICKPOCKET_BACK);
-	Info_AddChoice(dia_baalparvez_PICKPOCKET,DIALOG_PICKPOCKET,dia_baalparvez_PICKPOCKET_DoIt);
+	Info_AddChoice(dia_baalparvez_PICKPOCKET, DIALOG_PICKPOCKET ,dia_baalparvez_PICKPOCKET_DoIt);
 };
 
 func void dia_baalparvez_PICKPOCKET_DoIt()
 {
-	B_Beklauen();
+	B_Robbery();
 	Info_ClearChoices(dia_baalparvez_PICKPOCKET);
 };
 
@@ -54,15 +55,15 @@ func void dia_baalparvez_PICKPOCKET_BACK()
 };
 
 
-instance DIA_BAALPARVEZ_NOTALK_SLEEPER(C_Info)
+instance DIA_BAALPARVEZ_NOTALK_SLEEPER (C_Info)
 {
 	npc = gur_8004_parvez;
 	nr = 1;
 	condition = dia_baalparvez_notalk_sleeper_condition;
-	information = dia_baalparvez_notalk_sleeper_info;
+	info = dia_baalparvez_notalk_sleeper_info;
 	permanent = FALSE;
 	important = FALSE;
-	description = "Да пребудет с тобой Спящий!";
+	description = " May the Sleeper be with you! " ;
 };
 
 
@@ -76,7 +77,7 @@ func int dia_baalparvez_notalk_sleeper_condition()
 
 func void dia_baalparvez_notalk_sleeper_info()
 {
-	AI_Output(other,self,"DIA_BaalParvez_NoTalk_Sleeper_01_00");	//Да пребудет с тобой Спящий!
+	AI_Output(other,self, " DIA_BaalParvez_NoTalk_Sleeper_01_00 " );	// May the Sleeper be with you!
 	AI_Output(self,other,"DIA_BaalParvez_NoTalk_Sleeper_01_01");	//(вздох)
 	AI_StopProcessInfos(self);
 };
@@ -90,7 +91,7 @@ instance DIA_BAALPARVEZ_NOTALK_IMP(C_Info)
 	information = dia_baalparvez_notalk_imp_info;
 	permanent = TRUE;
 	important = FALSE;
-	description = "Все в порядке, приятель?";
+	description = " Is everything all right, buddy? " ;
 };
 
 
@@ -104,7 +105,7 @@ func int dia_baalparvez_notalk_imp_condition()
 
 func void dia_baalparvez_notalk_imp_info()
 {
-	AI_Output(other,self,"DIA_BaalParvez_NoTalk_Imp_01_00");	//Все в порядке, приятель?
+	AI_Output(other,self, " DIA_BaalParvez_NoTalk_Imp_01_00 " );	// Are you all right, mate?
 	AI_Output(self,other,"DIA_BaalParvez_NoTalk_Sleeper_01_01");	//(вздох)
 	AI_StopProcessInfos(self);
 };
@@ -118,12 +119,12 @@ instance DIA_BAALPARVEZ_SPECIALJOINT(C_Info)
 	information = dia_baalparvez_specialjoint_info;
 	permanent = TRUE;
 	important = FALSE;
-	description = "(предложить приготовленный 'Зов Сна')";
+	description = " (offer cooked 'Sleep Call') " ;
 };
 
 func int dia_baalparvez_specialjoint_condition()
 {
-	if((IDOLPARVEZ_YES == FALSE) && (CANBEGURU == TRUE) && (other.guild == GIL_SEK) && (Npc_HasItems(other,itmi_specialjoint) >= 1))
+	if (( IDOLPARVEZ_YES  ==  FALSE ) && ( CANBEGURU  ==  TRUE ) && (other.guild ==  GIL_SEK ) && (Npc_HasItems(other,itmi_specialjoint) >=  1 )) ;
 	{
 		return TRUE;
 	};
@@ -131,7 +132,7 @@ func int dia_baalparvez_specialjoint_condition()
 
 func void dia_baalparvez_specialjoint_info()
 {
-	AI_Output(other,self,"DIA_BaalParvez_SpecialJoint_01_00");	//Вот, господин мой, - примите сей скромный дар от вашего верного ученика.
+	AI_Output(other,self, " DIA_BaalParvez_SpecialJoint_01_00 " );	// Here, my lord, accept this modest gift from your faithful disciple.
 	AI_StopProcessInfos(self);
 	B_GiveInvItems(other,self,itmi_specialjoint,1);
 	if(C_BodyStateContains(self,BS_SIT))
@@ -143,7 +144,7 @@ func void dia_baalparvez_specialjoint_info()
 	AI_Output(self,other,"DIA_BaalParvez_SpecialJoint_01_01");	//Мммм...
 	IDOLPARVEZ_YES = TRUE;
 	Npc_SetRefuseTalk(self,5);
-	hero.aivar[AIV_INVINCIBLE] = FALSE;
+	hero.aivar[ AIV_INVINCIBLE ] = FALSE ;
 };
 
 
@@ -168,25 +169,25 @@ func int dia_baalparvez_specialjointok_condition()
 
 func void dia_baalparvez_specialjointok_info()
 {
-	AI_Output(self,other,"DIA_BaalParvez_SpecialJointOk_01_00");	//Во имя Спящего! Меня посетило видение!
-	AI_Output(self,other,"DIA_BaalParvez_SpecialJointOk_01_01");	//Это было невероятно! Я видел, что мы встретили нового брата, который был не похож на всех тех, что были до него.
-	AI_Output(self,other,"DIA_BaalParvez_SpecialJointOk_01_02");	//Он что-то делал для нас... В руке у него был меч, и он спускался по широкой лестнице. На этом видение закончилось.
-	AI_Output(self,other,"DIA_BaalParvez_SpecialJointOk_01_03");	//Он был очень похож на ТЕБЯ. Кто ты? Что тебе нужно?
-	AI_Output(other,self,"DIA_BaalParvez_SpecialJointOk_01_04");	//Я бы хотел присоединиться к Братству и прошу вашего согласия, господин.
-	AI_Output(self,other,"DIA_BaalParvez_SpecialJointOk_01_05");	//Хмм...(пристально смотрит) Кто послал тебя ко мне?
-	AI_Output(other,self,"DIA_BaalParvez_SpecialJointOk_01_06");	//Меня послал Идол Оран. Он сказал, что я должен получить на то ваше одобрение.
-	AI_Output(self,other,"DIA_BaalParvez_SpecialJointOk_01_07");	//Что же, если тебя прислал Идол Оран, - это вполне возможно.
-	AI_Output(self,other,"DIA_BaalParvez_SpecialJointOk_01_08");	//Окажи мне одну услугу. Докажи свою преданность, и тогда я дам свое согласие на то, чтобы ты стал одним из нас.
-	AI_Output(other,self,"DIA_BaalParvez_SpecialJointOk_01_09");	//Как я могу доказать свою преданность?
-	AI_Output(self,other,"DIA_BaalParvez_SpecialJointOk_01_10");	//Больше всего мы нуждаемся в новых душах, постигших истину и вступивших в Круг Братства.
-	AI_Output(self,other,"DIA_BaalParvez_SpecialJointOk_01_11");	//Если ты убедишь одного из неверных присоединиться к нашему Лагерю, этим ты докажешь свое стремление служить Братству.
-	AI_Output(other,self,"DIA_BaalParvez_SpecialJointOk_01_12");	//Где мне найти такого человека?
-	AI_Output(self,other,"DIA_BaalParvez_SpecialJointOk_01_13");	//В городе много людей, которые устали от страха и лжи. Души их жаждут просветления и поддержки.
-	AI_Output(self,other,"DIA_BaalParvez_SpecialJointOk_01_14");	//Думаю, тебе стоит поискать его именно здесь. В других местах ты едва ли добьешься успеха. Там живут настоящие варвары.
-	AI_Output(self,other,"DIA_BaalParvez_SpecialJointOk_01_15");	//Возвращайся ко мне, когда твои поиски подойдут к концу.
+	AI_Output(self,other, " DIA_BaalParvez_SpecialJointOk_01_00 " );	// In the name of the Sleeper! I had a vision!
+	AI_Output(self,other, " DIA_BaalParvez_SpecialJointOk_01_01 " );	// It was incredible! I saw that we met a new brother who was unlike all those who had come before him.
+	AI_Output(self,other, " DIA_BaalParvez_SpecialJointOk_01_02 " );	// He was doing something for us... He had a sword in his hand, and he was descending a wide staircase. This is where the vision ended.
+	AI_Output(self,other, " DIA_BaalParvez_SpecialJointOk_01_03 " );	// He looked a lot like YOU. Who are you? What do you need?
+	AI_Output(other,self, " DIA_BaalParvez_SpecialJointOk_01_04 " );	// I would like to join the Brotherhood and ask for your consent, sir.
+	AI_Output(self,other, " DIA_BaalParvez_SpecialJointOk_01_05 " );	// Hmm...(looks intently) Who sent you to me?
+	AI_Output(other,self, " DIA_BaalParvez_SpecialJointOk_01_06 " );	// Idol Oran sent me. He said that I should get your approval for that.
+	AI_Output(self,other, " DIA_BaalParvez_SpecialJointOk_01_07 " );	// Well, if Idol Oran sent you, it's quite possible.
+	AI_Output(self,other, " DIA_BaalParvez_SpecialJointOk_01_08 " );	// Do me one favor. Prove your loyalty, and then I will give my consent for you to become one of us.
+	AI_Output(other,self, " DIA_BaalParvez_SpecialJointOk_01_09 " );	// How can I prove my loyalty?
+	AI_Output(self,other, " DIA_BaalParvez_SpecialJointOk_01_10 " );	// Most of all, we need new souls who have comprehended the truth and entered the Circle of Brotherhood.
+	AI_Output(self,other, " DIA_BaalParvez_SpecialJointOk_01_11 " );	// If you convince one of the infidels to join our Camp, you will prove your desire to serve the Brotherhood.
+	AI_Output(other,self, " DIA_BaalParvez_SpecialJointOk_01_12 " );	// Where can I find such a person?
+	AI_Output(self,other, " DIA_BaalParvez_SpecialJointOk_01_13 " );	// There are many people in the city who are tired of fear and lies. Their souls yearn for enlightenment and support.
+	AI_Output(self,other, " DIA_BaalParvez_SpecialJointOk_01_14 " );	// I think you should look for it here. Elsewhere you will hardly succeed. Real barbarians live there.
+	AI_Output(self,other, " DIA_BaalParvez_SpecialJointOk_01_15 " );	// Return to me when your search is over.
 	Log_CreateTopic(TOPIC_NOVICECANJOIN,LOG_MISSION);
 	Log_SetTopicStatus(TOPIC_NOVICECANJOIN,LOG_Running);
-	B_LogEntry(TOPIC_NOVICECANJOIN,"Идол Парвез попросил меня привести к нему нового послушника. Это хороший шанс заслужить его расположение.");
+	B_LogEntry( TOPIC_NOVICECANJOIN , " Idol Parvez has asked me to bring a new acolyte to him. This is a good chance to earn his favor. " );
 	MIS_NOVICECANJOIN = LOG_Running;
 };
 
@@ -199,7 +200,7 @@ instance DIA_BAALPARVEZ_VALENTINOJOIN(C_Info)
 	information = dia_baalparvez_valentinojoin_info;
 	permanent = FALSE;
 	important = FALSE;
-	description = "Этот человек ищет твоего расположения, господин!";
+	description = " This person seeks your favor, sir! " ;
 };
 
 
@@ -217,27 +218,27 @@ func void dia_baalparvez_valentinojoin_info()
 {
 	var C_Npc Valentino;
 	Valentino = Hlp_GetNpc(VLK_421_Valentino);
-	Valentino.aivar[AIV_PARTYMEMBER] = FALSE;
+	Valentino.aivar[ AIV_PARTYMEMBER ] = FALSE ;
 	AI_GotoNpc(Valentino,self);
 	AI_TurnToNPC(Valentino,self);
-	AI_Output(other,self,"DIA_BaalParvez_ValentinoJoin_01_00");	//Этот человек ищет твоего расположения, господин!
-	AI_Output(self,other,"DIA_BaalParvez_ValentinoJoin_01_01");	//Кого ты привел ко мне? Достоин ли он?
-	AI_Output(other,self,"DIA_BaalParvez_ValentinoJoin_01_02");	//Он нуждается в духовном наставнике.
-	AI_Output(self,other,"DIA_BaalParvez_ValentinoJoin_01_03");	//Очень хорошо. С этого момента он будет одним из моих учеников.
+	AI_Output(other,self, " DIA_BaalParvez_ValentinoJoin_01_00 " );	// This man seeks your favor, master!
+	AI_Output(self,other, " DIA_BaalParvez_ValentinoJoin_01_01 " );	// Who did you bring to me? Is he worthy?
+	AI_Output(other,self, " DIA_BaalParvez_ValentinoJoin_01_02 " );	// He needs a spiritual mentor.
+	AI_Output(self,other, " DIA_BaalParvez_ValentinoJoin_01_03 " );	// Very good. From now on, he will be one of my students.
 	AI_TurnToNPC(self,Valentino);
 	AI_TurnToNPC(Valentino,self);
-	AI_Output(self,other,"DIA_BaalParvez_ValentinoJoin_01_04");	//Ты будешь приходить ко мне каждый день и слушать то, что я скажу тебе. Твоя душа еще может быть спасена.
+	AI_Output(self,other, " DIA_BaalParvez_ValentinoJoin_01_04 " );	// You will come to me every day and listen to what I have to say to you. Your soul can still be saved.
 	AI_TurnToNPC(self,other);
 	Npc_ExchangeRoutine(VLK_421_Valentino,"JoinPsiCamp");
 	Npc_ExchangeRoutine(gur_8004_parvez,"JoinPsiCamp");
 	MIS_NOVICECANJOIN = LOG_SUCCESS;
 	Log_SetTopicStatus(TOPIC_NOVICECANJOIN,LOG_SUCCESS);
-	B_LogEntry(TOPIC_NOVICECANJOIN,"Валентино теперь является учеником Идола Парвеза. Я выполнил поручение Гуру.");
+	B_LogEntry( TOPIC_NOVICECANJOIN , " Valentino is now a student of Idol Parvez. I have completed Guru's order. " );
 	B_GivePlayerXP(250);
 };
 
 
-instance DIA_BAALPARVEZ_AGREED(C_Info)
+instance DIA_BAALPARVEZ_AGREED (C_Info)
 {
 	npc = gur_8004_parvez;
 	nr = 1;
@@ -245,7 +246,7 @@ instance DIA_BAALPARVEZ_AGREED(C_Info)
 	information = dia_baalparvez_agreed_info;
 	permanent = FALSE;
 	important = FALSE;
-	description = "Господин! Я хочу стать одним из Гуру.";
+	description = " Sir! I want to become one of the Gurus. " ;
 };
 
 
@@ -260,13 +261,13 @@ func int dia_baalparvez_agreed_condition()
 func void dia_baalparvez_agreed_info()
 {
 	B_GivePlayerXP(200);
-	AI_Output(other,self,"DIA_BaalParvez_Agreed_01_00");	//Господин! Я хочу стать одним из Гуру.
-	AI_Output(self,other,"DIA_BaalParvez_Agreed_01_01");	//Хорошо. Ты доказал, что достоин этой чести!
-	AI_Output(self,other,"DIA_BaalParvez_Agreed_01_02");	//Иди к Идолу Орану.
-	AI_Output(self,other,"DIA_BaalParvez_Agreed_01_03");	//Теперь оставь меня. Мне нужно многое обсудить со своим новым учеником.
+	AI_Output(other,self, " DIA_BaalParvez_Agreed_01_00 " );	// Mister! I want to become one of the Gurus.
+	AI_Output(self,other, " DIA_BaalParvez_Agreed_01_01 " );	// Good. You have proven yourself worthy of this honor!
+	AI_Output(self,other, " DIA_BaalParvez_Agreed_01_02 " );	// Go to Idol Oran.
+	AI_Output(self,other, " DIA_BaalParvez_Agreed_01_03 " );	// Now leave me. I have a lot to discuss with my new student.
 	AI_TurnToNPC(self,VLK_421_Valentino);
-	AI_Output(self,other,"DIA_BaalParvez_Agreed_01_04");	//Следуй за мной, мой ученик.
-	B_LogEntry(TOPIC_PSICAMPJOIN,"Идол Парвез согласен на мое принятие в Круг Гуру.");
+	AI_Output(self,other, " DIA_BaalParvez_Agreed_01_04 " );	// Follow me, my student.
+	B_LogEntry( TOPIC_PSICAMPJOIN , " Idol Parvez agrees to my acceptance into the Guru Circle. " );
 	PARVEZAGREED = TRUE;
 	AI_StopProcessInfos(self);
 };
@@ -279,7 +280,7 @@ instance DIA_BAALPARVEZ_RUNEMAGICNOTWORK(C_Info)
 	condition = dia_baalparvez_runemagicnotwork_condition;
 	information = dia_baalparvez_runemagicnotwork_info;
 	permanent = FALSE;
-	description = "Как обстоят дела с вашей магией?";
+	description = " How is your magic doing? " ;
 };
 
 
@@ -294,11 +295,11 @@ func int dia_baalparvez_runemagicnotwork_condition()
 func void dia_baalparvez_runemagicnotwork_info()
 {
 	B_GivePlayerXP(200);
-	AI_Output(other,self,"DIA_BaalParvez_RuneMagicNotWork_01_00");	//Как обстоят дела с вашей магией?
-	AI_Output(self,other,"DIA_BaalParvez_RuneMagicNotWork_01_01");	//Наши магические руны ослабли и более не способны творить заклинания!
-	AI_Output(self,other,"DIA_BaalParvez_RuneMagicNotWork_01_02");	//Мы все крайне удивлены этому обстоятельству, но ничего не можем сделать.
-	AI_Output(self,other,"DIA_BaalParvez_RuneMagicNotWork_01_03");	//Все это довольно странно.
-	B_LogEntry(TOPIC_RUNEMAGICNOTWORK,"Гуру Братства также лишились власти над магией рун!");
+	AI_Output(other,self, " DIA_BaalParvez_RuneMagicNotWork_01_00 " );	// How's your magic doing?
+	AI_Output(self,other, " DIA_BaalParvez_RuneMagicNotWork_01_01 " );	// Our magic runes have weakened and are no longer able to cast spells!
+	AI_Output(self,other, " DIA_BaalParvez_RuneMagicNotWork_01_02 " );	// We are all extremely surprised by this circumstance, but there is nothing we can do.
+	AI_Output(self,other, " DIA_BaalParvez_RuneMagicNotWork_01_03 " );	// This is all pretty weird.
+	B_LogEntry( TOPIC_RUNEMAGICNOTWORK , "The Gurus of the Brotherhood have also lost their power over rune magic! " );
 	GURUMAGERUNESNOT = TRUE;
 };
 
@@ -309,7 +310,7 @@ instance DIA_BaalParvez_PrioratStart(C_Info)
 	condition = DIA_BaalParvez_PrioratStart_condition;
 	information = DIA_BaalParvez_PrioratStart_info;
 	permanent = FALSE;
-	description = "Меня прислал Идол Намиб.";
+	description = "The Namib Idol sent me. " ;
 };
 
 func int DIA_BaalParvez_PrioratStart_condition()
@@ -323,16 +324,16 @@ func int DIA_BaalParvez_PrioratStart_condition()
 func void DIA_BaalParvez_PrioratStart_info()
 {
 	B_GivePlayerXP(150);
-	AI_Output(other,self,"DIA_BaalParvez_PrioratStart_01_00");	//Меня прислал Идол Намиб.
+	AI_Output(other,self, " DIA_BaalParvez_PrioratStart_01_00 " );	// The Namib Idol sent me.
 	AI_Output(self,other,"DIA_BaalParvez_PrioratStart_01_01");	//(вздох)
-	AI_Output(other,self,"DIA_BaalParvez_PrioratStart_01_02");	//Дело касается пропавших послушников Братства. Ты ничего не знаешь об этом?
-	AI_Output(self,other,"DIA_BaalParvez_PrioratStart_01_03");	//Ну, раз ты в курсе происходящего, значит, Намиб тебе действительно доверяет.
-	AI_Output(self,other,"DIA_BaalParvez_PrioratStart_01_04");	//Отвечая на твой вопрос, я могу сказать тебе лишь одно - в городе их нет.
-	AI_Output(other,self,"DIA_BaalParvez_PrioratStart_01_05");	//Ты в этом уверен?
-	AI_Output(self,other,"DIA_BaalParvez_PrioratStart_01_06");	//Абсолютно! Они никогда бы не прошли мимо городской стражи незамеченными. Уж я бы об этом точно знал.
-	AI_Output(self,other,"DIA_BaalParvez_PrioratStart_01_07");	//Однако, если ты мне не веришь, то можешь поискать их сам.
-	AI_Output(other,self,"DIA_BaalParvez_PrioratStart_01_08");	//Хорошо, я передам Идолу Намибу твои слова. Прощай!
+	AI_Output(other,self, " DIA_BaalParvez_PrioratStart_01_02 " );	// The case concerns the missing acolytes of the Brotherhood. You don't know anything about it?
+	AI_Output(self,other, " DIA_BaalParvez_PrioratStart_01_03 " );	// Well, if you know what's going on, then the Namib really trusts you.
+	AI_Output(self,other, " DIA_BaalParvez_PrioratStart_01_04 " );	// In answer to your question, all I can tell you is that there aren't any in the city.
+	AI_Output(other,self, " DIA_BaalParvez_PrioratStart_01_05 " );	// Are you sure about this?
+	AI_Output(self,other, " DIA_BaalParvez_PrioratStart_01_06 " );	// Absolutely! They would never have passed the city guards unnoticed. I would have known about it for sure.
+	AI_Output(self,other, " DIA_BaalParvez_PrioratStart_01_07 " );	// However, if you don't believe me, you can look for them yourself.
+	AI_Output(other,self, " DIA_BaalParvez_PrioratStart_01_08 " );	// Okay, I'll pass on your words to the Namib Idol. Goodbye!
 	AI_Output(self,other,"DIA_BaalParvez_PrioratStart_01_09");	//(вздох)
 	PsiCamp_04_Ok = TRUE;
-	B_LogEntry(TOPIC_PrioratStart,"По словам Идола Парвеза, в городе пропавшие послушники не появлялись.");
+	B_LogEntry(TOPIC_PrioratStart, " According to Idol Parvez, missing novices did not appear in the city. " );
 };
