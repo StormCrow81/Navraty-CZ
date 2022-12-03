8a0feaf6c539019e92ac22c49b83f0db5624f048
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 18:40:37 +0200

    Update DIA_STRF_8124_Addon_Morigan.d

--- RU-Main/Story/Dialoge/DIA_STRF_8124_Addon_Morigan.d
+++ RU-Main/Story/Dialoge/DIA_STRF_8124_Addon_Morigan.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_STRF_8124_Addon_Morigan_EXIT(C_Info)
 {
 	npc = STRF_8124_Addon_Morigan;
@@ -26,7 +27,7 @@ instance DIA_STRF_8124_Addon_Morigan_HELLO(C_Info)
 	condition = DIA_STRF_8124_Addon_Morigan_hello_condition;
 	information = DIA_STRF_8124_Addon_Morigan_hello_info;
 	permanent = FALSE;
-	description = "Я могу чем-то помочь?";
+	description = " Is there something I can help you with? " ;
 };
 
 func int DIA_STRF_8124_Addon_Morigan_hello_condition()
@@ -36,9 +37,9 @@ func int DIA_STRF_8124_Addon_Morigan_hello_condition()
 
 func void DIA_STRF_8124_Addon_Morigan_hello_info()
 {
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_Hello_15_00");	//Я могу чем-то помочь?
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Hello_08_00");	//Да...(ворчливо) Например, оставить меня в покое.
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Hello_08_01");	//Если орки заметят, что я тут с тобой болтаю, мне придется несладко, приятель.
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_Hello_15_00 " );	// Is there anything I can do to help?
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Hello_08_00 " );	// Yes...(grouchily) For example, leave me alone.
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Hello_08_01 " );	// If the orcs notice that I'm talking to you here, I'm going to have a hard time, buddy.
 	AI_StopProcessInfos(self);
 };
 
@@ -49,7 +50,7 @@ instance DIA_STRF_8124_Addon_Morigan_Help(C_Info)
 	condition = DIA_STRF_8124_Addon_Morigan_Help_condition;
 	information = DIA_STRF_8124_Addon_Morigan_Help_info;
 	permanent = FALSE;
-	description = "А если серьезно?";
+	description = " Seriously? " ;
 };
 
 func int DIA_STRF_8124_Addon_Morigan_Help_condition()
@@ -62,10 +63,10 @@ func int DIA_STRF_8124_Addon_Morigan_Help_condition()
 
 func void DIA_STRF_8124_Addon_Morigan_Help_info()
 {
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_Help_01_00");	//А если серьезно?
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Help_01_01");	//Послушай...(нервно) Мне не нужна твоя помощь!
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Help_01_02");	//Но если действительно хочешь сделать доброе дело, принеси мне чего-нибудь попить.
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Help_01_03");	//А то у меня во рту все так пересохло, что даже сглотнуть больно.
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_Help_01_00 " );	// But seriously?
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Help_01_01 " );	// Look...(nervously) I don't need your help!
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Help_01_02 " );	// But if you really want to do a good deed, bring me something to drink.
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Help_01_03 " );	// Otherwise, everything in my mouth is so dry that it hurts even to swallow.
 };
 
 instance DIA_STRF_8124_Addon_Morigan_Water(C_Info)
@@ -75,12 +76,12 @@ instance DIA_STRF_8124_Addon_Morigan_Water(C_Info)
 	condition = DIA_STRF_8124_Addon_Morigan_Water_condition;
 	information = DIA_STRF_8124_Addon_Morigan_Water_info;
 	permanent = FALSE;
-	description = "Вот, хлебни воды.";
+	description = " Here, take a sip of water. " ;
 };
 
 func int DIA_STRF_8124_Addon_Morigan_Water_condition()
 {
-	if((Npc_KnowsInfo(hero,DIA_STRF_8124_Addon_Morigan_Help) == TRUE) && (Npc_HasItems(other,ItFo_Water) >= 1))
+	if (( Npc_KnowsInfo ( hero , DIA_STRF_8124_Addon_Morigan_Help ) ==  TRUE ) && ( Npc_HasItems ( other , ItFo_Water ) >=  1 )) ;
 	{
 		return TRUE;
 	};
@@ -89,10 +90,10 @@ func int DIA_STRF_8124_Addon_Morigan_Water_condition()
 func void DIA_STRF_8124_Addon_Morigan_Water_info()
 {
 	B_GivePlayerXP(100);
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_Water_01_00");	//Вот, хлебни воды.
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_Water_01_00 " );	// Here, take a sip of water.
 	B_GiveInvItems(other,self,ItFo_Water,1);
 	B_UseItem(self,ItFo_Water);
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Water_01_01");	//Так гораздо лучше.
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Water_01_01 " );	// Much better this way.
 };
 
 instance DIA_STRF_8124_Addon_Morigan_Father(C_Info)
@@ -102,7 +103,7 @@ instance DIA_STRF_8124_Addon_Morigan_Father(C_Info)
 	condition = DIA_STRF_8124_Addon_Morigan_Father_condition;
 	information = DIA_STRF_8124_Addon_Morigan_Father_info;
 	permanent = FALSE;
-	description = "Может, что-нибудь еще?";
+	description = " Maybe something else? " ;
 };
 
 func int DIA_STRF_8124_Addon_Morigan_Father_condition()
@@ -115,31 +116,31 @@ func int DIA_STRF_8124_Addon_Morigan_Father_condition()
 
 func void DIA_STRF_8124_Addon_Morigan_Father_info()
 {
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_Father_01_00");	//Может, что-нибудь еще?
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Father_01_01");	//Хммм...(задумчиво) Даже не знаю, стоит ли тебя просить об этом.
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_Father_01_02");	//А ты попробуй.
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Father_01_03");	//Ладно. В конце концов, кроме тебя мне здесь вряд ли кто-нибудь еще поможет.
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Father_01_04");	//Скажи, ты часто бываешь на поверхности?
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_Father_01_00 " );	// Maybe something else?
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Father_01_01 " );	// Hmmm...(thoughtfully) I don't even know if I should ask you this.
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_Father_01_02 " );	// And you try.
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Father_01_03 " );	// Okay. In the end, except for you, it is unlikely that anyone else will help me here.
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Father_01_04 " );	// Tell me, do you often go to the surface?
 	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_Father_01_05");	//Конечно.
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Father_01_06");	//И какова там ситуация?
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_Father_01_07");	//Не намного лучше, чем здесь! Орки почти полностью контролируют остров.
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_Father_01_08");	//Единственное, куда они еще не посмели сунуться, это сам Хоринис.
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_Father_01_09");	//Но вряд ли это случится в ближайшее время, поскольку там расположился большой отряд паладинов.
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Father_01_10");	//Что же, это не может не радовать...
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_Father_01_11");	//А почему ты, собственно, интересуешься?
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Father_01_12");	//Дело в том, что как раз в Хоринисе живет мой отец.
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Father_01_13");	//Если бы ты только смог передать ему весточку о том, что я жив.
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_Father_01_14");	//И кто твой отец?
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Father_01_15");	//Его зовут Карл, он кузнец. Если ты бывал в городе, то наверняка должен его знать.
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_Father_01_16");	//Да, я знаю его. Но захочет ли он услышать такие новости?
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Father_01_17");	//Я понимаю, что мое теперешнее положение не из лучших. 
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Father_01_18");	//Но я все-таки до сих пор жив, и он должен знать об этом!
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_Father_01_19");	//Хорошо, я передам ему это.
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_Father_01_20");	//Благодарю тебя, приятель! Большего мне и не надо.
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Father_01_06 " );	// And what's the situation there?
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_Father_01_07 " );	// Not much better than here! Orcs almost completely control the island.
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_Father_01_08 " );	// The only thing they haven't dared to poke their nose into is Khorinis himself.
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_Father_01_09 " );	// But it's unlikely to happen anytime soon, as there's a large band of paladins stationed there.
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Father_01_10 " );	// Well, this is good news...
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_Father_01_11 " );	// And why are you actually interested?
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Father_01_12 " );	// The fact is that my father lives in Khorinis.
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Father_01_13 " );	// If only you could get word to him that I'm alive.
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_Father_01_14 " );	// And who is your father?
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Father_01_15 " );	// His name is Karl, he is a blacksmith. If you've been to the city, you must know it for sure.
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_Father_01_16 " );	// Yes, I know him. But would he want to hear such news?
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Father_01_17 " );	// I understand that my current situation is not the best.
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Father_01_18 " );	// But I'm still alive, and he should know about it!
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_Father_01_19 " );	// Okay, I'll pass this on to him.
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_Father_01_20 " );	// Thank you, mate! I don't need more.
 	MIS_FatherNews = LOG_Running;
 	Log_CreateTopic(TOPIC_FatherNews,LOG_MISSION);
 	Log_SetTopicStatus(TOPIC_FatherNews,LOG_Running);
-	B_LogEntry(TOPIC_FatherNews,"Я пообещал Моригану навестить его отца в Хоринисе и передать ему, что его сын все еще жив. Его отца зовут Карл.");
+	B_LogEntry(TOPIC_FatherNews, " I promised Morigan to visit his father in Khorinis and tell him that his son is still alive. His father's name is Carl. " );
 };
 
 instance DIA_STRF_8124_Addon_Morigan_FatherDone(C_Info)
@@ -149,7 +150,7 @@ instance DIA_STRF_8124_Addon_Morigan_FatherDone(C_Info)
 	condition = DIA_STRF_8124_Addon_Morigan_FatherDone_condition;
 	information = DIA_STRF_8124_Addon_Morigan_FatherDone_info;
 	permanent = FALSE;
-	description = "Я поговорил с твоим отцом.";
+	description = " I talked to your father. " ;
 };
 
 func int DIA_STRF_8124_Addon_Morigan_FatherDone_condition()
@@ -163,17 +164,17 @@ func int DIA_STRF_8124_Addon_Morigan_FatherDone_condition()
 func void DIA_STRF_8124_Addon_Morigan_FatherDone_info()
 {
 	B_GivePlayerXP(150);
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_FatherDone_01_00");	//Я поговорил с твоим отцом.
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_FatherDone_01_01");	//(взволнованно) Ты меня не обманываешь?
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_FatherDone_01_02");	//Нет, конечно! Мне нет смысла этого делать.
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_FatherDone_01_03");	//И что он сказал?
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_FatherDone_01_04");	//Твой отец был рад услышать о том, что ты до сих пор жив.
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_FatherDone_01_05");	//И он будет молиться за тебя Инносу.
-	AI_Output(other,self,"DIA_STRF_8124_Addon_Morigan_FatherDone_01_06");	//В надежде, что когда-нибудь ты все-таки сможешь вернуться домой.
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_FatherDone_01_07");	//Эх...(печально) Я был бы и сам рад в это поверить. Да только отсюда не сбежишь.
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_FatherDone_01_08");	//Но в любом случае я благодарен тебе за твою помощь.
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_FatherDone_01_09");	//Вот, возьми этот кусок руды в знак моей признательности.
-	B_GiveInvItems(self,other,ItMi_Zeitspalt_Addon,1);
-	AI_Output(self,other,"DIA_STRF_8124_Addon_Morigan_FatherDone_01_10");	//К сожалению, больше мне дать тебе нечего.
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_FatherDone_01_00 " );	// I talked to your father.
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_FatherDone_01_01 " );	// (excitedly) Are you kidding me?
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_FatherDone_01_02 " );	// No, of course not! It makes no sense for me to do this.
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_FatherDone_01_03 " );	// And what did he say?
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_FatherDone_01_04 " );	// Your father was glad to hear that you are still alive.
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_FatherDone_01_05 " );	// And he will pray for you to Innos.
+	AI_Output(other,self, " DIA_STRF_8124_Addon_Morigan_FatherDone_01_06 " );	// In the hope that someday you can still return home.
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_FatherDone_01_07 " );	// Eh...(sadly) I'd be happy to believe it myself. Yes, you can't run away from here.
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_FatherDone_01_08 " );	// But anyway, I'm grateful for your help.
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_FatherDone_01_09 " );	// Here, take this piece of ore as a token of my appreciation.
+	B_GiveInvItems(self,other,ItMi_Zeitspalt_Addon, 1 );
+	AI_Output(self,other, " DIA_STRF_8124_Addon_Morigan_FatherDone_01_10 " );	// Unfortunately, I have nothing more to give you.
 	RT_Respect = RT_Respect + 1;
 };
