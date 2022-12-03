c435574753275ab0727a776c536a7b57cc295ed0
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 23:06:01 +0200

    Update DIA_dmt_1216_demos.d

--- RU-Main/Story/Dialoge/DIA_dmt_1216_demos.d
+++ RU-Main/Story/Dialoge/DIA_dmt_1216_demos.d
@@ -1,4 +1,5 @@
 
+
 instance DMT_1216_DEMOS_EXIT(C_Info)
 {
 	npc = dmt_1216_demos;
@@ -34,7 +35,7 @@ instance DMT_1216_DEMOS_HELLO(C_Info)
 
 func int dmt_1216_demos_hello_condition()
 {
-	if((MIS_GUARDIANSTEST == LOG_Running) && (KELIOS_AGREE == TRUE))
+	if (( MY_GUARDIANTEST  == LOG_Running ) && ( KELIOS_AGREE  ==  TRUE ))
 	{
 		return TRUE;
 	};
@@ -42,13 +43,13 @@ func int dmt_1216_demos_hello_condition()
 
 func void dmt_1216_demos_hello_info()
 {
-	AI_Output(self,other,"DMT_1216_Demos_Hello_00");	//(вкрадчиво) Приветствую тебя, путник. Вот и настало время нам с тобой встретиться.
+	AI_Output(self,other, " DMT_1216_Demos_Hello_00 " );	// (smartly) Greetings, traveler. Now it's time for us to meet.
 	AI_Output(other,self,"DMT_1216_Demos_Hello_01");	//Кто ты?
-	AI_Output(self,other,"DMT_1216_Demos_Hello_02");	//Мое имя Демос, и я второй страж своего создателя Инноса и Хранитель священного огня Баала.
-	AI_Output(self,other,"DMT_1216_Demos_Hello_03");	//Думаю, ты догадываешься, почему я здесь?
-	AI_Output(other,self,"DMT_1216_Demos_Hello_04");	//Да, мастер. Я должен пройти твое испытание и получить твое согласие на принятие меня в адепты Круга Хранителей.
-	AI_Output(self,other,"DMT_1216_Demos_Hello_05");	//Все верно...(могущественно) А раз так, то не будем отвлекаться на пустую болтовню.
-	AI_Output(self,other,"DMT_1216_Demos_Hello_06");	//Ты готов принять его?
+	AI_Output(self,other, " DMT_1216_Demos_Hello_02 " );	// My name is Demos, and I am the second guardian of my creator Innos and the Keeper of the sacred fire of Baal.
+	AI_Output(self,other, " DMT_1216_Demos_Hello_03 " );	// I think you can guess why I'm here?
+	AI_Output(other,self, " DMT_1216_Demos_Hello_04 " );	// Yes, master. I must pass your test and get your consent to accept me as an adherent of the Circle of Guardians.
+	AI_Output(self,other, " DMT_1216_Demos_Hello_05 " );	// That's right... (powerfully) And if so, then let's not be distracted by empty chatter.
+	AI_Output(self,other, " DMT_1216_Demos_Hello_06 " );	// Are you ready to accept it?
 	Info_ClearChoices(dmt_1216_demos_hello);
 	Info_AddChoice(dmt_1216_demos_hello,"Я готов.",dmt_1216_demos_hello_test);
 };
@@ -60,33 +61,33 @@ func void dmt_1216_demos_hello_test()
 	Wld_PlayEffect("FX_EarthQuake",self,self,0,0,0,FALSE);
 	Wld_PlayEffect("SFX_Circle",self,self,0,0,0,FALSE);
 	AI_PlayAni(self,"T_PRACTICEMAGIC5");
-	AI_Output(self,other,"DMT_1216_Demos_Hello_08");	//Хорошо. Слушай меня внимательно.
-	AI_Output(self,other,"DMT_1216_Demos_Hello_09");	//Чтобы пройти свое следующее испытание, тебе придется отправиться в место, именуемое Гадесом!
+	AI_Output(self,other, " DMT_1216_Demos_Hello_08 " );	// Good. Listen to me carefully.
+	AI_Output(self,other, " DMT_1216_Demos_Hello_09 " );	// To pass your next test, you'll have to travel to a place called Hades!
 	AI_Output(other,self,"DMT_1216_Demos_Hello_10");	//Гадес?
-	AI_Output(self,other,"DMT_1216_Demos_Hello_11");	//Это имя ему дали древние в те времена, когда человечество еще только начинало свой нелегкий путь в этом мире.
-	AI_Output(self,other,"DMT_1216_Demos_Hello_12");	//Но чтобы тебе было более понятно, я скажу проще: вы - люди - когда-то нарекли это некогда прекрасное место Долиной Рудников!
-	AI_Output(self,other,"DMT_1216_Demos_Hello_13");	//Думаю, это название тебе уже хорошо знакомо.
-	AI_Output(other,self,"DMT_1216_Demos_Hello_14");	//Конечно, я был в ней заключенным.
-	AI_Output(self,other,"DMT_1216_Demos_Hello_15");	//Сейчас мы не будем вдаваться в подробности. Тебя сейчас должно волновать другое.
+	AI_Output(self,other, " DMT_1216_Demos_Hello_11 " );	// This name was given to him by the ancients in those times when humanity was just beginning its difficult path in this world.
+	AI_Output(self,other, " DMT_1216_Demos_Hello_12 " );	// But to make it clearer to you, I'll put it simply: you - the people - once called this once beautiful place the Valley of Mines!
+	AI_Output(self,other, " DMT_1216_Demos_Hello_13 " );	// I think this name is already familiar to you.
+	AI_Output(other,self, " DMT_1216_Demos_Hello_14 " );	// Of course, I was a prisoner in it.
+	AI_Output(self,other, " DMT_1216_Demos_Hello_15 " );	// We won't go into details for now. You should be worried about something else now.
 	AI_Output(other,self,"DMT_1216_Demos_Hello_16");	//И что же?
-	AI_Output(self,other,"DMT_1216_Demos_Hello_17");	//Там, в долине, некогда был сокрыт один из артефактов древности, обладающий невероятной магической силой!
-	AI_Output(other,self,"DMT_1216_Demos_Hello_18");	//Что это был за предмет?
-	AI_Output(self,other,"DMT_1216_Demos_Hello_19");	//У него нет какого-то определенного названия, но его сущность олицетворяет противоположность моего создателя.
-	AI_Output(self,other,"DMT_1216_Demos_Hello_20");	//Думаю, тебе не составит труда понять, о чем именно идет речь.
-	AI_Output(self,other,"DMT_1216_Demos_Hello_21");	//В свое время этим артефактом владели орки, которые на протяжении тысячелетий были хозяевами этих земель.
-	AI_Output(self,other,"DMT_1216_Demos_Hello_22");	//Пока, естественно, сюда не пришли вы - люди!
-	AI_Output(self,other,"DMT_1216_Demos_Hello_23");	//Опасаясь, что этот предмет будет навсегда ими утерян, орки сокрыли его в одном из своих священных мест.
-	AI_Output(self,other,"DMT_1216_Demos_Hello_24");	//Где именно, я сказать не могу.
-	AI_Output(self,other,"DMT_1216_Demos_Hello_25");	//Но уверен, что с твоей помощью мне удастся это все-таки узнать.
-	AI_Output(other,self,"DMT_1216_Demos_Hello_26");	//Ты хочешь, чтобы я отыскал его для тебя?
-	AI_Output(self,other,"DMT_1216_Demos_Hello_27");	//Совершенно верно. Принеси мне этот артефакт, и ты получишь мое согласие!
-	AI_Output(other,self,"DMT_1216_Demos_Hello_28");	//М-да, задачка, кажется, не из простых.
-	AI_Output(self,other,"DMT_1216_Demos_Hello_32");	//Хорошо...(вкрадчиво) Тогда ступай, и пусть священный огонь поможет тебе в твоих поисках.
+	AI_Output(self,other, " DMT_1216_Demos_Hello_17 " );	// There, in the valley, one of the artifacts of antiquity was once hidden, possessing incredible magical power!
+	AI_Output(other,self, " DMT_1216_Demos_Hello_18 " );	// What was the item?
+	AI_Output(self,other, " DMT_1216_Demos_Hello_19 " );	// It has no specific name, but its essence represents the opposite of my creator.
+	AI_Output(self,other, " DMT_1216_Demos_Hello_20 " );	// I think it will not be difficult for you to understand what exactly we are talking about.
+	AI_Output(self,other, " DMT_1216_Demos_Hello_21 " );	// At one time, this artifact was owned by the orcs, who for millennia were the masters of these lands.
+	AI_Output(self,other, " DMT_1216_Demos_Hello_22 " );	// Until, of course, you people came here!
+	AI_Output(self,other, " DMT_1216_Demos_Hello_23 " );	// Fearing that this item would be lost forever, the orcs hid it in one of their sacred places.
+	AI_Output(self,other, " DMT_1216_Demos_Hello_24 " );	// Where exactly, I can't say.
+	AI_Output(self,other, " DMT_1216_Demos_Hello_25 " );	// But I'm sure that with your help, I'll still be able to find out.
+	AI_Output(other,self, " DMT_1216_Demos_Hello_26 " );	// Do you want me to find it for you?
+	AI_Output(self,other, " DMT_1216_Demos_Hello_27 " );	// Quite right. Bring me this artifact and you will have my consent!
+	AI_Output(other, self, " DMT_1216_Demos_Hello_28 " );	// M-yes, the task seems to be not an easy one.
+	AI_Output(self,other, " DMT_1216_Demos_Hello_32 " );	// Okay...(smoothly) Then go and let the sacred fire help you in your quest.
 	AI_StopProcessInfos(self);
 	MIS_DEMOSTEST = LOG_Running;
 	Log_CreateTopic(TOPIC_DEMOSTEST,LOG_MISSION);
 	Log_SetTopicStatus(TOPIC_DEMOSTEST,LOG_Running);
-	B_LogEntry(TOPIC_DEMOSTEST,"Демос, Хранитель огненной стихии, дал мне очередное испытание. Я должен найти артефакт, сокрытый в Долине Рудников. Предмет моих поисков по своей сути противоположен творцу стражей Огня. Орки - его последние хозяева - спрятали артефакт в одном из своих священных мест.");
+	B_LogEntry( TOPIC_DEMOSTEST , " Demos, Guardian of the Fire Element, has given me another test. I must find an artifact hidden in the Valley of Mines. The subject of my search is essentially the opposite of the creator of the guards of Fire. Orcs - his last masters - hid the artifact in one of their sacred local " );
 };
 
 instance DMT_1216_DEMOS_TEST(C_Info)
@@ -96,13 +97,13 @@ instance DMT_1216_DEMOS_TEST(C_Info)
 	condition = dmt_1216_demos_test_condition;
 	information = dmt_1216_demos_test_info;
 	permanent = FALSE;
-	description = "Я, кажется, нашел нужный тебе артефакт.";
+	description = " I think I found the artifact you need. " ;
 };
 
 
 func int dmt_1216_demos_test_condition()
 {
-	if((MIS_GUARDIANSTEST == LOG_Running) && (MIS_DEMOSTEST == LOG_Running) && (Npc_HasItems(other,itmi_idol_02) >= 1))
+	if (( MY_GUARDIANTEST  == LOG_Running) && ( MY_DEMOTEST  == LOG_Running) && (Npc_HasItems(other,itmi_idol_02) >=  1 )) ;
 	{
 		return TRUE;
 	};
@@ -111,67 +112,67 @@ func int dmt_1216_demos_test_condition()
 func void dmt_1216_demos_test_info()
 {
 	B_GivePlayerXP(200);
-	AI_Output(other,self,"DMT_1216_Demos_Test_00");	//Я, кажется, нашел нужный тебе артефакт.
-	AI_Output(self,other,"DMT_1216_Demos_Test_01");	//Если ты так уверен в этом, то просто дай мне на него взглянуть.
-	AI_Output(other,self,"DMT_1216_Demos_Test_02");	//Вот он, мастер.
+	AI_Output(other,self, " DMT_1216_Demos_Test_00 " );	// I think I found the artifact you need.
+	AI_Output(self,other, " DMT_1216_Demos_Test_01 " );	// If you're so sure about this, then just let me take a look at it.
+	AI_Output(other,self, " DMT_1216_Demos_Test_02 " );	// Here he is, master.
 	B_GiveInvItems(other,self,itmi_idol_02,1);
 	Npc_RemoveInvItems(self,itmi_idol_02,1);
-	AI_Output(self,other,"DMT_1216_Demos_Test_03");	//(внимательно вглядываясь) Да, это действительно он.
-	AI_Output(self,other,"DMT_1216_Demos_Test_04");	//Облик Тьмы - третья сущность темного бога...(могущественно) И теперь благодаря тебе она в надежных руках Хранителей.
-	AI_Output(other,self,"DMT_1216_Demos_Test_05");	//Третья сущность? Что это значит?
-	AI_Output(self,other,"DMT_1216_Demos_Test_08");	//Хммм...(вкрадчиво) Если хочешь, то я могу поведать тебе о них.
-	AI_Output(self,other,"DMT_1216_Demos_Test_09");	//В конце концов, ты заслужил право знать, ради чего рисковал своей жизнью.
-	AI_Output(other,self,"DMT_1216_Demos_Test_10");	//У меня как раз есть время выслушать твой рассказ.
-	AI_Output(self,other,"DMT_1216_Demos_Test_11");	//Хорошо. Ответь, ты когда-нибудь слышал про священные Залы Ирдората?
+	AI_Output(self,other, " DMT_1216_Demos_Test_03 " );	// (looking closely) Yes, it really is him.
+	AI_Output(self,other, " DMT_1216_Demos_Test_04 " );	// Shape of Darkness - the third essence of the dark god... (powerfully) And now, thanks to you, she is in the safe hands of the Guardians.
+	AI_Output(other,self, " DMT_1216_Demos_Test_05 " );	// Third entity? What does it mean?
+	AI_Output(self,other, " DMT_1216_Demos_Test_08 " );	// Hmmm...(smoothly) If you want, I can tell you about them.
+	AI_Output(self,other, " DMT_1216_Demos_Test_09 " );	// After all, you have earned the right to know why you risked your life.
+	AI_Output(other,self, " DMT_1216_Demos_Test_10 " );	// I just have time to listen to your story.
+	AI_Output(self,other, " DMT_1216_Demos_Test_11 " );	// Good. Tell me, have you ever heard of the sacred Halls of Irdorath?
 	if(ItWr_SCReadsHallsofIrdorath == TRUE)
 	{
-		AI_Output(other,self,"DMT_1216_Demos_Test_12");	//Да, я о них слышал.
-		AI_Output(self,other,"DMT_1216_Demos_Test_13");	//Тогда ты должен знать, что эти залы есть не что иное, как чертоги самого Зверя!
-		AI_Output(self,other,"DMT_1216_Demos_Test_14");	//И, соответственно, место заклинания самого темного бога...
+		AI_Output(other,self, " DMT_1216_Demos_Test_12 " );	// Yes, I heard about them.
+		AI_Output(self,other, " DMT_1216_Demos_Test_13 " );	// Then you should know that these halls are nothing but the halls of the Beast itself!
+		AI_Output(self,other, " DMT_1216_Demos_Test_14 " );	// And, accordingly, the place of the spell of the darkest god...
 	}
 	else
 	{
-		AI_Output(other,self,"DMT_1216_Demos_Test_15");	//Нет. Первый раз слышу о них.
-		AI_Output(self,other,"DMT_1216_Demos_Test_16");	//(могущественно) Тогда знай же, что Залы Ирдората есть не что иное, как чертоги самого Зверя!
-		AI_Output(self,other,"DMT_1216_Demos_Test_17");	//И, соответственно, место заклинания самого темного бога...
+		AI_Output(other,self, " DMT_1216_Demos_Test_15 " );	// No. First time I hear about them.
+		AI_Output(self,other, " DMT_1216_Demos_Test_16 " );	// (mighty) Then know that the Halls of Irdorath are none other than the halls of the Beast itself!
+		AI_Output(self,other, " DMT_1216_Demos_Test_17 " );	// And, accordingly, the place of the spell of the darkest god...
 	};
-	AI_Output(self,other,"DMT_1216_Demos_Test_18");	//Всего существует четыре подобных храма.
-	AI_Output(self,other,"DMT_1216_Demos_Test_19");	//Точнее, их было четыре, пока паладины Инноса не разрушили первые два.
-	AI_Output(self,other,"DMT_1216_Demos_Test_20");	//В свою очередь каждое из этих мест черпает свою силу в древних артефактах, в коих сам Белиар заключил часть своей божественной силы.
-	AI_Output(self,other,"DMT_1216_Demos_Test_21");	//Эта сила и являет сущность повелителя багровой Тьмы!
-	AI_Output(other,self,"DMT_1216_Demos_Test_22");	//Понятно. Значит, всего должно существовать четыре подобных артефакта?
-	AI_Output(self,other,"DMT_1216_Demos_Test_23");	//Так оно и есть. Их всего четыре. И один из них ты уже видел.
-	AI_Output(other,self,"DMT_1216_Demos_Test_24");	//А где можно найти остальные три?
-	AI_Output(self,other,"DMT_1216_Demos_Test_25");	//(ухмыляется) Думаю, тебе не стоит уж слишком сильно интересоваться всем этим.
-	AI_Output(other,self,"DMT_1216_Demos_Test_26");	//Но почему?
-	AI_Output(self,other,"DMT_1216_Demos_Test_27");	//Даже если ты найдешь их, то все равно не сможешь овладеть той силой, что сокрыта в этих предметах.
-	AI_Output(self,other,"DMT_1216_Demos_Test_28");	//Это и Хранителям-то не под силу, не говоря уже о простых смертных!
-	AI_Output(self,other,"DMT_1216_Demos_Test_29");	//Так что лучше оставь свои мысли об этом. Ты только зря потратишь свое время.
-	AI_Output(other,self,"DMT_1216_Demos_Test_30");	//Хорошо, как скажешь.
-	AI_Output(self,other,"DMT_1216_Demos_Test_31");	//(серьезно) Теперь давай вернемся к твоему испытанию.
-	AI_Output(self,other,"DMT_1216_Demos_Test_32");	//По моему мнению, ты его успешно прошел и, безусловно, заслужил мое согласие на принятие тебя в адепты нашего Круга!
+	AI_Output(self,other, " DMT_1216_Demos_Test_18 " );	// There are four such temples in total.
+	AI_Output(self,other, " DMT_1216_Demos_Test_19 " );	// More precisely, there were four of them, until the paladins of Innos destroyed the first two.
+	AI_Output(self,other, " DMT_1216_Demos_Test_20 " );	// In turn, each of these places draws its power from ancient artifacts, in which Beliar himself enclosed part of his divine power.
+	AI_Output(self,other, " DMT_1216_Demos_Test_21 " );	// This power is the essence of the lord of the crimson Darkness!
+	AI_Output(other,self, " DMT_1216_Demos_Test_22 " );	// Got it. So there must be four such artifacts in total?
+	AI_Output(self,other, " DMT_1216_Demos_Test_23 " );	// So it is. There are only four of them. And you've already seen one of them.
+	AI_Output(other,self, " DMT_1216_Demos_Test_24 " );	// Where can I find the other three?
+	AI_Output(self,other, " DMT_1216_Demos_Test_25 " );	// (grins) I don't think you should be too interested in all this.
+	AI_Output(other,self, " DMT_1216_Demos_Test_26 " );	// But why?
+	AI_Output(self,other, " DMT_1216_Demos_Test_27 " );	// Even if you find them, you still won't be able to master the power hidden in these items.
+	AI_Output(self,other, " DMT_1216_Demos_Test_28 " );	// This is beyond the power of the Guardians, not to mention mere mortals!
+	AI_Output(self,other, " DMT_1216_Demos_Test_29 " );	// So you better leave your thoughts on it. You're just wasting your time.
+	AI_Output(other,self, " DMT_1216_Demos_Test_30 " );	// Okay, whatever you say.
+	AI_Output(self,other, " DMT_1216_Demos_Test_31 " );	// (seriously) Now let's get back to your challenge.
+	AI_Output(self,other, " DMT_1216_Demos_Test_32 " );	// In my opinion, you successfully passed it and, of course, deserved my consent to accept you as an adherent of our Circle!
 	KNOWABOUTIRDORAT = TRUE;
 	MIS_DEMOSTEST = LOG_SUCCESS;
 	Log_SetTopicStatus(TOPIC_DEMOSTEST,LOG_SUCCESS);
-	B_LogEntry(TOPIC_DEMOSTEST,"Я принес Демосу артефакт, о котором он говорил, и он признал мое испытание пройденным.");
+	B_LogEntry( TOPIC_DEMOSTEST , " I brought Demos the artifact he was talking about, and he passed my test. " );
 	Info_ClearChoices(dmt_1216_demos_test);
 	Info_AddChoice(dmt_1216_demos_test,"Да, мастер.",dmt_1216_demos_test_pass);
 };
 
 func void dmt_1216_demos_test_pass()
 {
-	AI_Output(other,self,"DMT_1216_Demos_Test_33");	//Да, мастер.
+	AI_Output(other,self, " DMT_1216_Demos_Test_33 " );	// Yes, master.
 	Wld_PlayEffect("spellFX_INCOVATION_RED",self,self,0,0,0,FALSE);
 	Wld_PlayEffect("FX_EarthQuake",self,self,0,0,0,FALSE);
 	Wld_PlayEffect("SFX_Circle",self,self,0,0,0,FALSE);
 	AI_PlayAni(self,"T_PRACTICEMAGIC5");
-	AI_Output(self,other,"DMT_1216_Demos_Test_34");	//И властью, данной мне творцом, я вписываю свое решение в Книгу судеб...(могущественно) Да будет так!
-	AI_Output(self,other,"DMT_1216_Demos_Test_35");	//Теперь продолжай свой путь. Ищи Хранителя Фариона - третьего из нас, стражей Огня. Он даст тебе следующее испытание.
+	AI_Output(self,other, " DMT_1216_Demos_Test_34 " );	// And with the power given to me by the creator, I write my decision in the Book of Fates... (mightily) So be it!
+	AI_Output(self,other, " DMT_1216_Demos_Test_35 " );	// Now continue on your way. Look for Guardian Farion - the third of us, guardians of the Fire. He will give you the next test.
 	AI_Output(self,other,"DMT_1216_Demos_Test_36");	//Прощай!
-	B_LogEntry(TOPIC_GUARDIANSTEST,"Хранитель Демос дал свое согласие на принятие меня в адепты. Теперь мне необходимо найти Хранителя Фариона - последнего из стражей Огня. Он даст мне следующее испытание.");
+	B_LogEntry( TOPIC_GUARDIANSTEST , " Guardian Demos has agreed to accept me as an adept. Now I need to find Guardian Farion - the last of the guardians of Fire. He will give me the next test. " );
 	DEMOS_AGREE = TRUE;
 	Info_ClearChoices(dmt_1216_demos_test);
-	Info_AddChoice(dmt_1216_demos_test,"(закончить разговор)",dmt_1216_demos_test_end);
+	Info_AddChoice(dmt_1216_demos_test, " (end call) " ,dmt_1216_demos_test_end);
 };
 
 func void dmt_1216_demos_test_end()
