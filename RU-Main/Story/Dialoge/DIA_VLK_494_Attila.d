2cf3e7e20ace44fa703cffaf7f07ffacd71444fd
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 23:19:31 +0200

    Update DIA_VLK_494_Attila.d

--- RU-Main/Story/Dialoge/DIA_VLK_494_Attila.d
+++ RU-Main/Story/Dialoge/DIA_VLK_494_Attila.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_Attila_EXIT(C_Info)
 {
 	npc = VLK_494_Attila;
@@ -34,7 +35,7 @@ instance DIA_Attila_PICKPOCKET(C_Info)
 
 func int DIA_Attila_PICKPOCKET_Condition()
 {
-	return C_Beklauen(55,100);
+	return  C_Robbery ( 55 , 100 );
 };
 
 func void DIA_Attila_PICKPOCKET_Info()
@@ -46,7 +47,7 @@ func void DIA_Attila_PICKPOCKET_Info()
 
 func void DIA_Attila_PICKPOCKET_DoIt()
 {
-	B_Beklauen();
+	B_Robbery();
 	Info_ClearChoices(DIA_Attila_PICKPOCKET);
 };
 
@@ -56,7 +57,7 @@ func void DIA_Attila_PICKPOCKET_BACK()
 };
 
 
-instance DIA_Attila_Hallo(C_Info)
+instance DIA_Attila_Hallo (C_Info)
 {
 	npc = VLK_494_Attila;
 	nr = 1;
@@ -77,129 +78,129 @@ func int DIA_Attila_Hallo_Condition()
 
 func void DIA_Attila_Hallo_Info()
 {
-	AI_Output(self,other,"DIA_Attila_Hallo_09_00");	//Ах, наконец. Я ждал тебя, чужеземец.
+	AI_Output(self,other, " DIA_Attila_Hallo_09_00 " );	// Ah, finally. I've been waiting for you, stranger.
 	Info_ClearChoices(DIA_Attila_Hallo);
-	Info_AddChoice(DIA_Attila_Hallo,"Что тебе нужно от меня?",DIA_Attila_Hallo_Was);
-	Info_AddChoice(DIA_Attila_Hallo,"Кто ты?",DIA_Attila_Hallo_Wer);
+	Info_AddChoice(DIA_Attila_Hallo, " What do you want from me? " ,DIA_Attila_Hallo_Was);
+	Info_AddChoice(DIA_Attila_Hallo, " Who are you? " ,DIA_Attila_Hallo_Wer);
 	B_GivePlayerXP(XP_Attila_MetHim);
 };
 
-func void DIA_Attila_Hallo_Wer()
+func void DIA_Attila_Hello_Who()
 {
 	AI_Output(other,self,"DIA_Attila_Hallo_Wer_15_00");	//Кто ты?
-	AI_Output(self,other,"DIA_Attila_Hallo_Wer_09_01");	//Меня зовут Аттила, но разве мое имя важно? Наши имена ничего не значат.
-	AI_Output(self,other,"DIA_Attila_Hallo_Wer_09_02");	//Ты это должен знать, чужеземец.
+	AI_Output(self,other, " DIA_Attila_Hallo_Wer_09_01 " );	// My name is Attila, but does my name matter? Our names mean nothing.
+	AI_Output(self,other, " DIA_Attila_Hallo_Wer_09_02 " );	// You should know this, outlander.
 	Knows_Attila_Wer = TRUE;
 	Info_ClearChoices(DIA_Attila_Hallo);
 	if(Knows_Attila_Was == FALSE)
 	{
-		Info_AddChoice(DIA_Attila_Hallo,"Что тебе нужно от меня?",DIA_Attila_Hallo_Was);
+		Info_AddChoice(DIA_Attila_Hallo, " What do you want from me? " ,DIA_Attila_Hallo_Was);
 	};
-	Info_AddChoice(DIA_Attila_Hallo,"К чему весь этот фарс?",DIA_Attila_Hallo_Theater);
+	Info_AddChoice(DIA_Attila_Hallo, " What's all this farce for? " ,DIA_Attila_Hallo_Theater);
 };
 
 func void DIA_Attila_Hallo_Was()
 {
-	AI_Output(other,self,"DIA_Attila_Hallo_Was_15_00");	//Что тебе нужно от меня?
-	AI_Output(self,other,"DIA_Attila_Hallo_Was_09_01");	//Я здесь, чтобы объяснить тебе несколько вещей. И кроме этого, я собираюсь убить тебя.
+	AI_Output(other,self, " DIA_Attila_Hallo_Was_15_00 " );	// What do you want from me?
+	AI_Output(self,other, " DIA_Attila_Hallo_Was_09_01 " );	// I'm here to explain a few things to you. And besides that, I'm going to kill you.
 	Knows_Attila_Was = TRUE;
 	Info_ClearChoices(DIA_Attila_Hallo);
 	if(Knows_Attila_Wer == FALSE)
 	{
-		Info_AddChoice(DIA_Attila_Hallo,"Кто ты?",DIA_Attila_Hallo_Wer);
+		Info_AddChoice(DIA_Attila_Hallo, " Who are you? " ,DIA_Attila_Hallo_Wer);
 	};
-	Info_AddChoice(DIA_Attila_Hallo,"Кто платит тебе за это?",DIA_Attila_Hallo_Auftrag);
-	Info_AddChoice(DIA_Attila_Hallo,"К чему весь этот фарс?",DIA_Attila_Hallo_Theater);
+	Info_AddChoice(DIA_Attila_Hallo, " Who pays you for this? " ,DIA_Attila_Hallo_Auftrag);
+	Info_AddChoice(DIA_Attila_Hallo, " What's all this farce for? " ,DIA_Attila_Hallo_Theater);
 };
 
 func void DIA_Attila_Hallo_Theater()
 {
-	AI_Output(other,self,"DIA_Attila_Hallo_Theater_15_00");	//К чему весь этот фарс?
-	AI_Output(self,other,"DIA_Attila_Hallo_Theater_09_01");	//Ты не должен умереть в неведении. Считай это проявлением уважения к приговоренным.
+	AI_Output(other,self, " DIA_Attila_Hallo_Theater_15_00 " );	// Why all this farce?
+	AI_Output(self,other, " DIA_Attila_Hallo_Theater_09_01 " );	// You must not die in ignorance. Consider it a show of respect for the condemned.
 	Info_ClearChoices(DIA_Attila_Hallo);
-	Info_AddChoice(DIA_Attila_Hallo,"Я, пожалуй, пойду.",DIA_Attila_Hallo_Ende);
-	Info_AddChoice(DIA_Attila_Hallo,"Кто платит тебе за это?",DIA_Attila_Hallo_Auftrag);
-	Info_AddChoice(DIA_Attila_Hallo,"Почему ты хочешь убить меня?",DIA_Attila_Hallo_Warum);
+	Info_AddChoice(DIA_Attila_Hallo, " I think I'll go. " ,DIA_Attila_Hallo_Ende);
+	Info_AddChoice(DIA_Attila_Hallo, " Who pays you for this? " ,DIA_Attila_Hallo_Auftrag);
+	Info_AddChoice(DIA_Attila_Hallo, " Why do you want to kill me? " ,DIA_Attila_Hallo_Warum);
 };
 
-func void DIA_Attila_Hallo_Ende()
+func void DIA_Attila_Hello_End()
 {
-	AI_Output(other,self,"DIA_Attila_Hallo_Ende_15_00");	//Я, пожалуй, пойду...
-	AI_Output(self,other,"DIA_Attila_Hallo_Ende_09_01");	//Боюсь... я не могу позволить тебе этого. Смирись. Пришло время умереть.
+	AI_Output(other,self, " DIA_Attila_Hallo_Ende_15_00 " );	// I'll probably go...
+	AI_Output(self,other, " DIA_Attila_Hallo_Ende_09_01 " );	// I'm afraid... I can't let you do that. Humble yourself. It's time to die.
 	AI_DrawWeapon(self);
 	Info_ClearChoices(DIA_Attila_Hallo);
-	Info_AddChoice(DIA_Attila_Hallo,"Кто платит тебе за это?",DIA_Attila_Hallo_Auftrag);
-	Info_AddChoice(DIA_Attila_Hallo,"Почему ты хочешь убить меня?",DIA_Attila_Hallo_Warum);
+	Info_AddChoice(DIA_Attila_Hallo, " Who pays you for this? " ,DIA_Attila_Hallo_Auftrag);
+	Info_AddChoice(DIA_Attila_Hallo, " Why do you want to kill me? " ,DIA_Attila_Hallo_Warum);
 };
 
-func void DIA_Attila_Hallo_Auftrag()
+func void DIA_Attila_Hello_Order()
 {
-	AI_Output(other,self,"DIA_Attila_Hallo_Auftrag_15_00");	//Кто платит тебе за это?
-	AI_Output(self,other,"DIA_Attila_Hallo_Auftrag_09_01");	//Мои хозяева стараются работать в тени, как и я.
-	AI_Output(self,other,"DIA_Attila_Hallo_Auftrag_09_02");	//В условиях моего контракта записано, что я не имею права разглашать ни их имя, ни их резиденцию.
+	AI_Output(other,self, " DIA_Attila_Hallo_Auftrag_15_00 " );	// Who pays you for this?
+	AI_Output(self,other, " DIA_Attila_Hallo_Auftrag_09_01 " );	// My masters try to work in the shade, as do I.
+	AI_Output(self,other, " DIA_Attila_Hallo_Auftrag_09_02 " );	// It's written in the terms of my contract that I have no right to divulge either their name or their residence.
 	Info_ClearChoices(DIA_Attila_Hallo);
-	Info_AddChoice(DIA_Attila_Hallo,"Почему ты хочешь убить меня?",DIA_Attila_Hallo_Warum);
+	Info_AddChoice(DIA_Attila_Hallo, " Why do you want to kill me? " ,DIA_Attila_Hallo_Warum);
 };
 
-func void DIA_Attila_Hallo_Warum()
+func void DIA_Attila_Hello_Why()
 {
-	AI_Output(other,self,"DIA_Attila_Hallo_Warum_15_00");	//Почему ты хочешь убить меня?
-	if(Betrayal_Halvor == TRUE)
+	AI_Output(other,self, " DIA_Attila_Hallo_Warum_15_00 " );	// Why do you want to kill me?
+	if (Betrayal_Halvor ==  TRUE )
 	{
-		AI_Output(self,other,"DIA_Attila_Hallo_Warum_09_01");	//Ты сдал Халвора! Теперь он сидит в тюрьме. Так не пойдет.
+		AI_Output(self,other, " DIA_Attila_Hallo_Warum_09_01 " );	// You betrayed Halvor! Now he is in jail. Will not work.
 	};
 	if(Rengaru_InKnast == TRUE)
 	{
-		AI_Output(self,other,"DIA_Attila_Hallo_Warum_09_02");	//Ты продал Ренгару ополчению. Он всего лишь мелкий воришка, но тебе не следовало делать этого.
+		AI_Output(self,other, " DIA_Attila_Hallo_Warum_09_02 " );	// You sold Rengar to the militia. He's just a petty thief, but you shouldn't have done that.
 	};
-	if(Nagur_Ausgeliefert == TRUE)
+	if (Nagur_Ausgeliefert ==  TRUE )
 	{
-		AI_Output(self,other,"DIA_Attila_Hallo_Warum_09_03");	//Нагур попал за решетку по твоей вине. Кое-кому кажется, что это непростительная ошибка.
+		AI_Output(self,other, " DIA_Attila_Hallo_Warum_09_03 " );	// Nagur went to jail because of you. Some people think that this is an unforgivable mistake.
 	};
-	AI_Output(self,other,"DIA_Attila_Hallo_Warum_09_04");	//Мои хозяева недовольны этим. Чтобы не дать тебе совершить еще одну ошибку, они послали меня.
+	AI_Output(self,other, " DIA_Attila_Hallo_Warum_09_04 " );	// My masters are unhappy with this. To prevent you from making another mistake, they sent me.
 	Info_ClearChoices(DIA_Attila_Hallo);
-	Info_AddChoice(DIA_Attila_Hallo,"Я могу дать тебе золото.",DIA_Attila_Hallo_Gold);
-	Info_AddChoice(DIA_Attila_Hallo,"Дай мне хотя бы вынуть свое оружие.",DIA_Attila_Hallo_Attacke);
+	Info_AddChoice(DIA_Attila_Hallo, " I can give you gold. " ,DIA_Attila_Hallo_Gold);
+	Info_AddChoice(DIA_Attila_Hallo, " At least let me draw my weapon. " ,DIA_Attila_Hallo_Attacke);
 };
 
 func void DIA_Attila_Hallo_Gold()
 {
-	AI_Output(other,self,"DIA_Attila_Hallo_Gold_15_00");	//Я могу дать тебе золото - много золота.
-	AI_Output(self,other,"DIA_Attila_Hallo_Gold_09_01");	//Тщетно! Я не за этим сюда пришел. Единственная цена, которую ты заплатишь - твоя жизнь. И заплатишь ты ее прямо сейчас.
+	AI_Output(other,self, " DIA_Attila_Hallo_Gold_15_00 " );	// I can give you gold - lots of gold.
+	AI_Output(self,other, " DIA_Attila_Hallo_Gold_09_01 " );	// In vain! I didn't come here for this. The only price you will pay is your life. And you pay it right now.
 	AI_StopProcessInfos(self);
 	B_Attack(self,other,AR_NONE,1);
 };
 
-func void DIA_Attila_Hallo_Attacke()
+func void DIA_Attila_Hello_Attack()
 {
-	AI_Output(other,self,"DIA_Attila_Hallo_Attacke_15_00");	//Дай мне хотя бы вынуть свое оружие.
+	AI_Output(other,self, " DIA_Attila_Hallo_Attacke_15_00 " );	// At least let me draw my weapon.
 	if(Npc_HasEquippedWeapon(other) == TRUE)
 	{
-		AI_Output(self,other,"DIA_Attila_Hallo_Attacke_09_01");	//Хорошо, приготовься к своей последней битве.
+		AI_Output(self,other, " DIA_Attila_Hallo_Attacke_09_01 " );	// Okay, get ready for your final battle.
 		AI_StopProcessInfos(self);
 		B_Attack(self,other,AR_NONE,1);
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_Attila_Hallo_Attacke_09_02");	//Да у тебя нет никакого оружия на поясе. У тебя осталось очень мало времени, чужеземец. Смерть уже близко.
+		AI_Output(self,other, " DIA_Attila_Hallo_Attacke_09_02 " );	// You don't have any weapons on your belt. You have very little time left, outlander. Death is near.
 		AI_StopProcessInfos(self);
 		B_Attack(self,other,AR_NONE,2);
 	};
 };
 
 
-instance DIA_Attila_Willkommen(C_Info)
+instance DIA_Attila_Welcome (C_Info)
 {
 	npc = VLK_494_Attila;
 	nr = 1;
-	condition = DIA_Attila_Willkommen_Condition;
-	information = DIA_Attila_Willkommen_Info;
+	condition = DIA_Attila_Welcome_Condition;
+	information = DIA_Attila_Welcome_Info;
 	permanent = FALSE;
 	important = TRUE;
 };
 
 
-func int DIA_Attila_Willkommen_Condition()
+func int DIA_Attila_Welcome_Condition()
 {
 	if(Npc_IsInState(self,ZS_Talk) && (MIS_ThiefGuild_sucked == FALSE))
 	{
@@ -207,22 +208,22 @@ func int DIA_Attila_Willkommen_Condition()
 	};
 };
 
-func void DIA_Attila_Willkommen_Info()
+func void DIA_Attila_Welcome_Info()
 {
-	AI_Output(self,other,"DIA_Attila_Willkommen_09_00");	//Ах, наконец. Я ждал тебя, чужеземец.
+	AI_Output(self,other, " DIA_Attila_Willkommen_09_00 " );	// Ah, finally. I've been waiting for you, stranger.
 	B_GivePlayerXP(XP_Attila_Friend);
-	AI_Output(other,self,"DIA_Attila_Willkommen_15_01");	//Кто ты и что тебе нужно от меня?
-	AI_Output(self,other,"DIA_Attila_Willkommen_09_02");	//Это не важно. Важно лишь то, что ты делал. Ты оставался лояльным - даже если ты сам не знал об этом.
-	AI_Output(self,other,"DIA_Attila_Willkommen_09_03");	//Некоторые покровители прослышали про твою лояльность. И они дают тебе шанс. Так используй его.
-	AI_Output(other,self,"DIA_Attila_Willkommen_15_04");	//Эй, просто скажи мне кто они.
-	AI_Output(self,other,"DIA_Attila_Willkommen_09_05");	//У меня есть для тебя подарок. А все остальное в твоих руках, чужеземец.
+	AI_Output(other,self, " DIA_Attila_Willkommen_15_01 " );	// Who are you and what do you want from me?
+	AI_Output(self,other, " DIA_Attila_Willkommen_09_02 " );	// It doesn't matter. What matters is what you did. You remained loyal - even if you didn't know it yourself.
+	AI_Output(self,other, " DIA_Attila_Willkommen_09_03 " );	// Some patrons have heard of your loyalty. And they give you a chance. So use it.
+	AI_Output(other,self, " DIA_Attila_Willkommen_15_04 " );	// Hey, just tell me who they are.
+	AI_Output(self,other, " DIA_Attila_Willkommen_09_05 " );	// I have a present for you. Everything else is in your hands, stranger.
 	B_GiveInvItems(self,other,ItKe_ZhiefGuildKey_MIS,1);
 	Attila_Key = TRUE;
 	AI_StopProcessInfos(self);
 	Npc_ExchangeRoutine(self,"AFTER");
 };
 
-instance DIA_Attila_NachSchluessel(C_Info)
+instance DIA_Attila_NachSchluessel (C_Info)
 {
 	npc = VLK_494_Attila;
 	nr = 1;
@@ -242,24 +243,24 @@ func int DIA_Attila_NachSchluessel_Condition()
 
 func void DIA_Attila_NachSchluessel_Info()
 {
-	AI_Output(self,other,"DIA_Attila_NachSchluessel_09_00");	//Моя задача выполнена - пока.
-	AI_Output(self,other,"DIA_Attila_NachSchluessel_09_01");	//Но кто знает, может, наши пути опять пересекутся...
+	AI_Output(self,other, " DIA_Attila_NachSchluessel_09_00 " );	// My task is done - for now.
+	AI_Output(self,other, " DIA_Attila_NachSchluessel_09_01 " );	// But who knows, maybe our paths will cross again...
 	AI_StopProcessInfos(self);
 };
 
-instance DIA_Attila_Wer(C_Info)
+instance DIA_Attila_Wer (C_Info)
 {
 	npc = VLK_494_Attila;
 	nr = 1;
 	condition = DIA_Attila_Wer_Condition;
-	information = DIA_Attila_Wer_Info;
+	information = DIA_Attila_Who_Info;
 	permanent = FALSE;
-	description = "Кто ты?";
+	description = " Who are you? " ;
 };
 
 func int DIA_Attila_Wer_Condition()
 {
-	if(Npc_KnowsInfo(other,DIA_Attila_Willkommen) && (IS_LOVCACH == FALSE))
+	if ( Npc_KnowsInfo ( other , DIA_Attila_Willcome ) && ( IS_LOVCACH  ==  FALSE )) ;
 	{
 		return TRUE;
 	};
@@ -268,19 +269,19 @@ func int DIA_Attila_Wer_Condition()
 func void DIA_Attila_Wer_Info()
 {
 	AI_Output(other,self,"DIA_Attila_Hallo_Wer_15_00");	//Кто ты?
-	AI_Output(self,other,"DIA_Attila_Hallo_Wer_09_01");	//Меня зовут Аттила... но разве мое имя важно? Наши имена ничего не значат.
-	AI_Output(self,other,"DIA_Attila_Hallo_Wer_09_02");	//Ты это должен знать, чужеземец.
+	AI_Output(self,other, " DIA_Attila_Hallo_Wer_09_01 " );	// My name is Attila... but does my name matter? Our names mean nothing.
+	AI_Output(self,other, " DIA_Attila_Hallo_Wer_09_02 " );	// You should know this, outlander.
 	AI_StopProcessInfos(self);
 };
 
-instance DIA_Attila_Job(C_Info)
+instance DIA_Attila_Job (C_Info)
 {
 	npc = VLK_494_Attila;
 	nr = 1;
 	condition = DIA_Attila_Job_Condition;
 	information = DIA_Attila_Job_Info;
 	permanent = FALSE;
-	description = "А каковы твои обязанности в гильдии?";
+	description = " What are your duties in the guild? " ;
 };
 
 func int DIA_Attila_Job_Condition()
@@ -293,10 +294,10 @@ func int DIA_Attila_Job_Condition()
 
 func void DIA_Attila_Job_Info()
 {
-	AI_Output(other,self,"DIA_Attila_Hallo_Job_15_00");	//А каковы твои обязанности в гильдии?
-	AI_Output(self,other,"DIA_Attila_Hallo_Job_09_01");	//Раз теперь ты один из нас, то, пожалуй, расскажу.
-	AI_Output(self,other,"DIA_Attila_Hallo_Job_09_02");	//Я являюсь посредником между нашими людьми сверху и Кассией. Почти все дела проходят через меня.
-	AI_Output(self,other,"DIA_Attila_Hallo_Job_09_03");	//Также я доставляю различного рода послания.
+	AI_Output(other,self, " DIA_Attila_Hallo_Job_15_00 " );	// And what are your duties in the guild?
+	AI_Output(self,other, " DIA_Attila_Hallo_Job_09_01 " );	// Since you're one of us now, I'll probably tell you.
+	AI_Output(self,other, " DIA_Attila_Hallo_Job_09_02 " );	// I am an intermediary between our people from above and Cassia. Almost everything goes through me.
+	AI_Output(self,other, " DIA_Attila_Hallo_Job_09_03 " );	// I also deliver various kinds of messages.
 };
 
 instance DIA_Attila_CanTeach(C_Info)
@@ -306,7 +307,7 @@ instance DIA_Attila_CanTeach(C_Info)
 	condition = DIA_Attila_CanTeach_Condition;
 	information = DIA_Attila_CanTeach_Info;
 	permanent = FALSE;
-	description = "Чему ты можешь меня научить?";
+	description = " What can you teach me? " ;
 };
 
 func int DIA_Attila_CanTeach_Condition()
@@ -319,9 +320,9 @@ func int DIA_Attila_CanTeach_Condition()
 
 func void DIA_Attila_CanTeach_Info()
 {
-	AI_Output(other,self,"DIA_Attila_Hallo_CanTeach_15_00");	//Чему ты можешь меня научить?
-	AI_Output(self,other,"DIA_Attila_Hallo_CanTeach_09_01");	//Если хочешь, я могу показать тебе, как лучше обращаться с двуручным оружием.
-	AI_Output(self,other,"DIA_Attila_Hallo_CanTeach_09_02");	//Хотя обычно воры предпочитают шпаги или рапиры.
+	AI_Output(other,self, " DIA_Attila_Hallo_CanTeach_15_00 " );	// What can you teach me?
+	AI_Output(self,other, " DIA_Attila_Hallo_CanTeach_09_01 " );	// If you want, I can show you how to use two-handed weapons better.
+	AI_Output(self,other, " DIA_Attila_Hallo_CanTeach_09_02 " );	// Although thieves usually prefer swords or rapiers.
 };
 
 instance DIA_Attila_TEACH(C_Info)
@@ -331,7 +332,7 @@ instance DIA_Attila_TEACH(C_Info)
 	condition = DIA_Attila_teach_condition;
 	information = DIA_Attila_teach_info;
 	permanent = TRUE;
-	description = "Я нуждаюсь в тренировке.";
+	description = " I need a workout. " ;
 };
 
 func int DIA_Attila_teach_condition()
@@ -344,7 +345,7 @@ func int DIA_Attila_teach_condition()
 
 func void DIA_Attila_teach_info()
 {
-	AI_Output(other,self,"DIA_Attila_Teach_15_00");	//Я нуждаюсь в тренировке.
+	AI_Output(other,self, " DIA_Attila_Teach_15_00 " );	// I need a workout.
 	Info_ClearChoices(DIA_Attila_teach);
 	Info_AddChoice(DIA_Attila_teach,Dialog_Back,DIA_Attila_teach_back);
 	Info_AddChoice(DIA_Attila_teach,b_buildlearnstringforfight(PRINT_Learn2h1,B_GetLearnCostTalent(other,NPC_TALENT_2H,1)),DIA_Attila_teach_2h_1);
