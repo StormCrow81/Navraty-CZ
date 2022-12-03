22dd513bd5e84cabeeb588df1be8556503c24641
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 13:01:36 +0200

    Update DIA_Pal_205_Torwache.d

--- RU-Main/Story/Dialoge/DIA_Pal_205_Torwache.d
+++ RU-Main/Story/Dialoge/DIA_Pal_205_Torwache.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_PAL_205_Torwache_EXIT(C_Info)
 {
 	npc = Pal_205_Torwache;
@@ -23,7 +24,7 @@ func void DIA_PAL_205_Torwache_EXIT_Info()
 
 const string PAL_205_Checkpoint = "NW_CITY_CITYHALL_IN";
 
-var int PAL_205_schonmalreingelassen;
+var int PAL_205_always let in;
 
 instance DIA_PAL_205_Torwache_FirstWarn(C_Info)
 {
@@ -41,15 +42,15 @@ func int DIA_PAL_205_Torwache_FirstWarn_Condition()
 	if(Npc_GetDistToWP(other,PAL_205_Checkpoint) <= 550)
 	{
 		Npc_SetRefuseTalk(self,5);
-		return FALSE;
+		return  FALSE ;
 	};
 	if((B_GetGreatestPetzCrime(self) >= CRIME_ATTACK) && (PAL_205_schonmalreingelassen == TRUE))
 	{
-		self.aivar[AIV_PASSGATE] = FALSE;
+		self.aivar[ AIV_PASSGATE ] = FALSE ;
 	}
-	else if(PAL_205_schonmalreingelassen == TRUE)
+	else  if (PAL_205_schonmalreinlett ==  TRUE )
 	{
-		self.aivar[AIV_PASSGATE] = TRUE;
+		self.aivar[ AIV_PASSGATE ] = TRUE ;
 	};
 	if((self.aivar[AIV_Guardpassage_Status] == GP_NONE) && (self.aivar[AIV_PASSGATE] == FALSE) && (Hlp_StrCmp(Npc_GetNearestWP(self),self.wp) == TRUE) && (Npc_RefuseTalk(self) == FALSE))
 	{
@@ -64,21 +65,21 @@ func void DIA_PAL_205_Torwache_FirstWarn_Info()
 	{
 		if(B_GetGreatestPetzCrime(self) == CRIME_MURDER)
 		{
-			AI_Output(self,other,"DIA_PAL_205_Torwache_FirstWarn_12_01");	//Я нарушу свой самый священный долг, если пущу сюда убийцу.
+			AI_Output(self,other, " DIA_PAL_205_Torwache_FirstWarn_12_01 " );	// I'll be violating my most sacred duty if I let an assassin in here.
 		};
 		if(B_GetGreatestPetzCrime(self) == CRIME_THEFT)
 		{
-			AI_Output(self,other,"DIA_PAL_205_Torwache_FirstWarn_12_02");	//Ты обвиняешься в воровстве. Пока не доказано обратное, ты не можешь войти сюда!
+			AI_Output(self,other, " DIA_PAL_205_Torwache_FirstWarn_12_02 " );	// You are accused of stealing. Until proven otherwise, you cannot enter here!
 		};
 		if(B_GetGreatestPetzCrime(self) == CRIME_ATTACK)
 		{
-			AI_Output(self,other,"DIA_PAL_205_Torwache_FirstWarn_12_03");	//Говорят, ты принимал участие в драке. Я не пропущу тебя.
+			AI_Output(self,other, " DIA_PAL_205_Torwache_FirstWarn_12_03 " );	// They say you took part in the fight. I won't miss you.
 		};
-		AI_Output(self,other,"DIA_PAL_205_Torwache_FirstWarn_12_04");	//Иди к лорду Андрэ и урегулируй этот вопрос!
+		AI_Output(self,other, " DIA_PAL_205_Torwache_FirstWarn_12_04 " );	// Go to Lord Andre and settle this matter!
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_PAL_205_Torwache_FirstWarn_12_05");	//Только люди, состоящие на службе короля, могут войти в ратушу.
+		AI_Output(self,other, " DIA_PAL_205_Torwache_FirstWarn_12_05 " );	// Only people in the king's service can enter the town hall.
 		PALGUARD = TRUE;
 	};
 	other.aivar[AIV_LastDistToWP] = Npc_GetDistToWP(other,PAL_205_Checkpoint);
@@ -107,7 +108,7 @@ func int DIA_PAL_205_Torwache_SecondWarn_Condition()
 
 func void DIA_PAL_205_Torwache_SecondWarn_Info()
 {
-	AI_Output(self,other,"DIA_PAL_205_Torwache_SecondWarn_12_00");	//Если ты сделаешь еще шаг вперед, клянусь Инносом, я применю оружие!
+	AI_Output(self,other, " DIA_PAL_205_Torwache_SecondWarn_12_00 " );	// If you take another step forward, I swear by Innos, I will use my weapon!
 	other.aivar[AIV_LastDistToWP] = Npc_GetDistToWP(other,PAL_205_Checkpoint);
 	self.aivar[AIV_Guardpassage_Status] = GP_SecondWarnGiven;
 	AI_StopProcessInfos(self);
@@ -136,26 +137,26 @@ func int DIA_PAL_205_Torwache_Attack_Condition()
 func void DIA_PAL_205_Torwache_Attack_Info()
 {
 	other.aivar[AIV_LastDistToWP] = 0;
-	self.aivar[AIV_Guardpassage_Status] = GP_NONE;
+	self.aivar[AIV_Guardpassage_Status] = GP_NONE ;
 	AI_StopProcessInfos(self);
 	B_Attack(self,other,AR_GuardStopsIntruder,0);
 };
 
 
-instance DIA_PAL_205_Torwache_Hagen(C_Info)
+instance DIA_PAL_205_Torwache_Hagen (C_Info)
 {
 	npc = Pal_205_Torwache;
 	nr = 2;
 	condition = DIA_PAL_205_Torwache_Hagen_Condition;
 	information = DIA_PAL_205_Torwache_Hagen_Info;
 	permanent = TRUE;
-	description = "Мне нужно поговорить с лордом Хагеном!";
+	description = " I need to speak to Lord Hagen! " ;
 };
 
 
 func int DIA_PAL_205_Torwache_Hagen_Condition()
 {
-	if(PAL_205_schonmalreingelassen == FALSE)
+	if (PAL_205_schonmalreinlett ==  FALSE )
 	{
 		return TRUE;
 	};
@@ -163,8 +164,8 @@ func int DIA_PAL_205_Torwache_Hagen_Condition()
 
 func void DIA_PAL_205_Torwache_Hagen_Info()
 {
-	AI_Output(other,self,"DIA_PAL_205_Torwache_Hagen_15_00");	//Мне нужно поговорить с лордом Хагеном!
-	AI_Output(self,other,"DIA_PAL_205_Torwache_Hagen_12_01");	//Да ты знаешь, как часто я слышу это? Я не пущу тебя. А если ты все же попытаешься войти, мне придется убить тебя.
+	AI_Output(other,self, " DIA_PAL_205_Torwache_Hagen_15_00 " );	// I need to speak to Lord Hagen!
+	AI_Output(self,other, " DIA_PAL_205_Torwache_Hagen_12_01 " );	// Do you know how often I hear that? I won't let you in And if you still try to enter, I will have to kill you.
 	AI_StopProcessInfos(self);
 };
 
@@ -176,7 +177,7 @@ instance DIA_PAL_205_Torwache_PassAsMil(C_Info)
 	condition = DIA_PAL_205_Torwache_PassAsMil_Condition;
 	information = DIA_PAL_205_Torwache_PassAsMil_Info;
 	permanent = TRUE;
-	description = "Я состою в ополчении.";
+	description = " I'm in the militia. " ;
 };
 
 
@@ -190,12 +191,12 @@ func int DIA_PAL_205_Torwache_PassAsMil_Condition()
 
 func void DIA_PAL_205_Torwache_PassAsMil_Info()
 {
-	AI_Output(other,self,"DIA_PAL_205_Torwache_PassAsMil_15_00");	//Я состою в ополчении.
-	AI_Output(self,other,"DIA_PAL_205_Torwache_PassAsMil_12_01");	//Хорошо, ты можешь войти.
+	AI_Output(other,self, " DIA_PAL_205_Torwache_PassAsMil_15_00 " );	// I'm in the militia.
+	AI_Output(self,other, " DIA_PAL_205_Torwache_PassAsMil_12_01 " );	// Okay, you can come in.
 	CITYHALLACCESSGRANTED = TRUE;
 	PALGUARD = FALSE;
-	self.aivar[AIV_PASSGATE] = TRUE;
-	PAL_205_schonmalreingelassen = TRUE;
+	self.aivar[ AIV_PASSGATE ] = TRUE ;
+	PAL_205_schonmalreinlett = TRUE ;
 	AI_StopProcessInfos(self);
 };
 
@@ -207,7 +208,7 @@ instance DIA_PAL_205_Torwache_PassAsMage(C_Info)
 	condition = DIA_PAL_205_Torwache_PassAsMage_Condition;
 	information = DIA_PAL_205_Torwache_PassAsMage_Info;
 	permanent = TRUE;
-	description = "Я - маг Огня.";
+	description = " I am a fire mage. " ;
 };
 
 
@@ -221,12 +222,12 @@ func int DIA_PAL_205_Torwache_PassAsMage_Condition()
 
 func void DIA_PAL_205_Torwache_PassAsMage_Info()
 {
-	AI_Output(other,self,"DIA_PAL_205_Torwache_PassAsMage_15_00");	//Я - маг Огня.
-	AI_Output(self,other,"DIA_PAL_205_Torwache_PassAsMage_12_01");	//Да, конечно! Прошу простить меня, о Избранный. Я просто выполняю свой долг.
+	AI_Output(other,self, " DIA_PAL_205_Torwache_PassAsMage_15_00 " );	// I am a fire mage.
+	AI_Output(self,other, " DIA_PAL_205_Torwache_PassAsMage_12_01 " );	// Yes, of course! Please forgive me, O Chosen One. I'm just doing my duty.
 	CITYHALLACCESSGRANTED = TRUE;
 	PALGUARD = FALSE;
-	self.aivar[AIV_PASSGATE] = TRUE;
-	PAL_205_schonmalreingelassen = TRUE;
+	self.aivar[ AIV_PASSGATE ] = TRUE ;
+	PAL_205_schonmalreinlett = TRUE ;
 	AI_StopProcessInfos(self);
 };
 
@@ -238,7 +239,7 @@ instance DIA_PAL_205_Torwache_PassAsSld(C_Info)
 	condition = DIA_PAL_205_Torwache_PassAsSld_Condition;
 	information = DIA_PAL_205_Torwache_PassAsSld_Info;
 	permanent = TRUE;
-	description = "Дай мне пройти - я несу послание от наемников. ";
+	description = " Let me pass - I bring a message from the mercenaries. " ;
 };
 
 
@@ -252,12 +253,12 @@ func int DIA_PAL_205_Torwache_PassAsSld_Condition()
 
 func void DIA_PAL_205_Torwache_PassAsSld_Info()
 {
-	AI_Output(other,self,"DIA_PAL_205_Torwache_PassAsSld_15_00");	//Дай мне пройти - я несу послание от наемников.
-	AI_Output(self,other,"DIA_PAL_205_Torwache_PassAsSld_12_01");	//Хорошо, но предупреждаю тебя: если возникнут какие-нибудь проблемы, то ты не успеешь даже пожалеть об этом!
+	AI_Output(other,self, " DIA_PAL_205_Torwache_PassAsSld_15_00 " );	// Let me pass - I carry a message from the mercenaries.
+	AI_Output(self,other, " DIA_PAL_205_Torwache_PassAsSld_12_01 " );	// Okay, but I warn you: if there are any problems, you won't even have time to regret it!
 	CITYHALLACCESSGRANTED = TRUE;
 	PALGUARD = FALSE;
-	self.aivar[AIV_PASSGATE] = TRUE;
-	PAL_205_schonmalreingelassen = TRUE;
+	self.aivar[ AIV_PASSGATE ] = TRUE ;
+	PAL_205_schonmalreinlett = TRUE ;
 	AI_StopProcessInfos(self);
 };
 
@@ -269,7 +270,7 @@ instance DIA_PAL_205_TORWACHE_PASSASKDW(C_Info)
 	condition = dia_pal_205_torwache_passaskdw_condition;
 	information = dia_pal_205_torwache_passaskdw_info;
 	permanent = TRUE;
-	description = "Я посланник Магов Воды.";
+	description = " I am the messenger of the Water Mages. " ;
 };
 
 
@@ -283,12 +284,12 @@ func int dia_pal_205_torwache_passaskdw_condition()
 
 func void dia_pal_205_torwache_passaskdw_info()
 {
-	AI_Output(other,self,"DIA_PAL_205_Torwache_PassAsKDW_15_00");	//Я посланник магов Воды. У меня срочное поручение для лорда Хагена.
-	AI_Output(self,other,"DIA_PAL_205_Torwache_PassAsKDW_12_01");	//Хорошо, проходи, послушник. Но только постарайся не создавать проблем!
+	AI_Output(other,self, " DIA_PAL_205_Torwache_PassAsKDW_15_00 " );	// I am the messenger of the Waterbenders. I have an urgent assignment for Lord Hagen.
+	AI_Output(self,other, " DIA_PAL_205_Torwache_PassAsKDW_12_01 " );	// Okay, come on, novice. But just try not to cause problems!
 	CITYHALLACCESSGRANTED = TRUE;
 	PALGUARD = FALSE;
-	self.aivar[AIV_PASSGATE] = TRUE;
-	PAL_205_schonmalreingelassen = TRUE;
+	self.aivar[ AIV_PASSGATE ] = TRUE ;
+	PAL_205_schonmalreinlett = TRUE ;
 	AI_StopProcessInfos(self);
 };
 
@@ -300,7 +301,7 @@ instance DIA_PAL_205_TORWACHE_PASSASKDM(C_Info)
 	condition = dia_pal_205_torwache_passaskdm_condition;
 	information = dia_pal_205_torwache_passaskdm_info;
 	permanent = TRUE;
-	description = "Ты же не станешь задерживать слугу Бога Тьмы?";
+	description = " You wouldn't detain a servant of the God of Darkness, would you? " ;
 };
 
 
@@ -314,24 +315,24 @@ func int dia_pal_205_torwache_passaskdm_condition()
 
 func void dia_pal_205_torwache_passaskdm_info()
 {
-	AI_Output(other,self,"DIA_PAL_205_Torwache_PassAsKDM_15_00");	//Ты же не станешь задерживать слугу Бога Тьмы?
-	AI_Output(self,other,"DIA_PAL_205_Torwache_PassAsKDM_12_01");	//(недоверчиво) Проходи, некромант. Но я предупреждаю тебя: если ты устроишь здесь неприятности, у тебя не будет времени даже на раскаянье.
+	AI_Output(other,self, " DIA_PAL_205_Torwache_PassAsKDM_15_00 " );	// You won't detain a servant of the God of Darkness, will you?
+	AI_Output(self,other, " DIA_PAL_205_Torwache_PassAsKDM_12_01 " );	// (incredulously) Come on, necromancer. But I warn you, if you make trouble here, you won't even have time to repent.
 	CITYHALLACCESSGRANTED = TRUE;
 	PALGUARD = FALSE;
-	self.aivar[AIV_PASSGATE] = TRUE;
-	PAL_205_schonmalreingelassen = TRUE;
+	self.aivar[ AIV_PASSGATE ] = TRUE ;
+	PAL_205_schonmalreinlett = TRUE ;
 	AI_StopProcessInfos(self);
 };
 
 
-instance DIA_PAL_205_TORWACHE_PASSASSEK(C_Info)
+instance DIA_PAL_205_TORWACHE_PASSASSEK (C_Info)
 {
 	npc = Pal_205_Torwache;
 	nr = 3;
 	condition = dia_pal_205_torwache_passassek_condition;
 	information = dia_pal_205_torwache_passassek_info;
 	permanent = TRUE;
-	description = "У меня назначена встреча с главой города.";
+	description = " I have an appointment with the mayor. " ;
 };
 
 func int dia_pal_205_torwache_passassek_condition()
@@ -344,14 +345,14 @@ func int dia_pal_205_torwache_passassek_condition()
 
 func void dia_pal_205_torwache_passassek_info()
 {
-	AI_Output(other,self,"DIA_PAL_205_Torwache_PassAsSEK_15_00");	//У меня назначена встреча с главой города Лариусом. Тебя должны были предупредить.
-	AI_Output(self,other,"DIA_PAL_205_Torwache_PassAsSEK_12_01");	//Да, мне сказали об этом. Но если ты действительно тот, за кого себя выдаешь - ты должен знать пароль. Ведь ты его знаешь?
-	AI_Output(other,self,"DIA_PAL_205_Torwache_PassAsSEK_15_01");	//ЮБЕРИОН! Ну так что, ты впустишь меня?
-	AI_Output(self,other,"DIA_PAL_205_Torwache_PassAsSEK_12_02");	//Хорошо, проходи. Но я предупреждаю тебя: если ты устроишь здесь неприятности, у тебя не будет времени даже на раскаянье!
+	AI_Output(other,self, " DIA_PAL_205_Torwache_PassAsSEK_15_00 " );	// I have an appointment with Mayor Larius. You should have been warned.
+	AI_Output(self,other, " DIA_PAL_205_Torwache_PassAsSEK_12_01 " );	// Yes, I was told about it. But if you really are who you say you are, you must know the password. After all, you know him?
+	AI_Output(other,self, " DIA_PAL_205_Torwache_PassAsSEK_15_01 " );	// UBERION! So, will you let me in?
+	AI_Output(self,other, " DIA_PAL_205_Torwache_PassAsSEK_12_02 " );	// Okay, come on in. But I warn you: if you make trouble here, you won't even have time to repent!
 	CITYHALLACCESSGRANTED = TRUE;
 	PALGUARD = FALSE;
-	self.aivar[AIV_PASSGATE] = TRUE;
-	PAL_205_schonmalreingelassen = TRUE;
+	self.aivar[ AIV_PASSGATE ] = TRUE ;
+	PAL_205_schonmalreinlett = TRUE ;
 	AI_StopProcessInfos(self);
 };
 
@@ -363,7 +364,7 @@ instance DIA_PAL_205_Torwache_PERM(C_Info)
 	condition = DIA_PAL_205_Torwache_PERM_Condition;
 	information = DIA_PAL_205_Torwache_PERM_Info;
 	permanent = TRUE;
-	description = "Какая моя обязанность?";
+	description = " What is my duty? " ;
 };
 
 func int DIA_PAL_205_Torwache_PERM_Condition()
@@ -376,23 +377,23 @@ func int DIA_PAL_205_Torwache_PERM_Condition()
 
 func void DIA_PAL_205_Torwache_PERM_Info()
 {
-	AI_Output(other,self,"DIA_PAL_205_Torwache_PERM_15_00");	//А какая моя обязанность?
+	AI_Output(other,self, " DIA_PAL_205_Torwache_PERM_15_00 " );	// And what is my responsibility?
 
 	if((other.guild == GIL_PAL) || (other.guild == GIL_MIL))
 	{
-		AI_Output(self,other,"DIA_PAL_205_Torwache_PERM_12_01");	//У нас все схвачено.
+		AI_Output(self,other, " DIA_PAL_205_Torwache_PERM_12_01 " );	// We've got it all.
 	}
 	else if((other.guild == GIL_KDF) && (other.guild == GIL_KDW))
 	{
-		AI_Output(self,other,"DIA_PAL_205_Torwache_PERM_12_02");	//Мое задание наполняет меня, избранный.
+		AI_Output(self,other, " DIA_PAL_205_Torwache_PERM_12_02 " );	// My assignment fills me, chosen one.
 	}
 	else if((other.guild == GIL_SLD) || (other.guild == GIL_DJG))
 	{
-		AI_Output(self,other,"DIA_PAL_205_Torwache_PERM_12_03");	//Входи и помалкивай.
+		AI_Output(self,other, " DIA_PAL_205_Torwache_PERM_12_03 " );	// Come in and keep quiet.
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_PAL_205_Torwache_PERM_12_04");	//Чего ты хочешь?
+		AI_Output(self,other, " DIA_PAL_205_Torwache_PERM_12_04 " );	// What do you want?
 	};
 
 	AI_StopProcessInfos(self);
