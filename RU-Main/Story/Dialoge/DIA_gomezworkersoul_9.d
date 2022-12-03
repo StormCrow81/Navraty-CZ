145b063a9713566dee4e88a542643d22681ccb26
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 22:53:22 +0200

    Update DIA_gomezworkersoul_9.d

--- RU-Main/Story/Dialoge/DIA_gomezworkersoul_9.d
+++ RU-Main/Story/Dialoge/DIA_gomezworkersoul_9.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_GOMEZWORKERSOUL_9_EXIT(C_Info)
 {
 	npc = gomezworkersoul_9;
@@ -21,12 +22,12 @@ func void dia_gomezworkersoul_9_exit_info()
 };
 
 
-instance DIA_GOMEZWORKERSOUL_9_HALLO(C_Info)
+instance DIA_GOMEZWORKERSOUL_9_HELLO (C_Info)
 {
 	npc = gomezworkersoul_9;
 	nr = 1;
 	condition = dia_gomezworkersoul_9_hallo_condition;
-	information = dia_gomezworkersoul_9_hallo_info;
+	information = dia_gomezworkersoul_9_hello_info;
 	permanent = FALSE;
 	important = TRUE;
 };
@@ -40,28 +41,28 @@ func int dia_gomezworkersoul_9_hallo_condition()
 	};
 };
 
-func void dia_gomezworkersoul_9_hallo_info()
+func void dia_gomezworkersoul_9_hello_info()
 {
-	self.aivar[AIV_EnemyOverride] = FALSE;
+	self.aivar[AIV_EnemyOverride] = FALSE ;
 	Snd_Play("MFX_FEAR_CAST");
 	B_GivePlayerXP(50);
-	AI_Output(self,other,"DIA_GomezWorkerSoul_9_HALLO_01_00");	//(мертвым голосом) ЧТО ТЫ ИЩЕТ ТУТ? РАЗВЕ МЕРТВЫЕ В ОТВЕТЕ ПЕРЕД ЖИВЫМИ?
-	AI_Output(other,self,"DIA_GomezWorkerSoul_9_HALLO_01_01");	//Нет. Но мертвые в ответе перед мертвыми.
-	AI_Output(self,other,"DIA_GomezWorkerSoul_9_HALLO_01_02");	//О КОМ ТЫ ГОВОРИШЬ, СМЕРТНЫЙ?
-	AI_Output(other,self,"DIA_GomezWorkerSoul_9_HALLO_01_03");	//Душа бывшего рудного барона Гомеза в ответе перед тобой, призрак.
-	AI_Output(other,self,"DIA_GomezWorkerSoul_9_HALLO_01_04");	//И этот ответ подразумевает под собой твое освобождение.
-	AI_Output(other,self,"DIA_GomezWorkerSoul_9_HALLO_01_05");	//Правда, только в том случае, если ты даруешь его душе свое прощение.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_9_HALLO_01_00 " );	// (dead voice) WHAT ARE YOU LOOKING FOR HERE? IS THE DEAD ANSWER TO THE LIVING?
+	AI_Output(other,self, " DIA_GomezWorkerSoul_9_HALLO_01_01 " );	// No. But the dead are responsible to the dead.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_9_HALLO_01_02 " );	// WHAT ARE YOU TALKING ABOUT, MORTAL?
+	AI_Output(other,self, " DIA_GomezWorkerSoul_9_HALLO_01_03 " );	// The soul of former ore baron Gomez is accountable to you, ghost.
+	AI_Output(other,self, " DIA_GomezWorkerSoul_9_HALLO_01_04 " );	// And this answer implies your release.
+	AI_Output(other,self, " DIA_GomezWorkerSoul_9_HALLO_01_05 " );	// True, only if you grant his soul your forgiveness.
 	Info_ClearChoices(dia_gomezworkersoul_9_hallo);
-	Info_AddChoice(dia_gomezworkersoul_9_hallo,"Так ты даешь ему свое прощение?",dia_gomezworkersoul_9_hallo_end);
+	Info_AddChoice(dia_gomezworkersoul_9_hallo, " So you give him your forgiveness? " ,dia_gomezworkersoul_9_hallo_end);
 };
 
-func void dia_gomezworkersoul_9_hallo_end()
+func void dia_gomezworkersoul_9_hello_end()
 {
 	Snd_Play("SFX_INNOSEYE");
-	AI_Output(other,self,"DIA_GomezWorkerSoul_9_HALLO_End_01_01");	//Так ты даешь ему свое прощение?
-	AI_Output(self,other,"DIA_GomezWorkerSoul_9_HALLO_End_01_02");	//Хорошо, смертный. Если это дарует мне свободу, я прощаю душу Гомеза...
-	AI_Output(other,self,"DIA_GomezWorkerSoul_9_HALLO_End_01_03");	//Отлично! Тогда я позабочусь о том, чтобы твоя душа более не испытывала ужасных страданий и обрела покой.
-	AI_Output(self,other,"DIA_GomezWorkerSoul_9_HALLO_End_01_04");	//Аргххх...
+	AI_Output(other,self, " DIA_GomezWorkerSoul_9_HALLO_End_01_01 " );	// So you give him your forgiveness?
+	AI_Output(self,other, " DIA_GomezWorkerSoul_9_HALLO_End_01_02 " );	// All right, mortal. If it grants me freedom, I forgive Gomez's soul...
+	AI_Output(other,self, " DIA_GomezWorkerSoul_9_HALLO_End_01_03 " );	// Great! Then I will make sure that your soul no longer suffers terrible suffering and finds peace.
+	AI_Output(self,other, " DIA_GomezWorkerSoul_9_HALLO_End_01_04 " );	// Arghhh...
 	GOMEZSOULTALK_9 = TRUE;
 	AI_StopProcessInfos(self);
 };
