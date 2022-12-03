ae94a8590719f3729ca711c25dc135fce5a99b09
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 13:16:44 +0200

    Update DIA_SLD_809_Bennet_DI.d

--- RU-Main/Story/Dialoge/DIA_SLD_809_Bennet_DI.d
+++ RU-Main/Story/Dialoge/DIA_SLD_809_Bennet_DI.d
@@ -1,5 +1,6 @@
 
-instance DIA_Bennet_DI_EXIT(C_Info)
+
+instance DIA_Bennet_DI_EXIT (C_Info)
 {
 	npc = SLD_809_Bennet_DI;
 	nr = 999;
@@ -21,14 +22,14 @@ func void DIA_Bennet_DI_EXIT_Info()
 };
 
 
-instance DIA_Bennet_DI_Hello(C_Info)
+instance DIA_Bennet_DI_Hello (C_Info)
 {
 	npc = SLD_809_Bennet_DI;
 	nr = 10;
 	condition = DIA_Bennet_DI_Hello_Condition;
 	information = DIA_Bennet_DI_Hello_Info;
 	permanent = TRUE;
-	description = "У тебя все в порядке?";
+	description = " Are you all right? " ;
 };
 
 
@@ -42,20 +43,20 @@ func int DIA_Bennet_DI_Hello_Condition()
 
 func void DIA_Bennet_DI_Hello_Info()
 {
-	AI_Output(other,self,"DIA_Bennet_DI_Hello_15_00");	//У тебя все в порядке?
+	AI_Output(other,self, " DIA_Bennet_DI_Hello_15_00 " );	// Are you okay?
 
 	if(ORkSturmDI == FALSE)
 	{
-		AI_Output(self,other,"DIA_Bennet_DI_Hello_06_01");	//Кузница на корабле немного проржавела. Морская соль разъедает ее. Здесь будет непросто выковать что-нибудь приличное. Ну, а кроме этого...
+		AI_Output(self,other, " DIA_Bennet_DI_Hello_06_01 " );	// The forge on the ship is a bit rusty. Sea salt corrodes it. It won't be easy to forge something decent here. Well, other than that...
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_Bennet_DI_Hello_06_02");	//Да. Если только орки не вернутся.
+		AI_Output(self,other, " DIA_Bennet_DI_Hello_06_02 " );	// Yes. Unless the orcs come back.
 	};
 };
 
 
-instance DIA_Bennet_DI_TRADE(C_Info)
+instance DIA_Bennet_DI_TRADE (C_Info)
 {
 	npc = SLD_809_Bennet_DI;
 	nr = 7;
@@ -63,7 +64,7 @@ instance DIA_Bennet_DI_TRADE(C_Info)
 	information = DIA_Bennet_DI_TRADE_Info;
 	permanent = TRUE;
 	trade = TRUE;
-	description = "Какое оружие ты можешь продать мне?";
+	description = " What weapon can you sell me? " ;
 };
 
 
@@ -83,20 +84,20 @@ func void DIA_Bennet_DI_TRADE_Info()
 		AI_TurnToNPC(self,other);
 	};
 
-	AI_Output(other,self,"DIA_Bennet_DI_TRADE_15_00");	//Какое оружие ты можешь мне продать?
-	AI_Output(self,other,"DIA_Bennet_DI_TRADE_06_01");	//Сам знаешь - только лучшее.
+	AI_Output(other,self, " DIA_Bennet_DI_TRADE_15_00 " );	// What weapon can you sell me?
+	AI_Output(self,other, " DIA_Bennet_DI_TRADE_06_01 " );	// You know - only the best.
 	B_GiveTradeInv(self);
 };
 
 
-instance DIA_Bennet_DI_Smith(C_Info)
+instance DIA_Bennet_DI_Smith (C_Info)
 {
 	npc = SLD_809_Bennet_DI;
 	nr = 7;
 	condition = DIA_Bennet_DI_Smith_Condition;
 	information = DIA_Bennet_DI_Smith_Info;
 	permanent = TRUE;
-	description = "Ты сможешь научить меня ковать оружие? ";
+	description = " Can you teach me how to forge weapons? " ;
 };
 
 func int DIA_Bennet_DI_Smith_Condition()
@@ -109,8 +110,8 @@ func int DIA_Bennet_DI_Smith_Condition()
 
 func void DIA_Bennet_DI_Smith_Info()
 {
-	AI_Output(other,self,"DIA_Bennet_DI_Smith_15_00");	//Ты можешь научить меня ковальному делу?
-	AI_Output(self,other,"DIA_Bennet_DI_Smith_06_01");	//Зависит от того, что ты хочешь уметь.
+	AI_Output(other,self, " DIA_Bennet_DI_Smith_15_00 " );	// Can you teach me how to forge?
+	AI_Output(self,other, " DIA_Bennet_DI_Smith_06_01 " );	// Depends on what you want to be able to do.
 	Info_ClearChoices(DIA_Bennet_DI_Smith);
 	Info_AddChoice(DIA_Bennet_DI_Smith,Dialog_Back,DIA_Bennet_DI_Smith_BACK);
 
@@ -213,7 +214,7 @@ instance DIA_Bennet_TeachSTR(C_Info)
 	condition = DIA_Bennet_TeachSTR_Condition;
 	information = DIA_Bennet_TeachSTR_Info;
 	permanent = TRUE;
-	description = "Я хочу стать сильнее.";
+	description = " I want to get stronger. " ;
 };
 
 func int DIA_Bennet_TeachSTR_Condition()
@@ -226,8 +227,8 @@ func int DIA_Bennet_TeachSTR_Condition()
 
 func void DIA_Bennet_TeachSTR_Info()
 {
-	AI_Output(other,self,"DIA_Bennet_TeachSTR_15_00");	//Я хочу стать сильнее.
-	AI_Output(self,other,"DIA_Bennet_TeachSTR_06_01");	//В наше время твердая рука никогда не помешает.
+	AI_Output(other,self, " DIA_Bennet_TeachSTR_15_00 " );	// I want to get stronger.
+	AI_Output(self,other, " DIA_Bennet_TeachSTR_06_01 " );	// These days, a firm hand never hurts.
 	Info_ClearChoices(DIA_Bennet_TeachSTR);
 	Info_AddChoice(DIA_Bennet_TeachSTR,Dialog_Back,DIA_Bennet_TeachSTR_Back);
 	Info_AddChoice(DIA_Bennet_TeachSTR,b_buildlearnstringforskills(PRINT_LearnSTR1,B_GetLearnCostAttribute(other,ATR_STRENGTH)),DIA_Bennet_TeachSTR_STR_1);
@@ -252,14 +253,14 @@ func void DIA_Bennet_TeachSTR_STR_5()
 };
 
 
-instance DIA_Bennet_DI_DragonEgg(C_Info)
+instance DIA_Bennet_DI_DragonEgg (C_Info)
 {
 	npc = SLD_809_Bennet_DI;
 	nr = 99;
 	condition = DIA_Bennet_DI_DragonEgg_Condition;
 	information = DIA_Bennet_DI_DragonEgg_Info;
 	permanent = FALSE;
-	description = "У меня есть драконье яйцо.";
+	description = " I have a dragon egg. " ;
 };
 
 func int DIA_Bennet_DI_DragonEgg_Condition()
@@ -272,10 +273,10 @@ func int DIA_Bennet_DI_DragonEgg_Condition()
 
 func void DIA_Bennet_DI_DragonEgg_Info()
 {
-	AI_Output(other,self,"DIA_Bennet_DI_DragonEgg_15_00");	//У меня есть драконье яйцо.
+	AI_Output(other,self, " DIA_Bennet_DI_DragonEgg_15_00 " );	// I have a dragon egg.
 	AI_Output(self,other,"DIA_Bennet_DI_DragonEgg_06_01");	//И?
-	AI_Output(other,self,"DIA_Bennet_DI_DragonEgg_15_02");	//Ну. Я подумал...
-	AI_Output(self,other,"DIA_Bennet_DI_DragonEgg_06_03");	//Я знаю, о чем ты думаешь. Забудь об этом и оставь яйцо себе. Мне оно не нужно.
+	AI_Output(other,self, " DIA_Bennet_DI_DragonEgg_15_02 " );	// Well. I thought...
+	AI_Output(self,other, " DIA_Bennet_DI_DragonEgg_06_03 " );	// I know what you're thinking. Forget about it and keep the egg for yourself. I don't need it.
 	B_GivePlayerXP(XP_Ambient);
 };
 
@@ -287,7 +288,7 @@ instance DIA_Bennet_DI_UndeadDragonDead(C_Info)
 	condition = DIA_Bennet_DI_UndeadDragonDead_Condition;
 	information = DIA_Bennet_DI_UndeadDragonDead_Info;
 	permanent = FALSE;
-	description = "Мы сделали все, что было нужно сделать.";
+	description = " We've done everything that needed to be done. " ;
 };
 
 func int DIA_Bennet_DI_UndeadDragonDead_Condition()
@@ -300,13 +301,13 @@ func int DIA_Bennet_DI_UndeadDragonDead_Condition()
 
 func void DIA_Bennet_DI_UndeadDragonDead_Info()
 {
-	AI_Output(other,self,"DIA_Bennet_DI_UndeadDragonDead_15_00");	//Мы сделали все, что было нужно сделать.
-	AI_Output(self,other,"DIA_Bennet_DI_UndeadDragonDead_06_01");	//Рад слышать это. Мне уже успела надоесть эта старая кузница. Я хочу опять стоять на твердой земле.
+	AI_Output(other,self, " DIA_Bennet_DI_UndeadDragonDead_15_00 " );	// We've done everything that needed to be done.
+	AI_Output(self,other, " DIA_Bennet_DI_UndeadDragonDead_06_01 " );	// Glad to hear it. I'm already tired of this old forge. I want to be on solid ground again.
 	AI_StopProcessInfos(self);
 };
 
 
-instance DIA_Bennet_DI_PICKPOCKET(C_Info)
+instances DIA_Bennet_DI_PICKPOCKET (C_Info)
 {
 	npc = SLD_809_Bennet_DI;
 	nr = 900;
@@ -319,19 +320,19 @@ instance DIA_Bennet_DI_PICKPOCKET(C_Info)
 
 func int DIA_Bennet_DI_PICKPOCKET_Condition()
 {
-	return C_Beklauen(35,65);
+	return  C_Robbery ( 35 , 65 );
 };
 
 func void DIA_Bennet_DI_PICKPOCKET_Info()
 {
 	Info_ClearChoices(DIA_Bennet_DI_PICKPOCKET);
 	Info_AddChoice(DIA_Bennet_DI_PICKPOCKET,Dialog_Back,DIA_Bennet_DI_PICKPOCKET_BACK);
-	Info_AddChoice(DIA_Bennet_DI_PICKPOCKET,DIALOG_PICKPOCKET,DIA_Bennet_DI_PICKPOCKET_DoIt);
+	Info_AddChoice(DIA_Bennet_DI_PICKPOCKET, DIALOG_PICKPOCKET ,DIA_Bennet_DI_PICKPOCKET_DoIt);
 };
 
 func void DIA_Bennet_DI_PICKPOCKET_DoIt()
 {
-	B_Beklauen();
+	B_Robbery();
 	Info_ClearChoices(DIA_Bennet_DI_PICKPOCKET);
 };
 
@@ -340,14 +341,14 @@ func void DIA_Bennet_DI_PICKPOCKET_BACK()
 	Info_ClearChoices(DIA_Bennet_DI_PICKPOCKET);
 };
 
-instance DIA_Bennet_DI_BetterArmor(C_Info)
+instance DIA_Bennet_DI_BetterArmor (C_Info)
 {
 	npc = SLD_809_Bennet_DI;
 	nr = 8;
 	condition = DIA_Bennet_DI_BetterArmor_Condition;
 	information = DIA_Bennet_DI_BetterArmor_Info;
 	permanent = FALSE;
-	description = "У тебя получше доспехов не найдется?";
+	description = " Do you have better armor? " ;
 };
 
 func int DIA_Bennet_DI_BetterArmor_Condition()
@@ -363,33 +364,33 @@ func int DIA_Bennet_DI_BetterArmor_Condition()
 
 func void DIA_Bennet_DI_BetterArmor_Info()
 {
-	AI_Output(other,self,"DIA_Bennet_DI_BetterArmor_01_00");	//У тебя получше доспехов не найдется?
-	AI_Output(self,other,"DIA_Bennet_DI_BetterArmor_01_01");	//Хммм...(задумчиво) Я могу сковать для тебя самые лучшие доспехи из тех, которые тебе только доводилось видеть.
-	AI_Output(self,other,"DIA_Bennet_DI_BetterArmor_01_02");	//Однако для их создания потребуется огромное количество разнообразного материала.
-	AI_Output(other,self,"DIA_Bennet_DI_BetterArmor_01_03");	//Какого именно?
-	AI_Output(self,other,"DIA_Bennet_DI_BetterArmor_01_04");	//Для начала это полсотни драконьих чешуек! Они послужат основой для создания нагрудника верхней части доспеха и ножных щитков.
-	AI_Output(self,other,"DIA_Bennet_DI_BetterArmor_01_05");	//Далее, чтобы придать доспеху надлежащую прочность, его придется покрыть магической рудой. А это как минимум два десятка кусков!
-	AI_Output(self,other,"DIA_Bennet_DI_BetterArmor_01_06");	//Так же в работу пойдут десять кусков серы, пять черных жемчужин, два горшка смолы и самое главное - четыре куска лобовой драконьей кости.
-	AI_Output(self,other,"DIA_Bennet_DI_BetterArmor_01_07");	//Если сможешь достать все это, будет тебе самый прочный доспех на всем белом свете!
+	AI_Output(other,self, " DIA_Bennet_DI_BetterArmor_01_00 " );	// Do you have better armor?
+	AI_Output(self,other, " DIA_Bennet_DI_BetterArmor_01_01 " );	// Hmmm...(thoughtfully) I can forge for you the finest armor you've ever seen.
+	AI_Output(self,other, " DIA_Bennet_DI_BetterArmor_01_02 " );	// However, they require a huge variety of material to create.
+	AI_Output(other,self, " DIA_Bennet_DI_BetterArmor_01_03 " );	// Which one exactly?
+	AI_Output(self,other, " DIA_Bennet_DI_BetterArmor_01_04 " );	// For starters, that's fifty dragon scales! They will serve as the basis for creating the breastplate of the upper part of the armor and leg guards.
+	AI_Output(self,other, " DIA_Bennet_DI_BetterArmor_01_05 " );	// Further, in order to give the armor proper strength, it will have to be coated with magic ore. And this is at least two dozen pieces!
+	AI_Output(self,other, " DIA_Bennet_DI_BetterArmor_01_06 " );	// Also, ten pieces of sulfur, five black pearls, two pots of resin and, most importantly, four pieces of the frontal dragon bone will go into work.
+	AI_Output(self,other, " DIA_Bennet_DI_BetterArmor_01_07 " );	// If you can get it all, you'll have the strongest armor in the whole wide world!
 	MIS_BetterArmor = LOG_Running;
 	Log_CreateTopic(TOPIC_BetterArmor,LOG_MISSION);
 	Log_SetTopicStatus(TOPIC_BetterArmor,LOG_Running);
-	B_LogEntry(TOPIC_BetterArmor,"По словам Беннета, он сможет изготовить для меня доспехи исключительной прочности! Однако для этого ему понадобится огромное количество разнообразного материала, а именно - полсотни драконьих чешуек, два десятка кусков магической руды, десять кусков серы, пять черных жемчужин, два горшка смолы и самое главное - четыре куска лобовой драконьей кости. Если я достану ему все это - он сделает мне эти доспехи бесплатно.");
+	B_LogEntry(TOPIC_BetterArmor, " According to Bennett, he will be able to make armor of exceptional strength for me! However, for this he will need a huge amount of various materials, namely, fifty dragon scales, two dozen pieces of magic ore, ten pieces of sulfur, five black pearls, two a pot of resin and, most importantly, four pieces of a frontal dragon bone. If I get all this for him, he will make this armor for me for free. " );
 };
 
-instance DIA_Bennet_DI_BetterArmor_Done(C_Info)
+instance DIA_Bennet_DI_BetterArmor_Done (C_Info)
 {
 	npc = SLD_809_Bennet_DI;
 	nr = 8;
 	condition = DIA_Bennet_DI_BetterArmor_Done_Condition;
 	information = DIA_Bennet_DI_BetterArmor_Done_Info;
 	permanent = FALSE;
-	description = "У меня есть все, что ты просил.";
+	description = " I have everything you asked for. " ;
 };
 
 func int DIA_Bennet_DI_BetterArmor_Done_Condition()
 {
-	if((MIS_BetterArmor == LOG_Running) && (DayStartArmor == FALSE) && (Npc_HasItems(other,ItAt_DragonScale) >= 50) && (Npc_HasItems(other,ItAt_XragonSkull) >= 4) && (Npc_HasItems(other,ItMi_Nugget) >= 20) && (Npc_HasItems(other,ItMi_Sulfur) >= 10) && (Npc_HasItems(other,ItMi_Pitch) >= 2) && (Npc_HasItems(other,ItMi_DarkPearl) >= 5))
+	if ((MY_BetterArmor == LOG_Running) && (DayStartArmor ==  FALSE ) && (Npc_HasItems(other,It's_DragonScale) >=  50 ) && (Npc_HasItems(other,It's_XragonSkull) >=  4 ) && (Npc_HasItems(other,It's_XragonSkull) >= 20 ) && (Npc_HasItems(other,It's_DragonSkull) >=  20 ) ) && (Npc_HasItems(other,ItMi_Sulfur) >=  10 ) && (Npc_HasItems(other,ItMi_Pitch) >=  2 ) && (Npc_HasItems(other,ItMi_DarkPearl) >=  5 ))
 	{
 		return TRUE;
 	};
@@ -398,7 +399,7 @@ func int DIA_Bennet_DI_BetterArmor_Done_Condition()
 func void DIA_Bennet_DI_BetterArmor_Done_Info()
 {
 	B_GivePlayerXP(2500);
-	AI_Output(other,self,"DIA_Bennet_DI_BetterArmor_Done_01_00");	//У меня есть все, что ты просил.
+	AI_Output(other,self, " DIA_Bennet_DI_BetterArmor_Done_01_00 " );	// I have everything you asked for.
 	B_GiveInvItemsManyThings(other,self);
 	Npc_RemoveInvItems(hero,ItAt_DragonScale,50);
 	Npc_RemoveInvItems(hero,ItAt_XragonSkull,4);
@@ -417,13 +418,13 @@ func void DIA_Bennet_DI_BetterArmor_Done_Info()
 		};
 	};
 
-	AI_Output(self,other,"DIA_Bennet_DI_BetterArmor_Done_01_01");	//Хорошо...(одобрительно) Тогда я немедленно принимаюсь за работу.
-	AI_Output(other,self,"DIA_Bennet_DI_BetterArmor_Done_01_02");	//Сколько времени тебе надо, чтобы их изготовить?
-	AI_Output(self,other,"DIA_Bennet_DI_BetterArmor_Done_01_03");	//Трудно сказать! Но я дам тебе знать, как только они будут готовы.
+	AI_Output(self,other, " DIA_Bennet_DI_BetterArmor_Done_01_01 " );	// Okay... (approvingly) Then I'll get to work right away.
+	AI_Output(other,self, " DIA_Bennet_DI_BetterArmor_Done_01_02 " );	// How long does it take you to make them?
+	AI_Output(self,other, " DIA_Bennet_DI_BetterArmor_Done_01_03 " );	// Hard to say! But I'll let you know as soon as they're ready.
 	AI_Output(other,self,"DIA_Bennet_DI_BetterArmor_Done_01_04");	//Хорошо.
 	DayStartArmor = TRUE;
 	DayStartArmorCount = Wld_GetDay();
-	B_LogEntry(TOPIC_BetterArmor,"Я отдал Беннету все необходимые материалы для доспехов и он немедленно принялся за работу. Он не знает сколько времени займет их изготовление.");
+	B_LogEntry(TOPIC_BetterArmor, " I gave Bennett all the materials he needed for the armor and he immediately got to work. He doesn't know how long it will take. " );
 	AI_StopProcessInfos(self);
 };
 
@@ -439,7 +440,7 @@ instance DIA_Bennet_DI_BetterArmor_Finish(C_Info)
 
 func int DIA_Bennet_DI_BetterArmor_Finish_Condition()
 {
-	var int daynow;
+	where int daynow;
 
 	daynow = Wld_GetDay();
 
@@ -452,11 +453,11 @@ func int DIA_Bennet_DI_BetterArmor_Finish_Condition()
 func void DIA_Bennet_DI_BetterArmor_Finish_Info()
 {
 	B_GivePlayerXP(500);
-	AI_Output(self,other,"DIA_Bennet_DI_BetterArmor_Finish_01_00");	//Насчет твоих доспехов...(устало) Они готовы!
-	AI_Output(self,other,"DIA_Bennet_DI_BetterArmor_Finish_01_01");	//Вот, возьми их. Они надежно защитят тебя от любого врага.
+	AI_Output(self,other, " DIA_Bennet_DI_BetterArmor_Finish_01_00 " );	// About your armor...(wearily) They're ready!
+	AI_Output(self,other, " DIA_Bennet_DI_BetterArmor_Finish_01_01 " );	// Here, take them. They will reliably protect you from any enemy.
 	B_GiveInvItems(self,other,ItAr_DragonKiller,1);
 	MIS_BetterArmor = LOG_SUCCESS;
 	Log_SetTopicStatus(TOPIC_BetterArmor,LOG_SUCCESS);
-	B_LogEntry(TOPIC_BetterArmor,"Беннет выковал доспехи и отдал их мне.");
+	B_LogEntry(TOPIC_BetterArmor, " Bennet forged the armor and gave it to me. " );
 	AI_StopProcessInfos(self);
 };
