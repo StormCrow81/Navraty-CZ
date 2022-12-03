7ef01e7865947f5ad2f502db8500bec0336a5ff9
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 18:02:12 +0200

    Update DIA_SLD_820_Soeldner.d

--- RU-Main/Story/Dialoge/DIA_SLD_820_Soeldner.d
+++ RU-Main/Story/Dialoge/DIA_SLD_820_Soeldner.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_Sld_820_EXIT(C_Info)
 {
 	npc = SLD_820_Soeldner;
@@ -42,41 +43,41 @@ func int DIA_Sld_820_Halt_Condition()
 
 func void DIA_Sld_820_Halt_Info()
 {
-	AI_Output(self,other,"DIA_Sld_820_Halt_07_00");	//И куда ты идешь?
-	AI_Output(other,self,"DIA_Sld_820_Halt_15_01");	//В дом, конечно же.
-	AI_Output(self,other,"DIA_Sld_820_Halt_07_02");	//Онар платит нам за то, чтобы мы не пускали в дом парней вроде тебя!
+	AI_Output(self,other, " DIA_Sld_820_Halt_07_00 " );	// And where are you going?
+	AI_Output(other,self, " DIA_Sld_820_Halt_15_01 " );	// To the house, of course.
+	AI_Output(self,other, " DIA_Sld_820_Halt_07_02 " );	// Onar is paying us to keep guys like you out of the house!
 	if(other.guild == GIL_MIL)
 	{
-		AI_Output(self,other,"DIA_Sld_820_Halt_07_03");	//Здесь не место ополчению! Он именно так и сказал.
+		AI_Output(self,other, " DIA_Sld_820_Halt_07_03 " );	// This is no place for militia! He said exactly that.
 	};
-	AI_Output(other,self,"DIA_Sld_820_Halt_15_04");	//Я хочу поговорить с Ли!
-	AI_Output(self,other,"DIA_Sld_820_Halt_07_05");	//Что тебе нужно от него?
+	AI_Output(other,self, " DIA_Sld_820_Halt_15_04 " );	// I want to talk to Lee!
+	AI_Output(self,other, " DIA_Sld_820_Halt_07_05 " );	// What do you need from him?
 	Info_ClearChoices(DIA_Sld_820_Halt);
-	Info_AddChoice(DIA_Sld_820_Halt,"Он и я давние знакомые...",DIA_Sld_820_Halt_KenneLee);
+	Info_AddChoice(DIA_Sld_820_Halt, " He and I are old friends... " ,DIA_Sld_820_Halt_KenneLee);
 	if(other.guild == GIL_NONE)
 	{
-		Info_AddChoice(DIA_Sld_820_Halt,"Я хочу присоединиться к наемникам!",DIA_Sld_820_Halt_WannaJoin);
+		Info_AddChoice(DIA_Sld_820_Halt, " I want to join the mercenaries! " ,DIA_Sld_820_Halt_WannaJoin);
 	};
 };
 
 func void B_Sld_820_LeeIsRight()
 {
-	AI_Output(self,other,"B_Sld_820_LeeIsRight_07_00");	//Ли в правом крыле. И даже не пытайся заглянуть куда-либо еще!
+	AI_Output(self,other, " B_Sld_820_LeeIsRight_07_00 " );	// Lee in the right wing. And don't even try to look anywhere else!
 };
 
 func void DIA_Sld_820_Halt_WannaJoin()
 {
-	AI_Output(other,self,"DIA_Sld_820_Halt_WannaJoin_15_00");	//Я хочу присоединиться к наемникам!
-	AI_Output(self,other,"DIA_Sld_820_Halt_WannaJoin_07_01");	//Ах, свежее пушечное мясо! Добро пожаловать.
+	AI_Output(other,self, " DIA_Sld_820_Halt_WannaJoin_15_00 " );	// I want to join the mercenaries!
+	AI_Output(self,other, " DIA_Sld_820_Halt_WannaJoin_07_01 " );	// Ah, fresh cannon fodder! Welcome.
 	B_Sld_820_LeeIsRight();
-	AI_Output(self,other,"DIA_Sld_820_Halt_WannaJoin_07_02");	//Но только предупреждаю: оставь Онара в покое! Он очень не любит, когда с ним заговаривают посторонние. Особенно парни вроде тебя.
+	AI_Output(self,other, " DIA_Sld_820_Halt_WannaJoin_07_02 " );	// But just a warning: leave Onar alone! He really doesn't like it when strangers talk to him. Especially guys like you.
 	AI_StopProcessInfos(self);
 };
 
 func void DIA_Sld_820_Halt_KenneLee()
 {
-	AI_Output(other,self,"DIA_Sld_820_Halt_KenneLee_15_00");	//Он и я давние знакомые...
-	AI_Output(self,other,"DIA_Sld_820_Halt_KenneLee_07_01");	//ТЫ приятель Ли? Я не верю в это! Но все равно, проходи, и если он не узнает тебя, тогда держись... (смеется)
+	AI_Output(other,self, " DIA_Sld_820_Halt_KenneLee_15_00 " );	// He and I are old acquaintances...
+	AI_Output(self,other, " DIA_Sld_820_Halt_KenneLee_07_01 " );	// Are you Lee's buddy? I do not believe in that! But anyway, come on in, and if he doesn't recognize you, then hold on... (laughs)
 	B_Sld_820_LeeIsRight();
 	AI_StopProcessInfos(self);
 };
@@ -89,7 +90,7 @@ instance DIA_Sld_820_PERM(C_Info)
 	condition = DIA_Sld_820_PERM_Condition;
 	information = DIA_Sld_820_PERM_Info;
 	permanent = TRUE;
-	description = "Как дела?";
+	description = " How are you? " ;
 };
 
 
@@ -101,7 +102,7 @@ func int DIA_Sld_820_PERM_Condition()
 func void DIA_Sld_820_PERM_Info()
 {
 	AI_Output(other,self,"DIA_Sld_820_PERM_15_00");	//Как дела?
-	AI_Output(self,other,"DIA_Sld_820_PERM_07_01");	//Проходи, если хочешь, но не надо трепаться здесь.
+	AI_Output(self,other, " DIA_Sld_820_PERM_07_01 " );	// Come in if you want, but don't talk here.
 	AI_StopProcessInfos(self);
 };
 
