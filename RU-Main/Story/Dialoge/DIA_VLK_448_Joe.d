faf8ee095717fbbc1e9e1edd29591a0ad5dd5989
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 23:31:58 +0200

    Update DIA_VLK_448_Joe.d

--- RU-Main/Story/Dialoge/DIA_VLK_448_Joe.d
+++ RU-Main/Story/Dialoge/DIA_VLK_448_Joe.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_Joe_EXIT(C_Info)
 {
 	npc = VLK_448_Joe;
@@ -34,7 +35,7 @@ instance DIA_Joe_PICKPOCKET(C_Info)
 
 func int DIA_Joe_PICKPOCKET_Condition()
 {
-	return C_Beklauen(25,25);
+	return  C_Robbery ( 25 , 25 );
 };
 
 func void DIA_Joe_PICKPOCKET_Info()
@@ -78,12 +79,12 @@ func void DIA_Joe_PICKPOCKET_BACK()
 };
 
 
-instance DIA_Joe_Hallo(C_Info)
+instance DIA_Joe_Hello (C_Info)
 {
 	npc = VLK_448_Joe;
 	nr = 2;
 	condition = DIA_Joe_Hallo_Condition;
-	information = DIA_Joe_Hallo_Info;
+	information = DIA_Joe_Hello_Info;
 	permanent = FALSE;
 	important = TRUE;
 };
@@ -99,14 +100,14 @@ func int DIA_Joe_Hallo_Condition()
 
 func void DIA_Joe_Hallo_Info()
 {
-	AI_Output(self,other,"DIA_Joe_Hallo_10_00");	//Эй, спасибо, парень. Действительно, огромное спасибо. Я уж думал, мне никогда не выбраться отсюда...
+	AI_Output(self,other, " DIA_Joe_Hallo_10_00 " );	// Hey, thanks man. Indeed, many thanks. I thought I'd never get out of here...
 	TOPIC_END_Joe = TRUE;
 	B_GivePlayerXP(XP_Ambient * 4);
-	AI_Output(other,self,"DIA_Joe_Hallo_15_01");	//Что ты делаешь ЗДЕСЬ?
-	AI_Output(self,other,"DIA_Joe_Hallo_10_02");	//Я оказался взаперти. Дверь была открыта, и я хотел просто пошарить там - но как только я зашел внутрь, пришел стражник и запер эту чертову дверь.
-	AI_Output(self,other,"DIA_Joe_Hallo_10_03");	//Я попал в дурацкое положение - и был бы очень благодарен, если бы все это осталось между нами.
-	AI_Output(other,self,"DIA_Joe_Hallo_15_04");	//Понимаю. Да уж, тут действительно нечем гордиться.
-	AI_Output(self,other,"DIA_Joe_Hallo_10_05");	//Я сейчас был бы не прочь что-нибудь выпить.
+	AI_Output(other,self, " DIA_Joe_Hallo_15_01 " );	// What are you doing HERE?
+	AI_Output(self,other, " DIA_Joe_Hallo_10_02 " );	// I'm locked up. The door was open, and I just wanted to rummage around in there - but as soon as I went inside, a guard came and locked the damn door.
+	AI_Output(self,other, " DIA_Joe_Hallo_10_03 " );	// I'm in a stupid position - and would be very grateful if all this remained between us.
+	AI_Output(other,self, " DIA_Joe_Hallo_15_04 " );	// I understand. Yes, there really is nothing to be proud of.
+	AI_Output(self,other, " DIA_Joe_Hallo_10_05 " );	// I wouldn't mind a drink right now.
 	AI_StopProcessInfos(self);
 	Npc_ExchangeRoutine(self,"START");
 };
@@ -119,7 +120,7 @@ instance DIA_Joe_Perm(C_Info)
 	condition = DIA_Joe_Perm_Condition;
 	information = DIA_Joe_Perm_Info;
 	permanent = TRUE;
-	description = "Все в порядке?";
+	description = " Is everything okay? " ;
 };
 
 
@@ -133,8 +134,8 @@ func int DIA_Joe_Perm_Condition()
 
 func void DIA_Joe_Perm_Info()
 {
-	AI_Output(other,self,"DIA_Joe_Perm_15_00");	//Все в порядке?
-	AI_Output(self,other,"DIA_Joe_Perm_10_01");	//Спасибо, что освободил меня.
+	AI_Output(other,self, " DIA_Joe_Perm_15_00 " );	// Is everything okay?
+	AI_Output(self,other, " DIA_Joe_Perm_10_01 " );	// Thank you for freeing me.
 	AI_StopProcessInfos(self);
 };
 
@@ -146,7 +147,7 @@ instance DIA_Joe_Sign(C_Info)
 	condition = DIA_Joe_Sign_Condition;
 	information = DIA_Joe_Sign_Info;
 	permanent = FALSE;
-	description = "(Показать сигнал воров)";
+	description = " (Show thief alert) " ;
 };
 
 
@@ -161,8 +162,8 @@ func int DIA_Joe_Sign_Condition()
 func void DIA_Joe_Sign_Info()
 {
 	AI_PlayAni(other,"T_YES");
-	AI_Output(self,other,"DIA_Joe_Sign_10_00");	//Эй, привет - у нас есть общие друзья. И я хотел бы выразить свою благодарность за спасение.
-	AI_Output(self,other,"DIA_Joe_Sign_10_01");	//Вот, возьми эти отмычки - я уверен, они пригодятся тебе.
+	AI_Output(self,other, " DIA_Joe_Sign_10_00 " );	// Hey, hi - we have mutual friends. And I would like to express my gratitude for the salvation.
+	AI_Output(self,other, " DIA_Joe_Sign_10_01 " );	// Here, take these lock picks - I'm sure you'll need them.
 	B_GiveInvItems(self,other,ItKE_lockpick,5);
 	THIEF_REPUTATION = THIEF_REPUTATION + 1;
 	AI_StopProcessInfos(self);
@@ -176,7 +177,7 @@ instance DIA_JOE_INPRISOIN(C_Info)
 	condition = dia_joe_inprisoin_condition;
 	information = dia_joe_inprisoin_info;
 	permanent = TRUE;
-	description = "Говорят, ты пытался расплатиться кольцом.";
+	description = " They say you tried to pay with the ring. " ;
 };
 
 
@@ -190,49 +191,49 @@ func int dia_joe_inprisoin_condition()
 
 func void dia_joe_inprisoin_info()
 {
-	AI_Output(other,self,"DIA_Joe_InPrisoin_01_00");	//Говорят, ты пытался расплатиться кольцом.
-	AI_Output(self,other,"DIA_Joe_InPrisoin_01_01");	//Каким кольцом?!
-	if(Npc_HasItems(other,itri_hakonmissring) >= 1)
+	AI_Output(other,self, " DIA_Joe_InPrisoin_01_00 " );	// They say you tried to pay with the ring.
+	AI_Output(self,other, " DIA_Joe_InPrisoin_01_01 " );	// Which ring?!
+	if (Npc_HasItems(other,itri_hakonmissring) >=  1 )
 	{
 		B_GivePlayerXP(250);
-		AI_Output(other,self,"DIA_Joe_InPrisoin_01_02");	//Вот этим кольцом. Узнаешь?
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_03");	//Ну... ну да, я отдал его трактирщику Корагону за выпивку.
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_04");	//А что в этом такого?
-		AI_Output(other,self,"DIA_Joe_InPrisoin_01_05");	//Откуда оно у тебя?
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_06");	//(нервничает) Я... я его нашел.
-		AI_Output(other,self,"DIA_Joe_InPrisoin_01_07");	//Ты врешь! Может, все-таки скажешь правду?
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_08");	//Какую еще правду? Я ничего не знаю. Мамой клянусь!
-		AI_Output(other,self,"DIA_Joe_InPrisoin_01_09");	//Послушай, парень, это кольцо было украдено у одного торговца.
-		AI_Output(other,self,"DIA_Joe_InPrisoin_01_10");	//И если ты не скажешь мне сейчас, откуда оно у тебя, я просто сдам тебя городскому ополчению.
-		AI_Output(other,self,"DIA_Joe_InPrisoin_01_12");	//Говорят, в последнее время некоторые охранники очень любят пытать людей.
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_13");	//(в панике) Но... но я ничего не крал, честное слово! Я не крал этого кольца!
-		AI_Output(other,self,"DIA_Joe_InPrisoin_01_14");	//Тогда просто скажи, кто тебе его дал, - и я отпущу тебя.
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_15");	//Черт! Хорошо, я все скажу.
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_16");	//Мне его дал торговец по имени Гаспар.
+		AI_Output(other,self, " DIA_Joe_InPrisoin_01_02 " );	// With this ring. Do you recognize?
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_03 " );	// Well... well, yes, I gave it to the innkeeper Koragon for a drink.
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_04 " );	// What's wrong with that?
+		AI_Output(other,self, " DIA_Joe_InPrisoin_01_05 " );	// Where did you get it from?
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_06 " );	// (nervous) I... I found him.
+		AI_Output(other,self, " DIA_Joe_InPrisoin_01_07 " );	// You're lying! Can you still tell the truth?
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_08 " );	// What other truth? I do not know anything. I swear by mom!
+		AI_Output(other,self, " DIA_Joe_InPrisoin_01_09 " );	// Look, boy, this ring was stolen from a merchant.
+		AI_Output(other,self, " DIA_Joe_InPrisoin_01_10 " );	// And if you don't tell me now where you got it, I'll just turn you in to the city militia.
+		AI_Output(other,self, " DIA_Joe_InPrisoin_01_12 " );	// Some guards are said to be very fond of torturing people these days.
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_13 " );	// (panicked) But... but I didn't steal anything, I swear! I didn't steal that ring!
+		AI_Output(other,self, " DIA_Joe_InPrisoin_01_14 " );	// Then just tell me who gave it to you, and I'll let you go.
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_15 " );	// Damn! Okay, I'll tell you everything.
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_16 " );	// A trader named Gaspar gave it to me.
 		AI_Output(other,self,"DIA_Joe_InPrisoin_01_97");	//Зачем?
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_18");	//Ну, я... я выполнил для него одно поручение, и он дал мне это кольцо в качестве оплаты.
-		AI_Output(other,self,"DIA_Joe_InPrisoin_01_19");	//Что ты для него сделал?
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_20");	//Он сказал, что ему надо попасть в гостиницу, но он не хочет, чтобы Ханна - тамошняя хозяйка - видела его.
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_21");	//Еще он сказал, что тем самым, хочет сделать для нее один приятный сюрприз.
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_22");	//Поэтому и попросил меня на время отвлечь ее разговором.
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_23");	//Ну и пообещал, конечно, неплохо заплатить.
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_25");	//Я все сделал, как он велел. А потом он отдал мне это кольцо в качестве оплаты.
-		AI_Output(other,self,"DIA_Joe_InPrisoin_01_26");	//Все ясно. Вскоре мне понадобятся твои показания, чтобы посадить этого ублюдка в тюрьму.
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_27");	//Что?! Ты же обещал!
-		AI_Output(other,self,"DIA_Joe_InPrisoin_01_28");	//Ты меня не так понял - я не хочу тебя сдавать городскому ополчению.
-		AI_Output(other,self,"DIA_Joe_InPrisoin_01_29");	//Просто мне нужен свидетель, чтобы обвинить Гаспара в краже. Смекаешь?
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_30");	//Ну, я...(растерянно) хорошо, я сделаю это.
-		AI_Output(other,self,"DIA_Joe_InPrisoin_01_31");	//Вот и отлично! Можешь не волноваться, тебя не арестуют. Зато поможешь мне засадить этого ублюдка в тюрьму.
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_33");	//Я понимаю... можешь на меня рассчитывать.
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_18 " );	// Well, I... I did one errand for him, and he gave me this ring as payment.
+		AI_Output(other,self, " DIA_Joe_InPrisoin_01_19 " );	// What did you do for him?
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_20 " );	// He said he had to get to the inn, but he didn't want Hanna, the hostess there, to see him.
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_21 " );	// He also said that by doing so, he wants to make one pleasant surprise for her.
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_22 " );	// That's why he asked me to distract her with a conversation for a while.
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_23 " );	// Well, he promised, of course, to pay well.
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_25 " );	// I did everything as he said. And then he gave me this ring as payment.
+		AI_Output(other,self, " DIA_Joe_InPrisoin_01_26 " );	// Everything is clear. I'm going to need your testimony soon enough to put that bastard in jail.
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_27 " );	// What?! But you promised!
+		AI_Output(other,self, " DIA_Joe_InPrisoin_01_28 " );	// You misunderstood me - I don't want to hand you over to the city militia.
+		AI_Output(other,self, " DIA_Joe_InPrisoin_01_29 " );	// I just need a witness to accuse Gasper of stealing. Are you savvy?
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_30 " );	// Well, I...(bewildered) okay, I'll do it.
+		AI_Output(other,self, " DIA_Joe_InPrisoin_01_31 " );	// That's great! Don't worry, you won't be arrested. But you can help me put this bastard in jail.
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_33 " );	// I understand... you can count on me.
 		JOTELLSGASPAR = TRUE;
-		B_LogEntry(TOPIC_ABIGEILHELPHANNA,"Я поговорил с Джо и он мне все рассказал. Гаспар использовал его, чтобы отвлечь Ханну, пока сам пробрался в гостиницу и обворовал торговца Хакона. Потом он подбросил вещи Ханне, а кольцом расплатился с Джо. Теперь надо пойти и сообщить об этом лорду Андрэ. Думаю, после этого, он снимет все обвинения с Ханны.");
+		B_LogEntry( TOPIC_ABIGEILHELPHANNA , " I talked to Joe and he told me everything. Gaspard used it to distract Hannah while he sneaked into the inn and robbed Hakon the merchant. Then he planted things for Hannah and paid off Joe with the ring. Now we need to go and report about this to Lord Andre. I think after that, he will drop all charges against Hannah. " );
 	}
 	else
 	{
-		AI_Output(other,self,"DIA_Joe_InPrisoin_01_35");	//Какая разница, каким?
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_36");	//Не понимаю о чем ты, парень.
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_37");	//Я никому не давал никакого кольца.
-		AI_Output(self,other,"DIA_Joe_InPrisoin_01_38");	//Ты, наверное, ошибся. Так что оставь меня в покое.
+		AI_Output(other,self, " DIA_Joe_InPrisoin_01_35 " );	// What's the difference what?
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_36 " );	// I don't know what you mean, man.
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_37 " );	// I didn't give anyone any ring.
+		AI_Output(self,other, " DIA_Joe_InPrisoin_01_38 " );	// You must be wrong. So leave me alone.
 		AI_StopProcessInfos(self);
 	};
 };
