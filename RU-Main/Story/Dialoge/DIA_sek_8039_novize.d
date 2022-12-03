e81f3720c3e54ebffa3b52b153cc47c9f6cabdde
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-03 10:11:07 +0200

    Update DIA_sek_8039_novize.d

--- RU-Main/Story/Dialoge/DIA_sek_8039_novize.d
+++ RU-Main/Story/Dialoge/DIA_sek_8039_novize.d
@@ -1,9 +1,10 @@
 
-instance DIA_SEK_8039_NOVIZE_NOFOREVER(C_Info)
+
+instance DIA_SEK_8039_NOVIZE_NOFOREVER (C_Info)
 {
-	npc = sek_8039_novize;
+	npc = sec_8039_novice;
 	nr = 5;
-	condition = dia_sek_8039_novize_noforever_condition;
+	condition = dia_sek_8039_novice_noforever_condition;
 	information = dia_sek_8039_novize_noforever_info;
 	permanent = TRUE;
 	important = TRUE;
@@ -20,15 +21,15 @@ func int dia_sek_8039_novize_noforever_condition()
 
 func void dia_sek_8039_novize_noforever_info()
 {
-	if(Wld_IsTime(8,0,21,0))
+	if (Wld_IsTime( 8 , 0 , 21 , 0 ))
 	{
-		AI_Output(self,other,"DIA_SEK_8039_Novize_NoForever_01_00");	//Не отвлекай меня от работы!
-		AI_Output(self,other,"DIA_SEK_8039_Novize_NoForever_01_01");	//Если тебе что-то нужно - поговори с Ханисом. Он тут главный.
+		AI_Output(self,other, " DIA_SEK_8039_Novize_NoForever_01_00 " );	// Don't distract me from work!
+		AI_Output(self,other, " DIA_SEK_8039_Novize_NoForever_01_01 " );	// If you need something, talk to Hanis. He's in charge here.
 		AI_StopProcessInfos(self);
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_SEK_8039_Novize_NoForever_01_02");	//Отстань! Не мешай мне отдыхать...
+		AI_Output(self,other, " DIA_SEK_8039_Novize_NoForever_01_02 " );	// Back off! Don't let me rest...
 		AI_StopProcessInfos(self);
 	};
 };
