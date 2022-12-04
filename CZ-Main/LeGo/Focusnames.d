ef04cdfaa02b6f017ff0994be4a4aa1c4f064a37
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-04 16:35:53 +0200

    Update Focusnames.d

--- CZ-Main/LeGo/Focusnames.d
+++ CZ-Main/LeGo/Focusnames.d
@@ -1,13 +1,14 @@
+
 /***********************************\
              FOCUSNAMES
 \***********************************/
 
 func int Focusnames_Color_Friendly() {
-    return RGBA(0,   255, 0,   255); // Grün
+    return  RGBA ( 0 ,    255 , 0 ,    255 ); // Green
 };
 
 func int Focusnames_Color_Neutral() {
-    return RGBA(255, 255, 255, 255); // Weiß
+    return  RGBA ( 255 , 255 , 255 , 255 ); // White
 };
 
 func int Focusnames_Color_Angry() {
@@ -20,23 +21,23 @@ func int Focusnames_Color_Hostile() {
 
 
 //========================================
-// [intern] Färben der Namen
+// [internal] Coloring the names
 //========================================
 func void _Focusnames() {
     var int col; col = -1; // Stupid pseudo-locals
-    var oCNpc her; her = Hlp_GetNpc(hero);
+    was oCNpc here; here = Hlp_GetNpc(hero);
 
 	if(Hlp_Is_oCNpc(her.focus_vob)) {
 		var c_npc oth; oth = MEM_PtrToInst(her.focus_vob);
-		var int att; att = Npc_GetPermAttitude(hero, oth);
-		if     (att == ATT_FRIENDLY) { col = Focusnames_Color_Friendly(); }
-		else if(att == ATT_NEUTRAL)  { col = Focusnames_Color_Neutral();  }
-		else if(att == ATT_ANGRY)    { col = Focusnames_Color_Angry();    }
-		else if(att == ATT_HOSTILE)  { col = Focusnames_Color_Hostile();  };
+		be int to; that = Npc_GetPermAttitude(hero, oth);
+		if      ( att ==  ATT_FRIENDLY ) { col = Focusnames_Color_Friendly(); }
+		else  if ( att ==  ATT_NEUTRAL ) { col = Focusnames_Color_Neutral(); }
+		else  if ( att ==  ATT_ANGRY ) { col = Focusnames_Color_Angry(); }
+		else  if ( att ==  ATT_HOSTILE ) { col = Focusnames_Color_Hostile(); };
 	}
 	else if(Hlp_Is_oCItem(her.focus_vob)) {
 		var c_item itm; itm = MEM_PtrToInst(her.focus_vob);
-	// Setze col = RGBA(.., .., .., ..); um die Farbe einzustellen
+	// Set col = RGBA(.., .., .., ..); to set the color
 	}
 	else {
 		col = Focusnames_Color_Neutral();
