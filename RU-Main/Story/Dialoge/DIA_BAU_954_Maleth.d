da8907bfb90da7f0d364ec58748c68fe1ef376aa
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-04 21:18:30 +0200

    Update DIA_BAU_954_Maleth.d

--- RU-Main/Story/Dialoge/DIA_BAU_954_Maleth.d
+++ RU-Main/Story/Dialoge/DIA_BAU_954_Maleth.d
@@ -52,7 +52,7 @@ func void DIA_Maleth_Hallo_Info()
 			AI_Output(self,other, " DIA_Maleth_Hallo_08_01 " );	// I saw you come down from the mountains.
 		};
 		AI_Output(self,other, " DIA_Maleth_Hallo_08_02 " );	// You're lucky you didn't show up three weeks ago.
-		AI_Output(self,other, " DIA_Maleth_Hallo_08_03 " );	// We'd take you for a runaway convict. And we have a short conversation with them!
+		AI_Output(self,other, " DIA_Maleth_Hallo_08_03 " );	// We'd take you for a runaway convict. And we have very short conversations with them!
 		if(Npc_HasEquippedArmor(other) == FALSE)
 		{
 			AI_Output(self,other, " DIA_Maleth_Hallo_08_04 " );	// You look absolutely exhausted.
@@ -60,7 +60,7 @@ func void DIA_Maleth_Hallo_Info()
 		else
 		{
 			AI_Output(self,other, " DIA_Maleth_Hallo_08_05 " );	// You looked absolutely exhausted when you first showed up here.
-			AI_Output(self,other, " DIA_Maleth_Hallo_08_06 " );	// Well, now you look like a man!
+			AI_Output(self,other, " DIA_Maleth_Hallo_08_06 " );	// Now you look like a man again!
 		};
 	};
 	AI_Output(self,other, " DIA_Maleth_Hallo_08_07 " );	// What do you need here?
@@ -88,8 +88,8 @@ func int DIA_Maleth_BANDITS_Condition()
 func void DIA_Maleth_BANDITS_Info()
 {
 	AI_Output(other,self, " DIA_Maleth_BANDITS_15_00 " );	// I was attacked by bandits in the mountains.
-	AI_Output(self,other, " DIA_Maleth_BANDITS_08_01 " );	// That vile brat! It's probably the same bastards that stole our sheep last night!
-	AI_Output(self,other, " DIA_Maleth_BANDITS_08_02 " );	// You're still very lucky. Few people manage to get away from them alive.
+	AI_Output(self,other, " DIA_Maleth_BANDITS_08_01 " );	// Those vile bastardsIt's probably the same bastards that have been stealing our sheep!
+	AI_Output(self,other, " DIA_Maleth_BANDITS_08_02 " );	// You're very lucky. Few people manage to get away from them alive.
 };
 
 
@@ -116,12 +116,12 @@ func void DIA_Maleth_BanditsDEAD_Info()
 {
 	AI_Output(other,self, " DIA_Maleth_BanditsDEAD_15_00 " );	// These bandits won't bother you anymore...
 	AI_Output(self,other, " DIA_Maleth_BanditsDEAD_08_01 " );	// Why? Are they dead?
-	AI_Output(other,self, " DIA_Maleth_BanditsDEAD_15_02 " );	// They didn't attack that...
-	AI_Output(self,other, " DIA_Maleth_BanditsDEAD_08_03 " );	// Glory to Innos! It's a little here, but I want you to take it!
+	AI_Output(other,self, " DIA_Maleth_BanditsDEAD_15_02 " );	// Lets just say they didn't make it.
+	AI_Output(self,other, " DIA_Maleth_BanditsDEAD_08_03 " );	// Glory to Innos! It's very little, but I want you to take it!
 	B_GiveInvItems(self,other,ItFo_Wine,3);
 	MIS_Maleth_Bandits = LOG_SUCCESS;
 	B_GivePlayerXP(XP_MalethKillBandits);
-	AI_Output(self,other, " DIA_Maleth_BanditsDEAD_08_04 " );	// I'll tell the others about it!
+	AI_Output(self,other, " DIA_Maleth_BanditsDEAD_08_04 " );	// I'll tell the others about this!
 };
 
 
@@ -154,13 +154,13 @@ func int DIA_Maleth_BanditsALIVE_Condition()
 func void DIA_Maleth_BanditsALIVE_Info()
 {
 	AI_Output(other,self, " DIA_Maleth_BanditsALIVE_15_00 " );	// I know where these bandits are hiding...
-	AI_Output(self,other, " DIA_Maleth_BanditsALIVE_08_01 " );	// Do you want to attack them? I don't play these games! It's too dangerous!
+	AI_Output(self,other, " DIA_Maleth_BanditsALIVE_08_01 " );	// Do you want to attack them? I won't be joining you! It's too dangerous!
 	AI_Output(self,other, " DIA_Maleth_BanditsALIVE_08_02 " );	// Besides, I have to look after the sheep!
 	AI_Output(self,other, " DIA_Maleth_BanditsALIVE_08_03 " );	// But if you think you can rid us of these scum, everyone on this farm will be very grateful to you.
 	MIS_Maleth_Bandits = LOG_Running;
 	Log_CreateTopic(TOPIC_Maleth,LOG_MISSION);
 	Log_SetTopicStatus(TOPIC_Maleth,LOG_Running);
-	B_LogEntry(TOPIC_Maleth, " If I destroy the bandits that settled halfway from Xardas' tower to Lobart's farm, all the inhabitants of the farm will be very grateful to me. " );
+	B_LogEntry(TOPIC_Maleth, " If I destroy the bandits that settled halfway from Xardas' tower to Lobart's farm, all the inhabitants of the farm will be grateful to me. " );
 };
 
 
@@ -188,14 +188,14 @@ func void DIA_Maleth_ToTheCity_Info()
 	if(Npc_HasEquippedArmor(other) == FALSE)
 	{
 		AI_Output(self,other, " DIA_Maleth_ToTheCity_08_01 " );	// Given the way you look, you'll have to bribe the guards to get into the city.
-		AI_Output(self,other, " DIA_Maleth_ToTheCity_08_02 " );	// And you should know what they want to hear.
+		AI_Output(self,other, " DIA_Maleth_ToTheCity_08_02 " );	// Or you could tell them what they want to hear.
 	}
 	else
 	{
 		AI_Output(self,other, " DIA_Maleth_ToTheCity_08_03 " );	// They can let you in - if you tell them what they want to hear...
 	};
-	AI_Output(other,self,"DIA_Maleth_ToTheCity_15_04");	//И что это?
-	AI_Output(self,other, " DIA_Maleth_ToTheCity_08_05 " );	// Well, for example, that you are from Lobart's farm and go to the city blacksmith.
+	AI_Output(other,self,"DIA_Maleth_ToTheCity_15_04");	//And that is?
+	AI_Output(self,other, " DIA_Maleth_ToTheCity_08_05 " );	// Well, for example, that you are from Lobart's farm and are going to the city blacksmith.
 	Log_CreateTopic(TOPIC_City,LOG_MISSION);
 	Log_SetTopicStatus(TOPIC_City,LOG_Running);
 	B_LogEntry(TOPIC_City, " To get into the city, I can tell the guards at the gate that I'm coming from Lobart's farm and want to see the blacksmith. " );
@@ -204,7 +204,7 @@ func void DIA_Maleth_ToTheCity_Info()
 		AI_Output(self,other, " DIA_Maleth_ToTheCity_08_06 " );	// But that won't help you. You don't look like a farmer.
 		B_LogEntry(TOPIC_City, " Of course I must look like a farmer. " );
 	};
-	AI_Output(other,self,"DIA_Maleth_ToTheCity_15_07");	//Понятно.
+	AI_Output(other,self,"DIA_Maleth_ToTheCity_15_07");	//It's clear.
 };
 
 
@@ -230,11 +230,11 @@ func int DIA_Maleth_Equipment_Condition()
 func void DIA_Maleth_Equipment_Info()
 {
 	AI_Output(other,self, " DIA_Maleth_Equipment_15_00 " );	// I need gear!
-	AI_Output(self,other, " DIA_Maleth_Equipment_08_01 " );	// I can imagine. But I'll be honest with you: we don't have anything to share!
+	AI_Output(self,other, " DIA_Maleth_Equipment_08_01 " );	// Obviously. But I'll be honest with you: we don't have anything to share!
 	if ( ! Npc_IsDead(Lobart))
 	{
 		AI_Output(self,other, " DIA_Maleth_Equipment_08_02 " );	// Although, if you can pay, Lobart will sell you something.
-		AI_Output(self,other, " DIA_Maleth_Equipment_08_03 " );	// Well, or you can go to him and ask if he has any work.
+		AI_Output(self,other, " DIA_Maleth_Equipment_08_03 " );	// Or you can go to him and ask if he has any work.
 	};
 };
 
@@ -262,7 +262,7 @@ func void DIA_Maleth_LOBART_Info()
 {
 	AI_Output(other,self, " DIA_Maleth_LOBART_15_00 " );	// Where can I find Lobart?
 	AI_Output(self,other, " DIA_Maleth_LOBART_08_01 " );	// On the farm, of course! It's his farm!
-	AI_Output(self,other, " DIA_Maleth_LOBART_08_02 " );	// And don't try to trick him! He has already beaten and thrown out of the farm many loafers.
+	AI_Output(self,other, " DIA_Maleth_LOBART_08_02 " );	// And don't try to trick him! He has little patience for loafers.
 };
 
 
@@ -313,48 +313,48 @@ func void DIA_Maleth_PROBLEME_Info()
 {
 	AI_Output(other,self, " DIA_Maleth_PROBLEME_15_00 " );	// Nothing new lately?
 	AI_Output(self,other, " DIA_Maleth_PROBLEME_08_01 " );	// Just follow the road to town.
-	AI_Output(self,other, " DIA_Maleth_PROBLEME_08_02 " );	// If you see a man in a black cassock, you will understand what's new.
+	AI_Output(self,other, " DIA_Maleth_PROBLEME_08_02 " );	// If you see a man in black, you will understand what's new.
 	Info_ClearChoices(DIA_Maleth_PROBLEME);
 	Info_AddChoice(DIA_Maleth_PROBLEME,Dialog_Back,DIA_Maleth_PROBLEME_Back);
 	Info_AddChoice(DIA_Maleth_PROBLEME, " How are your sheep? " ,DIA_Maleth_PROBLEME_schafe);
-	Info_AddChoice(DIA_Maleth_PROBLEME, " Have the people in black cassocks visited you yet? " ,DIA_Maleth_PROBLEME_beidir);
+	Info_AddChoice(DIA_Maleth_PROBLEME, " Have the people in black robes visited you yet? " ,DIA_Maleth_PROBLEME_beidir);
 };
 
 func void DIA_Maleth_PROBLEME_beidir()
 {
-	AI_Output(other,self, " DIA_Maleth_PROBLEME_beidir_15_00 " );	// People in black cassocks haven't visited you yet?
-	AI_Output(self,other, " DIA_Maleth_PROBLEME_beidir_08_01 " );	// Yes. They were here a few days ago. They look like they've risen from hell.
+	AI_Output(other,self, " DIA_Maleth_PROBLEME_beidir_15_00 " );	// People in black robes haven't visited you yet?
+	AI_Output(self,other, " DIA_Maleth_PROBLEME_beidir_08_01 " );	// Yes. They were here a few days ago. They look like they've risen from the void itself.
 };
 
 func void DIA_Maleth_PROBLEMS_sheep()
 {
 	AI_Output(other,self, " DIA_Maleth_PROBLEME_schafe_15_00 " );	// How are your sheep?
-	AI_Output(self,other, " DIA_Maleth_PROBLEME_schafe_08_01 " );	// (angrily) As if you're interested. You have your own problems.
-	Info_AddChoice(DIA_Maleth_PROBLEME, " My problems? What do you know about this? " ,DIA_Maleth_PROBLEME_schafe_probleme);
+	AI_Output(self,other, " DIA_Maleth_PROBLEME_schafe_08_01 " );	// As if you're interested. You have your own problems.
+	Info_AddChoice(DIA_Maleth_PROBLEME, " My problems? What do you mean? " ,DIA_Maleth_PROBLEME_schafe_probleme);
 };
 
 func void DIA_Maleth_PROBLEMS_sheep_problems()
 {
-	AI_Output(other,self, " DIA_Maleth_PROBLEME_schafe_probleme_15_00 " );	// My problems? What do you know about it?
+	AI_Output(other,self, " DIA_Maleth_PROBLEME_schafe_probleme_15_00 " );	// My problems? What do you mean?
 	if((hero.guild == GIL_MIL) || (hero.guild == GIL_PAL))
 	{
-		AI_Output(self,other, " DIA_Maleth_PROBLEME_schafe_probleme_08_01 " );	// (sharply) Are you in the city guard or not? Then do something about those black-robed bastards.
+		AI_Output(self,other, " DIA_Maleth_PROBLEME_schafe_probleme_08_01 " );	// Are you in the city guard or not? Then do something about those black-robed bastards.
 	};
 	if((hero.guild == GIL_SLD) || (hero.guild == GIL_DJG))
 	{
-		AI_Output(self,other, " DIA_Maleth_PROBLEME_schafe_probleme_08_02 " );	// (angrily) You dirty mercenaries only think about one thing: how to put other people's money in your pockets.
+		AI_Output(self,other, " DIA_Maleth_PROBLEME_schafe_probleme_08_02 " );	// You dirty mercenaries only think about one thing: how to put other people's money in your pockets.
 	};
 	if(hero.guild == GIL_KDF)
 	{
-		AI_Output(self,other, " DIA_Maleth_PROBLEME_schafe_probleme_08_03 " );	// You powerful mages from the monastery should do something about these guys in black cassocks.
+		AI_Output(self,other, " DIA_Maleth_PROBLEME_schafe_probleme_08_03 " );	// You Pyromancers from the monastery should do something about these strangers in black.
 	};
 	if(hero.guild == GIL_KDW)
 	{
-		AI_Output(self,other, " DIA_Maleth_PROBLEME_schafe_probleme_08_04 " );	// Your noble Waterbenders should do something against these guys in black cassocks.
+		AI_Output(self,other, " DIA_Maleth_PROBLEME_schafe_probleme_08_04 " );	// You noble Waterbenders should do something against these guys in black robes for us.
 	};
 	if(hero.guild == GIL_KDM)
 	{
-		AI_Output(self,other, " DIA_Maleth_PROBLEME_schafe_probleme_08_05 " );	// Since when did dark magicians become compassionate?
+		AI_Output(self,other, " DIA_Maleth_PROBLEME_schafe_probleme_08_05 " );	// Since when did dark Wizards become so compassionate?
 	};
 	if((other.guild == GIL_SEK) || (other.guild == GIL_TPL) || (other.guild == GIL_GUR))
 	{
@@ -378,14 +378,14 @@ func void DIA_Maleth_PROBLEME_sheep_problems_money_here_order()
 	AI_Output(self,other, " DIA_Maleth_PROBLEME_schafe_probleme_geldher_auftrag_08_04 " );	// Easy for you to say, it's not your staff that's gone.
 	Log_CreateTopic(TOPIC_MalethsGehstock,LOG_MISSION);
 	Log_SetTopicStatus(TOPIC_MalethsGehstock,LOG_Running);
-	B_LogEntry(TOPIC_MalethsGehstock, " Maleth got drunk and lost his staff. As far as I know him, he never strayed far from Lobart's farm. Maybe I'll find her somewhere nearby. " );
+	B_LogEntry(TOPIC_MalethsGehstock, " Maleth got drunk and lost his staff. As far as I know him, he never strayed far from Lobart's farm. Maybe I'll find it somewhere nearby. " );
 	Info_ClearChoices(DIA_Maleth_PROBLEME);
 };
 
 func void DIA_Maleth_PROBLEMS_sheep_problems_threatening()
 {
 	AI_Output(other,self, " DIA_Maleth_PROBLEME_schafe_probleme_drohen_15_00 " );	// Hey, take it easy on the turns, mate.
-	AI_Output(self,other, " DIA_Maleth_PROBLEME_schafe_probleme_drohen_08_01 " );	// All you can do is talk. It's best for you to leave.
+	AI_Output(self,other, " DIA_Maleth_PROBLEME_schafe_probleme_drohen_08_01 " );	// All you can do is talk. It's best if you leave.
 };
 
 func void DIA_Maleth_PROBLEME_Back()
@@ -414,7 +414,7 @@ func int DIA_Maleth_WALKING STOCK_Condition()
 
 func void DIA_Maleth_WALKING STOCK_Info()
 {
-	AI_Output(other,self, " DIA_Maleth_GEHSTOCK_15_00 " );	// I think it's your staff.
+	AI_Output(other,self, " DIA_Maleth_GEHSTOCK_15_00 " );	// I think this is your staff.
 	AI_Output(self,other, " DIA_Maleth_GEHSTOCK_08_01 " );	// This is incredible! I...
 	AI_Output(other,self, " DIA_Maleth_GEHSTOCK_15_02 " );	// Wait a minute. Pay for it first.
 	AI_Output(self,other, " DIA_Maleth_GEHSTOCK_08_03 " );	// But... I have almost nothing.
@@ -425,12 +425,12 @@ func void DIA_Maleth_WALKING STOCK_Info()
 	B_GivePlayerXP(XP_FoundMalethsGehstock);
 	Info_ClearChoices(DIA_Maleth_GEHSTOCK);
 	Info_AddChoice(DIA_Maleth_GEHSTOCK, " Sorry, I'm not interested. I want gold from you. " ,DIA_Maleth_GEHSTOCK_gold);
-	Info_AddChoice(DIA_Maleth_WALKING STOCK, " Хорошо. " ,DIA_Maleth_WALKING STOCK_ok);
+	Info_AddChoice(DIA_Maleth_WALKING STOCK, " Good. " ,DIA_Maleth_WALKING STOCK_ok);
 };
 
 func void DIA_Maleth_WALKING STOCK_ok()
 {
-	AI_Output(other,self,"DIA_Maleth_GEHSTOCK_ok_15_00");	//Хорошо.
+	AI_Output(other,self,"DIA_Maleth_GEHSTOCK_ok_15_00");	//Good.
 	B_GiveInvItems(other,self,ItMw_MalethsGehstock_MIS,1);
 	AI_Output(self,other, " DIA_Maleth_GEHSTOCK_ok_08_01 " );	// Okay. Just go west to that forest over there. There you will see a gorge.
 	AI_Output(self,other, " DIA_Maleth_GEHSTOCK_ok_08_02 " );	// You'll probably find something in the cave below.
@@ -442,7 +442,7 @@ func void DIA_Maleth_WALKING STOCK_ok()
 
 func void DIA_Maleth_WALKING STOCK_gold()
 {
-	AI_Output(other,self, " DIA_Maleth_GEHSTOCK_gold_15_00 " );	// Sorry, I'm not interested. I want to get gold from you.
+	AI_Output(other,self, " DIA_Maleth_GEHSTOCK_gold_15_00 " );	// Sorry, I'm not interested. All I want is what's in your pockets.
 	AI_Output(self,other, " DIA_Maleth_GEHSTOCK_gold_08_01 " );	// That's all I have. This should be enough.
 	CreateInvItems(self,ItMi_Gold,35);
 	B_GiveInvItems(self,other,ItMi_Gold,35);
@@ -472,8 +472,8 @@ func int DIA_Maleth_PERM3_Condition()
 
 func void DIA_Maleth_PERM3_Info()
 {
-	AI_Output(other,self, " DIA_Maleth_PERM3_15_00 " );	// Look, don't overwork yourself.
-	AI_Output(self,other, " DIA_Maleth_PERM3_08_01 " );	// (angrily) Get out.
+	AI_Output(other,self, " DIA_Maleth_PERM3_15_00 " );	// Hey, don't overwork yourself.
+	AI_Output(self,other, " DIA_Maleth_PERM3_08_01 " );	// Get out!
 	AI_StopProcessInfos(self);
 };
 
