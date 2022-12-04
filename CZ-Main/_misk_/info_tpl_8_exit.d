25f369aadb976ae2aee5605d8975049d24980030
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-04 09:21:05 +0200

    Update info_tpl_8_exit.d

--- CZ-Main/_misk_/info_tpl_8_exit.d
+++ CZ-Main/_misk_/info_tpl_8_exit.d
@@ -1,4 +1,5 @@
 
+
 instance INFO_TPL_8_EXIT(C_Info)
 {
 	nr = 999;
@@ -26,7 +27,7 @@ instance INFO_TPL_8_EINERVONEUCHWERDEN(C_Info)
 	condition = info_tpl_8_einervoneuchwerden_condition;
 	information = info_tpl_8_einervoneuchwerden_info;
 	permanent = 1;
-	description = "Sháníte dobré lidi?";
+	description = " Are you looking for good people? " ;
 };
 
 
@@ -40,33 +41,33 @@ func int info_tpl_8_einervoneuchwerden_condition()
 
 func void info_tpl_8_einervoneuchwerden_info()
 {
-	AI_Output(other,self,"Info_Tpl_8_EinerVonEuchWerden_15_00");	//Sháníte dobré lidi?
-	AI_Output(self,other,"Info_Tpl_8_EinerVonEuchWerden_08_01");	//Samozřejmě, dobří lidé jsou vždy potřeba. Ale musíš na sobě ještě hodně pracovat. Jen ti nejlepší to mohou dotáhnout až na templáře.
-	AI_Output(self,other,"Info_Tpl_8_EinerVonEuchWerden_08_02");	//To je nejvyšší čest pro následovníky Bratrstva.
+	AI_Output(other,self, " Info_Tpl_8_EinerVonEuchWerden_15_00 " );	// Are you finding good people?
+	AI_Output(self,other, " Info_Tpl_8_EinerVonEuchWerden_08_01 " );	// Of course, good people are always needed. But you still have to work on yourself a lot. Only the best can make it to the Templars.
+	AI_Output(self,other, " Info_Tpl_8_EinerVonEuchWerden_08_02 " );	// This is the highest honor for followers of the Brotherhood.
 };
 
 
-instance INFO_TPL_8_WICHTIGEPERSONEN(C_Info)
+instance INFO_TPL_8_IMPORTANT PEOPLE (C_Info)
 {
 	nr = 3;
-	condition = info_tpl_8_wichtigepersonen_condition;
-	information = info_tpl_8_wichtigepersonen_info;
+	condition = info_tpl_8_important people_condition;
+	information = info_tpl_8_important people_info;
 	permanent = 1;
-	description = "Kdo tu velí?";
+	description = " Who's in charge here? " ;
 };
 
 
-func int info_tpl_8_wichtigepersonen_condition()
+func int info_tpl_8_important_persons_condition()
 {
 	return TRUE;
 };
 
-func void info_tpl_8_wichtigepersonen_info()
+func void info_tpl_8_important_persons_info()
 {
-	AI_Output(other,self,"Info_Tpl_8_WichtigePersonen_15_00");	//Kdo tu velí?
-	AI_Output(self,other,"Info_Tpl_8_WichtigePersonen_08_01");	//Sloužíme Guru. To je naše posvátná povinnost.
-	AI_Output(other,self,"Info_Tpl_8_WichtigePersonen_15_02");	//A kdo jsou tihle Guru?
-	AI_Output(self,other,"Info_Tpl_8_WichtigePersonen_08_03");	//Nejvyšším Guru je Baal Orun. To jemu se zodpovídají ostatní Guru, stejně jako Gor Na Kosh, vůdce templářů.
+	AI_Output(other,self,"Info_Tpl_8_WichtigePersonen_15_00");	//Who leads you?
+	AI_Output(self,other, " Info_Tpl_8_WichtigePersonen_08_01 " );	// We serve the Guru. This is our sacred duty.
+	AI_Output(other,self, " Info_Tpl_8_WichtigePersonen_15_02 " );	// And who are these Gurus?
+	AI_Output(self,other, " Info_Tpl_8_WichtigePersonen_08_03 " );	// The Supreme Guru is Baal Orun. The other Gurus answer to him, as does Gor Na Kosh, the leader of the Templars.
 };
 
 
@@ -76,7 +77,7 @@ instance INFO_TPL_8_DASLAGER(C_Info)
 	condition = info_tpl_8_daslager_condition;
 	information = info_tpl_8_daslager_info;
 	permanent = 1;
-	description = "Co mi řekneš o táboře?";
+	description = " What will you tell me about the camp? " ;
 };
 
 
@@ -90,9 +91,9 @@ func int info_tpl_8_daslager_condition()
 
 func void info_tpl_8_daslager_info()
 {
-	AI_Output(other,self,"Info_Tpl_8_DasLager_15_00");	//Co mi řekneš o táboře?
-	AI_Output(self,other,"Info_Tpl_8_DasLager_08_01");	//Jsi tady nový, takže si prozatím můžeš chodit, kam se ti zlíbí. Boje v táboře jsou zakázané.
-	AI_Output(self,other,"Info_Tpl_8_DasLager_08_02");	//Pokoušej se neporušovat tahle pravidla. Také nás nevyrušuj při rozjímání.
+	AI_Output(other,self, " Info_Tpl_8_DasLager_15_00 " );	// What will you tell me about the camp?
+	AI_Output(self,other, " Info_Tpl_8_DasLager_08_01 " );	// You're new here, so for now you can go wherever you like. Fighting in the camp is prohibited.
+	AI_Output(self,other, " Info_Tpl_8_DasLager_08_02 " );	// Try not to break these rules. Also, don't disturb us during our meditation.
 };
 
 
@@ -110,21 +111,21 @@ func int info_tpl_8_dielage_condition()
 {
 	if((other.guild != GIL_GUR) && (other.guild != GIL_TPL) && (other.guild != GIL_SEK))
 	{
-		return 1;
+		return  1 ;
 	};
 };
 
 func void info_tpl_8_dielage_info()
 {
-	AI_Output(other,self,"Info_Tpl_8_DieLage_15_00");	//Jak se vede?
-	AI_Output(self,other,"Info_Tpl_8_DieLage_08_01");	//Varuji tě: neúctu tady nestrpím. Mluvíš s jedním z templářů.
+	AI_Output(other,self,"Info_Tpl_8_DieLage_15_00");	//How are you?
+	AI_Output(self,other, " Info_Tpl_8_DieLage_08_01 " );	// I warn you: I will not tolerate disrespect here. You are talking to one of the templars.
 };
 
 func void b_assignambientinfos_tpl_8(var C_Npc slf)
 {
 	info_tpl_8_exit.npc = Hlp_GetInstanceID(slf);
 	info_tpl_8_einervoneuchwerden.npc = Hlp_GetInstanceID(slf);
-	info_tpl_8_wichtigepersonen.npc = Hlp_GetInstanceID(slf);
+	info_tpl_8_wichigepeople.npc = Hlp_GetInstanceID(slf);
 	info_tpl_8_daslager.npc = Hlp_GetInstanceID(slf);
 	info_tpl_8_dielage.npc = Hlp_GetInstanceID(slf);
 };
