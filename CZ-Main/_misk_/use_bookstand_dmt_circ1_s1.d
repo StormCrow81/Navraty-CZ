6fbf93a38d23a500eea15fbf40fa91b52f6ac24b
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-04 09:44:59 +0200

    Update use_bookstand_dmt_circ1_s1.d

--- CZ-Main/_misk_/use_bookstand_dmt_circ1_s1.d
+++ CZ-Main/_misk_/use_bookstand_dmt_circ1_s1.d
@@ -1,4 +1,5 @@
 
+
 func void use_bookstand_dmt_circ1_s1()
 {
 	var C_Npc her;
@@ -13,23 +14,23 @@ func void use_bookstand_dmt_circ1_s1()
 		Doc_SetPage(nDocID,1,"Book_Brown_R.tga",0);
 		Doc_SetFont(nDocID,-1,FONT_Book);
 		Doc_SetMargins(nDocID,0,275,20,30,20,1);
-		Doc_PrintLine(nDocID,0,"PRVNÍ KRUH");
+		Doc_PrintLine(nDocID, 0 , " FIRST CIRCLE " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLines(nDocID,0,"Runy prvního kruhu a ingredience potřebné k jejich vytvoření:");
+		Doc_PrintLines(nDocID, 0 , " Runes of the first circle and ingredients needed to create them: " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLine(nDocID,0,"- Temný šíp -");
-		Doc_PrintLine(nDocID,0,"Černá perla");
+		Doc_PrintLine(nDocID, 0 , " - Dark Arrow - " );
+		Doc_PrintLine(nDocID, 0 , " Black Pearl " );
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLine(nDocID,0,"- Vyvolání kostlivce -");
-		Doc_PrintLine(nDocID,0,"Kost z kostlivce");
+		Doc_PrintLine(nDocID, 0 , " - Invoke skeleton - " );
+		Doc_PrintLine(nDocID, 0 , " Skeleton bone " );
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"- Posedlost -");
-		Doc_PrintLine(nDocID,0,"Kost z kostlivce");
+		Doc_PrintLine(nDocID, 0 , " Skeleton bone " );
 		Doc_PrintLine(nDocID,0,"Ektoplazma");
 		Doc_PrintLine(nDocID,0,"Síra");
-		Doc_PrintLine(nDocID,0,"Pryskyřice");
+		Doc_PrintLine(nDocID, 0 , " Resin " );
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
@@ -37,11 +38,11 @@ func void use_bookstand_dmt_circ1_s1()
 		Doc_PrintLine(nDocID,0,"");
 		Doc_SetMargins(nDocID,-1,30,20,275,20,1);
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Na vytvoření runy jsou potřeba všechny požadované ingredience.");
+		Doc_PrintLines(nDocID, 1 , " All the required ingredients are needed to create the rune. " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Ten, kdo chce runu vytvořit, musí znát magickou formuli a mít runový kámen, stejně jako i svitek příslušného kouzla.");
+		Doc_PrintLines(nDocID, 1 , " The one who wants to create a rune must know the magic formula and have the rune stone, as well as the scroll of the corresponding spell. " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Teprve pak může runu vytvořit na runovém stole.");
+		Doc_PrintLines(nDocID, 1 , " Only then can he create the rune on the rune table. " );
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
@@ -58,7 +59,7 @@ func void use_bookstand_dmt_circ1_s1()
 		}
 		else
 		{
-			rnd = Hlp_Random(100);
+			rnd = Hlp_Random( 100 );
 			if(rnd <= 30)
 			{
 				B_Say(self,self,"$NOTHINGNEW");
@@ -77,7 +78,7 @@ func void use_bookstand_dmt_circ1_s1()
 			ATR_INTELLECT += 1;
 			Npc_SetTalentSkill(self,NPC_TALENT_INTELLECT,ATR_INTELLECT);
 			AI_Print("Inteligence + 1");
-			BookBonus_131 = TRUE;
+			BookBonus_131 = TRUE ;
 		};
 	};
 };
@@ -96,32 +97,32 @@ func void use_bookstand_dmt_circ2_s1()
 		Doc_SetPage(nDocID,1,"Book_Brown_R.tga",0);
 		Doc_SetFont(nDocID,-1,FONT_Book);
 		Doc_SetMargins(nDocID,0,275,20,30,20,1);
-		Doc_PrintLine(nDocID,0,"DRUHÝ KRUH");
+		Doc_PrintLine(nDocID, 0 , " SECOND CIRCLE " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLines(nDocID,0,"Runy druhého kruhu a ingredience potřebné k jejich vytvoření:");
+		Doc_PrintLines(nDocID, 0 , " Runes of the second circle and ingredients needed to create them: " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLine(nDocID,0,"- Rozervání duše -");
+		Doc_PrintLine(nDocID, 0 , " - Tearing the Soul - " );
 		Doc_PrintLine(nDocID,0,"Síra");
-		Doc_PrintLine(nDocID,0,"Pryskyřice");
+		Doc_PrintLine(nDocID, 0 , " Resin " );
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLine(nDocID,0,"- Vyvolání zombie -");
+		Doc_PrintLine(nDocID, 0 , " - Zombie summon - " );
 		Doc_PrintLine(nDocID,0,"Uhlí");
-		Doc_PrintLine(nDocID,0,"Maso z mrtvoly");
+		Doc_PrintLine(nDocID, 0 , " Meat from a corpse " );
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"- Únava -");
-		Doc_PrintLine(nDocID,0,"Kost z kostlivce");
+		Doc_PrintLine(nDocID, 0 , " Skeleton bone " );
 		Doc_PrintLine(nDocID,0,"Síra");
-		Doc_PrintLine(nDocID,0,"Kamenný krystal");
-		Doc_PrintLine(nDocID,0,"Křemen");
+		Doc_PrintLine(nDocID, 0 , " Stone Crystal " );
+		Doc_PrintLine(nDocID, 0 , " Quartz " );
 		Doc_SetMargins(nDocID,-1,30,20,275,20,1);
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Na vytvoření runy jsou potřeba všechny požadované ingredience.");
+		Doc_PrintLines(nDocID, 1 , " All the required ingredients are needed to create the rune. " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Ten, kdo chce runu vytvořit, musí znát magickou formuli a mít runový kámen, stejně jako i svitek příslušného kouzla.");
+		Doc_PrintLines(nDocID, 1 , " The one who wants to create a rune must know the magic formula and have the rune stone, as well as the scroll of the corresponding spell. " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Teprve pak může runu vytvořit na runovém stole.");
+		Doc_PrintLines(nDocID, 1 , " Only then can he create the rune on the rune table. " );
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
@@ -138,7 +139,7 @@ func void use_bookstand_dmt_circ2_s1()
 		}
 		else
 		{
-			rnd = Hlp_Random(100);
+			rnd = Hlp_Random( 100 );
 			if(rnd <= 30)
 			{
 				B_Say(self,self,"$NOTHINGNEW");
@@ -157,7 +158,7 @@ func void use_bookstand_dmt_circ2_s1()
 			ATR_INTELLECT += 1;
 			Npc_SetTalentSkill(self,NPC_TALENT_INTELLECT,ATR_INTELLECT);
 			AI_Print("Inteligence + 1");
-			BookBonus_132 = TRUE;
+			BookBonus_132 = TRUE ;
 		};
 	};
 };
@@ -176,37 +177,37 @@ func void use_bookstand_dmt_circ3_s1()
 		Doc_SetPage(nDocID,1,"Book_Brown_R.tga",0);
 		Doc_SetFont(nDocID,-1,FONT_Book);
 		Doc_SetMargins(nDocID,0,275,20,30,20,1);
-		Doc_PrintLine(nDocID,0,"TŘETÍ KRUH");
+		Doc_PrintLine(nDocID, 0 , " THIRD CIRCLE " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLines(nDocID,0,"Runy třetího kruhu a ingredience potřebné k jejich vytvoření:");
+		Doc_PrintLines(nDocID, 0 , " Runes of the third circle and ingredients needed to create them: " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLine(nDocID,0,"- Temné kopí -");
-		Doc_PrintLine(nDocID,0,"Černá perla");
-		Doc_PrintLine(nDocID,0,"Křemen");
-		Doc_PrintLine(nDocID,0,"Kamenný krystal");
+		Doc_PrintLine(nDocID, 0 , " - Dark Spear - " );
+		Doc_PrintLine(nDocID, 0 , " Black Pearl " );
+		Doc_PrintLine(nDocID, 0 , " Quartz " );
+		Doc_PrintLine(nDocID, 0 , " Stone Crystal " );
 		Doc_PrintLine(nDocID,0,"Síra");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"- Roj hmyzu -");
 		Doc_PrintLine(nDocID,0,"Akvamarín");
-		Doc_PrintLine(nDocID,0,"Tráva z bažin");
-		Doc_PrintLine(nDocID,0,"Pryskyřice");
+		Doc_PrintLine(nDocID, 0 , " Swamp Grass " );
+		Doc_PrintLine(nDocID, 0 , " Resin " );
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLine(nDocID,0,"- Vyvolání kostlivého válečníka -");
-		Doc_PrintLine(nDocID,0,"Kost z kostlivce");
-		Doc_PrintLine(nDocID,0,"Černá perla");
+		Doc_PrintLine(nDocID, 0 , " - Summon Skeleton Warrior - " );
+		Doc_PrintLine(nDocID, 0 , " Skeleton bone " );
+		Doc_PrintLine(nDocID, 0 , " Black Pearl " );
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_SetMargins(nDocID,-1,30,20,275,20,1);
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Na vytvoření runy jsou potřeba všechny požadované ingredience.");
+		Doc_PrintLines(nDocID, 1 , " All the required ingredients are needed to create the rune. " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Ten, kdo chce runu vytvořit, musí znát magickou formuli a mít runový kámen, stejně jako i svitek příslušného kouzla.");
+		Doc_PrintLines(nDocID, 1 , " The one who wants to create a rune must know the magic formula and have the rune stone, as well as the scroll of the corresponding spell. " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Teprve pak může runu vytvořit na runovém stole.");
+		Doc_PrintLines(nDocID, 1 , " Only then can he create the rune on the rune table. " );
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
@@ -224,7 +225,7 @@ func void use_bookstand_dmt_circ3_s1()
 		}
 		else
 		{
-			rnd = Hlp_Random(100);
+			rnd = Hlp_Random( 100 );
 			if(rnd <= 30)
 			{
 				B_Say(self,self,"$NOTHINGNEW");
@@ -243,7 +244,7 @@ func void use_bookstand_dmt_circ3_s1()
 			ATR_INTELLECT += 1;
 			Npc_SetTalentSkill(self,NPC_TALENT_INTELLECT,ATR_INTELLECT);
 			AI_Print("Inteligence + 1");
-			BookBonus_133 = TRUE;
+			BookBonus_133 = TRUE ;
 		};
 	};
 };
@@ -262,31 +263,31 @@ func void use_bookstand_dmt_circ4_s1()
 		Doc_SetPage(nDocID,1,"Book_Brown_R.tga",0);
 		Doc_SetFont(nDocID,-1,FONT_Book);
 		Doc_SetMargins(nDocID,0,275,20,30,20,1);
-		Doc_PrintLine(nDocID,0,"ČTVRTÝ KRUH");
+		Doc_PrintLine(nDocID, 0 , " FOURTH CIRCLE " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLines(nDocID,0,"Runy čtvrtého kruhu a ingredience potřebné k jejich vytvoření:");
+		Doc_PrintLines(nDocID, 0 , " Runes of the fourth circle and ingredients needed to create them: " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLine(nDocID,0,"- Vyvolání démona -");
-		Doc_PrintLine(nDocID,0,"Srdce démona");
+		Doc_PrintLine(nDocID, 0 , " - Invoke daemon - " );
+		Doc_PrintLine(nDocID, 0 , " Demon Heart " );
 		Doc_PrintLine(nDocID,0,"Síra x2");
-		Doc_PrintLine(nDocID,0,"Pryskyřice x3");
+		Doc_PrintLine(nDocID, 0 , " Resin x3 " );
 		Doc_PrintLine(nDocID,0,"Uhlí");
-		Doc_PrintLine(nDocID,0,"Černá perla x3");
-		Doc_PrintLine(nDocID,0,"Vyšší runový kámen");
+		Doc_PrintLine(nDocID, 0 , " Black Pearl x3 " );
+		Doc_PrintLine(nDocID, 0 , " Higher Runestone " );
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"- Koule smrti -");
-		Doc_PrintLine(nDocID,0,"Ohnivý jazyk");
+		Doc_PrintLine(nDocID, 0 , " Tongue of Fire " );
 		Doc_PrintLine(nDocID,0,"Síra");
-		Doc_PrintLine(nDocID,0,"Pryskyřice");
+		Doc_PrintLine(nDocID, 0 , " Resin " );
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_SetMargins(nDocID,-1,30,20,275,20,1);
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Na vytvoření runy jsou potřeba všechny požadované ingredience.");
+		Doc_PrintLines(nDocID, 1 , " All the required ingredients are needed to create the rune. " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Ten, kdo chce runu vytvořit, musí znát magickou formuli a mít runový kámen, stejně jako i svitek příslušného kouzla.");
+		Doc_PrintLines(nDocID, 1 , " The one who wants to create a rune must know the magic formula and have the rune stone, as well as the scroll of the corresponding spell. " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Teprve pak může runu vytvořit na runovém stole.");
+		Doc_PrintLines(nDocID, 1 , " Only then can he create the rune on the rune table. " );
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
@@ -303,7 +304,7 @@ func void use_bookstand_dmt_circ4_s1()
 		}
 		else
 		{
-			rnd = Hlp_Random(100);
+			rnd = Hlp_Random( 100 );
 			if(rnd <= 30)
 			{
 				B_Say(self,self,"$NOTHINGNEW");
@@ -322,7 +323,7 @@ func void use_bookstand_dmt_circ4_s1()
 			ATR_INTELLECT += 1;
 			Npc_SetTalentSkill(self,NPC_TALENT_INTELLECT,ATR_INTELLECT);
 			AI_Print("Inteligence + 1");
-			BookBonus_134 = TRUE;
+			BookBonus_134 = TRUE ;
 		};
 	};
 };
@@ -341,26 +342,26 @@ func void use_bookstand_dmt_circ5_s1()
 		Doc_SetPage(nDocID,1,"Book_Brown_R.tga",0);
 		Doc_SetFont(nDocID,-1,FONT_Book);
 		Doc_SetMargins(nDocID,0,275,20,30,20,1);
-		Doc_PrintLine(nDocID,0,"PÁTÝ KRUH");
+		Doc_PrintLine(nDocID, 0 , " FIFTH CIRCLE " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLines(nDocID,0,"Runy pátého kruhu a ingredience potřebné k jejich vytvoření:");
+		Doc_PrintLines(nDocID, 0 , " Runes of the fifth circle and ingredients needed to create them: " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLine(nDocID,0,"- Armáda temnot -");
-		Doc_PrintLine(nDocID,0,"Kost z kostlivce");
-		Doc_PrintLine(nDocID,0,"Černá perla");
-		Doc_PrintLine(nDocID,0,"Srdce kamenného golema");
-		Doc_PrintLine(nDocID,0,"Srdce démona");
+		Doc_PrintLine(nDocID, 0 , " - Army of Darkness - " );
+		Doc_PrintLine(nDocID, 0 , " Skeleton bone " );
+		Doc_PrintLine(nDocID, 0 , " Black Pearl " );
+		Doc_PrintLine(nDocID, 0 , " Stone Golem Heart " );
+		Doc_PrintLine(nDocID, 0 , " Demon Heart " );
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_SetMargins(nDocID,-1,30,20,275,20,1);
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Na vytvoření runy jsou potřeba všechny požadované ingredience.");
+		Doc_PrintLines(nDocID, 1 , " All the required ingredients are needed to create the rune. " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Ten, kdo chce runu vytvořit, musí znát magickou formuli a mít runový kámen, stejně jako i svitek příslušného kouzla.");
+		Doc_PrintLines(nDocID, 1 , " The one who wants to create a rune must know the magic formula and have the rune stone, as well as the scroll of the corresponding spell. " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Teprve pak může runu vytvořit na runovém stole.");
+		Doc_PrintLines(nDocID, 1 , " Only then can he create the rune on the rune table. " );
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
@@ -377,7 +378,7 @@ func void use_bookstand_dmt_circ5_s1()
 		}
 		else
 		{
-			rnd = Hlp_Random(100);
+			rnd = Hlp_Random( 100 );
 			if(rnd <= 30)
 			{
 				B_Say(self,self,"$NOTHINGNEW");
@@ -396,7 +397,7 @@ func void use_bookstand_dmt_circ5_s1()
 			ATR_INTELLECT += 1;
 			Npc_SetTalentSkill(self,NPC_TALENT_INTELLECT,ATR_INTELLECT);
 			AI_Print("Inteligence + 1");
-			BookBonus_135 = TRUE;
+			BookBonus_135 = TRUE ;
 		};
 	};
 };
@@ -415,32 +416,32 @@ func void use_bookstand_dmt_circ6_s1()
 		Doc_SetPage(nDocID,1,"Book_Brown_R.tga",0);
 		Doc_SetFont(nDocID,-1,FONT_Book);
 		Doc_SetMargins(nDocID,0,275,20,30,20,1);
-		Doc_PrintLine(nDocID,0,"ŠESTÝ KRUH");
+		Doc_PrintLine(nDocID, 0 , " SIXTH CIRCLE " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLines(nDocID,0,"Runy šestého kruhu a ingredience potřebné k jejich vytvoření:");
+		Doc_PrintLines(nDocID, 0 , " Runes of the sixth circle and ingredients needed to create them: " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLine(nDocID,0,"- Černá mlha -");
-		Doc_PrintLine(nDocID,0,"Kost z kostlivce");
-		Doc_PrintLine(nDocID,0,"Černá perla");
+		Doc_PrintLine(nDocID, 0 , " - Black Mist - " );
+		Doc_PrintLine(nDocID, 0 , " Skeleton bone " );
+		Doc_PrintLine(nDocID, 0 , " Black Pearl " );
 		Doc_PrintLine(nDocID,0,"Ektoplazma");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLine(nDocID,0,"- Řev mrtvých -");
-		Doc_PrintLine(nDocID,0,"Černá perla");
-		Doc_PrintLine(nDocID,0,"Srdce démona");
+		Doc_PrintLine(nDocID, 0 , " - Roar of the Dead - " );
+		Doc_PrintLine(nDocID, 0 , " Black Pearl " );
+		Doc_PrintLine(nDocID, 0 , " Demon Heart " );
 		Doc_PrintLine(nDocID,0,"Lebka");
-		Doc_PrintLine(nDocID,0,"Pryskyřice");
+		Doc_PrintLine(nDocID, 0 , " Resin " );
 		Doc_PrintLine(nDocID,0,"Síra");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_SetMargins(nDocID,-1,30,20,275,20,1);
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Na vytvoření runy jsou potřeba všechny požadované ingredience.");
+		Doc_PrintLines(nDocID, 1 , " All the required ingredients are needed to create the rune. " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Ten, kdo chce runu vytvořit, musí znát magickou formuli a mít runový kámen, stejně jako i svitek příslušného kouzla.");
+		Doc_PrintLines(nDocID, 1 , " The one who wants to create a rune must know the magic formula and have the rune stone, as well as the scroll of the corresponding spell. " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Teprve pak může runu vytvořit na runovém stole.");
+		Doc_PrintLines(nDocID, 1 , " Only then can he create the rune on the rune table. " );
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
@@ -457,7 +458,7 @@ func void use_bookstand_dmt_circ6_s1()
 		}
 		else
 		{
-			rnd = Hlp_Random(100);
+			rnd = Hlp_Random( 100 );
 			if(rnd <= 30)
 			{
 				B_Say(self,self,"$NOTHINGNEW");
@@ -476,7 +477,7 @@ func void use_bookstand_dmt_circ6_s1()
 			ATR_INTELLECT += 1;
 			Npc_SetTalentSkill(self,NPC_TALENT_INTELLECT,ATR_INTELLECT);
 			AI_Print("Inteligence + 1");
-			BookBonus_136 = TRUE;
+			BookBonus_136 = TRUE ;
 		};
 	};
 };
