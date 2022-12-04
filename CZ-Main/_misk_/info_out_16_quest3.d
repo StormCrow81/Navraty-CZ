48d90692cf69b8a377b81fad3d68a63da6c02141
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-04 09:31:10 +0200

    Update info_out_16_quest3.d

--- CZ-Main/_misk_/info_out_16_quest3.d
+++ CZ-Main/_misk_/info_out_16_quest3.d
@@ -1,11 +1,12 @@
 
+
 instance INFO_OUT_16_QUEST3(C_Info)
 {
 	nr = 4;
 	condition = info_out_16_quest3_condition;
 	information = info_out_16_quest3_info;
 	permanent = TRUE;
-	description = "Jak se tady žije?";
+	description = " How is life here? " ;
 };
 
 
@@ -16,9 +17,9 @@ func int info_out_16_quest3_condition()
 
 func void info_out_16_quest3_info()
 {
-	AI_Output(other,self,"Info_Out_16_Quest3_01_00");	//Jak se tady žije?
-	AI_Output(self,other,"Info_Out_16_Quest3_01_01");	//Skvěle!
-	AI_Output(self,other,"Info_Out_16_Quest3_01_02");	//Máme se tady dokonce až tak dobře, že se odsud ze všech sil snažíme uprchnout.
+	AI_Output(other,self, " Info_Out_16_Quest3_01_00 " );	// How is life here?
+	AI_Output(self,other,"Info_Out_16_Quest3_01_01");	//Oh, just great!
+	AI_Output(self,other, " Info_Out_16_Quest3_01_02 " );	// We're even doing so well here that we're trying our best to escape from here.
 };
 
 func void b_assignambientinfos_out_16(var C_Npc slf)
