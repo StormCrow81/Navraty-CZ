89af9b1cc032b883a60da5368fa37a79f58c4189
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-04 09:18:02 +0200

    Update info_tpl_13_exit.d

--- CZ-Main/_misk_/info_tpl_13_exit.d
+++ CZ-Main/_misk_/info_tpl_13_exit.d
@@ -1,4 +1,5 @@
 
+
 instance INFO_TPL_13_EXIT(C_Info)
 {
 	nr = 999;
@@ -26,7 +27,7 @@ instance INFO_TPL_13_EINERVONEUCHWERDEN(C_Info)
 	condition = info_tpl_13_einervoneuchwerden_condition;
 	information = info_tpl_13_einervoneuchwerden_info;
 	permanent = 1;
-	description = "Chtěl bych se stát templářem jako ty.";
+	description = " I would like to become a templar like you. " ;
 };
 
 
@@ -40,33 +41,33 @@ func int info_tpl_13_einervoneuchwerden_condition()
 
 func void info_tpl_13_einervoneuchwerden_info()
 {
-	AI_Output(other,self,"Info_Tpl_13_EinerVonEuchWerden_15_00");	//Chtěl bych se stát templářem jako ty.
-	AI_Output(self,other,"Info_Tpl_13_EinerVonEuchWerden_13_01");	//Víš, kolika věcí jsem se musel zříct, aby se mohl nazývat strážcem míru a ochráncem vyvolených našeho Bratrstva?
-	AI_Output(self,other,"Info_Tpl_13_EinerVonEuchWerden_13_02");	//Nemysli si, že se můžeš jen tak stát templářem a získat tomu přislouchající úctu a respekt.
-	AI_Output(self,other,"Info_Tpl_13_EinerVonEuchWerden_13_03");	//Nejprve by ses měl pořádně seznámit s naším učením.
-	AI_Output(self,other,"Info_Tpl_13_EinerVonEuchWerden_13_04");	//To ti zabere nějaký čas.
+	AI_Output(other,self, " Info_Tpl_13_EinerVonEuchWerden_15_00 " );	// I'd like to become a templar like you.
+	AI_Output(self,other, " Info_Tpl_13_EinerVonEuchWerden_13_01 " );	// Do you know how many things I had to renounce in order to be called the guardian of peace and the protector of the chosen of our Brotherhood?
+	AI_Output(self,other, " Info_Tpl_13_EinerVonEuchWerden_13_02 " );	// Don't think you can just become a Templar and gain the respect and honor that comes with it.
+	AI_Output(self,other, " Info_Tpl_13_EinerVonEuchWerden_13_03 " );	// First, you should thoroughly familiarize yourself with our teachings.
+	AI_Output(self,other, " Info_Tpl_13_EinerVonEuchWerden_13_04 " );	// This will take some time.
 };
 
 
-instance INFO_TPL_13_WICHTIGEPERSONEN(C_Info)
+instance INFO_TPL_13_IMPORTANT PEOPLE (C_Info)
 {
 	nr = 3;
-	condition = info_tpl_13_wichtigepersonen_condition;
-	information = info_tpl_13_wichtigepersonen_info;
+	condition = info_tpl_13_important people_condition;
+	information = info_tpl_13_important people_info;
 	permanent = 1;
-	description = "Kdo tomu tady velí?";
+	description = " Who's in charge here? " ;
 };
 
 
-func int info_tpl_13_wichtigepersonen_condition()
+func int info_tpl_13_important_persons_condition()
 {
 	return TRUE;
 };
 
-func void info_tpl_13_wichtigepersonen_info()
+func void info_tpl_13_important_persons_info()
 {
-	AI_Output(other,self,"Info_Tpl_13_WichtigePersonen_15_00");	//Kdo tomu tady velí?
-	AI_Output(self,other,"Info_Tpl_13_WichtigePersonen_13_01");	//Naši Guru a vybraní templáři. Naše víra utváří náš osud a Guru nám slouží jako proroci.
+	AI_Output(other,self, " Info_Tpl_13_WichtigePersonen_15_00 " );	// Who's in charge here?
+	AI_Output(self,other, " Info_Tpl_13_WichtigePersonen_13_01 " );	// Our Gurus and Chosen Templars. Our faith shapes our destiny and Gurus serve as our prophets.
 };
 
 
@@ -90,17 +91,17 @@ func int info_tpl_13_dielage_condition()
 
 func void info_tpl_13_dielage_info()
 {
-	AI_Output(other,self,"Info_Tpl_13_DieLage_15_00");	//Jak je?
-	AI_Output(self,other,"Info_Tpl_13_DieLage_13_01");	//Od té doby, co mě vybrali za templáře, se cítím jako nikdy předtím.
-	AI_Output(other,self,"Info_Tpl_13_DieLage_15_02");	//To zní zajímavě...
-	AI_Output(self,other,"Info_Tpl_13_DieLage_13_03");	//Ty nejsi věřící, tomu nemůžeš rozumět.
+	AI_Output(other,self,"Info_Tpl_13_DieLage_15_00");	//Whats up?
+	AI_Output(self,other, " Info_Tpl_13_DieLage_13_01 " );	// Ever since I was chosen as a Templar, I feel like never before.
+	AI_Output(other,self, " Info_Tpl_13_DieLage_15_02 " );	// That sounds interesting...
+	AI_Output(self,other, " Info_Tpl_13_DieLage_13_03 " );	// You are not a believer, you cannot understand this.
 };
 
 func void b_assignambientinfos_tpl_13(var C_Npc slf)
 {
 	info_tpl_13_exit.npc = Hlp_GetInstanceID(slf);
 	info_tpl_13_einervoneuchwerden.npc = Hlp_GetInstanceID(slf);
-	info_tpl_13_wichtigepersonen.npc = Hlp_GetInstanceID(slf);
+	info_tpl_13_wichigepeople.npc = Hlp_GetInstanceID(slf);
 	info_tpl_13_dielage.npc = Hlp_GetInstanceID(slf);
 };
 
