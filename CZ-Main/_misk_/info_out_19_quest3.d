943e4f7ba3e9aea264ad37631544027a3cc0bdc6
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-04 09:34:22 +0200

    Update info_out_19_quest3.d

--- CZ-Main/_misk_/info_out_19_quest3.d
+++ CZ-Main/_misk_/info_out_19_quest3.d
@@ -1,11 +1,12 @@
 
+
 instance INFO_OUT_19_QUEST3(C_Info)
 {
 	nr = 4;
 	condition = info_out_19_quest3_condition;
 	information = info_out_19_quest3_info;
 	permanent = TRUE;
-	description = "Zdravím!";
+	description = " Hello! " ;
 };
 
 
@@ -16,8 +17,8 @@ func int info_out_19_quest3_condition()
 
 func void info_out_19_quest3_info()
 {
-	AI_Output(other,self,"Info_Out_19_Quest3_01_00");	//Zdravím!
-	AI_Output(self,other,"Info_Out_19_Quest3_01_01");	//Co chceš?
+	AI_Output(other,self,"Info_Out_19_Quest3_01_00");	//Hello!
+	AI_Output(self,other,"Info_Out_19_Quest3_01_01");	//What is it?
 };
 
 func void b_assignambientinfos_out_19(var C_Npc slf)
