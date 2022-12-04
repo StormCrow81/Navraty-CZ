306a11871de5a8fc904fdde4f3bac43722abc593
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-04 11:32:01 +0200

    Update IT_Torch.d

--- CZ-Main/Items/IT_Torch.d
+++ CZ-Main/Items/IT_Torch.d
@@ -1,15 +1,16 @@
+
 const int Value_Fackel = 1;
 
 instance ItLsTorch(C_Item)
 {
-	name = "Pochodeň";
+	name = " Torch " ;
 	mainflag = ITEM_KAT_NONE;
 	flags = ITEM_MULTI;
 	value = Value_Fackel;
 	visual = "ItLs_Torch_01.3ds";
 	material = MAT_WOOD;
 	description = name;
-	text[2] = "Šance na zapálení protivníka (v režimu boje):";
+	text[ 2 ] = " Chance to ignite an opponent (in combat mode): " ;
 	count[2] = 15;
 	text[5] = NAME_Value;
 	count[5] = value;
@@ -18,45 +19,45 @@ instance ItLsTorch(C_Item)
 
 instance ItLsFireTorch(C_Item)
 {
-	name = "Hořící pochodeň";
+	name = " Burning Torch " ;
 	mainflag = ITEM_KAT_NONE;
 	flags = ITEM_MULTI;
 	value = Value_Fackel;
 	visual = "ITMI_FIRETORCH_01.3DS";
 	effect = "SPELLFX_FIRE_TORCH";
 	material = MAT_LEATHER;
-	description = "Hořící pochodeň";
+	description = " Burning Torch " ;
 	inv_animate = 1;
 };
 
 instance ItLsTorchburning(C_Item)
 {
-	name = "Hořící pochodeň";
+	name = " Burning Torch " ;
 	mainflag = ITEM_KAT_LIGHT;
 	flags = ITEM_TORCH;
 	value = 1;
 	visual = "ITLSTORCHBURNING.ZEN";
 	material = MAT_WOOD;
 	description = name;
-	text[4] = "Hořící pochodeň";
+	text[ 4 ] = " Burning Torch " ;
 };
 
 instance ItLsTorchburned(C_Item)
 {
-	name = "Pochodeň";
+	name = " Torch " ;
 	mainflag = ITEM_KAT_NONE;
 	flags = ITEM_TORCH | ITEM_MULTI;
 	value = 1;
 	visual = "ItLs_Torchburned_01.3ds";
 	material = MAT_WOOD;
-	text[4] = "Vyhořelá pochodeň...";
+	text[ 4 ] = " Burnt Torch... " ;
 	description = name;
 	inv_animate = 1;
 };
 
 instance ItLsTorchFirespit(C_Item)
 {
-	name = "Cirkusová pochodeň";
+	name = " Circus torch " ;
 	mainflag = ITEM_KAT_NONE;
 	flags = ITEM_TORCH;
 	value = 1;
