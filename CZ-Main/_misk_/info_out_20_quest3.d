0cbbac10997667ee69b3bb9ec445961962edb244
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-04 09:15:31 +0200

    Update info_out_20_quest3.d

--- CZ-Main/_misk_/info_out_20_quest3.d
+++ CZ-Main/_misk_/info_out_20_quest3.d
@@ -1,11 +1,12 @@
 
+
 instance INFO_OUT_20_QUEST3(C_Info)
 {
 	nr = 4;
 	condition = info_out_20_quest3_condition;
 	information = info_out_20_quest3_info;
 	permanent = TRUE;
-	description = "Jak se daří?";
+	description = " How are you doing? " ;
 };
 
 
@@ -16,8 +17,8 @@ func int info_out_20_quest3_condition()
 
 func void info_out_20_quest3_info()
 {
-	AI_Output(other,self,"Info_Out_20_Quest3_01_00");	//Jak se daří?
-	AI_Output(self,other,"Info_Out_20_Quest3_01_01");	//Co myslíš? Jako vždy, mizerne. Mám hodně práce a nic k jídlu.
+	AI_Output(other,self, " Info_Out_20_Quest3_01_00 " );	// How's it going?
+	AI_Output(self,other, " Info_Out_20_Quest3_01_01 " );	// What do you think? As always, crappy. I have a lot of work and nothing to eat.
 };
 
 func void b_assignambientinfos_out_20(var C_Npc slf)
