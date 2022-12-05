cb63f9d6b40825c9c10610ee4cdac8ca875876ba
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-05 19:23:44 +0200

    Update DIA_BAU_973_Rukhar.d

--- RU-Main/Story/Dialoge/DIA_BAU_973_Rukhar.d
+++ RU-Main/Story/Dialoge/DIA_BAU_973_Rukhar.d
@@ -39,7 +39,7 @@ func int DIA_Rukhar_HALLO_Condition()
 func void DIA_Rukhar_HELLO_Info()
 {
 	AI_Output(other,self, " DIA_Rukhar_HALLO_15_00 " );	// How's the beer here?
-	AI_Output(self,other, " DIA_Rukhar_HALLO_12_01 " );	// Not the best, but don't get too picky these days.
+	AI_Output(self,other, " DIA_Rukhar_HALLO_12_01 " );	// Not the best, but you can't be too picky these days.
 };
 
 
@@ -64,10 +64,10 @@ func int DIA_Rukhar_WASMACHSTDU_Condition()
 func void DIA_Rukhar_WASMACHSTDU_Info()
 {
 	AI_Output(other,self, " DIA_Rukhar_WASMACHSTDU_15_00 " );	// What are you doing here?
-	AI_Output(self,other, " DIA_Rukhar_WASMACHSTDU_12_01 " );	// Until a few weeks ago, he worked like a slave on Onar's farm.
-	AI_Output(self,other, " DIA_Rukhar_WASMACHSTDU_12_02 " );	// But one day I realized that I couldn't stand it anymore and ran away from there.
+	AI_Output(self,other, " DIA_Rukhar_WASMACHSTDU_12_01 " );	// Until a few weeks ago, I worked like a slave on Onar's farm.
+	AI_Output(self,other, " DIA_Rukhar_WASMACHSTDU_12_02 " );	// But one day I realized that I couldn't stand it anymore and ran away.
 	AI_Output(self,other, " DIA_Rukhar_WASMACHSTDU_12_03 " );	// I'm my own boss now. Everything that belongs to me is in this chest. And I don't need anything else.
-	AI_Output(self,other, " DIA_Rukhar_WASMACHSTDU_12_04 " );	// Do you want to compete with me?
+	AI_Output(self,other, " DIA_Rukhar_WASMACHSTDU_12_04 " );	// Say, might you be interested in a friendly contest?
 	Log_CreateTopic(TOPIC_Wettsaufen,LOG_MISSION);
 	Log_SetTopicStatus(TOPIC_Wettsaufen,LOG_Running);
 	B_LogEntry(TOPIC_Wettsaufen, " You can bet in the tavern. " );
@@ -95,11 +95,11 @@ func int DIA_Rukhar_COMPETITION_Condition()
 func void DIA_Rukhar_COMPETITION_Info()
 {
 	AI_Output(other,self, " DIA_Rukhar_WETTKAMPF_15_00 " );	// What is this contest?
-	AI_Output(self,other, " DIA_Rukhar_WETTKAMPF_12_01 " );	// I call it 'DRINK DOWN'!
+	AI_Output(self,other, " DIA_Rukhar_WETTKAMPF_12_01 " );	// I call it 'Drowner'!
 	AI_Output(self,other, " DIA_Rukhar_WETTKAMPF_12_02 " );	// The rules are very simple.
 	AI_Output(self,other, " DIA_Rukhar_WETTKAMPF_12_03 " );	// We take turns ordering a glass of this excellent beer offered by this old eccentric.
-	AI_Output(self,other, " DIA_Rukhar_WETTKAMPF_12_04 " );	// Each mug needs to be drained to the bottom so that the table stays dry when turned upside down.
-	AI_Output(self,other, " DIA_Rukhar_WETTKAMPF_12_05 " );	// Whoever can stand the longest wins. The loser pays for the beer and the bet. Well? How do you like it?
+	AI_Output(self,other, " DIA_Rukhar_WETTKAMPF_12_04 " );	// Each mug needs to be drained so well that the table stays dry when turned upside down.
+	AI_Output(self,other, " DIA_Rukhar_WETTKAMPF_12_05 " );	// Whoever lasts the longest wins. The loser pays for the beer and the bet. Well? You got the balls?
 	AI_Output(other,self, " DIA_Rukhar_WETTKAMPF_15_06 " );	// Maybe later when I'm not in such a hurry.
 };
 
@@ -128,7 +128,7 @@ func void DIA_Rukhar_HOLERANDOLPH_Info()
 	AI_Output(self,other, " DIA_Rukhar_HOLERANDOLPH_12_01 " );	// Of course. If you find anyone with the courage to face me, I'll take your bet.
 	AI_Output(self,other, " DIA_Rukhar_HOLERANDOLPH_12_02 " );	// Everyone puts the same stakes in the pot. Winner takes all! The size of the bet depends only on the size of your wallet.
 	AI_Output(self,other, " DIA_Rukhar_HOLERANDOLPH_12_03 " );	// Bring me someone who dares to face me, and I'll take your bet.
-	B_LogEntry(TOPIC_Wettsaufen, " Ruhar calls this game DRINK TO THE BOTTOM. No thanks, I'm not interested. I'll find someone else for him. Let him fool others, but not me. " );
+	B_LogEntry(TOPIC_Wettsaufen, " RuKhar calls this game DROWNER. No thanks, I'm not interested. I'll find someone else to compete for the hangover of the century. " );
 };
 
 
@@ -139,7 +139,7 @@ instance DIA_Rukhar_RANDOLPHWILL(C_Info)
 	condition = DIA_Rukhar_RANDOLPHWILL_Condition;
 	information = DIA_Rukhar_RANDOLPHWILL_Info;
 	permanent = TRUE;
-	description = " I've found a man who wants to fight you. " ;
+	description = " I've found a man who wants to test himself against you. " ;
 };
 
 
@@ -155,19 +155,19 @@ func int DIA_Rukhar_RANDOLPHWILL_Condition()
 
 func void DIA_Rukhar_RANDOLPHWILL_Info()
 {
-	AI_Output(other,self, " DIA_Rukhar_RANDOLPHWILL_15_00 " );	// I found a man who wants to fight you.
+	AI_Output(other,self, " DIA_Rukhar_RANDOLPHWILL_15_00 " );	// I found a man who wants to test himself against you.
 	AI_Output(self,other, " DIA_Rukhar_RANDOLPHWILL_12_01 " );	// Who is this?
 	AI_Output(other,self, " DIA_Rukhar_RANDOLPHWILL_15_02 " );	// Randolph.
-	AI_Output(self,other, " DIA_Rukhar_RANDOLPHWILL_12_03 " );	// Ho, ho, ho... (laughs) Randolph the braggart?! OK then. Why not!
+	AI_Output(self,other, " DIA_Rukhar_RANDOLPHWILL_12_03 " );	// Ho, ho, ho... Randolph the braggart?! OK then. Why not?
 	AI_Output(self,other, " DIA_Rukhar_RANDOLPHWILL_12_04 " );	// Send that slug to me and place your bet.
 	AI_Output(self,other, " DIA_Rukhar_RANDOLPHWILL_12_05 " );	// You can set it up again after 2 days at the earliest. Who knows? Maybe you'll be lucky and he'll sober up in time.
 	AI_Output(self,other, " DIA_Rukhar_RANDOLPHWILL_12_06 " );	// What's your bet?
 	Info_ClearChoices(DIA_Rukhar_RANDOLPHWILL);
-	Info_AddChoice(DIA_Route_RANDOLPHWILL, " Boy " ,DIA_Root_RANDOLPHWILL_nix);
+	Info_AddChoice(DIA_Route_RANDOLPHWILL, " Zero " ,DIA_Root_RANDOLPHWILL_nix);
 	Info_AddChoice(DIA_Rukhar_RANDOLPHWILL,"100",DIA_Rukhar_RANDOLPHWILL_100);
 	Info_AddChoice(DIA_Rukhar_RANDOLPHWILL,"50",DIA_Rukhar_RANDOLPHWILL_50);
 	Info_AddChoice(DIA_Rukhar_RANDOLPHWILL,"20",DIA_Rukhar_RANDOLPHWILL_20);
-	Info_AddChoice(DIA_Choice_RANDOLPHWILL, " 10 minutes " ,DIA_Choice_RANDOLPHWILL_10);
+	Info_AddChoice(DIA_Choice_RANDOLPHWILL, "10" ,DIA_Choice_RANDOLPHWILL_10);
 };
 
 func void DIA_Rukhar_RANDOLPHWILL_accept()
@@ -180,17 +180,17 @@ func void DIA_Rukhar_RANDOLPHWILL_accept()
 func void DIA_Rukhar_RANDOLPHWILL_more()
 {
 	Info_ClearChoices(DIA_Rukhar_RANDOLPHWILL);
-	Info_AddChoice(DIA_Route_RANDOLPHWILL, " Boy " ,DIA_Root_RANDOLPHWILL_nix);
+	Info_AddChoice(DIA_Route_RANDOLPHWILL, " Zero " ,DIA_Root_RANDOLPHWILL_nix);
 	Info_AddChoice(DIA_Rukhar_RANDOLPHWILL,"100",DIA_Rukhar_RANDOLPHWILL_100);
 	Info_AddChoice(DIA_Rukhar_RANDOLPHWILL,"50",DIA_Rukhar_RANDOLPHWILL_50);
 	Info_AddChoice(DIA_Rukhar_RANDOLPHWILL,"20",DIA_Rukhar_RANDOLPHWILL_20);
-	Info_AddChoice(DIA_Choice_RANDOLPHWILL, " 10 minutes " ,DIA_Choice_RANDOLPHWILL_10);
+	Info_AddChoice(DIA_Choice_RANDOLPHWILL, "10" ,DIA_Choice_RANDOLPHWILL_10);
 };
 
 func void DIA_Rukhar_RANDOLPHWILL_nix()
 {
-	AI_Output(other,self,"DIA_Rukhar_RANDOLPHWILL_nix_15_00");	//Ноль.
-	AI_Output(self,other, " DIA_Rukhar_RANDOLPHWILL_nix_12_01 " );	// Then we'd better forget about the race.
+	AI_Output(other,self,"DIA_Rukhar_RANDOLPHWILL_nix_15_00");	//Zero.
+	AI_Output(self,other, " DIA_Rukhar_RANDOLPHWILL_nix_12_01 " );	// Then we'd better forget it.
 	AI_StopProcessInfos(self);
 };
 
@@ -208,7 +208,7 @@ func void DIA_Rukhar_RANDOLPHWILL_10()
 func void DIA_Rukhar_RANDOLPHWILL_20()
 {
 	AI_Output(other,self, " DIA_Rukhar_RANDOLPHWILL_20_15_00 " );	// Twenty.
-	AI_Output(self,other, " DIA_Rukhar_RANDOLPHWILL_20_12_01 " );	// If you want to bet, then do it like a human.
+	AI_Output(self,other, " DIA_Rukhar_RANDOLPHWILL_20_12_01 " );	// If you want to bet, then do it like a man.
 	Rukhar_deployment = 20 ;
 	Rukhar_profit = 40 ;
 	Info_ClearChoices(DIA_Rukhar_RANDOLPHWILL);
@@ -219,7 +219,7 @@ func void DIA_Rukhar_RANDOLPHWILL_20()
 func void DIA_Rukhar_RANDOLPHWILL_50()
 {
 	AI_Output(other,self, " DIA_Rukhar_RANDOLPHWILL_50_15_00 " );	// Fifty!
-	AI_Output(self,other, " DIA_Rukhar_RANDOLPHWILL_50_12_01 " );	// Don't be shy! Bolder.
+	AI_Output(self,other, " DIA_Rukhar_RANDOLPHWILL_50_12_01 " );	// Don't be shy! You can afford more.
 	Rukhar_deployment = 50 ;
 	Rukhar_profit = 100 ;
 	Info_ClearChoices(DIA_Rukhar_RANDOLPHWILL);
@@ -229,8 +229,8 @@ func void DIA_Rukhar_RANDOLPHWILL_50()
 
 func void DIA_Rukhar_RANDOLPHWILL_100()
 {
-	AI_Output(other,self,"DIA_Rukhar_RANDOLPHWILL_100_15_00");	//Сто!
-	AI_Output(self,other, " DIA_Rukhar_RANDOLPHWILL_100_12_01 " );	// I already like this!
+	AI_Output(other,self,"DIA_Rukhar_RANDOLPHWILL_100_15_00");	//One hundred!
+	AI_Output(self,other, " DIA_Rukhar_RANDOLPHWILL_100_12_01 " );	// That's more like it!
 	Rukhar_deployment = 100 ;
 	Rukhar_profit = 200 ;
 	Info_ClearChoices(DIA_Rukhar_RANDOLPHWILL);
@@ -265,14 +265,14 @@ func void DIA_Rukhar_ICHSEHEDICH_Info()
 	AI_Output(other,self, " DIA_Rukhar_ICHSEHEDICH_15_00 " );	// Here's my bet.
 	if (B_GiveInvItems(other,self,ItMi_Gold,Rukhar_Einsatz))
 	{
-		AI_Output(self,other, " DIA_Rukhar_ICHSEHEDICH_12_01 " );	// Good. But just don't delay, bring Randolph quickly, do you hear?
+		AI_Output(self,other, " DIA_Rukhar_ICHSEHEDICH_12_01 " );	// Good. Don't delay, bring Randolph here soon.
 		DIA_Rukhar_ICHSEHEDICH_noPerm = TRUE ;
 		MIS_Rukhar_Competition = LOG_Running;
 		B_GivePlayerXP(XP_Ambient);
 	}
 	else
 	{
-		AI_Output(self,other, " DIA_Rukhar_ICHSEHEDICH_12_02 " );	// Very funny. Come back when you have money.
+		AI_Output(self,other, " DIA_Rukhar_ICHSEHEDICH_12_02 " );	// Very funny. Come back when you have the money.
 	};
 	AI_StopProcessInfos(self);
 };
@@ -341,7 +341,7 @@ func void DIA_Rukhar_HAENSELN_Info()
 	}
 	else
 	{
-		AI_Output(self,other, " DIA_Rukhar_HAENSELN_12_02 " );	// I'll get even with you.
+		AI_Output(self,other, " DIA_Rukhar_HAENSELN_12_02 " );	// I'll get even with you, I swear it.
 	};
 
 	AI_StopProcessInfos(self);
