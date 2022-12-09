bc1476fde63bbe76bbe8747d092cbb56446d6cb0
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-09 22:16:49 +0200

    Update B_Video.d

--- RU-Main/Story/B_AssignAmbientInfos/B_Video.d
+++ RU-Main/Story/B_AssignAmbientInfos/B_Video.d
@@ -1,72 +1,73 @@
 
+
 func void B_Video()
 {
-	AI_Output(self,other,"INTRO_Xardas_Speech_14_00");	//Один заключенный изменил судьбу сотен.
-	AI_Output(self,other,"INTRO_Xardas_Speech_14_01");	//Но он заплатил за это высокую цену...
-	AI_Output(self,other,"INTRO_Xardas_Speech_14_02");	//Он победил Спящего и уничтожил Барьер...
-	AI_Output(self,other,"INTRO_Xardas_Speech_14_03");	//...но, когда остальные заключенные вырвались на свободу, он остался лежать под грудой камней...
-	AI_Output(self,other,"INTRO_Xardas_Speech_14_04");	//Это я отправил его на бой со Спящим.
-	AI_Output(self,other,"INTRO_Xardas_Speech_14_05");	//И теперь я вызволю его оттуда.
-	AI_Output(self,other,"INTRO_Xardas_Speech_14_06");	//Он слаб, и многое забыл.
-	AI_Output(self,other,"INTRO_Xardas_Speech_14_07");	//Но он жив...
-	AI_Output(self,other,"INTRO_Xardas_Speech_14_08");	//Он вернулся!
-	AI_Output(self,other,"INTRO_DiegoGorn_12_00");	//(у костра) Конечно же, он еще жив. А ты как думал?
-	AI_Output(self,other,"INTRO_DiegoGorn_11_01");	//Будем надеяться...
-	AI_Output(self,other,"INTRO_DiegoGorn_12_02");	//(трясется земля) Ты чувствуешь это?
+	AI_Output(self,other, " INTRO_Xardas_Speech_14_00 " );	// One prisoner changed the fate of hundreds.
+	AI_Output(self,other, " INTRO_Xardas_Speech_14_01 " );	// But he paid a high price for it...
+	AI_Output(self,other, " INTRO_Xardas_Speech_14_02 " );	// He defeated the Sleeper and destroyed the Barrier...
+	AI_Output(self,other, " INTRO_Xardas_Speech_14_03 " );	// ...but when the rest of the prisoners broke free, he was left lying under a pile of stones...
+	AI_Output(self,other, " INTRO_Xardas_Speech_14_04 " );	// It was I who sent him to fight the Sleeper.
+	AI_Output(self,other, " INTRO_Xardas_Speech_14_05 " );	// And now I'll get him out of there.
+	AI_Output(self,other, " INTRO_Xardas_Speech_14_06 " );	// He is weak and has forgotten a lot.
+	AI_Output(self,other, " INTRO_Xardas_Speech_14_07 " );	// But he's alive...
+	AI_Output(self,other, " INTRO_Xardas_Speech_14_08 " );	// He's back!
+	AI_Output(self,other, " INTRO_DiegoGorn_12_00 " );	// (by the fire) Of course he's still alive. What did you think?
+	AI_Output(self,other, " INTRO_DiegoGorn_11_01 " );	// Let's hope...
+	AI_Output(self,other, " INTRO_DiegoGorn_12_02 " );	// (ground shakes) Do you feel it?
 	AI_Output(self,other,"INTRO_DiegoGorn_11_03");	//Что?
-	AI_Output(self,other,"INTRO_DiegoGorn_12_04");	//Земля...
-	AI_Output(other,self,"Extro_Tempel_15_01");	//(в изумлении) Ксардас! Что...
-	AI_Output(other,self,"Extro_Tempel_14_02");	//(недовольно) Не сейчас...
-	AI_Output(other,self,"Extro_Tempel_14_03");	//(фанатично) Я готов... выбери меня! Да...
-	AI_Output(other,self,"Extro_Tempel_15_04");	//(себе под нос) Где же он?
-	AI_Output(self,other,"OUTRO_Xardas_14_00");	//(цитирует) И человек убил зверя и вошел в царство Белиара...
-	AI_Output(other,self,"OUTRO_Xardas_15_01");	//Ксардас! Что именно произошло в Храме Ирдората?
-	AI_Output(self,other,"OUTRO_Xardas_14_02");	//С помощью Инноса ты уничтожил воплощение Зла.
-	AI_Output(self,other,"OUTRO_Xardas_14_03");	//И я впитал его силу...
-	AI_Output(self,other,"OUTRO_Xardas_14_04");	//С тех пор как Я покинул Круг Огня, это было единственной моей целью.
-	AI_Output(self,other,"OUTRO_Xardas_14_05");	//Чтобы достичь ее, я помог тебе выполнить твое предназначение.
-	AI_Output(self,other,"OUTRO_Xardas_14_06");	//То, что мне не удалось в Храме Спящего, наконец, свершилось.
-	AI_Output(self,other,"OUTRO_Xardas_14_07");	//Белиар выбрал меня.
-	AI_Output(other,self,"OUTRO_Xardas_15_08");	//Так ты теперь повинуешься богу тьмы?
-	AI_Output(self,other,"OUTRO_Xardas_14_09");	//Нет. Я повинуюсь Белиару не больше, чем ты повинуешься Инносу!
-	AI_Output(self,other,"OUTRO_Xardas_14_10");	//Даже боги не знают, какая судьба нас ждет...
-	AI_Output(self,other,"OUTRO_Xardas_14_11");	//И я только начинаю осознавать возможности, которые открываются предо мной...
-	AI_Output(self,other,"OUTRO_Xardas_14_12");	//Но одно можно сказать с определенностью. Мы еще встретимся...
-	AI_Output(self,other,"OUTRO_Xardas_04_00");	//(кричит) До встречи!
-	AI_Output(self,other,"Cutscene_Drachen_04_00");	//(смотрит вдаль, видит что-то) Хммм?
-	AI_Output(self,other,"Cutscene_Drachen_04_01");	//(бормочет) Ох, черт!
-	AI_Output(self,other,"Cutscene_Drachen_04_02");	//(задыхается кашляет)
-	AI_Output(self,other,"Cutscene_Drachen_04_03");	//Аааааа! (разное)
-	AI_Output(self,other,"Cutscene_Drachen_04_04");	//(долгий предсмертный крик, 4 секунды)
-	AI_Output(self,other,"Cutscene_OrcSturm_04_00");	//Закройте ворота!!! (разное)
-	AI_Output(self,other,"Cutscene_OrcSturm_04_01");	//Остановите их!!! (разное)
-	AI_Output(self,other,"Cutscene_OrcSturm_04_02");	//Аааааа! (разное)
-	AI_Output(self,other,"OUTRO_Schiff_12_00");	//Мы перегружены. Мы должны выбросить часть золота за борт.
-	AI_Output(other,self,"OUTRO_Schiff_15_01");	//Руки прочь от этого золота!
-	AI_Output(self,other,"OUTRO_Schiff_12_02");	//Послушай, будет действительно лучше, если мы...
-	AI_Output(other,self,"OUTRO_Schiff_15_03");	//Я даже и слышать не хочу об этом...
-	AI_Output(self,other,"OUTRO_Schiff_11_04");	//Я слышал, война с орками идет плохо...
+	AI_Output(self,other, " INTRO_DiegoGorn_12_04 " );	// Earth...
+	AI_Output(other,self, " Extro_Tempel_15_01 " );	// (in amazement) Xardas! What...
+	AI_Output(other,self, " Extro_Tempel_14_02 " );	// (angrily) Not now...
+	AI_Output(other,self, " Extro_Tempel_14_03 " );	// (fanatical) I'm ready... pick me! Yes...
+	AI_Output(other,self, " Extro_Tempel_15_04 " );	// (under his breath) Where is he?
+	AI_Output(self,other, " OUTRO_Xardas_14_00 " );	// (quotes) And the man killed the beast and entered the realm of Beliar...
+	AI_Output(other,self, " OUTRO_Xardas_15_01 " );	// Xardas! What exactly happened in the Temple of Irdorath?
+	AI_Output(self,other, " OUTRO_Xardas_14_02 " );	// With the help of Innos, you destroyed the incarnation of Evil.
+	AI_Output(self,other, " OUTRO_Xardas_14_03 " );	// And I absorbed his power...
+	AI_Output(self,other, " OUTRO_Xardas_14_04 " );	// Since I left the Circle of Fire, this has been my only goal.
+	AI_Output(self,other, " OUTRO_Xardas_14_05 " );	// To achieve it, I helped you fulfill your destiny.
+	AI_Output(self,other, " OUTRO_Xardas_14_06 " );	// What I failed in the Temple of the Sleeper is finally done.
+	AI_Output(self,other, " OUTRO_Xardas_14_07 " );	// Beliar chose me.
+	AI_Output(other,self, " OUTRO_Xardas_15_08 " );	// So you now obey the god of darkness?
+	AI_Output(self,other, " OUTRO_Xardas_14_09 " );	// No. I obey Beliar no more than you obey Innos!
+	AI_Output(self,other, " OUTRO_Xardas_14_10 " );	// Even the gods don't know what fate awaits us...
+	AI_Output(self,other, " OUTRO_Xardas_14_11 " );	// And I'm just beginning to realize the possibilities that open up before me...
+	AI_Output(self,other, " OUTRO_Xardas_14_12 " );	// But one thing is certain. We'll meet Again...
+	AI_Output(self,other, " OUTRO_Xardas_04_00 " );	// (shouts) See you!
+	AI_Output(self,other, " Cutscene_Drachen_04_00 " );	// (looks off into the distance, sees something) Hmmm?
+	AI_Output(self,other, " Cutscene_Drachen_04_01 " );	// (mumbles) Oh, damn!
+	AI_Output(self,other, " Cutscene_Drachen_04_02 " );	// (gasps coughs)
+	AI_Output(self,other, " Cutscene_Drachen_04_03 " );	// Ahhh! (different)
+	AI_Output(self,other, " Cutscene_Drachen_04_04 " );	// (long death scream, 4 seconds)
+	AI_Output(self,other, " Cutscene_OrcSturm_04_00 " );	// Close the gate!!! (different)
+	AI_Output(self,other, " Cutscene_OrcSturm_04_01 " );	// Stop them!!! (different)
+	AI_Output(self,other, " Cutscene_OrcSturm_04_02 " );	// Ahhh! (different)
+	AI_Output(self,other, " OUTRO_Schiff_12_00 " );	// We're overloaded. We must throw some of the gold overboard.
+	AI_Output(other,self, " OUTRO_Schiff_15_01 " );	// Hands off this gold!
+	AI_Output(self,other, " OUTRO_Schiff_12_02 " );	// Look, it would be really better if we...
+	AI_Output(other,self, " OUTRO_Schiff_15_03 " );	// I don't even want to hear about it...
+	AI_Output(self,other, " OUTRO_Schiff_11_04 " );	// I heard the orc war is going badly...
 	AI_Output(other,self,"OUTRO_Schiff_15_05");	//И?
-	AI_Output(self,other,"OUTRO_Schiff_11_06");	//Возможно, нам все равно нигде не удастся потратить все это золото...
-	AI_Output(other,self,"OUTRO_Schiff_15_07");	//Это золото останется на борту!
-	AI_Output(self,other,"OUTRO_Schiff_12_08");	//Что толку от этого золота, если мы опрокинемся в самый легкий шторм?
-	AI_Output(other,self,"OUTRO_Schiff_15_09");	//Но я не вижу шторма...
+	AI_Output(self,other, " OUTRO_Schiff_11_06 " );	// We may not be able to spend all this gold anywhere anyway...
+	AI_Output(other,self, " OUTRO_Schiff_15_07 " );	// This gold will stay on board!
+	AI_Output(self,other, " OUTRO_Schiff_12_08 " );	// What's the use of this gold if we capsize in the lightest storm?
+	AI_Output(other,self, " OUTRO_Schiff_15_09 " );	// But I don't see the storm...
 	AI_Output(self,other,"OUTRO_Schiff_12_10");	//ПОКА нет ...
-	AI_Output(other,self,"OUTRO_Schiff_15_11");	//Расслабься! Все будет в порядке!
-	AI_Output(other,self,"DIA_Addon_AddonIntro_15_00");	//(ошеломлен) ...люди?
-	AI_Output(self,other,"DIA_Addon_AddonIntro_14_01");	//(пренебрежительно) Люди слабы.
-	AI_Output(self,other,"DIA_Addon_AddonIntro_14_02");	//(пренебрежительно) Они слишком легко поддаются соблазнам зла.
-	AI_Output(self,other,"DIA_Addon_AddonIntro_14_03");	//Таким образом они вовлекаются в дела, которые не могут понять и уж тем более не могут контролировать.
-	AI_Output(self,other,"DIA_Addon_AddonIntro_14_04");	//Твердые в своей вере уже начали сражение с врагом.
-	AI_Output(self,other,"DIA_Addon_Xardas_AddonIntro_Add_14_00");	//После исчезновения Барьера и уничтожения Спящего, гнев Белиара стал еще страшнее!
-	AI_Output(self,other,"DIA_Addon_Xardas_AddonIntro_Add_14_01");	//Мощнейший артефакт будет возвращен в этот мир, так пишут в древних книгах.
-	AI_Output(self,other,"DIA_Addon_Xardas_AddonIntro_Add_14_02");	//(цитируя) 'Когда Бог Тьмы пошлет своих приспешников на поиски...'
-	AI_Output(self,other,"DIA_Addon_Xardas_AddonIntro_Add_14_03");	//Именно это и произошло.
-	AI_Output(self,other,"DIA_Addon_Xardas_AddonIntro_Add_14_04");	//Этот поиск начался давным-давно!
+	AI_Output(other,self, " OUTRO_Schiff_15_11 " );	// Relax! Everything will be OK!
+	AI_Output(other,self, " DIA_Addon_AddonIntro_15_00 " );	// (stunned) ...people?
+	AI_Output(self,other, " DIA_Addon_AddonIntro_14_01 " );	// (disparagingly) People are weak.
+	AI_Output(self,other, " DIA_Addon_AddonIntro_14_02 " );	// (disparagingly) They succumb too easily to the temptations of evil.
+	AI_Output(self,other, " DIA_Addon_AddonIntro_14_03 " );	// So they get involved in things they can't understand, let alone control.
+	AI_Output(self,other, " DIA_Addon_AddonIntro_14_04 " );	// Those who are firm in their faith have already begun the battle with the enemy.
+	AI_Output(self,other, " DIA_Addon_Xardas_AddonIntro_Add_14_00 " );	// After the disappearance of the Barrier and the destruction of the Sleeper, Beliar's anger became even more terrible!
+	AI_Output(self,other, " DIA_Addon_Xardas_AddonIntro_Add_14_01 " );	// The most powerful artifact will be returned to this world, as they say in ancient books.
+	AI_Output(self,other, " DIA_Addon_Xardas_AddonIntro_Add_14_02 " );	// (quoting) 'When the God of Darkness sends his minions to find...'
+	AI_Output(self,other, " DIA_Addon_Xardas_AddonIntro_Add_14_03 " );	// This is exactly what happened.
+	AI_Output(self,other, " DIA_Addon_Xardas_AddonIntro_Add_14_04 " );	// This search started a long time ago!
 	AI_Output(self,other,"DIA_Addon_Xardas_AddonIntro_Add_14_05");	//Во время поисков приспешники Белиара оскверняют древнейшие алтари богов.
 	AI_Output(self,other,"DIA_Addon_Xardas_AddonIntro_Add_14_06");	//Приспешники Белиара оскверняют древнейшие алтари богов.
 	AI_Output(self,other,"DIA_Addon_Xardas_AddonIntro_Add_14_07");	//Хранители этих священных реликвий пробудились. И их гнев сотрясает землю!
-	AI_Output(self,other,"DIA_Addon_Xardas_AddonIntro_Add_14_08");	//Каждый сильный маг на этом острове способен ощутить их гнев.
-	AI_Output(self,other,"DIA_Addon_Xardas_AddonIntro_Add_14_09");	//И некоторые из них определенно начали готовиться к встрече с угрозой.
+	AI_Output(self,other, " DIA_Addon_Xardas_AddonIntro_Add_14_08 " );	// Every strong mage on this island is capable of feeling their wrath.
+	AI_Output(self,other, " DIA_Addon_Xardas_AddonIntro_Add_14_09 " );	// And some of them have definitely begun to prepare for the threat.
 };
 
