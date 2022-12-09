d60266835d4cfe87c618a53bdaf3a986a587b77c
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-09 22:08:59 +0200

    Update B_AssignORC_AntiPaladinTalk.d

--- RU-Main/Story/B_AssignAmbientInfos/B_AssignORC_AntiPaladinTalk.d
+++ RU-Main/Story/B_AssignAmbientInfos/B_AssignORC_AntiPaladinTalk.d
@@ -1,4 +1,5 @@
 
+
 var int AntiPaladinTalkcount;
 
 func void B_AssignORC_AntiPaladinTalk(var C_Npc Antipaladin)
@@ -7,32 +8,32 @@ func void B_AssignORC_AntiPaladinTalk(var C_Npc Antipaladin)
 	{
 		if(AntiPaladinTalkcount == 0)
 		{
-			AI_Output(self,other,"DIA_ORC_AntiPaladin_18_01");	//КХРОКГНАР! Хозяин говорить ты паладин теперь! Хозяин прав. Хозяин всегда прав.
-			AI_Output(self,other,"DIA_ORC_AntiPaladin_18_02");	//Мы, предводители гордой армии орков, теперь сами прийти убить великий паладин.
-			AI_Output(self,other,"DIA_ORC_AntiPaladin_18_03");	//Хаген и его люди будут уничтожены. Даже ты не смочь помочь им.
+			AI_Output(self,other, " DIA_ORC_AntiPaladin_18_01 " );	// KHROKGNAR! Master say you are a paladin now! The owner is right. The owner is always right.
+			AI_Output(self,other, " DIA_ORC_AntiPaladin_18_02 " );	// We, the leaders of the proud army of orcs, now come to kill the great paladin ourselves.
+			AI_Output(self,other, " DIA_ORC_AntiPaladin_18_03 " );	// Hagen and his people will be destroyed. Even you can't help them.
 			B_GivePlayerXP(XP_Ambient);
 		}
 		else if(AntiPaladinTalkcount == 1)
 		{
-			AI_Output(self,other,"DIA_ORC_AntiPaladin_18_04");	//Хозяин говорить предводителям орков, принесите голову великого паладина. Ты умирать от моей руки. КХРОКГНАР!
+			AI_Output(self,other, " DIA_ORC_AntiPaladin_18_04 " );	// Master to tell the orc leaders, bring the head of the great paladin. You die by my hand. KHROKGNAR!
 		}
 		else if(AntiPaladinTalkcount == 2)
 		{
-			AI_Output(self,other,"DIA_ORC_AntiPaladin_18_05");	//Мы, предводители орков, теперь сами идти убивать тебя, паладин.
+			AI_Output(self,other, " DIA_ORC_AntiPaladin_18_05 " );	// We, the leaders of the orcs, now go to kill you, paladin.
 		}
 		else
 		{
-			AI_Output(self,other,"DIA_ORC_AntiPaladin_18_06");	//КХРОКГНАР! Мы убивать тебя, паладин.
+			AI_Output(self,other, " DIA_ORC_AntiPaladin_18_06 " );	// KHROKGNAR! We kill you, paladin.
 		};
 	}
 	else
 	{
-		AI_Output(self,other,"DIA_Orc_8215_Kan_PreHello_01_03");	//ДАБАР КАРАД ГОР ТАК НИР ВАК.
+		AI_Output(self,other, " DIA_Orc_8215_Kan_PreHello_01_03 " );	// DABAR KARAD GOR TAK NIR VAK.
 	};
 
 	AntiPaladinTalkcount = AntiPaladinTalkcount + 1;
 	TalkedTo_AntiPaladin = TRUE;
-	Antipaladin.aivar[AIV_EnemyOverride] = FALSE;
+	Antipaladin.aivar[AIV_EnemyOverride] = FALSE ;
 	AI_StopProcessInfos(Antipaladin);
 	Npc_SetRefuseTalk(Antipaladin,20);
 };
