d83725bcdc97ed972bef15dc091a14422984ffd1
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-09 20:05:08 +0200

    Update DIA_BDT_10019_Addon_Wache.d

--- RU-Main/Story/Dialoge/DIA_BDT_10019_Addon_Wache.d
+++ RU-Main/Story/Dialoge/DIA_BDT_10019_Addon_Wache.d
@@ -42,29 +42,29 @@ func void DIA_Addon_10019_Wache_Hi_Info()
 	AI_Output(self,other, " DIA_Addon_BDT_10019_Wache_Hi_06_00 " );	// Heading to the mine?
 	Info_ClearChoices(DIA_Addon_BDT_10019_Wache_Hi);
 	Info_AddChoice(DIA_Addon_BDT_10019_Wache_Hi, " Actually, I wanted to see Raven. " ,DIA_Addon_BDT_10019_Wache_Hi_Raven);
-	Info_AddChoice(DIA_Addon_BDT_10019_Wache_Hi,"Хорошо.",DIA_Addon_BDT_10019_Wache_Hi_Ja);
+	Info_AddChoice(DIA_Addon_BDT_10019_Wache_Hi,"Alright.",DIA_Addon_BDT_10019_Wache_Hi_Ja);
 };
 
 func void B_Say_Guard_broken()
 {
 	AI_Output(other,self, " DIA_Addon_BDT_10019_Wache_kaputt_15_00 " );	// Why did they die?
-	AI_Output(self,other, " DIA_Addon_BDT_10019_Wache_kaputt_06_01 " );	// (laughs viciously) Worked to death.
+	AI_Output(self,other, " DIA_Addon_BDT_10019_Wache_kaputt_06_01 " );	// Ha! Worked to death.
 	Info_ClearChoices(DIA_Addon_BDT_10019_Wache_Hi);
 };
 
 func void DIA_Addon_BDT_10019_Wache_Hi_Raven()
 {
 	AI_Output(other,self, " DIA_Addon_BDT_10019_Wache_Hi_Raven_15_00 " );	// Actually, I wanted to see Raven.
-	AI_Output(self,other, " DIA_Addon_BDT_10019_Wache_Hi_Raven_06_01 " );	// (warning) Hey! Just try to enter the temple - and you will perish just like these prisoners.
-	AI_Output(self,other, " DIA_Addon_BDT_10019_Wache_Hi_Raven_06_02 " );	// Let's better move into the mine.
+	AI_Output(self,other, " DIA_Addon_BDT_10019_Wache_Hi_Raven_06_01 " );	// Hey! Just try to enter the temple - and you will end up just like these prisoners.
+	AI_Output(self,other, " DIA_Addon_BDT_10019_Wache_Hi_Raven_06_02 " );	// Get yourself into the mine.
 	B_Say_Guard_broken();
 	Info_ClearChoices(DIA_Addon_BDT_10019_Wache_Hi);
 };
 
 func void DIA_Addon_BDT_10019_Wache_Hi_Ja()
 {
-	AI_Output(other,self,"DIA_Addon_BDT_10019_Wache_Hi_Ja_15_00");	//Хорошо.
-	AI_Output(self,other, " DIA_Addon_BDT_10019_Wache_Hi_Ja_06_01 " );	// Then try to be more careful than those poor guys.
+	AI_Output(other,self,"DIA_Addon_BDT_10019_Wache_Hi_Ja_15_00");	//Good.
+	AI_Output(self,other, " DIA_Addon_BDT_10019_Wache_Hi_Ja_06_01 " );	// Then try to be more careful than these poor slobs.
 	B_Say_Guard_broken();
 	Info_ClearChoices(DIA_Addon_BDT_10019_Wache_Hi);
 };
