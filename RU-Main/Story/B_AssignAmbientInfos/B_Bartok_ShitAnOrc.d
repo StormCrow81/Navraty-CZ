58ba6495cdb16d2c3d4efc9dca02ab7928fd813e
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-09 22:10:32 +0200

    Update B_Bartok_ShitAnOrc.d

--- RU-Main/Story/B_AssignAmbientInfos/B_Bartok_ShitAnOrc.d
+++ RU-Main/Story/B_AssignAmbientInfos/B_Bartok_ShitAnOrc.d
@@ -1,6 +1,7 @@
 
+
 func void B_Bartok_ShitAnOrc()
 {
-	AI_Output(self,other,"DIA_Bartok_Angekommen_04_02");	//(себе под нос) Орк прямо у ворот города - Черт побери...
+	AI_Output(self,other, " DIA_Bartok_Angekommen_04_02 " );	// (to himself) Orc right at the gates of the city - Damn...
 };
 
