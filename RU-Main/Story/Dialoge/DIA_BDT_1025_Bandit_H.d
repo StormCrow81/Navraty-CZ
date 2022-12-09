338317ed07f2732869450990a350f4eb6ef539c3
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-09 21:18:14 +0200

    Update DIA_BDT_1025_Bandit_H.d

--- RU-Main/Story/Dialoge/DIA_BDT_1025_Bandit_H.d
+++ RU-Main/Story/Dialoge/DIA_BDT_1025_Bandit_H.d
@@ -39,11 +39,11 @@ func int DIA_NAME_BANDIT_SCHAFBLEIBTHIER_Condition()
 
 func void DIA_NAME_BANDIT_SCHAFLEIBTHIER_Info()
 {
-	AI_Output(self,other, " DIA_NAME_BANDIT_SCHAFBLEIBTHIER_09_00 " );	// Hey you nerd! You didn't come here by chance for a sheep, did you?
+	AI_Output(self,other, " DIA_NAME_BANDIT_SCHAFBLEIBTHIER_09_00 " );	// Hey you, poof! You didn't come here for sheep, did you?
 	if (MIS_Akil_SchafDiebe ==  0 )
 	{
 		AI_Output(other,self, " DIA_NAME_BANDIT_SCHAFBLEIBTHIER_15_01 " );	// Sheep? What sheep?
-		AI_Output(self,other, " DIA_NAME_BANDIT_SCHAFBLEIBTHIER_09_02 " );	// This is good. Now get lost. There is nothing for you to do here.
+		AI_Output(self,other, " DIA_NAME_BANDIT_SCHAFBLEIBTHIER_09_02 " );	// That's good. Now get lost. There is nothing for you here.
 	};
 };
 
@@ -68,7 +68,7 @@ func int DIA_NAME_BANDIT_SchafKlau_Condition()
 
 func void DIA_NAME_BANDIT_SchafKlau_Info()
 {
-	AI_Output(self,other, " DIA_NAME_BANDIT_SchafKlau_09_00 " );	// Hey! I am speaking with you.
+	AI_Output(self,other, " DIA_NAME_BANDIT_SchafKlau_09_00 " );	// Hey! I'm talking to you.
 	Info_AddChoice(DIA_NAME_BANDIT_SchafKlau,Dialog_End,DIA_NAME_BANDIT_SchafKlau_weiter);
 };
 
@@ -105,17 +105,17 @@ var int DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_NoPerm;
 
 func void DIA_NAME_BANDIT_RETURNDASSCHAFRAUS_Info()
 {
-	AI_Output(other,self, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_15_00 " );	// You stole a sheep from Farmer Akil.
-	AI_Output(self,other, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_09_01 " );	// So what? What will you do?
+	AI_Output(other,self, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_15_00 " );	// You stole a sheep from Akil the farmer.
+	AI_Output(self,other, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_09_01 " );	// So what if I did? What will you do about it?
 	Info_ClearChoices(dia_name_bandit_rueckdasschafraus);
-	Info_AddChoice(dia_name_bandit_rueckdasschafraus, " Nothing. Why do I need this stupid sheep? " ,DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_nichts);
-	Info_AddChoice(dia_name_bandit_rueckdasschafraus, " Give me that sheep or you'll get shot in the eye. " ,DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_schaf);
+	Info_AddChoice(dia_name_bandit_rueckdasschafraus, " Nothing. What do I care for some stupid sheep? " ,DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_nichts);
+	Info_AddChoice(dia_name_bandit_rueckdasschafraus, " Give me that sheep or you'll get it. " ,DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_schaf);
 };
 
 func void DIA_NAME_BANDIT_RETURNDASSCHAFRAUS_sheep()
 {
-	AI_Output(other,self, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_schaf_15_00 " );	// Give me that sheep, or you'll get shot in the eye.
-	AI_Output(self,other, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_schaf_09_01 " );	// Then watch out, mate. Now you're going to have a hard time.
+	AI_Output(other,self, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_schaf_15_00 " );	// Give me that sheep, or you'll get it.
+	AI_Output(self,other, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_schaf_09_01 " );	// Then I guess you and I are gonna have a problem, aren't we?
 	AI_StopProcessInfos(self);
 	DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_NoPerm = TRUE ;
 	B_Attack(self,other,AR_SuddenEnemyInferno,1);
@@ -125,8 +125,8 @@ func void DIA_NAME_BANDIT_RETURNDASSCHAFRAUS_sheep()
 
 func void DIA_NAME_BANDIT_RETURNTHISSCAFRAUS_nothing()
 {
-	AI_Output(other,self, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_nichts_15_00 " );	// Nothing. And why do I need this stupid sheep?
-	AI_Output(self,other, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_nichts_09_01 " );	// Then don't get on my nerves. Get out!
+	AI_Output(other,self, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_nichts_15_00 " );	// Nothing. What do I care for some stupid sheep?
+	AI_Output(self,other, " DIA_NAME_BANDIT_RUECKDASSCHAFRAUS_nichts_09_01 " );	// Then don't waste my time. Get out!
 	hero.aivar[AIV_LastDistToWP] = Npc_GetDistToWP(hero,"NW_FOREST_CAVE1_IN_01");
 	AI_StopProcessInfos(self);
 };
