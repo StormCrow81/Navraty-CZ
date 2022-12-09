9903e23c33bd26e50abd0911706512845f1bd4b6
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-09 22:02:07 +0200

    Update B_Addon_Riordian_TeachWisp.d

--- RU-Main/Story/B_AssignAmbientInfos/B_Addon_Riordian_TeachWisp.d
+++ RU-Main/Story/B_AssignAmbientInfos/B_Addon_Riordian_TeachWisp.d
@@ -1,21 +1,22 @@
 
+
 func void B_DIA_Addon_Riordian_Teach_15_00()
 {
-	AI_Output(other,self,"DIA_Addon_Riordian_Teach_15_00");	//Покажи мне, как обучить мой огонек.
+	AI_Output(other,self, " DIA_Addon_Riordian_Teach_15_00 " );	// Show me how to train my wisp.
 };
 
 func void B_DIA_Addon_Riordian_Teach_WISPSKILL_X_10_00()
 {
-	AI_Output(self,other,"DIA_Addon_Riordian_Teach_WISPSKILL_X_10_00");	//Теперь твой огонек может найти кое-что еще, что тебе пригодится.
+	AI_Output(self,other, " DIA_Addon_Riordian_Teach_WISPSKILL_X_10_00 " );	// Now your wisp can find something else you might need.
 };
 
 func void B_DIA_Addon_Riordian_Teach_10_08()
 {
-	AI_Output(self,other,"DIA_Addon_Riordian_Teach1_10_00");	//Твой огонек ищет теперь все, что имеет смысл искать. Больше я его ничему научить не могу.
+	AI_Output(self,other, " DIA_Addon_Riordian_Teach1_10_00 " );	// Your flame is now looking for everything that makes sense to look for. There is nothing more I can teach him.
 };
 
 func void B_DIA_Addon_Riordian_Teach_10_01()
 {
-	AI_Output(self,other,"DIA_Addon_Riordian_Teach2_10_00");	//Что должен искать твой огонек?
+	AI_Output(self,other, " DIA_Addon_Riordian_Teach2_10_00 " );	// What should your flame be looking for?
 };
 
