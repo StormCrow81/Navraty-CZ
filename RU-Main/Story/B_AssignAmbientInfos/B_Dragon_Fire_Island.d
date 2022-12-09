7a320cb322fa4c639a72fa6b93cc19f20ea7a773
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-09 22:11:16 +0200

    Update B_Dragon_Fire_Island.d

--- RU-Main/Story/B_AssignAmbientInfos/B_Dragon_Fire_Island.d
+++ RU-Main/Story/B_AssignAmbientInfos/B_Dragon_Fire_Island.d
@@ -1,4 +1,5 @@
 
+
 instance DIA_Dragon_Fire_Island_Exit(C_Info)
 {
 	npc = Dragon_Fire_Island;
@@ -46,24 +47,24 @@ func int DIA_Dragon_Fire_Island_Hello_Condition()
 
 func void DIA_Dragon_Fire_Island_Hello_Info()
 {
-	AI_Output(self,other,"DIA_Dragon_Fire_Island_Hello_20_00");	//Стой, человек. Все мои чувства говорят мне, что убийца моей плоти и крови стоит передо мной.
-	AI_Output(other,self,"DIA_Dragon_Fire_Island_Hello_15_01");	//Да? Кого ты назвал убийцей, ты, порождение ада?
+	AI_Output(self,other, " DIA_Dragon_Fire_Island_Hello_20_00 " );	// Stop, man. All my senses tell me that the killer of my flesh and blood is standing before me.
+	AI_Output(other,self, " DIA_Dragon_Fire_Island_Hello_15_01 " );	// Yes? Who did you call the killer, you hellspawn?
 	if((hero.guild == GIL_DJG) && (DragonEggCounter >= 7))
 	{
-		AI_Output(self,other,"DIA_Dragon_Fire_Island_Hello_20_02");	//Тебе недостаточно того, что ты уничтожил почти всех наших отпрысков?
+		AI_Output(self,other, " DIA_Dragon_Fire_Island_Hello_20_02 " );	// It's not enough for you that you destroyed almost all of our offspring?
 	};
-	AI_Output(self,other,"DIA_Dragon_Fire_Island_Hello_20_03");	//На твоей совести также мой брат Феоматар, и ты заплатишь за это.
+	AI_Output(self,other, " DIA_Dragon_Fire_Island_Hello_20_03 " );	// My brother Feomatar is also on your conscience, and you will pay for this.
 	if((hero.guild == GIL_DJG) && (DragonEggCounter >= 7))
 	{
-		AI_Output(other,self,"DIA_Dragon_Fire_Island_Hello_15_04");	//Я также собираюсь добраться до остальных из вашего проклятого племени.
-		AI_Output(self,other,"DIA_Dragon_Fire_Island_Hello_20_05");	//Ах-х. Я с огромным удовольствием сожгу тебя заживо.
+		AI_Output(other,self, " DIA_Dragon_Fire_Island_Hello_15_04 " );	// I'm also going to get to the rest of your cursed tribe.
+		AI_Output(self,other, " DIA_Dragon_Fire_Island_Hello_20_05 " );	// Ahh. I will burn you alive with great pleasure.
 	};
-	AI_Output(other,self,"DIA_Dragon_Fire_Island_Hello_15_06");	//Хватит попусту сотрясать воздух. Давай перейдем к делу.
-	AI_Output(self,other,"DIA_Dragon_Fire_Island_Hello_20_07");	//Ты зашел слишком далеко, жалкий человечишка, но тебе не уйти отсюда живым.
+	AI_Output(other,self, " DIA_Dragon_Fire_Island_Hello_15_06 " );	// Stop shaking the air. Let's get down to business.
+	AI_Output(self,other, " DIA_Dragon_Fire_Island_Hello_20_07 " );	// You've gone too far, you pathetic human, but you won't get out of here alive.
 	Npc_RemoveInvItems(other,ItMi_InnosEye_MIS,1);
 	CreateInvItems(other,ItMi_InnosEye_Discharged_Mis,1);
 	AI_StopProcessInfos(self);
-	B_LogEntry(TOPIC_HallenVonIrdorath,"Еще один огненный дракон. Я уже по горло сыт этими тварями. Мне всего лишь нужно перебраться через пропасть, что лежит за этим огненным драконом.");
+	B_LogEntry(TOPIC_HallenVonIrdorath, " Another fire dragon. I'm fed up with these creatures. I just need to get across the chasm that lies behind this fire dragon. " );
 	DragonTalk_Exit_Free = FALSE;
 	self.flags = 0;
 };
