f134e90cae900170723b84ceeddd703addc51eae
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-09 22:02:33 +0200

    Update B_AkilsSchaf_KommMit.d

--- RU-Main/Story/B_AssignAmbientInfos/B_AkilsSchaf_KommMit.d
+++ RU-Main/Story/B_AssignAmbientInfos/B_AkilsSchaf_KommMit.d
@@ -1,17 +1,18 @@
 
-instance DIA_AkilsSchaf_KommMit(C_Info)
+
+instance DIA_AkilsSchaf_KommMit (C_Info)
 {
 	npc = Follow_Sheep_AKIL;
 	nr = 1;
 	condition = DIA_AkilsSchaf_KommMit_Condition;
 	information = DIA_AkilsSchaf_KommMit_Info;
-	description = "Пойдем со мной!";
+	description = " Come with me! " ;
 };
 
 
 func int DIA_AkilsSchaf_KommMit_Condition()
 {
-	if(self.aivar[AIV_PARTYMEMBER] == FALSE)
+	if (self.aivar[ AIV_PARTYMEMBER ] ==  FALSE )
 	{
 		return TRUE;
 	};
@@ -19,16 +20,16 @@ func int DIA_AkilsSchaf_KommMit_Condition()
 
 func void DIA_AkilsSchaf_KommMit_Info()
 {
-	B_KommMit();
+	B_Commit();
 	B_LieselMaeh();
-	self.aivar[AIV_PARTYMEMBER] = TRUE;
-	if(Npc_IsDead(BDT_1025_Bandit_H) == FALSE)
+	self.aivar[ AIV_PARTYMEMBER ] = TRUE ;
+	if (Npc_IsDead(BDT_1025_Bandit_H) ==  FALSE )
 	{
-		BDT_1025_Bandit_H.aivar[AIV_EnemyOverride] = FALSE;
+		BDT_1025_Bandit_H.aivar[AIV_EnemyOverride] = FALSE ;
 	};
-	if(Npc_IsDead(BDT_1026_Bandit_H) == FALSE)
+	if (Npc_IsDead(BDT_1026_Bandit_H) ==  FALSE )
 	{
-		BDT_1026_Bandit_H.aivar[AIV_EnemyOverride] = FALSE;
+		BDT_1026_Bandit_H.aivar[AIV_EnemyOverride] = FALSE ;
 	};
 	AI_StopProcessInfos(self);
 };
