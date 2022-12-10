e739645540f8ba39b48214850f799277c9779886
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-10 14:23:49 +0200

    Update B_AssignAmbientInfos_BAU_1.d

--- RU-Main/Story/B_AssignAmbientInfos/B_AssignAmbientInfos_BAU_1.d
+++ RU-Main/Story/B_AssignAmbientInfos/B_AssignAmbientInfos_BAU_1.d
@@ -45,7 +45,7 @@ func void DIA_BAU_1_JOIN_Info()
 	AI_Output(other, self, " DIA_BAU_1_JOIN_15_00 " );	// Tell me more about these mercenaries.
 	AI_Output(self,other, " DIA_BAU_1_JOIN_01_01 " );	// I can only advise you to stay away from them.
 	AI_Output(self,other, " DIA_BAU_1_JOIN_01_02 " );	// If one of them doesn't like your face, they can punch you in the nose without hesitation.
-	AI_Output(self,other, " DIA_BAU_1_JOIN_01_03 " );	// Of course, after that you can complain to Lee, but the nose will already be broken.
+	AI_Output(self,other, " DIA_BAU_1_JOIN_01_03 " );	// Of course, after that you can complain to Lee, but your nose will still be broken.
 };
 
 
@@ -68,8 +68,8 @@ func void DIA_BAU_1_PEOPLE_Info()
 {
 	AI_Output(other,self, " DIA_BAU_1_PEOPLE_15_00 " );	// Who's in charge here?
 	AI_Output(self,other, " DIA_BAU_1_PEOPLE_01_01 " );	// This is Onar's farm. Here he makes all the decisions.
-	AI_Output(self,other, " DIA_BAU_1_PEOPLE_01_02 " );	// Let me give you some advice: don't get into a fight with his mercenaries. These guys are not to be trifled with.
-	AI_Output(self,other, " DIA_BAU_1_PEOPLE_01_03 " );	// Silvio is generally a notorious bandit. But their leader, Lee, is a perfectly normal guy.
+	AI_Output(self,other, " DIA_BAU_1_PEOPLE_01_02 " );	// Let me give you some advice: don't get into a fight with his mercenaries. Those guys are not to be trifled with.
+	AI_Output(self,other, " DIA_BAU_1_PEOPLE_01_03 " );	// Silvio is a notorious bandit. But their leader, Lee, is a perfectly normal guy.
 };
 
 
@@ -91,7 +91,7 @@ func int DIA_BAU_1_LOCATION_Condition()
 func void DIA_BAU_1_LOCATION_Info()
 {
 	AI_Output(other, self, " DIA_BAU_1_LOCATION_15_00 " );	// What's interesting here?
-	AI_Output(self,other, " DIA_BAU_1_LOCATION_01_01 " );	// There are only fields and farms here. And a handful of mercenaries. But almost all of them are in the east, on Onar's farm.
+	AI_Output(self,other, " DIA_BAU_1_LOCATION_01_01 " );	// There are only fields and farms here, and a handful of mercenaries. But almost all of them are in the east, on Onar's farm.
 	AI_Output(self,other, " DIA_BAU_1_LOCATION_01_02 " );	// Secoba's farm is to the north, but they only go there to collect rent.
 	AI_Output(self,other, " DIA_BAU_1_LOCATION_01_03 " );	// To the south is a passage to Bengar's farm.
 };
@@ -117,27 +117,27 @@ func void DIA_BAU_1_STANDARD_Info()
 	AI_Output(other,self, " DIA_BAU_1_STANDARD_15_00 " );	// What's new?
 	if (chapter ==  1 )
 	{
-		AI_Output(self,other, " DIA_BAU_1_STANDARD_01_01 " );	// Onar hired mercenaries to protect him from the city's militia. If not for the mercenaries, they would have taken everything from us, to the last sheep!
+		AI_Output(self,other, " DIA_BAU_1_STANDARD_01_01 " );	// Onar hired mercenaries to protect him from the city's militia. If not for the mercenaries, they would have taken everything from us, down to the last sheep!
 	};
 	if (chapter ==  2 )
 	{
-		AI_Output(self,other, " DIA_BAU_1_STANDARD_01_02 " );	// Not much. As always, the same problems. Militia, orcs and field predators.
+		AI_Output(self,other, " DIA_BAU_1_STANDARD_01_02 " );	// Not much. Same shit, different shade of brown. Militia, orcs and field predators.
 	};
 	if (chapter ==  3 )
 	{
-		AI_Output(self,other, " DIA_BAU_1_STANDARD_01_03 " );	// Strange things are happening here. A few days ago I saw a figure in black. It wasn't a person.
+		AI_Output(self,other, " DIA_BAU_1_STANDARD_01_03 " );	// Strange things are happening here. A few days ago I saw a figure in black. It was no man, I know that in my soul.
 	};
 	if (chapter ==  4 )
 	{
-		AI_Output(self,other, " DIA_BAU_1_STANDARD_01_04 " );	// More and more orcs appear here. Sometimes it seems to me that for every orc killed, two more appear.
+		AI_Output(self,other, " DIA_BAU_1_STANDARD_01_04 " );	// More and more orcs keep appearing. Sometimes it seems to me that for every orc killed, two more appear.
 	};
 	if (chapter ==  5 )
 	{
-		AI_Output(self,other, " DIA_BAU_1_STANDARZ_01_05 " );	// This undead is driving me crazy! If this continues, we will all have a hard time.
+		AI_Output(self,other, " DIA_BAU_1_STANDARZ_01_05 " );	// This plague of undead is driving me crazy! If this continues, we will all be walking corpses soon.
 	};
 	if (Chapter >=  6 )
 	{
-		AI_Output(self,other, " DIA_BAU_1_STANDARD_01_06 " );	// Orcs have captured Khorinis! Now we all will soon come to an end!
+		AI_Output(self,other, " DIA_BAU_1_STANDARD_01_06 " );	// Orcs have captured Khorinis! It's over, do you understand? OVER!
 	};
 };
 
