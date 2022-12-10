faeca3197ea06549784e6b25ba6f86495b0463ee
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-10 15:44:48 +0200

    Update B_Addon_Riordian_TeachWisp.d

--- RU-Main/Story/B_AssignAmbientInfos/B_Addon_Riordian_TeachWisp.d
+++ RU-Main/Story/B_AssignAmbientInfos/B_Addon_Riordian_TeachWisp.d
@@ -7,16 +7,16 @@ func void B_DIA_Addon_Riordian_Teach_15_00()
 
 func void B_DIA_Addon_Riordian_Teach_WISPSKILL_X_10_00()
 {
-	AI_Output(self,other, " DIA_Addon_Riordian_Teach_WISPSKILL_X_10_00 " );	// Now your wisp can find something else you might need.
+	AI_Output(self,other, " DIA_Addon_Riordian_Teach_WISPSKILL_X_10_00 " );	// Now your wisp can find something you might need.
 };
 
 func void B_DIA_Addon_Riordian_Teach_10_08()
 {
-	AI_Output(self,other, " DIA_Addon_Riordian_Teach1_10_00 " );	// Your flame is now looking for everything that makes sense to look for. There is nothing more I can teach him.
+	AI_Output(self,other, " DIA_Addon_Riordian_Teach1_10_00 " );	// Your wisp is capable of finding the things you might need already. There is nothing more I can teach him.
 };
 
 func void B_DIA_Addon_Riordian_Teach_10_01()
 {
-	AI_Output(self,other, " DIA_Addon_Riordian_Teach2_10_00 " );	// What should your flame be looking for?
+	AI_Output(self,other, " DIA_Addon_Riordian_Teach2_10_00 " );	// What should your wisp be searching for?
 };
 
