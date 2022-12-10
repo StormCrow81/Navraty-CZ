0820c70c7f78796daaaabb5a501930dcd956405b
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-10 15:42:50 +0200

    Update B_Addon_Myxir_TeachRequest.d

--- RU-Main/Story/B_AssignAmbientInfos/B_Addon_Myxir_TeachRequest.d
+++ RU-Main/Story/B_AssignAmbientInfos/B_Addon_Myxir_TeachRequest.d
@@ -9,25 +9,25 @@ func void B_DIA_Addon_Myxir_TeachL1()
 {
 	AI_Output(self,other, " DIA_Addon_Myxir_TeachL1_12_00 " );	// Well, let's start with something simple. Let us first deal with the language of the peasants.
 	AI_Output(self,other, " DIA_Addon_Myxir_TeachL1_12_01 " );	// As a rule, texts written in the language of peasants are connected with our world, with such concepts as work, love, food production.
-	AI_Output(self,other, " DIA_Addon_Myxir_TeachL1_12_02 " );	// This is the main language of the city. You can read most of the texts you find here if you master it.
+	AI_Output(self,other, " DIA_Addon_Myxir_TeachL1_12_02 " );	// This is the main language of the city. You can read most of the texts you find there if you master it.
 };
 
 func void B_DIA_Addon_Myxir_TeachL2()
 {
 	AI_Output(self,other, " DIA_Addon_Myxir_TeachL2_12_00 " );	// You are already familiar with the language of the peasants. The language of the warriors is a bit more complex.
-	AI_Output(self,other, " DIA_Addon_Myxir_TeachL2_12_01 " );	// Texts written in the language of warriors mainly deal with wars and weapons. You will learn something useful.
+	AI_Output(self,other, " DIA_Addon_Myxir_TeachL2_12_01 " );	// Texts written in the language of warriors mainly deal with wars, weapons and martial arts.
 };
 
 func void B_DIA_Addon_Myxir_TeachL3()
 {
-	AI_Output(self,other, " DIA_Addon_Myxir_TeachL3_12_00 " );	// The high calm of the priests is hard to understand. But I'll gladly teach it to you.
+	AI_Output(self,other, " DIA_Addon_Myxir_TeachL3_12_00 " );	// The high tongues of the priests is hard to understand. But I'll gladly teach it to you if I can.
 	AI_Output(self,other, " DIA_Addon_Myxir_TeachL3_12_01 " );	// Among other things, the sacred books that describe the history and magic of the creators are written in the language of the priests.
-	AI_Output(self,other, " DIA_Addon_Myxir_TeachL3_12_02 " );	// Each of these texts is a real treasure, if, of course, you can understand it.
+	AI_Output(self,other, " DIA_Addon_Myxir_TeachL3_12_02 " );	// Each of these texts is a treasure of arcane knowledge, if, of course, you can understand it.
 };
 
 func void B_DIA_Addon_Myxir_TeachNoMore()
 {
-	AI_Output(self,other, " DIA_Addon_Myxir_TeachNoMore_12_00 " );	// I have nothing more to show you. You have learned the language of architects.
+	AI_Output(self,other, " DIA_Addon_Myxir_TeachNoMore_12_00 " );	// I have nothing more to show you. You have learned the language of the architects.
 };
 
 func void B_DIA_Addon_Myxir_Teach_LANGUAGE_X()
