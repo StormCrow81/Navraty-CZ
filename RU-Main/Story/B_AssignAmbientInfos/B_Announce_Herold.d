f79a7b08b7d4eba9aea64b47b0b9a88a217fdb9b
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-10 15:49:27 +0200

    Update B_Announce_Herold.d

--- RU-Main/Story/B_AssignAmbientInfos/B_Announce_Herold.d
+++ RU-Main/Story/B_AssignAmbientInfos/B_Announce_Herold.d
@@ -43,21 +43,21 @@ func void B_Announce_Herold()
 			else if(randy == 3)
 			{
 				AI_Output(self,self, " DIA_Herold_Announce_04_07 " );	// Martial law is declared in all cities and provinces of the kingdom.
-				AI_Output(self,self, " DIA_Herold_Announce_04_08 " );	// All civilian judges should lay down their duties and turn them over to royal paladins.
-				AI_Output(self,self, " DIA_Herold_Announce_04_09 " );	// Highly Honored Lord Andre is ordered to punish any crime or act of resistance to the royal guard in the most cruel manner.
-				AI_Output(self,self, " DIA_Herold_Announce_04_10 " );	// All residents of Khorinis guilty of a crime must immediately come to Lord Andre.
+				AI_Output(self,self, " DIA_Herold_Announce_04_08 " );	// All civilian judges will lay down their duties and turn them over to the royal paladins.
+				AI_Output(self,self, " DIA_Herold_Announce_04_09 " );	// The highly Honored Lord Andre is ordered to punish any crime or act of resistance to the royal guard in the most stern manner.
+				AI_Output(self,self, " DIA_Herold_Announce_04_10 " );	// All residents of Khorinis guilty of a crime must immediately come to Lord Andre to fave judgement.
 			}
 			else
 			{
 				AI_Output(self,self, " DIA_Herold_Announce_04_11 " );	// Considering the danger of an orc attack on the city, all the inhabitants of the city should prepare accordingly.
-				AI_Output(self,self, " DIA_Herold_Announce_04_12 " );	// Everyone should immediately start learning martial skills and arm themselves properly.
+				AI_Output(self,self, " DIA_Herold_Announce_04_12 " );	// Everyone should immediately start learning martial skills and arm themselves properly, as well as stock food and water.
 			};
 		}
 		else  if (chapter ==  3 )
 		{
 			if(MIS_RescueBennet != LOG_SUCCESS)
 			{
-				AI_Output(self,self, " DIA_Herold_Announce_04_13 " );	// The mercenary blacksmith Bennet, who treacherously killed the paladin Lothar, is declared cursed on behalf of Innos.
+				AI_Output(self,self, " DIA_Herold_Announce_04_13 " );	// The mercenary blacksmith Bennet, who treacherously killed the paladin Lothar, is declared accursed in the eyes of Innos.
 				AI_Output(self,self, " DIA_Herold_Announce_04_14 " );	// He is sentenced to death by hanging. The sentence will be carried out in the coming days.
 			}
 			else
@@ -67,7 +67,7 @@ func void B_Announce_Herold()
 		}
 		else  if (chapter ==  4 )
 		{
-			AI_Output(self,self, " DIA_Herold_Announce_04_16 " );	// Rumors of dragons appearing in the Khorinis Mine Valley are false.
+			AI_Output(self,self, " DIA_Herold_Announce_04_16 " );	// Rumors of dragons appearing in the Khorinis Mine Valley are lies and vicious misinformation.
 			AI_Output(self,self, " DIA_Herold_Announce_04_17 " );	// These rumors are spread by the enemy in order to sow fear and horror in the souls of the courageous inhabitants of Myrtana.
 			AI_Output(self,self, " DIA_Herold_Announce_04_18 " );	// To dispel these ridiculous rumors, a band of brave paladins was sent to the Valley of Mines under the command of a man who is well acquainted with this area.
 		}
@@ -82,7 +82,7 @@ func void B_Announce_Herold()
 			}
 			else
 			{
-				AI_Output(self,self, " DIA_Herold_Announce_04_21 " );	// Lord Andre is appointed supreme commander of the city of Khorinis. He takes up this position immediately.
+				AI_Output(self,self, " DIA_Herold_Announce_04_21 " );	// Lord Andre is appointed supreme commander of the city of Khorinis. He will take up this position immediately.
 				AI_Output(self,self, " DIA_Herold_Announce_04_22 " );	// Lord Hagen announced that he was personally heading to the Valley of Mines to ensure that the ship was loaded with magic ore.
 			};
 		};
