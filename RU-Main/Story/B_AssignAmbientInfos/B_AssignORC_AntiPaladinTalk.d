2ab82607ffd9508a662598032230680a9aab6bbb
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-10 22:48:02 +0200

    Update B_AssignORC_AntiPaladinTalk.d

--- RU-Main/Story/B_AssignAmbientInfos/B_AssignORC_AntiPaladinTalk.d
+++ RU-Main/Story/B_AssignAmbientInfos/B_AssignORC_AntiPaladinTalk.d
@@ -8,22 +8,22 @@ func void B_AssignORC_AntiPaladinTalk(var C_Npc Antipaladin)
 	{
 		if(AntiPaladinTalkcount == 0)
 		{
-			AI_Output(self,other, " DIA_ORC_AntiPaladin_18_01 " );	// KHROKGNAR! Master say you are a paladin now! The owner is right. The owner is always right.
+			AI_Output(self,other, " DIA_ORC_AntiPaladin_18_01 " );	// KHROKGNAR! Master say you are a paladin now! The leader is right. The leader is always right.
 			AI_Output(self,other, " DIA_ORC_AntiPaladin_18_02 " );	// We, the leaders of the proud army of orcs, now come to kill the great paladin ourselves.
 			AI_Output(self,other, " DIA_ORC_AntiPaladin_18_03 " );	// Hagen and his people will be destroyed. Even you can't help them.
 			B_GivePlayerXP(XP_Ambient);
 		}
 		else if(AntiPaladinTalkcount == 1)
 		{
-			AI_Output(self,other, " DIA_ORC_AntiPaladin_18_04 " );	// Master to tell the orc leaders, bring the head of the great paladin. You die by my hand. KHROKGNAR!
+			AI_Output(self,other, " DIA_ORC_AntiPaladin_18_04 " );	// Master tell the orc leaders, bring the head of the great paladin. You die by my hand. KHROKGNAR!
 		}
 		else if(AntiPaladinTalkcount == 2)
 		{
-			AI_Output(self,other, " DIA_ORC_AntiPaladin_18_05 " );	// We, the leaders of the orcs, now go to kill you, paladin.
+			AI_Output(self,other, " DIA_ORC_AntiPaladin_18_05 " );	// We, the leaders of the orcs, now come to kill you, paladin.
 		}
 		else
 		{
-			AI_Output(self,other, " DIA_ORC_AntiPaladin_18_06 " );	// KHROKGNAR! We kill you, paladin.
+			AI_Output(self,other, " DIA_ORC_AntiPaladin_18_06 " );	// KHROKGNAR! We kill you now, paladin.
 		};
 	}
 	else
