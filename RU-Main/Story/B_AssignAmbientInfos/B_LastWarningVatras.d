1db188903942f096c152cab85370e428edda8545
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-10 22:26:43 +0200

    Update B_LastWarningVatras.d

--- RU-Main/Story/B_AssignAmbientInfos/B_LastWarningVatras.d
+++ RU-Main/Story/B_AssignAmbientInfos/B_LastWarningVatras.d
@@ -3,17 +3,17 @@
 func void B_LastWarningVatras()
 {
 	AI_Output(self,other, " DIA_Addon_Vatras_LastWarning_ADD_05_00 " );	// Scoundrel, what have you done?
-	AI_Output(self,other, " DIA_Addon_Vatras_LastWarning_ADD_05_01 " );	// The Order of the Water has warned you many times about the danger of being caught in the net of evil.
+	AI_Output(self,other, " DIA_Addon_Vatras_LastWarning_ADD_05_01 " );	// The Order of Water has warned you many times about the danger of being caught in the web of evil.
 	AI_Output(self,other, " DIA_Addon_Vatras_LastWarning_ADD_05_02 " );	// However, I have learned that you have gone astray from the path of all-preserving balance.
-	AI_Output(self,other, " DIA_Addon_Vatras_LastWarning_ADD_05_03 " );	// You killed and robbed everyone and everything. You are guilty of many crimes.
-	AI_Output(self,other, " DIA_Addon_Vatras_LastWarning_ADD_05_04 " );	// Many innocent people died at YOUR hand.
-	AI_Output(self,other, " DIA_Addon_Vatras_LastWarning_ADD_05_05 " );	// Now you are an ally of the forces of darkness.
+	AI_Output(self,other, " DIA_Addon_Vatras_LastWarning_ADD_05_03 " );	// You have killed and robbed. You are guilty of many crimes.
+	AI_Output(self,other, " DIA_Addon_Vatras_LastWarning_ADD_05_04 " );	// Many innocent people died at your hand.
+	AI_Output(self,other, " DIA_Addon_Vatras_LastWarning_ADD_05_05 " );	// You are now truly an ally of the forces of darkness.
 };
 
 func void B_VatrasPissedOff()
 {
-	AI_Output(self,other, " DIA_Addon_Vatras_PissedOffPerm_Add_05_00 " );	// From now on, you can't count on our help.
-	AI_Output(self,other,"DIA_Addon_Vatras_PissedOffPerm_Add_05_01");	//Убирайся!
+	AI_Output(self,other, " DIA_Addon_Vatras_PissedOffPerm_Add_05_00 " );	// From now on, you are shunned by the circle of Water.
+	AI_Output(self,other,"DIA_Addon_Vatras_PissedOffPerm_Add_05_01");	//Begone!
 
 	if(Vatras_IsOnBoard == LOG_SUCCESS)
 	{
