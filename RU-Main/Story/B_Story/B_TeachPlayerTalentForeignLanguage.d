6ffb9e22827f3667848413cfdba44021b86ace8f
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-11 17:48:55 +0200

    Update B_TeachPlayerTalentForeignLanguage.d

--- RU-Main/Story/B_Story/B_TeachPlayerTalentForeignLanguage.d
+++ RU-Main/Story/B_Story/B_TeachPlayerTalentForeignLanguage.d
@@ -1,54 +1,55 @@
 
+
 func int B_TeachPlayerTalentForeignLanguage(var C_Npc slf,var C_Npc oth,var int Language)
 {
 	var int kosten;
 	var int money;
-	var int TEMPSSBMODE;
+	var int  TEMPSSBMODE ;
 
 	if(SBMODE == 2)
 	{
-		TEMPSSBMODE = 2;
+		TEMPSSBMODE = 2 ;
 	}
 	else if(SBMODE == 4)
 	{
-		TEMPSSBMODE = 4;
+		TEMPSSBMODE = 4 ;
 	}
 	else
 	{
-		TEMPSSBMODE = 1;
+		TEMPSSBMODE = 1 ;
 	};
 
 	kosten = B_GetLearnCostTalent(oth,NPC_TALENT_FOREIGNLANGUAGE,Language);
 
 	if(Language == LANGUAGE_4)
 	{
-		money = kosten * 200;
+		money = cost *  200 ;
 	}
 	else
 	{
-		money = (kosten * 200) / TEMPSSBMODE;
+		money = (cost *  200 ) /  TEMPSSBMODE ;
 	};
 
 	if(oth.lp < kosten)
 	{
 		AI_PrintClr(PRINT_NotEnoughLearnPoints,177,58,17);
-		return FALSE;
+		return  FALSE ;
 	};
 	if(Npc_HasItems(oth,ItMi_Gold) < money)
 	{
 		AI_PrintClr(Print_NotEnoughGold,177,58,17);
-		return FALSE;
+		return  FALSE ;
 	};
 
 	oth.lp = oth.lp - kosten;
-	RankPoints = RankPoints + kosten;
+	RankPoints = RankPoints + cost;
 	Npc_RemoveInvItems(oth,ItMi_Gold,money);
 	Log_CreateTopic(TOPIC_Language,LOG_NOTE);
 
 	if(Language == LANGUAGE_1)
 	{
 		PLAYER_TALENT_FOREIGNLANGUAGE[LANGUAGE_1] = TRUE;
-		AI_Print("Изучен обычный язык народа Зодчих...");
+		AI_Print( " Studied the common language of the people of the Architects... " );
 
 		B_LogEntry(TOPIC_Language,LogText_Addon_Language_1);
 
@@ -56,46 +57,46 @@ func int B_TeachPlayerTalentForeignLanguage(var C_Npc slf,var C_Npc oth,var int 
 		{
 			ATR_INTELLECT = ATR_INTELLECT + 1;
 			Npc_SetTalentSkill(oth,NPC_TALENT_INTELLECT,ATR_INTELLECT);
-			AI_Print("Интеллект + 1");
+			AI_Print( " Intelligence + 1 " );
 		};
 	};
 	if(Language == LANGUAGE_2)
 	{
 		PLAYER_TALENT_FOREIGNLANGUAGE[LANGUAGE_2] = TRUE;
-		AI_Print("Изучен язык касты воинов Зодчих...");
+		AI_Print( " The language of the Builders warrior caste has been learned... " );
 		B_LogEntry(TOPIC_Language,LogText_Addon_Language_2);
 
 		if((oth.guild == GIL_NOV) || (oth.guild == GIL_NDW) || (oth.guild == GIL_NDM) || (oth.guild == GIL_KDF) || (oth.guild == GIL_KDW) || (oth.guild == GIL_KDM) || (oth.guild == GIL_GUR))
 		{
 			ATR_INTELLECT = ATR_INTELLECT + 1;
 			Npc_SetTalentSkill(oth,NPC_TALENT_INTELLECT,ATR_INTELLECT);
-			AI_Print("Интеллект + 1");
+			AI_Print( " Intelligence + 1 " );
 		};
 	};
 	if(Language == LANGUAGE_3)
 	{
 		PLAYER_TALENT_FOREIGNLANGUAGE[LANGUAGE_3] = TRUE;
-		AI_Print("Изучен язык касты жрецов Зодчих...");
+		AI_Print( " Learned the language of the Builders priest caste... " );
 		B_LogEntry(TOPIC_Language,LogText_Addon_Language_3);
 
 		if((oth.guild == GIL_NOV) || (oth.guild == GIL_NDW) || (oth.guild == GIL_NDM) || (oth.guild == GIL_KDF) || (oth.guild == GIL_KDW) || (oth.guild == GIL_KDM) || (oth.guild == GIL_GUR))
 		{
 			ATR_INTELLECT = ATR_INTELLECT + 1;
 			Npc_SetTalentSkill(oth,NPC_TALENT_INTELLECT,ATR_INTELLECT);
-			AI_Print("Интеллект + 1");
+			AI_Print( " Intelligence + 1 " );
 		};
 	};
 	if(Language == LANGUAGE_4)
 	{
 		PLAYER_TALENT_FOREIGNLANGUAGE[3] = TRUE;
-		AI_Print("Изучен древний язык демонов...");
+		AI_Print( " Learned ancient demon language... " );
 		B_LogEntry(TOPIC_Language,LOGTEXT_ADDON_LANGUAGE_4);
 
 		if((oth.guild == GIL_NOV) || (oth.guild == GIL_NDW) || (oth.guild == GIL_NDM) || (oth.guild == GIL_KDF) || (oth.guild == GIL_KDW) || (oth.guild == GIL_KDM) || (oth.guild == GIL_GUR))
 		{
 			ATR_INTELLECT = ATR_INTELLECT + 1;
 			Npc_SetTalentSkill(oth,NPC_TALENT_INTELLECT,ATR_INTELLECT);
-			AI_Print("Интеллект + 1");
+			AI_Print( " Intelligence + 1 " );
 		};
 	};
 
