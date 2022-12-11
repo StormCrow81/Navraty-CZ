e1f58cf9ecd9c216d650b0980cd9d394dc9c3442
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-11 21:07:13 +0200

    Update B_TeachPlayerTalentTakeAnimalTrophy.d

--- RU-Main/Story/B_Story/B_TeachPlayerTalentTakeAnimalTrophy.d
+++ RU-Main/Story/B_Story/B_TeachPlayerTalentTakeAnimalTrophy.d
@@ -1,126 +1,127 @@
 
+
 func int B_TeachPlayerTalentTakeAnimalTrophy(var C_Npc slf,var C_Npc oth,var int trophy)
 {
-	var int kosten;
+	var int cost;
 	var int money;
-	var int TEMPSSBMODE;
+	var int  TEMPSSBMODE ;
 
 	if(SBMODE == 2)
 	{
-		TEMPSSBMODE = 2;
+		TEMPSSBMODE = 2 ;
 	}
 	else if(SBMODE == 4)
 	{
-		TEMPSSBMODE = 4;
+		TEMPSSBMODE = 4 ;
 	}
 	else
 	{
-		TEMPSSBMODE = 1;
+		TEMPSSBMODE = 1 ;
 	};
 
 	kosten = B_GetLearnCostTalent(oth,NPC_TALENT_TAKEANIMALTROPHY,trophy);
-	money = (kosten * 75) / TEMPSSBMODE;
+	money = (cost *  75 ) /  TEMPSSBMODE ;
 
 	if(oth.lp < kosten)
 	{
 		AI_PrintClr(PRINT_NotEnoughLearnPoints,177,58,17);
-		return FALSE;
+		return  FALSE ;
 	};
 	if(Npc_HasItems(oth,ItMi_Gold) < money)
 	{
 		AI_PrintClr(Print_NotEnoughGold,177,58,17);
-		return FALSE;
+		return  FALSE ;
 	};
 
 	oth.lp = oth.lp - kosten;
-	RankPoints = RankPoints + kosten;
+	RankPoints = RankPoints + cost;
 	Npc_RemoveInvItems(oth,ItMi_Gold,money);
 	Log_CreateTopic(TOPIC_TalentAnimalTrophy,LOG_NOTE);
-	B_LogEntry(TOPIC_TalentAnimalTrophy,"Теперь я умею:");
+	B_LogEntry(TOPIC_TalentAnimalTrophy, " Now I can: " );
 
 	if(trophy == TROPHY_Teeth)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_Teeth] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Вырывать зубы и клыки'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...вырывать зубы и клыки.");
+		AI_Print( " Learned the skill of butchering prey - 'Pull out teeth and fangs' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...pull out teeth and fangs. " );
 	};
 	if(trophy == TROPHY_Claws)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_Claws] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Ломать когти'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...ломать когти.");
+		AI_Print( " Learned the skill of butchering prey - 'Break claws' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...break claws. " );
 	};
 	if(trophy == TROPHY_Fur)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_Fur] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Снимать шкуры'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...снимать шкуры.");
+		AI_Print( " Learned the skill of butchering prey - 'Skinning' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...skin. " );
 	};
 	if(trophy == TROPHY_ReptileSkin)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_ReptileSkin] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Сдирать кожу с рептилий'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"... сдирать кожу с рептилий.");
+		AI_Print( " Learned butchering skill - 'Flay Reptiles' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ... skinning reptiles. " );
 	};
 	if(trophy == TROPHY_Heart)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_Heart] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Вырезать сердце'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...вырезать сердца у големов и демонов.");
+		AI_Print( " Learned the skill of butchering the prey - 'Cut out the heart' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...carve hearts from golems and demons. " );
 	};
 	if(trophy == TROPHY_ShadowHorn)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_ShadowHorn] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Отламывать рог мракорисов'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...отламывать рог у мракорисов.");
+		AI_Print( " Learned the skill of butchering the prey - 'Break off the horn of the obscurantists' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...break off the horns of the Aurors. " );
 	};
 	if(trophy == TROPHY_FireTongue)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_FireTongue] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Вырезать огненный язык'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...вырезать огненный язык у огненных ящериц.");
+		AI_Print( " Learned the skill of butchering prey - 'Cut fire tongue' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...cut fire tongue from fire lizards. " );
 	};
 	if(trophy == TROPHY_BFWing)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_BFWing] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Отрезать крылья'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...отрезать крылья у кровяных шершней.");
+		AI_Print( " Learned the skill of butchering prey - 'Cut Wings' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...cut off blood hornets' wings. " );
 	};
 	if(trophy == TROPHY_BFSting)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_BFSting] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Вырезать жало'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...вырезать жало у кровяных шершней.");
+		AI_Print( " Learned the skill of cutting prey - 'Cut the sting' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...cut the stingers off the blood hornets. " );
 	};
 	if(trophy == TROPHY_Mandibles)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_Mandibles] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Вырезать мандибулы'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...вырезать мандибулу у ползунов, полевых жуков и пауков.");
+		AI_Print( " Learned the skill of cutting prey - 'Cut mandibles' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...carve the mandible from creepers, field beetles, and spiders. " );
 	};
 	if(trophy == TROPHY_CrawlerPlate)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_CrawlerPlate] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Снимать панцири'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...снимать панцирь с ползунов.");
+		AI_Print( " Learned the skill of butchering prey - 'Remove shells' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...remove shells from sliders. " );
 	};
 	if(trophy == TROPHY_DrgSnapperHorn)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_DrgSnapperHorn] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Отламывать рог снепперов'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...отламывать рог у драконьих снепперов.");
+		AI_Print( " Learned the skill of butchering prey - 'Break the horn of snappers' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...break the horn of dragon snappers. " );
 	};
 	if(trophy == TROPHY_DragonScale)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_DragonScale] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Снимать чешую дракона'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...снимать чешую с драконов.");
+		AI_Print( " Learned the skill of butchering prey - 'Remove dragon scales' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...scaling dragons. " );
 	};
 	if(trophy == TROPHY_DragonBlood)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_DragonBlood] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Сцеживать кровь дракона'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...сцеживать кровь с драконов.");
+		AI_Print( " Learned butchering skill - 'Pump Dragon's Blood' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...pump blood from dragons. " );
 	};
 
 	Npc_SetTalentSkill(oth,NPC_TALENT_TAKEANIMALTROPHY,1);
@@ -130,103 +131,103 @@ func int B_TeachPlayerTalentTakeAnimalTrophy(var C_Npc slf,var C_Npc oth,var int
 
 func int B_TeachPlayerTalentTakeAnimalTrophyOrc(var C_Npc slf,var C_Npc oth,var int trophy)
 {
-	var int kosten;
+	var int cost;
 
 	kosten = B_GetLearnCostTalentOrc(oth,NPC_TALENT_TAKEANIMALTROPHY,trophy);
 
 	if(oth.lp < kosten)
 	{
 		AI_PrintClr(PRINT_NotEnoughLearnPoints,177,58,17);
-		return FALSE;
+		return  FALSE ;
 	};
 
 	oth.lp = oth.lp - kosten;
 	Log_CreateTopic(TOPIC_TalentAnimalTrophy,LOG_NOTE);
-	B_LogEntry(TOPIC_TalentAnimalTrophy,"Теперь я умею:");
+	B_LogEntry(TOPIC_TalentAnimalTrophy, " Now I can: " );
 
 	if(trophy == TROPHY_Teeth)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_Teeth] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Вырывать зубы и клыки'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...вырывать зубы и клыки.");
+		AI_Print( " Learned the skill of butchering prey - 'Pull out teeth and fangs' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...pull out teeth and fangs. " );
 	};
 	if(trophy == TROPHY_Claws)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_Claws] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Ломать когти'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...ломать когти.");
+		AI_Print( " Learned the skill of butchering prey - 'Break claws' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...break claws. " );
 	};
 	if(trophy == TROPHY_Fur)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_Fur] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Снимать шкуры'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...снимать шкуры.");
+		AI_Print( " Learned the skill of butchering prey - 'Skinning' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...skin. " );
 	};
 	if(trophy == TROPHY_ReptileSkin)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_ReptileSkin] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Сдирать кожу с рептилий'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"... сдирать кожу с рептилий.");
+		AI_Print( " Learned butchering skill - 'Flay Reptiles' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ... skinning reptiles. " );
 	};
 	if(trophy == TROPHY_Heart)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_Heart] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Вырезать сердце'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...вырезать сердца у големов и демонов.");
+		AI_Print( " Learned the skill of butchering the prey - 'Cut out the heart' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...carve hearts from golems and demons. " );
 	};
 	if(trophy == TROPHY_ShadowHorn)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_ShadowHorn] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Отламывать рог мракорисов'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...отламывать рог у мракорисов.");
+		AI_Print( " Learned the skill of butchering the prey - 'Break off the horn of the obscurantists' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...break off the horns of the Aurors. " );
 	};
 	if(trophy == TROPHY_FireTongue)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_FireTongue] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Вырезать огненный язык'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...вырезать огненный язык у огненных ящериц.");
+		AI_Print( " Learned the skill of butchering prey - 'Cut fire tongue' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...cut fire tongue from fire lizards. " );
 	};
 	if(trophy == TROPHY_BFWing)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_BFWing] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Отрезать крылья'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...отрезать крылья у кровяных шершней.");
+		AI_Print( " Learned the skill of butchering prey - 'Cut Wings' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...cut off blood hornets' wings. " );
 	};
 	if(trophy == TROPHY_BFSting)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_BFSting] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Вырезать жало'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...вырезать жало у кровяных шершней.");
+		AI_Print( " Learned the skill of cutting prey - 'Cut the sting' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...cut the stingers off the blood hornets. " );
 	};
 	if(trophy == TROPHY_Mandibles)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_Mandibles] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Вырезать мандибулы'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...вырезать мандибулу у ползунов, полевых жуков и пауков.");
+		AI_Print( " Learned the skill of cutting prey - 'Cut mandibles' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...carve the mandible from creepers, field beetles, and spiders. " );
 	};
 	if(trophy == TROPHY_CrawlerPlate)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_CrawlerPlate] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Снимать панцири'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...снимать панцирь с ползунов.");
+		AI_Print( " Learned the skill of butchering prey - 'Remove shells' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...remove shells from sliders. " );
 	};
 	if(trophy == TROPHY_DrgSnapperHorn)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_DrgSnapperHorn] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Отламывать рог снепперов'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...отламывать рог у драконьих снепперов.");
+		AI_Print( " Learned the skill of butchering prey - 'Break the horn of snappers' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...break the horn of dragon snappers. " );
 	};
 	if(trophy == TROPHY_DragonScale)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_DragonScale] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Снимать чешую дракона'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...снимать чешую с драконов.");
+		AI_Print( " Learned the skill of butchering prey - 'Remove dragon scales' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...scaling dragons. " );
 	};
 	if(trophy == TROPHY_DragonBlood)
 	{
 		PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_DragonBlood] = TRUE;
-		AI_Print("Изучен навык разделки добычи - 'Сцеживать кровь дракона'");
-		B_LogEntry(TOPIC_TalentAnimalTrophy,"...сцеживать кровь с драконов.");
+		AI_Print( " Learned butchering skill - 'Pump Dragon's Blood' " );
+		B_LogEntry(TOPIC_TalentAnimalTrophy, " ...pump blood from dragons. " );
 	};
 
 	Npc_SetTalentSkill(oth,NPC_TALENT_TAKEANIMALTROPHY,1);
