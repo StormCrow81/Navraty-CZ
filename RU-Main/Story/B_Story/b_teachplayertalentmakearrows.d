83aee98e7f978f71ee806fe99df8a9e8d41e0923
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-14 20:55:00 +0200

    Update b_teachplayertalentmakearrows.d

--- RU-Main/Story/B_Story/b_teachplayertalentmakearrows.d
+++ RU-Main/Story/B_Story/b_teachplayertalentmakearrows.d
@@ -1,56 +1,57 @@
 
+
 func int b_teachplayertalentmakearrows(var C_Npc slf,var C_Npc oth,var int talent)
 {
-	var int kosten;
+	var int cost;
 	var int money;
-	var int TEMPSSBMODE;
+	var int  TEMPSSBMODE ;
 
 	if(SBMODE == 2)
 	{
-		TEMPSSBMODE = 2;
+		TEMPSSBMODE = 2 ;
 	}
 	else if(SBMODE == 4)
 	{
-		TEMPSSBMODE = 4;
+		TEMPSSBMODE = 4 ;
 	}
 	else
 	{
-		TEMPSSBMODE = 1;
+		TEMPSSBMODE = 1 ;
 	};
 
 	kosten = B_GetLearnCostTalent(oth,talent,1);
-	money = (kosten * 100) / TEMPSSBMODE;
+	money = (cost *  100 ) /  TEMPSSBMODE ;
 
 	if(talent != NPC_TALENT_MAKEARROWS)
 	{
 		Print("*** ERROR: Wrong Parameter ***");
-		return FALSE;
+		return  FALSE ;
 	};
 	if(oth.lp < kosten)
 	{
 		AI_PrintClr(PRINT_NotEnoughLearnPoints,177,58,17);
-		return FALSE;
+		return  FALSE ;
 	};
 	if(Npc_HasItems(oth,ItMi_Gold) < money)
 	{
 		AI_PrintClr(Print_NotEnoughGold,177,58,17);
-		return FALSE;
+		return  FALSE ;
 	};
 
 	oth.lp = oth.lp - kosten;
-	RankPoints = RankPoints + kosten;
+	RankPoints = RankPoints + cost;
 	Npc_RemoveInvItems(oth,ItMi_Gold,money);
 
 	if(talent == NPC_TALENT_MAKEARROWS)
 	{
 		Npc_SetTalentSkill(oth,NPC_TALENT_MAKEARROWS,1);
-		AI_Print("Изучен рецепт изготовления - 'Обычные стрелы и болты'");
+		AI_Print( " Studied crafting recipe - 'Regular arrows and bolts' " );
 		KNOWHOWTOMAKEARROWS = TRUE;
 		Snd_Play("LevelUP");
 		Log_CreateTopic(TOPIC_TALENTMAKEARROWS,LOG_NOTE);
-		B_LogEntry(TOPIC_TALENTMAKEARROWS,"Теперь я могу самолично изготавливать стрелы и болты. Для этого мне необходимо иметь: пилу, для изготовления древесных заготовок у бревна; щипцы, для изготовления древка лука из заготовок на столярном станке; стальную заготовку, для изготовления на наковальне стального наконечника. После этого мне понадобится сборный стол, чтобы уже на нем изготовить сами стрелы или болты.");
+		B_LogEntry( TOPIC_TALENTMAKEARROWS , " Now I can make arrows and bolts myself. To do this, I need to have: a saw, for making wood blanks from a log; tongs, for making a bow shaft from blanks on a carpentry machine; a steel billet, for making a steel tip on an anvil .After that, I need a prefabricated table to make arrows or bolts on it. " );
 		return TRUE;
 	};
-	return FALSE;
+	return  FALSE ;
 };
 
