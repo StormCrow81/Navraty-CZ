b29d1299b57b941751ca218e32aa6e09c230e829
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-14 15:16:12 +0200

    Update b_countbackcrew.d

--- RU-Main/Story/B_Story/b_countbackcrew.d
+++ RU-Main/Story/B_Story/b_countbackcrew.d
@@ -1,9 +1,10 @@
 
+
 func void b_countbackcrew()
 {
 	if((MIS_HELPCREW == LOG_Running) && (PERMCOUNTBACKNW >= GLOBALCOUNTBACKNW) && (MOVECREWTOHOME == FALSE))
 	{
-		Log_AddEntry(TOPIC_HELPCREW,"Я поговорил со всеми, кто находился на корабле. Теперь только Иннос решит - кому жить, а кому нет!");
+		Log_AddEntry( TOPIC_HELPCREW , " I've talked to everyone on the ship. Now only Innos will decide who lives and who doesn't! " );
 		MOVECREWTOHOME = TRUE;
 	};
 	if(PYROKARUSEONCE == FALSE)
