23a9b709a61c0fdd3fade24341e430d8c7cecd4d
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-14 20:51:02 +0200

    Update b_heroknowforestbase.d

--- RU-Main/Story/B_Story/b_heroknowforestbase.d
+++ RU-Main/Story/B_Story/b_heroknowforestbase.d
@@ -1,15 +1,16 @@
 
+
 func void b_heroknowforestbase()
 {
 	if(HEROKNOWFORESTBASE == FALSE)
 	{
 		Log_CreateTopic(TOPIC_XBS,LOG_NOTE);
-		B_LogEntry(TOPIC_XBS,"Я нашел лесной лагерь, в котором живет небольшая часть бывшего Братства Спящего.");
+		B_LogEntry( TOPIC_XBS , " I found a forest camp where a small part of the former Brotherhood of the Sleeper lives. " );
 		HEROKNOWFORESTBASE = TRUE;
 	};
-	if(!Npc_KnowsInfo(hero,dia_strf_1107_fleeinfo) && (MIS_OCCOOKFLEE == LOG_Running) && (OCCOOKFLEEINFO == FALSE))
+	if ( ! Npc_KnowsInfo(hero,dia_strf_1107_fleeinfo) && ( MIS_OCCOOKFLEE  == LOG_Running) && ( OCCOOKFLEEINFO  ==  FALSE )) ;
 	{
-		B_LogEntry(TOPIC_OCCOOKFLEE,"Надо бы предложить Лесной лагерь повару в качестве убежища.");
+		B_LogEntry( TOPIC_OCCOOKFLEE , " We should offer the Cook's Forest Camp as a place of refuge. " );
 		OCCOOKFLEEINFO = TRUE;
 	};
 };
