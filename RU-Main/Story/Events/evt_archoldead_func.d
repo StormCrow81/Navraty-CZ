0a689e8f07c37430313bdafb7308d296841e1534
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-14 21:21:05 +0200

    Update evt_archoldead_func.d

--- RU-Main/Story/Events/evt_archoldead_func.d
+++ RU-Main/Story/Events/evt_archoldead_func.d
@@ -1,20 +1,21 @@
 
-var int EVT_ARCHOLDEAD_FUNC_OneTime;
 
-func void evt_archoldead_func()
+var int EVT_ARCHHOLDEAD_FUNC_OneTime;
+
+func void evt_archolded_func()
 {
-	if(Npc_IsDead(Skeleton_Lord_Archol))
+	if (Npc_IsDead(Skeleton_Lord_Wound))
 	{
-		Skeleton_Archol1.aivar[AIV_EnemyOverride] = FALSE;
-		Skeleton_Archol2.aivar[AIV_EnemyOverride] = FALSE;
-		Skeleton_Archol3.aivar[AIV_EnemyOverride] = FALSE;
-		Skeleton_Archol4.aivar[AIV_EnemyOverride] = FALSE;
-		Skeleton_Archol5.aivar[AIV_EnemyOverride] = FALSE;
-		Skeleton_Archol6.aivar[AIV_EnemyOverride] = FALSE;
+		Skeleton_Archol1.aivar[AIV_EnemyOverride] = FALSE ;
+		Skeleton_Archol2.aivar[AIV_EnemyOverride] = FALSE ;
+		Skeleton_Archol3.aivar[AIV_EnemyOverride] = FALSE ;
+		Skeleton_Archol4.aivar[AIV_EnemyOverride] = FALSE ;
+		Skeleton_Archol5.aivar[AIV_EnemyOverride] = FALSE ;
+		Skeleton_Archol6.aivar[AIV_EnemyOverride] = FALSE ;
 	};
-	if(EVT_ARCHOLDEAD_FUNC_OneTime == FALSE)
+	if (EVT_ARCHOLDEAD_FUNC_OneTime ==  FALSE )
 	{
-		B_LogEntry(TOPIC_HallenVonIrdorath,"Я нашел что-то вроде храма. Он населен большим количеством неживых существ. Все напоминает мне об этом жалком Храме Спящего! Но на этот раз я не откажусь. Не в этот раз!");
+		B_LogEntry(TOPIC_HallenVonIrdorath, " I've found something like a temple. It's inhabited by a lot of undead things. Everything reminds me of the accursed Temple of the Sleeper! But this time I won't refuse. Not this time! " );
 		EVT_ARCHOLDEAD_FUNC_OneTime = TRUE;
 	};
 };
