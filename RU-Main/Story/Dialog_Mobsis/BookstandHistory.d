17b50cf6722c774844bbe2e4027f68de8769d195
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-14 21:02:29 +0200

    Update BookstandHistory.d

--- RU-Main/Story/Dialog_Mobsis/BookstandHistory.d
+++ RU-Main/Story/Dialog_Mobsis/BookstandHistory.d
@@ -1,4 +1,5 @@
 
+
 var int History_1_permanent;
 var int History_2_permanent;
 var int History_3_permanent;
@@ -17,16 +18,16 @@ func void Use_BookstandHistory1_S1()
 		Doc_SetPage(nDocID,1,"Book_RED_R.tga",0);
 		Doc_SetFont(nDocID,-1,Font_Book_New_Small);
 		Doc_SetMargins(nDocID,0,275,20,30,20,1);
-		Doc_PrintLine(nDocID,0,"Путь борьбы ");
+		Doc_PrintLine(nDocID, 0 , " Path of struggle " );
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLines(nDocID,0,"Блок мечом - лучшая защита против удара меча человека. Бессмысленно ставить блоки против зверей или монстров.");
-		Doc_PrintLines(nDocID,0,"Более выгодным является держать противника на дистанции и разработать комбинацию из нескольких ударов.");
+		Doc_PrintLines(nDocID, 0 , " Blocking with a sword is the best defense against a human sword strike. Blocking against beasts or monsters is pointless. " );
+		Doc_PrintLines(nDocID, 0 , " It's more beneficial to keep the enemy at a distance and develop a combination of several blows. " );
 		Doc_SetMargins(nDocID,-1,30,20,275,20,1);
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Комбинации из нескольких ударов могут быть выполнены опытными бойцами. И тот, кто обладает мастерством в борьбе, также может выполнять дальнейшие комбинации.");
+		Doc_PrintLines(nDocID, 1 , " Multi-hit combinations can be performed by experienced fighters. And those who are proficient in fighting can also perform further combinations. " );
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLines(nDocID,1,"");
 		Doc_PrintLines(nDocID,1,"");
@@ -44,13 +45,13 @@ func void Use_BookstandHistory1_S1()
 
 			if(RhetorikSkillValue[1] < 100)
 			{
-				RhetorikSkillValue[1] = RhetorikSkillValue[1] + 1;
-				AI_Print("Риторика + 1");
+				RhetoricSkillValue[ 1 ] = RhetoricSkillValue[ 1 ] +  1 ;
+				AI_Print( " Rhetoric + 1 " );
 			};
 		}
 		else
 		{
-			rnd = Hlp_Random(100);
+			rnd = Hlp_Random( 100 );
 			if(rnd <= 30)
 			{
 				B_Say(self,self,"$NOTHINGNEW");
@@ -68,8 +69,8 @@ func void Use_BookstandHistory1_S1()
 		{
 			ATR_INTELLECT += 1;
 			Npc_SetTalentSkill(self,NPC_TALENT_INTELLECT,ATR_INTELLECT);
-			AI_Print("Интеллект + 1");
-			BookBonus_93 = TRUE;
+			AI_Print( " Intelligence + 1 " );
+			BookBonus_93 = TRUE ;
 		};
 	};
 };
@@ -88,19 +89,19 @@ func void Use_BookstandHistory2_S1()
 		Doc_SetPage(nDocID,1,"Book_RED_R.tga",0);
 		Doc_SetFont(nDocID,-1,Font_Book_New_Small);
 		Doc_SetMargins(nDocID,0,275,20,30,20,1);
-		Doc_PrintLine(nDocID,0,"Остров");
+		Doc_PrintLine(nDocID, 0 , " Island " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLines(nDocID,0,"Портовый город Хоринис расположен на острове недалеко от морского берега королевства Миртана.");
-		Doc_PrintLines(nDocID,0,"Остров известен, прежде всего, благодаря своей рудниковой долине. На протяжении многих лет через все долину простирался магический барьер и все заключенные королевства отправлялись туда.");
-		Doc_PrintLines(nDocID,0,"Так долина стала исправительной колонией для многих приговоренных, которые глубоко под землей добывали магическую руду. ");
+		Doc_PrintLines(nDocID, 0 , " The port city of Khorinis is located on an island near the sea coast of the kingdom of Myrtana. " );
+		Doc_PrintLines(nDocID, 0 , " The island is best known for its mining valley. For many years, a magical barrier stretched across the entire valley and all the imprisoned kingdoms went there. " );
+		Doc_PrintLines(nDocID, 0 , " Thus the valley became a penal colony for the many condemned who mined magic ore deep underground. " );
 		Doc_SetMargins(nDocID,-1,30,20,275,20,1);
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Кроме того, на острове есть несколько крестьянских усадьб, которые обрабатывают почву для выращивания пшеницы и свеклы, а также выращивают овец. ");
+		Doc_PrintLines(nDocID, 1 , " In addition, there are several farmsteads on the island that cultivate the soil for growing wheat and beets, as well as raising sheep. " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"На протяжении многих поколений самый большой двор принадлежит помещику, который сдает в аренду другим крестьянам близлежащие земли. ");
+		Doc_PrintLines(nDocID, 1 , " For generations, the largest yard has been owned by a landowner who leases nearby land to other peasants. " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Посреди острова стоит старый монастырь. Монастырь Инноса, которым управляют маги огня. Там они проводят магические и алхимические исследования и изготавливают вино.");
+		Doc_PrintLines(nDocID, 1 , " In the middle of the island stands an old monastery. The Monastery of Innos, run by firebenders. There they conduct magical and alchemical research and make wine. " );
 		Doc_Show(nDocID);
 		if(History_2_permanent == FALSE)
 		{
@@ -111,13 +112,13 @@ func void Use_BookstandHistory2_S1()
 
 			if(RhetorikSkillValue[1] < 100)
 			{
-				RhetorikSkillValue[1] = RhetorikSkillValue[1] + 1;
-				AI_Print("Риторика + 1");
+				RhetoricSkillValue[ 1 ] = RhetoricSkillValue[ 1 ] +  1 ;
+				AI_Print( " Rhetoric + 1 " );
 			};
 		}
 		else
 		{
-			rnd = Hlp_Random(100);
+			rnd = Hlp_Random( 100 );
 			if(rnd <= 30)
 			{
 				B_Say(self,self,"$NOTHINGNEW");
@@ -135,8 +136,8 @@ func void Use_BookstandHistory2_S1()
 		{
 			ATR_INTELLECT += 1;
 			Npc_SetTalentSkill(self,NPC_TALENT_INTELLECT,ATR_INTELLECT);
-			AI_Print("Интеллект + 1");
-			BookBonus_94 = TRUE;
+			AI_Print( " Intelligence + 1 " );
+			BookBonus_94 = TRUE ;
 		};
 	};
 };
@@ -156,19 +157,19 @@ func void Use_BookstandHistory3_S1()
 		Doc_SetPage(nDocID,1,"Book_RED_R.tga",0);
 		Doc_SetFont(nDocID,-1,Font_Book_New_Small);
 		Doc_SetMargins(nDocID,0,275,20,30,20,1);
-		Doc_PrintLine(nDocID,0,"Закон острова");
+		Doc_PrintLine(nDocID, 0 , " Island Law " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLines(nDocID,0,"Всюду, где собираются люди, общинная жизнь должна быть урегулирована. Будь то монастырь, город или во двор помещика. ");
-		Doc_PrintLines(nDocID,0,"Любое преступление против общины наказуемо. ");
-		Doc_PrintLines(nDocID,0,"Согласно законам общины запрещены любые нападения или участие в таковых. ");
-		Doc_PrintLines(nDocID,0,"Кража также является проступком, который не остается безнаказанным. Под защитой закона пребывает священное животное - овца.  ");
+		Doc_PrintLines(nDocID, 0 , " Wherever people gather, communal life must be regulated. Whether it be a monastery, a city or a landowner's yard. " );
+		Doc_PrintLines(nDocID, 0 , " Any crime against the community is punishable. " );
+		Doc_PrintLines(nDocID, 0 , " According to the laws of the community, any attack or participation in such is prohibited. " );
+		Doc_PrintLines(nDocID, 0 , " Theft is also a misdemeanor that does not go unpunished. A sacred animal, a sheep, is under the protection of the law.   " );
 		Doc_PrintLines(nDocID,0,"");
 		Doc_SetMargins(nDocID,-1,30,20,275,20,1);
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Кто убил человека, должен знать, что и он будет убит.");
-		Doc_PrintLines(nDocID,1,"А кто содействовал этому, должен будет заплатить высокий штраф. ");
-		Doc_PrintLines(nDocID,1,"Во всех высших управлениях правом управляют мужчины. В их ведомстве все происходящие события и они налагают наказания и штрафы, которые должны восстановить мир и справедливость. ");
-		Doc_PrintLines(nDocID,1,"Тот, кто стал жертвой проступка или может засвидетельствовать таковой - не должен молчать.");
+		Doc_PrintLines(nDocID, 1 , " Whoever killed a man should know that he will be killed too. " );
+		Doc_PrintLines(nDocID, 1 , " And who contributed to this will have to pay a heavy fine. " );
+		Doc_PrintLines(nDocID, 1 , " In all higher departments, men rule the law. In their department, all the events that take place and they impose punishments and fines that should restore peace and justice. " );
+		Doc_PrintLines(nDocID, 1 , " Anyone who has been the victim of a misdemeanor or who can witness such should not be silent. " );
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
@@ -183,13 +184,13 @@ func void Use_BookstandHistory3_S1()
 
 			if(RhetorikSkillValue[1] < 100)
 			{
-				RhetorikSkillValue[1] = RhetorikSkillValue[1] + 1;
-				AI_Print("Риторика + 1");
+				RhetoricSkillValue[ 1 ] = RhetoricSkillValue[ 1 ] +  1 ;
+				AI_Print( " Rhetoric + 1 " );
 			};
 		}
 		else
 		{
-			rnd = Hlp_Random(100);
+			rnd = Hlp_Random( 100 );
 			if(rnd <= 30)
 			{
 				B_Say(self,self,"$NOTHINGNEW");
@@ -207,8 +208,8 @@ func void Use_BookstandHistory3_S1()
 		{
 			ATR_INTELLECT += 1;
 			Npc_SetTalentSkill(self,NPC_TALENT_INTELLECT,ATR_INTELLECT);
-			AI_Print("Интеллект + 1");
-			BookBonus_95 = TRUE;
+			AI_Print( " Intelligence + 1 " );
+			BookBonus_95 = TRUE ;
 		};
 	};
 };
