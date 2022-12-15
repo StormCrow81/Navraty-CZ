31490f4138dfe66b788e63d67fa3d93895828469
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-15 18:56:16 +0200

    Update trshacken_s1.d

--- RU-Main/_misk_/trshacken_s1.d
+++ RU-Main/_misk_/trshacken_s1.d
@@ -1,18 +1,19 @@
 
+
 func void trshacken_s1()
 {
 	var C_Npc her;
 	her = Hlp_GetNpc(PC_Hero);
 	if(Hlp_GetInstanceID(self) == Hlp_GetInstanceID(her))
 	{
-		//print_percent_bar_none(ATR_STAMINA[0],ATR_STAMINA_MAX[0] * 10,"Выносливость",42,97);
+		// print_percent_bar_none(ATR_STAMINA[0],ATR_STAMINA_MAX[0] * 10,"Stamina",42,97);
 
 		if(CinemaMod == TRUE)
 		{
 			Wld_PlayEffect("DIALOGSCOPE_FX",hero,hero,0,0,0,FALSE);
 		};
 
-		self.aivar[AIV_INVINCIBLE] = TRUE;
+		self.aivar[ AIV_INVINCIBLE ] = TRUE ;
 		PLAYER_MOBSI_PRODUCTION = MOBSI_TRSHACKEN;
 		AI_ProcessInfos(her);
 	};
@@ -51,7 +52,7 @@ instance PC_TRSHACKEN_ADDON_HOUR(C_Info)
 	condition = pc_trshacken_addon_hour_condition;
 	information = pc_trshacken_addon_hour_info;
 	permanent = TRUE;
-	description = "Копать.";
+	description = " Dig. " ;
 };
 
 func int pc_trshacken_addon_hour_condition()
@@ -68,9 +69,9 @@ func void pc_trshacken_addon_hour_info()
 	{
 		RankPoints = RankPoints + 1;
 		TREASURETAKE = TRUE;
-		B_LogEntry(TOPIC_VIPERNUGGETS,"Я нашел пакет с рудой!");
+		B_LogEntry( TOPIC_VIPERNUGGETS , " I found an ore pack! " );
 		Wld_InsertItem(itmi_alefnuggetsbag,"OW_ALEF_TRS");
-		Wld_PlayEffect("spellFX_ItemAusbuddeln",itmi_alefnuggetsbag,itmi_alefnuggetsbag,0,0,0,FALSE);
+		Wld_PlayEffect( " spellFX_ItemAusbuddeln " ,itmi_refnuggetsbag,itmi_refnuggetsbag, 0 , 0 , 0 , FALSE );
 		B_Say_Overlay(hero,hero,"$FOUNDTREASURE");
 		Snd_Play("ORE_HACK");
 		B_GivePlayerXP(100);
