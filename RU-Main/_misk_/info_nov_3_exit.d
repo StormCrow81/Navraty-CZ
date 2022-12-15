64afa35b464f7779af548da4036017b1e75090f3
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-15 18:25:58 +0200

    Update info_nov_3_exit.d

--- RU-Main/_misk_/info_nov_3_exit.d
+++ RU-Main/_misk_/info_nov_3_exit.d
@@ -1,4 +1,5 @@
 
+
 instance INFO_NOV_3_EXIT(C_Info)
 {
 	nr = 999;
@@ -24,9 +25,9 @@ instance INFO_NOV_3_EINERVONEUCHWERDEN(C_Info)
 {
 	nr = 4;
 	condition = info_nov_3_einervoneuchwerden_condition;
-	information = info_nov_3_einervoneuchwerden_info;
+	information = info_nov_3_oneofyouwillbe_info;
 	permanent = 1;
-	description = "Как мне присоединиться к этому Лагерю?";
+	description = " How can I join this Camp? " ;
 };
 
 
@@ -40,32 +41,32 @@ func int info_nov_3_einervoneuchwerden_condition()
 
 func void info_nov_3_einervoneuchwerden_info()
 {
-	AI_Output(other,self,"Info_Nov_3_EinerVonEuchWerden_15_00");	//Как мне присоединиться к этому Лагерю?
-	AI_Output(self,other,"Info_Nov_3_EinerVonEuchWerden_03_01");	//Это очень просто. Иди, друг.
-	AI_Output(self,other,"Info_Nov_3_EinerVonEuchWerden_03_02");	//Поговори с Гуру - они наставят тебя на путь истинный.
+	AI_Output(other,self, " Info_Nov_3_EinerVonEuchWerden_15_00 " );	// How can I join this Camp?
+	AI_Output(self,other, " Info_Nov_3_EinerVonEuchWerden_03_01 " );	// It's very simple.
+	AI_Output(self,other, " Info_Nov_3_EinerVonEuchWerden_03_02 " );	// Talk to the Guru's - they will guide you on the right path.
 };
 
 
-instance INFO_NOV_3_WICHTIGEPERSONEN(C_Info)
+instance INFO_NOV_3_IMPORTANT PEOPLE (C_Info)
 {
 	nr = 3;
-	condition = info_nov_3_wichtigepersonen_condition;
-	information = info_nov_3_wichtigepersonen_info;
+	condition = info_nov_3_important people_condition;
+	information = info_nov_3_important people_info;
 	permanent = 1;
-	description = "Кто здесь главный?";
+	description = " Who's in charge here? " ;
 };
 
 
-func int info_nov_3_wichtigepersonen_condition()
+func int info_nov_3_important_persons_condition()
 {
 	return TRUE;
 };
 
-func void info_nov_3_wichtigepersonen_info()
+func void info_nov_3_important_persons_info()
 {
-	AI_Output(other,self,"Info_Nov_3_WichtigePersonen_15_00");	//Кто здесь главный?
-	AI_Output(self,other,"Info_Nov_3_WichtigePersonen_03_01");	//Идол Оран - старший среди Гуру! Он привел нас в это славное место.
-	AI_Output(self,other,"Info_Nov_3_WichtigePersonen_03_02");	//Все это было создано по его воле.
+	AI_Output(other,self, " Info_Nov_3_WichtigePersonen_15_00 " );	// Who's in charge here?
+	AI_Output(self,other, " Info_Nov_3_WichtigePersonen_03_01 " );	// The idol Oran is the eldest among the Guru's! He brought us to this glorious place.
+	AI_Output(self,other, " Info_Nov_3_WichtigePersonen_03_02 " );	// All this was created by his will.
 };
 
 
@@ -75,7 +76,7 @@ instance INFO_NOV_3_DASLAGER(C_Info)
 	condition = info_nov_3_daslager_condition;
 	information = info_nov_3_daslager_info;
 	permanent = 1;
-	description = "На что здесь стоит обратить внимание? ";
+	description = " What should I pay attention to here? " ;
 };
 
 
@@ -86,9 +87,9 @@ func int info_nov_3_daslager_condition()
 
 func void info_nov_3_daslager_info()
 {
-	AI_Output(other,self,"Info_Nov_3_DasLager_15_00");	//Расскажи мне что-нибудь об этом месте.
-	AI_Output(self,other,"Info_Nov_3_DasLager_03_01");	//Здесь есть такой Таллас. Он бесплатно дает болотник всем последователям секты.
-	AI_Output(self,other,"Info_Nov_3_DasLager_03_02");	//Ты найдешь его на площадке над кузней.
+	AI_Output(other,self, " Info_Nov_3_DasLager_15_00 " );	// Tell me something about this place.
+	AI_Output(self,other, " Info_Nov_3_DasLager_03_01 " );	// There is a man called Tallas here. He gives free swamp weed to all followers of the sect.
+	AI_Output(self,other, " Info_Nov_3_DasLager_03_02 " );	// You'll find him on the platform above the forge.
 };
 
 
@@ -98,7 +99,7 @@ instance INFO_NOV_3_DIELAGE(C_Info)
 	condition = info_nov_3_dielage_condition;
 	information = info_nov_3_dielage_info;
 	permanent = 1;
-	description = "Как дела?";
+	description = " How are you? " ;
 };
 
 
@@ -109,8 +110,8 @@ func int info_nov_3_dielage_condition()
 
 func void info_nov_3_dielage_info()
 {
-	AI_Output(other,self,"Info_Nov_3_DieLage_15_00");	//Как дела?
-	AI_Output(self,other,"Info_Nov_3_DieLage_03_01");	//Я на небесах, приятель!
+	AI_Output(other,self, " Info_Nov_3_DieLage_15_00 " );	// How are you?
+	AI_Output(self,other, " Info_Nov_3_DieLage_03_01 " );	// I'm in heaven, mate!
 };
 
 
@@ -120,13 +121,13 @@ instance INFO_NOV_3_SEKTEHEILEN(C_Info)
 	condition = info_nov_3_sekteheilen_condition;
 	information = info_nov_3_sekteheilen_info;
 	permanent = TRUE;
-	description = "Выпей напиток! Он помогает от головной боли.";
+	description = " Drink a drink! It helps with a headache. " ;
 };
 
 
 func int info_nov_3_sekteheilen_condition()
 {
-	if((self.aivar[AIV_MM_SleepEnd] == FALSE) && (Npc_HasItems(other,ItPo_HealObsession_MIS) > 0) && (MIS_SEKTEHEILEN == LOG_Running) && Npc_KnowsInfo(hero,dia_baalorun_sekteheilengot))
+	if ((self.aivar[AIV_MM_SleepEnd] ==  FALSE ) && (Npc_HasItems(other,ItPo_HealObsession_MY) >  0 ) && ( MY_SECTION_HEALTH  == LOG_Running) && Npc_KnowsInfo(hero,his_health_heal_obsession));
 	{
 		return TRUE;
 	};
@@ -135,21 +136,21 @@ func int info_nov_3_sekteheilen_condition()
 func void info_nov_3_sekteheilen_info()
 {
 	B_GivePlayerXP(50);
-	AI_Output(other,self,"Info_Nov_3_SekteHeilen_01_00");	//Выпей напиток! Он помогает от головной боли.
+	AI_Output(other,self, " Info_Nov_3_SekteHeilen_01_00 " );	// Have a drink! It helps with headaches.
 	B_GiveInvItems(other,self,ItPo_HealObsession_MIS,1);
 	B_UseItem(self,ItPo_HealObsession_MIS);
-	self.aivar[AIV_MM_SleepEnd] = TRUE;
-	SEKTEHEILENCOUNT = SEKTEHEILENCOUNT + 1;
+	self.aivar[AIV_MM_SleepEnd] = TRUE ;
+	SECTEHEILENCOUNT = SECTEHEILENCOUNT  +  1 ;
 	AI_Wait(self,0);
-	AI_Output(self,other,"Info_Nov_3_SekteHeilen_01_01");	//Моя голова - он больше не болит!!!
-	AI_Output(self,other,"Info_Nov_3_SekteHeilen_01_02");	//Огромное спасибо тебе!
+	AI_Output(self,other, " Info_Nov_3_SekteHeilen_01_01 " );	// My head - it doesn't hurt anymore!!!
+	AI_Output(self,other, " Info_Nov_3_SekteHeilen_01_02 " );	// Thank you so much!
 };
 
 func void b_assignambientinfos_sek_3(var C_Npc slf)
 {
 	info_nov_3_exit.npc = Hlp_GetInstanceID(slf);
-	info_nov_3_einervoneuchwerden.npc = Hlp_GetInstanceID(slf);
-	info_nov_3_wichtigepersonen.npc = Hlp_GetInstanceID(slf);
+	info_nov_3_oneofyouwillbe.npc = Hlp_GetInstanceID(slf);
+	info_nov_3_important persons.npc = Hlp_GetInstanceID(slf);
 	info_nov_3_daslager.npc = Hlp_GetInstanceID(slf);
 	info_nov_3_dielage.npc = Hlp_GetInstanceID(slf);
 	info_nov_3_sekteheilen.npc = Hlp_GetInstanceID(slf);
