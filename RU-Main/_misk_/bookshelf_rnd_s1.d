28ce69298652db05d3d2e143915d9f4dc7136914
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-15 18:20:48 +0200

    Update bookshelf_rnd_s1.d

--- RU-Main/_misk_/bookshelf_rnd_s1.d
+++ RU-Main/_misk_/bookshelf_rnd_s1.d
@@ -1,4 +1,5 @@
 
+
 func void bookshelf_rnd_s1()
 {
 	var C_Npc her;
@@ -10,7 +11,7 @@ func void bookshelf_rnd_s1()
 			Wld_PlayEffect("DIALOGSCOPE_FX",hero,hero,0,0,0,FALSE);
 		};
 
-		self.aivar[AIV_INVINCIBLE] = TRUE;
+		self.aivar[ AIV_INVINCIBLE ] = TRUE ;
 		PLAYER_MOBSI_PRODUCTION = MOBSI_BOOKSHELF;
 		AI_ProcessInfos(her);
 	};
@@ -51,7 +52,7 @@ instance PC_BOOKSHELF_RND(C_Info)
 	condition = pc_bookshelf_rnd_condition;
 	information = pc_bookshelf_rnd_info;
 	permanent = TRUE;
-	description = "Обыскать книжную полку.";
+	description = " Search the bookshelf. " ;
 };
 
 func int pc_bookshelf_rnd_condition()
@@ -72,18 +73,18 @@ func void pc_bookshelf_rnd_info()
 		if((Hlp_StrCmp(Npc_GetNearestWP(hero),"DT_E2_03")) && (Found_OneHandBook == FALSE))
 		{
 			Found_OneHandBook = TRUE;
-			CreateInvItems(hero,ItWr_EinhandBuch,1);
-			AI_Print("Найдена книга 'Оборона южных земель'!");
+			CreateInvItems(hero,ItWr_EinhandBuch, 1 );
+			AI_Print( "The book 'Defense of the Southern Lands' was found! " );
 		}
 		else	if((Hlp_StrCmp(Npc_GetNearestWP(hero),"OW_HIDE_BOOK")) && (Found_TwoHandBook == FALSE))
 		{
 			Found_TwoHandBook = TRUE;
 			CreateInvItems(hero,ItWr_ZweihandBuch,1);
-			AI_Print("Найдена книга 'Двойной блок'!");
+			AI_Print( " Book 'Double block' found! " );
 		}
 		else
 		{
-			AI_Print("Ничего не найдено!");
+			AI_Print( " Nothing found! " );
 		};
 	}
 	else
@@ -99,47 +100,47 @@ func void pc_bookshelf_rnd_info()
 				if(rnd_shelf2 == 0)
 				{
 					CreateInvItems(hero,ItSc_FireBolt,1);
-					AI_Print("Найден магический свиток 'Огненная стрела'!");
+					AI_Print( " Found magic scroll 'Fire Arrow'! " );
 				}
 				else if(rnd_shelf2 == 1)
 				{
 					CreateInvItems(hero,ItSc_Zap,1);
-					AI_Print("Найден магический свиток 'Молния'!");
+					AI_Print( " Found magic scroll 'Lightning'! " );
 				}
 				else if(rnd_shelf2 == 2)
 				{
 					CreateInvItems(hero,ItSc_Icebolt,1);
-					AI_Print("Найден магический свиток 'Ледяная стрела'!");
+					AI_Print( " Frostbolt magic scroll found! " );
 				}
 				else if(rnd_shelf2 == 3)
 				{
 					CreateInvItems(hero,ItSc_Sleep,1);
-					AI_Print("Найден магический свиток 'Сон'!");
+					AI_Print( " Found magic scroll 'Sleep'! " );
 				}
 				else if(rnd_shelf2 == 4)
 				{
 					CreateInvItems(hero,ItSc_Deathbolt,1);
-					AI_Print("Найден магический свиток 'Стрела мрака'!");
+					AI_Print( " Found the magic scroll 'Dark Arrow'! " );
 				}
 				else if(rnd_shelf2 == 5)
 				{
 					CreateInvItems(hero,ItSc_SumGobSkel,1);
-					AI_Print("Найден магический свиток 'Призыв скелета'!");
+					AI_Print( " Summon Skeleton Magic Scroll Found! " );
 				}
 				else if(rnd_shelf2 == 6)
 				{
 					CreateInvItems(hero,ItSc_Light,1);
-					AI_Print("Найден магический свиток 'Свет'!");
+					AI_Print( " Found magic scroll 'Light'! " );
 				}
 				else if(rnd_shelf2 == 7)
 				{
 					CreateInvItems(hero,ItSc_LightHeal,1);
-					AI_Print("Найден магический свиток 'Исцеление'!");
+					AI_Print( " Found magic scroll 'Healing'! " );
 				}
 				else
 				{
 					CreateInvItems(hero,ItSc_TrfScavenger,1);
-					AI_Print("Найден магический свиток 'Превращение в шершня'!");
+					AI_Print( " Found the magic scroll 'Transformation into a Hornet'! " );
 				};
 			}
 			else if(Npc_GetTalentSkill(hero,NPC_TALENT_MAGE) == 1)
@@ -149,57 +150,57 @@ func void pc_bookshelf_rnd_info()
 				if(rnd_shelf2 == 0)
 				{
 					CreateInvItems(hero,ItSc_SumWolf,1);
-					AI_Print("Найден магический свиток 'Призыв варга'!");
+					AI_Print( " Found magic scroll 'Summon Warg'! " );
 				}
 				else if(rnd_shelf2 == 1)
 				{
 					CreateInvItems(hero,ItSc_MediumHeal,1);
-					AI_Print("Найден магический свиток 'Очищающий свет'!");
+					AI_Print( " Found the magic scroll 'Cleansing Light'! " );
 				}
 				else if(rnd_shelf2 == 2)
 				{
 					CreateInvItems(hero,ItSc_HarmUndead,1);
-					AI_Print("Найден магический свиток 'Уничтожение нежити'!");
+					AI_Print( " Found magic scroll 'Destroy undead'! " );
 				}
 				else if(rnd_shelf2 == 3)
 				{
 					CreateInvItems(hero,ItSc_InstantFireball,1);
-					AI_Print("Найден магический свиток 'Огненный шар'!");
+					AI_Print( " Found magic scroll 'Fireball'! " );
 				}
 				else if(rnd_shelf2 == 4)
 				{
 					CreateInvItems(hero,ItSc_Icelance,1);
-					AI_Print("Найден магический свиток 'Ледяное копье'!");
+					AI_Print( " Found magic scroll 'Ice Spear'! " );
 				}
 				else if(rnd_shelf2 == 5)
 				{
 					CreateInvItems(hero,ItSc_SUMSHOAL,1);
-					AI_Print("Найден магический свиток 'Вызвать снежную стаю'!");
+					AI_Print( " Summon Snow Swarm Magic Scroll Found! " );
 				}
 				else if(rnd_shelf2 == 6)
 				{
 					CreateInvItems(hero,ItSc_Windfist,1);
-					AI_Print("Найден магический свиток 'Кулак ветра'!");
+					AI_Print( " Wind Fist Magic Scroll Found! " );
 				}
 				else if(rnd_shelf2 == 7)
 				{
 					CreateInvItems(hero,ItSc_GreenTentacle,1);
-					AI_Print("Найден магический свиток 'Опутать корнями'!");
+					AI_Print( " Found the magic scroll 'Entangle Roots'! " );
 				}
 				else if(rnd_shelf2 == 8)
 				{
 					CreateInvItems(hero,ItSc_ManaForLife,1);
-					AI_Print("Найден магический свиток 'Раздирание души'!");
+					AI_Print( " Soul Rend Magic Scroll Found! " );
 				}
 				else if(rnd_shelf2 == 9)
 				{
 					CreateInvItems(hero,ItSc_SumZombie,1);
-					AI_Print("Найден магический свиток 'Призыв зомби'!");
+					AI_Print( " Summon Zombie Magic Scroll Found! " );
 				}
 				else
 				{
 					CreateInvItems(hero,ItSc_TrfSnapper,1);
-					AI_Print("Найден магический свиток 'Превращение в глорха'!");
+					AI_Print( " Glorcha Transformation Magic Scroll Found! " );
 				};
 			}
 			else if(Npc_GetTalentSkill(hero,NPC_TALENT_MAGE) == 2)
@@ -209,92 +210,92 @@ func void pc_bookshelf_rnd_info()
 				if(rnd_shelf2 == 0)
 				{
 					CreateInvItems(hero,ItSc_FullHeal,1);
-					AI_Print("Найден магический свиток 'Прилив бодрости'!");
+					AI_Print( " Found magic scroll 'Spring of Vivacity'! " );
 				}
 				else if(rnd_shelf2 == 1)
 				{
 					CreateInvItems(hero,ItSc_Firestorm,1);
-					AI_Print("Найден магический свиток 'Огненный шторм'!");
+					AI_Print( " Firestorm magic scroll found! " );
 				}
 				else if(rnd_shelf2 == 2)
 				{
 					CreateInvItems(hero,ItSc_IceCube,1);
-					AI_Print("Найден магический свиток 'Ледяная глыба'!");
+					AI_Print( " Found the magic scroll 'Ice Block'! " );
 				}
 				else if(rnd_shelf2 == 3)
 				{
 					CreateInvItems(hero,ItSc_ThunderBall,1);
-					AI_Print("Найден магический свиток 'Шаровая молния'!");
+					AI_Print( " Found magic scroll 'Thunderball'! " );
 				}
 				else if(rnd_shelf2 == 4)
 				{
 					CreateInvItems(hero,ItSc_Fear,1);
-					AI_Print("Найден магический свиток 'Страх'!");
+					AI_Print( " Fear magic scroll found! " );
 				}
 				else if(rnd_shelf2 == 5)
 				{
 					CreateInvItems(hero,ItSc_CHARM,1);
-					AI_Print("Найден магический свиток 'Забвение'!");
+					AI_Print( " Found magic scroll 'Oblivion'! " );
 				}
 				else if(rnd_shelf2 == 6)
 				{
 					CreateInvItems(hero,ItSc_Swarm,1);
-					AI_Print("Найден магический свиток 'Рой'!");
+					AI_Print( " Found the magic scroll 'Roy'! " );
 				}
 				else if(rnd_shelf2 == 7)
 				{
 					CreateInvItems(hero,ItSc_SumSkel,1);
-					AI_Print("Найден магический свиток 'Призыв скелета-воина'!");
+					AI_Print( " Summon Skeleton Warrior Magic Scroll found! " );
 				}
 				else if(rnd_shelf2 == 8)
 				{
 					CreateInvItems(hero,ItSc_SumGol,1);
-					AI_Print("Найден магический свиток 'Призыв каменного голема'!");
+					AI_Print( " Found magic scroll 'Summon Stone Golem'! " );
 				}
 				else if(rnd_shelf2 == 9)
 				{
 					CreateInvItems(hero,ItSc_SUMFIREGOL,1);
-					AI_Print("Найден магический свиток 'Призыв огненного голема'!");
+					AI_Print( " Found magic scroll 'Summon Fire Golem'! " );
 				}
 				else if(rnd_shelf2 == 10)
 				{
 					CreateInvItems(hero,ItSc_TrfFireWaran,1);
-					AI_Print("Найден магический свиток 'Превращение в огненного ящера'!");
+					AI_Print( " Found the magic scroll 'Fire Lizard Transformation'! " );
 				}
 				else if(rnd_shelf2 == 11)
 				{
 					CreateInvItems(hero,ItSc_ChargeFireball,1);
-					AI_Print("Найден магический свиток 'Большой огненный шар'!");
+					AI_Print( " Great fireball magic scroll found! " );
 				}
 				else if(rnd_shelf2 == 12)
 				{
 					CreateInvItems(hero,ItSc_LightningFlash,1);
-					AI_Print("Найден магический свиток 'Удар молнии'!");
+					AI_Print( " Found magic scroll 'Thunderbolt'! " );
 				}
 				else if(rnd_shelf2 == 13)
 				{
 					CreateInvItems(hero,ItSc_SUMICEGOL,1);
-					AI_Print("Найден магический свиток 'Призыв ледяного голема'!");
+					AI_Print( " Summon Ice Golem Magic Scroll Found! " );
 				}
 				else if(rnd_shelf2 == 14)
 				{
 					CreateInvItems(hero,ItSc_Whirlwind,1);
-					AI_Print("Найден магический свиток 'Смерч'!");
+					AI_Print( " Found magic scroll 'Tornado'! " );
 				}
 				else if(rnd_shelf2 == 15)
 				{
 					CreateInvItems(hero,ItSc_SumDemon,1);
-					AI_Print("Найден магический свиток 'Призыв демона'!");
+					AI_Print( " Summon Demon Magic Scroll Found! " );
 				}
 				else if(rnd_shelf2 == 16)
 				{
 					CreateInvItems(hero,ItSc_Deathball,1);
-					AI_Print("Найден магический свиток 'Шар смерти'!");
+					AI_Print( "The magic scroll 'Death Ball' has been found! " );
 				}
 				else
 				{
 					CreateInvItems(hero,ItSc_TrfWarg,1);
-					AI_Print("Найден магический свиток 'Превращение в варга'!");
+					AI_Print( " Found magic scroll 'Transform into Warg'! " );
 				};
 			}
 			else if(Npc_GetTalentSkill(hero,NPC_TALENT_MAGE) >= 3)
@@ -304,57 +305,57 @@ func void pc_bookshelf_rnd_info()
 				if(rnd_shelf2 == 0)
 				{
 					CreateInvItems(hero,ItSc_Waterfist,1);
-					AI_Print("Найден магический свиток 'Кулак воды'!");
+					AI_Print( " Found magic scroll 'Water Fist'! " );
 				}
 				else if(rnd_shelf2 == 1)
 				{
 					CreateInvItems(hero,ItSc_IceWave,1);
-					AI_Print("Найден магический свиток 'Ледяная волна'!");
+					AI_Print( " Ice wave magic scroll found! " );
 				}
 				else if(rnd_shelf2 == 2)
 				{
 					CreateInvItems(hero,ItSc_ArmyOfDarkness,1);
-					AI_Print("Найден магический свиток 'Армия тьмы'!");
+					AI_Print( " Found the magic scroll 'Army of Darkness'! " );
 				}
 				else if(rnd_shelf2 == 3)
 				{
 					CreateInvItems(hero,ItSc_Whirlwind,1);
-					AI_Print("Найден магический свиток 'Смерч'!");
+					AI_Print( " Found magic scroll 'Tornado'! " );
 				}
 				else if(rnd_shelf2 == 4)
 				{
 					CreateInvItems(hero,ItSc_SUMSWPGOL,1);
-					AI_Print("Найден магический свиток 'Призыв болотного голема'!");
+					AI_Print( " Summon Swamp Golem Magic Scroll Found! " );
 				}
 				else if(rnd_shelf2 == 5)
 				{
 					CreateInvItems(hero,ItSc_TrfShadowbeast,1);
-					AI_Print("Найден магический свиток 'Превращение в мракориса'!");
+					AI_Print( " Found the magic scroll 'Mrakoris Transformation'! " );
 				}
 				else if(rnd_shelf2 == 6)
 				{
 					CreateInvItems(hero,ItSc_Firerain,1);
-					AI_Print("Найден магический свиток 'Огненный дождь'!");
+					AI_Print( " Fire Rain Magic Scroll Found! " );
 				}
 				else if(rnd_shelf2 == 7)
 				{
 					CreateInvItems(hero,ItSc_Geyser,1);
-					AI_Print("Найден магический свиток 'Гейзер'!");
+					AI_Print( " Geyser magic scroll found! " );
 				}
 				else if(rnd_shelf2 == 8)
 				{
 					CreateInvItems(hero,ItSc_Thunderstorm,1);
-					AI_Print("Найден магический свиток 'Шторм'!");
+					AI_Print( " Found magic scroll 'Storm'! " );
 				}
 				else if(rnd_shelf2 == 9)
 				{
 					CreateInvItems(hero,ItSc_MassDeath,1);
-					AI_Print("Найден магический свиток 'Черный туман'!");
+					AI_Print( " Found the magic scroll 'Black Mist'! " );
 				}
 				else
 				{
 					CreateInvItems(hero,ItSc_Skull,1);
-					AI_Print("Найден магический свиток 'Крик мертвых'!");
+					AI_Print( " Found the magic scroll 'Cry of the Dead'! " );
 				};
 			};
 
@@ -362,7 +363,7 @@ func void pc_bookshelf_rnd_info()
 		}
 		else
 		{
-			AI_Print("Ничего не найдено...");
+			AI_Print( " Nothing found... " );
 		};
 	};
 
