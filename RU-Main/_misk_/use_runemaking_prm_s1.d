aed4286d0d733b6ed82569829a720d0c967829e0
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-15 19:00:31 +0200

    Update use_runemaking_prm_s1.d

--- RU-Main/_misk_/use_runemaking_prm_s1.d
+++ RU-Main/_misk_/use_runemaking_prm_s1.d
@@ -1,4 +1,5 @@
 
+
 func void use_runemaking_prm_s1()
 {
 	var C_Npc her;
@@ -13,47 +14,47 @@ func void use_runemaking_prm_s1()
 		Doc_SetPage(nDocID,1,"Book_Brown_R.tga",0);
 		Doc_SetFont(nDocID,-1,Font_Book_New);
 		Doc_SetMargins(nDocID,0,275,20,30,20,1);
-		Doc_PrintLine(nDocID,0,"Истинная магия");
+		Doc_PrintLine(nDocID, 0 , " True Magic " );
 		Doc_SetFont(nDocID,-1,Font_Book_New_Small);
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLines(nDocID,0,"Руны истинной магии и необходимые для их изготовления ингредиенты: ");
+		Doc_PrintLines(nDocID, 0 , " Runes of true magic and the ingredients needed to make them: " );
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"Круг I");
 		Doc_PrintLine(nDocID,0,"");
 		Doc_PrintLine(nDocID,0,"- Свет -");
-		Doc_PrintLine(nDocID,0,"Золотая монета");
+		Doc_PrintLine(nDocID, 0 , " Gold coin " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLine(nDocID,0,"- Магический взлом -");
+		Doc_PrintLine(nDocID, 0 , " - Magic hack - " );
 		Doc_PrintLine(nDocID,0,"Отымчка");
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLine(nDocID,0,"- Исцеление -");
-		Doc_PrintLine(nDocID,0,"Лечебный корень");
+		Doc_PrintLine(nDocID, 0 , " - Healing - " );
+		Doc_PrintLine(nDocID, 0 , " Medicinal root " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLine(nDocID,0,"Круг II");
+		Doc_PrintLine(nDocID, 0 , " Circle II " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLine(nDocID,0,"- Вызвать варга -");
-		Doc_PrintLine(nDocID,0,"Шкура варга");
+		Doc_PrintLine(nDocID, 0 , " - Call warg - " );
+		Doc_PrintLine(nDocID, 0 , " Warg Skin " );
 		Doc_PrintLine(nDocID,0,"");
-		Doc_PrintLine(nDocID,0,"- Очищающий свет -");
-		Doc_PrintLine(nDocID,0,"Противоядие");
-		Doc_PrintLine(nDocID,0,"Святая вода");
+		Doc_PrintLine(nDocID, 0 , " - Cleansing Light - " );
+		Doc_PrintLine(nDocID, 0 , " Antidote " );
+		Doc_PrintLine(nDocID, 0 , " Holy Water " );
 		Doc_PrintLine(nDocID,0,"");
 		Doc_SetMargins(nDocID,-1,30,20,275,20,1);
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLine(nDocID,1,"- Уничтожение нежити -");
-		Doc_PrintLine(nDocID,1,"Святая вода");
+		Doc_PrintLine(nDocID, 1 , " - Destruction of undead - " );
+		Doc_PrintLine(nDocID, 1 , " Holy Water " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLine(nDocID,1,"Круг III");
+		Doc_PrintLine(nDocID, 1 , " Круг III " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLine(nDocID,1,"- Прилив бодрости -");
-		Doc_PrintLine(nDocID,1,"Серафис");
-		Doc_PrintLine(nDocID,1,"Ледяной кварц");
-		Doc_PrintLine(nDocID,1,"Болотник");
+		Doc_PrintLine(nDocID, 1 , " - Cheerfulness - " );
+		Doc_PrintLine(nDocID, 1 , " Seraphis " );
+		Doc_PrintLine(nDocID, 1 , " Ice Quartz " );
+		Doc_PrintLine(nDocID, 1 , " Swamp " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLine(nDocID,1,"- Призыв каменного голема -");
-		Doc_PrintLine(nDocID,1,"Сердце каменного голема");
+		Doc_PrintLine(nDocID, 1 , " - Summon stone golem - " );
+		Doc_PrintLine(nDocID, 1 , " Stone Golem Heart " );
 		Doc_PrintLine(nDocID,1,"");
-		Doc_PrintLines(nDocID,1,"Для изготовления руны необходимы ВСЕ указанные ингредиенты.");
+		Doc_PrintLines(nDocID, 1 , " ALL of the listed ingredients are required to make a rune. " );
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
 		Doc_PrintLine(nDocID,1,"");
@@ -68,7 +69,7 @@ func void use_runemaking_prm_s1()
 		}
 		else
 		{
-			rnd = Hlp_Random(100);
+			rnd = Hlp_Random( 100 );
 			if(rnd <= 30)
 			{
 				B_Say(self,self,"$NOTHINGNEW");
@@ -86,8 +87,8 @@ func void use_runemaking_prm_s1()
 		{
 			ATR_INTELLECT += 1;
 			Npc_SetTalentSkill(self,NPC_TALENT_INTELLECT,ATR_INTELLECT);
-			AI_Print("Интеллект + 1");
-			BookBonus_143 = TRUE;
+			AI_Print( " Intelligence + 1 " );
+			BookBonus_143 = TRUE ;
 		};
 	};
 };
