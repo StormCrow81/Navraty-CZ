abfb63838b90067d945e485431dddaa520475b89
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2022-12-26 13:34:36 +0200

    Update DIA_dmt_1212_dagot.d

--- RU-Main/Story/Dialoge/DIA_dmt_1212_dagot.d
+++ RU-Main/Story/Dialoge/DIA_dmt_1212_dagot.d
@@ -42,7 +42,7 @@ func void dmt_1212_dagot_hello_info()
 	B_GivePlayerXP(100);
 	AI_Output (self, other, " DMT_1212_Dagot_Hello_01 " );	// I understand that it was you who expelled the Sleeper from this world?
 	AI_Output (other, self, " DMT_1212_Dagot_Hello_02 " );	// Yes, it was me.
-	AI_Output (self, other, " DMT_1212_Dagot_Hello_03 " );	// Xardas told a lot about you. Few of the heroes of this world have succeeded in such a thing!
+	AI_Output (self, other, " DMT_1212_Dagot_Hello_03 " );	// Xardas has told me a lot about you. Few of the heroes of this world have succeeded in such a thing!
 	AI_Output (other, self, " DMT_1212_Dagot_Hello_04 " );	// Who are you? And what are you doing here, in the tower of Xardas?
 	AI_Output (self, other, " DMT_1212_Dagot_Hello_05 " );	// I am Dagoth, one of the nine Guardians of the sacred Halls of Bacchan.
 	AI_Output (other, self, " DMT_1212_Dagot_Hello_07 " );	// Are you also a necromancer and demon summoner like Xardas?
@@ -53,7 +53,7 @@ func void dmt_1212_dagot_hello_info()
 	AI_Output (self, other, " DMT_1212_Dagot_Hello_13 " );	// The Path of the Guardian. Its meaning cannot be understood and it cannot be defined in words. This is your essence, your inner world, your destiny!
 	AI_Output (self, other, " DMT_1212_Dagot_Hello_14 " );	// Each living creature has its own path predetermined, which it will follow throughout its life - from birth to death.
 	AI_Output (other, self, " DMT_1212_Dagot_Hello_15 " );	// What's my path?
-	AI_Output (self, other, " DMT_1212_Dagot_Hello_16 " );	// This shouldn't worry you right now. As well as the reason for my stay here.
+	AI_Output (self, other, " DMT_1212_Dagot_Hello_16 " );	// That shouldn't worry you right now. As well as the reason for my stay here.
 	AI_Output (other, self, " DMT_1212_Dagot_Hello_18 " );	// What deity do the Guardians serve? I very much doubt that those with the power of black magic are serving the gods.
 	AI_Output (self, other, " DMT_1212_Dagot_Hello_19 " );	// Keepers don't serve anyone. Their goal is to keep the balance of this world from the destructive actions of Beliar, Innos and Adanos!
 	AI_Output (other, self, " DMT_1212_Dagot_Hello_20 " );	// Adanos?! Isn't his very reason for existence the goal of maintaining balance between his brothers?
@@ -69,7 +69,7 @@ func void dmt_1212_dagot_hello_info()
 	AI_Output (other, self, " DMT_1212_Dagot_Hello_31 " );	// So you're saying that I can become a Guardian too?
 	AI_Output (self, other, " DMT_1212_Dagot_Hello_32 " );	// Possibly, but it's too early to talk about it now.
 	AI_Output (other, self, " DMT_1212_Dagot_Hello_33 " );	// What are the Halls of Bacchan? I've never heard of them.
-	AI_Output (self, other, " DMT_1212_Dagot_Hello_34 " );	// Sacred Halls of Bacchan ... The ancients called them the 'Halls of Chaos'. This is a place where there is nothing but emptiness ...
+	AI_Output (self, other, " DMT_1212_Dagot_Hello_34 " );	// The Sacred Halls of Bacchan ... The ancients called them the 'Halls of Chaos'. This is a place where there is nothing but emptiness ...
 	AI_Output (self, other, " DMT_1212_Dagot_Hello_35 " );	// ... and a force capable of forever changing the face of this world!
 	AI_Output (self, other, " DMT_1212_Dagot_Hello_36 " );	// And even powerful deities such as Innos, Beliar or Adanos will not be able to withstand this force if it comes into this world.
 	AI_Output (self, other, " DMT_1212_Dagot_Hello_37 " );	// And we - the Guardians, the guardians of these Halls - are called to hide this place and the secret that it has kept for millennia.
@@ -110,7 +110,7 @@ func void dmt_1212_dagot_hello_no()
 	AI_Output (other, self, " DMT_1212_Dagot_Hello_50 " );	// No, this is all too complicated for me.
 	Wld_PlayEffect("FX_EarthQuake",self,self,0,0,0,FALSE);
 	AI_Output (self, other, " DMT_1212_Dagot_Hello_51 " );	// Your decision is inscribed in the great Book of Destiny! You may not even realize what you have done now.
-	AI_Output (self, other, " DMT_1212_Dagot_Hello_52 " );	// Your answer disappointed me greatly. But this is exactly how it should have been.
+	AI_Output (self, other, " DMT_1212_Dagot_Hello_52 " );	// Your answer disappoints me greatly. But this is exactly how it should have been.
 	AI_Output (self, other, " DMT_1212_Dagot_Hello_53 " );	// We will never meet again. Goodbye!
 	GUARDIAN_WAY = FALSE;
 	MIS_GUARDIANS = LOG_Running;
@@ -172,7 +172,7 @@ func void dmt_1212_dagot_hellotwo_info()
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_04 " );	// Don't be surprised by this. Our meeting was already predetermined at the moment when you accepted my offer.
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_05 " );	// Sooner, later. It does not matter. All that matters is the fact that it happened and you are here.
 	AI_Output (other, self, " DMT_1212_Dagot_HelloTwo_06 " );	// Have you been waiting for me? What for?
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_07 " );	// My presence here, like our meeting, means that it's time for you to learn more about us - the Guardians ...
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_07 " );	// My presence here, like our first meeting, means that it's time for you to learn more about us - the Guardians ...
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_08 " );	// ... and take your first step towards your destiny.
 	AI_Output(self,other,"DMT_1212_Dagot_HelloTwo_09");	//Are you ready?
 	AI_Output(other,self,"DMT_1212_Dagot_HelloTwo_10");	//Yes, master.
@@ -181,28 +181,28 @@ func void dmt_1212_dagot_hellotwo_info()
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_13 " );	// There was nothing in this world - one endless emptiness. In this chaos, four elements were born - Earth, Water, Stone and Fire.
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_14 " );	// But in the chaos, these elements carried only destruction. And there was no rest for them in their eternal struggle among themselves.
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_15 " );	// And all this would lead to one fact: that they would destroy themselves and sink into oblivion ...
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_16 " );	// And they understood: in a reality ruled by Chaos, they would have no place. Only by getting rid of it, could they find peace.
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_16 " );	// And they understood: in a reality ruled by Chaos, they would have no place. Only by breaking free of it, could they find peace.
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_17 " );	// Then, having come to a fretful harmony with each other, the elements were able to expel the Great Chaos and imprison it in the place where it was generated ...
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_18 " );	// In the Halls of Bacchan ... Forever hiding it from this world behind the sacred gates, imposing on them the great seal of eternity.
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_19 " );	// And so that no one else could open these gates, each element gave birth to its own deity ...
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_20 " );	// So four brother deities came to this world ...
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_21 " );	// Innos, born of flaming Fire, was the first of the brothers ...
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_22 " );	// The second was Beliar, who was spawned by the earthly Darkness ...
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_23 " );	// Adanos became a deity, which was spawned by the Surface of Water ...
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_24 " );	// And the fourth of the brothers was Stonnos - the deity who gave birth to the solid Rock ...
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_25 " );	// And these deities kept the secret of the seal and what is hidden by it.
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_20 " );	// So four fraternal deities came to this world ...
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_21 " );	// Innos, born of flaming Fire and the light it creates, was the first of the brothers...
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_22 " );	// The second was Beliar, emerged from the darkness that comes into being where light fades and cold seeps in...
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_23 " );	// Adanos, spawned from the surface of the very first waters...
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_24 " );	// And the fourth of the brothers was Stonnos - the deity who gave birth to the solid Rock...
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_25 " );	// And these deities kept the secret of the seal and what is hidden behind it.
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_26 " );	// But the brothers could not live in harmony for long, since each considered himself the One True God, not accepting the balance of the elements.
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_27 " );	// And it so happened that Beliar, having argued with his brother Stonnos, killed him in desperate rage ...
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_28 " );	// And there were three of them ... That is why the name of the fourth deity has been forgotten by the tongues of mortals.
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_27 " );	// And it so happened that Beliar, having feuded with his brother Stonnos, killed him in desperate rage, becoming the first murderer ...
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_28 " );	// And then there were three of them ... That is why the name of the fourth deity has been forgotten by the tongues of mortals.
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_29 " );	// Innos, the first of the brothers, condemned Beliar's actions, and since then the brothers have hated each other ...
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_30 " );	// Adanos did not take either the side of Innos, or the side of Beliar, but stood between them, identifying himself with the wise middle in their confrontation.
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_30 " );	// Adanos did not take either the side of Innos, or the side of Beliar, but stood between them, identifying himself with the wisdom of neutrality in their confrontation.
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_31 " );	// And the brothers forgot why they were sent to this world. And the more their struggle flared up among themselves ...
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_32 " );	// ... the more the great seal of eternity, which guarded against true evil, weakened.
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_33 " );	// And there was a danger that Chaos will again take possession of this world and plunge everyone into the void of nothingness.
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_34 " );	// Then the primal elements that gave birth to these deities, and no longer wanted to look at their enmity and feared the appearance of Chaos in this world ...
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_35 " );	// ...told each deity to create three Guardians from a piece of themselves ...
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_33 " );	// And there was a danger that Chaos will again take possession of this world and plunge all life into the void of nothingness.
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_34 " );	// Then the primal elements that gave birth to these deities no longer wanted to behold at their enmity and feared the appearance of Chaos in this world ...
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_35 " );	// ...They forced the three deities to each create a guardian from a piece of themselves ...
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_36 " );	// ... so that those endowed with the strength and wisdom of their creators could take guardianship of the Halls of Bacchan.
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_37 " );	// The element itself created one guard, endowing him with the power of three and by the consent of the other elements ...
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_37 " );	// The elements themselves created one guard, endowing him with the power of the three and by the consent of the other elements ...
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_38 " );	// ... so that there was no more disagreement among the guards, as well as between their creators, and put him in charge of all the others.
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_39 " );	// And so there were nine Guardians of the sacred Circle of Equilibrium and Balance, and the main Guardian, the lord of Bacchan and the lord of the four elements ...
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_40 " );	// ... and to this day, the Guardians stand guard over the sacred Halls.
@@ -223,7 +223,7 @@ func void dmt_1212_dagot_hellotwo_info()
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_55 " );	// This is the wisdom of the path of the Guardians, which you are now a part of ...
 	AI_Output (other, self, " DMT_1212_Dagot_HelloTwo_56 " );	// But how can I become a Guardian like you?
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_57 " );	// Naturally, you cannot become exactly like us, since our origin is divine.
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_58 " );	// But, like all deities in this world, we, the Guardians, also have followers from the dead, who embody our thoughts and will.
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_58 " );	// But, like all deities in this world, we, the Guardians, also have followers from the realms of the dead, who embody our thoughts and will.
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_59 " );	// Like the paladins of Innos and the Fire mages, like the water mages of Adanos and his ring, like the servants of Beliar and all the creatures of Darkness - our warriors bear the name of the sacred Circle of Guardians ...
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_60 " );	// ... on our lips and our faith in our hearts!
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_61 " );	// The Asgardians are warriors of the elements, that is how they bear the name. From time immemorial, they stood guard over the Guardians and their great secret.
@@ -239,7 +239,7 @@ func void dmt_1212_dagot_hellotwo_info()
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_72 " );	// I will be the first Guardian whose test you must pass.
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_73 " );	// And, having received my consent and approval, you can meet with the next of us.
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_74 " );	// Having enlisted the support of all the Guardians, you will be honored to meet with the head of our Circle - the main Guardian, the ruler of Bacchan.
-	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_75 " );	// He will explain to you what your Test of Faith is ... But now you are still very far from that ...
+	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_75 " );	// He will explain to you what your Test of Faith is ... But for now you are still very far from that ...
 	AI_Output(other,self,"DMT_1212_Dagot_HelloTwo_76");	//I understand, master.
 	AI_Output (self, other, " DMT_1212_Dagot_HelloTwo_77 " );	// So are you ready to take my test?
 	MIS_GUARDIANSTEST = LOG_Running;
@@ -247,7 +247,7 @@ func void dmt_1212_dagot_hellotwo_info()
 	Log_SetTopicStatus(TOPIC_GUARDIANSTEST,LOG_Running);
 	B_LogEntry ( TOPIC_GUARDIANSTEST , " In order to become a warrior of the elements, I must meet the Guardians' Circle. Each of them will test my strength and perseverance. At the end, there will be a Test of Faith, which will be offered by the Chief Guardian. " );
 	Log_AddEntry ( TOPIC_GUARDIANSTEST , " Only after completing the challenge of one of the Guardians will I be able to appear before the next one. " );
-	Log_AddEntry ( TOPIC_GUARDIANS , " In one of the caves, I crossed paths with the Guardian again. Dagoth told me the legends of the past. I learned about the beginning of the world, about the gods, about the struggle of the gods, about the hatred of the gods, about the Guardians. About the sacred Circle of balance and balance. " );
+	Log_AddEntry ( TOPIC_GUARDIANS , " In one of the caves, I crossed paths with the Guardian again. Dagoth told me the legends of the past. I learned about the beginning of the world, about the gods, about the struggles of the gods, about the hatred of the gods, about the Guardians. About the sacred Circle of balance. " );
 	Log_AddEntry ( TOPIC_GUARDIANS , " Dagoth told me that no mortal can become a Guardian, and that my path is the path of the warrior of the elements, Asgard. This is the goal I will come to at the end of my path. " );
 	Info_ClearChoices(dmt_1212_dagot_hellotwo);
 	Info_AddChoice (dmt_1212_dagot_hellotwo, " Yes, master. I'm ready. " , Dmt_1212_dagot_hellotwo_test);
