533851e963ef752db09d877bec002656f37b1d3f
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2023-01-06 21:51:44 +0200

    Update DIA_vlk_6108_salentin.d

--- RU-Main/Story/Dialoge/DIA_vlk_6108_salentin.d
+++ RU-Main/Story/Dialoge/DIA_vlk_6108_salentin.d
@@ -41,10 +41,10 @@ func int dia_salentin_hallo_condition()
 func void dia_salentin_hallo_info()
 {
 	AI_Output(self,other, " DIA_Salentin_Hallo_01_00 " );	// (surprised) Hey! What are you doing here?! How did you even get here?
-	AI_Output(self,other, " DIA_Salentin_Hallo_01_01 " );	// If paladins notice, they'll kill you!
+	AI_Output(self,other, " DIA_Salentin_Hallo_01_01 " );	// If the paladins notice, they'll kill you!
 	AI_Output(other,self, " DIA_Salentin_Hallo_01_02 " );	// Don't worry, I'll deal with them somehow.
-	AI_Output(self,other, " DIA_Salentin_Hallo_01_03 " );	// WHAT?! And they won't listen to you! Do you have any idea what this place is?
-	AI_Output(self,other, " DIA_Salentin_Hallo_01_04 " );	// (sympathetically) Oh man, my advice to you is better get out of here before it's too late.
+	AI_Output(self,other, " DIA_Salentin_Hallo_01_03 " );	// WHAT?! They won't listen to you! Do you have any idea what this place is?
+	AI_Output(self,other, " DIA_Salentin_Hallo_01_04 " );	// Oh man, my advice to you is better get out of here before it's too late.
 	BEONETLU = TRUE;
 	AI_StopProcessInfos(self);
 };
@@ -72,13 +72,13 @@ func void dia_salentin_bauernaufstand_info()
 	AI_Output(self,other, " DIA_Salentin_Bauernaufstand_01_01 " );	// What?! Are you still here?
 	AI_Output(other,self, " DIA_Salentin_Bauernaufstand_01_03 " );	// Don't worry like that. I'm here on business.
 	AI_Output(self,other, " DIA_Salentin_Bauernaufstand_01_04 " );	// I wonder what business could bring a man like you here?
-	AI_Output(other,self, " DIA_Salentin_Bauernaufstand_01_05 " );	// I'm looking for one thing. More precisely, one amulet - Triramar. Do you know anything about him?
-	AI_Output(self,other, " DIA_Salentin_Bauernaufstand_01_06 " );	// What other Triramar? What is this?
-	AI_Output(other,self, " DIA_Salentin_Bauernaufstand_01_08 " );	// I told you - an amulet, and I really need it, I won't leave here without it.
+	AI_Output(other,self, " DIA_Salentin_Bauernaufstand_01_05 " );	// I'm looking for something. More precisely, an amulet - Triramar. Do you know anything about him?
+	AI_Output(self,other, " DIA_Salentin_Bauernaufstand_01_06 " );	// Triramar? What's that?
+	AI_Output(other,self, " DIA_Salentin_Bauernaufstand_01_08 " );	// I told you - an amulet. I really need it and I won't leave here without it.
 	AI_Output(self,other, " DIA_Salentin_Bauernaufstand_01_09 " );	// (laughs) You're not going anywhere from here anyway!
-	AI_Output(self,other, " DIA_Salentin_Bauernaufstand_01_10 " );	// The only way out of this island is through the magical portal behind the chapel to the north, but it's guarded by paladins.
+	AI_Output(self,other, " DIA_Salentin_Bauernaufstand_01_10 " );	// The only way off this island is through the magical portal behind the chapel to the north, but it's guarded by paladins.
 	AI_Output(self,other, " DIA_Salentin_Bauernaufstand_01_11 " );	// And I don't think they'll let you out of here alive.
-	AI_Output(self,other, " DIA_Salentin_Bauernaufstand_01_12 " );	// Yes, boy, you've got yourself in trouble!
+	AI_Output(self,other, " DIA_Salentin_Bauernaufstand_01_12 " );	// You, my friend, are in trouble.
 };
 
 
@@ -103,16 +103,16 @@ func int dia_salentin_triramar_condition()
 
 func void dia_salentin_triramar_info()
 {
-	AI_Output(other,self, " DIA_Salentin_Triramar_01_00 " );	// Chapel, you say? Can't an amulet be kept there?
-	AI_Output(self,other, " DIA_Salentin_Triramar_01_01 " );	// I have no idea what is stored there. I do not approach her even for the flight of an arrow, my life is dear to me.
-	AI_Output(other,self, " DIA_Salentin_Triramar_01_03 " );	// Stop scaring me! Just tell me - where is this chapel?
+	AI_Output(other,self, " DIA_Salentin_Triramar_01_00 " );	// Chapel, you say? Could the amulet be kept there?
+	AI_Output(self,other, " DIA_Salentin_Triramar_01_01 " );	// I have no idea what is stored there. I do not approach the place, ever.
+	AI_Output(other,self, " DIA_Salentin_Triramar_01_03 " );	// Just tell me - where is this chapel?
 	AI_Output(self,other, " DIA_Salentin_Triramar_01_04 " );	// Crazy, definitely crazy.
 	AI_Output(self,other, " DIA_Salentin_Triramar_01_06 " );	// The chapel is north of my hut. And since you're going there, I'll ask you for one favor...
-	AI_Output(self,other, " DIA_Salentin_Triramar_01_07 " );	// ...make sure you don't get killed too close to her - I could use your gear. (with sarcasm)
-	AI_Output(other,self, " DIA_Salentin_Triramar_01_08 " );	// (sourly) Yes, you're a real joker, as I see it. Tell me, are there many paladins in the chapel?
+	AI_Output(self,other, " DIA_Salentin_Triramar_01_07 " );	// ...make sure you don't get killed too close to it - I could use your gear.
+	AI_Output(other,self, " DIA_Salentin_Triramar_01_08 " );	// Funny. Tell me, are there many paladins in the chapel?
 	AI_Output(self,other, " DIA_Salentin_Triramar_01_11 " );	// No, the main part, together with the command, lodges in a camp in the east of the island.
 	AI_Output(other,self, " DIA_Salentin_Triramar_01_12 " );	// Then I'll visit them in the camp.
-	AI_Output(self,other, " DIA_Salentin_Triramar_01_13 " );	// WHAT?! (choking) Oh, yes, yes, come on, visit ... the next world.
+	AI_Output(self,other, " DIA_Salentin_Triramar_01_13 " );	// WHAT?! You really are suicidal!
 };
 
 
@@ -138,9 +138,9 @@ func int dia_salentin_getout_condition()
 func void dia_salentin_getout_info()
 {
 	AI_Output(other,self, " DIA_Salentin_GetOut_01_00 " );	// How else can you get off this island?
-	AI_Output(self,other, " DIA_Salentin_GetOut_02_01 " );	// None. I wish I had been stuck here all this time otherwise!
-	AI_Output(self,other, " DIA_Salentin_GetOut_04_03 " );	// I already have this island in my liver.
-	AI_Output(self,other, " DIA_Salentin_GetOut_05_04 " );	// Paladins, thank Innos, don't touch me, but they don't let me near the portal either.
+	AI_Output(self,other, " DIA_Salentin_GetOut_02_01 " );	// None!
+	AI_Output(self,other, " DIA_Salentin_GetOut_04_03 " );	// I have this island in my blood at this point.
+	AI_Output(self,other, " DIA_Salentin_GetOut_05_04 " );	// The Paladins, thank Innos, don't touch me, but they don't let me near the portal either.
 };
 
 
@@ -168,13 +168,13 @@ func void dia_salentin_howin_info()
 	AI_Output(other,self, " DIA_Salentin_HowIn_01_00 " );	// How did you end up here?
 	AI_Output(self,other, " DIA_Salentin_HowIn_01_04 " );	// Once I was a sailor and served on one of the merchant ships. We were transporting magical ore from a godforsaken colony...
 	AI_Output(self,other, " DIA_Salentin_HowIn_01_05 " );	// One day, while returning to the mainland, our ship was caught in a terrible storm. The ship did not survive it - it broke in two. And I was swept away to the open sea.
-	AI_Output(self,other, " DIA_Salentin_HowIn_01_09 " );	// I woke up already on the island, and a bunch of paladins crowded around me.
+	AI_Output(self,other, " DIA_Salentin_HowIn_01_09 " );	// I woke up on the island, and a bunch of paladins crowded around me.
 	AI_Output(other,self, " DIA_Salentin_HowIn_01_10 " );	// Why did they let you live?
 	AI_Output(self,other, " DIA_Salentin_HowIn_01_11 " );	// Well, paladins aren't beasts. Moreover, I ended up on their island against my will.
 	AI_Output(self,other, " DIA_Salentin_HowIn_01_13 " );	// They let me stay and live on this island. They gave me food, new clothes and even tools!
 	AI_Output(other,self, " DIA_Salentin_HowIn_01_14 " );	// Why didn't you stay with them?
 	AI_Output(self,other, " DIA_Salentin_HowIn_01_15 " );	// Boy, this is a military camp, not an inn.
-	AI_Output(self,other, " DIA_Salentin_HowIn_01_23 " );	// I try to avoid being seen by them. And I advise you to do the same!
+	AI_Output(self,other, " DIA_Salentin_HowIn_01_23 " );	// I try to avoid being around them. And I advise you to do the same!
 };
 
 
@@ -199,12 +199,12 @@ func int dia_salentin_canpass_condition()
 
 func void dia_salentin_canpass_info()
 {
-	AI_Output(other,self, " DIA_Salentin_CanPass_01_00 " );	// Do you visit the paladin camp yourself?
+	AI_Output(other,self, " DIA_Salentin_CanPass_01_00 " );	// Do you ever visit the paladin camp?
 	AI_Output(self,other, " DIA_Salentin_CanPass_01_01 " );	// Yes, a couple of times a month I bring them fish that I manage to catch.
-	AI_Output(self,other, " DIA_Salentin_CanPass_01_02 " );	// And in return they give me meat and water. So I don't have to go hungry here.
+	AI_Output(self,other, " DIA_Salentin_CanPass_01_02 " );	// And in return they give me meat and water. So at least my diet is somewhat varied.
 	AI_Output(other,self, " DIA_Salentin_CanPass_01_03 " );	// So they trust you if they let you into the camp?
 	AI_Output(self,other, " DIA_Salentin_CanPass_01_04 " );	// Not that they trust me, but I don't pose the slightest danger to them, so they tolerate me.
-	AI_Output(self,other, " DIA_Salentin_CanPass_01_05 " );	// Well, I, accordingly, try not to anger them. Why are you asking?
+	AI_Output(self,other, " DIA_Salentin_CanPass_01_05 " );	// I also try never to anger them. Why are you asking?
 	TELLSALENTINLAGER = TRUE;
 	Info_ClearChoices(dia_salentin_canpass);
 	if(ZIGOSMAGICISON == TRUE)
@@ -217,7 +217,7 @@ func void dia_salentin_canpass_info()
 func void dia_salentin_canpass_no()
 {
 	AI_Output(other,self, " DIA_Salentin_CanPass_No_01_00 " );	// Just wondering.
-	AI_Output(self,other,"DIA_Salentin_CanPass_No_01_01");	//Хммм...
+	AI_Output(self,other,"DIA_Salentin_CanPass_No_01_01");	//Hmmm...
 	Info_ClearChoices(dia_salentin_canpass);
 };
 
@@ -225,11 +225,11 @@ func void dia_salentin_canpass_yes()
 {
 	AI_Output(other,self, " DIA_Salentin_CanPass_Yes_01_00 " );	// I think I'll need you.
 	AI_Output(other,self, " DIA_Salentin_CanPass_Yes_01_01 " );	// Since the paladins trust you, I'll use you to infiltrate their camp.
-	AI_Output(self,other, " DIA_Salentin_CanPass_Yes_01_02 " );	// (cautiously) You mean? What did you think?
+	AI_Output(self,other, " DIA_Salentin_CanPass_Yes_01_02 " );	// Are you serious?
 	Info_ClearChoices(dia_salentin_canpass);
 	if(ZIGOSMAGICISON == TRUE)
 	{
-		Info_AddChoice(dia_salentin_canpass, " ...ГВАР САХ ЭСК ШАТАР...! " ,dia_salentin_canpass_use);
+		Info_AddChoice(dia_salentin_canpass, " ...GVAR SAKH ESK SHATAR...! " ,dia_salentin_canpass_use);
 	};
 };
 
@@ -241,11 +241,11 @@ func void dia_salentin_canpass_use()
 	Wld_PlayEffect("spellFX_Innoseye",self,self,0,0,0,FALSE);
 	AI_PlayAni(other,"T_MAGRUN_2_HEASHOOT");
 	AI_Output(other,self, " DIA_Salentin_CanPass_Use_01_00 " );	// ...GVAR SAKH ESK SHATAR...!
-	AI_Output(other,self, " DIA_Salentin_CanPass_Use_01_01 " );	// Beliar, Lord of Darkness! Grant me your power to take on the form of this person.
+	AI_Output(other,self, " DIA_Salentin_CanPass_Use_01_01 " );	// Beliar, Lord of Darkness! Grant me your power to take on the form of this mortal worm!
 	AI_Output(other,self, " DIA_Salentin_CanPass_Use_01_02 " );	// And complete what I'm here for!
 	AI_PlayAni(other,"T_MAGRUN_2_HEASHOOT");
 	AI_Output(self,other, " DIA_Salentin_CanPass_Use_01_03 " );	// O Innos! Who are you? Do you serve Beliar?!
-	AI_Output(self,other, " DIA_Salentin_CanPass_Use_01_04 " );	// And what is this magic?! Oh my god, what's happening to me! Nooo...
+	AI_Output(self,other, " DIA_Salentin_CanPass_Use_01_04 " );	// And what is this magic?! Oh my gods, what's happening to me! Nooo...
 	USESALENTINAMULET = TRUE;
 	Info_ClearChoices(dia_salentin_canpass);
 	if(ZIGOSMAGICISON == TRUE)
@@ -284,7 +284,7 @@ func int dia_salentin_canpassagain_condition()
 func void dia_salentin_canpassagain_info()
 {
 	AI_Output(other,self, " DIA_Salentin_CanPassAgain_01_00 " );	// I have business with you.
-	AI_Output(self,other, " DIA_Salentin_CanPassAgain_01_01 " );	// Which one exactly?
+	AI_Output(self,other, " DIA_Salentin_CanPassAgain_01_01 " );	// What exactly?
 	Info_ClearChoices(dia_salentin_canpassagain);
 	if(ZIGOSMAGICISON == TRUE)
 	{
@@ -304,7 +304,7 @@ func void dia_salentin_canpassagain_yes()
 {
 	AI_Output(other,self, " DIA_Salentin_CanPass_Yes_01_00 " );	// I think I'll need you.
 	AI_Output(other,self, " DIA_Salentin_CanPass_Yes_01_01 " );	// Since the paladins trust you, I'll use you to infiltrate their camp.
-	AI_Output(self,other, " DIA_Salentin_CanPass_Yes_01_02 " );	// (cautiously) You mean? What did you think?
+	AI_Output(self,other, " DIA_Salentin_CanPass_Yes_01_02 " );	// Are you serious?
 	Info_ClearChoices(dia_salentin_canpassagain);
 	if(ZIGOSMAGICISON == TRUE)
 	{
@@ -320,11 +320,11 @@ func void dia_salentin_canpassagain_use()
 	Wld_PlayEffect("spellFX_Innoseye",self,self,0,0,0,FALSE);
 	AI_PlayAni(other,"T_MAGRUN_2_HEASHOOT");
 	AI_Output(other,self, " DIA_Salentin_CanPass_Use_01_00 " );	// ...GVAR SAKH ESK SHATAR...!
-	AI_Output(other,self, " DIA_Salentin_CanPass_Use_01_01 " );	// Beliar, Lord of Darkness! Grant me your power to take on the form of this person.
+	AI_Output(other,self, " DIA_Salentin_CanPass_Use_01_01 " );	// Beliar, Lord of Darkness! Grant me your power to take on the form of this mortal worm!
 	AI_Output(other,self, " DIA_Salentin_CanPass_Use_01_02 " );	// And complete what I'm here for!
 	AI_PlayAni(other,"T_MAGRUN_2_HEASHOOT");
 	AI_Output(self,other, " DIA_Salentin_CanPass_Use_01_03 " );	// O Innos! Who are you? Do you serve Beliar?!
-	AI_Output(self,other, " DIA_Salentin_CanPass_Use_01_04 " );	// And what is this magic?! Oh my god, what's happening to me! Nooo...
+	AI_Output(self,other, " DIA_Salentin_CanPass_Use_01_04 " );	// And what is this magic?! Oh my gods, what's happening to me! Nooo...
 	USESALENTINAMULET = TRUE;
 	Info_ClearChoices(dia_salentin_canpassagain);
 	if(ZIGOSMAGICISON == TRUE)
@@ -363,10 +363,10 @@ func int dia_salentin_advice_condition()
 func void dia_salentin_advice_info()
 {
 	AI_Output(self,other, " DIA_Salentin_Advice_01_00 " );	// Hey, wait!
-	AI_Output(other,self, " DIA_Salentin_Advice_01_01 " );	// What else?
+	AI_Output(other,self, " DIA_Salentin_Advice_01_01 " );	// What?
 	AI_Output(self,other, " DIA_Salentin_Advice_01_02 " );	// If you're serious about visiting the paladin camp...
 	AI_Output(self,other, " DIA_Salentin_Advice_01_04 " );	// ...don't go there during the day - you won't have a single chance to do what you have in mind.
 	AI_Output(self,other, " DIA_Salentin_Advice_01_05 " );	// The best time is at night. The camp will sleep, - well, maybe, except for a couple of people.
 	AI_Output(other,self, " DIA_Salentin_Advice_01_07 " );	// How do you know all this?
-	AI_Output(self,other, " DIA_Salentin_Advice_01_08 " );	// What's the difference, where? Just remember what I told you.
+	AI_Output(self,other, " DIA_Salentin_Advice_01_08 " );	// What's it matter? Just remember what I told you.
 };
