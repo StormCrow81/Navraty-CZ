0dcd53101775bcdd10a1a3de1e3f9ea261da28b4
StormCrow81 <96226383+StormCrow81@users.noreply.github.com>
2023-01-07 18:52:28 +0200

    Update DIA_vlk_6021_fisk.d

--- RU-Main/Story/Dialoge/DIA_vlk_6021_fisk.d
+++ RU-Main/Story/Dialoge/DIA_vlk_6021_fisk.d
@@ -77,18 +77,18 @@ func void dia_fish_nw_hi_info()
 {
 	B_GivePlayerXP(500);
 	AI_Output(other,self, " DIA_Fisk_NW_Hi_01_00 " );	// I can't believe my eyes! Fisk, how did you end up here?
-	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_01 " );	// Hmm... Strange question for someone who somehow managed to get here.
-	AI_Output(other,self, " DIA_Fisk_NW_Hi_01_03 " );	// And how did you manage to get past the guard?!
-	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_04 " );	// You understand: business people can always come to an agreement. I made them an offer they couldn't refuse...(laughs)
-	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_05 " );	// And I myself look, you are also not a small mistake.
+	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_01 " );	// I could ask you the same.
+	AI_Output(other,self, " DIA_Fisk_NW_Hi_01_03 " );	// How did you manage to get past the guard?!
+	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_04 " );	// You see: business people can always come to an agreement. I made them an offer they couldn't refuse...(laughs)
+	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_05 " );	// And I'm a businessman.
 
 	if(other.guild == GIL_KDF)
 	{
-		AI_Output(self,other, " DIA_Fisk_NW_Hi_01_06 " );	// To be honest, this is the first time I've seen a bandit who managed to ingratiate himself with Firebenders. Yes, and become one!
+		AI_Output(self,other, " DIA_Fisk_NW_Hi_01_06 " );	// To be honest, this is the first time I've seen a bandit who managed to ingratiate himself with Firebenders, much less become one!
 	}
 	else if(other.guild == GIL_PAL)
 	{
-		AI_Output(self,other, " DIA_Fisk_NW_Hi_01_07 " );	// Looks like the paladins have completely lost their minds if they started to take fugitive convicts into their service!
+		AI_Output(self,other, " DIA_Fisk_NW_Hi_01_07 " );	// Looks like the paladins have completely lost their minds if they started to take crooks like you into their service!
 	}
 	else if(other.guild == GIL_DJG)
 	{
@@ -96,27 +96,27 @@ func void dia_fish_nw_hi_info()
 	}
 	else if(other.guild == GIL_KDW)
 	{
-		AI_Output(self,other, " DIA_Fisk_NW_Hi_01_0A " );	// To be honest, for the first time I see a bandit who was able to ingratiate himself with the Water Mages. Yes, and become one!
+		AI_Output(self,other, " DIA_Fisk_NW_Hi_01_0A " );	// To be honest, this is the first time I've see a bandit who was able to ingratiate himself with the Water Mages, much less become one!
 	}
 	else if(other.guild == GIL_KDM)
 	{
-		AI_Output(self,other, " DIA_Fisk_NW_Hi_01_0B " );	// To be honest, this is the first time I've seen a bandit who could become a necromancer! Amazing... (surprised)
+		AI_Output(self,other, " DIA_Fisk_NW_Hi_01_0B " );	// A bandit turned Necromancer... seems oddly fitting I suppose.
 	}
 	else if((other.guild == GIL_SEK) || (other.guild == GIL_TPL) || (other.guild == GIL_GUR))
 	{
-		AI_Output(self,other, " DIA_Fisk_NW_Hi_01_0C " );	// Already signed up for the sectarians! Amazing... (surprised)
+		AI_Output(self,other, " DIA_Fisk_NW_Hi_01_0C " );	// Already signed up for the sectarians have you?
 	};
 
-	AI_Output(other,self, " DIA_Fisk_NW_Hi_01_09 " );	// How would you say. All in all, I'm not exactly who I say I am. It's just... There in the swamps, to me... In general, there were things.
-	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_10 " );	// Okay, don't make excuses. In the end, there, in the camp, you helped me a lot. Hand washes hand - remember?
+	AI_Output(other,self, " DIA_Fisk_NW_Hi_01_09 " );	// All in all, I'm not exactly who I say I am. It's just... There in the swamps, to me...
+	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_10 " );	// Okay, don't make excuses. In the end, in the camp, you helped me a lot. Hand washes hand - remember?
 	AI_Output(other,self, " DIA_Fisk_NW_Hi_01_11 " );	// Yeah. I see you're doing well here too!
-	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_12 " );	// During the time that I spent in the camp, I managed to save up a good amount of capital. It was just enough to open his own small business here.
+	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_12 " );	// During the time that I spent in the camp, I managed to save up a good amount of capital. It was just enough to open my own small business here.
 	AI_Output(other,self, " DIA_Fisk_NW_Hi_01_13 " );	// Did the rest of the merchants mind?
-	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_14 " );	// I talked to them. No one seemed to object. But again, it cost me dearly. But I think it pays off.
-	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_15 " );	// Why should they worry? This place, as you can see, stands on the outskirts of the market square. Therefore, I have fewer clients than they do.
+	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_14 " );	// I talked to them. No one seemed to object. But again, it cost me dearly. I hope it pays off.
+	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_15 " );	// Why should they worry? This place stands on the outskirts of the market square. Therefore, I have fewer clients than they do.
 	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_16 " );	// Have to work a little harder than everyone else.
 	AI_Output(other,self, " DIA_Fisk_NW_Hi_01_17 " );	// Can you sell me something?
-	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_18 " );	// Of course, choose.
+	AI_Output(self,other, " DIA_Fisk_NW_Hi_01_18 " );	// Of course.
 };
 
 instance DIA_FISK_NW_TRADE (C_Info);
